d2673d465a794fdb4331dce1b88490fc5423b732
Marijn Haverbeke <marijnh@gmail.com>
2013-06-11 12:43:56 +0200

    Release 0.3.1 to fix release-making-script screwup in 0.3.0

--- acorn.js
+++ acorn.js
@@ -27,7 +27,7 @@
 })(function(exports) {
   "use strict";
 
-  exports.version = "0.3.0";
+  exports.version = "0.3.1";
 
   // The main exported interface (under `self.acorn` when in the
   // browser) is a `parse` function that takes a code string and
