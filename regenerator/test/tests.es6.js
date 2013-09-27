5e1bb3a1e1d9d6326fb55463def2d883d6196f32
Ben Newman <bn@cs.stanford.edu>
2013-09-27 12:41:08 -0400

    Add a test of infinite sequence generators.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -284,3 +284,49 @@ describe("switch statement generator", function() {
     check(gen(2), [2, "x", "y"], "second case");
   });
 });
+
+describe("infinite sequence generator", function() {
+  function *gen(start, step) {
+    step = step || 1;
+    while (true) {
+      yield start;
+      start += step;
+    }
+  }
+
+  function *limit(g, stop) {
+    while (true) {
+      var info = g.next();
+      if (info.done) {
+        return;
+      } else if (info.value < stop) {
+        yield info.value;
+      } else {
+        return;
+      }
+    }
+  }
+
+  it("should generate a lot of plausible values", function() {
+    var g = gen(10, 2);
+
+    assert.deepEqual(g.next(), { value: 10, done: false });
+    assert.deepEqual(g.next(), { value: 12, done: false });
+    assert.deepEqual(g.next(), { value: 14, done: false });
+    assert.deepEqual(g.next(), { value: 16, done: false });
+
+    var sum = 10 + 12 + 14 + 16;
+
+    for (var n = 0; n < 1000; ++n) {
+      var info = g.next();
+      sum += info.value;
+      assert.strictEqual(info.done, false);
+    }
+
+    assert.strictEqual(sum, 1017052);
+  });
+
+  it("should allow limiting", function() {
+    check(limit(gen(10, 3), 20), [10, 13, 16, 19]);
+  });
+});
