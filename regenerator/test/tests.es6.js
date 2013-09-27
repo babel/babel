746c654af5ab63487f04a4cbf9a78a80ea639d83
Ben Newman <bn@cs.stanford.edu>
2013-09-27 11:39:44 -0400

    Add a test of yielding in a for-in loop.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -221,3 +221,25 @@ describe("nested finally blocks", function() {
     check(gen(), [1, "thrown", 2, 3]);
   });
 });
+
+describe("for-in loop generator", function() {
+  function *gen(obj) {
+    var count = 0;
+    for (var key in (yield "why not", obj)) {
+      if (obj.hasOwnProperty(key)) {
+        if (key === "skip") {
+          break;
+        }
+        count += 1;
+        yield [key, obj[key]];
+      }
+    }
+    return count;
+  }
+
+  it("should visit properties until 'skip'", function() {
+    check(
+      gen({ a: 1, b: 2, skip: 3, c: 4 }),
+      ["why not", ["a", 1], ["b", 2]], 2);
+  });
+});
