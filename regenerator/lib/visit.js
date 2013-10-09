6564defff80b722bddea1ceb75efef17f014d08d
Ben Newman <bn@cs.stanford.edu>
2013-10-09 12:31:35 -0400

    Reimplement lib/visit.js using require("ast-types").traverse.

--- regenerator/lib/visit.js
+++ regenerator/lib/visit.js
@@ -8,70 +8,58 @@
  */
 
 var assert = require("assert");
-var recast = require("recast");
+var types = require("ast-types");
+var n = types.namedTypes;
+var b = types.builders;
 var hoist = require("./hoist").hoist;
 var Emitter = require("./emit").Emitter;
-var b = recast.builders;
 
 exports.transform = function(ast) {
-  var visitor = new GenFunVisitor;
-  return visitor.visit(ast);
+  return types.traverse(ast, visitNode);
 };
 
-var GenFunVisitor = recast.Visitor.extend({
-  visitFunctionExpression: function(fexp) {
-    return this._visitGenFun(fexp);
-  },
-
-  visitFunctionDeclaration: function(fdec) {
-    return this._visitGenFun(fdec);
-  },
-
-  _visitGenFun: function(gfun) {
-    this.genericVisit(gfun);
-
-    if (!gfun.generator) {
-      return;
-    }
-
-    gfun.generator = false;
-
-    // TODO Ensure that the context is named uniquely.
-    var contextId = b.identifier("$ctx");
-
-    if (gfun.expression) {
-      // Transform expression lambdas into normal functions.
-      gfun.expression = false;
-      gfun.body = b.blockStatement([
-        b.returnStatement(gfun.body)
-      ]);
-    }
-
-    var vars = hoist(gfun);
-
-    var emitter = new Emitter(contextId);
-    emitter.explode(gfun.body);
-
-    // TODO Need to do something about FunctionDeclarations.
-    // They don't need to be exploded, and it's probably safer to move
-    // them out of the inner function like the hoisted variables.
-    // How about this:
-    // 1. hoist the function names as variables, and then
-    // 2. turn each declaration site into an assignment to the name.
-
-    var outerBody = [];
-
-    if (vars && vars.declarations.length > 0) {
-      outerBody.push(vars);
-    }
-
-    outerBody.push(b.returnStatement(
-      b.callExpression(b.identifier("wrapGenerator"), [
-        emitter.getContextFunction(),
-        b.thisExpression()
-      ])
-    ));
-
-    gfun.body = b.blockStatement(outerBody);
+function visitNode(node) {
+  if (!n.Function.check(node) || !node.generator) {
+    return;
   }
-});
+
+  node.generator = false;
+
+  // TODO Ensure that the context is named uniquely.
+  var contextId = b.identifier("$ctx");
+
+  if (node.expression) {
+    // Transform expression lambdas into normal functions.
+    node.expression = false;
+    node.body = b.blockStatement([
+      b.returnStatement(node.body)
+    ]);
+  }
+
+  var vars = hoist(node);
+
+  var emitter = new Emitter(contextId);
+  emitter.explode(node.body);
+
+  // TODO Need to do something about FunctionDeclarations.
+  // They don't need to be exploded, and it's probably safer to move
+  // them out of the inner function like the hoisted variables.
+  // How about this:
+  // 1. hoist the function names as variables, and then
+  // 2. turn each declaration site into an assignment to the name.
+
+  var outerBody = [];
+
+  if (vars && vars.declarations.length > 0) {
+    outerBody.push(vars);
+  }
+
+  outerBody.push(b.returnStatement(
+    b.callExpression(b.identifier("wrapGenerator"), [
+      emitter.getContextFunction(),
+      b.thisExpression()
+    ])
+  ));
+
+  node.body = b.blockStatement(outerBody);
+}
