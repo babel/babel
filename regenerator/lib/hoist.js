cbe1e7bf6c29f633ee45560e8fe0fbf43b15e9b4
Ben Newman <bn@cs.stanford.edu>
2013-10-09 11:45:42 -0400

    Reimplement lib/hoist.js using require("ast-types").traverse.

--- regenerator/lib/hoist.js
+++ regenerator/lib/hoist.js
@@ -8,9 +8,10 @@
  */
 
 var assert = require("assert");
-var recast = require("recast");
-var b = recast.builders;
-var n = recast.namedTypes;
+var types = require("ast-types");
+var n = types.namedTypes;
+var b = types.builders;
+var hasOwn = Object.prototype.hasOwnProperty;
 
 // The hoist function takes a FunctionExpression or FunctionDeclaration
 // and replaces any Declaration nodes in its body with assignments, then
@@ -18,66 +19,10 @@ var n = recast.namedTypes;
 // declarations.
 exports.hoist = function(fun) {
   n.Function.assert(fun);
-  var hoister = new DeclarationHoister;
-  hoister.visit(fun.body);
-  return hoister.getHoistedVarDecl(fun);
-};
+  var vars = {};
 
-var DeclarationHoister = recast.Visitor.extend({
-  init: function() {
-    this.vars = {};
-  },
-
-  getHoistedVarDecl: function(fun) {
-    var paramNames = {};
-    fun.params.forEach(function(param) {
-      if (n.Identifier.check(param)) {
-        paramNames[param.name] = param;
-      } else {
-        // Variables declared by destructuring parameter patterns
-        // will be harmlessly re-declared.
-      }
-    });
-
-    var vars = this.vars;
-    var declarations = [];
-
-    Object.keys(vars).forEach(function(name) {
-      if (!paramNames.hasOwnProperty(name))
-        declarations.push(b.variableDeclarator(vars[name], null));
-    });
-
-    if (declarations.length === 0)
-      return null; // Be sure to handle this case!
-
-    return b.variableDeclaration("var", declarations);
-  },
-
-  visitVariableDeclaration: function(vdec) {
-    var expr = this._varDeclToExpr(vdec, false);
-    if (expr === null) {
-      this.remove();
-    } else {
-      return b.expressionStatement(expr);
-    }
-  },
-
-  visitForStatement: function(stmt) {
-    if (n.VariableDeclaration.check(stmt.init))
-      stmt.init = this._varDeclToExpr(stmt.init, false);
-    this.genericVisit(stmt);
-  },
-
-  visitForInStatement: function(stmt) {
-    if (n.VariableDeclaration.check(stmt.left))
-      stmt.left = this._varDeclToExpr(stmt.left, true);
-    this.genericVisit(stmt);
-  },
-
-  _varDeclToExpr: function(vdec, includeIdentifiers) {
+  function varDeclToExpr(vdec, includeIdentifiers) {
     n.VariableDeclaration.assert(vdec);
-
-    var vars = this.vars;
     var exprs = [];
 
     vdec.declarations.forEach(function(dec) {
@@ -99,29 +44,76 @@ var DeclarationHoister = recast.Visitor.extend({
       return exprs[0];
 
     return b.sequenceExpression(exprs);
-  },
+  }
 
-  visitFunctionDeclaration: function(fdec) {
-    // Deliberately not calling this.genericVisit() because we don't
-    // want to hoist variables out of inner functions.
+  types.traverse(fun.body, function(node) {
+    if (n.VariableDeclaration.check(node)) {
+      var expr = varDeclToExpr(node, false);
+      if (expr === null) {
+        this.replace();
+      } else {
+        this.replace(b.expressionStatement(expr));
+      }
 
-    this.vars[fdec.id.name] = fdec.id;
+    } else if (n.ForStatement.check(node)) {
+      if (n.VariableDeclaration.check(node.init)) {
+        var expr = varDeclToExpr(node.init, false);
+        this.get("init").replace(expr);
+      }
 
-    return b.expressionStatement(
-      b.assignmentExpression(
-        "=",
-        fdec.id,
-        b.functionExpression(
-          fdec.id,
-          fdec.params,
-          fdec.body,
-          fdec.generator,
-          fdec.expression
+    } else if (n.ForInStatement.check(node)) {
+      if (n.VariableDeclaration.check(node.left)) {
+        var expr = varDeclToExpr(node.left, true);
+        this.get("left").replace(expr);
+      }
+
+    } else if (n.FunctionDeclaration.check(node)) {
+      vars[node.id.name] = node.id;
+
+      this.replace(b.expressionStatement(
+        b.assignmentExpression(
+          "=",
+          node.id,
+          b.functionExpression(
+            node.id,
+            node.params,
+            node.body,
+            node.generator,
+            node.expression
+          )
         )
-      )
-    );
-  },
+      ));
 
-  // Do not descend into nested function expressions.
-  visitFunctionExpression: function() {}
-});
+      // Don't hoist variables out of inner functions.
+      return false;
+
+    } else if (n.FunctionExpression.check(node)) {
+      // Don't descend into nested function expressions.
+      return false;
+    }
+  });
+
+  var paramNames = {};
+  fun.params.forEach(function(param) {
+    if (n.Identifier.check(param)) {
+      paramNames[param.name] = param;
+    } else {
+      // Variables declared by destructuring parameter patterns will be
+      // harmlessly re-declared.
+    }
+  });
+
+  var declarations = [];
+
+  Object.keys(vars).forEach(function(name) {
+    if (!hasOwn.call(paramNames, name)) {
+      declarations.push(b.variableDeclarator(vars[name], null));
+    }
+  });
+
+  if (declarations.length === 0) {
+    return null; // Be sure to handle this case!
+  }
+
+  return b.variableDeclaration("var", declarations);
+};
