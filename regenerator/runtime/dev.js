6c9dccc3a1537ecdf129cb3e7feb813a2bf3a84d
Ben Newman <bn@cs.stanford.edu>
2013-10-28 14:47:05 -0400

    Move context.dispatchException outside of try block in .throw method.
    When the dispatched exception is not handled by the generator function,
    the exception should be re-thrown and not caught by the try statement in
    the .throw method, because catching the exception there (and continuing
    the while loop) causes an infinite loop.
    
    Wise words: if you don't test it, it's probably broken.
    
    Closes facebook/regenerator#44.

--- regenerator/runtime/dev.js
+++ regenerator/runtime/dev.js
@@ -100,11 +100,13 @@
         throw exception;
       }
 
-      while (true) try {
+      while (true) {
         context.dispatchException(exception);
-        return invoke();
-      } catch (thrown) {
-        exception = thrown;
+        try {
+          return invoke();
+        } catch (thrown) {
+          exception = thrown;
+        }
       }
     };
   }
