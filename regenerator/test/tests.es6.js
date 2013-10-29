688991b7309fa83050f90eb65b97f06337752a1c
Ben Newman <bn@cs.stanford.edu>
2013-10-28 21:33:30 -0400

    Add a test of .throw called on new-born generators.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -808,4 +808,23 @@ describe("generator .throw method", function() {
       assert.strictEqual(err, exception);
     }
   });
+
+  it("should immediately complete a new-born generator", function() {
+    var began = false;
+
+    function *gen() {
+      began = true;
+      yield 1;
+    }
+
+    var g = gen();
+    var exception = new Error("unhandled exception");
+    try {
+      g.throw(exception);
+      assert.ok(false, "should have thrown an exception");
+    } catch (err) {
+      assert.strictEqual(err, exception);
+      assert.strictEqual(began, false);
+    }
+  });
 });
