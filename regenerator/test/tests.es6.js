433bb3f6a1c3e7ff89b70d079e9949dfaa1d22a9
Ben Newman <bn@cs.stanford.edu>
2013-10-30 15:14:52 -0400

    Add a failing test of unqualified function call receiver objects.
    The problem here is that the generated code for the function call looks
    like `$ctx.sent(arg)`, so `$ctx` gets used as the receiver for the
    function call instead of the global object.
    
    A cute way to fix this would be to emit `(0,$ctx.sent)(arg)` instead.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -828,3 +828,34 @@ describe("generator .throw method", function() {
     }
   });
 });
+
+describe("unqualified function calls", function() {
+  it("should have a global `this` object", function() {
+    function getThis() {
+      return this;
+    }
+
+    // This is almost certainly the global object, but there's a chance it
+    // might be null or undefined (in strict mode).
+    var unqualifiedThis = getThis();
+
+    function *invoke() {
+      // It seems like a bug in the ES6 spec that we have to yield an
+      // argument instead of just calling (yield)().
+      return (yield "dummy")();
+    }
+
+    var g = invoke();
+    var info = g.next();
+
+    assert.deepEqual(info, { value: "dummy", done: false });
+
+    info = g.next(getThis);
+
+    // Avoid using assert.strictEqual when the arguments might equal the
+    // global object, since JSON.stringify chokes on circular structures.
+    assert.ok(info.value === unqualifiedThis);
+
+    assert.strictEqual(info.done, true);
+  });
+});
