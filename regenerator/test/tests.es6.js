991b6140e0819fc4c3632ab486b85ee32e8f9b3c
Ben Newman <bn@cs.stanford.edu>
2013-11-08 14:49:12 -0500

    Add a test of NewExpressions that causes the transformer to abort.
    As reported by @radford in facebook/regenerator#60.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -1021,3 +1021,30 @@ describe("isGeneratorFunction", function() {
     );
   });
 });
+
+describe("new expressions", function() {
+  it("should be able to contain yield sub-expressions", function() {
+    function A(first, second) {
+      this.first = first;
+      this.second = second;
+    }
+
+    function *gen() {
+      return yield new (yield 0)(yield 1, yield 2);
+    }
+
+    var g = gen();
+
+    assert.deepEqual(g.next(), { value: 0, done: false });
+    assert.deepEqual(g.next(A), { value: 1, done: false });
+    assert.deepEqual(g.next("asdf"), { value: 2, done: false });
+
+    var info = g.next("zxcv");
+    assert.strictEqual(info.done, false);
+    assert.ok(info.value instanceof A);
+    assert.strictEqual(info.value.first, "asdf");
+    assert.strictEqual(info.value.second, "zxcv");
+
+    assert.deepEqual(g.next("qwer"), { value: "qwer", done: true });
+  });
+});
