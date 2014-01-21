5c8923a76a11063f75f7e4997b8240b34c0bcfc2
Ben Newman <bn@cs.stanford.edu>
2014-01-21 11:41:09 -0500

    Regenerate bundle.js to incorporate regenerator.runtime().

--- regenerator/bundle.js
+++ regenerator/bundle.js
@@ -3205,13 +3205,19 @@ function regenerator(source, options) {
   return recast.print(path, recastOptions).code;
 }
 
+function runtime() {
+  require(runtime.dev);
+}
+
+runtime.dev = path.join(__dirname, "runtime", "dev.js");
+runtime.min = path.join(__dirname, "runtime", "min.js");
+
 // To modify an AST directly, call require("regenerator").transform(ast).
 regenerator.transform = transform;
 
-regenerator.runtime = {
-  dev: path.join(__dirname, "runtime", "dev.js"),
-  min: path.join(__dirname, "runtime", "min.js")
-};
+// To include the runtime in the current node process, call
+// require("regenerator").runtime().
+regenerator.runtime = runtime;
 
 // To transform a string of ES6 code, call require("regenerator")(source);
 module.exports = regenerator;
