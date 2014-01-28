9aef3bce5e525ba6b5950cfda54232d9128ccc9f
Ben Newman <bn@cs.stanford.edu>
2014-01-28 11:32:43 -0800

    Add test verifying semantics of idiom for delegate generator wrapping.
    As discussed here:
    https://github.com/tc39/agendas/pull/25#issuecomment-33512904

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -1149,7 +1149,7 @@ describe("newborn generators", function() {
 
     var g = outer(n);
 
-    // I would really like to be able to pass 3 to gen.next here, but V8
+    // I would really like to be able to pass 3 to g.next here, but V8
     // ignores values sent to newborn generators, and SpiderMonkey throws
     // a TypeError.
     assert.deepEqual(g.next(), {
@@ -1161,5 +1161,66 @@ describe("newborn generators", function() {
       value: [void 0, 4],
       done: true
     });
+  });
+
+  it("should support the ignore-initial-yield wrapper idiom", function() {
+    var markers = [];
+
+    function *inner() {
+      markers.push(0);
+      var sent1 = yield 1;
+      markers.push(2);
+      var sent2 = yield 2;
+      markers.push(3);
+      return [sent1, sent2];
+    }
+
+    function wrapper(delegate) {
+      var gen = (function*() {
+        // This yield is the "initial yield" whose argument we ignore.
+        var sent = yield "ignored", info;
+
+        markers.push(1);
+
+        while (!(info = delegate.next(sent)).done) {
+          sent = yield info.value;
+        }
+
+        markers.push(4);
+
+        return info.value;
+      })();
+
+      // Ensure that gen is not newborn and that the next invocation of
+      // gen.next(value) can send value to the initial yield expression.
+      gen.next();
+
+      return gen;
+    }
+
+    var n = inner();
+
+    assert.deepEqual(n.next(), {
+      value: 1,
+      done: false
+    });
+
+    var g = wrapper(n);
+
+    // Unlike in the previous spec, it's fine to pass 3 to g.next here,
+    // because g is not newborn, because g.next was already called once
+    // before g was returned from the wrapper function.
+    assert.deepEqual(g.next(3), {
+      value: 2,
+      done: false
+    });
+
+    assert.deepEqual(g.next(4), {
+      value: [3, 4],
+      done: true
+    });
+
+    // Ensure we encountered the marker points in the expected order.
+    assert.deepEqual(markers, [0, 1, 2, 3, 4]);
   });
 });
