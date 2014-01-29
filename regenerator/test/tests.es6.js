df4b4be9e79ae98cba421ac14e11b52f54f3c755
Ben Newman <bn@cs.stanford.edu>
2014-01-29 12:27:38 -0800

    Add a test of chaining generators using successive yield*s.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -1222,4 +1222,47 @@ describe("newborn generators", function() {
     // Ensure we encountered the marker points in the expected order.
     assert.deepEqual(markers, [0, 1, 2, 3, 4]);
   });
+
+  it("should allow chaining newborn and non-newborn generators", function() {
+    function *range(n) {
+      for (var i = 0; i < n; ++i) {
+        yield i;
+      }
+    }
+
+    function *chain(a, b) {
+      yield* a;
+      yield* b;
+    }
+
+    check(chain(range(3), range(5)), [0, 1, 2, 0, 1, 2, 3, 4]);
+
+    function *y3(x) {
+      return yield yield yield x;
+    }
+
+    function *y5(x) {
+      return yield yield yield yield yield x;
+    }
+
+    check(
+      chain(y3("foo"), y5("bar")),
+      ["foo", 1, 2, "bar", 4, 5, 6, 7]
+    );
+
+    var g3 = y3("three");
+    assert.deepEqual(g3.next(), {
+      value: "three",
+      done: false
+    });
+
+    var g5 = y5("five");
+    assert.deepEqual(g5.next(), {
+      value: "five",
+      done: false
+    });
+
+    var undef; // A little easier to read than void 0.
+    check(chain(g3, g5), [undef, 1, undef, 3, 4, 5]);
+  });
 });
