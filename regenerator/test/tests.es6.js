93f01ba1f6fa8e0a6003c77155d62624b92b567b
Ben Newman <bn@cs.stanford.edu>
2014-02-02 18:28:16 -0500

    Add a test of calling .throw while suspended on a delegate generator.
    This is an adaptation of the failing test case reported by @jonathanong in
    issue facebook/regenerator#82.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -648,6 +648,46 @@ describe("delegated yield", function() {
     check(outer(3), [0, 2, 1, 0, 1, 4]);
     check(outer(5), [0, 4, 3, "thrown", 4]);
   });
+
+  it("should dispatch .thrown exceptions correctly", function() {
+    var count = 0;
+
+    function *gen() {
+      yield* inner();
+      try {
+        yield* inner();
+      } catch (err) {
+        // pass
+      }
+      return yield* inner();
+    }
+
+    function *inner() {
+      return yield count++;
+    }
+
+    var g = gen();
+
+    assert.deepEqual(g.next(), {
+      value: 0,
+      done: false
+    });
+
+    assert.deepEqual(g.next(), {
+      value: 1,
+      done: false
+    });
+
+    assert.deepEqual(g.throw(new Error("lol")), {
+      value: 2,
+      done: false,
+    });
+
+    assert.deepEqual(g.next("sent"), {
+      value: "sent",
+      done: true
+    });
+  });
 });
 
 describe("function declaration hoisting", function() {
