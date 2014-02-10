104c67933d5a699cc0a3c58b41e5b39c43a47fdc
Ben Newman <bn@cs.stanford.edu>
2014-02-10 13:41:24 -0500

    Add test of labeled break and continue in nested try-finally.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -1392,3 +1392,89 @@ describe("newborn generators", function() {
     check(chain(g3, g5), [undef, 1, undef, 3, 4, 5]);
   });
 });
+
+describe("labeled break and continue statements", function() {
+  it("should be able to exit multiple try statements", function() {
+    var e1 = "first";
+    var e2 = "second";
+    var e3 = "third";
+    var e4 = "fourth";
+
+    function *gen(n, which) {
+      try {
+        yield 0;
+        raise(e1);
+
+      } finally {
+        yield 1;
+
+        loop:
+        for (var i = 0; i < n; ++i) {
+          yield i;
+
+          try {
+            raise(e2);
+          } finally {
+            yield 2;
+
+            try {
+              raise(e3);
+            } finally {
+              yield 3;
+
+              try {
+                raise(e4);
+              } finally {
+                yield 4;
+
+                if (which === "break") {
+                  yield "breaking";
+                  break loop;
+                }
+
+                if (which === "continue") {
+                  yield "continuing";
+                  continue loop;
+                }
+
+                yield 5;
+              }
+            }
+          }
+        }
+
+        yield 6;
+      }
+    }
+
+    try {
+      check(gen(1, "break"), [
+        0, 1, 0, 2, 3, 4, "breaking", 6
+      ]);
+      assert.ok(false, "should have thrown an exception");
+    } catch (err) {
+      assert.strictEqual(err, e1);
+    }
+
+    try {
+      check(gen(3, "continue"), [
+        0, 1, 0, 2, 3, 4, "continuing",
+        1, 2, 3, 4, "continuing",
+        2, 2, 3, 4, "continuing",
+        6 // Loop finished naturally.
+      ]);
+      assert.ok(false, "should have thrown an exception");
+    } catch (err) {
+      assert.strictEqual(err, e1);
+    }
+
+    try {
+      check(gen(3, "neither"), [
+        0, 1, 0, 2, 3, 4, 5
+      ]);
+      assert.ok(false, "should have thrown an exception");
+    } catch (err) {
+      assert.strictEqual(err, e4);
+    }
+  });
+});
