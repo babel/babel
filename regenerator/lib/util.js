733a6b0c6b1719e039560a86862cdf851e01d00a
Ben Newman <bn@cs.stanford.edu>
2014-03-05 21:16:08 -0500

    Remove unused guessTabWidth utility function.
    An improved version of this function is now used automatically by Recast:
    https://github.com/benjamn/recast/blob/e2972296a3/lib/lines.js#L362-L400

--- regenerator/lib/util.js
+++ regenerator/lib/util.js
@@ -10,33 +10,6 @@
 
 var hasOwn = Object.prototype.hasOwnProperty;
 
-exports.guessTabWidth = function(source) {
-  var counts = []; // Sparse array.
-  var lastIndent = 0;
-
-  source.split("\n").forEach(function(line) {
-    var indent = /^\s*/.exec(line)[0].length;
-    var diff = Math.abs(indent - lastIndent);
-    counts[diff] = ~~counts[diff] + 1;
-    lastIndent = indent;
-  });
-
-  var maxCount = -1;
-  var result = 2;
-
-  for (var tabWidth = 1;
-       tabWidth < counts.length;
-       tabWidth += 1) {
-    if (tabWidth in counts &&
-        counts[tabWidth] > maxCount) {
-      maxCount = counts[tabWidth];
-      result = tabWidth;
-    }
-  }
-
-  return result;
-};
-
 exports.defaults = function(obj) {
   var len = arguments.length;
   var extension;
