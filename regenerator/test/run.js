71f3206be0c47d2024d1dabd3b86ba067354c94c
Aaron Chou <aarongchou@gmail.com>
2014-03-15 12:41:42 -0700

    Fix copyright year across multiple files.

--- regenerator/test/run.js
+++ regenerator/test/run.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013, Facebook, Inc.
+ * Copyright (c) 2014, Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
