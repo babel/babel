761b74abf8e3f6411a4b2f0a4dae67f1bee477a4
Ben Newman <bn@cs.stanford.edu>
2014-05-01 14:23:41 -0400

    Borrow a failing test from Traceur that manifests facebook/regenerator#113.

--- regenerator/test/tests.es6.js
+++ regenerator/test/tests.es6.js
@@ -1633,3 +1633,60 @@ describe("for loop with var decl and no update expression", function() {
     check(range(), []);
   });
 });
+
+describe("generator function prototype", function() {
+  it("should follow the expected object model", function() {
+    var GeneratorFunctionPrototype = f.__proto__;
+    var GeneratorFunction = GeneratorFunctionPrototype.constructor;
+
+    assert.strictEqual(GeneratorFunction.name, 'GeneratorFunction');
+    assert.strictEqual(GeneratorFunction.prototype,
+                       GeneratorFunctionPrototype);
+    assert.strictEqual(GeneratorFunctionPrototype.prototype.constructor,
+                       GeneratorFunctionPrototype);
+    assert.strictEqual(GeneratorFunctionPrototype.prototype,
+                       f.prototype.__proto__);
+    assert.strictEqual(GeneratorFunctionPrototype.__proto__,
+                       Function.prototype);
+
+    var g = f();
+    assert.ok(g instanceof f);
+    assert.strictEqual(g.__proto__, f.prototype);
+
+    assert.deepEqual([], Object.getOwnPropertyNames(f.prototype));
+    assert.deepEqual([], Object.getOwnPropertyNames(g));
+
+    f.prototype.x = 42;
+
+    var g2 = f();
+    assert.strictEqual(g2.x, 42);
+
+    var g3 = new f();
+    assert.strictEqual(g3.x, 42);
+
+    function* f2() {
+      yield 1;
+    }
+
+    assert.strictEqual(f.__proto__, f2.__proto__);
+    assert.strictEqual(f.hasOwnProperty('constructor'), false);
+    assert.strictEqual(f.__proto__.constructor.name, 'GeneratorFunction');
+
+    // Intentionally at the end to test hoisting.
+    function* f() {
+      yield this;
+    }
+
+    function* f() {
+      yield 1;
+    }
+
+    var f2 = f;
+    f = 42;
+    var g = f2();
+
+    assert.deepEqual(g.next(), { value: 1, done: false });
+    assert.deepEqual(g.next(), { value: void 0, done: true });
+    assert.ok(g instanceof f2);
+  });
+});
