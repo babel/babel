5656e891482417e15dc81ea83d8d960d54bad9a4
Ben Newman <bn@cs.stanford.edu>
2014-05-02 17:22:19 -0400

    Turn generator function declarations into named, hoisted function expressions.

--- regenerator/lib/visit.js
+++ regenerator/lib/visit.js
@@ -139,26 +139,115 @@ function visitNode(node) {
     }
 
     if (path) {
-      var firstStmtPath = path.get("body", 0);
+      // Here we turn the FunctionDeclaration into a named
+      // FunctionExpression that will be assigned to a variable of the
+      // same name at the top of the enclosing block. This is important
+      // for a very subtle reason: named function expressions can refer to
+      // themselves by name without fear that the binding may change due
+      // to code executing outside the function, whereas function
+      // declarations are vulnerable to the following rebinding:
+      //
+      //   function f() { return f }
+      //   var g = f;
+      //   f = "asdf";
+      //   g(); // "asdf"
+      //
+      // One way to prevent the problem illustrated above is to transform
+      // the function declaration thus:
+      //
+      //   var f = function f() { return f };
+      //   var g = f;
+      //   f = "asdf";
+      //   g(); // f
+      //   g()()()()(); // f
+      //
+      // In the code below, we transform generator function declarations
+      // in the following way:
+      //
+      //   gen().next(); // { value: gen, done: true }
+      //   function *gen() {
+      //     return gen;
+      //   }
+      //
+      // becomes something like
+      //
+      //   var gen = wrapGenerator.mark(function *gen() {
+      //     return gen;
+      //   });
+      //   gen().next(); // { value: gen, done: true }
+      //
+      // which ensures that the generator body can always reliably refer
+      // to gen by name.
 
-      // If the first statement is a "use strict" declaration, make sure to
-      // insert our call afterwards
-      if (n.ExpressionStatement.check(firstStmtPath.value) &&
-          n.Literal.check(firstStmtPath.value.expression) &&
-          firstStmtPath.value.expression.value === "use strict") {
-        firstStmtPath = path.get("body", 1);
+      // Remove the FunctionDeclaration so that we can add it back as a
+      // FunctionExpression passed to wrapGenerator.mark.
+      this.replace();
+
+      // Change the type of the function to be an expression instead of a
+      // declaration. Note that all the other fields are the same.
+      node.type = "FunctionExpression";
+
+      var varDecl = b.variableDeclaration("var", [
+        b.variableDeclarator(
+          node.id,
+          b.callExpression(markMethod, [node])
+        )
+      ]);
+
+      if (node.comments) {
+        // Copy any comments preceding the function declaration to the
+        // variable declaration, to avoid weird formatting consequences.
+        varDecl.comments = node.comments;
+        node.comments = null;
       }
 
-      firstStmtPath.replace(
-        b.expressionStatement(b.callExpression(markMethod, [node.id])),
-        firstStmtPath.value
-      );
+      var bodyPath = path.get("body");
+      var bodyLen = bodyPath.value.length;
+
+      for (var i = 0; i < bodyLen; ++i) {
+        var firstStmtPath = bodyPath.get(i);
+        if (!shouldNotHoistAbove(firstStmtPath)) {
+          firstStmtPath.replace(varDecl, firstStmtPath.value);
+          return;
+        }
+      }
+
+      bodyPath.value.push(varDecl);
     }
 
   } else {
     n.FunctionExpression.assert(node);
     this.replace(b.callExpression(markMethod, [node]));
   }
+}
+
+function shouldNotHoistAbove(stmtPath) {
+  var value = stmtPath.value;
+  n.Statement.assert(value);
+
+  // If the first statement is a "use strict" declaration, make sure to
+  // insert hoisted declarations afterwards.
+  if (n.ExpressionStatement.check(value) &&
+      n.Literal.check(value.expression) &&
+      value.expression.value === "use strict") {
+    return true;
+  }
+
+  if (n.VariableDeclaration.check(value)) {
+    for (var i = 0; i < value.declarations.length; ++i) {
+      var decl = value.declarations[i];
+      if (n.CallExpression.check(decl.init) &&
+          n.MemberExpression.check(decl.init.callee) &&
+          n.Identifier.check(decl.init.callee.object) &&
+          n.Identifier.check(decl.init.callee.property) &&
+          decl.init.callee.object.name === "wrapGenerator" &&
+          decl.init.callee.property.name === "mark") {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 function renameArguments(funcPath, argsId) {
