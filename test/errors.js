b3657b3c2e7f5b9673e9875616d24642c7cea9f9
Sebastian McKenzie <sebmck@gmail.com>
2014-09-29 01:02:55 +1000

    add errors test

--- test/errors.js
+++ test/errors.js
@@ -1,3 +1,14 @@
-suite("errors", function () {
+var transform = require("../lib/6to5/transform");
+var assert    = require("assert");
 
+suite("errors", function () {
+  test("syntax", function () {
+    assert.throws(function () {
+      transform.test([
+        "arr.map(function () {",
+        "  return $@!@#;",
+        "});"
+      ]);
+    }, /Error: test: Line 2: Unexpected token ILLEGAL/);
+  });
 });
