710be2634b4f2eb990b565c58305848bfbb62832
Sebastian McKenzie <sebmck@gmail.com>
2014-10-02 09:53:35 +1000

    add multiple for-of test

---
+++ test/fixtures/for-of/multiple/expected.js
@@ -0,0 +1,7 @@
+for (var _iterator = arr[Symbol.iterator](), _step; !(_step = _iterator.next()).done; ) {
+  var i = _step.value;
+}
+
+for (var _iterator2 = numbers[Symbol.iterator](), _step2; !(_step2 = _iterator2.next()).done; ) {
+  var i = _step2.value;
+}
