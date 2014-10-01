01a89cfec14fa4875b98c3e829e5a76a2a1208f7
Marijn Haverbeke <marijnh@gmail.com>
2014-10-01 23:21:25 +0200

    Mark release 0.9.0

--- acorn.js
+++ acorn.js
@@ -28,7 +28,7 @@
 })(this, function(exports) {
   "use strict";
 
-  exports.version = "0.8.1";
+  exports.version = "0.9.0";
 
   // The main exported interface (under `self.acorn` when in the
   // browser) is a `parse` function that takes a code string and
