27cb0f6f875af48d8b8f284f8eea583be36b2601
Sebastian McKenzie <sebmck@gmail.com>
2014-10-12 09:34:01 +1100

    fix blacklist traversal bug - fixes #49

---
+++ test/fixtures/syntax/block-binding/return-ignore-siblings/actual.js
@@ -0,0 +1,6 @@
+let _message = message();
+console.log( _message );
+
+function message () {
+  return 'hello';
+}
---
+++ test/fixtures/syntax/block-binding/return-ignore-siblings/expected.js
@@ -0,0 +1,8 @@
+(function () {
+  var _message = message();
+  console.log(_message);
+
+  function message () {
+    return 'hello';
+  }
+})();
