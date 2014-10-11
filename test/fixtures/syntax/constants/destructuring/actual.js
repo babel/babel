2dbdb9e3a5f0592860d4bd4543630d661db7bb40
Sebastian McKenzie <sebmck@gmail.com>
2014-10-11 23:31:53 +1100

    support patterns in constants

---
+++ test/fixtures/syntax/constants/destructuring/actual.js
@@ -0,0 +1,4 @@
+const [a, b] = [1, 2];
+const [c, d] = [3, 4];
+const { e, f } = { e: 5, f: 6 };
+const { a: g, b: h } = { a: 7, b: 8 };
---
+++ test/fixtures/syntax/constants/destructuring/expected.js
@@ -0,0 +1,23 @@
+(function () {
+  var _ref = [1, 2];
+  var a = _ref[0];
+  var b = _ref[1];
+
+  var _ref2 = [3, 4];
+  var c = _ref2[0];
+  var d = _ref2[1];
+
+  var _ref3 = {
+    e: 5,
+    f: 6
+  };
+  var e = _ref3.e;
+  var f = _ref3.f;
+
+  var _ref4 = {
+    a: 7,
+    b: 8
+  };
+  var g = _ref4.a;
+  var h = _ref4.b;
+})();
