a9d101e3da07ba2d8ff94c5deb7d5322de3cd91f
Sebastian McKenzie <sebmck@gmail.com>
2014-10-14 08:07:03 +1100

    support nested arrow functions - fixes #60

---
+++ test/fixtures/syntax/arrow-functions/nested/actual.js
@@ -0,0 +1,13 @@
+module.exports = {
+  init() {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(config.mongodb, (err, db) => {
+        if (err) {
+          return reject(err);
+        }
+        this.db = db;
+        resolve(this);
+      });
+    });
+  }
+};
