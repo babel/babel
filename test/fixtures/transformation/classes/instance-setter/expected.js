c17878913be6ba8e854c03dcba56ddee996ef092
Sebastian McKenzie <sebmck@gmail.com>
2014-11-03 13:36:23 +1100

    add classProps declaration to simplify/nicen up class property defining - closes #88

--- test/fixtures/transformation/classes/instance-setter/expected.js
+++ test/fixtures/transformation/classes/instance-setter/expected.js
@@ -1,9 +1,14 @@
 "use strict";
 
+var _classProps = function (child, staticProps, instanceProps) {
+  if (staticProps) Object.defineProperties(child, staticProps);
+  if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
+};
+
 var Test = function() {
   var Test = function Test() {};
 
-  Object.defineProperties(Test.prototype, {
+  _classProps(Test, null, {
     test: {
       set: function(val) {
         this._test = val;
