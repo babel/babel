9f510fe15cfe7a5fd226f8d34881c3753d533931
Ben Newman <bn@cs.stanford.edu>
2014-11-06 12:39:50 -0500

    Add some failing tests of rejected promises.

--- regenerator/test/async.es6.js
+++ regenerator/test/async.es6.js
@@ -154,4 +154,75 @@ describe("async functions and await expressions", function() {
       }).catch(done);
     });
   });
+
+  describe("rejected promises", function() {
+    it("should cause await expressions to throw", function(done) {
+      var error = new Error("rejected");
+
+      async function f(arg) {
+        try {
+          return await arg;
+        } catch (e) {
+          assert.strictEqual(e, error);
+          return "did throw";
+        }
+      }
+
+      Promise.all([
+        f(Promise.reject(error)),
+        f(Promise.resolve("did not throw"))
+      ]).then(function(results) {
+        assert.deepEqual(results, [
+          "did throw",
+          "did not throw"
+        ]);
+        done();
+      }).catch(done);
+    });
+
+    it("should be returned by exceptional async functions", function(done) {
+      var error = new Error("rejected");
+
+      async function e(arg) {
+        if (arg) {
+          throw arg;
+        }
+        return "did not throw";
+      }
+
+      async function f(arg) {
+        return await e(arg);
+      }
+
+      async function g(arg) {
+        return await f(arg);
+      }
+
+      async function h(arg) {
+        return await Promise.all([
+          g(arg),
+          Promise.resolve("dummy")
+        ]);
+      }
+
+      Promise.all([
+        h(error).then(function() {
+          done(new Error("should not have resolved"));
+        }, function(e) {
+          assert.strictEqual(e, error);
+          return "ok1";
+        }),
+        h(null).then(function(result) {
+          assert.deepEqual(result, [
+            "did not throw",
+            "dummy"
+          ]);
+          return "ok2";
+        })
+      ]).then(function(results) {
+        assert.deepEqual(results, ["ok1", "ok2"]);
+        done();
+      }).catch(done);
+    });
+  });
 });
