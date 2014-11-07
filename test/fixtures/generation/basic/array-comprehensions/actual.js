e327e041abf82d49a1fed33134c9f7f6e822abe9
Sebastian McKenzie <sebmck@gmail.com>
2014-11-07 20:46:11 +1100

    add basic generation tests

---
+++ test/fixtures/generation/basic/array-comprehensions/actual.js
@@ -0,0 +1,3 @@
+var multiples = [for (num of nums) num * num];
+var multiples = [for (num of nums) for (num2 of nums) num * num];
+var multiples = [for (num of nums) if (num > 5) num * num];
---
+++ test/fixtures/generation/basic/template-literals/actual.js
@@ -0,0 +1,17 @@
+var foo = `i
+am a multiline string`;
+
+var foo = `i am also
+   a multiline string
+foobar`;
+
+var foo = `im a single line string`;
+
+var foo = tagit`i
+am a multiline string`;
+
+var foo = tagit`i am also
+   a multiline string
+foobar`;
+
+var foo = tagit`im a single line string`;
