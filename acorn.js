d231f10c36e5aa42893de9023c2d21a4d0c70d43
Ingvar Stepanyan <me@rreverser.com>
2014-11-09 15:08:59 +0200

    Disallow parenthesis in patterns
    Fixes #155

--- acorn.js
+++ acorn.js
@@ -1487,10 +1487,6 @@
       case "SpreadElement":
         break;
 
-      case "ParenthesizedExpression":
-        checkLVal(expr.expression);
-        break;
-
       default:
         raise(expr.start, "Assigning to rvalue");
     }
