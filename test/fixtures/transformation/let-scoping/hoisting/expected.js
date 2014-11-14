921d459f130af4f2b6c7848fa7eff0ba9388e9aa
Sebastian McKenzie <sebmck@gmail.com>
2014-11-14 19:38:29 +1100

    fix transformation let scoping hoisting test

--- test/fixtures/transformation/let-scoping/hoisting/expected.js
+++ test/fixtures/transformation/let-scoping/hoisting/expected.js
@@ -1,12 +1,12 @@
 "use strict";
 
 for (var _iterator = [1, 2, 3][Symbol.iterator](), _step; !(_step = _iterator.next()).done;) {
-  var i = _step.value;
   var x;
-  (function (i) {
+  (function () {
+    var i = _step.value;
     x = 5;
     fns.push(function () {
       return i * x;
     });
-  })(i);
+  })();
 }
