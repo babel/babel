cad3f6372340ccd9d15da9eae5d123d478f446d3
Sebastian McKenzie <sebmck@gmail.com>
2014-11-24 01:01:13 +1100

    update assignment expression destructuring test

--- test/fixtures/transformation/es6-destructuring/assignment-expression/expected.js
+++ test/fixtures/transformation/es6-destructuring/assignment-expression/expected.js
@@ -1,2 +1,9 @@
+"use strict";
+
+var _ref;
+var _toArray = function (arr) {
+  return Array.isArray(arr) ? arr : Array.from(arr);
+};
+
 var _temp;
-console.log((_temp = [1, 2, 3], x = _temp[0], _temp));
+console.log((_temp = [123], _ref = _toArray(_temp), x = _ref[0], _temp));
