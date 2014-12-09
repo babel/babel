5f26a0c5cc7e29bfed40d8e4233ad8318816cd82
Sebastian McKenzie <sebmck@gmail.com>
2014-12-09 23:28:07 +1100

    update modules tests

--- test/fixtures/transformation/es6-modules-common-interop/exports-variable/expected.js
+++ test/fixtures/transformation/es6-modules-common-interop/exports-variable/expected.js
@@ -3,9 +3,9 @@
 exports.foo7 = foo7;
 var foo = exports.foo = 1;
 var foo2 = exports.foo2 = function () {};
-var foo3;
+var foo3 = exports.foo3 = undefined;
 var foo4 = exports.foo4 = 2;
-var foo5;
+var foo5 = exports.foo5 = undefined;
 var foo6 = exports.foo6 = 3;
 function foo7() {}
 var foo8 = function foo8() {};
--- test/fixtures/transformation/es6-modules-ignore/exports-variable/expected.js
+++ test/fixtures/transformation/es6-modules-ignore/exports-variable/expected.js
@@ -2,9 +2,9 @@
 
 var foo = 1;
 var foo2 = function () {};
-var foo3;
+var foo3 = undefined;
 var foo4 = 2;
-var foo5;
+var foo5 = undefined;
 var foo6 = 3;
 function foo7() {}
 var foo8 = function foo8() {};
