841ab6802f572b1b2567497801249073261df1eb
Sebastian McKenzie <sebmck@gmail.com>
2014-12-10 18:59:24 +1100

    Make semicolon optional in do-while in >=ES6

--- acorn.js
+++ acorn.js
@@ -1603,7 +1603,11 @@
     labels.pop();
     expect(_while);
     node.test = parseParenExpression();
-    semicolon();
+    if (options.ecmaVersion >= 6) {
+      eat(_semi);
+    } else {
+      semicolon();
+    }
     return finishNode(node, "DoWhileStatement");
   }
 
