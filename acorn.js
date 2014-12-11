c671bcfaf7aa3f97222e668e168d92b1bf4cfb2f
Marijn Haverbeke <marijnh@gmail.com>
2014-12-11 14:52:32 +0100

    Bump version number post-0.10

--- acorn.js
+++ acorn.js
@@ -28,7 +28,7 @@
 })(this, function(exports) {
   "use strict";
 
-  exports.version = "0.10.0";
+  exports.version = "0.10.1";
 
   // The main exported interface (under `self.acorn` when in the
   // browser) is a `parse` function that takes a code string and
