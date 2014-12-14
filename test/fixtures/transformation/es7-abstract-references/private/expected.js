00cb90541a7fdb2ad7fba76d3aef239173bb09c3
Sebastian McKenzie <sebmck@gmail.com>
2014-12-14 14:49:25 +1100

    add support for experimental private abstract references syntax - closes #291

---
+++ test/fixtures/transformation/es7-abstract-references/private/expected.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var A = new WeakMap();
+var B = new WeakMap(), C = new WeakMap();
+(function () {
+  var F = new WeakMap(), G = new WeakMap();
+  var E = new WeakMap();
+  var D = function D() {};
+
+  return D;
+})()
+
+var H = (function () {
+  var J = new WeakMap(), K = new WeakMap();
+  var I = new WeakMap();
+  var _class = function () {};
+
+  return _class;
+})();
