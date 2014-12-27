4fd007948c46872f3b9acfba7be0e43ffd51a0d6
Sebastian McKenzie <sebmck@gmail.com>
2014-12-27 21:34:20 +1100

    fix system modules exports from test

--- test/fixtures/transformation/es6-modules-system/exports-from/expected.js
+++ test/fixtures/transformation/es6-modules-system/exports-from/expected.js
@@ -1,28 +1,29 @@
 System.register("es6-modules-system/exports-from/expected", ["foo"], function (_export) {
-  "use strict";
-
-  var __moduleName = "es6-modules-system/exports-from/expected";
-
+  var _foo;
   return {
     setters: [function (m) {
-      for (var i in m) {
-        _export(i, m[i]);
+      _foo = m
+    }],
+    execute: function () {
+      "use strict";
+
+      for (var i in _foo) {
+        _export(i, _foo[i])
       }
 
-      _export("foo", m.foo);
+      _export("foo", _foo.foo);
 
-      _export("foo", m.foo);
+      _export("foo", _foo.foo);
 
-      _export("bar", m.bar);
+      _export("bar", _foo.bar);
 
-      _export("bar", m.foo);
+      _export("bar", _foo.foo);
 
-      _export("default", m.foo);
+      _export("default", _foo.foo);
 
-      _export("default", m.foo);
+      _export("default", _foo.foo);
 
-      _export("bar", m.bar);
-    }],
-    execute: function () {}
+      _export("bar", _foo.bar);
+    }
   };
 });
