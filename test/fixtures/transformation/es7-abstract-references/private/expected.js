322aa246b8a2f6baae3bc3138c42059734ad25d3
Sebastian McKenzie <sebmck@gmail.com>
2014-12-28 01:01:13 +1100

    fix automatic closure on private declarations - fixes #335

--- test/fixtures/transformation/es7-abstract-references/private/expected.js
+++ test/fixtures/transformation/es7-abstract-references/private/expected.js
@@ -2,13 +2,13 @@
 
 var A = new WeakMap();
 var B = new WeakMap(), C = new WeakMap();
-(function () {
+var D = (function () {
   var F = new WeakMap(), G = new WeakMap();
   var E = new WeakMap();
   var D = function D() {};
 
   return D;
-})()
+})();
 
 var H = (function () {
   var J = new WeakMap(), K = new WeakMap();
