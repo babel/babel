22eeae1a93762d0c38b5572a7993e76248ab94b2
Sebastian McKenzie <sebmck@gmail.com>
2015-01-04 08:00:40 +1100

    use `assert` instead of `expect`

--- test/fixtures/transformation/esnext-es6-rest-parameters/declaration.js
+++ test/fixtures/transformation/esnext-es6-rest-parameters/declaration.js
@@ -1,8 +1,5 @@
-/* jshint esnext: true */
-/* global expect */
-
 function join(joinStr, ...items) {
   return items.join(joinStr);
 }
 
-expect(join(' ', 'a', 'b', 'c')).to.equal('a b c');
+assert.deepEqual(join(' ', 'a', 'b', 'c'), 'a b c');
