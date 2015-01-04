005362a615c87511b818da22491bbde9715d9202
Sebastian McKenzie <sebmck@gmail.com>
2015-01-04 19:48:14 +1100

    update tests to 2.4.9 fixes

--- test/fixtures/transformation/es7-array-comprehension/single-if/expected.js
+++ test/fixtures/transformation/es7-array-comprehension/single-if/expected.js
@@ -4,9 +4,9 @@ var arr = (function () {
   var _arr = [];
 
   for (var _iterator = nums[Symbol.iterator](), _step; !(_step = _iterator.next()).done;) {
-    var i = _step.value;
-    if (i > 1) {
-      _arr.push(i * i);
+    var _i = _step.value;
+    if (_i > 1) {
+      _arr.push(_i * _i);
     }
   }
 
