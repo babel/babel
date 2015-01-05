56f1683f06eb73954d0fe5aaf52cbe06d7f7fc90
Lars Kappert <lars@webpro.nl>
2015-01-05 14:57:52 +0100

    Also test for "module" identifier if needed

--- test/fixtures/transformation/es6-modules-umd/exports-default/expected.js
+++ test/fixtures/transformation/es6-modules-umd/exports-default/expected.js
@@ -3,7 +3,7 @@
 (function (factory) {
   if (typeof define === "function" && define.amd) {
     define(["exports", "module"], factory);
-  } else if (typeof exports !== "undefined") {
+  } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
     factory(exports, module);
   }
 })(function (exports, module) {
--- test/fixtures/transformation/es6-modules-umd/overview/expected.js
+++ test/fixtures/transformation/es6-modules-umd/overview/expected.js
@@ -3,7 +3,7 @@
 (function (factory) {
   if (typeof define === "function" && define.amd) {
     define(["exports", "module", "foo", "foo-bar", "./directory/foo-bar"], factory);
-  } else if (typeof exports !== "undefined") {
+  } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
     factory(exports, module, require("foo"), require("foo-bar"), require("./directory/foo-bar"));
   }
 })(function (exports, module, _foo, _fooBar, _directoryFooBar) {
