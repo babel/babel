5ce8ee6c2bdf512c0ac7e76ec8d51bf713becdc3
Sebastian McKenzie <sebmck@gmail.com>
2015-01-05 12:39:46 +1100

    add inheritance type check to tests

--- test/fixtures/transformation/optional-proto-to-assign/class/expected.js
+++ test/fixtures/transformation/optional-proto-to-assign/class/expected.js
@@ -11,6 +11,9 @@ var _defaults = function (obj, defaults) {
 };
 
 var _inherits = function (child, parent) {
+  if (typeof parent !== "function" && parent !== null) {
+    throw new TypeError("Super expression must either be null or a function, not " + typeof parent);
+  }
   child.prototype = Object.create(parent && parent.prototype, {
     constructor: {
       value: child,
