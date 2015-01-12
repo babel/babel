c7ac9b7815e2bf4b88e82f7aa0a80265eec544d6
Sebastian McKenzie <sebmck@gmail.com>
2015-01-12 11:47:04 +1100

    rename --reactCompat arg to --react-compat

--- bin/6to5/index.js
+++ bin/6to5/index.js
@@ -24,7 +24,7 @@ commander.option("-c, --remove-comments", "Remove comments from the compiled cod
 commander.option("-I, --indent [width]", "Indent width [2]", 2);
 commander.option("-a, --amd-module-ids", "Insert module id in AMD modules", false); // todo: remove in 3.0.0
 commander.option("-m, --module-ids", "Insert module id in modules", false);
-commander.option("--reactCompat", "Makes the react transformer produce pre-v0.12 code");
+commander.option("-R, --react-compat", "Makes the react transformer produce pre-v0.12 code");
 
 commander.on("--help", function(){
   var outKeys = function (title, obj) {
