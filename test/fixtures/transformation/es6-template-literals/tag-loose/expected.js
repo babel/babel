6b836de3068708ce0b83a9b517c842d89960fd25
Sebastian McKenzie <sebmck@gmail.com>
2015-01-14 07:56:45 +1100

    add #480 test

---
+++ test/fixtures/transformation/es6-template-literals/tag-loose/expected.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _taggedTemplateLiteralLoose = function (strings, raw) {
+  strings.raw = raw;
+  return strings;
+};
+
+var foo = bar(_taggedTemplateLiteralLoose(["wow\na", "b ", ""], ["wow\\na", "b ", ""]), 42, _.foobar());
