7e6da1d368c75fe980bf271feaa90643359e7045
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2015-01-18 01:41:20 -0200

    Add tests for the externalRegenerator transformer

---
+++ test/fixtures/transformation/optional-external-regenerator-runtime/basic/expected.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var _interopRequire = function (obj) {
+  return obj && (obj["default"] || obj);
+};
+
+var _regeneratorRuntime = _interopRequire(require("regenerator/runtime-module"));
+
+void _regeneratorRuntime.mark(function callee$0$0() {
+  return _regeneratorRuntime.wrap(function callee$0$0$(context$1$0) {
+    while (1) switch (context$1$0.prev = context$1$0.next) {
+      case 0:
+      case "end":
+        return context$1$0.stop();
+    }
+  }, callee$0$0, this);
+});
