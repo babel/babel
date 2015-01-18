02019d4d8f728817809d1e51d7889ae11f3e8d2f
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2015-01-18 21:18:25 -0200

    Drop unnecessary dynamic import redeclaration in AMD
    Instead of assigning the import a name and them declaring a new
    variabled referencing that name, output the import already with
    the destination name, since we know there must be no collisions
    because these IDs are also generated.

--- test/fixtures/transformation/optional-self-contained/runtime-amd/expected.js
+++ test/fixtures/transformation/optional-self-contained/runtime-amd/expected.js
@@ -1,8 +1,5 @@
-define(["exports", "6to5-runtime/6to5", "6to5-runtime/core-js", "6to5-runtime/regenerator", "someModule"], function (exports, _to5Runtime6to5, _to5RuntimeCoreJs, _to5RuntimeRegenerator, _someModule) {
+define(["exports", "6to5-runtime/6to5", "6to5-runtime/core-js", "6to5-runtime/regenerator", "someModule"], function (exports, _to5Runtime, _core, _regeneratorRuntime, _someModule) {
   "use strict";
 
-  var _to5Runtime = _to5Runtime6to5;
-  var _core = _to5RuntimeCoreJs;
-  var _regeneratorRuntime = _to5RuntimeRegenerator;
   var foo = _to5Runtime.interopRequire(_someModule);
 });
