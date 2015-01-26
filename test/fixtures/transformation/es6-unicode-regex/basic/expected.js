c907f8703a918c1ba8b18ab7dd274dd44c6a743e
James Kyle <me@thejameskyle.com>
2015-01-26 11:36:41 -0800

    Fix regex transpilation test for regexpu 1.1

--- test/fixtures/transformation/es6-unicode-regex/basic/expected.js
+++ test/fixtures/transformation/es6-unicode-regex/basic/expected.js
@@ -1,4 +1,4 @@
 "use strict";
 
 var string = "foo💩bar";
-var match = string.match(/foo((?:[\0-\t\x0B\f\x0E-\u2027\u202A-\uD7FF\uDC00-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF]))bar/);
+var match = string.match(/foo((?:[\0-\t\x0B\f\x0E-\u2027\u202A-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]))bar/);
