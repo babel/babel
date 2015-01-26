28698780467031d0d86032b0acc8ad62290574d4
Sebastian McKenzie <sebmck@gmail.com>
2015-01-27 07:16:57 +1100

    use hasOwnProperty in extends helper

--- test/fixtures/transformation/spec-proto-to-assign/object-literal/expected.js
+++ test/fixtures/transformation/spec-proto-to-assign/object-literal/expected.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _extends = function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { target[key] = source[key]; } } return target; };
+var _extends = function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(target, key)) { target[key] = source[key]; } } } return target; };
 
 var foo = _extends({}, bar);
 
