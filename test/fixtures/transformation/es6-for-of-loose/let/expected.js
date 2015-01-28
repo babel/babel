960a70287d1a849d22e7519db2c381bffe24c8b2
Sebastian McKenzie <sebmck@gmail.com>
2015-01-29 00:50:22 +1100

    add improved for-of loose behaviour that supports destructuring - fixes #615

--- test/fixtures/transformation/es6-for-of-loose/let/expected.js
+++ test/fixtures/transformation/es6-for-of-loose/let/expected.js
@@ -1,13 +1,14 @@
 "use strict";
 
 for (var _iterator = arr, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
-  var i = undefined;
+  var _ref;
   if (_isArray) {
     if (_i >= _iterator.length) break;
-    i = _iterator[_i++];
+    _ref = _iterator[_i++];
   } else {
     _i = _iterator.next();
     if (_i.done) break;
-    i = _i.value;
+    _ref = _i.value;
   }
+  var i = _ref;
 }
