47a45f37316464b5eab4ce3dd3b31e573245ab11
Sebastian McKenzie <sebmck@gmail.com>
2015-01-30 11:05:16 +1100

    add additional react ThisExpression conversion test

--- test/fixtures/transformation/react/arrow-functions/expected.js
+++ test/fixtures/transformation/react/arrow-functions/expected.js
@@ -4,3 +4,10 @@ var foo = function () {
     return React.createElement(_this, null);
   };
 };
+
+var bar = function () {
+  var _this = this;
+  return function () {
+    return React.createElement(_this.foo, null);
+  };
+};
