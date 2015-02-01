25566a24f6555ce1319f75b0dcd14378c3fbf8f2
Sebastian McKenzie <sebmck@gmail.com>
2015-02-01 18:33:36 +1100

    block hoist assignment pattern destructuring - fixes #652

--- test/fixtures/transformation/regenerator/destructuring/exec.js
+++ test/fixtures/transformation/regenerator/destructuring/exec.js
@@ -3,4 +3,11 @@ function* foo() {
   return bar;
 }
 
-assert.deepEqual(foo().next().value, "bar");
+assert.equal(foo().next().value, "bar");
+
+function *foo({ bar = 0 }) {
+  return bar;
+}
+
+assert.equal(foo({ bar: undefined }).next(), 0);
+assert.equal(foo({ bar: 3 }).next(), 3);
