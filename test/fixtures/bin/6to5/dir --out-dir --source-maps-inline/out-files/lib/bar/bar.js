41d721e3722392917629674c33f2bb2c7c5b5957
Sebastian McKenzie <sebmck@gmail.com>
2015-02-02 21:32:45 +1100

    fix source map tests

--- test/fixtures/bin/6to5/dir --out-dir --source-maps-inline/out-files/lib/bar/bar.js
+++ test/fixtures/bin/6to5/dir --out-dir --source-maps-inline/out-files/lib/bar/bar.js
@@ -1,4 +1,4 @@
 "use strict";
 
 var Test = function Test() {};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9iYXIvYmFyLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0lBQU0sSUFBSSxZQUFKLElBQUkiLCJmaWxlIjoic3JjL2Jhci9iYXIuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjbGFzcyBUZXN0IHtcblxufSJdfQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9iYXIvYmFyLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0lBQU0sZ0JBQUEiLCJmaWxlIjoic3JjL2Jhci9iYXIuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjbGFzcyBUZXN0IHtcblxufSJdfQ==
