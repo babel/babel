689ce048e696271c4907eea08e4e82d494419aad
Sebastian McKenzie <sebmck@gmail.com>
2015-02-07 23:52:41 +1100

    remove tail call exec test

--- test/fixtures/transformation/es6-tail-call/recursion/exec.js
+++
@@ -1,9 +0,0 @@
-var timeLimit = Date.now() + 5000;
-
-assert.equal((function f(n) {
-  assert.operator(Date.now(), '<', timeLimit, "Timeout");
-  if (n <= 0) {
-    return "foo";
-  }
-  return f(n - 1);
-})(1e6), "foo");
