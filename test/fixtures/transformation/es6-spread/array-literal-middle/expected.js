8d06a4301c0aee3ca2e8cff1232ac6087ec0bf23
Sebastian McKenzie <sebmck@gmail.com>
2015-02-12 16:35:55 +1100

    pdate tests to reflect toConsumableArray updates

--- test/fixtures/transformation/es6-spread/array-literal-middle/expected.js
+++ test/fixtures/transformation/es6-spread/array-literal-middle/expected.js
@@ -1,5 +1,5 @@
 "use strict";
 
-var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { var arr2 = []; for (var i = 0; i < arr.length; i++) arr2.push(arr[i]); return arr2; } else { return Array.from(arr); } };
+var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2.push(arr[i]); return arr2; } else { return Array.from(arr); } };
 
 var a = [b].concat(_toConsumableArray(c), [d]);
--- test/fixtures/transformation/es6-spread/contexted-computed-method-call-single-arg/expected.js
+++ test/fixtures/transformation/es6-spread/contexted-computed-method-call-single-arg/expected.js
@@ -1,6 +1,6 @@
 "use strict";
 
 var _obj;
-var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { var arr2 = []; for (var i = 0; i < arr.length; i++) arr2.push(arr[i]); return arr2; } else { return Array.from(arr); } };
+var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2.push(arr[i]); return arr2; } else { return Array.from(arr); } };
 
 (_obj = obj)[method].apply(_obj, _toConsumableArray(args));
