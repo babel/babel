e9cfd2df3eeed2801b7181227274934e45037f79
Sebastian McKenzie <sebmck@gmail.com>
2015-02-12 16:53:00 +1100

    fix toConsumableArray helper

--- test/fixtures/transformation/es6-spread/method-call-middle/expected.js
+++ test/fixtures/transformation/es6-spread/method-call-middle/expected.js
@@ -1,5 +1,5 @@
 "use strict";
 
-var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2.push(arr[i]); return arr2; } else { return Array.from(arr); } };
+var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } };
 
 add.apply(undefined, [foo].concat(_toConsumableArray(numbers), [bar]));
