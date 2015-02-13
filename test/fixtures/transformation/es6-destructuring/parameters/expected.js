228719102a651b19e440a9ea5d3e3131396fadb4
Sebastian McKenzie <sebmck@gmail.com>
2015-02-13 20:39:37 +1100

    update sliceToArray tests

--- test/fixtures/transformation/es6-destructuring/parameters/expected.js
+++ test/fixtures/transformation/es6-destructuring/parameters/expected.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _slicedToArray = function (arr, i) { if (!arr || !arr[Symbol.iterator]) { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } else if (Array.isArray(arr)) { return arr; } else { var _arr = []; for (var _iterator = arr[Symbol.iterator](), _step; !(_step = _iterator.next()).done;) { _arr.push(_step.value); if (i && _arr.length === i) break; } return _arr; } };
+var _slicedToArray = function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in arr) { var _arr = []; for (var _iterator = arr[Symbol.iterator](), _step; !(_step = _iterator.next()).done;) { _arr.push(_step.value); if (i && _arr.length === i) break; } return _arr; } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } };
 
 function somethingAdvanced(_ref) {
   var _ref$topLeft = _ref.topLeft;
