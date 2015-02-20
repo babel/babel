299e462a296f7e5185cdf66d8979c5000841cdf9
Sebastian McKenzie <sebmck@gmail.com>
2015-02-21 02:09:07 +1100

    unpack array rest elements when given a right hand array expression

--- test/fixtures/transformation/es6-destructuring/array-unpack-optimisation/expected.js
+++ test/fixtures/transformation/es6-destructuring/array-unpack-optimisation/expected.js
@@ -5,6 +5,12 @@ var a = 1;
 var b = 2;
 var a = 1;
 var b = 2;
+var a = 1;
+var b = 2;
+var c = [3, 4];
+var a = 1;
+var b = 2;
+var c = [3, 4];
 
 // deopt
 var _ref = [1, 2, 3];
@@ -13,14 +19,3 @@ var b = _ref[1];
 var _ref2 = [1, 2, 3];
 var a = _ref2[0];
 var b = _ref2[1];
-var _ref3 = [1, 2, 3];
-var a = _ref3[0];
-var b = _ref3[1];
-
-var items = _ref3.slice(2);
-
-var _ref4 = [1, 2, 3];
-var a = _ref4[0];
-var b = _ref4[1];
-
-var items = _ref4.slice(2);
