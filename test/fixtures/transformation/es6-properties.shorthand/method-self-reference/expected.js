e11b943514fd848a13b6dd0668907a46d4723a38
Sebastian McKenzie <sebmck@gmail.com>
2015-02-22 20:54:47 +1100

    revert to old named function wrapper - fixes #861

--- test/fixtures/transformation/es6-properties.shorthand/method-self-reference/expected.js
+++ test/fixtures/transformation/es6-properties.shorthand/method-self-reference/expected.js
@@ -1,33 +1,51 @@
 "use strict";
 
 var bar = {
-  foo: (function () {
-    function _getOuter() {
-      return foo;
-    }
-
-    return function foo() {
-      console.log(_getOuter());
+  foo: (function (_foo) {
+    var _fooWrapper = function foo() {
+      return _foo.apply(this, arguments);
     };
-  })()
+
+    _fooWrapper.toString = function () {
+      return _foo.toString();
+    };
+
+    return _fooWrapper;
+  })(function () {
+    console.log(foo);
+  })
 };
 
 var bar = {
-  foo: function foo() {
+  foo: (function (_foo) {
+    var _fooWrapper = function foo() {
+      return _foo.apply(this, arguments);
+    };
+
+    _fooWrapper.toString = function () {
+      return _foo.toString();
+    };
+
+    return _fooWrapper;
+  })(function () {
     var foo = 41;
     console.log(foo);
-  }
+  })
 };
 
 var foobar = 123;
 var foobar2 = {
-  foobar: (function () {
-    function _getOuter() {
-      return foobar;
-    }
-
-    return function foobar() {
-      console.log(_getOuter());
+  foobar: (function (_foobar) {
+    var _foobarWrapper = function foobar() {
+      return _foobar.apply(this, arguments);
     };
-  })()
+
+    _foobarWrapper.toString = function () {
+      return _foobar.toString();
+    };
+
+    return _foobarWrapper;
+  })(function () {
+    console.log(foobar);
+  })
 };
