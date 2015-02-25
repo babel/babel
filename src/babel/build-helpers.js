dc6129eb6a2036954ef295ecc300362649c96470
Sebastian McKenzie <sebmck@gmail.com>
2015-02-26 09:19:49 +1100

    fix helper whitelist in build script

--- src/babel/build-helpers.js
+++ src/babel/build-helpers.js
@@ -5,7 +5,7 @@ var t    = require("./types");
 
 module.exports = function (body, namespace, whitelist = []) {
   each(File.helpers, function (name) {
-    if (whitelist.length && whitelist.indexOf(key) >= 0) return;
+    if (whitelist.length && whitelist.indexOf(name) == -1) return;
 
     var key = t.identifier(t.toIdentifier(name));
     body.push(t.expressionStatement(
