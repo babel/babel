f7186980e5c4de00e3dfe503cc856ad24cb1fb8c
Sebastian McKenzie <sebmck@gmail.com>
2015-02-26 09:24:28 +1100

    add external helpers test #898

--- test/api.js
+++ test/api.js
@@ -1,6 +1,7 @@
-var transform = require("../lib/babel/transformation");
-var assert    = require("assert");
-var File      = require("../lib/babel/transformation/file");
+var buildExternalHelpers = require("../lib/babel/build-external-helpers");
+var transform            = require("../lib/babel/transformation");
+var assert               = require("assert");
+var File                 = require("../lib/babel/transformation/file");
 
 suite("api", function () {
   test("{ code: false }", function () {
@@ -18,5 +19,19 @@ suite("api", function () {
     assert.throws(function () {
       file.addHelper("foob");
     }, /Unknown helper foob/);
+  });
+
+  suite("buildExternalHelpers", function () {
+    test("all", function () {
+      var script = buildExternalHelpers();
+      assert.ok(script.indexOf("classCallCheck") >= -1);
+      assert.ok(script.indexOf("inherits") >= 0);
+    });
+
+    test("whitelist", function () {
+      var script = buildExternalHelpers(["inherits"]);
+      assert.ok(script.indexOf("classCallCheck") === -1);
+      assert.ok(script.indexOf("inherits") >= 0);
+    });
   });
 });
