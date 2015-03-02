c45ce58f0cc3943a24f4a58c599fa5c2e3164fc3
Ondrej Kraus <neverberlerfellerer@gmail.com>
2015-03-02 15:47:36 +0100

    stop assigning to global in generated helpers code

--- src/babel/build-external-helpers.js
+++ src/babel/build-external-helpers.js
@@ -6,18 +6,28 @@ import t from "./types";
 export default function (whitelist) {
   var namespace = t.identifier("babelHelpers");
 
-  var body      = [];
-  var container = t.functionExpression(null, [t.identifier("global")], t.blockStatement(body));
-  var tree      = t.program([t.expressionStatement(t.callExpression(container, [util.template("self-global")]))]);
-
+  var body = [];
   body.push(t.variableDeclaration("var", [
-    t.variableDeclarator(
-      namespace,
-      t.assignmentExpression("=", t.memberExpression(t.identifier("global"), namespace), t.objectExpression([]))
-    )
+    t.variableDeclarator(namespace, t.identifier("global"))
   ]));
 
   buildHelpers(body, namespace, whitelist);
+
+  var globalHelpersDeclar = t.variableDeclaration("var", [
+    t.variableDeclarator(
+      namespace,
+      t.objectExpression({})
+    )
+  ]);
+  var container = util.template("umd-commonjs-strict", {
+    AMD_ARGUMENTS:      t.arrayExpression([t.literal("exports")]),
+    COMMON_ARGUMENTS:   t.identifier("exports"),
+    BROWSER_ARGUMENTS:  t.identifier("root"),
+    UMD_ROOT:           namespace,
+    FACTORY_PARAMETERS: t.identifier("global"),
+    FACTORY_BODY:       body
+  });
+  var tree = t.program([globalHelpersDeclar, container]);
 
   return generator(tree).code;
 };
