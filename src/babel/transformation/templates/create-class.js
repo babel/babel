4c70c89c923fdb81855b7c692a91f0c4e0f19a03
Ondrej Kraus <neverberlerfellerer@gmail.com>
2015-03-10 02:37:09 +0100

    check for existence of Object.getOwnPropertySymbols before using it

--- src/babel/transformation/templates/create-class.js
+++ src/babel/transformation/templates/create-class.js
@@ -1,6 +1,7 @@
 (function() {
   function defineProperties(target, props) {
-    var keys = Object.getOwnPropertyNames(props).concat(Object.getOwnPropertySymbols(props));
+    var keys = Object.getOwnPropertyNames(props);
+    if (Object.getOwnPropertySymbols !== undefined) keys = keys.concat(Object.getOwnPropertySymbols(props));
     for (var i = 0, count = keys.length; i < count; ++i) {
       var prop = props[keys[i]];
       prop.configurable = true;
