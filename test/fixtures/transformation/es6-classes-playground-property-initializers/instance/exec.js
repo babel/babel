b606cc79ca42d55b28a46f9779a238b23d148ac9
Sebastian McKenzie <sebmck@gmail.com>
2015-03-17 11:26:18 +1100

    remove class playground property initializer tests

--- test/fixtures/transformation/es6-classes-playground-property-initializers/instance/exec.js
+++
@@ -1,7 +0,0 @@
-class MyClass {
-  myProp = { someValue: 42 };
-}
-
-var myClass = new MyClass;
-assert.ok(myClass.myProp !== MyClass.prototype.myProp);
-assert.equal(myClass.myProp.someValue, 42);
--- test/fixtures/transformation/es6-classes-playground-property-initializers/static/exec.js
+++
@@ -1,5 +0,0 @@
-class MyClass {
-  static myProp = { someValue: 42 };
-}
-
-assert.equal(MyClass.myProp.someValue, 42);
