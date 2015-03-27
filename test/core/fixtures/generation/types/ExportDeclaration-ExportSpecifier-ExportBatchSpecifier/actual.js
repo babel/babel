ad98186afcbcc34cf89948e52ccba0b8f5f94b5a
Sebastian McKenzie <sebmck@gmail.com>
2015-03-28 05:38:22 +1100

    remove redundant semicolons from export declarations

--- test/core/fixtures/generation/types/ExportDeclaration-ExportSpecifier-ExportBatchSpecifier/actual.js
+++ test/core/fixtures/generation/types/ExportDeclaration-ExportSpecifier-ExportBatchSpecifier/actual.js
@@ -2,8 +2,8 @@ export default 42;
 export default {};
 export default [];
 export default foo;
-export default function () {};
-export default class {};
+export default function () {}
+export default class {}
 export default function foo() {}
 export default class Foo {}
 export * from "foo";
