617ed0922703c4862da1570e027d84391ef3a612
Ben <me@benng.me>
2015-03-28 15:32:07 -0400

    Incorrect package path
    I'm pretty sure that this is a mistake, since the path doesn't exist relative to this file. Is it supposed to point to the babel-core package.json like this?

--- src/babel/api/register/node.js
+++ src/babel/api/register/node.js
@@ -158,7 +158,7 @@ export default function (opts = {}) {
 
 try {
   var runtimePackage = require("babel-runtime/package");
-  var version = require("../../package").version;
+  var version = require("../../../../package").version;
   if (runtimePackage.version !== version) {
     throw new ReferenceError(`The verison of babel-runtime of ${runtimePackage.runtime} that you have installed does not match the babel verison of ${version}`);
   }
