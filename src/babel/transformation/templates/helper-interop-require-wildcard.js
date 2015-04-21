5ce38cacaa8c55e15cd98b5620fe5fec83a59338
jeffmo <lbljeffmo@gmail.com>
2015-04-21 11:21:18 -0400

    Copy statics off of foreign exports objects when doing an ES6 import

--- src/babel/transformation/templates/helper-interop-require-wildcard.js
+++ src/babel/transformation/templates/helper-interop-require-wildcard.js
@@ -1,3 +1,16 @@
 (function (obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
+  if (obj && obj.__esModule) {
+    return obj;
+  } else {
+    var hop = Object.prototype.hasOwnProperty;
+    var es_obj = { "default": obj };
+    if (typeof obj === "object" && obj !== null) {
+      for (var key in obj) {
+        if (key !== "default" && hop.call(obj, key)) {
+          es_obj[key] = obj[key];
+        }
+      }
+    }
+    return es_obj;
+  }
 })
--- test/core/fixtures/transformation/es6.modules-common/overview/expected.js
+++ test/core/fixtures/transformation/es6.modules-common/overview/expected.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _interopRequireWildcard = function (obj) { return obj && obj.__esModule ? obj : { "default": obj }; };
+var _interopRequireWildcard = function (obj) { if (obj && obj.__esModule) { return obj; } else { var hop = Object.prototype.hasOwnProperty; var es_obj = { "default": obj }; if (typeof obj === "object" && obj !== null) { for (var key in obj) { if (key !== "default" && hop.call(obj, key)) { es_obj[key] = obj[key]; } } } return es_obj; } };
 
 Object.defineProperty(exports, "__esModule", {
   value: true
