00c0a958db4f3dd0f4b8eb142025309f8730bdde
Sebastian McKenzie <sebmck@gmail.com>
2015-04-25 18:08:00 +0100

    fix runtime tests to reflect core-js 0.9.0

--- test/core/fixtures/transformation/runtime/full/expected.js
+++ test/core/fixtures/transformation/runtime/full/expected.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _Object$defineProperty = require("babel-runtime/core-js/object/define-property")["default"];
+
 var _Symbol = require("babel-runtime/core-js/symbol")["default"];
 
 var _regeneratorRuntime = require("babel-runtime/regenerator")["default"];
@@ -8,9 +10,10 @@ var _interopRequireDefault = require("babel-runtime/helpers/interop-require-defa
 
 var _interopRequireWildcard = require("babel-runtime/helpers/interop-require-wildcard")["default"];
 
-Object.defineProperty(exports, "__esModule", {
+_Object$defineProperty(exports, "__esModule", {
   value: true
 });
+
 exports.giveWord = giveWord;
 var marked0$0 = [giveWord].map(_regeneratorRuntime.mark);
 
