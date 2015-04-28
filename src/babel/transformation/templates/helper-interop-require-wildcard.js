433d7047393b7518b9d58c308167f379cf4c6793
Sebastian McKenzie <sebmck@gmail.com>
2015-04-28 16:56:57 +0100

    add null check to interop-require-wildcard helper

--- src/babel/transformation/templates/helper-interop-require-wildcard.js
+++ src/babel/transformation/templates/helper-interop-require-wildcard.js
@@ -3,8 +3,10 @@
     return obj;
   } else {
     var newObj = {};
-    for (var key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
+    if (obj != null) {
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
+      }
     }
     newObj.default = obj;
     return newObj;
