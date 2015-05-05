235726eee65f7a9ef522b02218ed62b68e9f9c54
Sebastian McKenzie <sebmck@gmail.com>
2015-05-05 15:38:17 +0100

    remove unnecessary descriptor.initializer check

--- src/babel/transformation/templates/helper-define-decorated-property-descriptor.js
+++ src/babel/transformation/templates/helper-define-decorated-property-descriptor.js
@@ -7,9 +7,7 @@
   for (var _key in _descriptor) descriptor[_key] = _descriptor[_key];
 
   // initialize it
-  if (descriptor.initializer) {
-    descriptor.value = descriptor.initializer.call(target);
-  }
+  descriptor.value = descriptor.initializer.call(target);
 
   Object.defineProperty(target, key, descriptor);
 })
