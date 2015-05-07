0bbfd427af7037437afa4700b114bbc5c2e0ce4d
Sebastian McKenzie <sebmck@gmail.com>
2015-05-07 16:23:47 +0100

    do module formatter on program exit rather than entrance

--- src/babel/transformation/transformers/internal/module-formatter.js
+++ src/babel/transformation/transformers/internal/module-formatter.js
@@ -1,13 +1,15 @@
 import * as strict from "../../helpers/strict";
 
-export function Program(program, parent, scope, file) {
-  strict.wrap(program, function () {
-    program.body = file.dynamicImports.concat(program.body);
-  });
+export var Program = {
+  exit(program, parent, scope, file) {
+    strict.wrap(program, function () {
+      program.body = file.dynamicImports.concat(program.body);
+    });
 
-  if (!file.transformers["es6.modules"].canTransform()) return;
+    if (!file.transformers["es6.modules"].canTransform()) return;
 
-  if (file.moduleFormatter.transform) {
-    file.moduleFormatter.transform(program);
+    if (file.moduleFormatter.transform) {
+      file.moduleFormatter.transform(program);
+    }
   }
-}
+};
