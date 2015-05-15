02dfd18023fd5201cd38279d49f58e6c3341946f
Sebastian McKenzie <sebmck@gmail.com>
2015-05-15 17:42:28 +0100

    add back descriptor.initializer existence check

--- src/babel/transformation/templates/helper-define-decorated-property-descriptor.js
+++ src/babel/transformation/templates/helper-define-decorated-property-descriptor.js
@@ -7,7 +7,9 @@
   for (var _key in _descriptor) descriptor[_key] = _descriptor[_key];
 
   // initialize it
-  descriptor.value = descriptor.initializer.call(target);
+  if (descriptor.initializer) {
+    descriptor.value = descriptor.initializer.call(target);
+  }
 
   Object.defineProperty(target, key, descriptor);
 })
