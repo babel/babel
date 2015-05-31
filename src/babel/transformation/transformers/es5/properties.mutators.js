152ccb2ce8433b3bfedde74196d27fc840e6c2dc
Sebastian McKenzie <sebmck@gmail.com>
2015-05-31 15:40:07 +0100

    check if ObjectExpression has any gets/sets before filtering

--- src/babel/transformation/transformers/es5/properties.mutators.js
+++ src/babel/transformation/transformers/es5/properties.mutators.js
@@ -2,20 +2,25 @@ import * as defineMap from "../../helpers/define-map";
 import * as t from "../../../types";
 
 export function ObjectExpression(node, parent, scope, file) {
-  var mutatorMap = {};
   var hasAny = false;
+  for (var prop of (node.properties: Array)) {
+    if (prop.kind === "get" || prop.kind === "set") {
+      hasAny = true;
+      break;
+    }
+  }
+  if (!hasAny) return;
+
+  var mutatorMap = {};
 
   node.properties = node.properties.filter(function (prop) {
     if (prop.kind === "get" || prop.kind === "set") {
-      hasAny = true;
       defineMap.push(mutatorMap, prop, prop.kind, file);
       return false;
     } else {
       return true;
     }
   });
-
-  if (!hasAny) return;
 
   return t.callExpression(
     t.memberExpression(t.identifier("Object"), t.identifier("defineProperties")),
