83854db13f24f15ee7cc6304bd728171efd9ed4f
Jesse McCarthy <git_commits@jessemccarthy.net>
2015-06-10 11:29:16 -0400

    Optimization.
    * Short circuit iterating body to detect hoisting.
    * `else if` instead of `if` for mutually exclusive condition.

--- src/babel/transformation/transformers/internal/block-hoist.js
+++ src/babel/transformation/transformers/internal/block-hoist.js
@@ -16,14 +16,17 @@ export var BlockStatement = {
     var hasChange = false;
     for (var i = 0; i < node.body.length; i++) {
       var bodyNode = node.body[i];
-      if (bodyNode && bodyNode._blockHoist != null) hasChange = true;
+      if (bodyNode && bodyNode._blockHoist != null) {
+        hasChange = true;
+        break;
+      }
     }
     if (!hasChange) return;
 
     node.body = sortBy(node.body, function(bodyNode){
       var priority = bodyNode && bodyNode._blockHoist;
       if (priority == null) priority = 1;
-      if (priority === true) priority = 2;
+      else if (priority === true) priority = 2;
 
       // Higher priorities should move toward the top.
       return -1 * priority;
