668274edcb8eea74a8c2fd7d7fce4edd78361f26
Sebastian McKenzie <sebmck@gmail.com>
2015-06-13 18:49:37 +0100

    remove resolve-rc file

--- src/babel/tools/resolve-rc.js
+++
@@ -1,5 +0,0 @@
-console.trace("I know someone out there is hotlinking straight to this file. This is a " +
-              "PRIVATE API. I hate you, but I wont break your code just because you're a " +
-              "programmer who practices bad habits.");
-
-export { default } from "../transformation/file/options/resolve-rc";
