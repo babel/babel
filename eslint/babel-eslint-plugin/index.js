e9e6dee39a258f1fa273b125d17335e0546a0455
jquense <monastic.panic@gmail.com>
2015-06-19 00:08:09 -0400

    Typos and copy

--- eslint/babel-eslint-plugin/index.js
+++ eslint/babel-eslint-plugin/index.js
@@ -3,6 +3,12 @@
 module.exports = {
   rules: {
     'object-shorthand': require('./rules/object-shorthand'),
-    'generator-star': require('./rules/object-shorthand')
+    'generator-star-spacing': require('./rules/generator-star-spacing'),
+    'generator-star': require('./rules/generator-star')
+  },
+  rulesConfig: {
+    'generator-star-spacing': 0,
+    'generator-star': 0,
+    'object-shorthand': 0
   }
 };
