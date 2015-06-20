c07540a2c46f757ced8ab684413a0b60732d6d19
Sebastian McKenzie <sebmck@gmail.com>
2015-06-20 22:47:32 +0100

    extend module IgnoreFormatter from DefaultFormatter - fixes #1763, closes #1771

--- src/babel/transformation/modules/ignore.js
+++ src/babel/transformation/modules/ignore.js
@@ -1,6 +1,7 @@
+import DefaultFormatter from "./_default";
 import * as t from "../../types";
 
-export default class IgnoreFormatter {
+export default class IgnoreFormatter extends DefaultFormatter {
   exportDeclaration(node, nodes) {
     var declar = t.toStatement(node.declaration, true);
     if (declar) nodes.push(t.inherits(declar, node));
