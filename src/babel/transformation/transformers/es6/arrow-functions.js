036451986941026f26071113d1f0b14c0f601d71
Sebastian McKenzie <sebmck@gmail.com>
2015-06-22 00:06:43 +0100

    remove unused import

--- src/babel/transformation/transformers/es6/arrow-functions.js
+++ src/babel/transformation/transformers/es6/arrow-functions.js
@@ -1,9 +1,6 @@
-import * as t from "../../../types";
-
 export var visitor = {
   ArrowFunctionExpression(node) {
     this.ensureBlock();
-
     node.expression = false;
     node.type = "FunctionExpression";
     node.shadow = true;
