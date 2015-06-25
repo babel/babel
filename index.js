31c1286a3c9fdd7885c5e1974838b9b1e5a01327
Sebastian McKenzie <sebmck@gmail.com>
2015-06-25 11:51:45 +0100

    protect internal files from hotlinking

---
+++ index.js
@@ -0,0 +1,28 @@
+var path = require("path");
+var lib  = path.join(__dirname, "lib");
+
+var before = Object.keys(require.cache);
+
+var babel = require("./lib/babel/api/node.js");
+module.exports = babel;
+
+var after = Object.keys(require.cache);
+
+var internalFiles = after.filter(function (filename) {
+  // we had this file before
+  if (before.indexOf(filename) >= 0) {
+    return false;
+  }
+
+  // internal file
+  if (filename.indexOf(lib) >= 0) {
+    return true;
+  }
+});
+
+internalFiles.forEach(function (filename) {
+  var cache = require.cache[filename];
+  cache.exports = {
+    "": "Don't hotlink internal Babel files."
+  };
+});
---
+++ tools/build-plugins/protect.js
@@ -0,0 +1,15 @@
+module.exports = function (babel) {
+  var t = babel.types;
+
+  return new babel.Plugin("protect-internal-apis", {
+    visitor: {
+      Program: function (node, parent, scope, file) {
+        if (file.opts.filename.indexOf("tools/protect") >= 0) return;
+
+        this.unshiftContainer("body", [
+          t.expressionStatement(t.callExpression(file.addImport("babel-core/lib/babel/tools/protect"), [t.identifier("module")]))
+        ]);
+      }
+    }
+  });
+}
