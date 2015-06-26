5e3ea5b6e7d28fd3ee1676497b842633518fea76
Sebastian McKenzie <sebmck@gmail.com>
2015-06-26 19:48:48 +0100

    add update-ref master to merge-development-with-master script

--- tools/merge-development-with-master.sh
+++ tools/merge-development-with-master.sh
@@ -5,6 +5,7 @@ if [ "$TRAVIS_BRANCH" != "development" ]; then
   exit 0;
 fi
 
+git update-ref HEAD master
 git checkout master
 git merge "${TRAVIS_COMMIT}"
 git push "https://${GH_TOKEN}@github.com/babel/babel"
