7f06bb79c006d989df8fb74f3683fd8438d37cb8
Sebastian McKenzie <sebmck@gmail.com>
2015-06-26 23:58:57 +0100

    attempt to fix master fetching for automatic travis merge

--- tools/merge-development-with-master.sh
+++ tools/merge-development-with-master.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_BRANCH" != "development" ]; then
   exit 0;
 fi
 
-git update-ref HEAD master
+git fetch origin master:master
 git checkout master
 git merge "$TRAVIS_COMMIT"
 git push "https://${GH_TOKEN}@github.com/babel/babel"
