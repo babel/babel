eed750206c24ed8dc2c5c934abc9d7ec4a728c57
kpdecker <kpdecker@gmail.com>
2015-06-30 01:22:06 -0500

    Convert template objects to singletons
    Create file-scoped template objects that are instantiated once and reused between different tagged template evaluations. This is a closer match to the spec behavior which demands reuse, but does not match the spec exactly with respect to scoping as it’s possible to have similar template objects defined in different file scopes within the same program. For now we are not able to cleanly handle this case as it would require a registry or similar lookup mechanism that is consistent across different combinations of builds and optimizers.
    
    This has a 40x increase in six-speed throughput.
    
    Partial fix for #971

--- packages/babel/test/fixtures/transformation/es6.template-literals/tag-loose/expected.js
+++ packages/babel/test/fixtures/transformation/es6.template-literals/tag-loose/expected.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _taggedTemplateLiteralLoose_3_wowNaB = _taggedTemplateLiteralLoose(["wow\na", "b ", ""], ["wow\\na", "b ", ""]);
+
 function _taggedTemplateLiteralLoose(strings, raw) { strings.raw = raw; return strings; }
 
-var foo = bar(_taggedTemplateLiteralLoose(["wow\na", "b ", ""], ["wow\\na", "b ", ""]), 42, _.foobar());
+var foo = bar(_taggedTemplateLiteralLoose_3_wowNaB, 42, _.foobar());
