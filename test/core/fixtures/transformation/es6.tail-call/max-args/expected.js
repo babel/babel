0509fb148f71e0cceaf60a7f35ab1a96091e1ca2
Sebastian McKenzie <sebmck@gmail.com>
2015-07-06 20:12:10 +0100

    add out of bounds check to default parameter template - addresses #1845 - cc @RReverser

--- test/core/fixtures/transformation/es6.tail-call/max-args/expected.js
+++ test/core/fixtures/transformation/es6.tail-call/max-args/expected.js
@@ -7,7 +7,7 @@ var count = function count() {
   _function: while (_again) {
     i = undefined;
     _again = false;
-    var i = _arguments[0] === undefined ? 10 : _arguments[0];
+    var i = _arguments.length >= 0 && _arguments[0] === undefined ? 10 : _arguments[0];
 
     if (!i) return;
     _arguments = [i - 1];
@@ -23,7 +23,7 @@ function count2() {
   _function2: while (_again2) {
     i = undefined;
     _again2 = false;
-    var i = _arguments2[0] === undefined ? 10 : _arguments2[0];
+    var i = _arguments2.length >= 0 && _arguments2[0] === undefined ? 10 : _arguments2[0];
 
     if (!i) return;
     _arguments2 = [i - 1];
