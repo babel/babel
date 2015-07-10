6141c66a631535bc149e214cbcb47b27a6f79797
Sebastian McKenzie <sebmck@gmail.com>
2015-07-10 16:16:36 +0100

    remove internal protection

--- src/babel/tools/protect.js
+++
@@ -1,9 +0,0 @@
-import path from "path";
-
-var root = path.resolve(__dirname, "../../../");
-
-export default function (module) {
-  if (module.parent && module.parent.filename.indexOf(root) !== 0) {
-    throw new Error("Don't hotlink internal Babel files.");
-  }
-}
