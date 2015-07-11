90a5b964806092a68237ab237fac533fbd56bfef
James Kyle <me@thejameskyle.com>
2015-07-11 12:07:40 -0700

    Add descriptions to transformation/transformers/es6/arrow-functions

--- src/babel/transformation/transformers/es6/arrow-functions.js
+++ src/babel/transformation/transformers/es6/arrow-functions.js
@@ -1,11 +1,26 @@
 /**
- * [Please add a description.]
+ * Turn arrow functions into normal functions.
+ *
+ * @example
+ *
+ * **In**
+ *
+ * ```javascript
+ * arr.map(x => x * x);
+ * ```
+ *
+ * **Out**
+ *
+ * ```javascript
+ * arr.map(function (x) {
+ *   return x * x;
+ * });
  */
 
 export var visitor = {
 
   /**
-   * [Please add a description.]
+   * Look for arrow functions and turn them into normal functions.
    */
 
   ArrowFunctionExpression(node) {
