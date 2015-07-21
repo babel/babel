e7aecdce711b604a3d5e0be9ad05dff7ef910cc8
Sebastian McKenzie <sebmck@gmail.com>
2015-07-21 22:34:14 +0100

    convert comment node types from babel to espree - ref facebook/react#4449

--- eslint/babel-eslint-parser/index.js
+++ eslint/babel-eslint-parser/index.js
@@ -447,6 +447,14 @@ exports.parse = function (code) {
   ast.tokens = acornToEsprima.toTokens(tokens);
 
   // add comments
+  for (var i = 0; i < comments.length; i++) {
+    var comment = comments[i];
+    if (comment.type === "CommentBlock") {
+      comment.type = "Block";
+    } else if (comment.type === "CommentLine") {
+      comment.type = "Line";
+    }
+  }
   ast.comments = comments;
   exports.attachComments(ast, comments, tokens);
 
