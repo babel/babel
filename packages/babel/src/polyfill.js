1eab9f83f65eb9ddd04443f006d6e83dc6c08a14
Paul Betts <paul@paulbetts.org>
2015-07-21 23:53:32 -0700

    Double quotes

--- packages/babel/src/polyfill.js
+++ packages/babel/src/polyfill.js
@@ -1,4 +1,4 @@
-import packageJson from '../package.json';
+import packageJson from "../package.json";
 
 if (global._babelPolyfill === packageJson.version) {
   throw new Error(`Polyfill package ${packageJson.version} doesn't match previously loaded version: ${global._babelPolyfill}`);
