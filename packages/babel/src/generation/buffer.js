4d846e368676fe66cf9649b2fee1dc511e3913eb
Sebastian McKenzie <sebmck@gmail.com>
2015-07-28 18:54:47 +0100

    consider comment starting character to be a terminatorless separator #2106

--- packages/babel/src/generation/buffer.js
+++ packages/babel/src/generation/buffer.js
@@ -264,7 +264,7 @@ export default class Buffer {
 
         this.parenPushNewlineState = null;
 
-        if (cha === "\n") {
+        if (cha === "\n" || cha === "/") {
           // we're going to break this terminator expression so we need to add a parentheses
           this._push("(");
           this.indent();
