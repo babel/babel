39fbd550c4c8a4e5f3fe811312deb27d7c74d129
Sebastian McKenzie <sebmck@gmail.com>
2015-08-12 02:57:36 +0100

    also transforming of File asts

--- packages/babel-core/src/helpers/normalize-ast.js
+++ packages/babel-core/src/helpers/normalize-ast.js
@@ -7,9 +7,13 @@ import * as t from "babel-types";
  */
 
 export default function (ast, comments, tokens) {
-  if (ast && ast.type === "Program") {
-    return t.file(ast, comments || [], tokens || []);
-  } else {
-    throw new Error("Not a valid ast?");
+  if (ast) {
+    if (ast.type === "Program") {
+      return t.file(ast, comments || [], tokens || []);
+    } else if (ast.type === "File") {
+      return ast;
+    }
   }
+
+  throw new Error("Not a valid ast?");
 }
