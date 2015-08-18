cc73bb56b67757740b175fe296916b154dbe00f7
Henry Zhu <hi@henryzoo.com>
2015-08-18 10:52:55 -0400

    Merge pull request babel/eslint-plugin-babel#10 from aruberto/master
    add arrow-parens to index.js

--- eslint/babel-eslint-plugin/index.js
+++ eslint/babel-eslint-plugin/index.js
@@ -6,11 +6,13 @@ module.exports = {
     'new-cap': require('./rules/new-cap'),
     'object-curly-spacing': require('./rules/object-curly-spacing'),
     'object-shorthand': require('./rules/object-shorthand'),
+    'arrow-parens': require('./rules/arrow-parens'),
   },
   rulesConfig: {
     'generator-star-spacing': 0,
     'new-cap': 0,
     'object-curly-spacing': 0,
     'object-shorthand': 0,
+    'arrow-parens': 0,
   }
 };
