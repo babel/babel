298f11dccc539fef030898680334efdd12b41754
lbryant <lbryant@hubspot.com>
2015-08-21 19:18:26 -0400

    add test

---
+++ packages/babel/src/transformation/templates/umd-runner-body-global-root.js
@@ -0,0 +1,11 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(AMD_ARGUMENTS, factory);
+  } else if (COMMON_TEST) {
+    factory(COMMON_ARGUMENTS);
+  } else {
+    var mod = { exports: {} };
+    factory(mod.exports, BROWSER_ARGUMENTS);
+    global[MODULE_NAME] = mod.exports;
+  }
+});
---
+++ packages/babel/test/fixtures/transformation/es6.modules-umd/exports-umd-global-root/expected.js
@@ -0,0 +1,17 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define("mylib/es6.modules-umd/exports-umd-global-root/expected", ["exports"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global["mylib/es6.modules-umd/exports-umd-global-root/expected"] = mod.exports;
+  }
+})(this, function (exports) {
+  "use strict";
+
+  myModule();
+});
