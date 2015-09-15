6d72b025910f4d3e427150ffe9c3383eabe366cd
Sebastian McKenzie <sebmck@gmail.com>
2015-09-15 06:27:56 +0100

    add babel-preset-2015 package

---
+++ packages/babel-preset-es2015/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: [
+    require("babel-plugin-transform-es2015-template-literals"),
+    require("babel-plugin-transform-es2015-literals"),
+    require("babel-plugin-transform-es2015-function-name"),
+    require("babel-plugin-transform-es2015-arrow-functions"),
+    require("babel-plugin-transform-es2015-block-scoped-functions"),
+    require("babel-plugin-transform-es2015-classes"),
+    require("babel-plugin-transform-es2015-object-super"),
+    require("babel-plugin-transform-es2015-shorthand-properties"),
+    require("babel-plugin-transform-es2015-computed-properties"),
+    require("babel-plugin-transform-es2015-for-of"),
+    require("babel-plugin-transform-es2015-sticky-regex"),
+    require("babel-plugin-transform-es2015-unicode-regex"),
+    require("babel-plugin-transform-es2015-constants"),
+    require("babel-plugin-transform-es2015-spread"),
+    require("babel-plugin-transform-es2015-parameters"),
+    require("babel-plugin-transform-es2015-destructuring"),
+    require("babel-plugin-transform-es2015-block-scoping"),
+    require("babel-plugin-transform-es2015-tail-call")
+  ]
+};
