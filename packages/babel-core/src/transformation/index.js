b865b8862e810a1a2d460d4e2c953b3ca570312b
Sebastian McKenzie <sebmck@gmail.com>
2015-09-15 06:29:07 +0100

    remove filters, aliases, deprecations and more from plugin pipeline

--- packages/babel-core/src/transformation/index.js
+++ packages/babel-core/src/transformation/index.js
@@ -1,33 +1,6 @@
 import Pipeline from "./pipeline";
 
 var pipeline = new Pipeline;
-
-import transformers from "./transformers";
-
-for (var key in transformers) {
-  var transformer = transformers[key];
-
-  if (typeof transformer === "object") {
-    var metadata = transformer.metadata = transformer.metadata || {};
-    metadata.group = metadata.group || "builtin-basic";
-  }
-}
-
-pipeline.addTransformers(transformers);
-
-import deprecated from "./transformers/deprecated";
-pipeline.addDeprecated(deprecated);
-
-import aliases from "./transformers/aliases";
-pipeline.addAliases(aliases);
-
-import * as filters from "./transformers/filters";
-pipeline.addFilter(filters.internal);
-pipeline.addFilter(filters.blacklist);
-pipeline.addFilter(filters.whitelist);
-pipeline.addFilter(filters.stage);
-pipeline.addFilter(filters.optional);
-
 var transform = pipeline.transform.bind(pipeline);
 transform.fromAst = pipeline.transformFromAst.bind(pipeline);
 transform.lint = pipeline.lint.bind(pipeline);
--- packages/babel-core/src/transformation/pipeline.js
+++ packages/babel-core/src/transformation/pipeline.js
@@ -1,83 +1,12 @@
-import PluginManager from "./file/plugin-manager";
 import normalizeAst from "../helpers/normalize-ast";
-import Plugin from "./plugin";
-import assign from "lodash/object/assign";
 import File from "./file";
 
 export default class Pipeline {
-  transformers = Object.create(null);
-  namespaces   = Object.create(null);
-  deprecated   = Object.create(null);
-  aliases      = Object.create(null);
-  filters      = [];
-
-  addTransformers(transformers) {
-    for (var key in transformers) {
-      this.addTransformer(key, transformers[key]);
-    }
-    return this;
-  }
-
-  addTransformer(key, plugin) {
-    if (this.transformers[key]) throw new Error(); // todo: error
-
-    var namespace = key.split(".")[0];
-    this.namespaces[namespace] = this.namespaces[namespace] || [];
-    this.namespaces[namespace].push(key);
-    this.namespaces[key] = namespace;
-
-    if (typeof plugin === "function") {
-      plugin = PluginManager.memoisePluginContainer(plugin);
-      plugin.key = key;
-      plugin.metadata.optional = true;
-
-      if (key === "react.displayName") {
-        plugin.metadata.optional = false;
-      }
-    } else {
-      plugin = new Plugin(key, plugin);
-    }
-
-    this.transformers[key] = plugin;
-  }
-
-  addAliases(names) {
-    assign(this.aliases, names);
-    return this;
-  }
-
-  addDeprecated(names) {
-    assign(this.deprecated, names);
-    return this;
-  }
-
-  addFilter(filter: Function) {
-    this.filters.push(filter);
-    return this;
-  }
-
-  canRun(plugin, fileOpts) {
-    if (plugin.metadata.plugin) {
-      return true;
-    }
-
-    for (var filter of (this.filters: Array)) {
-      var result = filter(plugin, fileOpts);
-      if (result != null) return result;
-    }
-
-    return true;
-  }
-
   lint(code: string, opts?: Object = {}) {
     opts.code = false;
     opts.mode = "lint";
     return this.transform(code, opts);
   }
-
-  /**
-   * Build dependency graph by recursing `metadata.modules`. WIP.
-   */
 
   pretransform(code: string, opts?: Object) {
     var file = new File(opts, this);
@@ -106,33 +35,5 @@ export default class Pipeline {
       file.addAst(ast);
       return file.transform();
     });
-  }
-
-  _ensureTransformerNames(type: string, rawKeys: Array<string>) {
-    var keys = [];
-
-    for (var i = 0; i < rawKeys.length; i++) {
-      var key = rawKeys[i];
-      var deprecatedKey = this.deprecated[key];
-      var aliasKey = this.aliases[key];
-      if (aliasKey) {
-        keys.push(aliasKey);
-      } else if (deprecatedKey) {
-        // deprecated key, remap it to the new one
-        console.error(`[BABEL] The transformer ${key} has been renamed to ${deprecatedKey}`);
-        rawKeys.push(deprecatedKey);
-      } else if (this.transformers[key]) {
-        // valid key
-        keys.push(key);
-      } else if (this.namespaces[key]) {
-        // namespace, append all transformers within this namespace
-        keys = keys.concat(this.namespaces[key]);
-      } else {
-        // invalid key
-        throw new ReferenceError(`Unknown transformer ${key} specified in ${type}`);
-      }
-    }
-
-    return keys;
   }
 }
