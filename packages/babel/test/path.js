24d766fc57b566130189d6c2ba62a999a62bcec1
Justin Ridgewell <justin@ridgewell.name>
2015-09-30 14:07:07 -0400

    Replace arrow expression body with block statement
    Without this, the only way to replace the arrow function is to either
    manually override its `node.body`, or duplicate the arrow:
    
    ```js
    // Old
    ArrowFunctionExpression: function (node) {
      node.body = t.blockStatement(...);
      // Or
      return t.ArrowFunctionExpression(
        node.params,
        t.blockStatement(...),
        node.async
      );
    }
    
    // New
    ArrowFunctionExpression: function() {
      this.get("body").replaceWith(t.blockStatement(...));
    }
    ```

--- packages/babel/test/path.js
+++ packages/babel/test/path.js
@@ -20,4 +20,50 @@ suite("traversal path", function () {
 
     chai.expect(actualCode).to.be.equal("console.whatever();");
   });
+
+  test("replaceWith (arrow expression body to block statement body)", function () {
+    var expectCode = "var fn = () => true;";
+
+    var actualCode = transform(expectCode, {
+      blacklist: "strict",
+      plugins: [new Plugin("foobar", {
+        visitor: {
+          ArrowFunctionExpression: function () {
+            this.get("body").replaceWith({
+              type: "BlockStatement",
+              body: [{
+                type: "ReturnStatement",
+                argument: {
+                  type: "Literal",
+                  value: true
+                }
+              }]
+            });
+          }
+        }
+      })]
+    }).code;
+
+    chai.expect(actualCode).to.be.equal("var fn = function fn() {\n  return true;\n};");
+  });
+
+  test("replaceWith (arrow block statement body to expression body)", function () {
+    var expectCode = "var fn = () => { return true; }";
+
+    var actualCode = transform(expectCode, {
+      blacklist: "strict",
+      plugins: [new Plugin("foobar", {
+        visitor: {
+          ArrowFunctionExpression: function () {
+            this.get("body").replaceWith({
+              type: "Literal",
+              value: true
+            });
+          }
+        }
+      })]
+    }).code;
+
+    chai.expect(actualCode).to.be.equal("var fn = function fn() {\n  return true;\n};");
+  });
 });
