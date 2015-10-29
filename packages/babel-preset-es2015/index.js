198025aea91f8b1f36a1d95203eb8a25e61bee04
Sebastian McKenzie <sebmck@gmail.com>
2015-10-29 23:49:03 +0000

    add babel-plugin-transform-es2015-modules-commonjs to babel-preset-es2015

--- packages/babel-preset-es2015/index.js
+++ packages/babel-preset-es2015/index.js
@@ -18,6 +18,7 @@ module.exports = {
     require("babel-plugin-transform-es2015-destructuring"),
     require("babel-plugin-transform-es2015-block-scoping"),
     require("babel-plugin-transform-es2015-typeof-symbol"),
-    require("babel-plugin-transform-regenerator")
+    require("babel-plugin-transform-es2015-modules-commonjs"),
+    require("babel-plugin-transform-regenerator"),
   ]
 };
