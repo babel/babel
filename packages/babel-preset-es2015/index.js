d3268a5cda9ea934ead6fd41b0ad7f9308de1906
Sebastian McKenzie <sebmck@gmail.com>
2015-10-30 17:59:54 +0000

    when using regenerator in the es2015 preset, don't transform async and asyncGenerators

--- packages/babel-preset-es2015/index.js
+++ packages/babel-preset-es2015/index.js
@@ -19,6 +19,6 @@ module.exports = {
     require("babel-plugin-transform-es2015-block-scoping"),
     require("babel-plugin-transform-es2015-typeof-symbol"),
     require("babel-plugin-transform-es2015-modules-commonjs"),
-    require("babel-plugin-transform-regenerator"),
+    [require("babel-plugin-transform-regenerator"), { async: false, asyncGenerators: false }],
   ]
 };
