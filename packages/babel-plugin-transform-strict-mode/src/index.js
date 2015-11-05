5802e2e334770d0c2e879b4e42acb63348573d92
Sebastian McKenzie <sebmck@gmail.com>
2015-11-05 11:04:14 +0000

    add strict option to bable-plugin-transform-strict-mode

--- packages/babel-plugin-transform-strict-mode/src/index.js
+++ packages/babel-plugin-transform-strict-mode/src/index.js
@@ -3,9 +3,11 @@ import * as t from "babel-types";
 export default function () {
   return {
     visitor: {
-      Program(path) {
-        let { node } = path;
+      Program(path, state) {
+        if (state.opts.strict === false) return;
         
+        let { node } = path;
+
         for (let directive of (node.directives: Array<Object>)) {
           if (directive.value.value === "use strict") return;
         }
