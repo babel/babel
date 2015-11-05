35b5ff103cf30a149d5576dd5e67f59f1a7c30ae
Sebastian McKenzie <sebmck@gmail.com>
2015-11-05 10:00:57 +0000

    fix mocha.opts reference

--- scripts/test-cov.sh
+++ scripts/test-cov.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha -- `scripts/_get-test-directories.sh` --opts mocha.opts
+node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha -- `scripts/_get-test-directories.sh` --opts test/mocha.opts
 test -n "`which open`" && open coverage/lcov-report/index.html
