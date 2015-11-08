0f90b6eaf2ca757834eacdaa2ed90addecebddc4
James Kyle <me@thejameskyle.com>
2015-11-08 15:35:14 -0800

    Add link to npm upgrade guide on windows. Re: https://github.com/babel/babel/commit/1c3b4aa41088a9167646143f5116a02af0b8d409#commitcomment-14211658

--- packages/babel-cli/src/babel-doctor/rules/npm-3.js
+++ packages/babel-cli/src/babel-doctor/rules/npm-3.js
@@ -5,7 +5,11 @@ export default function () {
   return new Promise(function (resolve) {
     binVersionCheck("npm", ">=3.3.0", function (err) {
       if (err) {
-        resolve([false, `Your npm version is outdated. Upgrade to the latest version by running:\n$ ${chalk.magenta("npm install -g npm")}`]);
+        let message = `Your npm version is outdated. Upgrade to the latest version by running:\n$ ${chalk.magenta("npm install -g npm")}.`;
+        if (process.platform === 'win32') {
+          message += ` See this guide if you are having trouble upgrading: ${chalk.underline.blue("https://github.com/npm/npm/wiki/Troubleshooting#upgrading-on-windows")}`;
+        }
+        resolve([false, message]);
       } else {
         resolve([true, "You're on npm >=3.3.0"]);
       }
