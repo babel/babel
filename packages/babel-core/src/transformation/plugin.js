defa9e1b093d2384b898573debd5ef2b343d0829
Sebastian McKenzie <sebmck@gmail.com>
2015-11-08 06:23:30 -0800

    change template literal to normal string

--- packages/babel-core/src/transformation/plugin.js
+++ packages/babel-core/src/transformation/plugin.js
@@ -84,7 +84,7 @@ export default class Plugin extends Store {
   normaliseVisitor(visitor: Object): Object {
     for (let key of GLOBAL_VISITOR_PROPS) {
       if (visitor[key]) {
-        throw new Error(`Plugins aren't allowed to specify catch-all enter/exit handlers. Please target individual nodes.`);
+        throw new Error("Plugins aren't allowed to specify catch-all enter/exit handlers. Please target individual nodes.");
       }
     }
 
