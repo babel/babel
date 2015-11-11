f601e626780df3b33773ca21ff62752afea14c72
Sebastian McKenzie <sebmck@gmail.com>
2015-11-10 16:02:45 -0800

    add regression test - fixes #2941

---
+++ packages/babel-plugin-transform-es2015-classes/test/fixtures/regression/2941/expected.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _class = function _class() {
+  babelHelpers.classCallCheck(this, _class);
+};
+
+exports.default = _class;
