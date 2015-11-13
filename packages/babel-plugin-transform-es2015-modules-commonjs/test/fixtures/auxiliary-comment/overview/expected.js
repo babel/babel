1563e74cc0cc7a922f6082cd8c40054bfffb317e
kpdecker <kpdecker@gmail.com>
2015-11-13 12:42:15 -0600

    Add a few auxiliary comment tests

---
+++ packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/auxiliary-comment/overview/expected.js
@@ -0,0 +1,38 @@
+/*before*/"use strict";
+
+/*before*/Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/*before*/exports.test = undefined;
+/*before*/
+require("foo");
+
+/*before*/
+require("foo-bar");
+
+/*before*/
+require("./directory/foo-bar");
+
+/*before*/
+var _foo = require("foo2");
+
+/*before*/
+var _foo2 = babelHelpers.interopRequireDefault(_foo);
+
+/*before*/
+var _foo3 = require("foo3");
+
+/*before*/var /*after*/foo2 = babelHelpers.interopRequireWildcard(_foo3);
+/*before*/
+var _foo4 = require("foo4");
+
+/*before*/
+var _foo5 = require("foo5");
+
+/*before*/exports. /*after*/test = test;
+var test = /*before*/exports. /*after*/test = 5;
+
+/*before*/_foo4. /*after*/bar;
+/*before*/_foo5. /*after*/foo;
+/*before*/_foo2.default;
+/*after*/
