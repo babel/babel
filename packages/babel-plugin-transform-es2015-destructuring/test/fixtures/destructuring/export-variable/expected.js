29a31762762b5815bdba2f905e2e5fae9b5167c2
Logan Smyth <loganfsmyth@gmail.com>
2015-12-05 14:27:42 -0800

    Move destructuring statements off export statements before processing - fixes T6720

---
+++ packages/babel-plugin-transform-es2015-destructuring/test/fixtures/destructuring/export-variable/expected.js
@@ -0,0 +1,8 @@
+var _ref = {};
+var a = _ref.a;
+var b = _ref.b;
+var _ref$c = _ref.c;
+var d = _ref$c.d;
+var _ref$c$e$f = _ref$c.e.f;
+var f = _ref$c$e$f === undefined ? 4 : _ref$c$e$f;
+export { a, b, d, f };
