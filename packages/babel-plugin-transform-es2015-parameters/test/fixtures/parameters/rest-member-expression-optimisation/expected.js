3dfb4eb8c62e7d5a7f90c6c5d87ed2b5bc3eed6b
Victor Felder <victorfelder@gmail.com>
2015-12-14 11:30:01 +0100

    Simplify rest transformation

--- packages/babel-plugin-transform-es2015-parameters/test/fixtures/parameters/rest-member-expression-optimisation/expected.js
+++ packages/babel-plugin-transform-es2015-parameters/test/fixtures/parameters/rest-member-expression-optimisation/expected.js
@@ -1,11 +1,11 @@
 var t = function () {
-  var x = arguments.length <= 0 || arguments[0] === undefined ? undefined : arguments[0];
-  var y = arguments.length <= 1 || arguments[1] === undefined ? undefined : arguments[1];
+  var x = arguments.length <= 0 ? undefined : arguments[0];
+  var y = arguments.length <= 1 ? undefined : arguments[1];
 };
 
 function t() {
-  var x = arguments.length <= 0 || arguments[0] === undefined ? undefined : arguments[0];
-  var y = arguments.length <= 1 || arguments[1] === undefined ? undefined : arguments[1];
+  var x = arguments.length <= 0 ? undefined : arguments[0];
+  var y = arguments.length <= 1 ? undefined : arguments[1];
 }
 
 function t() {
