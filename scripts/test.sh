87b7495eedb9fdc32595acb73c96401f46e821e0
Krasimir Tsonev <krasimir@outset.ws>
2016-01-06 07:12:32 +0200

    Adding --watch option to mocha call if TEST_WATCH is supplied

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
 
+MOCHA_OPS="test/mocha.opts"
+MOCHA_WATCH=""
+
 if [ -z "$TEST_GREP" ]; then
    TEST_GREP=""
 fi
 
-node node_modules/mocha/bin/_mocha `scripts/_get-test-directories.sh` --opts test/mocha.opts --grep "$TEST_GREP"
+if [ "$TEST_WATCH" ]; then
+   MOCHA_WATCH="--watch"
+fi
+
+node node_modules/mocha/bin/_mocha `scripts/_get-test-directories.sh` --opts $MOCHA_OPS --grep "$TEST_GREP" $MOCHA_WATCH
