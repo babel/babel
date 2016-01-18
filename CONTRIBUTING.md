e367d6eaed0d903e105d3177bd11f3e92b9a412c
Krasimir Tsonev <krasimir@outset.ws>
2016-01-18 05:42:21 +0200

    Removing TEST_WATCH but adding TEST_DEBUG

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -66,10 +66,10 @@ This is mostly overkill and you can limit the package to a select by using the `
 $ TEST_ONLY=babel-cli make test
 ```
 
-If you want to run tests continuously:
+If you want to run tests with the debugger of node:
 
 ```sh
-TEST_ONLY=babel-cli TEST_WATCH=true make test
+TEST_ONLY=babel-cli TEST_DEBUG=true make test
 ```
 
 Use the `TEST_GREP` variable to run a subset of tests by name:
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-MOCHA_WATCH=""
+NODE_DEBUG=""
 
 if [ -z "$TEST_GREP" ]; then
    TEST_GREP=""
 fi
 
-if [ "$TEST_WATCH" ]; then
-   MOCHA_WATCH="--watch"
+if [ "$TEST_DEBUG" ]; then
+   NODE_DEBUG="debug"
 fi
 
-node node_modules/mocha/bin/_mocha `scripts/_get-test-directories.sh` --opts test/mocha.opts --grep "$TEST_GREP" $MOCHA_WATCH
+node $NODE_DEBUG node_modules/mocha/bin/_mocha `scripts/_get-test-directories.sh` --opts test/mocha.opts --grep "$TEST_GREP"
