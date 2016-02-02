0878685a5a8f1916fbe95e4277d3d2d57ce7d06b
Joshua Peek <josh@joshpeek.com>
2016-02-01 17:03:13 -0800

    Defer requiring chokidar dependency until --watch flag is passed

--- packages/babel-cli/src/babel/file.js
+++ packages/babel-cli/src/babel/file.js
@@ -1,7 +1,6 @@
 let convertSourceMap = require("convert-source-map");
 let pathExists       = require("path-exists");
 let sourceMap        = require("source-map");
-let chokidar         = require("chokidar");
 let slash            = require("slash");
 let path             = require("path");
 let util             = require("./util");
@@ -133,6 +132,7 @@ module.exports = function (commander, filenames, opts) {
     walk();
 
     if (commander.watch) {
+      let chokidar = require("chokidar");
       chokidar.watch(filenames, {
         persistent: true,
         ignoreInitial: true
