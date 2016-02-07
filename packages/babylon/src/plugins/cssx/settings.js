11ed3bdcb7e5c2c70350f1b72952b4eb71ee21f8
Krasimir Tsonev <krasimir@outset.ws>
2016-02-07 06:22:42 +0200

    Allowing ! and ? in the cssx values

--- packages/babylon/src/plugins/cssx/settings.js
+++ packages/babylon/src/plugins/cssx/settings.js
@@ -6,7 +6,7 @@ const CSSXPropertyAllowedCodes = [
 ].map(stringToCode);
 
 const CSSXValueAllowedCodes = [
-  ' ' , '\n', '\t', '#', '.', '-', '(', ')', '[', ']', '\'', '"', '%', ',', ':', '/', '\\'
+  ' ' , '\n', '\t', '#', '.', '-', '(', ')', '[', ']', '\'', '"', '%', ',', ':', '/', '\\', '!', '?'
 ].map(stringToCode);
 
 const CSSXSelectorAllowedCodes = [
