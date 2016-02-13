9e383b09b90db7f99bb4c458001b57b65bff815f
Logan Smyth <loganfsmyth@gmail.com>
2016-02-12 22:42:05 -0800

    Clean up babylon bundle to allow it to be re-bundled - fixes T6930

--- packages/babylon/scripts/prepublish.js
+++ packages/babylon/scripts/prepublish.js
@@ -1,2 +1,8 @@
 require("./_util").updateMain("index.js");
-require("child_process").execSync(__dirname + "/../../../node_modules/.bin/browserify -s babylon -e " + __dirname + "/../lib/index.js -o " + __dirname + "/../index.js", { encoding: "utf8" });
+require("child_process").execSync(
+  __dirname + "/../../../node_modules/.bin/browserify " +
+  "--standalone babylon " +
+  "--entry " + __dirname + "/../lib/index.js " +
+  "--plugin bundle-collapser/plugin " +
+  "--plugin derequire/plugin " +
+  "--outfile " + __dirname + "/../index.js", { encoding: "utf8" });
