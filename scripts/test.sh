028d1c644c25460fecb252c66fa7478c20fc9c5e
Krasimir Tsonev <krasimir@outset.ws>
2016-02-21 18:25:53 +0200

    Reverting the changes in scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 set -e
 
-NODE_DEBUG=""
-
 if [ -z "$TEST_GREP" ]; then
    TEST_GREP=""
 fi
 
-if [ "$TEST_DEBUG" ]; then
-   NODE_DEBUG="debug"
-fi
-
-node $NODE_DEBUG node_modules/mocha/bin/_mocha `scripts/_get-test-directories.sh` --opts test/mocha.opts --grep "$TEST_GREP"
+node node_modules/mocha/bin/_mocha `scripts/_get-test-directories.sh` --opts test/mocha.opts --grep "$TEST_GREP"
