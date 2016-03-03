9a180797c073b9644b0cdfa51e1c4f3ce3d672d3
Amjad Masad <amjad.masad@gmail.com>
2016-03-03 14:49:20 -0800

    Revert "Remove flow"
    This reverts commit 2827ff6b01dcce69e9d3c0402e96b52b3a2a47ee.

--- src/tokenizer/state.js
+++ src/tokenizer/state.js
@@ -1,83 +1,129 @@
+import type { TokContext } from "./context";
+import type { Token } from "./index";
 import { Position } from "../util/location";
 import { types as ct } from "./context";
 import { types as tt } from "./types";
 
 export default class State {
-  init(options, input) {
-    // TODO
+  init(options: Object, input: string) {
     this.strict = options.strictMode === false ? false : options.sourceType === "module";
 
-    // TODO
     this.input = input;
 
-    // Used to signify the start of a potential arrow function
     this.potentialArrowAt = -1;
 
-    // Flags to track whether we are in a function, a generator.
     this.inMethod = this.inFunction = this.inGenerator = this.inAsync = false;
 
-    // Labels in scope.
     this.labels = [];
 
-    // Leading decorators.
     this.decorators = [];
 
-    // Token store.
     this.tokens = [];
 
-    // Comment store.
     this.comments = [];
 
-    // Comment attachment store
     this.trailingComments = [];
     this.leadingComments  = [];
     this.commentStack     = [];
 
-    // The current position of the tokenizer in the input.
     this.pos = this.lineStart = 0;
     this.curLine = 1;
 
-    // Properties of the current token:
-    // Its type
     this.type = tt.eof;
-
-    // For tokens that include more information than their type, the value
     this.value = null;
-
-    // Its start and end offset
     this.start = this.end = this.pos;
-
-    // And, if locations are used, the {line, column} object
-    // corresponding to those offsets
     this.startLoc = this.endLoc = this.curPosition();
 
-    // Position information for the previous token
     this.lastTokEndLoc = this.lastTokStartLoc = null;
     this.lastTokStart = this.lastTokEnd = this.pos;
 
-    // The context stack is used to superficially track syntactic
-    // context to predict whether a regular expression is allowed in a
-    // given position.
     this.context = [ct.b_stat];
     this.exprAllowed = true;
 
-    // Used to signal to callers of `readWord1` whether the word
-    // contained any escape sequences. This is needed because words with
-    // escape sequences must not be interpreted as keywords.
-    this.containsEsc = false;
-
-    // TODO
-    this.containsOctal = false;
+    this.containsEsc = this.containsOctal = false;
     this.octalPosition = null;
 
     return this;
   }
 
+  // TODO
+  strict: boolean;
+
+  // TODO
+  input: string;
+
+  // Used to signify the start of a potential arrow function
+  potentialArrowAt: number;
+
+  // Flags to track whether we are in a function, a generator.
+  inFunction: boolean;
+  inGenerator: boolean;
+  inMethod: boolean;
+
+  // Labels in scope.
+  labels: Array<Object>;
+
+  // Leading decorators.
+  decorators: Array<Object>;
+
+  // Token store.
+  tokens: Array<Object>;
+
+  // Comment store.
+  comments: Array<Object>;
+
+  // Comment attachment store
+  trailingComments: Array<Object>;
+  leadingComments: Array<Object>;
+  commentStack: Array<Object>;
+
+  // The current position of the tokenizer in the input.
+  pos: number;
+  lineStart: number;
+  curLine: number;
+
+  // Properties of the current token:
+  // Its type
+  type: Token;
+
+  // For tokens that include more information than their type, the value
+  value: any;
+
+  // Its start and end offset
+  start: number;
+  end: number;
+
+  // And, if locations are used, the {line, column} object
+  // corresponding to those offsets
+  startLoc: Position;
+  endLoc: Position;
+
+  // Position information for the previous token
+  lastTokEndLoc: ?Position;
+  lastTokStartLoc: ?Position;
+  lastTokStart: number;
+  lastTokEnd: number;
+
+  // The context stack is used to superficially track syntactic
+  // context to predict whether a regular expression is allowed in a
+  // given position.
+  context: Array<TokContext>;
+  exprAllowed: boolean;
+
+  // Used to signal to callers of `readWord1` whether the word
+  // contained any escape sequences. This is needed because words with
+  // escape sequences must not be interpreted as keywords.
+  containsEsc: boolean;
+
+  // TODO
+  containsOctal: boolean;
+  octalPosition: ?number;
+
   curPosition() {
     return new Position(this.curLine, this.pos - this.lineStart);
   }
 
-  clone(skipArrays) {
+  clone(skipArrays?) {
     let state = new State;
     for (let key in this) {
       let val = this[key];
