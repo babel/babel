a46e9738e5fdc5fa6a4e3ddd97f977b4241f58e2
Bouke van der Bijl <boukevanderbijl@gmail.com>
2016-03-06 14:59:02 -0500

    Add applyPlugin function to File
    This makes it possible to apply all the AST transformations without
    creating a result, so that the caller can apply any extra
    transformations to the AST if they want to before generating a result.

--- packages/babel-core/src/transformation/file/index.js
+++ packages/babel-core/src/transformation/file/index.js
@@ -437,7 +437,7 @@ export default class File extends Store {
     this.log.debug("End set AST");
   }
 
-  transform(): BabelFileResult {
+  applyPlugins() {
     // In the "pass per preset" mode, we have grouped passes.
     // Otherwise, there is only one plain pluginPasses array.
     this.pluginPasses.forEach((pluginPasses, index) => {
@@ -447,7 +447,10 @@ export default class File extends Store {
       this.log.debug(`End transform traverse`);
       this.call("post", pluginPasses);
     });
+  }
 
+  transform(): BabelFileResult {
+    this.applyPlugins();
     return this.generate();
   }
 
