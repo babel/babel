ad3d5bea44c6488fca9c6044ee53f6e678c8249f
Amjad Masad <amjad.masad@gmail.com>
2016-03-08 15:28:29 -0800

    Add more test cases

--- packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/source-map/exec.js
+++ packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/source-map/exec.js
@@ -7,7 +7,12 @@ var tests = [
   'import {bar2, baz} from "foo";',
   'import {bar as baz2} from "foo";',
   'import {bar as baz3, xyz} from "foo";',
+  'import bar, * as bar2 from "foo";',
+  'import bar, {bar2, bar3 as bar4} from "foo";',
 
+  'export var a;',
+  'export default function(){};',
+  'export default function f(){};',
   'export default 42;',
   'export {foo};',
   'export { foo as default };',
