256c0100cbff9212f526e2639e3103246283b99c
Logan Smyth <loganfsmyth@gmail.com>
2016-03-12 15:23:42 -0800

    Wrap parens around default exports starting with function/class - fixes T7136

--- packages/babel-generator/test/fixtures/harmony-edgecase/exports/expected.js
+++ packages/babel-generator/test/fixtures/harmony-edgecase/exports/expected.js
@@ -5,5 +5,16 @@ export { a as b, c as d };
 export {};
 export default i = 20;
 export function test() {}
+export class test2 {}
 export var i = 20;
 export let i = 42;
+
+export default (function () {})();
+export default (class {})();
+export default {};
+
+export default function test3() {}
+export default class test4 {}
+
+export default function () {}
+export default class {}
