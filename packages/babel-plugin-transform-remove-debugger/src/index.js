279d3370163355ae6ca3c25a5b2728739f35f3e9
flames of love <mechanicofthesequence@gmail.com>
2016-04-05 22:25:25 +0200

    remove debugger statements’ parent if it’s the statement parent

--- packages/babel-plugin-transform-remove-debugger/src/index.js
+++ packages/babel-plugin-transform-remove-debugger/src/index.js
@@ -2,6 +2,8 @@ export default function () {
   return {
     visitor: {
       DebuggerStatement(path) {
+        let parent = path.getStatementParent();
+        if (parent === path.parentPath) parent.remove();
         path.remove();
       }
     }
