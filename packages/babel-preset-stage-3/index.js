17ce0a57c5f0c488f520b7ce6cb18a142cdd5dd6
Jacob Rask <jacob@jacobrask.net>
2016-06-22 18:16:14 +0200

    Promote trailing-function-commas to stage 3 (#3522)
    See https://github.com/tc39/proposals

--- packages/babel-preset-stage-3/index.js
+++ packages/babel-preset-stage-3/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require("babel-plugin-syntax-trailing-function-commas"),
     require("babel-plugin-transform-async-to-generator"),
     require("babel-plugin-transform-exponentiation-operator")
   ]
