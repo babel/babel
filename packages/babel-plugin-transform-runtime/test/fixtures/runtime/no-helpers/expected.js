8d14f9f4d057efdae3bc0892732497557bfb9678
Sebastian McKenzie <kittens@users.noreply.github.com>
2016-07-27 15:36:58 +0100

    Add `helpers: false` option to babel-plugin-transform-runtime (#3603)

---
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime/no-helpers/expected.js
@@ -0,0 +1,5 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+let Foo = function Foo() {
+  _classCallCheck(this, Foo);
+};
