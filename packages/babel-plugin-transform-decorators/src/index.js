2a89729e0590ec4dafc4421931574480e7f36857
Henry Zhu <hi@henryzoo.com>
2016-08-02 20:38:44 -0400

    Show a more informative error message when using the decorator transform and link to the legacy one [skip ci]

--- packages/babel-plugin-transform-decorators/src/index.js
+++ packages/babel-plugin-transform-decorators/src/index.js
@@ -71,7 +71,20 @@ export default function ({ types: t }) {
   }
 
   function doError(path) {
-    throw path.buildCodeFrameError("Decorators are not supported yet in 6.x pending proposal update.");
+    throw path.buildCodeFrameError(
+`Decorators are not officially supported yet in 6.x pending a proposal update.
+However, if you need to use them you can install the legacy decorators transform with:
+
+npm install babel-plugin-transform-decorators-legacy --save-dev
+
+and add the following line to your .babelrc file:
+
+{
+  "plugins": ["transform-decorators-legacy"]
+}
+
+The repo url is: https://github.com/loganfsmyth/babel-plugin-transform-decorators-legacy.
+    `);
   }
 
   return {
