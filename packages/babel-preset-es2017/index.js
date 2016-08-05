9eadc46866c9c64a68cf68c8c7eeb5312bf2e4c3
Marco Bettiolo <marco@bettiolo.it>
2016-08-05 15:30:01 -0700

    Using double quoted strings

--- packages/babel-preset-es2017/index.js
+++ packages/babel-preset-es2017/index.js
@@ -2,12 +2,12 @@ module.exports = {
   plugins: [
     // ES2017: Async Functions
     // https://github.com/tc39/ecmascript-asyncawait
-    require('babel-plugin-transform-async-to-generator'),
+    require("babel-plugin-transform-async-to-generator"),
 
     // ES2017: Trailing commas in function parameter lists and calls
     // https://jeffmo.github.io/es-trailing-function-commas/
-    require('babel-plugin-syntax-trailing-function-commas'),
+    require("babel-plugin-syntax-trailing-function-commas"),
   ]
 };
 
-require('./polyfill');
+require("./polyfill");
