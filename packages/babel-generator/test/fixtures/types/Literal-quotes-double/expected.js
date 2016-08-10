f09cbf4b20e4a84054d8cd774d210453fb5ece02
David Pfahler <david@excellenteasy.com>
2016-08-10 15:06:40 +0200

    test(babel-generator): Literal w/ different opts
    Literal tests for combinations of the options `format.quotes` and `format.avoidEscapingQuotes`.

---
+++ packages/babel-generator/test/fixtures/types/Literal-quotes-double/expected.js
@@ -0,0 +1,17 @@
+5;
+5.0;
+"foobar";
+"\n\r\u2028\u2029";
+"\n\r\u2028\u2029";
+"He said: \"...\"";
+"He said: '...'";
+"He said: \"...\"";
+"He said: \"...\", then later he said: \"...\"";
+/foobar/g;
+null;
+true;
+false;
+5.;
+0b10;
+0o70;
+0X1F;
