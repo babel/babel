55ec6306dc09229b36fe44af682b72b133687a6f
Henry Baez <0072016@users.noreply.github.com>
2016-08-11 19:25:44 -0700

    update GULP v1.9.3 ruby gem

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,51 +1,11 @@
-var plumber = require("gulp-plumber");
-var through = require("through2");
-var chalk   = require("chalk");
-var newer   = require("gulp-newer");
-var babel   = require("gulp-babel");
-var watch   = require("gulp-watch");
-var gutil   = require("gulp-util");
-var gulp    = require("gulp");
-var path    = require("path");
 
-var scripts = "./packages/*/src/**/*.js";
-var dest = "packages";
-
-var srcEx, libFragment;
-
-if (path.win32 === path) {
-  srcEx = /(packages\\[^\\]+)\\src\\/;
-  libFragment = "$1\\lib\\";
-} else {
-  srcEx = new RegExp("(packages/[^/]+)/src/");
-  libFragment = "$1/lib/";
-}
-
-gulp.task("default", ["build"]);
-
-gulp.task("build", function () {
-  return gulp.src(scripts)
-    .pipe(plumber({
-      errorHandler: function (err) {
-        gutil.log(err.stack);
-      }
-    }))
-    .pipe(through.obj(function (file, enc, callback) {
-      file._path = file.path;
-      file.path = file.path.replace(srcEx, libFragment);
-      callback(null, file);
-    }))
-    .pipe(newer(dest))
-    .pipe(through.obj(function (file, enc, callback) {
-      gutil.log("Compiling", "'" + chalk.cyan(file._path) + "'...");
-      callback(null, file);
-    }))
-    .pipe(babel())
-    .pipe(gulp.dest(dest));
-});
-
-gulp.task("watch", ["build"], function (callback) {
-  watch(scripts, {debounceDelay: 200}, function () {
-    gulp.start("build");
-  });
-});
+   - var::wpHandle 
+   -  # .scrutinizer-ci.com
+   -  <?php
+   -  Xmxg8
+   -  Xmx8g
+   -  /**
+   - {{{hr/}}}
+    -  */
+    ?>
+    @0072016
