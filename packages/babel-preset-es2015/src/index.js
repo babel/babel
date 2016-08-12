49bdb12b989a6d2b2cd54def6023a7d8de618cc9
Dom Talbot <domjtalbot@users.noreply.github.com>
2016-08-12 15:29:43 +0100

    Correct more linting errors for pull request 3652

--- packages/babel-preset-es2015/src/index.js
+++ packages/babel-preset-es2015/src/index.js
@@ -39,11 +39,11 @@ function preset(context, opts) {
 
   if (typeof loose !== "boolean") throw new Error("Preset es2015 'loose' option must be a boolean.");
   if (typeof strict !== "boolean") {
-    throw new Error("Preset es2015 'strict' option must be a boolean.")
-  };
+    throw new Error("Preset es2015 'strict' option must be a boolean.");
+  }
   if (typeof allowTopLevelThis !== "boolean") {
-    throw new Error("Preset es2015 'allowTopLevelThis' option must be a boolean.")
-  };
+    throw new Error("Preset es2015 'allowTopLevelThis' option must be a boolean.");
+  }
   if (modules !== false && moduleTypes.indexOf(modules) === -1) {
     throw new Error("Preset es2015 'modules' option must be 'false' to indicate no modules\n" +
       "or a module type which be be one of: 'commonjs' (default), 'amd', 'umd', 'systemjs'");
