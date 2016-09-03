9c028bd5bd4443bf0494fed1e99bed802122cbce
Justin Johansson <thebabellion@gmail.com>
2016-09-03 14:51:58 +0930

    Convert remaining internal requires in presets to ES2015 import syntax

--- packages/babel-preset-latest/src/index.js
+++ packages/babel-preset-latest/src/index.js
@@ -3,8 +3,8 @@ import * as presetES2016 from "babel-preset-es2016";
 import * as presetES2017 from "babel-preset-es2017";
 
 //Rather than exporting a default object to represent the preset, we can
-//export a default function instead as this preset demonstrates. This
-//allows one to further configure a preset by specific options.
+//also export a default function instead, as this preset demonstrates.
+//This allows one to further configure a preset by way of specific options.
 export default function (context, opts = {}) {
   return {
     presets: [
--- packages/babel-preset-react/src/index.js
+++ packages/babel-preset-react/src/index.js
@@ -4,23 +4,45 @@ import transformSyntaxFlow from "babel-plugin-syntax-flow";
 import transformSyntaxJSX from "babel-plugin-syntax-jsx";
 import transformReactDisplayName from "babel-plugin-transform-react-display-name";
 
+//These imports not yet used...
 //import transformReactJSXSource from "babel-plugin-transform-react-jsx-source";
 //import transformReactJSXSelf from "babel-plugin-transform-react-jsx-self";
 
-export default {
-  plugins: [
-    transformReactJSX,
-    transformFlowStripTypes,
-    transformSyntaxFlow,
-    transformSyntaxJSX,
-    transformReactDisplayName
-  ],
-  env: {
-    development: {
-      plugins: [
-        // transformReactJSXSource,
-        // transformReactJSXSelf
-      ]
+export default function (/*context*//*, opts = {}*/) {
+  return {
+    plugins: [
+      transformReactJSX,
+      transformFlowStripTypes,
+      transformSyntaxFlow,
+      transformSyntaxJSX,
+      transformReactDisplayName
+    ],
+    env: {
+      development: {
+        plugins: [
+//          transformReactJSXSource,
+//          transformReactJSXSelf
+        ]
+      }
     }
-  }
-};
+  };
+}
+
+//Alternatively, via a default object...
+//export default {
+//  plugins: [
+//    transformReactJSX,
+//    transformFlowStripTypes,
+//    transformSyntaxFlow,
+//    transformSyntaxJSX,
+//    transformReactDisplayName
+//  ],
+//  env: {
+//    development: {
+//      plugins: [
+//        // transformReactJSXSource,
+//        // transformReactJSXSelf
+//      ]
+//    }
+//  }
+//};
