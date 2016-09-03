71dff8c834191356bfc58d24f37e3601df96987e
Daniel Tschinder <daniel@tschinder.de>
2016-09-03 18:12:34 +0200

    Allow overwritting of sourceRoot (#3685)
    Also replaces lodash/extend with Object.assign

--- packages/babel-register/src/node.js
+++ packages/babel-register/src/node.js
@@ -48,10 +48,11 @@ function compile(filename) {
   let result;
 
   // merge in base options and resolve all the plugins and presets relative to this file
-  let opts = new OptionManager().init(extend(deepClone(transformOpts), {
-    filename,
-    sourceRoot: path.dirname(filename)
-  }));
+  let opts = new OptionManager().init(extend(
+    { sourceRoot: path.dirname(filename) }, // sourceRoot can be overwritten
+    deepClone(transformOpts),
+    { filename }
+  ));
 
   let cacheKey = `${JSON.stringify(opts)}:${babel.version}`;
 
