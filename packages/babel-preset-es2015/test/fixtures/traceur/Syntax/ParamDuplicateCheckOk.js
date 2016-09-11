bc58d7587b7b166c8d2979982a9c440fbe6fdb35
Timothy Gu <timothygu99@gmail.com>
2016-09-11 13:51:22 -0700

    Remove ES2015 tests than do not parse in ES2016
    These tests are originally from Traceur, which does not fully support
    ES2016.
    
    See babel/babylon#106 for a rationale why these code blocks are not
    valid ES2016.

--- packages/babel-preset-es2015/test/fixtures/traceur/Syntax/ParamDuplicateCheckOk.js
+++ packages/babel-preset-es2015/test/fixtures/traceur/Syntax/ParamDuplicateCheckOk.js
@@ -1,11 +1,3 @@
-function f(x, y = function(x) {}) {
-  'use strict';
-}
-
-var f2 = (x, y = function(x) {}) => {
-  'use strict';
-};
-
 function g() {
   'use strict';
   function h(x, y = function(x) {}) {
