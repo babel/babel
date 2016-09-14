1dc13163fa7d3eed3c71539f499f63de04908f47
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-09-14 14:54:58 +0200

    fix default exported classes without a name
    This correctly requeues class without name so the es3 transform can
    transform the default keyword.

--- packages/babel-plugin-transform-es2015-modules-commonjs/src/index.js
+++ packages/babel-plugin-transform-es2015-modules-commonjs/src/index.js
@@ -267,6 +267,11 @@ export default function () {
                   ]);
                 } else {
                   path.replaceWith(buildExportsAssignment(defNode, t.toExpression(declaration.node)));
+
+                  // Manualy re-queue `export default class {}` expressions so that the ES3 transform
+                  // has an opportunity to convert them. Ideally this would happen automatically from the
+                  // replaceWith above. See T7166 for more info.
+                  path.parentPath.requeue(path.get("expression.left"));
                 }
               } else {
                 path.replaceWith(buildExportsAssignment(t.identifier("default"), declaration.node));
