8d753bf4ba0de31bd2d8f74174ff23e50fbcdc8a
Kai Cataldo <kaicataldo@gmail.com>
2016-09-18 22:58:30 -0400

    Fix babel-plugin-transform-es2015-modules-umd tests

--- packages/babel-plugin-transform-es2015-modules-umd/test/fixtures/umd/export-from/expected.js
+++ packages/babel-plugin-transform-es2015-modules-umd/test/fixtures/umd/export-from/expected.js
@@ -1,26 +1,25 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(["exports"], factory);
+    define(["exports", "foo"], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports);
+    factory(exports, require("foo"));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports);
+    factory(mod.exports, global.foo);
     global.actual = mod.exports;
   }
-})(this, function (exports) {
+})(this, function (exports, _foo) {
   "use strict";
 
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
-  exports.foo = foo;
-  exports.foo = foo;
-  exports.bar = bar;
-  exports.bar = foo;
-  exports.default = foo;
-  exports.default = foo;
-  exports.bar = bar;
+  Object.defineProperty(exports, "foo", {
+    enumerable: true,
+    get: function () {
+      return _foo.foo;
+    }
+  });
 });
--- packages/babel-plugin-transform-es2015-modules-umd/test/fixtures/umd/exports-variable/actual.js
+++ packages/babel-plugin-transform-es2015-modules-umd/test/fixtures/umd/exports-variable/actual.js
@@ -1,9 +1,9 @@
 export var foo = 1;
-export var foo = 1, bar = 2;
-export var foo2 = function () {};
-export var foo3;
-export let foo4 = 2;
-export let foo5;
-export const foo6 = 3;
-export function foo7 () {}
-export class foo8 {}
+export var foo2 = 1, bar = 2;
+export var foo3 = function () {};
+export var foo4;
+export let foo5 = 2;
+export let foo6;
+export const foo7 = 3;
+export function foo8 () {}
+export class foo9 {}
