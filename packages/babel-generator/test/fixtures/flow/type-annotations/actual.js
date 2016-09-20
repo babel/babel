cb4b2b07444fd7c767d68878f4f8a8c646190c2e
Basil Hosmer <bhosmer@fb.com>
2016-09-20 02:10:55 -0700

    flow plugin: generate exact object type annotations (#3702)

--- packages/babel-generator/test/fixtures/flow/type-annotations/actual.js
+++ packages/babel-generator/test/fixtures/flow/type-annotations/actual.js
@@ -102,3 +102,16 @@ export type { foo } from "bar";
 export interface foo { p: number };
 export interface foo<T> { p: T };
 var a: ?Array<?string>;
+var a: {| numVal: number |};
+var a: {| numVal: number; |};
+var a: {| numVal: number; [indexer: string]: number |};
+var a: ?{| numVal: number |};
+var a: {| numVal: number; strVal: string |}
+var a: {| subObj: {strVal: string} |}
+var a: {| subObj: ?{strVal: string} |}
+var a: {| param1: number; param2: string |}
+var a: {| param1: number; param2?: string |}
+var a: {| [a: number]: string; [b: number]: string; |};
+var a: {| add(x: number, ...y: Array<string>): void |};
+var a: {| subtract: (x: number, ...y: Array<string>) => void |};
+var a: {| id<T>(x: T): T; |};
