8f6d4ae5cfa6be1807029e6432d4428b2f01111c
Alberto Piai <apiai@sensational.ch>
2016-09-20 18:19:09 +0200

    Fix `typeof Symbol.prototype` (#3686)
    * formatting
    
    * fix `typeof Symbol.prototype`
    
    Babel uses a helper function to return the correct value for `typeof
    obj` when obj is a Symbol and support for Symbol has been polyfilled.
    
    This function assumes that `obj.constructor === Symbol` implies `typeof
    obj === 'symbol'`.
    
    This isn't true when obj is `Symbol.prototype`. In that case (REPL from
    node 6, the same holds in Firefox):
    
    ```
    > Symbol.prototype.constructor === Symbol
    true
    > typeof Symbol.prototype
    'object'
    >
    ```
    
    AFAICS, that's the only case where the assumption doesn't hold.
    
    The test added by this patch fails only on node 0.10, as 0.12 already
    has a native implementation of Symbol and the polyfill code doesn't run.
    
    This caused a problem in core-js when it's compiled with babel (the
    issue was isolated by @skozin here:
    https://github.com/zloirock/core-js/issues/189#issuecomment-209864582).

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -8,7 +8,11 @@ export default helpers;
 helpers.typeof = template(`
   (typeof Symbol === "function" && typeof Symbol.iterator === "symbol")
     ? function (obj) { return typeof obj; }
-    : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+    : function (obj) {
+        return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype
+          ? "symbol"
+          : typeof obj;
+      };
 `);
 
 helpers.jsx = template(`
