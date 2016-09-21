f3a6e4b0aeab0f039484a75a69cb35ab90937e87
Kai Cataldo <kaicataldo@users.noreply.github.com>
2016-09-21 04:52:59 -0400

    Fix tests with duplicate named exports (#4538)
    * Fix babel-plugin-transform-flow-strip-types tests
    
    * Fix babel-plugin-transform-es2015-modules-umd tests
    
    * Fix babel-generator tests
    
    * Fix babel-plugin-transform-es2015-modules-systemjs tests
    
    * Fix babel-plugin-transform-es2015-modules-commonjs tests
    
    * Fix babel-plugin-transform-es2015-modules-amd tests

--- packages/babel-generator/test/fixtures/harmony-edgecase/exports/actual.js
+++ packages/babel-generator/test/fixtures/harmony-edgecase/exports/actual.js
@@ -1,20 +1,10 @@
 export * from "OK"
 export { name } from "OK"
 export { a as b, c as d } from "hello"
-export { a as b, c as d }
+export { a as e, c as f }
 export { }
 export default i = 20
 export function test() { }
 export class test2 { }
-export var i = 20
-export let i = 42
-
-export default (function(){})();
-export default (class {})();
-export default {};
-
-export default function test3(){}
-export default class test4 {}
-
-export default function(){}
-export default class {}
+export var j = 20
+export let k = 42
---
+++ packages/babel-plugin-transform-es2015-modules-systemjs/test/fixtures/systemjs/export-default-6/expected.js
@@ -0,0 +1,10 @@
+System.register([], function (_export, _context) {
+  "use strict";
+
+  _export("default", class {});
+
+  return {
+    setters: [],
+    execute: function () {}
+  };
+});
