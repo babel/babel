b9ccba302f89baeb072ee56996c6366945fb096a
Kai Cataldo <kaicataldo@gmail.com>
2016-09-21 01:16:42 -0400

    Fix babel-plugin-transform-es2015-modules-amd tests

---
+++ packages/babel-plugin-transform-es2015-modules-amd/test/fixtures/amd/export-from-6/expected.js
@@ -0,0 +1,19 @@
+define(["exports", "foo"], function (exports, _foo) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function () {
+      return _foo.foo;
+    }
+  });
+  Object.defineProperty(exports, "bar", {
+    enumerable: true,
+    get: function () {
+      return _foo.bar;
+    }
+  });
+});
--- packages/babel-plugin-transform-es2015-modules-amd/test/fixtures/amd/export-named-2/expected.js
+++ packages/babel-plugin-transform-es2015-modules-amd/test/fixtures/amd/export-named-2/expected.js
@@ -5,10 +5,5 @@ define(["exports"], function (exports) {
     value: true
   });
   exports.foo = foo;
-  exports.foo = foo;
-  exports.bar = bar;
-  exports.bar = foo;
-  exports.default = foo;
-  exports.default = foo;
   exports.bar = bar;
 });
