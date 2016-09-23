a58f727d9751225b7349a029e6f459d9748cadc0
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-09-23 12:26:58 +0200

    Fix destructuring evaluation with call expressions
    Do not optimize destructions with callExpressions, as the call
    might change the value of a variable that we are assigning to.
    
    Fixes #4054

--- packages/babel-plugin-transform-es2015-destructuring/src/index.js
+++ packages/babel-plugin-transform-es2015-destructuring/src/index.js
@@ -221,6 +221,9 @@ export default function ({ types: t }) {
       for (let elem of (arr.elements: Array)) {
         // deopt on spread elements
         if (t.isSpreadElement(elem)) return false;
+
+        // deopt call expressions as they might change LHS variables
+        if (t.isCallExpression(elem)) return false;
       }
 
       // deopt on reference to left side identifiers
