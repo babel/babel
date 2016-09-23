e453b919b5f3dd954c381bfc292ca55757b4b177
Pawel Badenski <pawel.badenski@gmail.com>
2016-09-23 11:01:43 +0100

    Use existing external source maps if they exist
    Example: required when using TypeScript and external source maps.

--- packages/babel-register/src/node.js
+++ packages/babel-register/src/node.js
@@ -44,6 +44,15 @@ function mtime(filename) {
   return +fs.statSync(filename).mtime;
 }
 
+function findSourceMappingURL(source) {
+    var m = /\/\/[#@] ?sourceMappingURL=([^\s'"]+)\s*$/.exec(source);
+    if (m && m[1]) {
+        return m[1];
+    } else {
+        return undefined;
+    }
+}
+
 function compile(filename) {
   let result;
 
@@ -64,6 +73,11 @@ function compile(filename) {
     if (cached && cached.mtime === mtime(filename)) {
       result = cached;
     }
+  }
+
+  var sourceMappingURL = findSourceMappingURL(fs.readFileSync(filename));
+  if (sourceMappingURL !== undefined && fs.existsSync(path.dirname(filename) + "/" + sourceMappingURL)) {
+      opts.inputSourceMap = JSON.parse(fs.readFileSync(path.dirname(filename) + "/" + sourceMappingURL));
   }
 
   if (!result) {
