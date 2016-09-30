6f363b60bb6970239ab3d8b0ab779d3fa71f761b
Henry Zhu <hi@henryzoo.com>
2016-09-30 17:29:18 -0400

    Move async-generators to stage-3 (#4617)

--- packages/babel-preset-stage-2/src/index.js
+++ packages/babel-preset-stage-2/src/index.js
@@ -2,14 +2,12 @@ import presetStage3 from "babel-preset-stage-3";
 
 import transformClassProperties from "babel-plugin-transform-class-properties";
 import transformDecorators from "babel-plugin-transform-decorators";
-import transformAsyncGeneratorFunctions from "babel-plugin-transform-async-generator-functions";
 
 export default {
   presets: [
     presetStage3
   ],
   plugins: [
-    transformAsyncGeneratorFunctions,
     transformClassProperties,
     transformDecorators
   ]
