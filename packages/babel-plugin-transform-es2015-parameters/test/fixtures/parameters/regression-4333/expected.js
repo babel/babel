12d2673038e81c6862e48c77e96db8774ac8da70
Moti Zilberman <motiz88@gmail.com>
2016-10-05 17:15:04 +0300

    Add test for rest params name collision with top-level name (#4333) (#4673)

---
+++ packages/babel-plugin-transform-es2015-parameters/test/fixtures/parameters/regression-4333/expected.js
@@ -0,0 +1,8 @@
+var args = 'bar';
+function foo() {
+  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  return args;
+}
