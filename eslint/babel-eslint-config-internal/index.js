1536dfda0a2b9b5f123b34789998c654d2df8ec2
Dan Harper <intouch@danharper.me>
2016-10-05 21:35:44 +0100

    flowtype plugin (babel/eslint-config-babel#6)

--- eslint/babel-eslint-config-internal/index.js
+++ eslint/babel-eslint-config-internal/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "babel-eslint",
   extends: "eslint:recommended",
-  plugins: ["flow-vars", "babel"],
+  plugins: ["flowtype", "babel"],
   rules: {
     "flowtype/define-flow-type": "warn",
     "flowtype/use-flow-type": "warn",
