9fc51d62929e6d134abc8f63d840f0cf27c63665
Moti Zilberman <motiz88@gmail.com>
2016-10-14 18:51:22 +0300

    Consolidate contiguous var declarations in destructuring transform (#4690)
    * Consolidate contiguous var declarations in destructuring transform
    
    Fixes #3081.
    
    * Simplify var node coalescing in es2015-destructuring
    
    * Revert "Simplify var node coalescing in es2015-destructuring"
    
    This reverts commit 15cb373f0726f68225f7080a7ae206a63af174ee.
    
    * More careful condition for var coalescing in es2015-destructuring

--- packages/babel-plugin-transform-es2015-destructuring/test/fixtures/destructuring/array/expected.js
+++ packages/babel-plugin-transform-es2015-destructuring/test/fixtures/destructuring/array/expected.js
@@ -1,7 +1,7 @@
-var a = "hello";
-var _ref = [", ", "junk"];
-var b = _ref[0];
-var c = "world";
+var a = "hello",
+    _ref = [", ", "junk"],
+    b = _ref[0],
+    c = "world";
 a = "hello";
 var _ref2 = [", ", "junk"];
 b = _ref2[0];
