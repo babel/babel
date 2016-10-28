fad6483aac4ba59f71f8d0af0ade46a733509179
Peter Mikula <peter.mikula@gmail.com>
2016-10-28 20:41:10 +0300

    fixes invalid line offsets in merged sourcemaps

--- packages/babel-cli/src/babel/file.js
+++ packages/babel-cli/src/babel/file.js
@@ -52,7 +52,7 @@ module.exports = function (commander, filenames, opts) {
           }
         });
 
-        offset = code.split("\n").length;
+        offset = code.split("\n").length -1;
       }
     });
 
