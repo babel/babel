f2b4030e7fcfbeb9a4bcf9865db5540b67f5fa3d
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2016-11-10 15:41:50 +0900

    Update fixtures that have a copy of the helper

--- packages/babel-plugin-transform-react-constant-elements/test/fixtures/constant-elements/parameter-destructure-rest/expected.js
+++ packages/babel-plugin-transform-react-constant-elements/test/fixtures/constant-elements/parameter-destructure-rest/expected.js
@@ -1,4 +1,4 @@
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+function _objectWithoutProperties(obj, keys) { var target = {}; if (obj == null) return target; var i, key, toExclude = []; for (i in keys) { key = keys[i]; toExclude.push(typeof key == "symbol" ? key : "" + key); } var objKeys = Object.getOwnPropertyNames(obj).concat(Object.getOwnPropertySymbols(obj)); for (i in objKeys) { key = objKeys[i]; if (!Object.prototype.propertyIsEnumerable.call(obj, key)) continue; if (toExclude.indexOf(key) >= 0) continue; target[key] = obj[key]; } return target; }
 
 function render(_ref) {
   let text = _ref.text,
