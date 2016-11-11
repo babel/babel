76702a3a57780aa29afa49f1d43e4a9f7de3e052
guybedford <guybedford@gmail.com>
2016-11-11 17:46:37 +0200

    use ordered modules array over object properties

--- packages/babel-plugin-transform-es2015-modules-systemjs/test/fixtures/systemjs/imports-numbered/expected.js
+++ packages/babel-plugin-transform-es2015-modules-systemjs/test/fixtures/systemjs/imports-numbered/expected.js
@@ -2,7 +2,7 @@ System.register(["2", "1"], function (_export, _context) {
   "use strict";
 
   return {
-    setters: [function (_2) {}, function (_1) {}],
+    setters: [function (_) {}, function (_2) {}],
     execute: function () {}
   };
 });
