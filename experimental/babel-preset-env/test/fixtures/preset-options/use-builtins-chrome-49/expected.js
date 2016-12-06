69e9138637308e782a44806d4271a368bfb022e1
Henry Zhu <hi@henryzoo.com>
2016-12-06 10:31:30 -0500

    extra tests

---
+++ experimental/babel-preset-env/test/fixtures/preset-options/use-builtins-chrome-49/expected.js
@@ -0,0 +1,18 @@
+import "core-js/modules/es6.map";
+import "core-js/modules/es6.set";
+import "core-js/modules/es6.weak-map";
+import "core-js/modules/es6.weak-set";
+import "core-js/modules/es6.promise";
+import "core-js/modules/es6.function.name";
+import "core-js/modules/es6.regexp.match";
+import "core-js/modules/es6.regexp.replace";
+import "core-js/modules/es6.regexp.split";
+import "core-js/modules/es6.regexp.search";
+import "core-js/modules/es6.array.from";
+import "core-js/modules/es7.object.values";
+import "core-js/modules/es7.object.entries";
+import "core-js/modules/es7.object.get-own-property-descriptors";
+import "core-js/modules/es7.string.pad-start";
+import "core-js/modules/es7.string.pad-end";
+
+Math.pow(1, 2);
