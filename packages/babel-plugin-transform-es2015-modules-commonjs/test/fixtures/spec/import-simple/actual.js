cf7a7e4e422b83e54e67db4062fb812de13883d8
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2016-12-10 02:23:11 +0900

    Add an easy to read import test case with all import forms

---
+++ packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/spec/import-simple/actual.js
@@ -0,0 +1,9 @@
+import 'a';
+import {} from 'b';
+import * as ns from 'c';
+import d from 'd'
+import {name} from 'e';
+
+ns.name;
+d;
+name;
---
+++ packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/spec/import-simple/expected.js
@@ -0,0 +1,15 @@
+'use strict';
+
+require('a');
+
+require('b');
+
+var _c = babelHelpers.specRequireInterop(require('c'));
+
+var _d = babelHelpers.specRequireInterop(require('d'));
+
+var _e = babelHelpers.specRequireInterop(require('e'));
+
+_c.name;
+_d.default;
+_e.name;
