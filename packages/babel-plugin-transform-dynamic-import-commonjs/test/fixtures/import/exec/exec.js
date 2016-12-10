0c319fc8f87f54b8013c87912c03cbc1c21f8683
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2016-12-10 23:32:55 +0900

    Keep the exec test ES5-compatible

--- packages/babel-plugin-transform-dynamic-import-commonjs/test/fixtures/import/exec/exec.js
+++ packages/babel-plugin-transform-dynamic-import-commonjs/test/fixtures/import/exec/exec.js
@@ -1,6 +1,6 @@
 // NOTE: relative imports don't work yet in the test runner
 
-return import('os').then(e => {
+return import('os').then(function (e) {
   assert(Object.isFrozen(e))
   assert(e.__esModule)
   assert.deepEqual(Object.keys(e), ['default'])
