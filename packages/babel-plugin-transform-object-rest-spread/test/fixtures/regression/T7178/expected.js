45afbe7b0711266c94ca10d9d51dde1f424b9bc8
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2016-12-16 19:10:45 +0900

    Add missing fixture update

--- packages/babel-plugin-transform-object-rest-spread/test/fixtures/regression/T7178/expected.js
+++ packages/babel-plugin-transform-object-rest-spread/test/fixtures/regression/T7178/expected.js
@@ -6,7 +6,7 @@ var _props2 = _interopRequireDefault(_props);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectWithoutProperties(obj, keys) { var target = {}; if (obj == null) return target; var i, key, toExclude = []; for (i in keys) { key = keys[i]; toExclude.push(typeof key == "symbol" ? key : "" + key); } var objKeys = Object.getOwnPropertyNames(obj).concat(Object.getOwnPropertySymbols(obj)); for (i in objKeys) { key = objKeys[i]; if (!Object.prototype.propertyIsEnumerable.call(obj, key)) continue; if (toExclude.indexOf(key) >= 0) continue; target[key] = obj[key]; } return target; }
+function _objectWithoutProperties(obj, keys) { var target = {}; if (obj == null) return target; var i, key, toExclude = []; for (i = 0; i < keys.length; i++) { key = keys[i]; toExclude.push(typeof key == "symbol" ? key : "" + key); } var objKeys = []; for (i in obj) { objKeys.push(i); } if (Object.getOwnPropertySymbols) { objKeys = objKeys.concat(Object.getOwnPropertySymbols(obj)); } for (i = 0; i < objKeys.length; i++) { key = objKeys[i]; if (!Object.prototype.propertyIsEnumerable.call(obj, key)) continue; if (toExclude.indexOf(key) >= 0) continue; target[key] = obj[key]; } return target; }
 
 console.log(_props2.default);
 
