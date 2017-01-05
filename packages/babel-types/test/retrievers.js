2d40141a03e8055c7ef397b781b39678d8a91dbc
Ryan Tsao <rtsao@uber.com>
2017-01-05 10:54:06 -0800

    Added failing test for getBindingIdentifiers

--- packages/babel-types/test/retrievers.js
+++ packages/babel-types/test/retrievers.js
@@ -3,7 +3,7 @@ import assert from "assert";
 import { parse } from "babylon";
 
 function getBody(program) {
-  return parse(program).program.body;
+  return parse(program, {sourceType: "module"}).program.body;
 }
 
 describe("retrievers", function () {
@@ -17,6 +17,11 @@ describe("retrievers", function () {
       const program = "var foo = 1; function bar() { var baz = 2; }";
       const ids = t.getBindingIdentifiers(getBody(program));
       assert.deepEqual(Object.keys(ids), ["bar", "foo"]);
+    });
+    it("export named declarations", function () {
+      const program = "export const foo = 'foo';";
+      const ids = t.getBindingIdentifiers(getBody(program));
+      assert.deepEqual(Object.keys(ids), ["foo"]);
     });
   });
 });
