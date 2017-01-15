aa606af88c0031b12d0a5764eeb049dc78f779e6
Adonis K <aklp08@gmail.com>
2017-01-15 14:07:28 +0200

    remove trailing function commas from stage-3 preset (#5117)
    trailing function commas is now a finished proposal and was moved to stage 4 (https://github.com/tc39/proposal-trailing-function-commas)

--- packages/babel-preset-stage-3/src/index.js
+++ packages/babel-preset-stage-3/src/index.js
@@ -1,4 +1,3 @@
-import syntaxTrailingFunctionCommas from "babel-plugin-syntax-trailing-function-commas";
 import transformAsyncToGenerator from "babel-plugin-transform-async-to-generator";
 import transformExponentiationOperator from "babel-plugin-transform-exponentiation-operator";
 import transformObjectRestSpread from "babel-plugin-transform-object-rest-spread";
@@ -6,7 +5,6 @@ import transformAsyncGeneratorFunctions from "babel-plugin-transform-async-gener
 
 export default {
   plugins: [
-    syntaxTrailingFunctionCommas, // in ES2017 (remove as a breaking change)
     transformAsyncToGenerator, // in ES2017 (remove as a breaking change)
     transformExponentiationOperator,  // in ES2016 (remove as a breaking change)
     transformAsyncGeneratorFunctions,
