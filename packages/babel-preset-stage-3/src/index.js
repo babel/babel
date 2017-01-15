12827303b4a091a742dad3874e2eb3e830cee59c
Adonis K <aklp08@gmail.com>
2017-01-15 14:14:43 +0200

    remove async function to es2015 generator from stage-3 preset (#5117)
    async functions is now a finished proposal and was moved to stage 4 (https://github.com/tc39/ecmascript-asyncawait)

--- packages/babel-preset-stage-3/src/index.js
+++ packages/babel-preset-stage-3/src/index.js
@@ -1,10 +1,8 @@
-import transformAsyncToGenerator from "babel-plugin-transform-async-to-generator";
 import transformObjectRestSpread from "babel-plugin-transform-object-rest-spread";
 import transformAsyncGeneratorFunctions from "babel-plugin-transform-async-generator-functions";
 
 export default {
   plugins: [
-    transformAsyncToGenerator, // in ES2017 (remove as a breaking change)
     transformAsyncGeneratorFunctions,
     transformObjectRestSpread
   ]
