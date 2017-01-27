d2faddf0d9bcba5915f11e6888cafe6059c77d76
Daniel Tschinder <daniel@tschinder.de>
2017-01-27 19:43:06 +0100

    Fix export default async function to be FunctionDeclaration (#324)

--- src/parser/statement.js
+++ src/parser/statement.js
@@ -837,6 +837,8 @@ pp.parseExport = function (node) {
     let needsSemi = false;
     if (this.eat(tt._function)) {
       expr = this.parseFunction(expr, true, false, false, true);
+    } else if (this.eatContextual("async") && this.eat(tt._function)) {
+      expr = this.parseFunction(expr, true, false, true, true);
     } else if (this.match(tt._class)) {
       expr = this.parseClass(expr, true, true);
     } else {
