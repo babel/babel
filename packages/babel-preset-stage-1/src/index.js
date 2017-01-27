48eb562399e84696617417677cb02f60528d61d1
Artem Yavorsky <aqson@me.com>
2017-01-27 11:36:43 +0200

    [7.0] Fix plugins’ order.

--- packages/babel-preset-stage-1/src/index.js
+++ packages/babel-preset-stage-1/src/index.js
@@ -1,14 +1,14 @@
 import presetStage2 from "babel-preset-stage-2";
 
-import transformExportExtensions from "babel-plugin-transform-export-extensions";
 import transformDecoratorsLegacy from "babel-plugin-transform-decorators-legacy";
+import transformExportExtensions from "babel-plugin-transform-export-extensions";
 
 export default {
   presets: [
     presetStage2
   ],
   plugins: [
-    transformExportExtensions,
-    transformDecoratorsLegacy
+    transformDecoratorsLegacy,
+    transformExportExtensions
   ]
 };
