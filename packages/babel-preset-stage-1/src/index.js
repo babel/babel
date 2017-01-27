3b63ba168e9d166f044af43cdbee98eb8b268176
Artem Yavorsky <aqson@me.com>
2017-01-27 03:25:38 +0200

    [7.0] Add legacy-decorators to stage1. Fixes #5220

--- packages/babel-preset-stage-1/src/index.js
+++ packages/babel-preset-stage-1/src/index.js
@@ -1,12 +1,14 @@
 import presetStage2 from "babel-preset-stage-2";
 
 import transformExportExtensions from "babel-plugin-transform-export-extensions";
+import transformDecoratorsLegacy from "babel-plugin-transform-decorators-legacy";
 
 export default {
   presets: [
     presetStage2
   ],
   plugins: [
-    transformExportExtensions
+    transformExportExtensions,
+    transformDecoratorsLegacy
   ]
 };
