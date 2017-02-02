0ee810cd47c72b36a8416849a4a84ab8860cb5ff
xtina-starr <c.thompsonms@gmail.com>
2017-02-02 14:30:56 -0500

    checks if babel is installed globally and displays correct cli message - fixes #5228

--- packages/babel/cli.js
+++ packages/babel/cli.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
+import isGlobal from "is-global"
+
+const globalMessage = isGlobal() ? ' -g' : '';
 
 console.error("You have mistakenly installed the `babel` package, which is a no-op in Babel 6.\n" +
   "Babel's CLI commands have been moved from the `babel` package to the `babel-cli` package.\n" +
   "\n" +
-  "    npm uninstall babel\n" +
-  "    npm install babel-cli\n" +
+  "    npm uninstall" + globalMessage + " babel\n" +
+  "    npm install" + globalMessage + " babel-cli\n" +
   "\n" +
   "See http://babeljs.io/docs/usage/cli/ for setup instructions.");
 process.exit(1);
