2f2f8436bd3c27fc5639ce07ee14f29bf9822903
xtina-starr <c.thompsonms@gmail.com>
2017-02-03 13:30:01 -0500

    switch back to babel

--- packages/babel/cli.js
+++ packages/babel/cli.js
@@ -6,7 +6,7 @@ const globalMessage = isGlobal() ? " -g" : "";
 console.error("You have mistakenly installed the `babel` package, which is a no-op in Babel 6.\n" +
   "Babel's CLI commands have been moved from the `babel` package to the `babel-cli` package.\n" +
   "\n" +
-  "    npm uninstall" + globalMessage + " babel-cli\n" +
+  "    npm uninstall" + globalMessage + " babel\n" +
   "    npm install --save-dev babel-cli\n" +
   "\n" +
   "See http://babeljs.io/docs/usage/cli/ for setup instructions.");
