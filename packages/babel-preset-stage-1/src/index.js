3a0ce47f353b5acece5915bf5a48d8949b5ee912
Henry Zhu <hi@henryzoo.com>
2017-02-10 17:01:31 -0500

    Scoped packages: leave legacy decorators for now [skip ci]
    Until we merge babel/babel#5290

--- packages/babel-preset-stage-1/src/index.js
+++ packages/babel-preset-stage-1/src/index.js
@@ -1,6 +1,6 @@
 import presetStage2 from "@babel/preset-stage-2";
 
-import transformDecoratorsLegacy from "@babel/plugin-transform-decorators-legacy";
+import transformDecoratorsLegacy from "babel-plugin-transform-decorators-legacy";
 import transformExportExtensions from "@babel/plugin-transform-export-extensions";
 
 export default {
