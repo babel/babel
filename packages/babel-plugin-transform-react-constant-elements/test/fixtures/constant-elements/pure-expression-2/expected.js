2aa2de8c6f8ace49ab5a83a7cbab82dea90036a1
Samuel Reed <samuel.trace.reed@gmail.com>
2017-02-13 11:02:38 +0700

    feature: Support pure expressions in transform-react-constant-elements (#4812)

---
+++ packages/babel-plugin-transform-react-constant-elements/test/fixtures/constant-elements/pure-expression-2/expected.js
@@ -0,0 +1,8 @@
+function render(offset) {
+  var _ref = <div tabIndex={offset + 1} />;
+
+  return function () {
+    return _ref;
+  };
+}
+
---
+++ packages/babel-plugin-transform-react-constant-elements/test/fixtures/constant-elements/pure-expression-3/expected.js
@@ -0,0 +1,10 @@
+const OFFSET = 3;
+
+var _ref = <div tabIndex={OFFSET + 1} />;
+
+var Foo = React.createClass({
+  render: function () {
+    return _ref;
+  }
+});
+
