f5179a30a122ead39a99980b6f1df9432e2b1c11
Logan Smyth <loganfsmyth@gmail.com>
2017-02-13 18:12:09 -0800

    Revert "Rewrite Hub as interface #5047"

--- packages/babel-traverse/src/hub.js
+++ packages/babel-traverse/src/hub.js
@@ -1,14 +1,6 @@
-import Scope from "./scope";
-export interface HubInterface {
-  mark?: (type: string, message: string) => void;
-  addHelper?: (name: string) => Object;
-  getScope?: () => Scope;
-  getCode?: () => string;
-  buildError:(node: Object, msg: string, Error: Error) => Error;
-}
-
 export default class Hub {
-  buildError(node, msg, BuildError = TypeError): Error {
-    return new BuildError(msg);
+  constructor(file, options) {
+    this.file = file;
+    this.options = options;
   }
 }
