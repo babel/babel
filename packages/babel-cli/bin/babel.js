51bb6766aa29d9bda4261fbcd735d998047e3c97
Adonis K <aklp08@gmail.com>
2017-02-19 23:10:31 +0200

    Load local copy of babel-cli when possible, fallback to global

--- packages/babel-cli/bin/babel.js
+++ packages/babel-cli/bin/babel.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
 
-require("../lib/babel");
+const path       = require("path");
+const resolveCwd = require("resolve-cwd");
+const localCLI   = resolveCwd("babel-cli");
+
+function resolveLocalOrGlobalBinary () {
+  if (localCLI && path.relative(localCLI, __filename) !== "") {
+    if (process.stdout.isTTY) {
+      console.log("Using the locally installed Babel CLI");
+    }
+
+    const localBase = localCLI.replace("index.js", "");
+    return path.join(localBase, "bin/babel.js");
+  } else {
+    return "../lib/babel";
+  }
+}
+
+module.exports = resolveLocalOrGlobalBinary;
+
+require(resolveLocalOrGlobalBinary());
