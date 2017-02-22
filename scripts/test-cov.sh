cdbbc6a147c0fe1f72cab598be49ba45d395c689
Fernando Montoya <montogeek@gmail.com>
2017-02-22 09:59:20 -0500

    Update scripts/test-cov.sh

--- scripts/test-cov.sh
+++ scripts/test-cov.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-node_modules/.bin/nyc node_modules/mocha/bin/_mocha --opts test/mocha.opts `scripts/_get-test-directories.sh`
-node_modules/.bin/nyc report --reporter=json
+node_modules/.bin/jest --coverage
