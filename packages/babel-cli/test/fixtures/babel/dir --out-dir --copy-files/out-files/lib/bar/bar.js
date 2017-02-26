640a0da6c84f6d9c0bb129e86712f602ba4222b7
Aaron Ang <awz.ang@gmail.com>
2017-02-26 10:58:55 -0800

    Test `--copy-files`

---
+++ packages/babel-cli/test/fixtures/babel/dir --out-dir --copy-files/out-files/lib/bar/bar.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Test = function Test() {
+  _classCallCheck(this, Test);
+};
