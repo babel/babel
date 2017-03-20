dccd5a75931ce0091a61e71338b82f670b08cb81
Henry Zhu <hi@henryzoo.com>
2017-03-20 11:44:33 -0400

    Merge pull request babel/babel-eslint#447 from kaicataldo/clean-up-eslint
    Chore: Clean up and upgrades

--- eslint/babel-eslint-parser/.eslintrc.js
+++ eslint/babel-eslint-parser/.eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   root: true,
   extends: "babel",
-  parserOptions: {
-    ecmaVersion: 7,
-    sourceType: "module"
-  },
   rules: {
     "no-var": 0,
     "max-len": 0
--- eslint/babel-eslint-parser/eslint-tester.js
+++
@@ -1,6 +0,0 @@
-var ESLintTester = require("./eslint").RuleTester;
-
-console.log("Use babel-eslint for test suite");
-ESLintTester.setDefaultConfig({
-    parser: "../../index"
-});
