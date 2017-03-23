299e51298c52ae24d1bef1d953348f626837941e
Damien Maillard <dmaillard06@gmail.com>
2017-03-23 16:55:14 +0100

    Fix path.remove() leading & trailing comments sharing (#5504)

--- packages/babel-traverse/src/path/comments.js
+++ packages/babel-traverse/src/path/comments.js
@@ -15,14 +15,17 @@ export function shareCommentsWithSiblings() {
   const leading = node.leadingComments;
   if (!trailing && !leading) return;
 
-  let prev = this.getSibling(this.key - 1);
-  let next = this.getSibling(this.key + 1);
+  const prev = this.getSibling(this.key - 1);
+  const next = this.getSibling(this.key + 1);
+  const hasPrev = Boolean(prev.node);
+  const hasNext = Boolean(next.node);
+  if (hasPrev && hasNext) {
 
-  if (!prev.node) prev = next;
-  if (!next.node) next = prev;
-
-  prev.addComments("trailing", leading);
-  next.addComments("leading", trailing);
+  } else if (hasPrev) {
+    prev.addComments("trailing", trailing);
+  } else if (hasNext) {
+    next.addComments("leading", leading);
+  }
 }
 
 export function addComment(type, content, line?) {
@@ -45,7 +48,11 @@ export function addComments(type: string, comments: Array) {
   const key = `${type}Comments`;
 
   if (node[key]) {
-    node[key] = node[key].concat(comments);
+    if (type === "leading") {
+      node[key] = comments.concat(node[key]);
+    } else {
+      node[key] = node[key].concat(comments);
+    }
   } else {
     node[key] = comments;
   }
---
+++ packages/babel-traverse/test/fixtures/comments/attachment/expected.js
@@ -0,0 +1,20 @@
+{}
+{
+  hasPrev;
+  /* top */
+  /* left */ /* right */
+  /* bottom */
+}
+{
+  /* top */
+  /* left */ /* right */
+  /* bottom */
+  hasNext;
+}
+{
+  hasPrev;
+  /* top */
+  /* left */ /* right */
+  /* bottom */
+  hasNext;
+}
