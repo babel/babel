be82d6e70c515809cce3d52285b873f64f4520f9
Boopathi Rajaa <me@boopathi.in>
2017-03-23 16:52:58 +0100

    remove copyPath

--- packages/babel-traverse/src/cache.js
+++ packages/babel-traverse/src/cache.js
@@ -13,9 +13,3 @@ export function clearPath() {
 export function clearScope() {
   scope = new WeakMap;
 }
-
-export function copyPath(source, destination) {
-  if (path.has(source)) {
-    path.set(destination, path.get(source));
-  }
-}
