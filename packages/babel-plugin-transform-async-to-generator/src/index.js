26da384f9542c475350406d4e3e156d4a7dfa798
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2017-03-24 15:17:07 +0900

    Always use the native (or polyfilled) Promise in transform-async-to-generator
    Fixes #5531

--- packages/babel-plugin-transform-async-to-generator/src/index.js
+++ packages/babel-plugin-transform-async-to-generator/src/index.js
@@ -9,6 +9,17 @@ export default function () {
       Function(path, state) {
         if (!path.node.async || path.node.generator) return;
 
+        // Ensure any bindings at the Program level are renamed first
+        // so any further renames cannot accidentally rename the Promise
+        // binding used by the inline helper
+        const programScope = path.scope.getProgramParent();
+        if (programScope.hasBinding("Promise", true)) {
+          programScope.rename("Promise");
+        }
+        if (path.scope.hasBinding("Promise", true)) {
+          path.scope.rename("Promise");
+        }
+
         remapAsyncToGenerator(path, state.file, {
           wrapAsync: state.addHelper("asyncToGenerator"),
         });
---
+++ packages/babel-plugin-transform-async-to-generator/test/fixtures/async-to-generator/shadowed-promise-nested/expected.js
@@ -0,0 +1,25 @@
+let foo = (() => {
+  var _ref = _asyncToGenerator(function* () {
+    let bar = (() => {
+      var _ref2 = _asyncToGenerator(function* () {
+        return _Promise2.resolve();
+      });
+
+      return function bar() {
+        return _ref2.apply(this, arguments);
+      };
+    })();
+
+    let _Promise2;
+
+    yield bar();
+  });
+
+  return function foo() {
+    return _ref.apply(this, arguments);
+  };
+})();
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+let _Promise;
