713e486adaf2c214a6d1aea4111475fc39a94e02
Aaron Ang <awz.ang@gmail.com>
2017-03-24 00:04:05 -0700

    Only override name of identifier object for function renaming

--- packages/babel-helper-function-name/src/index.js
+++ packages/babel-helper-function-name/src/index.js
@@ -140,7 +140,7 @@ export default function ({ node, parent, scope, id }) {
     if (t.isIdentifier(id)) {
       const binding = scope.parent.getBinding(id.name);
       if (binding && binding.constant && scope.getBinding(id.name) === binding) {
-        node.id = t.identifier(id.loc.identifierName);
+        node.id = Object.assign({}, id, { name: id.loc.identifierName });
         node.id[t.NOT_LOCAL_BINDING] = true;
         return;
       }
