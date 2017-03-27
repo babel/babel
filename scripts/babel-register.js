67253c5d277e4edd46ef0dca865b3f031e6b3659
Henry Zhu <hi@henryzoo.com>
2017-03-27 17:19:02 -0400

    babelrc false to relevant files

--- scripts/babel-register.js
+++ scripts/babel-register.js
@@ -1,11 +1,10 @@
 "use strict";
 
 const register = require("babel-register").default;
-const config = require("../.babelrc");
 
-register(Object.assign({}, config, {
+register({
   extensions: [".js"],
   // Only js files in the test folder but not in the subfolder fixtures.
   only: [/packages\/.+\/test\/(?!fixtures\/).+\.js$/],
   compact: true,
-}));
+});
