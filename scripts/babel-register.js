7935dd6642d2190d8405dc510641d985ccab3fc8
Henry Zhu <hi@henryzoo.com>
2017-03-27 16:44:42 -0400

    pass config

--- scripts/babel-register.js
+++ scripts/babel-register.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const register = require("babel-register").default;
-register();
-register({
+const config = require("../.babelrc");
+
+register(Object.assign({}, config, {
   extensions: [".js"],
   // Only js files in the test folder but not in the subfolder fixtures.
   only: [/packages\/.+\/test\/(?!fixtures\/).+\.js$/],
-  babelrc: false,
   compact: true,
-});
+}));
