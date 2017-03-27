56c116ecc84cd2c5bdb05469f141b116b0e82a44
Andres Suarez <zertosh@gmail.com>
2017-03-27 12:47:44 -0400

    Reuse "then" callbacks in asyncToGenerator helpher

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -230,15 +230,13 @@ helpers.asyncToGenerator = template(`
           if (info.done) {
             resolve(value);
           } else {
-            Promise.resolve(value).then(function (value) {
-              step("next", value);
-            }, function (err) {
-              step("throw", err);
-            });
+            Promise.resolve(value).then(_next, _throw);
           }
         }
+        function _next(value) { step("next", value); }
+        function _throw(err) { step("throw", err); }
 
-        step("next");
+        _next();
       });
     };
   })
