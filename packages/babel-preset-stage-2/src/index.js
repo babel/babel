216cbf5bd6f84079e933ebfb126d7dd9c755eb36
Daniel Kao <dkao@diplateevo.com>
2017-04-08 10:14:23 -0400

    Move syntax-dynamic-import to stage-3 (#5610)

--- packages/babel-preset-stage-2/src/index.js
+++ packages/babel-preset-stage-2/src/index.js
@@ -1,6 +1,5 @@
 import presetStage3 from "babel-preset-stage-3";
 
-import syntaxDynamicImport from "babel-plugin-syntax-dynamic-import";
 import transformClassProperties from "babel-plugin-transform-class-properties";
 import transformUnicodePropertyRegex from "babel-plugin-transform-unicode-property-regex";
 
@@ -10,7 +9,6 @@ export default function () {
       presetStage3,
     ],
     plugins: [
-      syntaxDynamicImport,
       transformClassProperties,
       transformUnicodePropertyRegex,
     ],
--- packages/babel-preset-stage-3/src/index.js
+++ packages/babel-preset-stage-3/src/index.js
@@ -1,9 +1,11 @@
+import syntaxDynamicImport from "babel-plugin-syntax-dynamic-import";
 import transformObjectRestSpread from "babel-plugin-transform-object-rest-spread";
 import transformAsyncGeneratorFunctions from "babel-plugin-transform-async-generator-functions";
 
 export default function () {
   return {
     plugins: [
+      syntaxDynamicImport,
       transformAsyncGeneratorFunctions,
       transformObjectRestSpread,
     ],
