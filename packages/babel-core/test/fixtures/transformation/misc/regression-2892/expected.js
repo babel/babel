c4ec382a4e7919052115905a46da1c2f4be834ba
Steven Hargrove <steven.hargrove@gmail.com>
2017-05-01 19:27:26 -0400

    updated test fixture to reflect change

--- packages/babel-core/test/fixtures/transformation/misc/regression-2892/expected.js
+++ packages/babel-core/test/fixtures/transformation/misc/regression-2892/expected.js
@@ -46,7 +46,7 @@ var foo = function () {
   };
 }();
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } } function _next(value) { step("next", value); } function _throw(err) { step("throw", err); } _next(); }); }; }
+function _asyncToGenerator(fn) { return function () { var _this = this, _arguments = arguments; return new Promise((resolve, reject) => { var gen = fn.apply(_this, _arguments); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
