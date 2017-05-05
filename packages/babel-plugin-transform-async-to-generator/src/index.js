634c750558e5ce149b879b66888d5ffd41865cab
Logan Smyth <loganfsmyth@gmail.com>
2017-05-05 01:01:35 -0700

    Ensure helpers that reference globals continue to reference the globals properly.

--- packages/babel-plugin-transform-async-to-generator/src/index.js
+++ packages/babel-plugin-transform-async-to-generator/src/index.js
@@ -9,13 +9,6 @@ export default function() {
       Function(path, state) {
         if (!path.node.async || path.node.generator) return;
 
-        // Ensure any Promise bindings at the Program level are renamed
-        // so the asyncToGenerator helper only sees the native Promise
-        const programScope = path.scope.getProgramParent();
-        if (programScope.hasBinding("Promise", true)) {
-          programScope.rename("Promise");
-        }
-
         remapAsyncToGenerator(path, state.file, {
           wrapAsync: state.addHelper("asyncToGenerator"),
         });
