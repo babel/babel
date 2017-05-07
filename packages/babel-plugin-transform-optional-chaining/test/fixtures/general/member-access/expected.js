3faca62a77fcff9e5e487ab7e843fee9f7621959
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-05-07 11:21:27 +0200

    wip

--- packages/babel-plugin-transform-optional-chaining/test/fixtures/general/member-access/expected.js
+++ packages/babel-plugin-transform-optional-chaining/test/fixtures/general/member-access/expected.js
@@ -1,3 +1,5 @@
 var _temp;
 
 (_temp = foo) != null ? _temp.bar : undefined;
+
+((_temp = ((_temp = a) != null ? _temp.b : undefined).c) != null ? _temp.d : undefined).e;
--- packages/babel-plugin-transform-optional-chaining/test/fixtures/general/nested-member-access/expected.js
+++
@@ -1,3 +0,0 @@
-var _temp;
-
-((_temp = ((_temp = a) != null ? _temp.b : undefined).c) != null ? _temp.d : undefined).e;
