ae43e064018e239316428e9cdc61cf4fb983cb23
Buu Nguyen <buunguyen@gmail.com>
2017-05-25 08:59:01 -0700

    Backport minNodeVersion test option from #5765 for 6.x

--- packages/babel-helper-fixtures/src/index.js
+++ packages/babel-helper-fixtures/src/index.js
@@ -3,8 +3,11 @@ import trimEnd from "lodash/trimEnd";
 import resolve from "try-resolve";
 import clone from "lodash/clone";
 import merge from "lodash/merge";
+import semver from "semver";
 import path from "path";
 import fs from "fs";
+
+const nodeVersion = semver.clean(process.version.slice(1));
 
 function humanize(val, noext) {
   if (noext) val = path.basename(val, path.extname(val));
@@ -124,6 +127,22 @@ export default function get(entryLoc): Array<Suite> {
           filename: expectLocAlias
         }
       };
+
+      // If there's node requirement, check it before pushing task
+      if (taskOpts.minNodeVersion) {
+        const minimumVersion = semver.clean(taskOpts.minNodeVersion);
+
+        if (minimumVersion == null) {
+          throw new Error(`'minNodeVersion' has invalid semver format: ${taskOpts.minNodeVersion}`);
+        }
+
+        if (semver.lt(nodeVersion, minimumVersion)) {
+          return;
+        }
+
+        // Delete to avoid option validation error
+        delete taskOpts.minNodeVersion;
+      }
 
       // traceur checks
 
