81bbfad57a9e3ffa67bc8932c0b2299b9ca17d72
HE Shi-Jun <hax@heshijun.net>
2017-05-26 21:07:39 +0800

    spaces indent

--- packages/babel-plugin-transform-es2015-object-super/test/fixtures/object-super/accessor-semantic/exec.js
+++ packages/babel-plugin-transform-es2015-object-super/test/fixtures/object-super/accessor-semantic/exec.js
@@ -1,26 +1,26 @@
 class X {
-	get x() { return this._x; }
-	set x(value) { this._x = value; }
+  get x() { return this._x; }
+  set x(value) { this._x = value; }
 }
 
 class X1 extends X {
   getSuperX() { return super.x; }
-	constructor() {
-		super();
-		this.x = 0;
-		super.x = 1;
-	}
+  constructor() {
+    super();
+    this.x = 0;
+    super.x = 1;
+  }
 }
 
 class X2 extends X {
-	get x() { return this._x2; }
-	set x(value) { this._x2 = value; }
+  get x() { return this._x2; }
+  set x(value) { this._x2 = value; }
   getSuperX() { return super.x; }
-	constructor() {
-		super();
-		this.x = 0;
-		super.x = 1;
-	}
+  constructor() {
+    super();
+    this.x = 0;
+    super.x = 1;
+  }
 }
 
 let x1 = new X1();
