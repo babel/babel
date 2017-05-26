9365bf96c6a5e3c74af5a31860d0ab0161c4963b
HE Shi-Jun <hax@heshijun.net>
2017-05-26 21:35:12 +0800

    add test cases to right place

---
+++ packages/babel-plugin-transform-es2015-classes/test/fixtures/exec/super-accessor.js
@@ -0,0 +1,34 @@
+class X {
+  get x() { return this._x; }
+  set x(value) { this._x = value; }
+}
+
+class X1 extends X {
+  getSuperX() { return super.x; }
+  constructor() {
+    super();
+    this.x = 0;
+    super.x = 1;
+  }
+}
+
+class X2 extends X {
+  get x() { return this._x2; }
+  set x(value) { this._x2 = value; }
+  getSuperX() { return super.x; }
+  constructor() {
+    super();
+    this.x = 0;
+    super.x = 1;
+  }
+}
+
+let x1 = new X1();
+assert.equal(x1.x, 1);
+assert.equal(x1.getSuperX(), 1);
+
+let x2 = new X2();
+assert.equal(x2.x, 0);
+assert.equal(x2.getSuperX(), 1);
+
+
