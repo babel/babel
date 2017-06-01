5cdb4625f84defc93ae82dda8523d4767b5784fd
Peeyush Kushwaha <peeyush.p97@gmail.com>
2017-06-01 17:50:30 +0530

    Updated affected expected.js tests

--- packages/babel-plugin-transform-object-rest-spread/test/fixtures/object-rest/symbol/expected.js
+++ packages/babel-plugin-transform-object-rest-spread/test/fixtures/object-rest/symbol/expected.js
@@ -1,14 +1,18 @@
-var _ref3;
+var _ref3, _Symbol$for3;
 
 let _ref = {},
+    _Symbol$for = Symbol.for("foo"),
     {
-  [Symbol.for("foo")]: foo
+  [_Symbol$for]: foo
 } = _ref,
-    rest = babelHelpers.objectWithoutProperties(_ref, [Symbol.for("foo")].map(babelHelpers.toPropertyKey));
+    rest = babelHelpers.objectWithoutProperties(_ref, [_Symbol$for].map(babelHelpers.toPropertyKey));
 
 var _ref2 = {};
-({ [Symbol.for("foo")]: foo } = _ref2);
-rest = babelHelpers.objectWithoutProperties(_ref2, [Symbol.for("foo")].map(babelHelpers.toPropertyKey));
+
+var _Symbol$for2 = Symbol.for("foo");
+
+({ [_Symbol$for2]: foo } = _ref2);
+rest = babelHelpers.objectWithoutProperties(_ref2, [_Symbol$for2].map(babelHelpers.toPropertyKey));
 
 
-if (_ref3 = {}, ({ [Symbol.for("foo")]: foo } = _ref3), rest = babelHelpers.objectWithoutProperties(_ref3, [Symbol.for("foo")].map(babelHelpers.toPropertyKey)), _ref3) {}
+if (_ref3 = {}, _Symbol$for3 = Symbol.for("foo"), ({ [_Symbol$for3]: foo } = _ref3), rest = babelHelpers.objectWithoutProperties(_ref3, [_Symbol$for3].map(babelHelpers.toPropertyKey)), _ref3) {}
