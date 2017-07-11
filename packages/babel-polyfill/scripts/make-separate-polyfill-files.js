bc123ad02a87f7618d7bc9d72b991f67a936e50e
Brian Ng <bng412@gmail.com>
2017-07-11 18:13:26 -0500

    Change trailing comma option for polyfill scripts (#5939)

--- packages/babel-polyfill/scripts/make-separate-polyfill-files.js
+++ packages/babel-polyfill/scripts/make-separate-polyfill-files.js
@@ -15,11 +15,11 @@ polyfills
   .forEach(polyfill => {
     fs.writeFileSync(
       relative(`src/core-js/modules/${polyfill}.js`),
-      `require("core-js/modules/${polyfill}");`,
+      `require("core-js/modules/${polyfill}");`
     );
   });
 
 fs.writeFileSync(
   relative("src/regenerator-runtime/runtime.js"),
-  'require("regenerator-runtime/runtime");',
+  'require("regenerator-runtime/runtime");'
 );
