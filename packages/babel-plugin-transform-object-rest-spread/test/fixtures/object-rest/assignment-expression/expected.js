0848c3c8c9aee18d3a5cd55c4b10b1b413573491
Justin Ridgewell <justin@ridgewell.name>
2017-07-13 01:48:22 -0400

    Remove maybePopFromStatements
    It prevented you from pushing into the `params` of a function.
    
    ```js
    fnpath.pushContainer("params", t.identifier("memo"))
    ```

--- packages/babel-plugin-transform-object-rest-spread/test/fixtures/object-rest/assignment-expression/expected.js
+++ packages/babel-plugin-transform-object-rest-spread/test/fixtures/object-rest/assignment-expression/expected.js
@@ -8,6 +8,7 @@ var _c = c2;
   a2
 } = _c);
 b2 = babelHelpers.objectWithoutProperties(_c, ["a2"]);
+_c;
 console.log((_c2 = c3, ({
   a3
 } = _c2), b3 = babelHelpers.objectWithoutProperties(_c2, ["a3"]), _c2));
