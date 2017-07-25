4a3524311882d0a68f8cc36b9332a42ad7c889a2
Peeyush Kushwaha <peeyush.p97@gmail.com>
2017-07-25 20:16:52 +0530

    Non string computed keys in object-rest-spread (#5757)

--- packages/babel-plugin-transform-react-constant-elements/test/fixtures/constant-elements/parameter-destructure-rest/expected.js
+++ packages/babel-plugin-transform-react-constant-elements/test/fixtures/constant-elements/parameter-destructure-rest/expected.js
@@ -1,10 +1,8 @@
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
 function render(_ref) {
   let text = _ref.text,
       className = _ref.className,
       id = _ref.id,
-      props = _objectWithoutProperties(_ref, ["text", "className", "id"]);
+      props = babelHelpers.objectWithoutProperties(_ref, ["text", "className", "id"]);
 
   var _ref2 = <Component text={text} className={className} id={id} />;
 
