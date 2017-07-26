9f8ccbb4c03ef3152b585e002e78330d1f2daef6
Henry Zhu <hi@henryzoo.com>
2017-07-26 16:51:35 -0400

    update stage 2

--- packages/babel-preset-stage-2/src/index.js
+++ packages/babel-preset-stage-2/src/index.js
@@ -1,7 +1,7 @@
 import presetStage3 from "babel-preset-stage-3";
 
 import transformClassProperties from "babel-plugin-transform-class-properties";
-import transformFunctionSent from "babel-plugin-transform-function-sent2";
+import transformFunctionSent from "babel-plugin-transform-function-sent";
 
 export default function() {
   return {
