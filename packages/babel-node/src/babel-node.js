8081fc73c76f8ecb8f03ac1314e5ed525cc6b865
Brian Ng <bng412@gmail.com>
2017-07-26 09:53:14 -0500

    updates

--- packages/babel-node/src/babel-node.js
+++ packages/babel-node/src/babel-node.js
@@ -33,7 +33,7 @@ function getNormalizedV8Flag(arg) {
   return arg;
 }
 
-getV8Flags(function (err, v8Flags) {
+getV8Flags(function(err, v8Flags) {
   babelArgs.forEach(function(arg) {
     const flag = arg.split("=")[0];
 
@@ -58,7 +58,10 @@ getV8Flags(function (err, v8Flags) {
         break;
 
       default:
-        if (v8Flags.indexOf(getNormalizedV8Flag(flag)) >= 0 || arg.indexOf("--trace") === 0) {
+        if (
+          v8Flags.indexOf(getNormalizedV8Flag(flag)) >= 0 ||
+          arg.indexOf("--trace") === 0
+        ) {
           args.unshift(arg);
         } else {
           args.push(arg);
@@ -79,9 +82,11 @@ getV8Flags(function (err, v8Flags) {
     if (err.code !== "MODULE_NOT_FOUND") throw err;
 
     const child_process = require("child_process");
-    const proc = child_process.spawn(process.argv[0], args, { stdio: "inherit" });
-    proc.on("exit", function (code, signal) {
-      process.on("exit", function () {
+    const proc = child_process.spawn(process.argv[0], args, {
+      stdio: "inherit",
+    });
+    proc.on("exit", function(code, signal) {
+      process.on("exit", function() {
         if (signal) {
           process.kill(process.pid, signal);
         } else {
