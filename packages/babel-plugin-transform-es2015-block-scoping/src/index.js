75808a2d14a5872472eb12ee5135faca4950d57a
Sarup Banskota <sbanskota08@gmail.com>
2017-08-03 04:30:33 +0700

    Prevent getFunctionParent from returning Program (#5923)

--- packages/babel-plugin-transform-es2015-block-scoping/src/index.js
+++ packages/babel-plugin-transform-es2015-block-scoping/src/index.js
@@ -123,7 +123,7 @@ function convertBlockScopedToVar(
 
   // Move bindings from current block scope to function scope.
   if (moveBindingsToParent) {
-    const parentScope = scope.getFunctionParent();
+    const parentScope = scope.getFunctionParent() || scope.getProgramParent();
     const ids = path.getBindingIdentifiers();
     for (const name in ids) {
       const binding = scope.getOwnBinding(name);
@@ -371,7 +371,7 @@ class BlockScoping {
 
   updateScopeInfo(wrappedInClosure) {
     const scope = this.scope;
-    const parentScope = scope.getFunctionParent();
+    const parentScope = scope.getFunctionParent() || scope.getProgramParent();
     const letRefs = this.letReferences;
 
     for (const key in letRefs) {
