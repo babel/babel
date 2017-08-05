e2e19dc3aa0cc394154e2d75af52217fc7fda429
jonathan schatz <jon@instamotor.com>
2017-08-05 12:14:18 -0700

    cleanup / fix tests

--- regenerator/test/tests.transform.js
+++ regenerator/test/tests.transform.js
@@ -48,56 +48,101 @@ describe("_blockHoist nodes", function() {
   });
 });
 
-describe("marked", function() {
-  it("should work with a single function", function() {
-    var ast = recast.parse('function* foo(){};', {
-        parser: require("babylon")
-    });
-    const code = recast.print(transform(ast)).code;
-    assert.notStrictEqual(
-      code.indexOf(
-        'var _marked = regeneratorRuntime.mark(foo);'
-      ), -1
-    );
-  });
-  it("should work with multiple functions", function() {
-    var ast = recast.parse([
-      'function* foo(){};',
-      'function* bar() {};'
-    ].join("\n"), {
-        parser: require("babylon")
-    });
-    const code = recast.print(transform(ast)).code;
-    assert.notStrictEqual(
-      code.indexOf(
-        'var _marked = regeneratorRuntime.mark(foo), _marked2 = regeneratorRuntime.mark(bar);'
-      ), -1
-    );
-  });
-});
+context("functions", function() {
+  var itMarksCorrectly = function(marked, varName) {
+    // marked should be a VariableDeclarator
+    n.VariableDeclarator.assert(marked);
 
-// TODO - how do i get recast to return comments?
-describe("pure", function() {
-  xit("should work with a function expression", function() {
-    var ast = recast.parse('var a = function* foo(){};', {
-        parser: require("babylon")
+    // using our variable name
+    assert.strictEqual(marked.id.name, varName);
+
+    // assiging a call expression to regeneratorRuntime.mark()
+    n.CallExpression.assert(marked.init);
+    assert.strictEqual(marked.init.callee.object.name, 'regeneratorRuntime')
+    assert.strictEqual(marked.init.callee.property.name, 'mark')
+
+    // with said call expression marked as a pure function
+    assert.strictEqual(marked.init.leadingComments[0].value, '#__PURE__');
+  };
+
+  describe("function declarations", function() {
+    it("should work with a single function", function() {
+      var ast = recast.parse('function* foo(){};', {
+          parser: require("babylon")
+      });
+
+      // get our declarations
+      const declaration = transform(ast).program.body[0];
+      n.VariableDeclaration.assert(declaration);
+      const declarations = declaration.declarations;
+
+      // verify our declaration is marked correctly
+      itMarksCorrectly(declarations[0], '_marked');
+
+      // and has our function name as its first argument
+      assert.strictEqual(declarations[0].init.arguments[0].name, 'foo');
     });
-    const code = recast.print(transform(ast)).code;
-    assert.notStrictEqual(
-      code.indexOf(
-        'var a = /*#__PURE__*/regeneratorRuntime.mark(function foo() {'
-      ), -1
-    );
+
+    it("should work with multiple functions", function() {
+      var ast = recast.parse([
+        'function* foo(){};',
+        'function* bar() {};'
+      ].join("\n"), {
+          parser: require("babylon")
+      });
+
+      // get our declarations
+      const declaration = transform(ast).program.body[0];
+      n.VariableDeclaration.assert(declaration);
+      const declarations = declaration.declarations;
+
+      // verify our declarations are marked correctly and have our function name
+      // as their first argument
+      itMarksCorrectly(declarations[0], '_marked');
+      n.Identifier.assert(declarations[0].init.arguments[0]);
+      assert.strictEqual(declarations[0].init.arguments[0].name, 'foo');
+
+      itMarksCorrectly(declarations[1], '_marked2');
+      n.Identifier.assert(declarations[1].init.arguments[0]);
+      assert.strictEqual(declarations[1].init.arguments[0].name, 'bar');
+    });
   });
-  xit("should work with a function declaration", function() {
-    var ast = recast.parse('function* foo(){};', {
-      parser: require("babylon")
+
+  describe("function expressions", function() {
+    it("should work with a named function", function() {
+      var ast = recast.parse('var a = function* foo(){};', {
+          parser: require("babylon")
+      });
+
+      // get our declarations
+      const declaration = transform(ast).program.body[0];
+      n.VariableDeclaration.assert(declaration);
+      const declarator = declaration.declarations[0];
+
+      // verify our declaration is marked correctly
+      itMarksCorrectly(declarator, 'a');
+
+      // and that our first argument is our original function expression
+      n.FunctionExpression.assert(declarator.init.arguments[0]);
+      assert.strictEqual(declarator.init.arguments[0].id.name, 'foo');
     });
-    const code = recast.print(transform(ast)).code;
-    assert.notStrictEqual(
-      code.indexOf(
-        'var a = /*#__PURE__*/Object.defineProperty( /*#__PURE__*/regeneratorRuntime.mark'
-      ), -1
-    );
+
+    it("should work with an anonymous function", function() {
+      var ast = recast.parse('var a = function* (){};', {
+          parser: require("babylon")
+      });
+
+      // get our declarations
+      const declaration = transform(ast).program.body[0];
+      n.VariableDeclaration.assert(declaration);
+      const declarator = declaration.declarations[0];
+
+      // verify our declaration is marked correctly
+      itMarksCorrectly(declarator, 'a');
+
+      // and that our first argument is our original function expression
+      n.FunctionExpression.assert(declarator.init.arguments[0]);
+      assert.strictEqual(declarator.init.arguments[0].id.name, '_callee');
+    });
   });
 });
