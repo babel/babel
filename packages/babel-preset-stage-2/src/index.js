5249d011d0f9b4a32d698c61971e24dacf43b56e
Rick Waldron <waldron.rick@gmail.com>
2017-08-08 17:04:52 -0400

    Add numeric separator to stage 2 preset

--- packages/babel-preset-stage-2/src/index.js
+++ packages/babel-preset-stage-2/src/index.js
@@ -2,10 +2,15 @@ import presetStage3 from "babel-preset-stage-3";
 
 import transformClassProperties from "babel-plugin-transform-class-properties";
 import transformFunctionSent from "babel-plugin-transform-function-sent";
+import transformNumericSeparator from "babel-plugin-transform-numeric-separator";
 
 export default function() {
   return {
     presets: [presetStage3],
-    plugins: [transformClassProperties, transformFunctionSent],
+    plugins: [
+      transformClassProperties,
+      transformFunctionSent,
+      transformNumericSeparator,
+    ],
   };
 }
