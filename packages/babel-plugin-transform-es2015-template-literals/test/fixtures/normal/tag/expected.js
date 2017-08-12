96a3a11d8dea80eb3bdef29a3428d46964f9ea25
Noah Lemen <noah.lemen@gmail.com>
2017-08-12 18:12:18 -0400

    spec/loose/default switch for template literal transform, update/re-org tests

--- packages/babel-plugin-transform-es2015-template-literals/test/fixtures/normal/tag/expected.js
+++
@@ -1,5 +0,0 @@
-var _templateObject = _taggedTemplateLiteral(["wow\na", "b ", ""], ["wow\\na", "b ", ""]);
-
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-
-var foo = bar(_templateObject, 42, _.foobar());
