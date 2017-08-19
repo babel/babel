76475637665ae72a689509aa9d8890c034708658
Justin Ridgewell <justin@ridgewell.name>
2017-08-19 00:19:37 -0400

    Remove babel-standalone
    This reverts #6029 due to issues in #6120.
    
    We need to first move `babel-loader` into the monorepo (and update its
    dependencies on `babel-core`). Afterwards, we can re-add
    `babel-standalone`.

--- scripts/_get-test-directories.sh
+++ scripts/_get-test-directories.sh
@@ -7,10 +7,6 @@ for f in packages/*; do
   if [ -n "$TEST_ONLY" ] && [[ `basename $f` != *"$TEST_ONLY"* ]]; then
     continue
   fi
-  # Exclude babel-standalone from coverage runs
-  if [ "$TEST_TYPE" = "cov" ] && [ `basename $f` = 'babel-standalone' ]; then 
-    continue
-  fi
 
   if [ -d "$f/test" ]; then
     TEST_DIRS="$f/test $TEST_DIRS"
