c022c7a8fd7cc426a5bdfbfc21fb5aa1902e7204
Astha <asthamaths@gmail.com>
2017-08-22 00:19:28 +0530

    Removed the deprecated jsx pragma detection code and the concerned tests that included jsx-pragma

--- packages/babel-plugin-transform-react-jsx/src/index.js
+++ packages/babel-plugin-transform-react-jsx/src/index.js
@@ -21,23 +21,7 @@ export default function({ types: t }) {
   });
 
   visitor.Program = function(path, state) {
-    const { file } = state;
     let id = state.opts.pragma || "React.createElement";
-
-    for (const comment of (file.ast.comments: Array<Object>)) {
-      const matches = JSX_ANNOTATION_REGEX.exec(comment.value);
-      if (matches) {
-        id = matches[1];
-        if (id === "React.DOM") {
-          throw file.buildCodeFrameError(
-            comment,
-            "The @jsx React.DOM pragma has been deprecated as of React 0.12",
-          );
-        } else {
-          break;
-        }
-      }
-    }
 
     state.set("jsxIdentifier", () =>
       id
