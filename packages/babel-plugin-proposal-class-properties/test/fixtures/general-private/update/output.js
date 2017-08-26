db8c209696cc88545b2fe6c48a2d65d206a19548
Justin Ridgewell <justin@ridgewell.name>
2017-08-26 17:29:56 -0400

    Review updates

--- packages/babel-plugin-proposal-class-properties/test/fixtures/general-private/update/output.js
+++ packages/babel-plugin-proposal-class-properties/test/fixtures/general-private/update/output.js
@@ -12,9 +12,9 @@ var Foo = function () {
     value: function test(other) {
       var _this$foo, _other$obj, _other$obj$foo, _other$obj2;
 
-      babelHelpers.privateClassPropertyPutSpec(this, _foo, (_this$foo = babelHelpers.privateClassPropertyGetSpec(this, _foo)) + 1), _this$foo;
+      babelHelpers.privateClassPropertyPutSpec(this, _foo, (_this$foo = +babelHelpers.privateClassPropertyGetSpec(this, _foo)) + 1), _this$foo;
       babelHelpers.privateClassPropertyPutSpec(this, _foo, babelHelpers.privateClassPropertyGetSpec(this, _foo) + 1);
-      babelHelpers.privateClassPropertyPutSpec(_other$obj = other.obj, _foo, (_other$obj$foo = babelHelpers.privateClassPropertyGetSpec(_other$obj, _foo)) + 1), _other$obj$foo;
+      babelHelpers.privateClassPropertyPutSpec(_other$obj = other.obj, _foo, (_other$obj$foo = +babelHelpers.privateClassPropertyGetSpec(_other$obj, _foo)) + 1), _other$obj$foo;
       babelHelpers.privateClassPropertyPutSpec(_other$obj2 = other.obj, _foo, babelHelpers.privateClassPropertyGetSpec(_other$obj2, _foo) + 1);
     }
   }]);
