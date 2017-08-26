ab1170333850447e94a012f79f17d1ec7bf3046a
Justin Ridgewell <justin@ridgewell.name>
2017-08-26 01:59:42 -0400

    Fix postfix, again.

--- packages/babel-plugin-proposal-class-properties/test/fixtures/general-private/update/output.js
+++ packages/babel-plugin-proposal-class-properties/test/fixtures/general-private/update/output.js
@@ -10,16 +10,12 @@ var Foo = function () {
   babelHelpers.createClass(Foo, [{
     key: "test",
     value: function test(other) {
-      var _other$obj, _other$obj2, _this$foo, _other$obj3, _other$obj$foo, _other$obj4;
+      var _this$foo, _other$obj, _other$obj$foo, _other$obj2;
 
+      babelHelpers.privateClassPropertyPutSpec(this, _foo, (_this$foo = babelHelpers.privateClassPropertyGetSpec(this, _foo)) + 1), _this$foo;
       babelHelpers.privateClassPropertyPutSpec(this, _foo, babelHelpers.privateClassPropertyGetSpec(this, _foo) + 1);
-      babelHelpers.privateClassPropertyPutSpec(this, _foo, babelHelpers.privateClassPropertyGetSpec(this, _foo) + 1);
-      babelHelpers.privateClassPropertyPutSpec(_other$obj = other.obj, _foo, babelHelpers.privateClassPropertyGetSpec(_other$obj, _foo) + 1);
+      babelHelpers.privateClassPropertyPutSpec(_other$obj = other.obj, _foo, (_other$obj$foo = babelHelpers.privateClassPropertyGetSpec(_other$obj, _foo)) + 1), _other$obj$foo;
       babelHelpers.privateClassPropertyPutSpec(_other$obj2 = other.obj, _foo, babelHelpers.privateClassPropertyGetSpec(_other$obj2, _foo) + 1);
-      0, (babelHelpers.privateClassPropertyPutSpec(this, _foo, (_this$foo = babelHelpers.privateClassPropertyGetSpec(this, _foo)) + 1), _this$foo);
-      0, babelHelpers.privateClassPropertyPutSpec(this, _foo, babelHelpers.privateClassPropertyGetSpec(this, _foo) + 1);
-      0, (babelHelpers.privateClassPropertyPutSpec(_other$obj3 = other.obj, _foo, (_other$obj$foo = babelHelpers.privateClassPropertyGetSpec(_other$obj3, _foo)) + 1), _other$obj$foo);
-      0, babelHelpers.privateClassPropertyPutSpec(_other$obj4 = other.obj, _foo, babelHelpers.privateClassPropertyGetSpec(_other$obj4, _foo) + 1);
     }
   }]);
   return Foo;
