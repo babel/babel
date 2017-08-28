40805894c573bd513d4784246c90c6af625cf57c
Noah Lemen <noah.lemen@gmail.com>
2017-08-28 14:57:09 -0400

    default to spec mode for template literal transform (#6098)
    * spec/loose/default switch for template literal transform, update/re-org tests
    
    * update readme
    
    * flip if statements
    
    * consolidate else/if
    
    * readme wording modification, updates to examples

---
+++ packages/babel-plugin-transform-es2015-template-literals/test/fixtures/loose/tag/expected.js
@@ -0,0 +1,5 @@
+var _templateObject = _taggedTemplateLiteralLoose(["wow\na", "b ", ""], ["wow\\na", "b ", ""]);
+
+function _taggedTemplateLiteralLoose(strings, raw) { strings.raw = raw; return strings; }
+
+var foo = bar(_templateObject, 42, _.foobar());
--- packages/babel-plugin-transform-es2015-template-literals/test/fixtures/normal/tag-loose/actual.js
+++
@@ -1,3 +0,0 @@
-var foo = bar`wow\na${ 42 }b ${_.foobar()}`;
-var bar = bar`wow\nab${ 42 } ${_.foobar()}`;
-var bar = bar`wow\naB${ 42 } ${_.baz()}`;
