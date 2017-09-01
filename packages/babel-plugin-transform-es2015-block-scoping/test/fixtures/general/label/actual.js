4c606e91e950455336736a852b82bf0d40a3acab
Justin Ridgewell <justin@ridgewell.name>
2017-09-01 00:27:49 -0400

    Update babylon and remove bad label tests

--- packages/babel-plugin-transform-es2015-block-scoping/test/fixtures/general/label/actual.js
+++ packages/babel-plugin-transform-es2015-block-scoping/test/fixtures/general/label/actual.js
@@ -1,15 +1,3 @@
-let x, y;
-{
-  a: let x;
-  let y;
-}
-
-switch (0) {
-  case 0: a: let x=0;
-}
-
-// it should break from inside of switch statements using labels
-
 loop:
 for (var i = 0; i < 10; i++) {
   const z = 3; // force loop function
