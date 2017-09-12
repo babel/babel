1ed188c986f40974c11a69999e4c698a34974b5a
Matias Hernandez <matiasfh@gmail.com>
2017-09-12 17:56:45 -0300

    fix expected to use double quoutes

--- packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/build-for-loose-mode/looseMode-true/expected.js
+++ packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/build-for-loose-mode/looseMode-true/expected.js
@@ -5,6 +5,6 @@ exports.__esModule = true;
 var _mod = require("mod");
 
 Object.keys(_mod).forEach(function (key) {
-  if (key === 'default' || key === '__esModule') return;
+  if (key === "default" || key === "__esModule") return;
   exports[key] = obj[key];
 });
