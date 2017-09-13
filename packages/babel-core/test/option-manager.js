0c5fae2faa251e3c2459071cacc41408ef56150d
Sven SAULEAU <xtuc@users.noreply.github.com>
2017-09-13 05:59:00 +0200

    Make sure source type is module when parsing .mjs (#5700)
    * feat: force source type module for mjs extension
    
    * style: fix lint

--- packages/babel-core/test/option-manager.js
+++ packages/babel-core/test/option-manager.js
@@ -44,6 +44,17 @@ describe("option-manager", () => {
     });
   });
 
+  describe("source type", function() {
+    it("should set module for .mjs extension", () => {
+      const config = manageOptions({
+        sourceType: "program",
+        filename: "foo.mjs",
+      });
+
+      assert.equal(config.options.sourceType, "module");
+    });
+  });
+
   describe("presets", function() {
     function presetTest(name) {
       it(name, function() {
