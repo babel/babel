5bb6a83fa858755864e0368aa21a4823308f0f3f
Maël Nison <nison.mael@gmail.com>
2017-09-12 19:13:35 -0700

    Add new tests for export * from with other names.

--- packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/strict/export-all/expected.js
+++ packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/strict/export-all/expected.js
@@ -3,6 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.b = b;
+exports.default = _default;
+Object.defineProperty(exports, "c", {
+  enumerable: true,
+  get: function () {
+    return _mod.c;
+  }
+});
+exports.f = exports.e = exports.d = exports.a = exports.z = void 0;
 
 var _mod = require("mod");
 
@@ -15,3 +24,20 @@ Object.keys(_mod).forEach(function (key) {
     }
   });
 });
+var z = 100;
+exports.z = z;
+
+class a {}
+
+exports.a = a;
+
+function b() {}
+
+var d = 42;
+exports.d = d;
+var e = 1,
+    f = 2;
+exports.f = f;
+exports.e = e;
+
+function _default() {}
