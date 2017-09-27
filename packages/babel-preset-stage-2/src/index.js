243f461d11a3ef92021cd4a34e91b97cbbb9f292
Justin Ridgewell <jridgewell@google.com>
2017-09-27 14:52:55 -0400

    Add to stage 2

--- packages/babel-preset-stage-2/src/index.js
+++ packages/babel-preset-stage-2/src/index.js
@@ -3,6 +3,7 @@ import presetStage3 from "babel-preset-stage-3";
 import transformFunctionSent from "babel-plugin-transform-function-sent";
 import transformExportNamespace from "babel-plugin-transform-export-namespace";
 import transformNumericSeparator from "babel-plugin-transform-numeric-separator";
+import transformThrowExpressions from "babel-plugin-transform-throw-expressions";
 
 export default function() {
   return {
@@ -11,6 +12,7 @@ export default function() {
       transformFunctionSent,
       transformExportNamespace,
       transformNumericSeparator,
+      transformThrowExpressions,
     ],
   };
 }
