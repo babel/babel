7c8a6cb4611f13124fcd13aeea2b5035ac5f21fc
Joe Lim <xjlim@users.noreply.github.com>
2017-09-29 21:15:35 -0700

    remove inline plugin from Babel's .babelrc (#6348)

--- .babelrc.js
+++ .babelrc.js
@@ -6,24 +6,6 @@ let envOpts = {
 
 module.exports = {
   comments: false,
-  plugins: [
-    // temp until next release
-    function() {
-      return {
-        visitor: {
-          "Function": function(path) {
-            const node = path.node;
-            for (let i = 0; i < node.params.length; i++) {
-              const param = node.params[i];
-              if (param.type === "AssignmentPattern") {
-                param.left.optional = false;
-              }
-            }
-          }
-        }
-      };
-    }
-  ],
   presets: [
     ["env", envOpts],
     "stage-0",
