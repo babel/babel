01ab585b5fb7c33a113126851d0a29271b28296e
Pasindu Perera <udnisap@users.noreply.github.com>
2017-09-30 14:22:42 -0700

    refactor babel-messages inline usage of traverseNeedsParent

--- packages/babel-messages/src/index.js
+++ packages/babel-messages/src/index.js
@@ -8,8 +8,6 @@ import * as util from "util";
  */
 
 export const MESSAGES = {
-  traverseNeedsParent:
-    "You must pass a scope and parentPath unless traversing a Program/File. Instead of that you tried to traverse a $1 node without passing scope and parentPath.",
   traverseVerifyVisitorProperty:
     "You passed `traverse()` a visitor object with the property $1 that has the invalid property $2",
   traverseVerifyNodeType:
