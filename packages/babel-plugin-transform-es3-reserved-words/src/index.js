e125c06814da7e065d594dc1fb6a00a20ab914e8
Mauro Bringolf <mauro@webkinder.ch>
2017-10-14 10:36:46 +0200

    Replace .includes with .indexOf and better node set to visit

--- packages/babel-plugin-transform-es3-reserved-words/src/index.js
+++ packages/babel-plugin-transform-es3-reserved-words/src/index.js
@@ -1,11 +1,11 @@
 import { es3, es2017 } from "./reserved-words";
 
 export default function() {
-  const reservedKeywords = es3.filter(w => !es2017.includes(w));
+  const reservedKeywords = es3.filter(w => !(es2017.indexOf(w) !== -1));
 
   return {
     visitor: {
-      Identifier(path) {
+      "BindingIdentifier|ReferencedIdentifier"(path) {
         if (reservedKeywords.includes(path.node.name)) {
           path.scope.rename(path.node.name);
         }
