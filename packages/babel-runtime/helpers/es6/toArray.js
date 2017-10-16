919bdf5e7960bfb40033751329b65eb1944fe1be
Henry Zhu <hi@henryzoo.com>
2017-10-16 18:25:27 -0400

    Scoped: remove unncessary deps from standalone [skip ci]

--- packages/babel-runtime/helpers/es6/toArray.js
+++ packages/babel-runtime/helpers/es6/toArray.js
@@ -1,4 +1,4 @@
-import _Array$from from "../../core-js/array/from";
+import _Array$from from "@babel/runtime/core-js/array/from";
 export default function _toArray(arr) {
   return Array.isArray(arr) ? arr : _Array$from(arr);
 }
