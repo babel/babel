f48410ee28975a0a9a7eaae77f8b04b8bf425d71
Lucas Azzola <derflatulator@gmail.com>
2017-10-18 16:29:33 +1100

    Fix note and copy to an inline comment

--- packages/babel-plugin-transform-nullish-coalescing-operator/README.md
+++ packages/babel-plugin-transform-nullish-coalescing-operator/README.md
@@ -18,7 +18,7 @@ var _object$foo;
 var foo = (_object$foo = object.foo, _object$foo !== null && _object$foo !== void 0 ? _object$foo : "default");
 ```
 
-> **NOTE:** We cannot use `!= null` here because `document.all` is `!= null` and
+> **NOTE:** We cannot use `!= null` here because `document.all == null` and
 > `document.all` has been deemed not "nullish".
 
 ## Installation
--- packages/babel-plugin-transform-nullish-coalescing-operator/src/index.js
+++ packages/babel-plugin-transform-nullish-coalescing-operator/src/index.js
@@ -18,6 +18,8 @@ export default function({ types: t }) {
           t.sequenceExpression([
             t.assignmentExpression("=", ref, node.left),
             t.conditionalExpression(
+              // We cannot use `!= null` here because `document.all == null`
+              // and `document.all` has been deemed not "nullish".
               t.logicalExpression(
                 "&&",
                 t.binaryExpression("!==", t.clone(ref), t.nullLiteral()),
