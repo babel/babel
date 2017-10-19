f3a783ce13199bf92f1c69c696a6e032d2fb8406
Brian Ng <bng412@gmail.com>
2017-10-19 09:25:54 -0500

    Remove syntax-trailing-function-commas from Babel presets

--- packages/babel-preset-es2017/src/index.js
+++ packages/babel-preset-es2017/src/index.js
@@ -1,8 +1,7 @@
-import syntaxTrailingFunctionCommas from "babel-plugin-syntax-trailing-function-commas";
 import transformAsyncToGenerator from "@babel/plugin-transform-async-to-generator";
 
 export default function() {
   return {
-    plugins: [syntaxTrailingFunctionCommas, transformAsyncToGenerator],
+    plugins: [transformAsyncToGenerator],
   };
 }
