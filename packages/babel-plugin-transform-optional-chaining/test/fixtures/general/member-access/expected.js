9e0f5235b1ca5167c368a576ad7c5af62d20b0e3
Lucas Azzola <derflatulator@gmail.com>
2017-10-22 06:55:39 +1100

    Optional Chaining: Account for document.all (#6525)

--- packages/babel-plugin-transform-optional-chaining/test/fixtures/general/member-access/expected.js
+++ packages/babel-plugin-transform-optional-chaining/test/fixtures/general/member-access/expected.js
@@ -1,12 +1,12 @@
 var _foo, _a, _a$b$c, _a$b, _a$b$c$d, _a$b$c2, _a$b$c2$d, _orders, _orders2, _orders2$, _client, _orders$client$key, _a2, _c, _a3;
 
-(_foo = foo) == null ? void 0 : _foo.bar;
-(_a = a) == null ? void 0 : (_a$b$c = _a.b.c) == null ? void 0 : _a$b$c.d.e;
-(_a$b = a.b) == null ? void 0 : (_a$b$c$d = _a$b.c.d) == null ? void 0 : _a$b$c$d.e;
-(_a$b$c2 = a.b.c) == null ? void 0 : (_a$b$c2$d = _a$b$c2.d) == null ? void 0 : _a$b$c2$d.e;
-(_orders = orders) == null ? void 0 : _orders[0].price;
-(_orders2 = orders) == null ? void 0 : (_orders2$ = _orders2[0]) == null ? void 0 : _orders2$.price;
-orders[(_client = client) == null ? void 0 : _client.key].price;
-(_orders$client$key = orders[client.key]) == null ? void 0 : _orders$client$key.price;
-(0, (_a2 = a) == null ? void 0 : _a2.b).c;
-(0, (_c = (0, (_a3 = a) == null ? void 0 : _a3.b).c) == null ? void 0 : _c.d).e;
+(_foo = foo) === null || _foo === void 0 ? void 0 : _foo.bar;
+(_a = a) === null || _a === void 0 ? void 0 : (_a$b$c = _a.b.c) === null || _a$b$c === void 0 ? void 0 : _a$b$c.d.e;
+(_a$b = a.b) === null || _a$b === void 0 ? void 0 : (_a$b$c$d = _a$b.c.d) === null || _a$b$c$d === void 0 ? void 0 : _a$b$c$d.e;
+(_a$b$c2 = a.b.c) === null || _a$b$c2 === void 0 ? void 0 : (_a$b$c2$d = _a$b$c2.d) === null || _a$b$c2$d === void 0 ? void 0 : _a$b$c2$d.e;
+(_orders = orders) === null || _orders === void 0 ? void 0 : _orders[0].price;
+(_orders2 = orders) === null || _orders2 === void 0 ? void 0 : (_orders2$ = _orders2[0]) === null || _orders2$ === void 0 ? void 0 : _orders2$.price;
+orders[(_client = client) === null || _client === void 0 ? void 0 : _client.key].price;
+(_orders$client$key = orders[client.key]) === null || _orders$client$key === void 0 ? void 0 : _orders$client$key.price;
+(0, (_a2 = a) === null || _a2 === void 0 ? void 0 : _a2.b).c;
+(0, (_c = (0, (_a3 = a) === null || _a3 === void 0 ? void 0 : _a3.b).c) === null || _c === void 0 ? void 0 : _c.d).e;
