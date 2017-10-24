eb19ea18cc754d3d5cdf98b638ecf3a5328339b7
Sylvain Delabye <syl.delabye@gmail.com>
2017-10-24 20:17:05 +0200

    Remove stale emoji tests in plugin-transform-unicode-property-regex (#6548)

--- packages/babel-plugin-transform-unicode-property-regex/test/fixtures/with-unicode-flag/emoji/actual.js
+++
@@ -1,2 +0,0 @@
-var regexEmojiModifier = /\p{Emoji_Modifier}/u;
-var regexEmojiComponent = /\p{Emoji_Component}/u;
--- packages/babel-plugin-transform-unicode-property-regex/test/fixtures/with-unicode-flag/emoji/expected.js
+++
@@ -1,2 +0,0 @@
-var regexEmojiModifier = /[\u{1F3FB}-\u{1F3FF}]/u;
-var regexEmojiComponent = /[#\*0-9\u200D\u20E3\uFE0F\u{1F1E6}-\u{1F1FF}\u{1F3FB}-\u{1F3FF}\u{E0020}-\u{E007F}]/u;
