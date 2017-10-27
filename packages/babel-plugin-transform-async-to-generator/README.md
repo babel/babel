6628c952a52bd1605d40209ea1b55eefe41b56ae
Henry Zhu <hi@henryzoo.com>
2017-10-27 16:34:27 -0400

    Merge transform-async-to-module-method into transform-async-to-generator

--- packages/babel-plugin-transform-async-to-generator/README.md
+++ packages/babel-plugin-transform-async-to-generator/README.md
@@ -2,6 +2,8 @@
 
 > Turn async functions into ES2015 generators
 
+> In Babel 7, `transform-async-to-module-method` was merged into this plugin
+
 ## Example
 
 **In**
@@ -23,6 +25,18 @@ var foo = _asyncToGenerator(function* () {
 });
 ```
 
+**Out with options**
+
+> Turn async functions into a Bluebird coroutine
+
+```javascript
+var Bluebird = require("bluebird");
+
+var foo = Bluebird.coroutine(function* () {
+  yield bar();
+});
+```
+
 ## Installation
 
 ```sh
@@ -35,9 +49,24 @@ npm install --save-dev @babel/plugin-transform-async-to-generator
 
 **.babelrc**
 
+Without options:
+
 ```json
 {
   "plugins": ["@babel/transform-async-to-generator"]
+}
+```
+
+With options:
+
+```json
+{
+  "plugins": [
+    ["@babel/transform-async-to-generator", {
+      "module": "bluebird",
+      "method": "coroutine"
+    }]
+  ]
 }
 ```
 
--- packages/babel-plugin-transform-async-to-generator/src/index.js
+++ packages/babel-plugin-transform-async-to-generator/src/index.js
@@ -1,6 +1,30 @@
 import remapAsyncToGenerator from "@babel/helper-remap-async-to-generator";
+import { addNamed } from "@babel/helper-module-imports";
 
-export default function() {
+export default function({ types: t }, options) {
+  const { method, module } = options;
+
+  if (method && module) {
+    return {
+      visitor: {
+        Function(path, state) {
+          if (!path.node.async || path.node.generator) return;
+
+          let wrapAsync = state.methodWrapper;
+          if (wrapAsync) {
+            wrapAsync = t.cloneDeep(wrapAsync);
+          } else {
+            wrapAsync = state.methodWrapper = addNamed(path, method, module);
+          }
+
+          remapAsyncToGenerator(path, state.file, {
+            wrapAsync,
+          });
+        },
+      },
+    };
+  }
+
   return {
     visitor: {
       Function(path, state) {
