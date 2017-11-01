915951bf0ce98e5bdf0dafc71187cc160019c227
Artem Yavorsky <artem@yavorsky.org>
2017-11-01 11:50:10 +0200

    Split up fixtures with evaluated variables.

--- experimental/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/builtins-used-evaluated-class-methods/actual.js
+++ experimental/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/builtins-used-evaluated-class-methods/actual.js
@@ -1,13 +1,12 @@
-var arrayInstance = [];
 var objectClass = Object;
+var arrayInstance = [];
 var assignStr = "assign";
+var entriesStr = "entries";
 var valuesStr = "values";
 var inclidesStr = "includes";
 var findStr = "find";
 
-// Allow instance methods be assigned to variables.
-arrayInstance[inclidesStr](); // include
-externalVar[findStr]; // include
 // Allow static methods be assigned to variables only directly in the module.
 externalVar[valuesStr]; // don't include
 objectClass[assignStr]({}); // include
+arrayInstance[entriesStr]({}); // don't include
--- experimental/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/builtins-used-evaluated-instance-methods/expected.js
+++ experimental/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/builtins-used-evaluated-instance-methods/expected.js
@@ -1,18 +1,10 @@
-import "@babel/polyfill/lib/core-js/modules/es6.object.assign";
 import "@babel/polyfill/lib/core-js/modules/es6.array.find";
 import "@babel/polyfill/lib/core-js/modules/es7.array.includes";
 var arrayInstance = [];
-var objectClass = Object;
-var assignStr = "assign";
-var valuesStr = "values";
 var inclidesStr = "includes";
 var findStr = "find"; // Allow instance methods be assigned to variables.
 
 arrayInstance[inclidesStr](); // include
 
 externalVar[findStr]; // include
-// Allow static methods be assigned to variables only directly in the module.
 
-externalVar[valuesStr]; // don't include
-
-objectClass[assignStr]({}); // include
