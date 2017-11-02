c75db455ac61a1e3eb4d4b3792bb9e5702c725c1
Amin Marashi <marashi.s.amin@gmail.com>
2017-11-03 01:46:16 +0800

    Fix gen_error file

--- packages/babel-register/test/__data__/gen_error.js
+++ packages/babel-register/test/__data__/gen_error.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
-module.exports = () => { let error; try { const impossible_result = ''.toFixed(); } catch (e) { error = e; } return error.stack.toString().replace('\n', ''); }
+module.exports = () => { 'use strict'; let error; try { const impossible_result = ''.toFixed(); } catch (e) { error = e; } return error.stack.toString().replace('\n', ''); }
 
 
