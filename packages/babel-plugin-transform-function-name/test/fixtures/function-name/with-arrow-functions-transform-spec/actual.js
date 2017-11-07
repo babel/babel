d9f6e71a1c481c03d4e12cba148575eef9af19c7
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2017-11-07 16:14:07 +0900

    Missing fixture updates

--- packages/babel-plugin-transform-function-name/test/fixtures/function-name/with-arrow-functions-transform-spec/actual.js
+++ packages/babel-plugin-transform-function-name/test/fixtures/function-name/with-arrow-functions-transform-spec/actual.js
@@ -1,5 +1,4 @@
 // These are actually handled by transform-arrow-functions
-// x's name does not match exactly to avoid shadowing the const x
 const x = () => x;
 const y = x => x();
 const z = { z: () => y(x) }.z;
--- packages/babel-plugin-transform-function-name/test/fixtures/function-name/with-arrow-functions-transform-spec/expected.js
+++ packages/babel-plugin-transform-function-name/test/fixtures/function-name/with-arrow-functions-transform-spec/expected.js
@@ -1,10 +1,9 @@
 var _this = this;
 
 // These are actually handled by transform-arrow-functions
-// x's name does not match exactly to avoid shadowing the const x
-const x = function _x() {
+const _x = function x() {
   babelHelpers.newArrowCheck(this, _this);
-  return x;
+  return _x;
 }.bind(this);
 
 const y = function y(x) {
@@ -15,6 +14,6 @@ const y = function y(x) {
 const z = {
   z: function z() {
     babelHelpers.newArrowCheck(this, _this);
-    return y(x);
+    return y(_x);
   }.bind(this)
 }.z;
