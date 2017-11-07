05f204de91549a8136c3412b5363f8dec5e8daa5
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2017-11-07 16:03:18 +0900

    Much simpler version of the above fix

--- packages/babel-plugin-transform-arrow-functions/test/fixtures/arrow-functions/spec-self-referential/expected.js
+++ packages/babel-plugin-transform-arrow-functions/test/fixtures/arrow-functions/spec-self-referential/expected.js
@@ -2,20 +2,22 @@ var _this = this;
 
 var fooCalls = [];
 
-var jumpTable = function _jumpTable(name, ...args) {
+var _jumpTable = function jumpTable(name, ...args) {
   babelHelpers.newArrowCheck(this, _this);
 
-  if (jumpTable[name]) {
-    jumpTable[name](...args);
+  if (_jumpTable[name]) {
+    _jumpTable[name](...args);
   }
 }.bind(this);
 
-Object.assign(jumpTable, {
+Object.assign(_jumpTable, {
   foo(...args) {
     fooCalls.push(args);
   }
 
 });
-jumpTable('foo', 'bar');
+
+_jumpTable('foo', 'bar');
+
 assert.isArray(fooCalls[0]);
 assert.strictEqual(fooCalls[0][0], 'bar');
