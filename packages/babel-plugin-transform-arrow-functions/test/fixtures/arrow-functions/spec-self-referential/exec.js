2ff5c3edd7b54a765755c5134fdbd330d720e422
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2017-11-07 16:33:22 +0900

    ...actually update _all_ the fixtures

--- packages/babel-plugin-transform-arrow-functions/test/fixtures/arrow-functions/spec-self-referential/exec.js
+++ packages/babel-plugin-transform-arrow-functions/test/fixtures/arrow-functions/spec-self-referential/exec.js
@@ -1,8 +1,6 @@
 var fooCalls = []
-var jumpTable = (name, ...args) => { if (jumpTable[name]) { jumpTable[name](...args) } }
-Object.assign(jumpTable, { foo (...args) { fooCalls.push(args) } });
+var jumpTable = (name, arg) => { if (jumpTable[name]) { jumpTable[name](arg) } }
+Object.assign(jumpTable, { foo (arg) { fooCalls.push(arg) } });
 jumpTable('foo', 'bar')
 
-assert.isArray(fooCalls[0])
-assert.strictEqual(fooCalls[0][0], 'bar')
-assert.match(jumpTable.name, /jumpTable/)
+assert.strictEqual(fooCalls[0], 'bar')
--- packages/babel-plugin-transform-arrow-functions/test/fixtures/arrow-functions/spec-self-referential/expected.js
+++ packages/babel-plugin-transform-arrow-functions/test/fixtures/arrow-functions/spec-self-referential/expected.js
@@ -2,22 +2,21 @@ var _this = this;
 
 var fooCalls = [];
 
-var _jumpTable = function jumpTable(name, ...args) {
+var _jumpTable = function jumpTable(name, arg) {
   babelHelpers.newArrowCheck(this, _this);
 
   if (_jumpTable[name]) {
-    _jumpTable[name](...args);
+    _jumpTable[name](arg);
   }
 }.bind(this);
 
 Object.assign(_jumpTable, {
-  foo(...args) {
-    fooCalls.push(args);
+  foo(arg) {
+    fooCalls.push(arg);
   }
 
 });
 
 _jumpTable('foo', 'bar');
 
-assert.isArray(fooCalls[0]);
-assert.strictEqual(fooCalls[0][0], 'bar');
+assert.strictEqual(fooCalls[0], 'bar');
