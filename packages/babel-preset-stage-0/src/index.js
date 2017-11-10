5802d25f49b763f605016533f2bb4bddf1c5ac88
DZ Chan <cdzos97@gmail.com>
2017-11-10 10:44:13 +0800

    add syntax plugin for pattern matching remove vscode setting

--- packages/babel-preset-stage-0/src/index.js
+++ packages/babel-preset-stage-0/src/index.js
@@ -1,5 +1,6 @@
 import presetStage1 from "@babel/preset-stage-1";
 
+import syntaxPatternMatching from "@babel/plugin-syntax-pattern-matching";
 import transformDoExpressions from "@babel/plugin-proposal-do-expressions";
 import transformFunctionBind from "@babel/plugin-proposal-function-bind";
 
@@ -23,6 +24,10 @@ export default function(context, opts = {}) {
 
   return {
     presets: [[presetStage1, { loose, useBuiltIns }]],
-    plugins: [transformDoExpressions, transformFunctionBind],
+    plugins: [
+      syntaxPatternMatching,
+      transformDoExpressions,
+      transformFunctionBind,
+    ],
   };
 }
