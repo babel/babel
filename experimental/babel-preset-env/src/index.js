de3597983a6f3fec92395caf0d6b892ba545573e
Artem Yavorsky <info@yavorsky.org>
2017-11-15 22:13:59 +0200

    [preset-env] Exclude transform-typeof-symbol with `loose` option. (#6831)

--- experimental/babel-preset-env/src/index.js
+++ experimental/babel-preset-env/src/index.js
@@ -111,12 +111,25 @@ const getPlatformSpecificDefaultFor = (targets: Targets): ?Array<string> => {
   return isAnyTarget || isWebTarget ? defaultWebIncludes : null;
 };
 
+const getOptionSpecificExcludesFor = ({
+  loose,
+}: {
+  loose: boolean,
+}): Array<string> => {
+  const defaultExcludes = [];
+  if (loose) {
+    defaultExcludes.push("transform-typeof-symbol");
+  }
+  return defaultExcludes;
+};
+
 const filterItems = (
   list,
   includes,
   excludes,
   targets,
-  defaultItems,
+  defaultIncludes,
+  defaultExcludes,
 ): Set<string> => {
   const result = new Set();
 
@@ -135,8 +148,12 @@ const filterItems = (
     }
   }
 
-  if (defaultItems) {
-    defaultItems.forEach(item => !excludes.has(item) && result.add(item));
+  if (defaultIncludes) {
+    defaultIncludes.forEach(item => !excludes.has(item) && result.add(item));
+  }
+
+  if (defaultExcludes) {
+    defaultExcludes.forEach(item => !includes.has(item) && result.delete(item));
   }
 
   return result;
@@ -187,6 +204,8 @@ export default function buildPreset(
     include.plugins,
     exclude.plugins,
     transformTargets,
+    null,
+    getOptionSpecificExcludesFor({ loose }),
   );
 
   let polyfills;
---
+++ experimental/babel-preset-env/test/fixtures/preset-options/loose-with-typeof-symbol-includes/expected.js
@@ -0,0 +1,3 @@
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+_typeof(Symbol());
