a40a54df1963a1e35c621bdaad5e5ddafb610fea
Artem Yavorsky <artem@yavorsky.org>
2017-11-21 00:42:07 +0200

    Update umd/amd fixtures.

--- packages/babel-plugin-transform-modules-amd/test/fixtures/loose/export-from-2/expected.js
+++ packages/babel-plugin-transform-modules-amd/test/fixtures/loose/export-from-2/expected.js
@@ -2,5 +2,6 @@ define(["exports", "foo"], function (_exports, _foo) {
   "use strict";
 
   _exports.__esModule = true;
+  _exports.foo = void 0;
   _exports.foo = _foo.foo;
 });
--- packages/babel-plugin-transform-modules-amd/test/fixtures/loose/export-from-6/expected.js
+++ packages/babel-plugin-transform-modules-amd/test/fixtures/loose/export-from-6/expected.js
@@ -2,6 +2,7 @@ define(["exports", "foo"], function (_exports, _foo) {
   "use strict";
 
   _exports.__esModule = true;
+  _exports.bar = _exports.default = void 0;
   _exports.default = _foo.foo;
   _exports.bar = _foo.bar;
 });
