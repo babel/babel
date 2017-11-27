881fc14329f1b30f4e94c341f336c32dd287364f
Brian Ng <bng412@gmail.com>
2017-11-27 12:08:35 -0600

    Add handling parens for extends clause in generator (#6897)

--- packages/babel-generator/src/node/parentheses.js
+++ packages/babel-generator/src/node/parentheses.js
@@ -27,6 +27,10 @@ const PRECEDENCE = {
   "**": 10,
 };
 
+const isClassExtendsClause = (node: Object, parent: Object): boolean =>
+  (t.isClassDeclaration(parent) || t.isClassExpression(parent)) &&
+  parent.superClass === node;
+
 export function NullableTypeAnnotation(node: Object, parent: Object): boolean {
   return t.isArrayTypeAnnotation(parent);
 }
@@ -35,7 +39,10 @@ export { NullableTypeAnnotation as FunctionTypeAnnotation };
 
 export function UpdateExpression(node: Object, parent: Object): boolean {
   // (foo++).test()
-  return t.isMemberExpression(parent) && parent.object === node;
+  return (
+    (t.isMemberExpression(parent) && parent.object === node) ||
+    isClassExtendsClause(node, parent)
+  );
 }
 
 export function ObjectExpression(
@@ -60,6 +67,10 @@ export function Binary(node: Object, parent: Object): boolean {
     t.isBinaryExpression(parent, { operator: "**" })
   ) {
     return parent.left === node;
+  }
+
+  if (isClassExtendsClause(node, parent)) {
+    return true;
   }
 
   if (
@@ -151,7 +162,8 @@ export function YieldExpression(node: Object, parent: Object): boolean {
     t.isCallExpression(parent) ||
     t.isMemberExpression(parent) ||
     t.isNewExpression(parent) ||
-    (t.isConditionalExpression(parent) && node === parent.test)
+    (t.isConditionalExpression(parent) && node === parent.test) ||
+    isClassExtendsClause(node, parent)
   );
 }
 
@@ -170,7 +182,8 @@ export function UnaryLike(node: Object, parent: Object): boolean {
     t.isMemberExpression(parent, { object: node }) ||
     t.isCallExpression(parent, { callee: node }) ||
     t.isNewExpression(parent, { callee: node }) ||
-    t.isBinaryExpression(parent, { operator: "**", left: node })
+    t.isBinaryExpression(parent, { operator: "**", left: node }) ||
+    isClassExtendsClause(node, parent)
   );
 }
 
@@ -208,6 +221,10 @@ export function AssignmentExpression(node: Object): boolean {
   } else {
     return ConditionalExpression(...arguments);
   }
+}
+
+export function NewExpression(node: Object, parent: Object): boolean {
+  return isClassExtendsClause(node, parent);
 }
 
 // Walk up the print stack to determine if our node can come first
