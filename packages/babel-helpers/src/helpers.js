9d9710c765563addb4ec78e6eebe3e6ab15f1557
Daniel Tschinder <daniel@tschinder.de>
2017-11-30 22:50:10 +0100

    Copy getters and setters correctly in interopWildcard (#6850)

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -449,7 +449,16 @@ helpers.interopRequireWildcard = defineHelper(`
       var newObj = {};
       if (obj != null) {
         for (var key in obj) {
-          if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
+          if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            var desc = Object.defineProperty && Object.getOwnPropertyDescriptor
+              ? Object.getOwnPropertyDescriptor(obj, key)
+              : {};
+            if (desc.get || desc.set) {
+              Object.defineProperty(newObj, key, desc);
+            } else {
+              newObj[key] = obj[key];
+            }
+          }
         }
       }
       newObj.default = obj;
