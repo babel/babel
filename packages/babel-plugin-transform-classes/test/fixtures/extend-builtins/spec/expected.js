4f6cf5e2129c9cc164a471b37588a5332f499c25
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2017-12-13 15:01:05 +0100

    Update tests

--- packages/babel-plugin-transform-classes/test/fixtures/extend-builtins/spec/expected.js
+++ packages/babel-plugin-transform-classes/test/fixtures/extend-builtins/spec/expected.js
@@ -8,11 +8,11 @@ var _gPO = Object.getPrototypeOf || function _gPO(o) { return o.__proto__; };
 
 var _sPO = Object.setPrototypeOf || function _sPO(o, p) { o.__proto__ = p; };
 
-var _construct = Reflect.construct || function _construct(Parent, args, Class) { var Constructor, a = [null]; a.push.apply(a, args); Constructor = Parent.bind.apply(Parent, a); return _sPO(new Constructor(), Class.prototype); };
+var _construct = typeof Reflect === "object" && Reflect.construct || function _construct(Parent, args, Class) { var Constructor, a = [null]; a.push.apply(a, args); Constructor = Parent.bind.apply(Parent, a); return _sPO(new Constructor(), Class.prototype); };
 
-var _cache = typeof WeakMap === "function" && new WeakMap();
+var _cache = typeof Map === "function" && new Map();
 
-function _wrapNativeSuper(Class) { if (_cache) { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() {} Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writeable: true, configurable: true } }); return _sPO(Wrapper, _sPO(function Super() { return _construct(Class, arguments, _gPO(this).constructor); }, Class)); }
+function _wrapNativeSuper(Class) { if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() {} Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writeable: true, configurable: true } }); return _sPO(Wrapper, _sPO(function Super() { return _construct(Class, arguments, _gPO(this).constructor); }, Class)); }
 
 var List =
 /*#__PURE__*/
