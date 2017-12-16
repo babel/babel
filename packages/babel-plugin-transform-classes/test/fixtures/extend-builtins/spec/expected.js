f8e74d319a85f438d60d9d9dec8a93e1ac6c3c5d
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2017-12-16 15:49:30 +0100

    Update tests

--- packages/babel-plugin-transform-classes/test/fixtures/extend-builtins/spec/expected.js
+++ packages/babel-plugin-transform-classes/test/fixtures/extend-builtins/spec/expected.js
@@ -12,7 +12,7 @@ var _construct = typeof Reflect === "object" && Reflect.construct || function _c
 
 var _cache = typeof Map === "function" && new Map();
 
-function _wrapNativeSuper(Class) { if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() {} Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writeable: true, configurable: true } }); return _sPO(Wrapper, _sPO(function Super() { return _construct(Class, arguments, _gPO(this).constructor); }, Class)); }
+function _wrapNativeSuper(Class) { if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() {} Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writeable: true, configurable: true } }); return _sPO(Wrapper, _sPO(function Super() { return _construct(Class, arguments, _gPO(this).constructor); }, Class)); }
 
 var List =
 /*#__PURE__*/
