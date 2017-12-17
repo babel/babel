2e3c95da4d3672922e0faf6c858383e022a25dd4
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2017-12-17 14:53:01 +1100

    update expected test

---
+++ packages/babel-plugin-transform-classes/test/fixtures/loose/single-transform/expected.js
@@ -0,0 +1,14 @@
+var Alert =
+/*#__PURE__*/
+function () {
+  function Alert() {}
+
+  Object.defineProperty(Alert, "VERSION", {
+    get: function get() {
+      return VERSION;
+    },
+    enumerable: true,
+    configurable: true
+  });
+  return Alert;
+}();
