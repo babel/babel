437a811dbc5e5ff254d539095787bd17238717af
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2017-12-18 16:33:18 +0900

    Avoid adding #__PURE__ annotation to .bind(this)() expressions
    Fixes the #__PURE__ annotation getting added to (async () => {})() IIFEs when the arrow function transform is running with spec: true.

---
+++ packages/babel-plugin-transform-async-to-generator/test/fixtures/async-to-generator/async-iife-with-regenerator/expected.js
@@ -0,0 +1,53 @@
+babelHelpers.asyncToGenerator(
+/*#__PURE__*/
+regeneratorRuntime.mark(function _callee() {
+  return regeneratorRuntime.wrap(function _callee$(_context) {
+    while (1) switch (_context.prev = _context.next) {
+      case 0:
+        _context.next = 2;
+        return 'ok';
+
+      case 2:
+      case "end":
+        return _context.stop();
+    }
+  }, _callee, this);
+}))();
+babelHelpers.asyncToGenerator(
+/*#__PURE__*/
+regeneratorRuntime.mark(function _callee2() {
+  return regeneratorRuntime.wrap(function _callee2$(_context2) {
+    while (1) switch (_context2.prev = _context2.next) {
+      case 0:
+        _context2.next = 2;
+        return 'ok';
+
+      case 2:
+      case "end":
+        return _context2.stop();
+    }
+  }, _callee2, this);
+}))();
+
+/*#__PURE__*/
+(function () {
+  var _notIIFE = babelHelpers.asyncToGenerator(
+  /*#__PURE__*/
+  regeneratorRuntime.mark(function _callee3() {
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) switch (_context3.prev = _context3.next) {
+        case 0:
+          _context3.next = 2;
+          return 'ok';
+
+        case 2:
+        case "end":
+          return _context3.stop();
+      }
+    }, _callee3, this);
+  }));
+
+  return function notIIFE() {
+    return _notIIFE.apply(this, arguments);
+  };
+})();
