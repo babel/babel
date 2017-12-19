1561760cf741b31e6f1ad150ff9bf20358f69225
Andy Hanson <anhans@microsoft.com>
2017-12-19 15:10:47 -0800

    Support parsing `export default abstract class {}`

--- packages/babel-generator/test/fixtures/typescript/class-abstract/expected.js
+++ packages/babel-generator/test/fixtures/typescript/class-abstract/expected.js
@@ -3,5 +3,6 @@ abstract class C {}
 declare abstract class C {}
 
 export abstract class C {} // `export abstract class { }` is not valid.
-// `export default abstract class C { }` is not valid.
-// `abstract class` is not valid as an expression.
+
+export default abstract class {}
+export default abstract class C {} // `abstract class` is not valid as an expression.
