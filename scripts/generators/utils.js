4a4516a668178bb40f01af83dcddd593e4977575
Boris Cherny <boris@performancejs.com>
2017-12-23 23:57:25 -0800

    move generator scripts to scripts/generators

---
+++ scripts/generators/utils.js
@@ -0,0 +1,30 @@
+exports.stringifyValidator = function stringifyValidator(
+  validator,
+  nodePrefix
+) {
+  if (validator === undefined) {
+    return "any";
+  }
+
+  if (validator.each) {
+    return `Array<${stringifyValidator(validator.each, nodePrefix)}>`;
+  }
+
+  if (validator.chainOf) {
+    return stringifyValidator(validator.chainOf[1], nodePrefix);
+  }
+
+  if (validator.oneOf) {
+    return validator.oneOf.map(JSON.stringify).join(" | ");
+  }
+
+  if (validator.oneOfNodeTypes) {
+    return validator.oneOfNodeTypes.map(_ => nodePrefix + _).join(" | ");
+  }
+
+  if (validator.type) {
+    return nodePrefix + validator.type;
+  }
+
+  return ["any"];
+};
