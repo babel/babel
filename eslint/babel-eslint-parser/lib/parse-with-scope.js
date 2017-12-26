624a425f7da138772c95092c3b0d07aaac0db0d8
Toru Nagashima <star.ctor@gmail.com>
2017-12-26 10:50:13 +0900

    Fix: add Literal type to visitorKeys (babel/babel-eslint#562)
    * Fix: add Literal type to visitorKeys
    * Fix: use ./visitor-keys

--- eslint/babel-eslint-parser/lib/parse-with-scope.js
+++ eslint/babel-eslint-parser/lib/parse-with-scope.js
@@ -1,13 +1,12 @@
 "use strict";
 
-const t = require("@babel/types");
+const visitorKeys = require("./visitor-keys");
 const analyzeScope = require("./analyze-scope");
 const parse = require("./parse");
 
 module.exports = function(code, options) {
   const ast = parse(code, options);
   const scopeManager = analyzeScope(ast, options);
-  const visitorKeys = t.VISITOR_KEYS;
 
   return { ast, scopeManager, visitorKeys };
 };
--- eslint/babel-eslint-parser/lib/visitor-keys.js
+++ eslint/babel-eslint-parser/lib/visitor-keys.js
@@ -5,6 +5,7 @@ const ESLINT_VISITOR_KEYS = require("eslint-visitor-keys").KEYS;
 
 module.exports = Object.assign(
   {
+    Literal: ESLINT_VISITOR_KEYS.Literal,
     MethodDefinition: ["decorators"].concat(
       ESLINT_VISITOR_KEYS.MethodDefinition
     ),
