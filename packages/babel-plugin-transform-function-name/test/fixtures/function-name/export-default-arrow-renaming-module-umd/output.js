9c06ae31068d008c91fffdce0ea51ce7952383cb
Raja Sekar <rajasekar89be@gmail.com>
2018-01-08 04:52:38 +0530

    Renamed files
    Renamed files
    
    Deleted expected.json from babylon

---
+++ packages/babel-plugin-transform-function-name/test/fixtures/function-name/export-default-arrow-renaming-module-umd/output.js
@@ -0,0 +1,30 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.input = mod.exports;
+  }
+})(this, function (_exports) {
+  "use strict";
+
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.default = void 0;
+
+  var _default = function _default(_a) {
+    return {
+      a: function a() {
+        return _a;
+      }
+    };
+  };
+
+  _exports.default = _default;
+});
