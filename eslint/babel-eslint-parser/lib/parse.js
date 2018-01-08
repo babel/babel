2afde9943402092413bb124bf99c801d3e81c0c6
Henry Zhu <hi@henryzoo.com>
2018-01-08 17:46:57 -0500

    Add other parser plugins, update yarn.lock (babel/babel-eslint#569)

--- eslint/babel-eslint-parser/lib/parse.js
+++ eslint/babel-eslint-parser/lib/parse.js
@@ -38,6 +38,9 @@ module.exports = function(code, options) {
       "classPrivateProperties",
       "bigInt",
       "optionalCatchBinding",
+      "throwExpressions",
+      "pipelineOperator",
+      "nullishCoalescingOperator",
     ],
   };
 
