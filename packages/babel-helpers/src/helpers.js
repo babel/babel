b3246ebb7d85fe1ae6b16aef1f4c2d4d7f81d125
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-01-09 20:00:21 +0100

    Wrap wrapNativeSuper helpers in redefining functions for better tree-shakeability

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -445,46 +445,57 @@ helpers.inheritsLoose = () => template.program.ast`
 
 // Based on https://github.com/WebReflection/babel-plugin-transform-builtin-classes
 helpers.wrapNativeSuper = () => template.program.ast`
-  var _gPO = Object.getPrototypeOf || function _gPO(o) { return o.__proto__ };
-  var _sPO = Object.setPrototypeOf || function _sPO(o, p) { o.__proto__ = p; return o };
-  var _construct = (typeof Reflect === "object" && Reflect.construct) ||
-    function _construct(Parent, args, Class) {
-      var Constructor, a = [null];
-      a.push.apply(a, args);
-      Constructor = Parent.bind.apply(Parent, a);
-      return _sPO(new Constructor, Class.prototype);
-    };
-
-  var _cache = typeof Map === "function" ? new Map() : undefined;
+  function _gPO(o) {
+    _gPO = Object.getPrototypeOf || function _gPO(o) { return o.__proto__ };
+    return _gPO(o);
+  }
+  function _sPO(o, p) {
+    _sPO = Object.setPrototypeOf || function _sPO(o, p) { o.__proto__ = p; return o };
+    return _sPO(o, p);
+  }
+  function _construct(Parent, args, Class) {
+    _construct = (typeof Reflect === "object" && Reflect.construct) ||
+      function _construct(Parent, args, Class) {
+        var Constructor, a = [null];
+        a.push.apply(a, args);
+        Constructor = Parent.bind.apply(Parent, a);
+        return _sPO(new Constructor, Class.prototype);
+      };
+    return _construct(Parent, args, Class);
+  }
 
   export default function _wrapNativeSuper(Class) {
-    if (typeof Class !== "function") {
-      throw new TypeError("Super expression must either be null or a function");
-    }
+    var _cache = typeof Map === "function" ? new Map() : undefined;
 
-    if (typeof _cache !== "undefined") {
-      if (_cache.has(Class)) return _cache.get(Class);
-      _cache.set(Class, Wrapper);
-    }
-
-    function Wrapper() {}
-    Wrapper.prototype = Object.create(Class.prototype, {
-      constructor: {
-        value: Wrapper,
-        enumerable: false,
-        writable: true,
-        configurable: true,
+    _wrapNativeSuper = function _wrapNativeSuper(Class) {
+      if (typeof Class !== "function") {
+        throw new TypeError("Super expression must either be null or a function");
       }
-    });
-    return _sPO(
-      Wrapper,
-      _sPO(
-        function Super() {
-          return _construct(Class, arguments, _gPO(this).constructor);
-        },
-        Class
-      )
-    );
+      if (typeof _cache !== "undefined") {
+        if (_cache.has(Class)) return _cache.get(Class);
+        _cache.set(Class, Wrapper);
+      }
+      function Wrapper() {}
+      Wrapper.prototype = Object.create(Class.prototype, {
+        constructor: {
+          value: Wrapper,
+          enumerable: false,
+          writable: true,
+          configurable: true,
+        }
+      });
+      return _sPO(
+        Wrapper,
+        _sPO(
+          function Super() {
+            return _construct(Class, arguments, _gPO(this).constructor);
+          },
+          Class
+        )
+      );
+    }
+
+    return _wrapNativeSuper(Class)
   }
 `;
 
--- packages/babel-plugin-transform-classes/test/fixtures/extend-builtins/loose/output.js
+++ packages/babel-plugin-transform-classes/test/fixtures/extend-builtins/loose/output.js
@@ -1,14 +1,12 @@
 function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
 
-var _gPO = Object.getPrototypeOf || function _gPO(o) { return o.__proto__; };
+function _gPO(o) { _gPO = Object.getPrototypeOf || function _gPO(o) { return o.__proto__; }; return _gPO(o); }
 
-var _sPO = Object.setPrototypeOf || function _sPO(o, p) { o.__proto__ = p; return o; };
+function _sPO(o, p) { _sPO = Object.setPrototypeOf || function _sPO(o, p) { o.__proto__ = p; return o; }; return _sPO(o, p); }
 
-var _construct = typeof Reflect === "object" && Reflect.construct || function _construct(Parent, args, Class) { var Constructor, a = [null]; a.push.apply(a, args); Constructor = Parent.bind.apply(Parent, a); return _sPO(new Constructor(), Class.prototype); };
+function _construct(Parent, args, Class) { _construct = typeof Reflect === "object" && Reflect.construct || function _construct(Parent, args, Class) { var Constructor, a = [null]; a.push.apply(a, args); Constructor = Parent.bind.apply(Parent, a); return _sPO(new Constructor(), Class.prototype); }; return _construct(Parent, args, Class); }
 
-var _cache = typeof Map === "function" ? new Map() : undefined;
-
-function _wrapNativeSuper(Class) { if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() {} Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _sPO(Wrapper, _sPO(function Super() { return _construct(Class, arguments, _gPO(this).constructor); }, Class)); }
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() {} Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _sPO(Wrapper, _sPO(function Super() { return _construct(Class, arguments, _gPO(this).constructor); }, Class)); }; return _wrapNativeSuper(Class); }
 
 var List =
 /*#__PURE__*/
