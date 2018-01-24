06ee19c32955821760e2f750e25f766360bfd83d
Owen Buckley <obuckley@kenzan.com>
2018-01-24 15:16:09 -0500

    remove extra space

--- packages/babel-template/src/populate.js
+++ packages/babel-template/src/populate.js
@@ -18,7 +18,7 @@ export default function populatePlaceholders(
         const placeholderName = placeholder.name;
 
         throw new Error(
-          `Error: No substitution given for "${placeholderName}". If this is not meant to be a 
+          `Error: No substitution given for "${placeholderName}". If this is not meant to be a
             placeholder you may want to consider passing one of the following options to babel-template:
             - { placeholderPattern: false, placeholderWhitelist: new Set(['${placeholderName}'])}
             - { placeholderPattern: /^${placeholderName}$/ }`,
