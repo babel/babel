a86d311986e7b08fd0109ae5a38763e880bdecb8
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-03-05 17:11:16 +0100

    Reuse the `assertThisInitialized` helper in `possibleConstructorReturn` (#7493)

--- packages/babel-plugin-transform-classes/test/fixtures/extend-builtins/spec/output.js
+++ packages/babel-plugin-transform-classes/test/fixtures/extend-builtins/spec/output.js
@@ -1,6 +1,8 @@
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (call && (typeof call === "object" || typeof call === "function")) { return call; } if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _possibleConstructorReturn(self, call) { if (call && (typeof call === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
