8e49bbbb091b53c31b82a72a8d68cc33cd92ac34
Brian Ng <bng412@gmail.com>
2018-03-30 09:28:48 -0500

    Add quotes rule to index

--- eslint/babel-eslint-plugin/index.js
+++ eslint/babel-eslint-plugin/index.js
@@ -2,16 +2,17 @@
 
 module.exports = {
   rules: {
-    'generator-star-spacing': require('./rules/generator-star-spacing'),
-    'new-cap': require('./rules/new-cap'),
-    'object-curly-spacing': require('./rules/object-curly-spacing'),
     'array-bracket-spacing': require('./rules/array-bracket-spacing'),
-    'object-shorthand': require('./rules/object-shorthand'),
     'arrow-parens': require('./rules/arrow-parens'),
-    'no-await-in-loop': require('./rules/no-await-in-loop'),
     'flow-object-type': require('./rules/flow-object-type'),
     'func-params-comma-dangle': require('./rules/func-params-comma-dangle'),
+    'generator-star-spacing': require('./rules/generator-star-spacing'),
+    'new-cap': require('./rules/new-cap'),
+    'no-await-in-loop': require('./rules/no-await-in-loop'),
     'no-invalid-this': require('./rules/no-invalid-this'),
+    'object-curly-spacing': require('./rules/object-curly-spacing'),
+    'object-shorthand': require('./rules/object-shorthand'),
+    'quotes': require('./rules/quotes'),
     'semi': require('./rules/semi'),
   },
   rulesConfig: {
@@ -25,6 +26,7 @@ module.exports = {
     'flow-object-type': 0,
     'func-params-comma-dangle': 0,
     'no-invalid-this': 0,
+    'quotes': 0,
     'semi': 0,
   }
 };
