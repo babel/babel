fa2c6c516403140675dcfb7faf8753dfa1a98c02
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-04-05 17:20:47 +0200

    Never extract class fields into an initializer function. (#7659)
    It was needed to avoid collision of identifiers used in fields
    initializers with variables declared in the constructor, but it
    broke `this` handling.
    This commit removes that behavior and instead renames the
    colliding variables.

--- packages/babel-plugin-proposal-class-properties/test/fixtures/loose/constructor-collision/output.js
+++ packages/babel-plugin-proposal-class-properties/test/fixtures/loose/constructor-collision/output.js
@@ -2,12 +2,6 @@ var foo = "bar";
 
 var Foo = function Foo() {
   babelHelpers.classCallCheck(this, Foo);
-
-  _initialiseProps.call(this);
-
-  var foo = "foo";
-};
-
-var _initialiseProps = function () {
   this.bar = foo;
+  var _foo = "foo";
 };
