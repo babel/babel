85951b1461a82ca3e04b3fdc9c09b42ffd479e64
Justin Ridgewell <jridgewell@google.com>
2018-04-05 17:06:37 +0100

    Fix options

--- packages/babel-plugin-proposal-class-properties/test/fixtures/public-loose/regression-T7364/output.mjs
+++ packages/babel-plugin-proposal-class-properties/test/fixtures/public-loose/regression-T7364/output.mjs
@@ -2,19 +2,10 @@ class MyClass {
   constructor() {
     var _this = this;
 
-    Object.defineProperty(this, "myAsyncMethod", {
-      configurable: true,
-      enumerable: true,
-      writable: true,
-      value: function () {
-        var _ref = babelHelpers.asyncToGenerator(function* () {
-          console.log(_this);
-        });
-
-        return function value() {
-          return _ref.apply(this, arguments);
-        };
-      }()
+    this.myAsyncMethod =
+    /*#__PURE__*/
+    babelHelpers.asyncToGenerator(function* () {
+      console.log(_this);
     });
   }
 
@@ -24,19 +15,10 @@ class MyClass {
   constructor() {
     var _this2 = this;
 
-    Object.defineProperty(this, "myAsyncMethod", {
-      configurable: true,
-      enumerable: true,
-      writable: true,
-      value: function () {
-        var _ref2 = babelHelpers.asyncToGenerator(function* () {
-          console.log(_this2);
-        });
-
-        return function value() {
-          return _ref2.apply(this, arguments);
-        };
-      }()
+    this.myAsyncMethod =
+    /*#__PURE__*/
+    babelHelpers.asyncToGenerator(function* () {
+      console.log(_this2);
     });
   }
 
@@ -46,19 +28,10 @@ export default class MyClass3 {
   constructor() {
     var _this3 = this;
 
-    Object.defineProperty(this, "myAsyncMethod", {
-      configurable: true,
-      enumerable: true,
-      writable: true,
-      value: function () {
-        var _ref3 = babelHelpers.asyncToGenerator(function* () {
-          console.log(_this3);
-        });
-
-        return function value() {
-          return _ref3.apply(this, arguments);
-        };
-      }()
+    this.myAsyncMethod =
+    /*#__PURE__*/
+    babelHelpers.asyncToGenerator(function* () {
+      console.log(_this3);
     });
   }
 
