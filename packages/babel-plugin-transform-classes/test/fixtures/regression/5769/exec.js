446211e98bb7bab3114365108162692cd24c5392
Justin Ridgewell <jridgewell@google.com>
2018-04-07 19:12:47 +0100

    Add issue case

---
+++ packages/babel-plugin-transform-classes/test/fixtures/regression/5769/exec.js
@@ -0,0 +1,22 @@
+class Point {
+  getX() {
+    assert.equal(this.x, 3); // C
+  }
+}
+
+class ColorPoint extends Point {
+  constructor() {
+    super();
+    this.x = 2;
+    super.x = 3;
+    assert.equal(this.x, 3)   // A
+    assert.equal(super.x, undefined)  // B
+  }
+
+  m() {
+    this.getX()
+  }
+}
+
+const cp = new ColorPoint();
+cp.m();
