a62cfe904586bea845549f8578872ef40e67183c
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-04-12 15:06:48 +0200

    Disallow arguments in class properties

--- packages/babel-core/test/fixtures/transformation/misc/class-property-initializer-blocks-shadow/output.js
+++ packages/babel-core/test/fixtures/transformation/misc/class-property-initializer-blocks-shadow/output.js
@@ -1,10 +1,6 @@
 class A {
   prop1 = () => this;
   static prop2 = () => this;
-  prop3 = () => arguments;
-  static prop4 = () => arguments;
   prop5 = this;
   static prop6 = this;
-  prop7 = arguments;
-  static prop8 = arguments;
 }
