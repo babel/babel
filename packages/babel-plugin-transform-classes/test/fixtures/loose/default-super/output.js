92e95826c9f97dda941d053e9b9b4444bb06f78f
Justin Ridgewell <justin@ridgewell.name>
2018-04-14 14:55:01 -0400

    Fix default class super inheritance
    Fixes #7683.

---
+++ packages/babel-plugin-transform-classes/test/fixtures/loose/default-super/output.js
@@ -0,0 +1,22 @@
+var Test =
+/*#__PURE__*/
+function () {
+  "use strict";
+
+  function Test() {
+    return Object.prototype.constructor;
+  }
+
+  Test.test = function test() {
+    return Function.prototype.constructor;
+  };
+
+  return Test;
+}(); // Instances
+
+
+expect(Object.getPrototypeOf(Test.prototype)).toBe(Object.prototype);
+expect(new Test()).toBe(Object); // Static
+
+expect(Object.getPrototypeOf(Test)).toBe(Function.prototype);
+expect(Test.test()).toBe(Function);
