dfd6ebebb7e261074965f7dccb95777b4c82335e
Jason Quense <monastic.panic@gmail.com>
2018-04-17 17:58:08 -0400

    Update helpers.js

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -76,11 +76,11 @@ helpers.asyncIterator = () => template.program.ast`
     if (typeof Symbol === "function") {
       if (Symbol.asyncIterator) {
         method = iterable[Symbol.asyncIterator]
-        if (method != null) return method();
+        if (method != null) return method.call(iterable);
       }
       if (Symbol.iterator) {
         method = iterable[Symbol.iterator]
-        if (method != null) return method();
+        if (method != null) return method.call(iterable);
       }
     }
     throw new TypeError("Object is not async iterable");
