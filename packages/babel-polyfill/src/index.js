f3831208dcda816cc4244b07e80f7526f40bc80a
Henry Zhu <hi@henryzoo.com>
2018-04-21 11:59:37 -0400

    update message [skip ci]

--- packages/babel-polyfill/src/index.js
+++ packages/babel-polyfill/src/index.js
@@ -5,8 +5,8 @@ if (global._babelPolyfill) {
   console.warn(
     "@babel/polyfill is loaded more than once on this page. This is probably not desirable/intended " +
       "and may have consequences if different versions of the polyfills are applied sequentially. " +
-      "If you do need to load the polyfill more than once, use @babel/polyfill/noConflict " +
-      "instead to bypass the error.",
+      "If you do need to load the polyfill more than once, use @babel/polyfill/lib/noConflict " +
+      "instead to bypass the warning.",
   );
 }
 
