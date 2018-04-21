5cb44c228e3f27dc6db72e6820ff4d8d88c70db7
Henry Zhu <hi@henryzoo.com>
2018-04-21 14:35:06 -0400

    add console check [skip ci]

--- packages/babel-polyfill/src/index.js
+++ packages/babel-polyfill/src/index.js
@@ -1,11 +1,11 @@
 import "core-js/shim";
 import "regenerator-runtime/runtime";
 
-if (global._babelPolyfill) {
+if (global._babelPolyfill && typeof console !== "undefined" && console.warn) {
   console.warn(
     "@babel/polyfill is loaded more than once on this page. This is probably not desirable/intended " +
       "and may have consequences if different versions of the polyfills are applied sequentially. " +
-      "If you do need to load the polyfill more than once, use @babel/polyfill/lib/noConflict " +
+      "If you do need to load the polyfill more than once, use @babel/polyfill/noConflict " +
       "instead to bypass the warning.",
   );
 }
