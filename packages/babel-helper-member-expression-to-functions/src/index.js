3616137864f4999031a548e6539bf9354039b093
Justin Ridgewell <justin@ridgewell.name>
2018-04-21 22:15:02 -0400

    Memoize computed super properties (#7776)
    Fixes #7775.

--- packages/babel-helper-member-expression-to-functions/src/index.js
+++ packages/babel-helper-member-expression-to-functions/src/index.js
@@ -9,6 +9,12 @@ const handle = {
     if (parentPath.isUpdateExpression({ argument: node })) {
       const { operator, prefix } = parent;
 
+      // Give the state handler a chance to memoize the member,
+      // since we'll reference it twice.
+      if (this.memoize) {
+        this.memoize(member);
+      }
+
       const value = t.binaryExpression(
         operator[0],
         t.unaryExpression("+", this.get(member)),
@@ -38,6 +44,12 @@ const handle = {
       let value = right;
 
       if (operator !== "=") {
+        // Give the state handler a chance to memoize the member,
+        // since we'll reference it twice.
+        if (this.memoize) {
+          this.memoize(member);
+        }
+
         value = t.binaryExpression(
           operator.slice(0, -1),
           this.get(member),
@@ -67,6 +79,8 @@ const handle = {
 // it wishes to be transformed.
 // Additionally, the caller must pass in a state object with at least
 // get, set, and call methods.
+// Optionally, a memoize method may be defined on the state, which will be
+// called when the member is a self-referential update.
 export default function memberExpressionToFunctions(path, visitor, state) {
   path.traverse(visitor, Object.assign({}, state, handle));
 }
