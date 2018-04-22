45e94f16963bd5cf3f901b8420829b2ae351d991
namiltd <namiltd@users.noreply.github.com>
2018-04-22 20:43:34 +0200

    code optimization

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -377,18 +377,16 @@ helpers.objectSpread = () => template.program.ast`
 
   export default function _objectSpread(target) {
     for (var i = 1; i < arguments.length; i++) {
-      var source = (arguments[i] != null) ? arguments[i] : {};
-      if (source !== false) {
-        var ownKeys = Object.keys(source);
-        if (typeof Object.getOwnPropertySymbols === 'function') {
-          ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
-            return Object.getOwnPropertyDescriptor(source, sym).enumerable;
-          }));
-        }
-        ownKeys.forEach(function(key) {
-          defineProperty(target, key, source[key]);
-        });
+      var source = ((arguments[i] != null) && (arguments[i] !== false)) ? arguments[i] : {};
+      var ownKeys = Object.keys(source);
+      if (typeof Object.getOwnPropertySymbols === 'function') {
+        ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
+          return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+        }));
       }
+      ownKeys.forEach(function(key) {
+        defineProperty(target, key, source[key]);
+      });
     }
     return target;
   }
