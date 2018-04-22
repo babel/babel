5a67dd30b270ac7f44ef0b69bf72ef208288d675
namiltd <namiltd@users.noreply.github.com>
2018-04-22 15:00:31 +0200

    source !== false

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -378,7 +378,7 @@ helpers.objectSpread = () => template.program.ast`
   export default function _objectSpread(target) {
     for (var i = 1; i < arguments.length; i++) {
       var source = (arguments[i] != null) ? arguments[i] : {};
-      if (typeof source === 'object') {
+      if (source !== false) {
         var ownKeys = Object.keys(source);
         if (typeof Object.getOwnPropertySymbols === 'function') {
           ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
