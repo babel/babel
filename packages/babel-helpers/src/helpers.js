0a01bd3906822e68701210effb06a609808dca27
namiltd <namiltd@users.noreply.github.com>
2018-04-23 08:59:11 +0200

    Object test

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -377,7 +377,8 @@ helpers.objectSpread = () => template.program.ast`
 
   export default function _objectSpread(target) {
     for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i] != null && arguments[i] !== false ? arguments[i] : {};
+      var source = arguments[i];
+      if (source !== Object(source)) continue;
       var ownKeys = Object.keys(source);
       if (typeof Object.getOwnPropertySymbols === 'function') {
         ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
--- packages/babel-preset-env/test/fixtures/preset-options/shippedProposals-use-builtins-usage/output.js
+++ packages/babel-preset-env/test/fixtures/preset-options/shippedProposals-use-builtins-usage/output.js
@@ -14,7 +14,7 @@ require("core-js/modules/es6.symbol");
 
 require("core-js/modules/es6.promise");
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null && arguments[i] !== false ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; if (source !== Object(source)) continue; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
