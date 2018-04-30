114c2d80e6846d618d993ca8994ae6b2283462d7
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2018-05-01 00:45:31 +0530

    added failing tests

--- packages/babel-plugin-transform-template-literals/test/fixtures/default/cache-revision/exec.js
+++ packages/babel-plugin-transform-template-literals/test/fixtures/default/cache-revision/exec.js
@@ -1,15 +1,32 @@
-var tag = v => v;
+// var tag = v => v;
+//
+// function foo() {
+//   return tag`some template`;
+// }
+// function bar() {
+//   return tag`some template`;
+// }
+// expect(foo()).toBe(foo());
+// expect(foo()).toEqual(["some template"]);
+//
+// expect(bar()).toBe(bar());
+// expect(bar()).toEqual(["some template"]);
+//
+// expect(bar()).not.toBe(foo());
 
-function foo() {
-  return tag`some template`;
+const tagger = (strings) => strings;
+
+function text(value) {
+  return tagger`some ${value} strings`;
 }
-function bar() {
-  return tag`some template`;
-}
-expect(foo()).toBe(foo());
-expect(foo()).toEqual(["some template"]);
 
-expect(bar()).toBe(bar());
-expect(bar()).toEqual(["some template"]);
+const foo = text(); // => ["some ", " strings"]
+const bar = text('hello'); // => ["some ", " strings"]
 
-expect(bar()).not.toBe(foo());
+expect(Array.isArray(foo)).toBe(true);
+expect(Array.isArray(bar)).toBe(true);
+
+// Yes, even though bar was passed "hello".
+// The expressions passed to the template literal (inside the `${}`)
+// are not part of the Strings array. Only the literal strings are in it.
+expect(foo).toBe(bar);
