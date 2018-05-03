da47f2fe63dd5c49241fafc32f4de3aff3503674
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-05-03 11:17:52 +0200

    Update snapshots to reflect optimization

--- packages/babel-plugin-transform-template-literals/test/fixtures/default/template-revision/output.js
+++ packages/babel-plugin-transform-template-literals/test/fixtures/default/template-revision/output.js
@@ -2,15 +2,15 @@ var _templateObject, _templateObject2, _templateObject3, _templateObject4, _temp
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-tag(_templateObject = _templateObject || /*#__PURE__*/ _taggedTemplateLiteral([void 0], ["\\unicode and \\u{55}"]));
-tag(_templateObject2 = _templateObject2 || /*#__PURE__*/ _taggedTemplateLiteral([void 0], ["\\01"]));
-tag(_templateObject3 = _templateObject3 || /*#__PURE__*/ _taggedTemplateLiteral([void 0, "right"], ["\\xg", "right"]), 0);
-tag(_templateObject4 = _templateObject4 || /*#__PURE__*/ _taggedTemplateLiteral(["left", void 0], ["left", "\\xg"]), 0);
-tag(_templateObject5 = _templateObject5 || /*#__PURE__*/ _taggedTemplateLiteral(["left", void 0, "right"], ["left", "\\xg", "right"]), 0, 1);
-tag(_templateObject6 = _templateObject6 || /*#__PURE__*/ _taggedTemplateLiteral(["left", void 0, "right"], ["left", "\\u000g", "right"]), 0, 1);
-tag(_templateObject7 = _templateObject7 || /*#__PURE__*/ _taggedTemplateLiteral(["left", void 0, "right"], ["left", "\\u{-0}", "right"]), 0, 1);
+tag(_templateObject || (_templateObject = /*#__PURE__*/ _taggedTemplateLiteral([void 0], ["\\unicode and \\u{55}"])));
+tag(_templateObject2 || (_templateObject2 = /*#__PURE__*/ _taggedTemplateLiteral([void 0], ["\\01"])));
+tag(_templateObject3 || (_templateObject3 = /*#__PURE__*/ _taggedTemplateLiteral([void 0, "right"], ["\\xg", "right"])), 0);
+tag(_templateObject4 || (_templateObject4 = /*#__PURE__*/ _taggedTemplateLiteral(["left", void 0], ["left", "\\xg"])), 0);
+tag(_templateObject5 || (_templateObject5 = /*#__PURE__*/ _taggedTemplateLiteral(["left", void 0, "right"], ["left", "\\xg", "right"])), 0, 1);
+tag(_templateObject6 || (_templateObject6 = /*#__PURE__*/ _taggedTemplateLiteral(["left", void 0, "right"], ["left", "\\u000g", "right"])), 0, 1);
+tag(_templateObject7 || (_templateObject7 = /*#__PURE__*/ _taggedTemplateLiteral(["left", void 0, "right"], ["left", "\\u{-0}", "right"])), 0, 1);
 
 function a() {
   var undefined = 4;
-  tag(_templateObject8 = _templateObject8 || /*#__PURE__*/ _taggedTemplateLiteral([void 0], ["\\01"]));
+  tag(_templateObject8 || (_templateObject8 = /*#__PURE__*/ _taggedTemplateLiteral([void 0], ["\\01"])));
 }
