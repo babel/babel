0214f2057392e6d917adcc96f40296732a794a86
Logan Smyth <loganfsmyth@gmail.com>
2018-05-06 22:36:20 -0700

    Bug fixing for Node 10

--- packages/babel-helper-transform-fixture-test-runner/test/index.js
+++ packages/babel-helper-transform-fixture-test-runner/test/index.js
@@ -4,20 +4,35 @@ describe("helper-transform-fixture-test-runner", function() {
   it("should not execute code in Node's global context", function() {
     try {
       global.foo = "outer";
-      runCodeInTestContext(`
-        expect(global.foo).toBeUndefined();
-        global.foo = "inner";
-      `);
+      runCodeInTestContext(
+        `
+          expect(global.foo).toBeUndefined();
+          global.foo = "inner";
+        `,
+        {
+          filename: `${__filename}.fake1`,
+        },
+      );
 
       expect(global.foo).toBe("outer");
-      runCodeInTestContext(`
-        expect(global.foo).toBe("inner");
-      `);
+      runCodeInTestContext(
+        `
+          expect(global.foo).toBe("inner");
+        `,
+        {
+          filename: `${__filename}.fake2`,
+        },
+      );
     } finally {
       delete global.foo;
-      runCodeInTestContext(`
-        delete global.foo;
-      `);
+      runCodeInTestContext(
+        `
+          delete global.foo;
+        `,
+        {
+          filename: `${__filename}.fake3`,
+        },
+      );
     }
   });
 });
