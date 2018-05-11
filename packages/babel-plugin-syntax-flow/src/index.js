1c359ad9fcf84f7db98232fe0f0f2a3b31db0947
Sam Goldman <samwgoldman@gmail.com>
2018-05-10 17:09:38 -0700

    Add all option to babel-plugin-syntax-flow
    The Flow parser has some conditional logic based on whether types should
    be parsed or not, which is based on either (a) the @flow pragma in the
    docblock of the file of (b) the `all` configuration, provided via
    command line or .flowconfig.
    
    This commit adds the ability to provide the `all` configuration to
    Babel as well, via the syntax-flow plugin. This should be set to `true`
    if the project uses all=true.

--- packages/babel-plugin-syntax-flow/src/index.js
+++ packages/babel-plugin-syntax-flow/src/index.js
@@ -1,11 +1,14 @@
 import { declare } from "@babel/helper-plugin-utils";
 
-export default declare(api => {
+export default declare((api, options) => {
   api.assertVersion(7);
+
+  const { all } = options;
 
   return {
     manipulateOptions(opts, parserOpts) {
       parserOpts.plugins.push("flow");
+      parserOpts.flowAll = all;
     },
   };
 });
--- packages/babylon/src/options.js
+++ packages/babylon/src/options.js
@@ -15,6 +15,7 @@ export type Options = {
   strictMode: ?boolean,
   ranges: boolean,
   tokens: boolean,
+  flowAll: boolean,
 };
 
 export const defaultOptions: Options = {
@@ -51,6 +52,9 @@ export const defaultOptions: Options = {
   ranges: false,
   // Adds all parsed tokens to a `tokens` property on the `File` node
   tokens: false,
+  // When enabled and plugins includes flow, all files should be parsed as if
+  // the @flow pragma was provided.
+  flowAll: false,
 };
 
 // Interpret and default an options object
