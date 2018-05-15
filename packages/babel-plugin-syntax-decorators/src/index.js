bd8af2cbee11e48334b8e292bdb60b6e0b76b78c
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-05-15 21:34:10 +0200

    Update syntax-decorators options
    * Add decoratorsBeforeExport to the syntax plugin
    * Require legacy: true, like in the transform plugin

--- packages/babel-plugin-syntax-decorators/src/index.js
+++ packages/babel-plugin-syntax-decorators/src/index.js
@@ -3,14 +3,37 @@ import { declare } from "@babel/helper-plugin-utils";
 export default declare((api, options) => {
   api.assertVersion(7);
 
-  const { legacy = false } = options;
+  const { legacy = false, decoratorsBeforeExport } = options;
   if (typeof legacy !== "boolean") {
     throw new Error("'legacy' must be a boolean.");
   }
 
+  if (legacy !== true) {
+    throw new Error(
+      "The new decorators proposal is not supported yet." +
+        ' You must pass the `"legacy": true` option to' +
+        " @babel/plugin-syntax-decorators",
+    );
+  }
+
+  if (decoratorsBeforeExport !== undefined) {
+    if (legacy) {
+      throw new Error(
+        "'decoratorsBeforeExport' can't be used with legacy decorators.",
+      );
+    }
+    if (typeof decoratorsBeforeExport !== "boolean") {
+      throw new Error("'decoratorsBeforeExport' must be a boolean.");
+    }
+  }
+
   return {
     manipulateOptions(opts, parserOpts) {
-      parserOpts.plugins.push(legacy ? "decorators-legacy" : "decorators");
+      parserOpts.plugins.push(
+        legacy
+          ? "decorators-legacy"
+          : ["decorators", { decoratorsBeforeExport }],
+      );
     },
   };
 });
