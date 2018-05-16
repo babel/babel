925ea38ecb4787d779bfb120ed663e07a8249397
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-05-16 15:52:53 +0200

    Add tests

---
+++ packages/babel-plugin-syntax-decorators/test/index.js
@@ -0,0 +1,66 @@
+import { parse } from "@babel/core";
+import syntaxDecorators from "../lib";
+
+function makeParser(code, options) {
+  return () =>
+    parse(code, {
+      babelrc: false,
+      configFile: false,
+      plugins: [[syntaxDecorators, options]],
+    });
+}
+
+describe("'legacy' option", function() {
+  test("must be boolean", function() {
+    expect(makeParser("", { legacy: "legacy" })).toThrow();
+  });
+
+  test("'legacy': false", function() {
+    expect(makeParser("({ @dec fn() {} })", { legacy: false })).toThrow();
+  });
+
+  test("'legacy': true", function() {
+    expect(makeParser("({ @dec fn() {} })", { legacy: true })).notToThrow();
+  });
+
+  test("defaults to 'false'", function() {
+    expect(makeParser("({ @dec fn() {} })", {})).toThrow();
+  });
+});
+
+describe("'decoratorsBeforeExport' option", function() {
+  test("must be boolean", function() {
+    expect(makeParser("", { decoratorsBeforeExport: "before" })).toThrow();
+  });
+
+  test("is incompatible with legacy", function() {
+    expect(
+      makeParser("", { decoratorsBeforeExport: false, legacy: true }),
+    ).toThrow();
+  });
+
+  const BEFORE = "@dec export class Foo {}";
+  const AFTER = "export @dec class Foo {}";
+
+  run(BEFORE, undefined, false);
+  run(AFTER, undefined, true);
+  run(BEFORE, true, false);
+  run(AFTER, true, true);
+  run(BEFORE, false, true);
+  run(AFTER, false, false);
+
+  function run(code, before, throws) {
+    const name =
+      (before === undefined ? "default" : before) +
+      " - decorators " +
+      (code === BEFORE ? "before" : "after") +
+      "export";
+
+    test(name, function() {
+      const expectTheParser = expect(
+        makeParser(code, { decoratorsBeforeExport: before }),
+      );
+      throws ? expectTheParser.toThrow() : expectTheParser.not.toThrow();
+    });
+  }
+});
