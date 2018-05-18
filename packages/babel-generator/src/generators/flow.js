6baa36cdc50b677af9927a9e2e5004e6944c9ba6
Sam Goldman <samwgoldman@gmail.com>
2018-05-18 14:11:27 +0100

    Support Flow's inline interface syntax (#7973)
    * Support Flow's inline interface syntax
    
    * Fix babel-generator

--- packages/babel-generator/src/generators/flow.js
+++ packages/babel-generator/src/generators/flow.js
@@ -262,6 +262,18 @@ function andSeparator() {
   this.space();
 }
 
+export function InterfaceTypeAnnotation(node: Object) {
+  this.word("interface");
+  if (node.extends && node.extends.length) {
+    this.space();
+    this.word("extends");
+    this.space();
+    this.printList(node.extends, node);
+  }
+  this.space();
+  this.print(node.body, node);
+}
+
 export function IntersectionTypeAnnotation(node: Object) {
   this.printJoin(node.types, node, { separator: andSeparator });
 }
