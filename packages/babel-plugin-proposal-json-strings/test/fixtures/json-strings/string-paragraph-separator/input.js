61638c96b4a283f0b51e0559b3bbc7a9eb8a8ed1
Justin Ridgewell <justin@ridgewell.name>
2018-05-19 01:29:18 -0400

    Handle multi-escape sequences

--- packages/babel-plugin-proposal-json-strings/test/fixtures/json-strings/string-paragraph-separator/input.js
+++ packages/babel-plugin-proposal-json-strings/test/fixtures/json-strings/string-paragraph-separator/input.js
@@ -3,22 +3,25 @@ expect("\u2029").toBe("\u2029");
 
 
 expect("before after".length).toBe(12);
-//            ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char (between 'before' and 'after')
-
-expect(" ".length).toBe(1);
-//      ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
-
-expect("\ ".length).toBe(0);
-//       ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
-// Weirdly, that'll just become an empty string
-
 expect("before after").toBe("before\u2029after");
 //            ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char (between 'before' and 'after')
 
+expect(" ".length).toBe(1);
 expect(" ").toBe("\u2029");
 //      ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
 
+expect("\ ".length).toBe(0);
 expect("\ ").toBe("");
 //       ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
-// Weirdly, that'll just become an empty string
 
+expect("\\ ".length).toBe(2);
+expect("\\ ").toBe("\\\u2029");
+//        ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
+
+expect("\\\ ".length).toBe(1);
+expect("\\\ ").toBe("\\");
+//         ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
+
+expect("\\\\ ".length).toBe(3);
+expect("\\\\ ").toBe("\\\\\u2029");
+//          ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
--- packages/babel-plugin-proposal-json-strings/test/fixtures/json-strings/string-paragraph-separator/output.js
+++ packages/babel-plugin-proposal-json-strings/test/fixtures/json-strings/string-paragraph-separator/output.js
@@ -1,15 +1,19 @@
 expect("\u2029".length).toBe(1);
 expect("\u2029").toBe("\u2029");
-expect("before\u2029after".length).toBe(12); //            ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char (between 'before' and 'after')
-
-expect("\u2029".length).toBe(1); //      ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
-
-expect("\ ".length).toBe(0); //       ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
-// Weirdly, that'll just become an empty string
-
+expect("before\u2029after".length).toBe(12);
 expect("before\u2029after").toBe("before\u2029after"); //            ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char (between 'before' and 'after')
 
+expect("\u2029".length).toBe(1);
 expect("\u2029").toBe("\u2029"); //      ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
 
+expect("\ ".length).toBe(0);
 expect("\ ").toBe(""); //       ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
-// Weirdly, that'll just become an empty string
+
+expect("\\\u2029".length).toBe(2);
+expect("\\\u2029").toBe("\\\u2029"); //        ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
+
+expect("\\\ ".length).toBe(1);
+expect("\\\ ").toBe("\\"); //         ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
+
+expect("\\\\\u2029".length).toBe(3);
+expect("\\\\\u2029").toBe("\\\\\u2029"); //          ^ That's a U+2029 PARAGRAPH SEPARATOR UTF-16 char
