758521e43a82258ab1f7aeaff6530cb2cc477915
malbernaz <albernazmiguel@gmail.com>
2018-05-20 21:07:48 -0300

    passes the jsxPragma options from preset-typescript to plugin-transform-typescript

--- packages/babel-preset-typescript/src/index.js
+++ packages/babel-preset-typescript/src/index.js
@@ -1,10 +1,10 @@
 import { declare } from "@babel/helper-plugin-utils";
 import transformTypeScript from "@babel/plugin-transform-typescript";
 
-export default declare(api => {
+export default declare((api, { jsxPragma = "React" }) => {
   api.assertVersion(7);
 
   return {
-    plugins: [transformTypeScript],
+    plugins: [[transformTypeScript, { jsxPragma }]],
   };
 });
