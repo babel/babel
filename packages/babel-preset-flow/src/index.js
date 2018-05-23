5cd1276a27277c495cf4ea81bc429f1746febaad
Logan Smyth <loganfsmyth@gmail.com>
2018-05-23 13:40:22 -0700

    Expose the Flow 'all' option on the preset too. (#8016)
    | Q                        | A <!--(Can use an emoji 👍) -->
    | ------------------------ | ---
    | Fixed Issues?            | 
    | Patch: Bug Fix?          | 
    | Major: Breaking Change?  |
    | Minor: New Feature?      |
    | Tests Added + Pass?      | Yes
    | Documentation PR         | <!-- If so, add `[skip ci]` to your commit message to skip CI -->
    | Any Dependency Changes?  |
    | License                  | MIT
    
    The `all` option landed in https://github.com/babel/babel/pull/7934/files#diff-3a8233bcd2766d2c7d87f23f944f7726R3 but it is only exposed from the plugin, not the preset, so this exposes it there too since the flow preset is what we want people to use.

--- packages/babel-preset-flow/src/index.js
+++ packages/babel-preset-flow/src/index.js
@@ -1,10 +1,10 @@
 import { declare } from "@babel/helper-plugin-utils";
 import transformFlowStripTypes from "@babel/plugin-transform-flow-strip-types";
 
-export default declare(api => {
+export default declare((api, { all }) => {
   api.assertVersion(7);
 
   return {
-    plugins: [transformFlowStripTypes],
+    plugins: [[transformFlowStripTypes, { all }]],
   };
 });
