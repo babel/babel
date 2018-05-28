38c5b66f4a759f1d9cfac15f04fa16997d2a33c3
Globegitter <markus.padourek@gmail.com>
2018-05-28 11:47:39 +0200

    Import relative modules based on moduleId if specified.

---
+++ packages/babel-plugin-transform-modules-amd/test/fixtures/amd/module-name-import/output.js
@@ -0,0 +1,6 @@
+define("amd/module-name-import/input", ["amd/module-name-import/module"], function (_module) {
+  "use strict";
+
+  _module = babelHelpers.interopRequireDefault(_module);
+  foobar();
+});
