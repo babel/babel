7da7e760a6ffa6566a0e949ad5344a68f4a428c4
Guy Bedford <guybedford@gmail.com>
2018-06-01 23:07:29 +0200

    support import.meta in system transform

---
+++ packages/babel-plugin-transform-modules-systemjs/test/fixtures/dynamic-import/import-meta/output.mjs
@@ -0,0 +1,10 @@
+System.register([], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [],
+    execute: function () {
+      console.log(_context.meta.url);
+    }
+  };
+});
