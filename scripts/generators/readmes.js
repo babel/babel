f6148090022abf978dea4dd690377b4cacd0568f
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-06-04 17:32:01 +0200

    feat: [skip ci] add link to issues in READMEs

--- scripts/generators/readmes.js
+++ scripts/generators/readmes.js
@@ -15,13 +15,35 @@ const packageDir = join(cwd, "packages");
 const packages = readdirSync(packageDir);
 const getWebsiteLink = n => `https://new.babeljs.io/docs/en/next/${n}.html`;
 const getPackageJson = pkg => require(join(packageDir, pkg, "package.json"));
+const getIssueLabelLink = l =>
+  `https://github.com/babel/babel/issues?utf8=%E2%9C%93&q=is%3Aissue+label%3A%22${encodeURIComponent(
+    l
+  )}%22`;
 
-const generateReadme = ({ link, name, description }) =>
+const labels = {
+  "babel-preset-typescript": getIssueLabelLink("area: typescript"),
+  "babel-preset-flow": getIssueLabelLink("area: flow"),
+  "babel-preset-react": getIssueLabelLink("area: react"),
+  "babel-parser": getIssueLabelLink("pkg: babylon"),
+  "babel-cli": getIssueLabelLink("pkg: cli"),
+  "babel-core": getIssueLabelLink("pkg: core"),
+  "babel-polyfill": getIssueLabelLink("pkg: polyfill"),
+  "babel-preset-env": getIssueLabelLink("pkg: preset-env"),
+  "babel-regiser": getIssueLabelLink("pkg: regiser"),
+  "babel-traverse": getIssueLabelLink("pkg: traverse"),
+  "babel-types": getIssueLabelLink("pkg: types"),
+};
+
+const generateReadme = ({ websiteLink, issuesLink, name, description }) =>
   `# ${name}
 
 > ${description}
 
-See our website [${name}](${link}) for more information.
+See our website [${name}](${websiteLink}) for more information${
+    issuesLink
+      ? ` or the [issues](${issuesLink}) associated with this package`
+      : ""
+  }.
 
 ## Install
 
@@ -45,8 +67,15 @@ packages
     const readmePath = join(packageDir, id, "README.md");
 
     // generate
-    const link = getWebsiteLink(id);
-    const readme = generateReadme({ link, name, description });
+    const websiteLink = getWebsiteLink(id);
+    const issuesLink = labels[id];
+
+    const readme = generateReadme({
+      websiteLink,
+      issuesLink,
+      name,
+      description,
+    });
 
     // write
     writeFileSync(readmePath, readme);
