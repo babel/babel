ede5e79914a229d4c72ee6289d3aa2d095548520
Ben Newman <ben@benjamn.com>
2018-06-12 18:37:30 -0400

    Update regenerator-preset to use Babel 7 plugins.

--- regenerator/packages/regenerator-preset/index.js
+++ regenerator/packages/regenerator-preset/index.js
@@ -7,12 +7,11 @@
 
 module.exports = {
   plugins: [
-    require("babel-plugin-syntax-async-functions"),
-    require("babel-plugin-syntax-async-generators"),
-    require("babel-plugin-transform-es2015-classes"),
-    require("babel-plugin-transform-es2015-arrow-functions"),
-    require("babel-plugin-transform-es2015-block-scoping"),
-    require("babel-plugin-transform-es2015-for-of"),
+    require("@babel/plugin-syntax-async-generators"),
+    require("@babel/plugin-transform-classes"),
+    require("@babel/plugin-transform-arrow-functions"),
+    require("@babel/plugin-transform-block-scoping"),
+    require("@babel/plugin-transform-for-of"),
     require("regenerator-transform").default
   ]
 };
