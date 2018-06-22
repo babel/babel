e72d3eb5d03437f49338eef4a0b39ecb10969ea3
Logan Smyth <loganfsmyth@gmail.com>
2018-06-21 21:34:52 -0700

    Ensure that we don't get unexpected output files for tests that throw.

--- packages/babel-plugin-proposal-class-properties/test/fixtures/private-loose/static-infer-name/output.js
+++
@@ -1,8 +0,0 @@
-var _num, _class, _temp;
-
-var Foo = (_temp = _class = function Foo() {
-  babelHelpers.classCallCheck(this, Foo);
-}, _num = babelHelpers.classPrivateFieldKey("num"), Object.defineProperty(_class, _num, {
-  writable: true,
-  value: 0
-}), _temp);
--- packages/babel-plugin-transform-classes/test/fixtures/spec/super-call-only-allowed-in-derived-constructor/output.js
+++
@@ -1,9 +0,0 @@
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-let Foo = function Foo() {
-  "use strict";
-
-  _classCallCheck(this, Foo);
-
-  super();
-};
