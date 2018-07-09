31b1b97968f77cbee765c2e34d5b07f85755fc65
Henry Zhu <hi@henryzoo.com>
2018-07-09 16:44:23 -0400

    Breaking: throw error with using Stage presets

--- packages/babel-preset-stage-0/src/index.js
+++ packages/babel-preset-stage-0/src/index.js
@@ -1,39 +1,31 @@
-import { declare } from "@babel/helper-plugin-utils";
-import presetStage1 from "@babel/preset-stage-1";
+export default function() {
+  throw new Error(`
+As of v7.0.0-beta.52, we've decided to remove
+the official Babel Stage presets. You can find more information
+at issue #7770: https://github.com/babel/babel/issues/7770, but
+the TL;DR is that it's causing more harm than convenience in that
+the preset is always out of date, each change is usually going to
+require a major version bump and thus people will be behind,
+and it encouraged too many people to opt-in to too many proposals
+that they didn't intend to.
 
-import transformFunctionBind from "@babel/plugin-proposal-function-bind";
+If you want the same configuration as before, you can use this configuration,
+although keep in mind that Stage 0 contains Stage 1 which is also deprecated.
 
-export default declare((api, opts = {}) => {
-  api.assertVersion(7);
-
-  const {
-    loose = false,
-    useBuiltIns = false,
-    decoratorsLegacy = true,
-    pipelineProposal = "minimal",
-  } = opts;
-
-  if (typeof loose !== "boolean") {
-    throw new Error("@babel/preset-stage-0 'loose' option must be a boolean.");
-  }
-  if (typeof useBuiltIns !== "boolean") {
-    throw new Error(
-      "@babel/preset-stage-0 'useBuiltIns' option must be a boolean.",
-    );
-  }
-  if (typeof decoratorsLegacy !== "boolean") {
-    throw new Error(
-      "@babel/preset-stage-0 'decoratorsLegacy' option must be a boolean.",
-    );
-  }
-
-  return {
-    presets: [
-      [
-        presetStage1,
-        { loose, useBuiltIns, decoratorsLegacy, pipelineProposal },
-      ],
+{
+  presets: [
+    [
+      "@babel/preset-stage-1",
+      { loose, useBuiltIns, decoratorsLegacy, pipelineProposal },
     ],
-    plugins: [transformFunctionBind],
-  };
-});
+  ],
+  plugins: ["@babel/plugin-proposal-function-bind"],
+}
+
+This will be the last publish of "@babel/preset-stage-0", and it won't be
+in the final release.
+
+If it's a hassle to maintain, you can certainly make your own preset to use
+across projects, or there might be one in the community to use.
+  `);
+}
