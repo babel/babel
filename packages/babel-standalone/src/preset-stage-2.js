1ce68670f7e51b6bb4ebbe323b916d887c2c9c93
Henry Zhu <hi@henryzoo.com>
2018-07-09 16:29:44 -0400

    inline stage presets into standalone, remove yearly preset folder entirely, fix fixtures

---
+++ packages/babel-standalone/src/preset-stage-2.js
@@ -0,0 +1,22 @@
+import presetStage3 from "./preset-stage-3";
+
+import transformDecorators from "@babel/plugin-proposal-decorators";
+import transformFunctionSent from "@babel/plugin-proposal-function-sent";
+import transformExportNamespaceFrom from "@babel/plugin-proposal-export-namespace-from";
+import transformNumericSeparator from "@babel/plugin-proposal-numeric-separator";
+import transformThrowExpressions from "@babel/plugin-proposal-throw-expressions";
+
+export default (_, opts = {}) => {
+  const { loose = false, useBuiltIns = false, decoratorsLegacy = false } = opts;
+
+  return {
+    presets: [[presetStage3, { loose, useBuiltIns }]],
+    plugins: [
+      [transformDecorators, { legacy: decoratorsLegacy }],
+      transformFunctionSent,
+      transformExportNamespaceFrom,
+      transformNumericSeparator,
+      transformThrowExpressions,
+    ],
+  };
+};
