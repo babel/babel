0e473e9e580ba093ba662562bca9ff940fda98ea
Steven Hargrove <steven.hargrove@weather.com>
2018-07-22 18:36:01 -0400

    update test expectation for regenerator-runtime

--- packages/babel-plugin-transform-regenerator/test/fixtures/variable-renaming/retain-lines/expected.js
+++ packages/babel-plugin-transform-regenerator/test/fixtures/variable-renaming/retain-lines/expected.js
@@ -1,6 +1,6 @@
-var func = /*#__PURE__*/regeneratorRuntime.mark(function _callee() {var actual;return regeneratorRuntime.wrap(function _callee$(_context) {while (1) switch (_context.prev = _context.next) {case 0:_context.next = 2;return (
-                    obj.
-                    method().
-                    method2());case 2:
+var func = /*#__PURE__*/regeneratorRuntime.mark(function _callee() {var actual;return regeneratorRuntime.wrap(function _callee$(_context) {while (1) switch (_context.prev = _context.next) {case 0:_context.next = 2;
+                return obj.
+                method().
+                method2();case 2:
 
                 actual = true;case 3:case "end":return _context.stop();}}, _callee, this);});
