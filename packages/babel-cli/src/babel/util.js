cb517047452b583af59b0d34385940819a7facb4
Jan Schmidle <jan@jschmidle.org>
2018-08-03 18:06:27 +0200

    Babel cli fix recursion in file mode (#8418)
    * added test case
    
    * remove unnessecary parameter
    
    * check for type directory as well in readRecursive filter

--- packages/babel-cli/src/babel/util.js
+++ packages/babel-cli/src/babel/util.js
@@ -15,11 +15,15 @@ export function readdir(
   includeDotfiles: boolean,
   filter: ReaddirFilter,
 ) {
-  return readdirRecursive(
-    dirname,
-    filename =>
-      (includeDotfiles || filename[0] !== ".") && (!filter || filter(filename)),
-  );
+  return readdirRecursive(dirname, (filename, _index, currentDirectory) => {
+    const stat = fs.statSync(path.join(currentDirectory, filename));
+
+    if (stat.isDirectory()) return true;
+
+    return (
+      (includeDotfiles || filename[0] !== ".") && (!filter || filter(filename))
+    );
+  });
 }
 
 export function readdirForCompilable(
