72fa863fb0f0babef24d9696ae6a203c07622307
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-08-13 07:33:58 +0200

    Fix _isNaiveReflectConstruct helper
    Date#toString is generic in ES2015 [1] and it doesn't throw, but it returns "Invalid Date"
    
    [1]: https://github.com/tc39/ecma262/issues/1268#issuecomment-410104832

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -506,10 +506,12 @@ helpers.setPrototypeOf = helper("7.0.0-beta.0")`
 
 helpers.isNativeReflectConstruct = helper("7.9.0")`
   export default function _isNativeReflectConstruct() {
-    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
-
-    // core-js@3
-    if (Reflect.construct.sham) return false;
+    if (
+      typeof Reflect === "undefined" ||
+      !Reflect.construct ||
+      // core-js@3
+      Reflect.construct.sham
+    ) return false;
 
     // Proxy can't be polyfilled. Every browser implemented
     // proxies before or at the same time as Reflect.construct,
@@ -523,8 +525,12 @@ helpers.isNativeReflectConstruct = helper("7.9.0")`
     try {
       // If the internal slots aren't set, this throws an error similar to
       //   TypeError: this is not a Date object.
-      Date.prototype.toString.call(Reflect.construct(Date, [], function() {}));
-      return true;
+      // Date#toString is generic in ES2015 [1] and it doesn't throw, but it
+      // returns "Invalid Date" if the receiver is not a properly initialized
+      // Date object.
+      // [1]: https://github.com/tc39/ecma262/issues/1268#issuecomment-410104832
+
+      return Date.prototype.toString.call(Reflect.construct(Date, [], function() {})) !== "Invalid Date";
     } catch (e) {
       return false;
     }
