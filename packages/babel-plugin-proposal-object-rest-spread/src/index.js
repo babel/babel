cec89c594581ef56cbc90696ac0f83ccf763e1e9
Jason Quense <monastic.panic@gmail.com>
2018-08-22 11:23:41 -0400

    fix: object rest with default values bug
    fixes #8323

--- packages/babel-plugin-proposal-object-rest-spread/src/index.js
+++ packages/babel-plugin-proposal-object-rest-spread/src/index.js
@@ -96,10 +96,14 @@ export default declare((api, opts) => {
     const bindings = path.getOuterBindingIdentifierPaths();
 
     Object.keys(bindings).forEach(bindingName => {
-      if (path.scope.getBinding(bindingName).references > 1) {
+      const binding = bindings[bindingName].parentPath;
+      if (
+        path.scope.getBinding(bindingName).references > 1 ||
+        !binding.isObjectProperty()
+      ) {
         return;
       }
-      bindings[bindingName].parentPath.remove();
+      binding.remove();
     });
   }
 
