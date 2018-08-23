a3bb55934b1eccd4145e38ca7dc154f6fe9063a1
Henry Zhu <smiley.we@gmail.com>
2018-08-23 19:35:54 -0400

    rm [skip ci]

--- scripts/clone-license.sh
+++ scripts/clone-license.sh
@@ -1,3 +1,3 @@
 echo "Cloning LICENSE to babel packages"
 cat LICENSE
-ls -db ./packages/*/ | egrep -v '.*packages\/(babel-parser|babel-plugin-transform-object-assign|babel-plugin-transform-object-set-prototype-of-to-assign)\/?$' | xargs -n 1 cp LICENSE
+ls -db ./packages/*/ | egrep -v '.*packages\/(babel-parser|babel-plugin-transform-object-assign)\/?$' | xargs -n 1 cp LICENSE
