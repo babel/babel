2a1c76e070ca9366ae75dc88b46af409927ee88b
Alexandre Borela <alexandre.borela@gmail.com>
2018-08-23 02:21:45 -0300

    Add logical assignment plugin (babel/babel-eslint#674)
    Fix babel/babel-eslint#672

--- eslint/babel-eslint-parser/lib/parse.js
+++ eslint/babel-eslint-parser/lib/parse.js
@@ -47,6 +47,7 @@ module.exports = function(code, options) {
       "throwExpressions",
       ["pipelineOperator", { proposal: "minimal" }],
       "nullishCoalescingOperator",
+      "logicalAssignment",
     ],
   };
 
