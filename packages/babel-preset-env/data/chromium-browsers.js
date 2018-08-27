8dcf8880cc17d43a1ebc1ff521beaa65a530a7c4
ylemkimon <mail@ylem.kim>
2018-08-27 22:05:24 +0900

    Add version data for Chromium-based browsers

---
+++ packages/babel-preset-env/data/chromium-browsers.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Samsung Internet release notes: https://developer.samsung.com/internet/android/releases
+  // Chrome version page (chrome://version) is accessible
+  // Indicated in user agent string as Chrome/XX
+  "samsung 4": 44,
+  "samsung 5": 51,
+  "samsung 6.2": 56,
+  "samsung 7.2": 59,
+  
+  // UC Browser developer edition: https://plus.ucweb.com/download/
+  // Indicated in user agent string as Chrome/XX
+  "and_uc 11.4": 40,
+  "and_uc 11.8": 57,
+};
