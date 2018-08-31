ebee0855f580930dd5e8f9081eacc8482d856007
Balamurugan B <balamuruganb2020@gmail.com>
2018-08-31 10:33:21 +0530

    Polyfill multi load warning
    log warning instead of throwing error message if polyfill loaded already

--- packages/babel-polyfill/src/index.js
+++ packages/babel-polyfill/src/index.js
@@ -1,5 +1,10 @@
-if (global._babelPolyfill) {
-  throw new Error("only one instance of babel-polyfill is allowed");
+if (global._babelPolyfill && typeof console !== "undefined" && console.warn) {
+  console.warn(
+    "@babel/polyfill is loaded more than once on this page. This is probably not desirable/intended " +
+      "and may have consequences if different versions of the polyfills are applied sequentially. " +
+      "If you do need to load the polyfill more than once, use @babel/polyfill/noConflict " +
+      "instead to bypass the warning.",
+  );
 }
 global._babelPolyfill = true;
 
