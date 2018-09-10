61ec9577ad1b26adb1abe03057a268d9426adbe8
Logan Smyth <loganfsmyth@gmail.com>
2018-09-09 17:59:59 -0700

    Fix prerelease checks in .availableHelper and transform-runtime definitions.

--- packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs2/math-undefined-version/output.js
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs2/math-undefined-version/output.js
@@ -1,51 +1,17 @@
-var _Math$trunc = require("@babel/runtime-corejs2/core-js/math/trunc");
-
-var _Math$tanh = require("@babel/runtime-corejs2/core-js/math/tanh");
-
-var _Math$sinh = require("@babel/runtime-corejs2/core-js/math/sinh");
-
-var _Math$sign = require("@babel/runtime-corejs2/core-js/math/sign");
-
-var _Math$log2 = require("@babel/runtime-corejs2/core-js/math/log2");
-
-var _Math$log1p = require("@babel/runtime-corejs2/core-js/math/log1p");
-
-var _Math$log = require("@babel/runtime-corejs2/core-js/math/log10");
-
-var _Math$imul = require("@babel/runtime-corejs2/core-js/math/imul");
-
-var _Math$hypot = require("@babel/runtime-corejs2/core-js/math/hypot");
-
-var _Math$fround = require("@babel/runtime-corejs2/core-js/math/fround");
-
-var _Math$expm = require("@babel/runtime-corejs2/core-js/math/expm1");
-
-var _Math$cosh = require("@babel/runtime-corejs2/core-js/math/cosh");
-
-var _Math$clz = require("@babel/runtime-corejs2/core-js/math/clz32");
-
-var _Math$cbrt = require("@babel/runtime-corejs2/core-js/math/cbrt");
-
-var _Math$atanh = require("@babel/runtime-corejs2/core-js/math/atanh");
-
-var _Math$asinh = require("@babel/runtime-corejs2/core-js/math/asinh");
-
-var _Math$acosh = require("@babel/runtime-corejs2/core-js/math/acosh");
-
-_Math$acosh;
-_Math$asinh;
-_Math$atanh;
-_Math$cbrt;
-_Math$clz;
-_Math$cosh;
-_Math$expm;
-_Math$fround;
-_Math$hypot;
-_Math$imul;
-_Math$log;
-_Math$log1p;
-_Math$log2;
-_Math$sign;
-_Math$sinh;
-_Math$tanh;
-_Math$trunc;
+Math.acosh;
+Math.asinh;
+Math.atanh;
+Math.cbrt;
+Math.clz32;
+Math.cosh;
+Math.expm1;
+Math.fround;
+Math.hypot;
+Math.imul;
+Math.log10;
+Math.log1p;
+Math.log2;
+Math.sign;
+Math.sinh;
+Math.tanh;
+Math.trunc;
