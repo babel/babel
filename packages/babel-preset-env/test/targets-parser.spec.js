408ad074b7d8facfe254e7842a3fcfe70469dc20
Chikara Chan <771334038@qq.com>
2018-11-02 18:30:50 +0800

    feat: add opera and and_ff mapper

--- packages/babel-preset-env/test/targets-parser.spec.js
+++ packages/babel-preset-env/test/targets-parser.spec.js
@@ -178,6 +178,7 @@ describe("getTargets", () => {
         chrome: "61.0.0",
         safari: "10.1.0",
         firefox: "60.0.0",
+        opera: "48.0.0",
         ios: "10.3.0",
         edge: "16.0.0",
       });
@@ -193,6 +194,7 @@ describe("getTargets", () => {
         chrome: "61.0.0",
         safari: "10.1.0",
         firefox: "60.0.0",
+        opera: "48.0.0",
         ios: "10.3.0",
         edge: "16.0.0",
       });
@@ -208,6 +210,7 @@ describe("getTargets", () => {
         chrome: "61.0.0",
         safari: "10.1.0",
         firefox: "60.0.0",
+        opera: "48.0.0",
         ios: "10.3.0",
         ie: "11.0.0",
         edge: "16.0.0",
@@ -224,6 +227,7 @@ describe("getTargets", () => {
       ).toEqual({
         chrome: "61.0.0",
         safari: "10.1.0",
+        opera: "48.0.0",
         ios: "10.3.0",
         ie: "11.0.0",
         edge: "16.0.0",
