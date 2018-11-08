7b54ab620bd0ca6ca40680631285e3a89c72a628
ylemkimon <mail@ylem.kim>
2018-11-09 01:29:49 +0900

    preset-env: fix `opera` from `esmodules` target and Browserslist not used (#8555)

--- packages/babel-preset-env/src/targets-parser.js
+++ packages/babel-preset-env/src/targets-parser.js
@@ -30,15 +30,16 @@ const validateTargetNames = (validTargets, targets) => {
 };
 
 const browserNameMap = {
+  and_chr: "chrome",
   android: "android",
   chrome: "chrome",
-  and_chr: "chrome",
   edge: "edge",
   firefox: "firefox",
   ie: "ie",
   ios_saf: "ios",
-  safari: "safari",
   node: "node",
+  opera: "opera",
+  safari: "safari",
 };
 
 export const isBrowsersQueryValid = (
