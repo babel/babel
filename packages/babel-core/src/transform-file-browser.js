654c78f656aaf4e56200f21812b5497c546bc08f
Daniel Tschinder <daniel@tschinder.de>
2018-11-08 20:15:17 -0800

    Fix browser files to have the same API as the nodejs ones

--- packages/babel-core/src/transform-file-browser.js
+++ packages/babel-core/src/transform-file-browser.js
@@ -1,14 +1,29 @@
 // @flow
-import type { FileResult } from "./transformation";
 
-export default function transformFile(
-  filename: string,
-  opts?: Object = {},
-  callback: (?Error, FileResult | null) => void,
+// duplicated from transform-file so we do not have to import anything here
+type TransformFile = {
+  (filename: string, callback: Function): void,
+  (filename: string, opts: ?Object, callback: Function): void,
+};
+
+export const transformFile: TransformFile = (function transformFile(
+  filename,
+  opts,
+  callback,
 ) {
   if (typeof opts === "function") {
     callback = opts;
   }
 
   callback(new Error("Transforming files is not supported in browsers"), null);
+}: Function);
+
+export function transformFileSync() {
+  throw new Error("Transforming files is not supported in browsers");
+}
+
+export function transformFileAsync() {
+  return Promise.reject(
+    new Error("Transforming files is not supported in browsers"),
+  );
 }
