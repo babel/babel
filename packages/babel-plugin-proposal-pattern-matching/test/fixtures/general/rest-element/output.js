ee16233e78eccc2a9c0b1e0f5e41a33cb08b26ff
Vincent Chan <okcdz@diverse.space>
2018-11-10 12:01:54 +0800

    support rest element

---
+++ packages/babel-plugin-proposal-pattern-matching/test/fixtures/general/rest-element/output.js
@@ -0,0 +1,8 @@
+let _case = input,
+    _rest,
+    _rest2,
+    _rest3,
+    length,
+    restLength;
+
+if (Array.isArray(_case) && _case[0] === 1 && _case[1] === 2 && (_rest = _case.slice(2))) console.log('rest: ', rest);else if (Array.isArray(_case) && _case[0] === 1 && _case[1] === 2 && _case[2] === 3 && (_rest2 = _case.slice(3), Object.is(_rest2) && (_length = _rest2.length, typeof _length !== "undefined"))) console.log('length of rest', _length);else if (Array.isArray(_case) && _case[0] === 1 && _case[1] === 2 && _case[2] === 3 && (_rest3 = _case.slice(3), Object.is(_rest3) && (_restLength = _rest3.length, typeof _restLength !== "undefined"))) console.log('length of rest', _restLength);
