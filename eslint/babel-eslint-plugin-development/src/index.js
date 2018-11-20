b615efba8787ce1f3e8f447917eb62cfe6e8545c
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-11-20 21:20:58 +0100

    Export rules

--- eslint/babel-eslint-plugin-development/src/index.js
+++ eslint/babel-eslint-plugin-development/src/index.js
@@ -3,5 +3,7 @@
 module.exports = {
   rules: {
     "no-deprecated-clone": require("./rules/no-deprecated-clone"),
+    "no-undefined-identifier": require("./rules/no-undefined-identifier"),
+    "plugin-name": require("./rules/plugin-name"),
   },
 };
