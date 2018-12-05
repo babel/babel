d915f31bcb9fffc09ffc84c32bad1ffe19bf8a58
Daniel Tschinder <daniel@tschinder.de>
2018-12-05 13:24:42 -0800

    Add tests for createTypeAnnotationBasedOnTypeof

---
+++ packages/babel-types/test/builders/flow/createTypeAnnotationBasedOnTypeof.js
@@ -0,0 +1,31 @@
+import { createTypeAnnotationBasedOnTypeof } from "@babel/types";
+
+describe("builders", function() {
+  describe("flow", function() {
+    describe("createTypeAnnotationBasedOnTypeof", function() {
+      const values = {
+        string: typeof "string",
+        number: typeof 123,
+        true: typeof true,
+        object: typeof {},
+        undefined: typeof undefined,
+        function: typeof function() {},
+        symbol: typeof Symbol(),
+      };
+
+      for (const name in values) {
+        const value = values[name];
+        it(name, function() {
+          const result = createTypeAnnotationBasedOnTypeof(value);
+          expect(result).toMatchSnapshot();
+        });
+      }
+
+      it("invalid", function() {
+        expect(() =>
+          createTypeAnnotationBasedOnTypeof("thisdoesnotexist"),
+        ).toThrow(Error);
+      });
+    });
+  });
+});
