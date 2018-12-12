d711daf436c6dddbe31417c997b32a382e8e3902
Denis Pushkarev <zloirock@zloirock.ru>
2018-12-13 03:00:04 +0700

    add fixtures for `runtime-corejs3`

--- packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs3/modules/output.js
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs3/modules/output.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime-corejs2/helpers/interopRequireDefault");
+var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
 
-var _Object$keys = require("@babel/runtime-corejs2/core-js/object/keys");
+var _Object$keys = require("@babel/runtime-corejs3/core-js/object/keys");
 
-var _Object$defineProperty = require("@babel/runtime-corejs2/core-js/object/define-property");
+var _Object$defineProperty = require("@babel/runtime-corejs3/core-js/object/define-property");
 
 _Object$defineProperty(exports, "__esModule", {
   value: true
