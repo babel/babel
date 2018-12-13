f4eec5ca798c60b705efcd8e99d4745957590000
Daniel Tschinder <daniel@tschinder.de>
2018-12-12 21:24:44 -0800

    Add new flag that indicates if a module has exports (#9171)

--- packages/babel-plugin-transform-modules-commonjs/test/fixtures/interop/multi-load/output.js
+++ packages/babel-plugin-transform-modules-commonjs/test/fixtures/interop/multi-load/output.js
@@ -2,6 +2,10 @@
 
 var _interopRequireDefault3 = require("@babel/runtime/helpers/interopRequireDefault");
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _interopRequireDefault2 = _interopRequireDefault3(require("@babel/runtime/helpers/interopRequireDefault"));
 
 console.log(_interopRequireDefault2.default);
