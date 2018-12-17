c21439a07c9e4f736dd596f4177c721c7478c0ea
Denis Pushkarev <zloirock@zloirock.ru>
2018-12-18 04:07:41 +0700

    update built-ins data in preset-env

--- packages/babel-preset-env/test/fixtures/preset-options/use-builtins-require/output.js
+++ packages/babel-preset-env/test/fixtures/preset-options/use-builtins-require/output.js
@@ -1,5 +1,9 @@
 require("core-js/modules/es.symbol.async-iterator");
 
+require("core-js/modules/es.symbol.description");
+
+require("core-js/modules/es.array.sort");
+
 require("core-js/modules/es.object.define-getter");
 
 require("core-js/modules/es.object.define-setter");
@@ -8,19 +12,27 @@ require("core-js/modules/es.object.lookup-getter");
 
 require("core-js/modules/es.object.lookup-setter");
 
-require("core-js/modules/es.array.sort");
+require("core-js/modules/es.promise.finally");
 
 require("core-js/modules/es.string.pad-start");
 
 require("core-js/modules/es.string.pad-end");
-
-require("core-js/modules/es.promise.finally");
 
 require("core-js/modules/web.dom-collections.for-each");
 
 require("core-js/modules/web.dom-collections.iterator");
 
 require("core-js/modules/web.immediate");
+
+require("core-js/modules/web.queue-microtask");
+
+require("core-js/modules/web.url");
+
+require("core-js/modules/web.url.to-json");
+
+require("core-js/modules/web.url-search-params");
+
+require("core-js/modules/web.url-search-params.sort");
 
 require("core-js/modules/web.timers");
 
