6f60d7a89765da56789aa1f38557d9cd24829ced
Denis Pushkarev <zloirock@zloirock.ru>
2018-12-19 20:12:14 +0700

    add some tests

---
+++ packages/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/spread/output.mjs
@@ -0,0 +1,4 @@
+import "core-js/modules/web.dom-collections.iterator";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/es.array.iterator";
+a = [b, ...c];
---
+++ packages/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/typed-array-edge-13/output.mjs
@@ -0,0 +1,4 @@
+import "core-js/modules/es.typed-array.includes";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.typed-array.int8-array";
+new Int8Array(1);
