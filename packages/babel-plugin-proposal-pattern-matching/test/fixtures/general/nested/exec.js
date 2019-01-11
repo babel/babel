83eb44e3ed836a6c267dd052c5773fc093d4b5b0
Greg Price <gnprice@gmail.com>
2019-01-11 06:53:10 -0800

    Fill out more of the "nested" test.

--- packages/babel-plugin-proposal-pattern-matching/test/fixtures/general/nested/exec.js
+++ packages/babel-plugin-proposal-pattern-matching/test/fixtures/general/nested/exec.js
@@ -1,20 +1,20 @@
 
 function f(input) {
   case (input) {
-//    when { name: { firstName },  age: x} -> return ['object', x, firstName];
-//    when { name: "Chen",  age: 3, number: [1, 2, 3] } -> return ['object'];
-//    when [1, { type: 3 }, b] -> return ['array', b];
-    when [{type}] -> return ['array'];
+    when { name: { firstName },  age: x} -> return ['object', x, firstName];
+    when { name: "Chen",  age: 3, number: [1, 2, 3] } -> return ['object'];
+    when [1, { type: 3 }, b] -> return ['array', b];
   }
   return [];
 }
 
-const _case = [{type: 3}];
-let _type;
-expect(
-  Array.isArray(_case) && _case[0]  && (_type = _case[0].type, typeof _type !== "undefined")
-).toBe(true);
+expect(f({name: { firstName: "Greg", etc: "etc" }, age: 1})).toEqual(['object', 1, "Greg"]);
 
-expect(f(_case)).toEqual(['array']);
+expect(f({name: "Chen", age: 3, number: [1, 2, 3]})).toEqual(['object']);
+expect(f({name: "Chen", age: 4, number: [1, 2, 3]})).toEqual([]);
+expect(f({name: "Chen", age: 3, number: [1, 2]})).toEqual([]);
 
-//expect(f([1, {type: 3, stuff: 4}, 5])).toEqual(['array', 5]);
+// Known failure.
+// expect(f({name: "Chen", age: 3, number: [1, 2, 3, 4]})).toEqual([]);
+
+expect(f([1, {type: 3, stuff: 4}, 5])).toEqual(['array', 5]);
