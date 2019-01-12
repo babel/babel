a24206eb9670a6b7f66495d6359b3f1d288a00da
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-01-13 00:35:22 +0100

    Fix error for decorators not enabled (#9321)
    * Fix error for decorators not enabled
    
    * Update error message

--- packages/babel-helper-create-class-features-plugin/src/features.js
+++ packages/babel-helper-create-class-features-plugin/src/features.js
@@ -39,9 +39,17 @@ export function isLoose(file, feature) {
 }
 
 export function verifyUsedFeatures(path, file) {
-  if (hasOwnDecorators(path)) {
+  if (hasOwnDecorators(path.node)) {
     if (!hasFeature(file, FEATURES.decorators)) {
-      throw path.buildCodeFrameError("Decorators are not enabled.");
+      throw path.buildCodeFrameError(
+        "Decorators are not enabled." +
+          "\nIf you are using " +
+          '["@babel/plugin-proposal-decorators", { "legacy": true }], ' +
+          'make sure it comes *before* "@babel/plugin-proposal-class-properties" ' +
+          "and enable loose mode, like so:\n" +
+          '\t["@babel/plugin-proposal-decorators", { "legacy": true }]\n' +
+          '\t["@babel/plugin-proposal-class-properties", { "loose": true }]',
+      );
     }
 
     if (path.isPrivate()) {
