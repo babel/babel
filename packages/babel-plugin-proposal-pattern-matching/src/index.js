2bfeba3cba6167c4a61b54bdede195fdda4eefd6
Greg Price <gnprice@gmail.com>
2019-01-11 21:41:05 -0800

    Start new implementation of transform; `jest matching -t prim` works!

--- packages/babel-plugin-proposal-pattern-matching/src/index.js
+++ packages/babel-plugin-proposal-pattern-matching/src/index.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 import { declare } from "@babel/helper-plugin-utils";
 import syntaxPatternMatching from "@babel/plugin-syntax-pattern-matching";
 import { types as t, template } from "../../babel-core";
@@ -5,243 +6,106 @@ import { types as t, template } from "../../babel-core";
 export default declare(api => {
   api.assertVersion(7);
 
-  const identifierSubstitueVisitor = {
-    Identifier(path) {
-      const { substitutionsMap } = this;
-      const { name } = path.node;
-      const subName = substitutionsMap.get(name);
-      if (typeof subName !== "undefined") {
-        path.replaceWith(t.identifier(subName));
-      }
+  const matchPattern = (pattern, id, { stmts, scope }) => {
+    switch (pattern.type) {
+      case "NumericLiteral":
+      case "StringLiteral":
+      case "BooleanLiteral":
+      case "NullLiteral":
+        stmts.push(
+          t.ifStatement(
+            t.binaryExpression("!==", id, pattern),
+            t.continueStatement(null),
+          ),
+        );
+        return;
+
+      case "Identifier":
+        stmts.push(
+          template`
+            const SRCID = ID;
+          `({ SRCID: pattern, ID: id }),
+        );
+        return;
+
+      case "ObjectMatchPattern":
+        stmts.push(
+          template`/* TODO object match */`({}, { preserveComments: true }),
+        );
+        return;
+
+      case "ArrayMatchPattern":
+        stmts.push(
+          template`/* TODO array match */`({}, { preserveComments: true }),
+        );
+        return;
+
+      case "RegExpLiteral":
+      default:
+        // TODO better error; use path.buildCodeFrameError ?
+        throw new Error("Bad expression in pattern");
+    }
+  };
+
+  const whenVisitor = {
+    WhenClause(path, { discriminantId, stmts: outerStmts, outerLabel, scope }) {
+      const { pattern, body } = path.node;
+
+      const wrapper = template`
+        do { } while (0);
+      `();
+      const stmts = wrapper.body.body; // DoWhileS -> BlockS -> []
+
+      matchPattern(pattern, discriminantId, { stmts, scope });
+
+      stmts.push(body);
+
+      stmts.push(t.continueStatement(outerLabel));
+
+      outerStmts.push(wrapper);
     },
   };
 
-  const clauses = [];
-  const ids = [];
+  const caseVisitor = {
+    CaseStatement(path, state) {
+      const { discriminant, cases } = path.node;
 
-  // every when clause will create a "virtual" scope
-  const WhenClauseVisitor = {
-    WhenClause(path) {
-      function generateTestExpr(
-        expr,
-        pattern,
-        substitutionsMap,
-        isRoot = false,
-      ) {
-        if (isRoot && t.isIdentifier(pattern)) {
-          // always true
-          if (pattern.name === "undefined") {
-            return t.booleanLiteral(true);
-          } else {
-            const bindingId = path.scope.generateUidIdentifier(pattern.name);
-            substitutionsMap.set(pattern.name, bindingId.name);
-            return template.expression`(BINDING_ID = EXPR, true)`({
-              BINDING_ID: bindingId,
-              EXPR: expr,
-            });
-          }
-        } else if (
-          t.isNullLiteral(pattern) ||
-          t.isStringLiteral(pattern) ||
-          t.isBooleanLiteral(pattern) ||
-          t.isNumericLiteral(pattern)
-        ) {
-          return t.binaryExpression("===", expr, pattern);
-        } else if (t.isObjectMatchPattern(pattern)) {
-          return generateObjectTestExpr(expr, pattern, substitutionsMap);
-        } else if (t.isArrayMatchPattern(pattern)) {
-          return generateArrayTestExpr(expr, pattern, substitutionsMap);
-        } else {
-          throw new Error("Syntax Error: not a Match Pattern");
-        }
-      }
+      // TODO is this the right namespace for a label?
+      const outerLabel = path.scope.generateUidIdentifier("case");
+      const wrapper = template`
+        LABEL:
+        do {
+        } while (0);
+      `({ LABEL: outerLabel });
+      const stmts = wrapper.body.body.body; // LabeledS -> DoWhileS -> BlockS -> []
 
-      function generateObjectTestExpr(expr, objPattern, substitutionsMap) {
-        const objectId = template.expression`EXPR !== undefined && EXPR !== null`(
-          { EXPR: expr },
-        );
+      const discriminantId = path.scope.generateUidIdentifierBasedOnNode(
+        discriminant,
+      );
+      stmts.push(
+        t.variableDeclaration("const", [
+          t.variableDeclarator(discriminantId, discriminant),
+        ]),
+      );
 
-        return objPattern.properties.reduce((acc, property) => {
-          let bindingId;
-          const { key, element } = property;
-          if (element && t.isIdentifier(element)) {
-            bindingId = path.scope.generateUidIdentifier(element.name);
-            substitutionsMap.set(element.name, bindingId.name);
-          } else {
-            bindingId = path.scope.generateUidIdentifier(key.name);
-            substitutionsMap.set(key.name, bindingId.name);
-          }
+      const { scope } = path;
+      path.traverse(whenVisitor, { discriminantId, stmts, outerLabel, scope });
+      // console.log(stmts[1].body.body);
 
-          let test;
-
-          if (element && !t.isIdentifier(element)) {
-            test = generateTestExpr(bindingId, element, substitutionsMap);
-          } else {
-            test = template.expression`typeof BINDING_ID !== "undefined"`({
-              BINDING_ID: bindingId,
-            });
-          }
-
-          return template.expression`LEFT && (BINDING_ID = EXPR.PROP, TEST)`({
-            LEFT: acc,
-            EXPR: expr,
-            PROP: property.key,
-            BINDING_ID: bindingId,
-            TEST: test,
-          });
-        }, objectId);
-      }
-
-      function generateArrayTestExpr(expr, arrayPattern, substitutionsMap) {
-        const clauses = [
-          template.expression`Array.isArray(EXPR)`({ EXPR: expr }),
-        ];
-
-        const { elements } = arrayPattern;
-
-        if (
-          elements.length > 0 &&
-          t.isMatchRestElement(elements[elements.length - 1])
-        ) {
-          if (elements.length > 1) {
-            clauses.push(
-              template.expression`EXPR.length >= LEN`({
-                EXPR: expr,
-                LEN: t.numericLiteral(elements.length - 1),
-              }),
-            );
-          }
-        } else {
-          clauses.push(
-            template.expression`EXPR.length == LEN`({
-              EXPR: expr,
-              LEN: t.numericLiteral(elements.length),
-            }),
-          );
-        }
-
-        elements.forEach((element, index) => {
-          if (t.isIdentifier(element)) {
-            const subExpr = t.memberExpression(
-              expr,
-              t.numericLiteral(index),
-              true,
-            );
-            const bindingId = path.scope.generateUidIdentifier(element.name);
-            substitutionsMap.set(element.name, bindingId.name);
-            clauses.push(
-              template.expression`(BINDING_ID = SUB_EXPR, typeof BINDING_ID !== "undefined")`(
-                {
-                  SUB_EXPR: subExpr,
-                  BINDING_ID: bindingId,
-                },
-              ),
-            );
-          } else if (t.isMatchRestElement(element)) {
-            if (index !== elements.length - 1) {
-              throw new Error(
-                "Syntax Error: RestElement must be at the end of ArrayPattern",
-              );
-            }
-            const restId = path.scope.generateUidIdentifier("rest");
-            ids.push(restId.name);
-            if (t.isIdentifier(element.body)) {
-              clauses.push(
-                template.expression`(REST_ID = EXPR.slice(NUM))`({
-                  REST_ID: restId,
-                  EXPR: expr,
-                  NUM: t.numericLiteral(index),
-                }),
-              );
-            } else {
-              const subTest = generateTestExpr(
-                restId,
-                element.body,
-                substitutionsMap,
-              );
-              clauses.push(
-                template.expression`(REST_ID = EXPR.slice(NUM), SUB_TEST)`({
-                  REST_ID: restId,
-                  EXPR: expr,
-                  NUM: t.numericLiteral(index),
-                  SUB_TEST: subTest,
-                }),
-              );
-            }
-          } else {
-            const subExpr = t.memberExpression(
-              expr,
-              t.numericLiteral(index),
-              true,
-            );
-            clauses.push(generateTestExpr(subExpr, element, substitutionsMap));
-          }
-        });
-
-        return clauses.slice(1).reduce((acc, clause) => {
-          return t.logicalExpression("&&", acc, clause);
-        }, clauses[0]);
-      }
-
-      const substitutionsMap = new Map();
-      const { pattern, body, matchGuard } = path.node;
-      const { caseId } = this;
-      let test = generateTestExpr(caseId, pattern, substitutionsMap, true);
-
-      if (matchGuard) {
-        const matchGuardPath = path.get("matchGuard");
-        matchGuardPath.traverse(identifierSubstitueVisitor, {
-          substitutionsMap,
-        });
-        test = t.logicalExpression("&&", test, matchGuard);
-      }
-
-      path.traverse(identifierSubstitueVisitor, { substitutionsMap });
-      clauses.push({
-        test,
-        body,
-        substitutionsMap,
-      });
+      path.replaceWith(
+        template`
+          LABEL:
+          do {
+            STMTS
+          } while (0);
+        `({ LABEL: outerLabel, STMTS: stmts }),
+      );
     },
   };
 
   return {
     name: "proposal-pattern-matching",
     inherits: syntaxPatternMatching,
-    visitor: {
-      CaseStatement(path) {
-        const { discriminant } = path.node;
-        const caseId = path.scope.generateUidIdentifier("case");
-        clauses.splice(0);
-        ids.splice(0);
-        path.traverse(WhenClauseVisitor, { caseId });
-        const variableDeclaration = t.variableDeclaration("let", [
-          t.variableDeclarator(caseId, discriminant),
-        ]);
-
-        let ifStatement;
-        let lastStatement;
-        clauses.forEach(({ test, body, substitutionsMap }) => {
-          for (const id of substitutionsMap.values()) {
-            ids.push(id);
-          }
-          if (typeof ifStatement === "undefined") {
-            lastStatement = ifStatement = t.ifStatement(test, body, null);
-          } else {
-            const newStmt = t.ifStatement(test, body, null);
-            lastStatement.alternate = newStmt;
-            lastStatement = newStmt;
-          }
-        });
-
-        ids.forEach(id => {
-          variableDeclaration.declarations.push(
-            t.variableDeclarator(t.identifier(id), null),
-          );
-        });
-
-        path.replaceWithMultiple([variableDeclaration, ifStatement]);
-      },
-    },
+    visitor: caseVisitor,
   };
 });
