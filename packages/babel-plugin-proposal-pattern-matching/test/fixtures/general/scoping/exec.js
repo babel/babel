f3d3f2cc2afeab09537ac51c9d4a432454b1f34a
Greg Price <gnprice@gmail.com>
2019-01-12 00:41:11 -0800

    Add tests for hoisting, and continue/break.

--- packages/babel-plugin-proposal-pattern-matching/test/fixtures/general/scoping/exec.js
+++ packages/babel-plugin-proposal-pattern-matching/test/fixtures/general/scoping/exec.js
@@ -1,3 +1,85 @@
+/*
+  // From the spec:
+
+  while (true) {
+    case (42) {
+      when v -> {
+        var hoistMe = v
+        const noHoist = v
+        function alsoMe () { return v }
+        if (v) { continue } // skips next line
+        break // breaks out of the `while` loop
+      }
+      when y -> function foo () {} // function statement, not function expression
+    }
+  }
+  console.log(hoistMe) // 42 -- variables are hoisted as in `if`
+  console.log(alsoMe()) // 42 -- so are functions
+  console.log(foo) // non-block function syntax treated as _statement_
+  console.log(noHoist) // SyntaxError -- `const`/`let` are block-scoped
+*/
+
+(() => {
+  do {
+    case (42) {
+      when v -> {
+        var hoistMe = v;
+        const noHoist = v;
+        function alsoMe () { return v }
+      }
+    }
+  } while (0);
+
+  expect(hoistMe).toBe(42);
+
+  // TODO spec says SyntaxError; is that quite right?
+  // If so, test that specifically
+  expect(typeof noHoist).toBe('undefined');
+
+  expect(alsoMe()).toBe(42);
+})();
+
+
+(() => {
+  do {
+    case (42) {
+      when v -> {}
+      // TODO currently a SyntaxError
+      // when y -> function foo () {}
+    }
+  } while (0);
+
+  //expect(typeof foo).toBe('function');
+})();
+
+
+/* TODO broken
+(() => {
+  do {
+    case (42) {
+      when v -> {
+        if (v) { continue }
+        expect(true).toBe(false);
+      }
+    }
+    expect(true).toBe(false);
+  } while (0);
+})();
+
+
+(() => {
+  do {
+    case (42) {
+      when v -> {
+        break;  // TODO test distinctly from `continue`
+        expect(true).toBe(false);
+      }
+    }
+    expect(true).toBe(false);
+  } while (0);
+})();
+*/
+
 
 case ({ x: 1 }) {
   when { x } -> {
