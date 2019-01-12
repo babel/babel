c7cfa0d8da5d553e30482967148b526c74850d19
Greg Price <gnprice@gmail.com>
2019-01-12 00:22:47 -0800

    Add (failing, commented-out) tests for object rest elements.

--- packages/babel-plugin-proposal-pattern-matching/test/fixtures/match-logic/object/exec.js
+++ packages/babel-plugin-proposal-pattern-matching/test/fixtures/match-logic/object/exec.js
@@ -37,3 +37,35 @@ expect(match_x1({x: "1"})).toBe(false);
 expect(match_x1({x: null})).toBe(false);
 expect(match_x1({x: undefined})).toBe(false);
 expect(match_x1({x: 1.0})).toBe(true);
+
+
+/*
+  case (input) {
+    when {x, ...y} -> ... // binds all-other-properties to `y`.
+    when {x, ...{y}} -> ... // SyntaxError
+  }
+*/
+
+/* TODO not implemented
+
+function match_x_rest(input) {
+  case (input) {
+    when {x, ...rest} -> return [x, rest];
+  }
+  return false;
+}
+
+expect(match_x_rest({x: 1, y: 2})).toEqual([1, {y: 2}]);
+expect(match_x_rest({x: 1})).toEqual([1, {}]);
+expect(match_x_rest({y: 2})).toBe(false);
+
+
+function match_x_restobj(input) {
+  case (input) {
+    // TODO test this is a SyntaxError
+    // when {x, ...{y}} -> return [x, rest];
+  }
+  return false;
+}
+
+*/
