1ab2bb73592c19e228fcedb0c3a562012a5f24e5
Greg Price <gnprice@gmail.com>
2019-01-13 22:49:15 -0800

    Try another way to put plugin in standalone preset-stage-1.

--- packages/babel-standalone/src/preset-stage-1.js
+++ packages/babel-standalone/src/preset-stage-1.js
@@ -3,7 +3,7 @@ import presetStage2 from "./preset-stage-2";
 import transformExportDefaultFrom from "@babel/plugin-proposal-export-default-from";
 import transformLogicalAssignmentOperators from "@babel/plugin-proposal-logical-assignment-operators";
 import transformOptionalChaining from "@babel/plugin-proposal-optional-chaining";
-import transformPatternMatching from "@babel/plugin-proposal-pattern-matching";
+import transformPatternMatching from "../../babel-plugin-proposal-pattern-matching";
 import transformPipelineOperator from "@babel/plugin-proposal-pipeline-operator";
 import transformNullishCoalescingOperator from "@babel/plugin-proposal-nullish-coalescing-operator";
 import transformDoExpressions from "@babel/plugin-proposal-do-expressions";
