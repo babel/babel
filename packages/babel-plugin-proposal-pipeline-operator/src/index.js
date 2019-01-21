93dcf97db11c7f8ff328b2c5cc62ffcd58c0e371
Thiago Arrais <thiago.arrais@gmail.com>
2019-01-21 13:21:50 -0300

    Hoist proposals to module root

--- packages/babel-plugin-proposal-pipeline-operator/src/index.js
+++ packages/babel-plugin-proposal-pipeline-operator/src/index.js
@@ -3,13 +3,13 @@ import syntaxPipelineOperator from "@babel/plugin-syntax-pipeline-operator";
 import minimalVisitor from "./minimalVisitor";
 import smartVisitor from "./smartVisitor";
 
+const visitorsPerProposal = {
+  minimal: minimalVisitor,
+  smart: smartVisitor,
+};
+
 export default declare((api, options) => {
   api.assertVersion(7);
-
-  const visitorsPerProposal = {
-    minimal: minimalVisitor,
-    smart: smartVisitor,
-  };
 
   return {
     name: "proposal-pipeline-operator",
