fd5de03825491af6748c3f7a6151989dc170ded5
Thiago Arrais <thiago.arrais@gmail.com>
2019-01-22 18:10:30 -0300

    Include async plugin (because node 6)

--- packages/babel-plugin-proposal-pipeline-operator/test/fixtures/smart/await/output.js
+++ packages/babel-plugin-proposal-pipeline-operator/test/fixtures/smart/await/output.js
@@ -1,9 +1,20 @@
-async function myFunction(n) {
-  const _pipe = n;
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-  const _pipe2 = Math.abs(_pipe);
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-  const _pipe3 = Promise.resolve(_pipe2);
+function myFunction(_x) {
+  return _myFunction.apply(this, arguments);
+}
 
-  return await _pipe3;
+function _myFunction() {
+  _myFunction = _asyncToGenerator(function* (n) {
+    const _pipe = n;
+
+    const _pipe2 = Math.abs(_pipe);
+
+    const _pipe3 = Promise.resolve(_pipe2);
+
+    return yield _pipe3;
+  });
+  return _myFunction.apply(this, arguments);
 }
