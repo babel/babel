0ea41bf0dd693ecbc017c725ad5698435ed63e76
Daniel Tschinder <daniel@tschinder.de>
2019-01-21 22:43:06 -0800

    Revert "Differentiate object spread and non-spread properties (#9341)"
    This reverts commit 3ae5e79ec8ab1e6d1c6927f35d4a1eeac04faf9a.

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -389,20 +389,16 @@ helpers.objectSpread = helper("7.0.0-beta.0")`
 
   export default function _objectSpread(target) {
     for (var i = 1; i < arguments.length; i++) {
-      if (i % 2) {
-        var source = (arguments[i] != null) ? arguments[i] : {};
-        var ownKeys = Object.keys(source);
-        if (typeof Object.getOwnPropertySymbols === 'function') {
-          ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
-            return Object.getOwnPropertyDescriptor(source, sym).enumerable;
-          }));
-        }
-        ownKeys.forEach(function(key) {
-          defineProperty(target, key, source[key]);
-        });
-      } else {
-        Object.defineProperties(target, Object.getOwnPropertyDescriptors(arguments[i]));
+      var source = (arguments[i] != null) ? arguments[i] : {};
+      var ownKeys = Object.keys(source);
+      if (typeof Object.getOwnPropertySymbols === 'function') {
+        ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
+          return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+        }));
       }
+      ownKeys.forEach(function(key) {
+        defineProperty(target, key, source[key]);
+      });
     }
     return target;
   }
