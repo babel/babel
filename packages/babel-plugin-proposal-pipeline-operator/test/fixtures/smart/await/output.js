60225756eae76daf058ad13e354a18c96d368f4d
Thiago Arrais <thiago.arrais@gmail.com>
2019-01-24 13:04:48 -0300

    Avoid running this await test in Node 6

--- packages/babel-plugin-proposal-pipeline-operator/test/fixtures/smart/await/output.js
+++ packages/babel-plugin-proposal-pipeline-operator/test/fixtures/smart/await/output.js
@@ -1,16 +1,5 @@
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+async function myFunction(n) {
+  var _ref, _ref2, _n;
 
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
-function myFunction(_x) {
-  return _myFunction.apply(this, arguments);
-}
-
-function _myFunction() {
-  _myFunction = _asyncToGenerator(function* (n) {
-    var _ref, _ref2, _n;
-
-    return _ref = (_ref2 = (_n = n, Math.abs(_n)), Promise.resolve(_ref2)), yield _ref;
-  });
-  return _myFunction.apply(this, arguments);
+  return _ref = (_ref2 = (_n = n, Math.abs(_n)), Promise.resolve(_ref2)), await _ref;
 }
