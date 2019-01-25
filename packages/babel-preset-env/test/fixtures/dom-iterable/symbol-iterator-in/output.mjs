2b97065098c7b11864da58881ff8ceed9e1d95c8
Denis Pushkarev <zloirock@zloirock.ru>
2019-01-25 22:02:55 +0700

    update definitions for core-js@3

--- packages/babel-preset-env/test/fixtures/dom-iterable/symbol-iterator-in/output.mjs
+++ packages/babel-preset-env/test/fixtures/dom-iterable/symbol-iterator-in/output.mjs
@@ -1,4 +1,5 @@
 import "core-js/modules/es.symbol";
+import "core-js/modules/es.symbol.description";
 import "core-js/modules/es.symbol.iterator";
 import "core-js/modules/web.dom-collections.iterator";
 import "core-js/modules/es.array.iterator";
--- packages/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/builtins-used/output.mjs
+++ packages/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/builtins-used/output.mjs
@@ -1,5 +1,6 @@
 import "core-js/modules/es.symbol.iterator";
 import "core-js/modules/es.symbol";
+import "core-js/modules/es.symbol.description";
 import "core-js/modules/es.symbol.match";
 import "core-js/modules/es.string.match";
 import "core-js/modules/es.promise";
