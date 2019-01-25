33c43bece161ad9493ec7a8d7759ef3038f4f72b
Denis Pushkarev <zloirock@zloirock.ru>
2019-01-26 00:21:44 +0700

    add `corejs` option to `preset-env` as a temporal solution for support different `core-js` versions

---
+++ packages/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/corejs2-edge-13/input.mjs
@@ -0,0 +1,2 @@
+new Int8Array(1);
+new Promise(foo).finally(bar);
---
+++ packages/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/corejs2-edge-13/output.mjs
@@ -0,0 +1,4 @@
+import "core-js/modules/es6.promise";
+import "core-js/modules/es7.promise.finally";
+new Int8Array(1);
+new Promise(foo).finally(bar);
