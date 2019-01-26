741b72294206c8b0d805848d12ef1082ab6a6324
Moti Zilberman <motiz88@gmail.com>
2019-01-26 11:07:45 +0000

    Destructuring: Fix array unpacking assignments with holes on RHS

--- packages/babel-plugin-transform-destructuring/test/fixtures/destructuring/array-unpack-optimisation/output.js
+++ packages/babel-plugin-transform-destructuring/test/fixtures/destructuring/array-unpack-optimisation/output.js
@@ -34,3 +34,10 @@ var _ref7 = [clazz.foo(), bar],
 var _ref8 = [clazz.foo, bar],
     a = _ref8[0],
     b = _ref8[1];
+var a,
+    b = 2;
+a = 1;
+b = 2;
+a = void 0;
+b = 2;
+; // Avoid completion record special case
