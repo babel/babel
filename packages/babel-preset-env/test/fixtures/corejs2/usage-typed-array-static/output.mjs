f84ea73268b8bbd0c20b071868461c3a68f8c730
Denis Pushkarev <zloirock@zloirock.ru>
2019-01-31 11:27:02 +0700

    add fixtures for different core-js versions

---
+++ packages/babel-preset-env/test/fixtures/corejs2/usage-typed-array-static/output.mjs
@@ -0,0 +1,2 @@
+import "core-js/modules/es6.typed.int8-array";
+Int8Array.of();
--- packages/babel-preset-env/test/fixtures/preset-options-add-used-built-ins/corejs2-edge-13/output.mjs
+++
@@ -1,4 +0,0 @@
-import "core-js/modules/es6.promise";
-import "core-js/modules/es7.promise.finally";
-new Int8Array(1);
-new Promise(foo).finally(bar);
