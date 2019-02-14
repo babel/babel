b6ab5b62d7c82f9fb7489bab701355d01ce0c3eb
Denis Pushkarev <zloirock@zloirock.ru>
2019-02-14 21:23:12 +0700

    add `Promise` dependencies on dynamic import, fixes #7402

--- packages/babel-preset-env/src/polyfills/corejs3/usage-plugin.js
+++ packages/babel-preset-env/src/polyfills/corejs3/usage-plugin.js
@@ -86,6 +86,12 @@ export default function(
       },
     },
 
+    CallExpression({ node }) {
+      if (t.isImport(node.callee)) {
+        this.addUnsupported(PromiseDependencies);
+      }
+    },
+
     // Symbol()
     // new Promise
     ReferencedIdentifier({ node: { name }, parent, scope }) {
