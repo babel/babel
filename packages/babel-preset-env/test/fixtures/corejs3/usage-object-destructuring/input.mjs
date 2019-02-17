0f912d6ff5fa47c3b01a25828c6b09dfde2bd95b
Denis Pushkarev <zloirock@zloirock.ru>
2019-02-18 00:42:03 +0700

    rework corejs3/usage plugin, fix many destructuring-ralated cases

---
+++ packages/babel-preset-env/test/fixtures/corejs3/usage-object-destructuring/input.mjs
@@ -0,0 +1,13 @@
+var bind, toString;
+
+var { bind } = Function;
+
+var { entries } = Object;
+
+({ toString } = Math);
+
+const [{ exec }] = something;
+
+const { 'trim': foo } = '1';
+
+const { foo: { filter } } = { foo: [] }
