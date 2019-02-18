f44b29522c43b501c83583dd681b2a7e853e4e03
Tim McClure <mccluret@gmail.com>
2019-02-18 13:57:29 -0500

    Remove `transform-classes` from priv method tests (#9537)

--- packages/babel-plugin-proposal-private-methods/test/fixtures/accessors/updates/output.js
+++ packages/babel-plugin-proposal-private-methods/test/fixtures/accessors/updates/output.js
@@ -1,11 +1,5 @@
-var Cl =
-/*#__PURE__*/
-function () {
-  "use strict";
-
-  function Cl() {
-    babelHelpers.classCallCheck(this, Cl);
-
+class Cl {
+  constructor() {
     _privateFieldValue.set(this, {
       get: _get_privateFieldValue,
       set: _set_privateFieldValue
@@ -19,43 +13,38 @@ function () {
     this.publicField = "not secret string";
   }
 
-  babelHelpers.createClass(Cl, [{
-    key: "publicGetPrivateField",
-    value: function publicGetPrivateField() {
-      return babelHelpers.classPrivateFieldGet(this, _privateFieldValue);
-    }
-  }, {
-    key: "publicSetPrivateField",
-    value: function publicSetPrivateField(newValue) {
-      babelHelpers.classPrivateFieldSet(this, _privateFieldValue, newValue);
-    }
-  }, {
-    key: "testUpdates",
-    value: function testUpdates() {
-      var _this$privateFieldVal, _this$privateFieldVal2;
+  publicGetPrivateField() {
+    return babelHelpers.classPrivateFieldGet(this, _privateFieldValue);
+  }
 
-      babelHelpers.classPrivateFieldSet(this, _privateField, 0);
-      this.publicField = 0;
-      babelHelpers.classPrivateFieldSet(this, _privateFieldValue, (babelHelpers.classPrivateFieldSet(this, _privateFieldValue, (_this$privateFieldVal2 = +babelHelpers.classPrivateFieldGet(this, _privateFieldValue)) + 1), _this$privateFieldVal2));
-      this.publicFieldValue = this.publicFieldValue++;
-      babelHelpers.classPrivateFieldSet(this, _privateFieldValue, +babelHelpers.classPrivateFieldGet(this, _privateFieldValue) + 1);
-      ++this.publicFieldValue;
-      babelHelpers.classPrivateFieldSet(this, _privateFieldValue, babelHelpers.classPrivateFieldGet(this, _privateFieldValue) + 1);
-      this.publicFieldValue += 1;
-      babelHelpers.classPrivateFieldSet(this, _privateFieldValue, -(babelHelpers.classPrivateFieldGet(this, _privateFieldValue) ** babelHelpers.classPrivateFieldGet(this, _privateFieldValue)));
-      this.publicFieldValue = -(this.publicFieldValue ** this.publicFieldValue);
-    }
-  }, {
-    key: "publicFieldValue",
-    get: function () {
-      return this.publicField;
-    },
-    set: function (newValue) {
-      this.publicField = newValue;
-    }
-  }]);
-  return Cl;
-}();
+  publicSetPrivateField(newValue) {
+    babelHelpers.classPrivateFieldSet(this, _privateFieldValue, newValue);
+  }
+
+  get publicFieldValue() {
+    return this.publicField;
+  }
+
+  set publicFieldValue(newValue) {
+    this.publicField = newValue;
+  }
+
+  testUpdates() {
+    var _this$privateFieldVal, _this$privateFieldVal2;
+
+    babelHelpers.classPrivateFieldSet(this, _privateField, 0);
+    this.publicField = 0;
+    babelHelpers.classPrivateFieldSet(this, _privateFieldValue, (babelHelpers.classPrivateFieldSet(this, _privateFieldValue, (_this$privateFieldVal2 = +babelHelpers.classPrivateFieldGet(this, _privateFieldValue)) + 1), _this$privateFieldVal2));
+    this.publicFieldValue = this.publicFieldValue++;
+    babelHelpers.classPrivateFieldSet(this, _privateFieldValue, +babelHelpers.classPrivateFieldGet(this, _privateFieldValue) + 1);
+    ++this.publicFieldValue;
+    babelHelpers.classPrivateFieldSet(this, _privateFieldValue, babelHelpers.classPrivateFieldGet(this, _privateFieldValue) + 1);
+    this.publicFieldValue += 1;
+    babelHelpers.classPrivateFieldSet(this, _privateFieldValue, -(babelHelpers.classPrivateFieldGet(this, _privateFieldValue) ** babelHelpers.classPrivateFieldGet(this, _privateFieldValue)));
+    this.publicFieldValue = -(this.publicFieldValue ** this.publicFieldValue);
+  }
+
+}
 
 var _privateField = new WeakMap();
 
