aa348bbe6f9e5cf8b2ec15d4d58a496fcdac8fc5
Justin Ridgewell <justin@ridgewell.name>
2019-02-26 13:08:53 -0500

    Add tests

---
+++ packages/babel-preset-env/test/fixtures/preset-options/safari-tagged-template-literals/output.js
@@ -0,0 +1,13 @@
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["Safari 12 borked"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+tag(_templateObject());
