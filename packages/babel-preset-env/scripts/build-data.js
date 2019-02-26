2021f15f275a8c17a8e2408b9742f244e0aff056
Justin Ridgewell <justin@ridgewell.name>
2019-02-26 12:58:05 -0500

    preset-env: minimally supporting version
    This changes the "does browser support X" algorithm to "lowest version such that all lower versions support it".
    
    Eg, given `{ chrome70: true, chrome60: false, chrome50: true }`, the lowest version is chrome70, not chrome50.
    
    This is done to remove Tagged Template Literal support from Safari, which introduced a bug in Safari 12 but correctly implemented the feature in Safari 11-9.

--- packages/babel-preset-env/scripts/build-data.js
+++ packages/babel-preset-env/scripts/build-data.js
@@ -217,16 +217,26 @@ const getLowestImplementedVersion = ({ features }, env) => {
           unreleasedLabelForEnv === version.version ||
           !isNaN(parseFloat(version.version))
       )
-      // Sort in asc order, with unreleasedLabelForEnv coming last.
+      // Sort in desc order, with unreleasedLabelForEnv coming last.
       .sort(({ semver: av }, { semver: bv }) => {
-        if (av === unreleasedLabelForEnv) return 1;
-        if (bv === unreleasedLabelForEnv) return -1;
-        if (semver.gt(av, bv)) return 1;
-        if (semver.gt(bv, av)) return -1;
+        if (av === unreleasedLabelForEnv) return -1;
+        if (bv === unreleasedLabelForEnv) return 1;
+        if (semver.gt(av, bv)) return -1;
+        if (semver.gt(bv, av)) return 1;
         return 0;
       });
 
-    return reportedVersions.find(version => version.implements);
+    // Find the lowest version such that all lower versions implement it.
+    // Eg, given { chrome70: true, chrome60: false, chrome50: true }, the
+    // lowest version is chrome70, not chrome50.
+    let lowest = null;
+    for (const version of reportedVersions) {
+      if (!version.implements) {
+        break;
+      }
+      lowest = version;
+    }
+    return lowest;
   });
 
   const envFiltered = envTests.filter(t => t);
