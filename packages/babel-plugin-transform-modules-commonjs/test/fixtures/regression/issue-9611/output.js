47bee79acf39e4fba0e21c4e2c52ea6cd11fce67
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-02-28 18:52:37 +0100

    Test

---
+++ packages/babel-plugin-transform-modules-commonjs/test/fixtures/regression/issue-9611/output.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = _default;
+
+var _x = _interopRequireDefault(require("./x"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _default(x) {}
