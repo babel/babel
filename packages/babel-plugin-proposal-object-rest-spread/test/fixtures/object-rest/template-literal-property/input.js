eda1e3198868049856875db2d80478551f947ca2
pnowak <piotr.nowak.25.02.1978@gmail.com>
2019-03-05 21:22:42 +0100

    Remove BinaryExpression from test #9563

--- packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-rest/template-literal-property/input.js
+++ packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-rest/template-literal-property/input.js
@@ -4,17 +4,6 @@ const {
   given_name: givenName,
   'last_name': lastName,
   [`country`]: country,
-  [prefix + 'state']: state,
   [`${prefix}consents`]: consents,
   ...rest
 } = input;
-
-const secondInput = {};
-
-const {
-  name: name,
-  'first_name': first,
-  [`middle`]: middle,
-  [`${prefix}last`]: last,
-  ...another
-} = secondInput;
--- packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-rest/template-literal-property/output.js
+++ packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-rest/template-literal-property/output.js
@@ -1,22 +1,9 @@
 const input = {};
-
-const _ref = prefix + 'state',
-      _ref2 = `${prefix}consents`,
+const _ref = `${prefix}consents`,
       {
   given_name: givenName,
   'last_name': lastName,
   [`country`]: country,
-  [_ref]: state,
-  [_ref2]: consents
+  [_ref]: consents
 } = input,
-      rest = babelHelpers.objectWithoutProperties(input, ["given_name", "last_name", `country`, _ref, _ref2].map(babelHelpers.toPropertyKey));
-
-const secondInput = {};
-const _ref3 = `${prefix}last`,
-      {
-  name: name,
-  'first_name': first,
-  [`middle`]: middle,
-  [_ref3]: last
-} = secondInput,
-      another = babelHelpers.objectWithoutProperties(secondInput, ["name", "first_name", `middle`, _ref3].map(babelHelpers.toPropertyKey));
+      rest = babelHelpers.objectWithoutProperties(input, ["given_name", "last_name", `country`, _ref].map(babelHelpers.toPropertyKey));
