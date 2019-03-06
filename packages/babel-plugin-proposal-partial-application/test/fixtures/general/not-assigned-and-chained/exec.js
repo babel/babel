dabf03437c95723520f09f23b404be94b59e3791
Behrang Yarahmadi <behrang.yarahmdi@trivago.com>
2019-03-06 13:45:40 +0100

    use Promise in the test instead of async await

--- packages/babel-plugin-proposal-partial-application/test/fixtures/general/not-assigned-and-chained/exec.js
+++ packages/babel-plugin-proposal-partial-application/test/fixtures/general/not-assigned-and-chained/exec.js
@@ -1,7 +1,7 @@
 "use strict";
 
-async function square(x) {
-  return await x * x;
+function square(n){
+  return new Promise(resolve => resolve(n * n));
 }
 
 square(?)(3).then(res => expect(res).toBe(9));
