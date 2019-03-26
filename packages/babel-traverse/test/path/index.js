16909beb110a9e18d44f45a79b33a97e3b5ebc23
Daniel Tschinder <daniel@tschinder.de>
2019-03-26 16:06:28 -0700

    Allow false as default value

--- packages/babel-traverse/test/path/index.js
+++ packages/babel-traverse/test/path/index.js
@@ -2,33 +2,39 @@ import { NodePath } from "../../lib";
 
 describe("NodePath", () => {
   describe("setData/getData", () => {
-    it("can set default value", function() {
+    it("can set default value", () => {
       const path = new NodePath({}, {});
 
       expect(path.getData("foo", "test")).toBe("test");
     });
-    it("can set false", function() {
+    it("can set false", () => {
       const path = new NodePath({}, {});
       path.setData("foo", false);
 
       expect(path.getData("foo", true)).toBe(false);
     });
 
-    it("can set true", function() {
+    it("can set true", () => {
       const path = new NodePath({}, {});
       path.setData("foo", true);
 
       expect(path.getData("foo", false)).toBe(true);
     });
 
-    it("can set null", function() {
+    it("can set null", () => {
       const path = new NodePath({}, {});
       path.setData("foo", null);
 
       expect(path.getData("foo", true)).toBe(null);
     });
 
-    it("does not use object base properties", function() {
+    it("can use false as default", () => {
+      const path = new NodePath({}, {});
+
+      expect(path.getData("foo", false)).toBe(false);
+    });
+
+    it("does not use object base properties", () => {
       const path = new NodePath({}, {});
 
       expect(path.getData("__proto__", "test")).toBe("test");
