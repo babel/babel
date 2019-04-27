2254f7ccea0a144031b7477da21a2d7f9428acb3
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-27 11:16:34 +0200

    Current behavior for preset-env destructuring and edge

---
+++ packages/babel-preset-env/test/fixtures/preset-options/destructuring-edge/output.js
@@ -0,0 +1,9 @@
+((a, _ref) => {
+  let _ref$b = _ref.b,
+      b = _ref$b === void 0 ? 0 : _ref$b,
+      _ref$c = _ref.c,
+      c = _ref$c === void 0 ? 3 : _ref$c;
+  return a === 1 && b === 2 && c === 3;
+})(1, {
+  b: 2
+});
