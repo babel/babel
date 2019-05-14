ab841aacf1bed9d43e8bef25be98c0f8fe836f62
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-05-14 21:55:20 +0200

    Alias other mobile browsers, since they aren't included by compat-table

--- packages/babel-preset-env/src/targets-parser.js
+++ packages/babel-preset-env/src/targets-parser.js
@@ -44,13 +44,16 @@ const validateTargetNames = (targets: Targets): void => {
 
 const browserNameMap = {
   and_chr: "chrome",
+  and_ff: "firefox",
   android: "android",
   chrome: "chrome",
   edge: "edge",
   firefox: "firefox",
   ie: "ie",
+  ie_mob: "ie",
   ios_saf: "ios",
   node: "node",
+  op_mob: "opera",
   opera: "opera",
   safari: "safari",
   samsung: "samsung",
