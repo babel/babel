17720675ca60902c5d94c4048559859651042b02
Tan Li Hau <lhtan93@gmail.com>
2019-05-26 10:01:28 +0800

    test case for loose private-loose

---
+++ packages/babel-plugin-proposal-class-properties/test/fixtures/private-loose/destructuring-object-pattern-1/output.js
@@ -0,0 +1,18 @@
+var Foo = function Foo(props) {
+  "use strict";
+
+  babelHelpers.classCallCheck(this, Foo);
+  Object.defineProperty(this, _client, {
+    writable: true,
+    value: void 0
+  });
+  babelHelpers.classPrivateFieldLooseBase(this, _client)[_client] = 'foo';
+  ;
+  ({
+    x: this.x = babelHelpers.classPrivateFieldLooseBase(this, _client)[_client],
+    y: babelHelpers.classPrivateFieldLooseBase(this, _client)[_client],
+    z: this.z = babelHelpers.classPrivateFieldLooseBase(this, _client)[_client]
+  } = props);
+};
+
+var _client = babelHelpers.classPrivateFieldLooseKey("client");
