922ce97fd66f16fba964b58e33c5f748b5a598cd
Devin Alexander Torres <d@devinus.io>
2019-06-08 20:10:21 -0400

    feat(transform-runtime): add more type hints for instance methods

--- packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs3/instance-inference-optimization/output.js
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs3/instance-inference-optimization/output.js
@@ -1,9 +1,71 @@
+var _flagsInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/flags");
+
+var _bindInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/bind");
+
+var _includesInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/includes");
+
+var _repeatInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/repeat");
+
+var _sliceInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/slice");
+
+var _lastIndexOfInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/last-index-of");
+
+var _indexOfInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/index-of");
+
 var _concatInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/concat");
 
-var _context;
+var _context, _context2, _context3, _context4, _context5, _context6, _context7;
 
 "".concat(b);
 
 _concatInstanceProperty(_context = []).call(_context, b);
 
 _concatInstanceProperty(a).call(a, b);
+
+"".indexOf();
+
+_indexOfInstanceProperty(_context2 = []).call(_context2);
+
+_indexOfInstanceProperty(a).call(a);
+
+"".lastIndexOf();
+
+_lastIndexOfInstanceProperty(_context3 = []).call(_context3);
+
+_lastIndexOfInstanceProperty(a).call(a);
+
+"".slice();
+
+_sliceInstanceProperty(_context4 = []).call(_context4);
+
+_sliceInstanceProperty(a).call(a);
+
+_repeatInstanceProperty(_context5 = "").call(_context5, b);
+
+[].repeat(b);
+
+_repeatInstanceProperty(a).call(a, b);
+
+_includesInstanceProperty(_context6 = "").call(_context6, b);
+
+_includesInstanceProperty(_context7 = []).call(_context7, b);
+
+_includesInstanceProperty(a).call(a, b);
+
+"".bind;
+[].bind;
+
+_bindInstanceProperty(() => {});
+
+_bindInstanceProperty(function () {});
+
+_bindInstanceProperty(a);
+
+"".flags;
+[].flags;
+
+_flagsInstanceProperty(/a/i);
+
+_flagsInstanceProperty(new RegExp("a", "i"));
+
+_flagsInstanceProperty(a);
