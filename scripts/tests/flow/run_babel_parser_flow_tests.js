6d2f111d6047b93741f351b3fa34f7159dda1bb7
Tan Li Hau <tanhauhau@users.noreply.github.com>
2019-06-15 18:31:12 +0800

    flow - allow type parameter defaults in function declarations (#10084)
    * flow - allow type parameter defaults in function declarations
    
    * fix flow test
    
    * add intern_comments option
    
    * fix flow parser test
    
    * remove allowdefault from flowParseTypeParameterDeclaration
    
    * rename test cases

--- scripts/tests/flow/run_babel_parser_flow_tests.js
+++ scripts/tests/flow/run_babel_parser_flow_tests.js
@@ -113,7 +113,10 @@ const options = {
     ["flow", { all: true }],
     "flowComments",
     "jsx",
+    "classProperties",
     "classPrivateProperties",
+    "classPrivateMethods",
+    "bigInt",
   ],
   sourceType: "module",
   ranges: true,
@@ -127,6 +130,7 @@ const flowOptionsMapping = {
   esproposal_nullish_coalescing: "nullishCoalescingOperator",
   esproposal_optional_chaining: "optionalChaining",
   types: "flowComments",
+  intern_comments: false,
 };
 
 const summary = {
@@ -169,10 +173,12 @@ tests.forEach(section => {
           }
           return;
         }
-        if (!flowOptionsMapping[option]) {
+        if (!(option in flowOptionsMapping)) {
           throw new Error("Parser options not mapped " + option);
         }
-        babelParserOptions.plugins.push(flowOptionsMapping[option]);
+        if (flowOptionsMapping[option]) {
+          babelParserOptions.plugins.push(flowOptionsMapping[option]);
+        }
       });
     }
 
