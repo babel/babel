77fd7cd4c4eb0c1c38059f0a85b5afb7a33fc3d1
Brian Ng <bng412@gmail.com>
2019-06-30 04:00:43 -0500

    Allow `defaults` query in preset-env (#8897)

---
+++ packages/babel-preset-env/test/fixtures/preset-options/browserslist-defaults-2/output.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var test = function test(a, _ref, c) {
+  var _ref2 = _slicedToArray(_ref, 1),
+      b = _ref2[0];
+
+  return "";
+};
