5b86353b35534a1824fa4c7a3ee71f601a73c798
Artem Butusov <art.sormy@gmail.com>
2019-07-03 10:49:20 -0400

    Added es3 backward compatibility for react helper code (#10155)
    * Added es3 backward compatibility for react helper code
    
    * tweak comment [skip ci]

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -26,13 +26,14 @@ helpers.typeof = helper("7.0.0-beta.0")`
   }
 `;
 
+// "for" is a reserved keyword in ES3 so escaping it here for backward compatibility
 helpers.jsx = helper("7.0.0-beta.0")`
   var REACT_ELEMENT_TYPE;
 
   export default function _createRawReactElement(type, props, key, children) {
     if (!REACT_ELEMENT_TYPE) {
       REACT_ELEMENT_TYPE = (
-        typeof Symbol === "function" && Symbol.for && Symbol.for("react.element")
+        typeof Symbol === "function" && Symbol["for"] && Symbol["for"]("react.element")
       ) || 0xeac7;
     }
 
