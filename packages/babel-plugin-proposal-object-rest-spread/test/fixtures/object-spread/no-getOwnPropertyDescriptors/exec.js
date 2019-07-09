97bc5a298182fdcaf0a3bc00bfdd4b0fb55247dd
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-07-09 19:53:38 +0200

    Test 2

--- packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-spread/no-getOwnPropertyDescriptors/exec.js
+++ packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-spread/no-getOwnPropertyDescriptors/exec.js
@@ -1,6 +1,6 @@
 const oldGOPDs = Object.getOwnPropertyDescriptors;
 Object.getOwnPropertyDescriptors = null;
 
-({ ...{ a: 1 }, ...{} });
+({ ...{ a: 1 }, b: 1, ...{} });
 
 Object.getOwnPropertyDescriptors = oldGOPDs;
