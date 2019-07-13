d7714ad47b3bafebb9d948eb07923c74e18fc66c
Letladi Sebesho <sletladi@gmail.com>
2019-07-13 22:38:23 +0200

    added flow to directory cmd

--- packages/babel-cli/src/babel/options.js
+++ packages/babel-cli/src/babel/options.js
@@ -1,3 +1,5 @@
+// @flow
+
 import fs from "fs";
 
 import commander from "commander";
@@ -151,7 +153,12 @@ commander.option(
 commander.version(pkg.version + " (@babel/core " + version + ")");
 commander.usage("[options] <files ...>");
 
-export default function parseArgv(args: Array<string>) {
+export type cmdOptions = {
+  babelOptions: Object,
+  cliOptions: Object,
+};
+
+export default function parseArgv(args: Array<string>): cmdOptions {
   //
   commander.parse(args);
 
