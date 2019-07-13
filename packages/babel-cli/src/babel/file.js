01e08c110a17594669dfee632397eb2156959bba
Letladi Sebesho <sletladi@gmail.com>
2019-07-13 23:13:20 +0200

    added flow to file cmd

--- packages/babel-cli/src/babel/file.js
+++ packages/babel-cli/src/babel/file.js
@@ -6,9 +6,18 @@ import path from "path";
 import fs from "fs";
 
 import * as util from "./util";
+import { type cmdOptions } from "./options";
 
-export default async function({ cliOptions, babelOptions }) {
-  function buildResult(fileResults) {
+type compileOutput = {
+  code: string,
+  map: Object,
+};
+
+export default async function({
+  cliOptions,
+  babelOptions,
+}: cmdOptions): Promise<void> {
+  function buildResult(fileResults): compileOutput {
     const map = new sourceMap.SourceMapGenerator({
       file:
         cliOptions.sourceMapTarget ||
@@ -74,7 +83,7 @@ export default async function({ cliOptions, babelOptions }) {
     };
   }
 
-  function output(fileResults) {
+  function output(fileResults: Array<string>): void {
     const result = buildResult(fileResults);
 
     if (cliOptions.outFile) {
@@ -91,8 +100,8 @@ export default async function({ cliOptions, babelOptions }) {
     }
   }
 
-  function readStdin() {
-    return new Promise((resolve, reject) => {
+  function readStdin(): Promise<string> {
+    return new Promise((resolve: Function, reject: Function) => {
       let code = "";
 
       process.stdin.setEncoding("utf8");
@@ -109,7 +118,7 @@ export default async function({ cliOptions, babelOptions }) {
     });
   }
 
-  async function stdin() {
+  async function stdin(): Promise<void> {
     const code = await readStdin();
 
     const res = await util.transform(
@@ -126,7 +135,7 @@ export default async function({ cliOptions, babelOptions }) {
     output([res]);
   }
 
-  async function walk(filenames) {
+  async function walk(filenames: Array<string>): Promise<void> {
     const _filenames = [];
 
     filenames.forEach(function(filename) {
@@ -151,7 +160,7 @@ export default async function({ cliOptions, babelOptions }) {
     });
 
     const results = await Promise.all(
-      _filenames.map(async function(filename) {
+      _filenames.map(async function(filename): Promise<void> {
         let sourceFilename = filename;
         if (cliOptions.outFile) {
           sourceFilename = path.relative(
@@ -168,7 +177,7 @@ export default async function({ cliOptions, babelOptions }) {
               {
                 sourceFileName: sourceFilename,
                 // Since we're compiling everything to be merged together,
-                // "inline" applies to the final output file, but to the individual
+                // "inline" applies to the final output file, but not to the individual
                 // files being concatenated.
                 sourceMaps:
                   babelOptions.sourceMaps === "inline"
@@ -184,7 +193,6 @@ export default async function({ cliOptions, babelOptions }) {
           }
 
           console.error(err);
-          return null;
         }
       }),
     );
@@ -192,7 +200,7 @@ export default async function({ cliOptions, babelOptions }) {
     output(results);
   }
 
-  async function files(filenames) {
+  async function files(filenames: Array<string>): Promise<void> {
     if (!cliOptions.skipInitialBuild) {
       await walk(filenames);
     }
@@ -208,7 +216,7 @@ export default async function({ cliOptions, babelOptions }) {
             pollInterval: 10,
           },
         })
-        .on("all", function(type, filename) {
+        .on("all", function(type: string, filename: string) {
           if (!util.isCompilableExtension(filename, cliOptions.extensions)) {
             return;
           }
