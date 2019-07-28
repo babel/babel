1508d88393f5697595d54d8a2ab4ec5adeaa223e
Tan Li Hau <lhtan93@gmail.com>
2019-07-28 10:54:07 +0800

    update test fixtures

--- packages/babel-preset-env/test/fixtures/corejs3/usage-for-of-destructure-with/output.mjs
+++ packages/babel-preset-env/test/fixtures/corejs3/usage-for-of-destructure-with/output.mjs
@@ -2,9 +2,8 @@ import "core-js/modules/es.array.iterator";
 import "core-js/modules/es.string.pad-end";
 import "core-js/modules/es.string.pad-start";
 
-for (const _ref of foo) {
-  const {
-    padStart
-  } = _ref;
+for (const {
+  padStart
+} of foo) {
   console.log('b'.padEnd(5));
 }
