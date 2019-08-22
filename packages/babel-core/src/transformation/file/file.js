c3e0152daa3081b3c567008b6fb7b379fdc8be3e
Simen Bekkhus <sbekkhus91@gmail.com>
2019-08-22 14:38:27 +0200

    feat: if code frame error is on a single line, highlight the whole path

--- packages/babel-core/src/transformation/file/file.js
+++ packages/babel-core/src/transformation/file/file.js
@@ -287,6 +287,13 @@ export default class File {
               line: loc.start.line,
               column: loc.start.column + 1,
             },
+            end:
+              loc.start.line === loc.end.line
+                ? {
+                    line: loc.end.line,
+                    column: loc.end.column + 1,
+                  }
+                : undefined,
           },
           { highlightCode },
         );
