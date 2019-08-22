6f6651778a9c33b0ef9dc099ee8d178f7c2b73cc
Huáng Jùnliàng <i@jhuang.me>
2019-08-22 15:48:21 -0400

    make Node 6 CI happy

--- packages/babel-plugin-proposal-slice-notation/test/fixtures/invalid-type/array-buffer/exec.js
+++ packages/babel-plugin-proposal-slice-notation/test/fixtures/invalid-type/array-buffer/exec.js
@@ -1,6 +1,9 @@
 var a = new ArrayBuffer(8);
 expect(() => a[::]).toThrow(/Slice notation only supports array, string and typed array/);
 
-var b = new SharedArrayBuffer(8);
-expect(() => b[::]).toThrow(/Slice notation only supports array, string and typed array/);
+var semver = require("semver");
+if (semver.gte(process.version, "8.10.0")) {
+  var b = new SharedArrayBuffer(8);
+  expect(() => b[::]).toThrow(/Slice notation only supports array, string and typed array/);
+}
 
