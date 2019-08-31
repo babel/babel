5696c94caf6e68e41745812343ca73d721b22625
Shrey Banga <shrey@quip.com>
2019-08-31 14:17:46 -0700

    Handle ArrayPattern

--- packages/babel-parser/src/parser/comments.js
+++ packages/babel-parser/src/parser/comments.js
@@ -138,6 +138,7 @@ export default class CommentsParser extends BaseParser {
           this.adjustCommentsAfterTrailingComma(node, node.arguments);
           break;
         case "ArrayExpression":
+        case "ArrayPattern":
           this.adjustCommentsAfterTrailingComma(node, node.elements);
           break;
       }
