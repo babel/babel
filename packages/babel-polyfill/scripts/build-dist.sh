42e5974fd4dff5338366738cb05dbaf9ff12d38b
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-09-06 19:24:15 +0200

    Remove old uglify option
    It's false by default

--- packages/babel-polyfill/scripts/build-dist.sh
+++ packages/babel-polyfill/scripts/build-dist.sh
@@ -12,6 +12,6 @@ node $BROWSERIFY_CMD lib/index.js \
   --plugin derequire/plugin \
   >dist/polyfill.js
 node $UGLIFY_CMD dist/polyfill.js \
-  --compress keep_fnames,keep_fargs,warnings=false \
+  --compress keep_fnames,keep_fargs \
   --mangle keep_fnames \
   >dist/polyfill.min.js
