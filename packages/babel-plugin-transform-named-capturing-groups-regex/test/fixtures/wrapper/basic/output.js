fbf09b81177c98cf6365fe13b8f276fa5a0b0cec
Huáng Jùnliàng <i@jhuang.me>
2019-09-13 10:00:02 -0400

    chore: restore test fixtures

--- packages/babel-plugin-transform-named-capturing-groups-regex/test/fixtures/wrapper/basic/output.js
+++ packages/babel-plugin-transform-named-capturing-groups-regex/test/fixtures/wrapper/basic/output.js
@@ -1,3 +1,3 @@
-"foo".match(babelHelpers.wrapRegExp(/([\0-\t\x0B\f\x0E-\u2027\u202A-\uFFFF])\1/, {
+"foo".match(babelHelpers.wrapRegExp(/(.)\1/, {
   double: 1
 }));
--- packages/babel-plugin-transform-named-capturing-groups-regex/test/fixtures/wrapper/skips-anonymous-capturing-groups/output.js
+++ packages/babel-plugin-transform-named-capturing-groups-regex/test/fixtures/wrapper/skips-anonymous-capturing-groups/output.js
@@ -1,3 +1,3 @@
-"abba".match(babelHelpers.wrapRegExp(/([\0-\t\x0B\f\x0E-\u2027\u202A-\uFFFF])([\0-\t\x0B\f\x0E-\u2027\u202A-\uFFFF])\2\1/, {
+"abba".match(babelHelpers.wrapRegExp(/(.)(.)\2\1/, {
   n: 2
 }));
