a464a610cdc48830d533e3ce76f5b5e291b8b473
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-09-25 01:56:45 +0200

    Typos [skip ci]
    Co-Authored-By: Huáng Jùnliàng <i@jhuang.me>

--- packages/babel-parser/src/parser/expression.js
+++ packages/babel-parser/src/parser/expression.js
@@ -954,9 +954,9 @@ export default class ExpressionParser extends LValParser {
             // Since "async" is an identifier and normally identifiers
             // can't be followed by expression, the tokenizer assumes
             // that "function" starts a statement.
-            // Fixing it in the tokenizer would mean traking not only the
+            // Fixing it in the tokenizer would mean tracking not only the
             // previous token ("async"), but also the one before to know
-            // if its beforeExpr value.
+            // its beforeExpr value.
             // It's easier and more efficient to adjust the context here.
             throw new Error("Internal error");
           }
