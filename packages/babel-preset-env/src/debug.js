15eac94e0ab6e352a0f2bc4e45239a953e4f53c1
Huáng Jùnliàng <i@jhuang.me>
2019-09-25 15:09:21 -0400

    fix: normalize preset-env filename output

--- packages/babel-preset-env/src/debug.js
+++ packages/babel-preset-env/src/debug.js
@@ -56,6 +56,10 @@ export const logEntryPolyfills = (
   polyfillTargets: Targets,
   allBuiltInsList: { [key: string]: Targets },
 ) => {
+  // normalize filename to generate consistent preset-env test fixtures
+  if (process.env.BABEL_ENV === "test") {
+    filename = filename.replace(/\\/g, "/");
+  }
   if (!importPolyfillIncluded) {
     console.log(`\n[${filename}] Import of ${polyfillName} was not found.`);
     return;
--- packages/babel-preset-env/src/polyfills/regenerator/entry-plugin.js
+++ packages/babel-preset-env/src/polyfills/regenerator/entry-plugin.js
@@ -33,8 +33,13 @@ export default function() {
     },
     post() {
       if (this.opts.debug && this.regeneratorImportExcluded) {
+        let filename = this.file.opts.filename;
+        // normalize filename to generate consistent preset-env test fixtures
+        if (process.env.BABEL_ENV === "test") {
+          filename = filename.replace(/\\/g, "/");
+        }
         console.log(
-          `\n[${this.file.opts.filename}] Based on your targets, regenerator-runtime import excluded.`,
+          `\n[${filename}] Based on your targets, regenerator-runtime import excluded.`,
         );
       }
     },
