ce919197981c4e91445c4ee9c5f634bc5f64e957
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-10-08 19:42:24 +0200

    Move test assertions to the top level to ensure that they run

--- packages/babel-plugin-transform-block-scoping/test/fixtures/general/issue-10339/exec.js
+++ packages/babel-plugin-transform-block-scoping/test/fixtures/general/issue-10339/exec.js
@@ -8,27 +8,31 @@ const code = multiline([
   "}"
 ]);
 
+let programPath;
+let forOfPath;
+let functionPath;
+
 transform(code, {
   configFile: false,
   plugins: [
     "../../../../lib",
     {
       post({ path }) {
+        programPath = path;
         path.traverse({
-          Program(path) {
-            expect(Object.keys(path.scope.bindings)).toEqual(["foo", "bar"]);
-          },
-          ForOfStatement(path) {
-            // for declarations should be transformed to for bindings
-            expect(path.scope.bindings).toEqual({});
-            // The body should be wrapped into closure
-            expect(path.get("body").scope.bindings).toEqual({});
-          },
-          FunctionExpression(path) {
-            expect(Object.keys(path.scope.bindings)).toEqual(["foo", "bar", "qux", "quux"]);
-          }
-        })
+          ForOfStatement(path) { forOfPath = path },
+          FunctionExpression(path) { functionPath = path }
+        });
       }
     }
   ]
 });
+
+expect(Object.keys(programPath.scope.bindings)).toEqual(["foo", "bar"]);
+
+// for declarations should be transformed to for bindings
+expect(forOfPath.scope.bindings).toEqual({});
+// The body should be wrapped into closure
+expect(forOfPath.get("body").scope.bindings).toEqual({});
+
+expect(Object.keys(functionPath.scope.bindings)).toEqual(["foo", "bar", "qux", "quux"]);
