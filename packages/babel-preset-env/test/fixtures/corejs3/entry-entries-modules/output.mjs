272d85d0ad653ca61011a6a7af07c272035cad0e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-10-15 23:46:10 +0200

    Inject core-js@3 imports in Program:exit instead of on post() (#10146)
    * Inject corejs3 imports in Program:exit instead of post
    
    This allows them to be requeued and transformed by other plugins.
    
    * Also entry
    
    * Rebase
    
    * Update packages/babel-preset-env/src/polyfills/corejs3/entry-plugin.js [skip ci]
    
    Co-Authored-By: Brian Ng <bng412@gmail.com>

--- packages/babel-preset-env/test/fixtures/corejs3/entry-entries-modules/output.mjs
+++ packages/babel-preset-env/test/fixtures/corejs3/entry-entries-modules/output.mjs
@@ -1,3 +1,3 @@
-import "core-js/modules/es.symbol";
-import "core-js/modules/es.object.from-entries";
-import "core-js/modules/esnext.string.replace-all";
+import 'core-js/modules/es.symbol';
+import 'core-js/modules/es.object.from-entries';
+import 'core-js/modules/esnext.string.replace-all';
