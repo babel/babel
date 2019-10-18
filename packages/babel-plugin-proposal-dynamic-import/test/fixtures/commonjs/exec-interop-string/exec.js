684fb607166d26b4ad1038368b87e1d9f9193e6b
samuel <kwokyinlun@gmail.com>
2019-10-18 20:48:44 +0800

    fix: [#10561] _interopRequireWildcard should cache object only

---
+++ packages/babel-plugin-proposal-dynamic-import/test/fixtures/commonjs/exec-interop-string/exec.js
@@ -0,0 +1,3 @@
+return import("./mod.js").then(({ default: def }) => {
+  expect(def).toBe("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==");
+});
