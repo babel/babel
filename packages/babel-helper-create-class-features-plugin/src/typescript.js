54aa6e25dd70baab0ebeec145c2ff17d2fe2804b
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-10-22 11:18:51 +0200

    Remove multiple spaces

--- packages/babel-helper-create-class-features-plugin/src/typescript.js
+++ packages/babel-helper-create-class-features-plugin/src/typescript.js
@@ -9,8 +9,8 @@ export function assertFieldTransformed(path: NodePath) {
     throw path.buildCodeFrameError(
       `TypeScript 'declare' fields must first be transformed by ` +
         `@babel/plugin-transform-typescript.\n` +
-        `If you have already enabled  that plugin (or '@babel/preset-typescript'), make sure ` +
-        `that it runs  before any plugin related to additional class features:\n` +
+        `If you have already enabled that plugin (or '@babel/preset-typescript'), make sure ` +
+        `that it runs before any plugin related to additional class features:\n` +
         ` - @babel/plugin-proposal-class-properties\n` +
         ` - @babel/plugin-proposal-private-methods\n` +
         ` - @babel/plugin-proposal-decorators`,
