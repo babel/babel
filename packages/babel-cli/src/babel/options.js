2fd0db89165f057f4deb9674a81e151871bfb251
Mohammad Ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-11-10 02:18:54 +0330

    ran prettier

--- packages/babel-cli/src/babel/options.js
+++ packages/babel-cli/src/babel/options.js
@@ -73,7 +73,10 @@ commander.option(
   "Do not include superfluous whitespace characters and line terminators.",
   booleanify,
 );
-commander.option("--minified [true|false]", "Save as many bytes when printing.");
+commander.option(
+  "--minified [true|false]",
+  "Save as many bytes when printing.",
+);
 commander.option(
   "--auxiliary-comment-before [string]",
   "Print a comment before any injected non-user code.",
