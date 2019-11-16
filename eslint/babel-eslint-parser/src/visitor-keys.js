6659a871e2b4671dc50eb38c2032f14ece4a4f14
Kai Cataldo <kai@kaicataldo.com>
2019-11-16 13:35:45 -0500

    Update ESLint/Prettier config for eslint/* packages

--- eslint/babel-eslint-parser/src/visitor-keys.js
+++ eslint/babel-eslint-parser/src/visitor-keys.js
@@ -7,9 +7,9 @@ module.exports = Object.assign(
   {
     Literal: ESLINT_VISITOR_KEYS.Literal,
     MethodDefinition: ["decorators"].concat(
-      ESLINT_VISITOR_KEYS.MethodDefinition
+      ESLINT_VISITOR_KEYS.MethodDefinition,
     ),
     Property: ["decorators"].concat(ESLINT_VISITOR_KEYS.Property),
   },
-  BABEL_VISITOR_KEYS
+  BABEL_VISITOR_KEYS,
 );
