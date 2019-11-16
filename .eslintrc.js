14de8fb328ff8f291996e1ebe849de88048f3670
Kai Cataldo <kai@kaicataldo.com>
2019-11-16 00:34:53 -0500

    Fix up .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,43 @@
-{
-  "root": true,
-  "plugins": ["prettier", "@babel/development", "import"],
-  "extends": "babel",
-  "rules": {
+module.exports = {
+  root: true,
+  plugins: ["prettier", "@babel/development", "import"],
+  extends: "babel",
+  rules: {
     "prettier/prettier": "error",
-    "max-len": "off"
+    // TODO: remove after babel-eslint-config-internal is fully integrated into this repository.
+    "max-len": "off",
   },
-  "env": {
-    "node": true
+  env: {
+    node: true,
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["packages/*/src/**/*.js", "codemods/*/src/**/*.js"],
-      "rules": {
+      files: ["packages/*/src/**/*.js", "codemods/*/src/**/*.js"],
+      rules: {
         "@babel/development/no-undefined-identifier": "error",
         "@babel/development/no-deprecated-clone": "error",
         "import/no-extraneous-dependencies": "error",
-        "guard-for-in": "error"
-      }
+        "guard-for-in": "error",
+      },
     },
     {
-      "files": [
+      files: [
         "packages/*/test/**/*.js",
         "codemods/*/test/**/*.js",
         "packages/babel-helper-transform-fixture-test-runner/src/helpers.js",
-        "test/**/*.js"
+        "test/**/*.js",
       ],
-      "env": {
-        "jest": true
-      }
+      env: {
+        jest: true,
+      },
     },
     {
-      "files": ["packages/babel-plugin-*/src/index.js"],
-      "excludedFiles": ["packages/babel-plugin-transform-regenerator/**/*.js"],
-      "rules": {
+      files: ["packages/babel-plugin-*/src/index.js"],
+      excludedFiles: ["packages/babel-plugin-transform-regenerator/**/*.js"],
+      rules: {
         "@babel/development/plugin-name": "error",
-        "eqeqeq": ["error", "always", { "null": "ignore" }]
-      }
-    }
-  ]
-}
+        eqeqeq: ["error", "always", { null: "ignore" }],
+      },
+    },
+  ],
+};
