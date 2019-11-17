e9e6918df6eeefc56f0deab9f7710de67f7c97d3
Kai Cataldo <kai@kaicataldo.com>
2019-11-16 19:21:00 -0500

    Do not run eslint/* tests in Node v6

---
+++ jest.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  collectCoverageFrom: [
+    "packages/*/src/**/*.mjs",
+    "packages/*/src/**/*.js",
+    "codemods/*/src/**/*.mjs",
+    "codemods/*/src/**/*.js",
+    "eslint/*/src/**/*.mjs",
+    "eslint/*/src/**/*.js",
+  ],
+  // The eslint/* packages use ESLint v6, which has dropped support for Node v6.
+  testRegex: `./(packages|codemods${
+    /^v6./u.test(process.version) ? "" : "|eslint"
+  })/[^/]+/test/.+\\.m?js$`,
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/test/fixtures/",
+    "/test/debug-fixtures/",
+    "/babel-parser/test/expressions/",
+    "/test/tmp/",
+    "/test/__data__/",
+    "/test/helpers/",
+    "<rootDir>/test/warning\\.js",
+    "<rootDir>/build/",
+    "_browser\\.js",
+  ],
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/test/testSetupFile.js"],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/packages/babel-standalone/babel(\\.min)?\\.js",
+    "<rootDir>/packages/babel-preset-env-standalone/babel-preset-env(\\.min)?\\.js",
+    "/test/(fixtures|tmp|__data__)/",
+    "<rootDir>/(packages|codemods|eslint)/[^/]+/lib/",
+  ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/packages/babel-standalone/babel(\\.min)?\\.js",
+    "<rootDir>/packages/babel-preset-env-standalone/babel-preset-env(\\.min)?\\.js",
+    "/test/(fixtures|tmp|__data__)/",
+  ],
+  modulePathIgnorePatterns: [
+    "/test/fixtures/",
+    "/test/tmp/",
+    "/test/__data__/",
+    "<rootDir>/build/",
+  ],
+  moduleNameMapper: {
+    "^@babel/([a-zA-Z0-9_-]+)$": "<rootDir>/packages/babel-$1/",
+  },
+};
