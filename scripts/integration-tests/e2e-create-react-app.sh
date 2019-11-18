19dc1ab50ec0888fa6b61dd7f195cd552bbf1739
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-11-18 21:46:42 +0100

    Fix

--- scripts/integration-tests/e2e-create-react-app.sh
+++ scripts/integration-tests/e2e-create-react-app.sh
@@ -21,7 +21,7 @@ cd tmp/create-react-app
 #                                   TEST                                       #
 #==============================================================================#
 
-startLocalRegistry "$PWD"/../../../../scripts/integration-tests/verdaccio-config.yml
+startLocalRegistry "$PWD"/../../verdaccio-config.yml
 yarn install
 # "yarn upgrade --scope @babel --latest" doesn't seem to work.
 # a means "all", while \n is the enter needed to confirm the selection.
