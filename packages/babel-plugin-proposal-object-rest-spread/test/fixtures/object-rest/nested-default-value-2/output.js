eb52bfb29a27c3baf4238f3e661aaad91e75dfe6
Huáng Jùnliàng <jlhwung@gmail.com>
2019-11-19 17:50:23 -0500

    chore: rename test case

--- packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-rest/nested-default-value-2/output.js
+++
@@ -1,9 +0,0 @@
-const {
-  InputProps: {
-    classes
-  } = {
-    foo: 1
-  }
-} = props,
-      InputPropsOther = babelHelpers.objectWithoutProperties(props.InputProps, ["classes"]),
-      other = babelHelpers.objectWithoutProperties(props, ["InputProps"]);
