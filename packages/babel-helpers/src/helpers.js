52d95c5bea912c3ba301d02866b8e46301ce9fca
Haroen Viaene <fingebimus@me.com>
2019-11-27 17:25:05 +0100

    fix(helper): update spelling of _assertThisInitialized helper
    I have not updated the tests yet, since I'm not sure if this is something worth changing. Let me know if it's worth it and I'll clone the repository and update all the tests (28 instances)

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -713,7 +713,7 @@ helpers.objectWithoutProperties = helper("7.0.0-beta.0")`
 helpers.assertThisInitialized = helper("7.0.0-beta.0")`
   export default function _assertThisInitialized(self) {
     if (self === void 0) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+      throw new ReferenceError("this hasn't been initialized - super() hasn't been called");
     }
     return self;
   }
