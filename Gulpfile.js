22ad3df22bccaa94d325ce154bf8bcb23201e6d4
Huáng Jùnliàng <jlhwung@gmail.com>
2019-11-27 10:58:49 -0500

    chore: farewell webpack

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,7 +10,6 @@ const fancyLog = require("fancy-log");
 const filter = require("gulp-filter");
 const gulp = require("gulp");
 const path = require("path");
-const webpack = require("webpack");
 const rollup = require("rollup");
 const rollupAlias = require("@rollup/plugin-alias");
 const rollupBabel = require("rollup-plugin-babel");
@@ -21,7 +20,6 @@ const rollupNodeBuiltins = require("rollup-plugin-node-builtins");
 const rollupNodeGlobals = require("rollup-plugin-node-globals");
 const rollupNodeResolve = require("rollup-plugin-node-resolve");
 const rollupReplace = require("rollup-plugin-replace");
-const { registerStandalonePackageTask } = require("./scripts/gulp-tasks");
 
 const defaultSourcesGlob = "./@(codemods|packages|eslint)/*/src/**/*.js";
 
@@ -215,9 +213,9 @@ const presetEnvStandaloneBundle = [
 ];
 
 gulp.task("build-rollup", () => buildRollup(libBundles));
-gulp.task("build-babel-standalone-rollup", () => buildRollup(standaloneBundle));
+gulp.task("build-babel-standalone", () => buildRollup(standaloneBundle));
 
-gulp.task("build-babel-preset-env-standalone-rollup", () =>
+gulp.task("build-babel-preset-env-standalone", () =>
   buildRollup(presetEnvStandaloneBundle)
 );
 
@@ -240,42 +238,4 @@ gulp.task(
       gulp.task("build-no-bundle")
     );
   })
-);
-
-registerStandalonePackageTask(
-  gulp,
-  "babel",
-  "Babel",
-  path.join(__dirname, "packages"),
-  require("./packages/babel-standalone/package.json").version
-);
-
-const presetEnvWebpackPlugins = [
-  new webpack.NormalModuleReplacementPlugin(
-    /\.\/available-plugins/,
-    require.resolve(
-      path.join(
-        __dirname,
-        "./packages/babel-preset-env-standalone/src/available-plugins"
-      )
-    )
-  ),
-  new webpack.NormalModuleReplacementPlugin(
-    /caniuse-lite\/data\/regions\/.+/,
-    require.resolve(
-      path.join(
-        __dirname,
-        "./packages/babel-preset-env-standalone/src/caniuse-lite-regions"
-      )
-    )
-  ),
-];
-
-registerStandalonePackageTask(
-  gulp,
-  "babel-preset-env",
-  "babelPresetEnv",
-  path.join(__dirname, "packages"),
-  require("./packages/babel-preset-env-standalone/package.json").version,
-  presetEnvWebpackPlugins
 );
