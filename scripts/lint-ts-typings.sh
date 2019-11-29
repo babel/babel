76fb9b8cb5b11e24f3877e37df164f3d3ba7ea42
Huáng Jùnliàng <jlhwung@gmail.com>
2019-11-28 20:18:10 -0500

    chore: fix tsc command

--- scripts/lint-ts-typings.sh
+++ scripts/lint-ts-typings.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-node="node"
 tsFlags="--strict"
 
-$node ./node_modules/typescript/bin/tsc $tsFlags ./packages/babel-types/lib/index.d.ts
+yarn tsc $tsFlags ./packages/babel-types/lib/index.d.ts
