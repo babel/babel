e87beb21478b1abf6b5748bd6f41ed2de693e67f
James Beavers <james.beavers@lifeomic.com>
2019-12-05 11:03:57 -0500

    enforce sort-keys on shipped-proposals

--- packages/babel-preset-env/data/shipped-proposals.js
+++ packages/babel-preset-env/data/shipped-proposals.js
@@ -1,17 +1,20 @@
+/* eslint sort-keys: "error" */
 // These mappings represent the syntax proposals that have been
 // shipped by browsers, and are enabled by the `shippedProposals` option.
 
 const proposalPlugins = {};
 
-// Please keep these in alphabetical order!
-const pluginSyntaxMap = new Map([
-  ["proposal-async-generator-functions", "syntax-async-generators"],
-  ["proposal-json-strings", "syntax-json-strings"],
-  ["proposal-nullish-coalescing-operator", "syntax-nullish-coalescing-operator"],
-  ["proposal-object-rest-spread", "syntax-object-rest-spread"],
-  ["proposal-optional-catch-binding", "syntax-optional-catch-binding"],
-  ["proposal-optional-chaining", "syntax-optional-chaining"],
-  ["proposal-unicode-property-regex", null],
-]);
+// use intermediary object to enforce alphabetical key order
+const pluginSyntaxObject = {
+  "proposal-async-generator-functions": "syntax-async-generators",
+  "proposal-json-strings": "syntax-json-strings",
+  "proposal-nullish-coalescing-operator": "syntax-nullish-coalescing-operator",
+  "proposal-object-rest-spread": "syntax-object-rest-spread",
+  "proposal-optional-catch-binding": "syntax-optional-catch-binding",
+  "proposal-optional-chaining": "syntax-optional-chaining",
+  "proposal-unicode-property-regex": null,
+};
 
-module.exports = { proposalPlugins, pluginSyntaxMap };
+const pluginSyntaxMap = new Map(Object.entries(pluginSyntaxObject));
+
+module.exports = { pluginSyntaxMap, proposalPlugins };
