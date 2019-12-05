2b472912e4a0f0994193d07a476512cc47ffdece
Huáng Jùnliàng <jlhwung@gmail.com>
2019-12-04 22:28:25 -0500

    Tune makefile scripts (#10781)
    * chore: replace node by `yarn node`
    
    * chore: fix tsc command
    
    * chore: use `yarn` to wrap node
    
    * chore: supress yarn run output
    
    * chore: disable silent mode for builtin yarn command

--- scripts/test-cov.sh
+++ scripts/test-cov.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
+node="yarn --silent node"
 jestArgs="--coverage"
 
 if [ -n "$CI" ]; then
   jestArgs="${jestArgs} --maxWorkers=4 --ci"
 fi
 
-node_modules/.bin/jest $jestArgs
+$node node_modules/.bin/jest $jestArgs
