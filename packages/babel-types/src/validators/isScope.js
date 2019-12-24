fc5365fe4a3b9acf7116d63df6a23193e5a40ed0
Huáng Jùnliàng <jlhwung@gmail.com>
2019-12-24 15:18:46 -0500

    Fix parameter expression get binding (#10912)
    * fix: parameter expression closure should not have access to the declaration inside function body
    
    * fix: renameVisitor should skip when a pattern is a scope
    
    * address review comments

--- packages/babel-types/src/validators/isScope.js
+++ packages/babel-types/src/validators/isScope.js
@@ -4,6 +4,7 @@ import {
   isCatchClause,
   isBlockStatement,
   isScopable,
+  isPattern,
 } from "./generated";
 
 /**
@@ -16,6 +17,12 @@ export default function isScope(node: Object, parent: Object): boolean {
 
   if (isBlockStatement(node) && isCatchClause(parent, { body: node })) {
     return false;
+  }
+
+  // If a Pattern is an immediate descendent of a Function, it must be in the params.
+  // Hence we skipped the parentKey === "params" check
+  if (isPattern(node) && isFunction(parent)) {
+    return true;
   }
 
   return isScopable(node);
