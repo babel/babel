c349a20e1c598e4e5e47cae6f0bf41cbd6048879
wetinee <wetinee0908@gmail.com>
2020-01-09 11:15:56 +0800

    add plugin description

--- packages/babel-compat-data/scripts/data/plugin-features.js
+++ packages/babel-compat-data/scripts/data/plugin-features.js
@@ -99,6 +99,7 @@ const es2017 = {
 
 const es2018 = {
   "proposal-async-generator-functions": "Asynchronous Iterators",
+  "proposal-numeric-separator": "numeric separator",
   "proposal-object-rest-spread": "object rest/spread properties",
 
   "transform-dotall-regex": "s (dotAll) flag for regular expressions",
