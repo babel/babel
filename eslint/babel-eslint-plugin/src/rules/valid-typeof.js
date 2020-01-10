4cb214827319738bcf6f54812af1398f1aac44c7
Kai Cataldo <kai@kaicataldo.com>
2020-01-10 14:22:39 -0500

    @babel/eslint-plugin: remove deprecated rules (#10975)

--- eslint/babel-eslint-plugin/src/rules/valid-typeof.js
+++
@@ -1,8 +0,0 @@
-import ruleComposer from "eslint-rule-composer";
-import eslint from "eslint";
-
-const validTypeOf = new eslint.Linter().getRules().get("valid-typeof");
-
-export default ruleComposer.filterReports(validTypeOf, problem => {
-  return problem.node.value !== "bigint";
-});
