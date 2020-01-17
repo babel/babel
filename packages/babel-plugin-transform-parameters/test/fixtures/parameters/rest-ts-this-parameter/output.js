85ddc297c2643b1dbc32527ef672c03135a3dd52
Benoît Zugmeyer <bzugmeyer@gmail.com>
2020-01-17 21:00:37 +0100

    Fix rest parameters indexing with TypeScript 'this parameter' (#9714)
    * Fix rest parameters indexing with TypeScript 'this parameter'
    
    The TypeScript [this parameter][0] is a fake parameter and should not be
    taken into account when counting the function parameters.  This patch
    skips it by using the condition taken from the `transform-typescript`
    plugin.
    
    Note: since the `transform-typescript` plugin is removing this kind of
    parameter, including it before the `transform-parameters` plugin solves
    the issue.  This patch fixes the issue in other cases.
    
    [0]: https://www.typescriptlang.org/docs/handbook/functions.html#this-parameters
    
    * nit: improve the 'this parameter' detection condition
    
    * improve performance by checking the parameter list length before
    accessing it
    * simplify the test a bit by using the `isIdentifier` second
    parameter

---
+++ packages/babel-plugin-transform-parameters/test/fixtures/parameters/rest-ts-this-parameter/output.js
@@ -0,0 +1,11 @@
+function u(this: Foo) {
+  arguments.length <= 0 ? undefined : arguments[0];
+}
+
+function v(this: Foo, event: string) {
+  for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    args[_key - 1] = arguments[_key];
+  }
+
+  args;
+}
