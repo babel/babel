5bdbe2877b5895c3bf7f88f12dcc29cdd493ef08
vedantroy <vroy101@gmail.com>
2020-01-31 02:04:28 -0500

    Refresh test output

--- packages/babel-plugin-transform-modules-commonjs/test/fixtures/misc/for-of-in-export/output.js
+++ packages/babel-plugin-transform-modules-commonjs/test/fixtures/misc/for-of-in-export/output.js
@@ -5,55 +5,58 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.bar = exports.foo = void 0;
 let foo;
-exports.foo = foo;
-let bar;
-exports.bar = bar;
+exports.bar = exports.foo = foo;
 
 for (let _foo of []) {
-  exports.foo = foo = _foo
+  exports.bar = exports.foo = foo = _foo
 }
 
-for (let _foo2 in {}) {
-  exports.foo = foo = _foo2
+for (let _foo2 in []) {
+  exports.bar = exports.foo = foo = _foo2
 }
 
-for (let {
-  foo: _foo3
-} of {}) {
-  exports.foo = foo = _foo3
+for (let _foo3 of []) {
+  exports.bar = exports.foo = _foo3
+  let foo = 42;
 }
 
-for (let {
+let _foo4;
+
+for ({
   foo: _foo4
-} of {}) {
-  exports.foo = _foo4
-  let foo = 3;
+} of []) {
+  exports.bar = exports.foo = foo = _foo4
 }
 
-for (let {
+let qux;
+
+let _foo5;
+
+for ({
   foo: _foo5,
-  bar: _bar
-} of {}) {
-  exports.foo = foo = _foo5
-  exports.bar = bar = _bar
-}
-
-for (let {
-  foo: _foo6,
-  bar: _bar2
-} of {}) {
-  exports.foo = foo = _foo6
-  exports.bar = _bar2
-  let bar = 3;
-}
-
-for (let _foo7 of []) {
-  exports.foo = _foo7
-  let foo = 3;
+  bar,
+  qux
+} of []) {
+  exports.bar = exports.foo = foo = _foo5
 }
 
 {
-  let foo = 3;
+  let foo;
 
   for (foo of []) {}
+
+  for ([foo] of []) {}
+}
+
+let _foo6;
+
+for ([_foo6, qux, [_foo6, ..._foo6]] of []) {
+  exports.bar = exports.foo = foo = _foo6
+}
+
+let _foo7;
+
+for ([_foo7, qux, [_foo7, ..._foo7]] of []) {
+  exports.bar = exports.foo = _foo7
+  let foo = 42;
 }
