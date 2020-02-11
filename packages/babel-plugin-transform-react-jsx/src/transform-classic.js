bfe6783ff09798fc49dce05c46d2bb65e66ce6ba
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-02-11 12:41:27 +0100

    Mark transpiled JSX elements as pure

--- packages/babel-plugin-transform-react-jsx/src/transform-classic.js
+++ packages/babel-plugin-transform-react-jsx/src/transform-classic.js
@@ -3,12 +3,18 @@ import jsx from "@babel/plugin-syntax-jsx";
 import helper from "@babel/helper-builder-react-jsx";
 import { types as t } from "@babel/core";
 
+const DEFAULT = {
+  pragma: "React.createElement",
+  pragmaFrag: "React.Fragment",
+};
+
 export default declare((api, options) => {
   const THROW_IF_NAMESPACE =
     options.throwIfNamespace === undefined ? true : !!options.throwIfNamespace;
 
-  const PRAGMA_DEFAULT = options.pragma || "React.createElement";
-  const PRAGMA_FRAG_DEFAULT = options.pragmaFrag || "React.Fragment";
+  const PRAGMA_DEFAULT = options.pragma || DEFAULT.pragma;
+  const PRAGMA_FRAG_DEFAULT = options.pragmaFrag || DEFAULT.pragmaFrag;
+  const PURE_ANNOTATION = options.pure;
 
   const JSX_ANNOTATION_REGEX = /\*?\s*@jsx\s+([^\s]+)/;
   const JSX_FRAG_ANNOTATION_REGEX = /\*?\s*@jsxFrag\s+([^\s]+)/;
@@ -35,6 +41,7 @@ export default declare((api, options) => {
 
     post(state, pass) {
       state.callee = pass.get("jsxIdentifier")();
+      state.pure = PURE_ANNOTATION ?? !pass.get("pragmaSet");
     },
 
     throwIfNamespace: THROW_IF_NAMESPACE,
@@ -46,20 +53,16 @@ export default declare((api, options) => {
 
       let pragma = PRAGMA_DEFAULT;
       let pragmaFrag = PRAGMA_FRAG_DEFAULT;
-      let pragmaSet = !!options.pragma;
-      let pragmaFragSet = !!options.pragmaFrag;
 
       if (file.ast.comments) {
         for (const comment of (file.ast.comments: Array<Object>)) {
           const jsxMatches = JSX_ANNOTATION_REGEX.exec(comment.value);
           if (jsxMatches) {
             pragma = jsxMatches[1];
-            pragmaSet = true;
           }
           const jsxFragMatches = JSX_FRAG_ANNOTATION_REGEX.exec(comment.value);
           if (jsxFragMatches) {
             pragmaFrag = jsxFragMatches[1];
-            pragmaFragSet = true;
           }
         }
       }
@@ -67,8 +70,8 @@ export default declare((api, options) => {
       state.set("jsxIdentifier", createIdentifierParser(pragma));
       state.set("jsxFragIdentifier", createIdentifierParser(pragmaFrag));
       state.set("usedFragment", false);
-      state.set("pragmaSet", pragmaSet);
-      state.set("pragmaFragSet", pragmaFragSet);
+      state.set("pragmaSet", pragma !== DEFAULT.pragma);
+      state.set("pragmaFragSet", pragmaFrag !== DEFAULT.pragmaFrag);
     },
     exit(path, state) {
       if (
