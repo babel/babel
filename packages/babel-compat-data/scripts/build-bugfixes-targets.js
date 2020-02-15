cd06d59a4eec9c0fbcb54687794faefe47c4928d
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-02-15 17:04:36 +0100

    Fix build script

--- packages/babel-compat-data/scripts/build-bugfixes-targets.js
+++ packages/babel-compat-data/scripts/build-bugfixes-targets.js
@@ -2,13 +2,7 @@
 
 // NOTE: This script must be run _after_ build-data.js
 
-const semver = require("semver");
 const path = require("path");
-
-// TODO: This is an internal file of a separate package
-const {
-  semverify,
-} = require("../../babel-helper-compilation-targets/lib/utils");
 
 const {
   getLowestImplementedVersion,
@@ -23,50 +17,51 @@ const pluginFeatures = require("./data/plugin-features");
 const generatedTargets = {};
 const overlappingPlugins = {};
 
+const allReplacedFeatures = {};
+
 const has = Function.call.bind(Object.hasOwnProperty);
 
 for (const [plugin, { replaces, features }] of Object.entries(data)) {
   if (!has(overlappingPlugins, replaces)) {
     overlappingPlugins[replaces] = [];
     generatedTargets[replaces] = {};
+    allReplacedFeatures[replaces] = [];
   }
   generatedTargets[plugin] = {};
 
-  let replacedFeatures = pluginFeatures[replaces];
-  if (!Array.isArray(replacedFeatures)) {
-    replacedFeatures = replacedFeatures.features;
-  }
+  allReplacedFeatures[replaces].push(...features);
+
+  overlappingPlugins[replaces].push(plugin);
 
   for (const env of environments) {
     const supportedWithBugfix = getLowestImplementedVersion({ features }, env);
     if (supportedWithBugfix) {
       generatedTargets[plugin][env] = supportedWithBugfix;
     }
+  }
+}
 
+for (const [replaced, features] of Object.entries(allReplacedFeatures)) {
+  let replacedFeatures = pluginFeatures[replaced];
+  if (!Array.isArray(replacedFeatures)) {
+    replacedFeatures = replacedFeatures.features;
+  }
+
+  for (const env of environments) {
     const stillNotSupported = getLowestImplementedVersion(
       { features: replacedFeatures },
       env,
       name => features.some(feat => name.includes(feat))
     );
-    if (!stillNotSupported) continue;
 
-    if (
-      !has(generatedTargets[replaces], env) ||
-      compare(stillNotSupported, generatedTargets[replaces][env]) == -1
-    ) {
-      generatedTargets[replaces][env] = stillNotSupported;
+    if (stillNotSupported) {
+      generatedTargets[replaced][env] = stillNotSupported;
     }
   }
-
-  overlappingPlugins[replaces].push(plugin);
 }
 
 for (const plugin of Object.values(generatedTargets)) {
   addOperaAndElectron(plugin);
-}
-
-function compare(a, b) {
-  return semver.compare(semverify(a), semverify(b));
 }
 
 for (const [filename, data] of [
