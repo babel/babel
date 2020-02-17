cd41cc9165aa920e5ec9af1cf38762830e21ce44
Tan Li Hau <tanhauhau@users.noreply.github.com>
2020-02-18 02:20:40 +0800

    Remove jsonCompatibleStrings option (#9958)

--- packages/babel-generator/src/generators/types.js
+++ packages/babel-generator/src/generators/types.js
@@ -205,9 +205,6 @@ export function StringLiteral(node: Object) {
 
   // ensure the output is ASCII-safe
   const opts = this.format.jsescOption;
-  if (this.format.jsonCompatibleStrings) {
-    opts.json = true;
-  }
   const val = jsesc(node.value, opts);
 
   return this.token(val);
