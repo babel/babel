e4604dae5ffe738a400ba42c2b0755980170c10a
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-02-27 21:49:36 +0100

    Use Yarn 2 (#11167)
    * Update husky
    * Update Makefile
    * Update CI config
    * Update e2e and utils bash scripts
    * Patch lerna
    * Directly spawn node. This won't work with PnP
    * Don't make lerna ignore codemods and eslint packages
    * Add yarn.lock
    * Use process.execPath
    * Updates the e2e upgrade
    * Updates build
    * Update e2e old babel
    * Fix e2e babel old
    * Simplify e2e old babel script
    * Update lockfile
    * Update preset-env fixtures
    * Remove nested lock files
    * Fix codesandbox

--- scripts/integration-tests/e2e-babel-old-version.sh
+++ scripts/integration-tests/e2e-babel-old-version.sh
@@ -21,17 +21,26 @@ cd ../..
 #==============================================================================#
 
 startLocalRegistry "$PWD"/scripts/integration-tests/verdaccio-config.yml
-yarn upgrade --scope @babel
 
-node -e "\
-  var pkg = require('./package.json');\
-  pkg.devDependencies['@babel/core'] = '7.0.0';\
-  Object.assign(pkg.resolutions, {\
-    '@babel/core': '7.0.0',\
-    '@babel/helpers': '7.0.0',\
-    '@babel/traverse': '7.0.0'\
-  });\
-  fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2));\
+node "$PWD"/scripts/integration-tests/utils/bump-babel-dependencies.js
+(
+  yarn why @babel/core | grep -o "@babel/core@npm:.* (via npm:.*)";
+  yarn why @babel/helpers | grep -o "@babel/helpers@npm:.* (via npm:.*)";
+  yarn why @babel/traverse | grep -o "@babel/traverse@npm:.* (via npm:.*)"
+) | uniq | node -e "
+  var pkg = require('./package.json');
+  var packages = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+  pkg.devDependencies['@babel/core'] = '7.0.0';
+
+  packages.forEach(desc => {
+    const { name, specifier } = desc
+      .match(/(?<name>@babel\/[a-z]+).*\(via (?<specifier>npm:[^)]+)\)/)
+      .groups;
+    pkg.resolutions[name + '@' + specifier] = '7.0.0';
+  });
+
+  fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2));
 "
 
 rm yarn.lock
