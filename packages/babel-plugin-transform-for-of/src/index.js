eda6528957cc2e585dab9901bdbbf312d5cdd88e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-03-15 01:09:15 +0100

    Add "allowArrayLike" support to the for-of transform

--- packages/babel-plugin-transform-for-of/src/index.js
+++ packages/babel-plugin-transform-for-of/src/index.js
@@ -6,11 +6,17 @@ import transformWithoutHelper from "./no-helper-implementation";
 export default declare((api, options) => {
   api.assertVersion(7);
 
-  const { loose, assumeArray } = options;
+  const { loose, assumeArray, allowArrayLike } = options;
 
   if (loose === true && assumeArray === true) {
     throw new Error(
       `The loose and assumeArray options cannot be used together in @babel/plugin-transform-for-of`,
+    );
+  }
+
+  if (assumeArray === true && allowArrayLike === true) {
+    throw new Error(
+      `The assumeArray and allowArrayLike options cannot be used together in @babel/plugin-transform-for-of`,
     );
   }
 
@@ -86,12 +92,12 @@ export default declare((api, options) => {
   `);
 
   const buildForOfLoose = template.statements(`
-    for (var ITERATOR_HELPER = CREATE_ITERATOR_HELPER(OBJECT), STEP_KEY;
+    for (var ITERATOR_HELPER = CREATE_ITERATOR_HELPER(OBJECT, ALLOW_ARRAY_LIKE), STEP_KEY;
         !(STEP_KEY = ITERATOR_HELPER()).done;) BODY;
   `);
 
   const buildForOf = template.statements(`
-    var ITERATOR_HELPER = CREATE_ITERATOR_HELPER(OBJECT), STEP_KEY;
+    var ITERATOR_HELPER = CREATE_ITERATOR_HELPER(OBJECT, ALLOW_ARRAY_LIKE), STEP_KEY;
     try {
       for (ITERATOR_HELPER.s(); !(STEP_KEY = ITERATOR_HELPER.n()).done;) BODY;
     } catch (err) {
@@ -200,6 +206,7 @@ export default declare((api, options) => {
         const nodes = builder.build({
           CREATE_ITERATOR_HELPER: state.addHelper(builder.helper),
           ITERATOR_HELPER: scope.generateUidIdentifier("iterator"),
+          ALLOW_ARRAY_LIKE: allowArrayLike ? t.booleanLiteral(true) : null,
           STEP_KEY: t.identifier(stepKey),
           OBJECT: node.right,
           BODY: node.body,
---
+++ packages/babel-plugin-transform-for-of/test/fixtures/spec-allowArrayLike/simple/output.js
@@ -0,0 +1,13 @@
+var _iterator = babelHelpers.createForOfIteratorHelper(p2, true),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var x = _step.value;
+    arr.push(x);
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
