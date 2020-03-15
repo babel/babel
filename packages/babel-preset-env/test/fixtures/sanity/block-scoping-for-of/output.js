3e4f3bdc9554f176c1be50e3a66b564d969f0f4a
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-03-15 16:33:19 +0100

    Allow rest/spread on polyfilled or builtin iterables when `Symbol` unsupported
    Currently, when `Symbol` is not supported, we allow using rest/spread with:
    - arrays
    - strings
    - arguments
    
    With this PR, it will be also possible to use it with
    - maps
    - sets
    - binary arrays
    
    While in old browsers es6 builtins would still need to be polyfilled, it's way
    easier to polyfill them because `Symbol` cannot be reliably polyfilled.
    
    I didn't use `instanceof` becase:
    - it doesn't work with polyfills not attatched to the global scope
    - when using Babel to load polyfills, it would force the inclusion of `Map` and `Set` polyfills even if they are not used
    
    Downside: the current approach of relying on `toString || construcor.name` doesn't work with subclasses.

--- packages/babel-preset-env/test/fixtures/sanity/block-scoping-for-of/output.js
+++ packages/babel-preset-env/test/fixtures/sanity/block-scoping-for-of/output.js
@@ -1,8 +1,10 @@
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-function _iterableToArrayLimit(arr, i) { if ((typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) && Object.prototype.toString.call(arr) !== "[object Arguments]") return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _unsupportedIterableToArray(o) { if (!o) return; if (typeof o === "string") return Array.from(o); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Arguments" || n === "Map" || n === "Set" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return Array.from(o); }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
--- packages/babel-runtime/helpers/toArray.js
+++ packages/babel-runtime/helpers/toArray.js
@@ -2,10 +2,12 @@ var arrayWithHoles = require("./arrayWithHoles");
 
 var iterableToArray = require("./iterableToArray");
 
+var unsupportedIterableToArray = require("./unsupportedIterableToArray");
+
 var nonIterableRest = require("./nonIterableRest");
 
 function _toArray(arr) {
-  return arrayWithHoles(arr) || iterableToArray(arr) || nonIterableRest();
+  return arrayWithHoles(arr) || iterableToArray(arr) || unsupportedIterableToArray(arr) || nonIterableRest();
 }
 
 module.exports = _toArray;
