fceb4877b14063719899844754be58ff668ccc59
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-03-19 16:54:44 +0100

    Add test

---
+++ packages/babel-plugin-transform-react-jsx-development/test/fixtures/windows/self-inside-arrow-windows/output.mjs
@@ -0,0 +1,36 @@
+import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+
+var _jsxFileName = "C:\\Users\\travis\\build\\babel\\babel\\packages\\babel-plugin-transform-react-jsx-development\\test\\fixtures\\windows\\self-inside-arrow\\input.mjs",
+    _this = this;
+
+_jsxDEV("div", {}, void 0, false, {
+  fileName: _jsxFileName,
+  lineNumber: 1,
+  columnNumber: 1
+}, this);
+
+(function () {
+  return _jsxDEV("div", {}, void 0, false, {
+    fileName: _jsxFileName,
+    lineNumber: 2,
+    columnNumber: 7
+  }, _this);
+});
+
+function fn() {
+  var _this2 = this;
+
+  _jsxDEV("div", {}, void 0, false, {
+    fileName: _jsxFileName,
+    lineNumber: 5,
+    columnNumber: 3
+  }, this);
+
+  (function () {
+    return _jsxDEV("div", {}, void 0, false, {
+      fileName: _jsxFileName,
+      lineNumber: 6,
+      columnNumber: 9
+    }, _this2);
+  });
+}
