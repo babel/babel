f37a3b9eef7aebe54e288f80ffcb62df0d396242
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-03-19 21:25:29 +0100

    Pass the correct `this` to JSX's `__self` with the old plugin

---
+++ packages/babel-preset-react/test/fixtures/preset-options/development-windows/output.js
@@ -0,0 +1,12 @@
+var _jsxFileName = "C:\\Users\\travis\\build\\babel\\babel\\packages\\babel-preset-react\\test\\fixtures\\preset-options\\development-windows\\input.js";
+
+/*#__PURE__*/
+React.createElement(Foo, {
+  bar: "baz",
+  __self: this,
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 1,
+    columnNumber: 1
+  }
+});
