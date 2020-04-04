ae1aa9c6e361b9711c69e034900a2afc7a2bd70c
Justin Ridgewell <justin@ridgewell.name>
2020-04-03 21:51:30 -0400

    Update PrivateInExpectedIn message
    Co-Authored-By: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>

--- packages/babel-parser/src/parser/location.js
+++ packages/babel-parser/src/parser/location.js
@@ -125,7 +125,7 @@ export const Errors = Object.freeze({
     "Topic reference was used in a lexical context without topic binding",
   PrimaryTopicRequiresSmartPipeline:
     "Primary Topic Reference found but pipelineOperator not passed 'smart' for 'proposal' option.",
-  PrivateInExpectedIn: "Expected `in` keyword to follow private name",
+  PrivateInExpectedIn: "Private names are only allowed in property accesses (`obj.#%0`) or in `in` expressions (`#%0 in obj`)",
   PrivateNameRedeclaration: "Duplicate private name #%0",
   RecordExpressionBarIncorrectEndSyntaxType:
     "Record expressions ending with '|}' are only allowed when the 'syntaxType' option of the 'recordAndTuple' plugin is set to 'bar'",
