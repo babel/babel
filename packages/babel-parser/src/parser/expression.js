a97c83354991892e44f6b3b3192d02813791ebb5
Huáng Jùnliàng <jlhwung@gmail.com>
2020-04-07 21:05:59 -0400

    fix: do not push new token context when function is following dot/questionDot

--- packages/babel-parser/src/parser/expression.js
+++ packages/babel-parser/src/parser/expression.js
@@ -2125,17 +2125,12 @@ export default class ExpressionParser extends LValParser {
     } else if (this.state.type.keyword) {
       name = this.state.type.keyword;
 
-      // `class` and `function` keywords push new context into this.context.
+      // `class` and `function` keywords push function-type token context into this.context.
       // But there is no chance to pop the context if the keyword is consumed
       // as an identifier such as a property name.
-      // If the previous token is a dot, this does not apply because the
-      // context-managing code already ignored the keyword
-      if (
-        (name === "class" || name === "function") &&
-        (this.state.lastTokEnd !== this.state.lastTokStart + 1 ||
-          this.input.charCodeAt(this.state.lastTokStart) !== charCodes.dot)
-      ) {
-        this.state.context.pop();
+      const context = this.state.context;
+      if (context[context.length - 1].token === "function") {
+        context.pop();
       }
     } else {
       throw this.unexpected();
