9b48a8ead739b1cb786ab03a8148b7d96c17b295
Justin Ridgewell <justin@ridgewell.name>
2020-04-14 15:51:51 -0400

    Fix nested classes reference private fields (#11405)
    * Fix nested classes reference private fields
    
    * Process only visible private fields when redeclaring
    
    * Comments
    
    * Skip class traversal if there are no private fields
    
    * Handle redeclared private field in computed key

---
+++ packages/babel-plugin-proposal-class-properties/test/fixtures/private-loose/nested-class/output.js
@@ -0,0 +1,35 @@
+var Foo = /*#__PURE__*/function () {
+  "use strict";
+
+  function Foo() {
+    babelHelpers.classCallCheck(this, Foo);
+    Object.defineProperty(this, _foo, {
+      writable: true,
+      value: 1
+    });
+  }
+
+  babelHelpers.createClass(Foo, [{
+    key: "test",
+    value: function test() {
+      var Nested = /*#__PURE__*/function () {
+        function Nested() {
+          babelHelpers.classCallCheck(this, Nested);
+        }
+
+        babelHelpers.createClass(Nested, [{
+          key: "test",
+          value: function test() {
+            babelHelpers.classPrivateFieldLooseBase(this, _foo)[_foo];
+          }
+        }]);
+        return Nested;
+      }();
+
+      babelHelpers.classPrivateFieldLooseBase(this, _foo)[_foo];
+    }
+  }]);
+  return Foo;
+}();
+
+var _foo = babelHelpers.classPrivateFieldLooseKey("foo");
