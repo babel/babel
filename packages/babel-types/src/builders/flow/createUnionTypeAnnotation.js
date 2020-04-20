41b3cca76763d2f3b203cddd5b69d5e9ca469df7
Alexey Berezin <beraliv.spb@gmail.com>
2020-04-20 20:31:44 +0300

    🔄 createUnionTypeAnnotation => createFlowUnionType

--- packages/babel-types/src/builders/flow/createUnionTypeAnnotation.js
+++ packages/babel-types/src/builders/flow/createUnionTypeAnnotation.js
@@ -1,19 +1,15 @@
 // @flow
-import { unionTypeAnnotation } from "../generated";
-import removeTypeDuplicates from "../../modifications/flow/removeTypeDuplicates";
+import createFlowUnionType from "./createFlowUnionType";
 
 /**
  * Takes an array of `types` and flattens them, removing duplicates and
  * returns a `UnionTypeAnnotation` node containg them.
+ *
+ * @deprecated use `createFlowUnionType`
+ * @see {createFlowUnionType}
  */
 export default function createUnionTypeAnnotation(
   types: Array<Object>,
 ): Object {
-  const flattened = removeTypeDuplicates(types);
-
-  if (flattened.length === 1) {
-    return flattened[0];
-  } else {
-    return unionTypeAnnotation(flattened);
-  }
+  return createFlowUnionType(types);
 }
