111ca086ec98912f5ad04c2c39bc6a43f84b334d
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-04-21 21:10:02 +0200

    Update babel-plugin-dynamic-import-node to 2.3.3

--- packages/babel-plugin-proposal-dynamic-import/test/fixtures/commonjs/shadowed-require/output.js
+++ packages/babel-plugin-proposal-dynamic-import/test/fixtures/commonjs/shadowed-require/output.js
@@ -2,5 +2,5 @@ var _require2 = "foo";
 
 (async function () {
   var _require = "bar";
-  await Promise.resolve("./mod").then(s => babelHelpers.interopRequireWildcard(require(s)));
+  await Promise.resolve().then(() => babelHelpers.interopRequireWildcard(require("./mod")));
 })();
