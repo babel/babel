d4c8db5177ccde3d5eb5f7f490daa8b259376010
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-04-21 21:26:33 +0200

    Update babel-plugin-dynamic-import-node to 2.3.3 (#11458)

--- packages/babel-plugin-proposal-dynamic-import/test/fixtures/commonjs/script/output.js
+++ packages/babel-plugin-proposal-dynamic-import/test/fixtures/commonjs/script/output.js
@@ -1,1 +1,1 @@
-var modP = Promise.resolve("mod").then(s => babelHelpers.interopRequireWildcard(require(s)));
+var modP = Promise.resolve().then(() => babelHelpers.interopRequireWildcard(require("mod")));
