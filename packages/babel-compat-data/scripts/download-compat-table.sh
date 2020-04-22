928b9f8c9518284eac6d0598633f2ec373fc6d0c
Huáng Jùnliàng <jlhwung@gmail.com>
2020-04-22 10:42:15 -0400

    chore: add node.js 14 support data (#11462)

--- packages/babel-compat-data/scripts/download-compat-table.sh
+++ packages/babel-compat-data/scripts/download-compat-table.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-COMPAT_TABLE_COMMIT=dd0983ae16fce677fccb3b74fbb778d435e59790
+COMPAT_TABLE_COMMIT=df1466b4ddae4c1c9d8b385332a800eeb7f28049
 GIT_HEAD=build/compat-table/.git/HEAD
 
 if [ -d "build/compat-table" ]; then
@@ -16,5 +16,5 @@ fi
 
 rm -rf build/compat-table
 mkdir -p build
-git clone --branch=gh-pages --single-branch --shallow-since=2020-04-18 https://github.com/kangax/compat-table.git build/compat-table
+git clone --branch=gh-pages --single-branch --shallow-since=2020-04-20 https://github.com/kangax/compat-table.git build/compat-table
 cd build/compat-table && git checkout -qf $COMPAT_TABLE_COMMIT
