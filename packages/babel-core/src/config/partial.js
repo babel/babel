d05334bde291c87ca688055b0be6b08f9b9ce219
Jacob Dreesen <jacob@hdreesen.de>
2020-04-25 10:24:22 +0200

    Fix typo and remove outdated doc comment (#11472)
    * Fix typo
    
    * Fix outdated doc comment [skip ci]

--- packages/babel-core/src/config/partial.js
+++ packages/babel-core/src/config/partial.js
@@ -189,11 +189,7 @@ class PartialConfig {
   }
 
   /**
-   * Returns true if their is a config file in the filesystem for this config.
-   *
-   * While this only means .babelrc(.mjs)?/package.json#babel right now, it
-   * may well expand in the future, so using this is recommended vs checking
-   * this.babelrc directly.
+   * Returns true if there is a config file in the filesystem for this config.
    */
   hasFilesystemConfig(): boolean {
     return this.babelrc !== undefined || this.config !== undefined;
