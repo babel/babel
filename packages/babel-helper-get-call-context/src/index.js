b41cc1023093c6ec626aa276a01944ae25c732f8
Oliver Dunk <oliver@oliverdunk.com>
2020-05-04 20:47:40 +0100

    Support OptionalCallExpressions

--- packages/babel-helper-get-call-context/src/index.js
+++ packages/babel-helper-get-call-context/src/index.js
@@ -1,7 +1,12 @@
 import type { NodePath } from "@babel/traverse";
 
 export default function(callPath: NodePath): NodePath {
-  callPath.assertCallExpression();
+  if (!callPath.isCallExpression() && !callPath.isOptionalCallExpression()) {
+    throw new Error(
+      `Expected type "CallExpression" or "OptionalCallExpression" ` +
+        `but instead got "${callPath.type}".`,
+    );
+  }
 
   let calleePath = callPath.get("callee");
 
