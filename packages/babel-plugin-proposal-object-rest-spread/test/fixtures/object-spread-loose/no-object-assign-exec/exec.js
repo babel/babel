654e0a3e8fd57873aabad510976e2e11857a11c4
Justin Ridgewell <jridgewell@google.com>
2020-05-06 23:30:58 -0400

    Use unique object property to avoid polluted contexts

--- packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-spread-loose/no-object-assign-exec/exec.js
+++ packages/babel-plugin-proposal-object-rest-spread/test/fixtures/object-spread-loose/no-object-assign-exec/exec.js
@@ -1,23 +1,26 @@
 "use strict";
-Object.defineProperty(Object.prototype, 'NOSET', {
+const NOSET = `NOSET${__filename}`;
+const NOWRITE = `NOWRITE${__filename}`;
+
+Object.defineProperty(Object.prototype, NOSET, {
   get(value) {
     // noop
   },
 });
 
-Object.defineProperty(Object.prototype, 'NOWRITE', {
+Object.defineProperty(Object.prototype, NOWRITE, {
   writable: false,
   value: 'abc',
 });
 
-const obj = { NOSET: 123 };
+const obj = { [NOSET]: 123 };
 // this won't work as expected if transformed as Object.assign (or equivalent)
 // because those trigger object setters (spread don't)
 expect(() => {
   const objSpread = { ...obj };
 }).toThrow();
 
-const obj2 = { NOWRITE: 456 };
+const obj2 = { [NOWRITE]: 456 };
 // this throws  `TypeError: Cannot assign to read only property 'NOWRITE'`
 // if transformed as Object.assign (or equivalent) because those use *assignment* for creating properties
 // (spread defines them)
