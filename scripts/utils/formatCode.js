19a7acf4bd6351574f61d93b72653de71f2771a2
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-05-18 19:07:04 +0200

    Don't use prettier on older node versions on CI

--- scripts/utils/formatCode.js
+++ scripts/utils/formatCode.js
@@ -1,11 +1,22 @@
 "use strict";
-const prettier = require("prettier");
 
-module.exports = function formatCode(code, filename) {
-  filename = filename || __filename;
-  const prettierConfig = prettier.resolveConfig.sync(filename);
-  prettierConfig.filepath = filename;
-  prettierConfig.parser = "babel";
+// TODO: Remove this `if` in Babel 8
+// Prettier only supports Node.js 10+, so we can fallback to not formatting
+// o CI on older Node.js versions
 
-  return prettier.format(code, prettierConfig);
-};
+if (process.env.CI && parseInt(process.version.node, 10) < 10) {
+  module.exports = function formatCode(code) {
+    return code;
+  };
+} else {
+  const prettier = require("prettier");
+
+  module.exports = function formatCode(code, filename) {
+    filename = filename || __filename;
+    const prettierConfig = prettier.resolveConfig.sync(filename);
+    prettierConfig.filepath = filename;
+    prettierConfig.parser = "babel";
+
+    return prettier.format(code, prettierConfig);
+  };
+}
