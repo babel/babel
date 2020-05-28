7691248433caca89e247d876238e0f4fbc67dd32
Huáng Jùnliàng <jlhwung@gmail.com>
2020-05-27 22:39:23 -0400

    add with-transform test cases

--- packages/babel-plugin-proposal-class-properties/test/fixtures/private/parenthesized-optional-member-call-with-transform/output.js
+++ packages/babel-plugin-proposal-class-properties/test/fixtures/private/parenthesized-optional-member-call-with-transform/output.js
@@ -4,7 +4,7 @@ class Foo {
   }
 
   test() {
-    var _o$Foo$self$getSelf, _o$Foo$self$getSelf2;
+    var _o$Foo$self$getSelf, _o$Foo$self$getSelf2, _o$Foo, _o$Foo$self, _o$Foo$self2;
 
     const o = {
       Foo: Foo
@@ -15,8 +15,8 @@ class Foo {
     (o === null || o === void 0 ? void 0 : babelHelpers.classStaticPrivateFieldSpecGet(o.Foo, Foo, _m).bind(o.Foo))();
     (o === null || o === void 0 ? void 0 : babelHelpers.classStaticPrivateFieldSpecGet(o.Foo, Foo, _m).bind(o.Foo))().toString;
     (o === null || o === void 0 ? void 0 : babelHelpers.classStaticPrivateFieldSpecGet(o.Foo, Foo, _m).bind(o.Foo))().toString();
-    ((_o$Foo$self$getSelf = (o.Foo?.self.getSelf)()) === null || _o$Foo$self$getSelf === void 0 ? void 0 : babelHelpers.classStaticPrivateFieldSpecGet(_o$Foo$self$getSelf, Foo, _m).bind(_o$Foo$self$getSelf))();
-    ((_o$Foo$self$getSelf2 = (o.Foo.self?.getSelf)()) === null || _o$Foo$self$getSelf2 === void 0 ? void 0 : babelHelpers.classStaticPrivateFieldSpecGet(_o$Foo$self$getSelf2, Foo, _m).bind(_o$Foo$self$getSelf2))();
+    ((_o$Foo$self$getSelf = ((_o$Foo = o.Foo) === null || _o$Foo === void 0 ? void 0 : (_o$Foo$self = _o$Foo.self).getSelf.bind(_o$Foo$self))()) === null || _o$Foo$self$getSelf === void 0 ? void 0 : babelHelpers.classStaticPrivateFieldSpecGet(_o$Foo$self$getSelf, Foo, _m).bind(_o$Foo$self$getSelf))();
+    ((_o$Foo$self$getSelf2 = ((_o$Foo$self2 = o.Foo.self) === null || _o$Foo$self2 === void 0 ? void 0 : _o$Foo$self2.getSelf.bind(_o$Foo$self2))()) === null || _o$Foo$self$getSelf2 === void 0 ? void 0 : babelHelpers.classStaticPrivateFieldSpecGet(_o$Foo$self$getSelf2, Foo, _m).bind(_o$Foo$self$getSelf2))();
   }
 
 }
