79f56194bfd250d28b87255708a2613565b47122
Will L <triwnlproductions@gmail.com>
2020-06-10 21:11:08 -0400

    add node check from feedback

--- packages/babel-helper-member-expression-to-functions/src/index.js
+++ packages/babel-helper-member-expression-to-functions/src/index.js
@@ -337,8 +337,7 @@ const handle = {
     // [MEMBER = _VALUE] = ARR -> [_destructureSet(MEMBER) = _VALUE] = ARR
     // [...MEMBER] -> [..._destructureSet(MEMBER)]
     if (
-      (parentPath.isForXStatement() &&
-        parentPath.parentPath.isAssignmentPattern({ left: node })) ||
+      parentPath.isForXStatement({ left: node }) ||
       // { KEY: MEMBER } = OBJ
       (parentPath.isObjectProperty({ value: node }) &&
         parentPath.parentPath.isObjectPattern()) ||
