601c824873a2606b0292ea1460a3a79f5f704215
Huáng Jùnliàng <jlhwung@gmail.com>
2020-06-21 16:04:12 -0400

    Refine babel core types (#11544)
    Co-authored-by: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>

--- packages/babel-core/src/config/files/import.js
+++ packages/babel-core/src/config/files/import.js
@@ -1,3 +1,4 @@
+// @flow
 // We keep this in a seprate file so that in older node versions, where
 // import() isn't supported, we can try/catch around the require() call
 // when loading this file.
