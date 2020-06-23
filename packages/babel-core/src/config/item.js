96f4fe656b84dae67ffeb76e2989350bc9c295f1
Jason Miller <developit@users.noreply.github.com>
2020-06-23 12:17:37 -0400

    Use a symbol

--- packages/babel-core/src/config/item.js
+++ packages/babel-core/src/config/item.js
@@ -40,7 +40,7 @@ export function createConfigItem(
 }
 
 export function getItemDescriptor(item: mixed): UnloadedDescriptor | void {
-  if (item instanceof ConfigItem || item._isConfigItem === true) {
+  if (item && item[Symbol.for("@babel/core@7 - ConfigItem")] === true) {
     return item._descriptor;
   }
 
