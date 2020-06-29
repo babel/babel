dd1b57d0407b08336ce222ac88b237aef31b13e9
Oliver Dunk <oliver@oliverdunk.com>
2020-06-29 23:15:48 +0100

    Add comment explaining what a transparent expression wrapper is

--- packages/babel-helper-skip-transparent-expr-wrappers/src/index.js
+++ packages/babel-helper-skip-transparent-expr-wrappers/src/index.js
@@ -13,6 +13,11 @@ export function getCallContext(callPath: NodePath): NodePath {
   return skipTransparentExprWrappers(calleePath);
 }
 
+// A transparent expression wrapper is an AST node that most plugins will wish
+// to skip, as its presence does not affect the behaviour of the code. This
+// includes expressions used for types, and extra parenthesis. For example, in
+// (a as any)(), this helper can be used to skip the TSAsExpression when
+// determining the callee.
 export function isTransparentExprWrapper(node: Node) {
   return (
     t.isTSAsExpression(node) ||
