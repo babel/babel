db862e7903186993c9ab775d9c7b9a1864205980
James Addison <jay@jp-hosting.net>
2020-07-04 13:06:09 +0100

    Property safety: ensure that ignoreSuites/ignoreTasks are populated prior to access

--- packages/babel-helper-transform-fixture-test-runner/src/index.js
+++ packages/babel-helper-transform-fixture-test-runner/src/index.js
@@ -351,7 +351,12 @@ export default function (
   const suites = getFixtures(fixturesLoc);
 
   for (const testSuite of suites) {
-    if (suiteOpts.ignoreSuites.includes(testSuite.title)) continue;
+    if (
+      suiteOpts.ignoreSuites &&
+      suiteOpts.ignoreSuites.includes(testSuite.title)
+    ) {
+      continue;
+    }
 
     describe(name + "/" + testSuite.title, function () {
       jest.addMatchers({
@@ -360,8 +365,9 @@ export default function (
 
       for (const task of testSuite.tests) {
         if (
-          suiteOpts.ignoreTasks.includes(task.title) ||
-          suiteOpts.ignoreTasks.includes(testSuite.title + "/" + task.title)
+          suiteOpts.ignoreTasks &&
+          (suiteOpts.ignoreTasks.includes(task.title) ||
+            suiteOpts.ignoreTasks.includes(testSuite.title + "/" + task.title))
         ) {
           continue;
         }
