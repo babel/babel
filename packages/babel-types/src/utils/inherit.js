3e55270135410c5b0f75de7b9a283565357fe595
James Addison <jay@jp-hosting.net>
2020-07-06 08:27:41 +0100

    Reduce dependency on lodash functions: includes, uniq, repeat, isinteger (#11790)
    * Replace lodash 'includes' usage with Array.prototype.includes
    
    * Replace lodash 'values' usage with Object.values
    
    * Replace lodash 'uniq' usage with Array.from(new Set(...))
    
    * Property safety: ensure that ignoreSuites/ignoreTasks are populated prior to access
    
    * Property safety: ensure that blacklistTypes is populated prior to access
    
    * Revert "Replace lodash 'values' usage with Object.values"
    
    This reverts commit 9fd3679d6db03066daee09fad0050e5292a32aa1.
    
    * Replace lodash 'repeat' usage with String.prototype.repeat
    
    * Replace lodash 'isinteger' usage with Number.isInteger
    
    * Remove explicit lodash dependency from babel-generator package
    
    * Update packages/babel-helper-transform-fixture-test-runner/src/index.js
    
    Co-authored-by: Brian Ng <bng412@gmail.com>
    
    * Rely on optional chaining operator as sole boolean check
    
    * Handle additional optional chaining operator simplification
    
    * Update type signature
    
    Co-authored-by: Brian Ng <bng412@gmail.com>

--- packages/babel-types/src/utils/inherit.js
+++ packages/babel-types/src/utils/inherit.js
@@ -1,12 +1,12 @@
 // @flow
-import uniq from "lodash/uniq";
-
 export default function inherit(
   key: string,
   child: Object,
   parent: Object,
 ): void {
   if (child && parent) {
-    child[key] = uniq([].concat(child[key], parent[key]).filter(Boolean));
+    child[key] = Array.from(
+      new Set([].concat(child[key], parent[key]).filter(Boolean)),
+    );
   }
 }
