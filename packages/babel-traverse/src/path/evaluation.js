c3065e8a16a82363bdc32f579e25f33938a17cce
Johan Holmerin <johan@holmer.in>
2020-07-15 19:37:23 +0200

    Document evaluate deopt property

--- packages/babel-traverse/src/path/evaluation.js
+++ packages/babel-traverse/src/path/evaluation.js
@@ -407,19 +407,24 @@ function evaluateQuasis(path, quasis: Array<Object>, state, raw = false) {
 /**
  * Walk the input `node` and statically evaluate it.
  *
- * Returns an object in the form `{ confident, value }`. `confident` indicates
- * whether or not we had to drop out of evaluating the expression because of
- * hitting an unknown node that we couldn't confidently find the value of.
+ * Returns an object in the form `{ confident, value, deopt }`. `confident`
+ * indicates whether or not we had to drop out of evaluating the expression
+ * because of hitting an unknown node that we couldn't confidently find the
+ * value of, in which case `deopt` is the path of said node.
  *
  * Example:
  *
  *   t.evaluate(parse("5 + 5")) // { confident: true, value: 10 }
  *   t.evaluate(parse("!true")) // { confident: true, value: false }
- *   t.evaluate(parse("foo + foo")) // { confident: false, value: undefined }
+ *   t.evaluate(parse("foo + foo")) // { confident: false, value: undefined, deopt: NodePath }
  *
  */
 
-export function evaluate(): { confident: boolean, value: any } {
+export function evaluate(): {
+  confident: boolean,
+  value: any,
+  deopt?: NodePath,
+} {
   const state = {
     confident: true,
     deoptPath: null,
