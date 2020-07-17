373b95fedcb2f8739b3d1fe3daab9f3e6380b283
Huáng Jùnliàng <jlhwung@gmail.com>
2020-07-17 17:52:49 -0400

    feat: add export-namespace-from to preset-env

---
+++ packages/babel-preset-env/test/fixtures/export-namespace-from/auto-esm-not-supported/output.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.foo = void 0;
+
+var _foo = _interopRequireDefault(require("./foo.mjs"));
+
+exports.foo = _foo;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
