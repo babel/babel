2bf38fb9149eb514a13bb608e5a9a0c06ad5cacd
Huáng Jùnliàng <jlhwung@gmail.com>
2020-07-21 09:44:56 -0400

    fix: disallow \8, \9 in strict mode string (#11852)

--- packages/babel-parser/src/parser/error-message.js
+++ packages/babel-parser/src/parser/error-message.js
@@ -139,6 +139,7 @@ export const ErrorMessages = Object.freeze({
   StrictEvalArgumentsBinding: "Binding '%0' in strict mode",
   StrictFunction:
     "In strict mode code, functions can only be declared at top level or inside a block",
+  StrictNumericEscape: "The only valid numeric escape in strict mode is '\\0'",
   StrictOctalLiteral: "Legacy octal literals are not allowed in strict mode",
   StrictWith: "'with' in strict mode",
   SuperNotAllowed:
