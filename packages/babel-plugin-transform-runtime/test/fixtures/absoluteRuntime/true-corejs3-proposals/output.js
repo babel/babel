175413c07fad4f783b67807e963eacb95d3054da
sz-coder <szcod3r@gmail.com>
2020-07-30 00:52:10 +0200

    - Removed helper code from test output files (tests for issue #11885).

--- packages/babel-plugin-transform-runtime/test/fixtures/absoluteRuntime/true-corejs3-proposals/output.js
+++ packages/babel-plugin-transform-runtime/test/fixtures/absoluteRuntime/true-corejs3-proposals/output.js
@@ -1,24 +1,6 @@
-var _getIterator = require("<CWD>/node_modules/@babel/runtime-corejs3/core-js/get-iterator");
-
-var _Array$isArray = require("<CWD>/node_modules/@babel/runtime-corejs3/core-js/array/is-array");
-
-var _getIteratorMethod = require("<CWD>/node_modules/@babel/runtime-corejs3/core-js/get-iterator-method");
-
-var _Symbol = require("<CWD>/node_modules/@babel/runtime-corejs3/core-js/symbol");
-
-var _Array$from = require("<CWD>/node_modules/@babel/runtime-corejs3/core-js/array/from");
-
-var _sliceInstanceProperty = require("<CWD>/node_modules/@babel/runtime-corejs3/core-js/instance/slice");
-
 var _regeneratorRuntime = require("<CWD>/node_modules/@babel/runtime-corejs3/regenerator");
 
 var _mapInstanceProperty = require("<CWD>/node_modules/@babel/runtime-corejs3/core-js/instance/map");
-
-function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof _Symbol === "undefined" || _getIteratorMethod(o) == null) { if (_Array$isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = _getIterator(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
-
-function _unsupportedIterableToArray(o, minLen) { var _context2; if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = _sliceInstanceProperty(_context2 = Object.prototype.toString.call(o)).call(_context2, 8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return _Array$from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 var _marked = /*#__PURE__*/_regeneratorRuntime.mark(makeIterator);
 
@@ -44,7 +26,7 @@ function makeIterator() {
   }, _marked);
 }
 
-var _iterator = _createForOfIteratorHelper(makeIterator()),
+var _iterator = babelHelpers.createForOfIteratorHelper(makeIterator()),
     _step;
 
 try {
