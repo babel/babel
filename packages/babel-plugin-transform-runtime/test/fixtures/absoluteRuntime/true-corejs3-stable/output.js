4ce1b70adec6defff7020fb9d7f24122a5752ba9
SZ-Coder <65417876+sz-coder@users.noreply.github.com>
2020-07-30 16:35:37 +0200

    Fix incorrect module path when absoluteRuntime is specified (#11893)
    * - Added failing tests for issue #11885.
    
    * - Fix issue #11885: absoluteRuntime does not work as expected with corejs3.
    
    * - Removed helper code from test output files (tests for issue #11885).
    
    * chore: add @babel/runtime-corejs3 to dev deps
    
    * chore: update test fixtures
    
    Co-authored-by: Huáng Jùnliàng <jlhwung@gmail.com>

---
+++ packages/babel-plugin-transform-runtime/test/fixtures/absoluteRuntime/true-corejs3-stable/output.js
@@ -0,0 +1,41 @@
+var _regeneratorRuntime = require("<CWD>/packages/babel-plugin-transform-runtime/node_modules/@babel/runtime-corejs3/regenerator");
+
+var _mapInstanceProperty = require("<CWD>/packages/babel-plugin-transform-runtime/node_modules/@babel/runtime-corejs3/core-js-stable/instance/map");
+
+var _marked = /*#__PURE__*/_regeneratorRuntime.mark(makeIterator);
+
+_mapInstanceProperty(Array);
+
+function makeIterator() {
+  return _regeneratorRuntime.wrap(function makeIterator$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return 1;
+
+        case 2:
+          _context.next = 4;
+          return 2;
+
+        case 4:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, _marked);
+}
+
+var _iterator = babelHelpers.createForOfIteratorHelper(makeIterator()),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var itItem = _step.value;
+    console.log(itItem);
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
