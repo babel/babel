43b08e2ba33e2e10a1b701dbb04d27ae747ccaa5
James Addison <jay@jp-hosting.net>
2020-07-31 10:40:57 +0100

    Ensure arrow function is used unambiguously

--- packages/babel-types/src/converters/valueToNode.js
+++ packages/babel-types/src/converters/valueToNode.js
@@ -21,10 +21,11 @@ try {
   nodeUtilTypes = util.types || util;
 } catch {}
 
-const isRegExp = value =>
-  nodeUtilTypes
+const isRegExp = value => {
+  return nodeUtilTypes
     ? nodeUtilTypes.isRegExp(value)
     : Object.prototype.toString.call(value) === '[object RegExp]';
+};
 
 export default function valueToNode(value: any): Object {
   // undefined
