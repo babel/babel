371e152cd2e9796909c3705a4470ee4cfe33d8f7
Joe Haddad <timer150@gmail.com>
2020-08-31 15:16:47 -0400

    fix(jsx): do not throw on generated element in development mode (#12017)
    * Fix `jsxDEV` for generated elements
    
    * Throw invariant error
    
    * Add test
    
    * test: revise test layout
    
    * fix: node 6 does not support trailing comma
    
    * Use undefined node
    
    Co-authored-by: Huáng Jùnliàng <jlhwung@gmail.com>

--- packages/babel-helper-builder-react-jsx-experimental/src/index.js
+++ packages/babel-helper-builder-react-jsx-experimental/src/index.js
@@ -437,7 +437,7 @@ You can set \`throwIfNamespace: false\` to bypass this warning.`,
     const location = path.node.loc;
     if (!location) {
       // the element was generated and doesn't have location information
-      return;
+      return path.scope.buildUndefinedNode();
     }
 
     if (!state.fileNameIdentifier) {
