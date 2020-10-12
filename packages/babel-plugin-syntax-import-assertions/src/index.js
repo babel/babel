b0601b1ad590f5b719f93fcb1bbe5a7dbdbd2323
Huáng Jùnliàng <jlhwung@gmail.com>
2020-10-12 16:27:24 -0400

    remove "version" options from syntax-import-assertions

--- packages/babel-plugin-syntax-import-assertions/src/index.js
+++ packages/babel-plugin-syntax-import-assertions/src/index.js
@@ -1,21 +1,13 @@
 import { declare } from "@babel/helper-plugin-utils";
 
-export default declare((api, { version }) => {
+export default declare(api => {
   api.assertVersion(7);
-
-  if (typeof version !== "string" || version !== "september-2020") {
-    throw new Error(
-      "The 'importAssertions' plugin requires a 'version' option," +
-        " representing the last proposal update. Currently, the" +
-        " only supported value is 'september-2020'.",
-    );
-  }
 
   return {
     name: "syntax-import-assertions",
 
     manipulateOptions(opts, parserOpts) {
-      parserOpts.plugins.push(["importAssertions", { version }]);
+      parserOpts.plugins.push(["importAssertions"]);
     },
   };
 });
