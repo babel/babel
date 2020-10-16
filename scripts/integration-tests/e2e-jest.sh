53612329d212c450ee762568b84408d8e4266586
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-10-16 15:03:58 +0200

    Run jest't tests in the e2e tests

---
+++ scripts/integration-tests/e2e-jest.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#==============================================================================#
+#                                  SETUP                                       #
+#==============================================================================#
+
+# Start in scripts/integration-tests/ even if run from root directory
+cd "$(dirname "$0")" || exit
+
+source utils/local-registry.sh
+source utils/cleanup.sh
+
+# Echo every command being executed
+set -x
+
+# Clone jest
+git clone --depth=1 https://github.com/facebook/jest tmp/jest
+cd tmp/jest || exit
+
+# Update @babel/* dependencies
+bump_deps="$PWD/../../utils/bump-babel-dependencies.js"
+node "$bump_deps"
+for d in ./packages/*/
+do
+  (cd "$d"; node "$bump_deps")
+done
+
+#==============================================================================#
+#                                 ENVIRONMENT                                  #
+#==============================================================================#
+node -v
+yarn --version
+python --version
+
+#==============================================================================#
+#                                   TEST                                       #
+#==============================================================================#
+
+startLocalRegistry "$PWD"/../../verdaccio-config.yml
+yarn install
+yarn build
+
+# Test
+CI=true yarn test
+
+cleanup
