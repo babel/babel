6d522fe677154eb2b44149f00622ce815b52a6a3
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-11-04 16:03:28 +0100

    Update test

--- packages/babel-plugin-proposal-private-methods/test/fixtures/accessors-to-private-property/shadowed-nested-class-key/input.js
+++ packages/babel-plugin-proposal-private-methods/test/fixtures/accessors-to-private-property/shadowed-nested-class-key/input.js
@@ -17,14 +17,17 @@ class A {
       [this.#foo]() {
         this.#foo = 1;
         this.#bar = 2;
+        this.#baz = 1.2;
       }
       [this.#bar]() {
         this.#foo = 3;
         this.#bar = 4;
+        this.#baz = 3.4;
       }
       [this.#baz]() {
-        this.#foo = 3;
-        this.#bar = 4;
+        this.#foo = 5;
+        this.#bar = 6;
+        this.#baz = 5.6;
       }
     }
   }
--- packages/babel-plugin-proposal-private-methods/test/fixtures/accessors-to-private-property/shadowed-nested-class-key/output.js
+++ packages/babel-plugin-proposal-private-methods/test/fixtures/accessors-to-private-property/shadowed-nested-class-key/output.js
@@ -30,16 +30,19 @@ class A {
       [this.#foo._]() {
         this.#foo = 1;
         this.#bar._ = 2;
+        this.#baz._ = 1.2;
       }
 
       [this.#bar._]() {
         this.#foo = 3;
         this.#bar._ = 4;
+        this.#baz._ = 3.4;
       }
 
       [this.#baz]() {
-        this.#foo = 3;
-        this.#bar._ = 4;
+        this.#foo = 5;
+        this.#bar._ = 6;
+        this.#baz._ = 5.6;
       }
 
     }
