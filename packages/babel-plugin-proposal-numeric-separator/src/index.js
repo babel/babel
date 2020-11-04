837ae56c9bc63e6d5bcbe66ccf0c60fc01a98fcd
fraywing <austin@rupie.io>
2020-11-04 10:40:37 -0800

    Adding fix for plugin-proposal-numeric-separator per https://github.com/babel/babel/issues/12310

--- packages/babel-plugin-proposal-numeric-separator/src/index.js
+++ packages/babel-plugin-proposal-numeric-separator/src/index.js
@@ -9,7 +9,7 @@ import syntaxNumericSeparator from "@babel/plugin-syntax-numeric-separator";
  */
 function remover({ node }: NodePath<BigIntLiteral | NumericLiteral>) {
   const { extra } = node;
-  if (extra && extra.raw.includes("_")) {
+  if (extra?.raw?.includes("_")) {
     extra.raw = extra.raw.replace(/_/g, "");
   }
 }
