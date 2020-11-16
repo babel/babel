f54f1ee492b1169ad68280038168f9bbb685cd0f
Huáng Jùnliàng <jlhwung@gmail.com>
2020-11-16 10:16:46 -0500

    Fix: correctly transform `this.#m?.(...arguments)` (#12350)
    * add tests on @babel/helper-optimise-call-expression
    
    * fix: correctly optimise `a.b?.(...arguments)`
    
    * add integration test with properties transform

--- packages/babel-helper-optimise-call-expression/src/index.js
+++ packages/babel-helper-optimise-call-expression/src/index.js
@@ -1,17 +1,42 @@
 import * as t from "@babel/types";
 
-export default function (callee, thisNode, args, optional) {
+/**
+ * A helper function that generates a new call expression with given thisNode.
+ It will also optimize `(...arguments)` to `.apply(arguments)`
+ *
+ * @export
+ * @param {Node} callee The callee of call expression
+ * @param {Node} thisNode The desired this of call expression
+ * @param {Node[]} args The arguments of call expression
+ * @param {boolean} optional Whether the call expression is optional
+ * @returns {CallExpression | OptionalCallExpression} The generated new call expression
+ */
+export default function (
+  callee: Node,
+  thisNode: Node,
+  args: Node[],
+  optional: boolean,
+): CallExpression | OptionalCallExpression {
   if (
     args.length === 1 &&
     t.isSpreadElement(args[0]) &&
     t.isIdentifier(args[0].argument, { name: "arguments" })
   ) {
-    // eg. super(...arguments);
+    // a.b?.(...arguments);
+    if (optional) {
+      return t.optionalCallExpression(
+        t.optionalMemberExpression(callee, t.identifier("apply"), false, true),
+        [thisNode, args[0].argument],
+        false,
+      );
+    }
+    // a.b(...arguments);
     return t.callExpression(t.memberExpression(callee, t.identifier("apply")), [
       thisNode,
       args[0].argument,
     ]);
   } else {
+    // a.b?.(arg1, arg2)
     if (optional) {
       return t.optionalCallExpression(
         t.optionalMemberExpression(callee, t.identifier("call"), false, true),
@@ -19,6 +44,7 @@ export default function (callee, thisNode, args, optional) {
         false,
       );
     }
+    // a.b(arg1, arg2)
     return t.callExpression(t.memberExpression(callee, t.identifier("call")), [
       thisNode,
       ...args,
---
+++ packages/babel-helper-optimise-call-expression/test/index.js
@@ -0,0 +1,103 @@
+import { parse } from "@babel/parser";
+import generator from "@babel/generator";
+import * as t from "@babel/types";
+import optimizeCallExpression from "..";
+
+function transformInput(input, thisIdentifier) {
+  const ast = parse(input);
+  const callExpression = ast.program.body[0].expression;
+  return generator(
+    optimizeCallExpression(
+      callExpression.callee,
+      thisIdentifier
+        ? t.identifier(thisIdentifier)
+        : callExpression.callee.object,
+      callExpression.arguments,
+      callExpression.type === "OptionalCallExpression",
+    ),
+  ).code;
+}
+
+describe("@babel/helper-optimise-call-expression", () => {
+  test("optimizeCallExpression should work when thisNode is implied from callee", () => {
+    expect(transformInput("a.b(...arguments)")).toMatchInlineSnapshot(
+      `"a.b.apply(a, arguments)"`,
+    );
+    expect(transformInput("a[b](...arguments)")).toMatchInlineSnapshot(
+      `"a[b].apply(a, arguments)"`,
+    );
+    expect(transformInput("a.b?.(...arguments)")).toMatchInlineSnapshot(
+      `"a.b?.apply(a, arguments)"`,
+    );
+    expect(transformInput("a[b]?.(...arguments)")).toMatchInlineSnapshot(
+      `"a[b]?.apply(a, arguments)"`,
+    );
+
+    expect(transformInput("a.b(...args)")).toMatchInlineSnapshot(
+      `"a.b.call(a, ...args)"`,
+    );
+    expect(transformInput("a[b](...args)")).toMatchInlineSnapshot(
+      `"a[b].call(a, ...args)"`,
+    );
+    expect(transformInput("a.b?.(...args)")).toMatchInlineSnapshot(
+      `"a.b?.call(a, ...args)"`,
+    );
+    expect(transformInput("a[b]?.(...args)")).toMatchInlineSnapshot(
+      `"a[b]?.call(a, ...args)"`,
+    );
+
+    expect(transformInput("a.b(arg1, arg2)")).toMatchInlineSnapshot(
+      `"a.b.call(a, arg1, arg2)"`,
+    );
+    expect(transformInput("a[b](arg1, arg2)")).toMatchInlineSnapshot(
+      `"a[b].call(a, arg1, arg2)"`,
+    );
+    expect(transformInput("a.b?.(arg1, arg2)")).toMatchInlineSnapshot(
+      `"a.b?.call(a, arg1, arg2)"`,
+    );
+    expect(transformInput("a[b]?.(arg1, arg2)")).toMatchInlineSnapshot(
+      `"a[b]?.call(a, arg1, arg2)"`,
+    );
+  });
+
+  test("optimizeCallExpression should work when thisNode is provided", () => {
+    expect(transformInput("a.b(...arguments)", "c")).toMatchInlineSnapshot(
+      `"a.b.apply(c, arguments)"`,
+    );
+    expect(transformInput("a[b](...arguments)", "c")).toMatchInlineSnapshot(
+      `"a[b].apply(c, arguments)"`,
+    );
+    expect(transformInput("a.b?.(...arguments)", "c")).toMatchInlineSnapshot(
+      `"a.b?.apply(c, arguments)"`,
+    );
+    expect(transformInput("a[b]?.(...arguments)", "c")).toMatchInlineSnapshot(
+      `"a[b]?.apply(c, arguments)"`,
+    );
+
+    expect(transformInput("a.b(...args)", "c")).toMatchInlineSnapshot(
+      `"a.b.call(c, ...args)"`,
+    );
+    expect(transformInput("a[b](...args)", "c")).toMatchInlineSnapshot(
+      `"a[b].call(c, ...args)"`,
+    );
+    expect(transformInput("a.b?.(...args)", "c")).toMatchInlineSnapshot(
+      `"a.b?.call(c, ...args)"`,
+    );
+    expect(transformInput("a[b]?.(...args)", "c")).toMatchInlineSnapshot(
+      `"a[b]?.call(c, ...args)"`,
+    );
+
+    expect(transformInput("a.b(arg1, arg2)", "c")).toMatchInlineSnapshot(
+      `"a.b.call(c, arg1, arg2)"`,
+    );
+    expect(transformInput("a[b](arg1, arg2)", "c")).toMatchInlineSnapshot(
+      `"a[b].call(c, arg1, arg2)"`,
+    );
+    expect(transformInput("a.b?.(arg1, arg2)", "c")).toMatchInlineSnapshot(
+      `"a.b?.call(c, arg1, arg2)"`,
+    );
+    expect(transformInput("a[b]?.(arg1, arg2)", "c")).toMatchInlineSnapshot(
+      `"a[b]?.call(c, arg1, arg2)"`,
+    );
+  });
+});
