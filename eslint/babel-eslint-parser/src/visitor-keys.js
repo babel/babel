66ed8eba6b4d23a1f0b8144bf9edea6a053e0701
Huáng Jùnliàng <jlhwung@gmail.com>
2020-11-18 12:26:47 -0500

    support new estree shape in babel-eslint

--- eslint/babel-eslint-parser/src/visitor-keys.js
+++ eslint/babel-eslint-parser/src/visitor-keys.js
@@ -8,11 +8,15 @@ export const newTypes = {
   Literal: ESLINT_VISITOR_KEYS.Literal,
   MethodDefinition: ["decorators"].concat(ESLINT_VISITOR_KEYS.MethodDefinition),
   Property: ["decorators"].concat(ESLINT_VISITOR_KEYS.Property),
+  // todo: remove this when Acorn supports class properties
+  PropertyDefinition: t.VISITOR_KEYS.ClassProperty,
+  // todo: remove this when Acorn supports class properties
+  PrivateIdentifier: [],
 };
 
 // AST Types that shares `"type"` property with Babel but have different shape
 export const conflictTypes = {
-  // todo: remove this when class features are supported
+  // todo: remove this when we drop Babel 7 support
   ClassPrivateMethod: ["decorators"].concat(
     ESLINT_VISITOR_KEYS.MethodDefinition,
   ),
--- eslint/babel-eslint-plugin/src/rules/semi.js
+++ eslint/babel-eslint-plugin/src/rules/semi.js
@@ -79,7 +79,7 @@ function report(context, node, missing) {
 export default ruleComposer.joinReports([
   rule,
   context => ({
-    "ClassProperty, ClassPrivateProperty"(node) {
+    "ClassProperty, ClassPrivateProperty, PropertyDefinition"(node) {
       const options = context.options[1];
       const exceptOneLine = options && options.omitLastInOneLineBlock === true;
       const sourceCode = context.getSourceCode();
