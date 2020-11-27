f44454160701d02a1d112ff5d1f47683aa7637dc
Huáng Jùnliàng <jlhwung@gmail.com>
2020-11-27 15:16:43 -0500

    refactor: move @babel/helper-get-function-arity to ts

--- packages/babel-helper-get-function-arity/src/index.ts
+++ packages/babel-helper-get-function-arity/src/index.ts
@@ -1,7 +1,8 @@
 import * as t from "@babel/types";
+import type { Function } from "@babel/types";
 
-export default function (node): number {
-  const params: Array<Object> = node.params;
+export default function (node: Function): number {
+  const params = node.params;
   for (let i = 0; i < params.length; i++) {
     const param = params[i];
     if (t.isAssignmentPattern(param) || t.isRestElement(param)) {
