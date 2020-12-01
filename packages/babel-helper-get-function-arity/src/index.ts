5e4b85ab1cc3cd1c29213e3e26e6caed7c63521a
Huáng Jùnliàng <jlhwung@gmail.com>
2020-12-01 10:45:05 -0500

    refactor: move @babel/helper-get-function-arity to ts (#12414)
    * refactor: move @babel/helper-get-function-arity to ts
    
    * Review comments
    
    * chore: add flow interface

--- packages/babel-helper-get-function-arity/src/index.ts
+++ packages/babel-helper-get-function-arity/src/index.ts
@@ -1,7 +1,7 @@
 import * as t from "@babel/types";
 
-export default function (node): number {
-  const params: Array<Object> = node.params;
+export default function (node: t.Function): number {
+  const params = node.params;
   for (let i = 0; i < params.length; i++) {
     const param = params[i];
     if (t.isAssignmentPattern(param) || t.isRestElement(param)) {
