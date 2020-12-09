ce9deb33943e89a2ae38514fac794b5393a5f1d2
sosukesuzuki <aosukeke@gmail.com>
2020-12-10 04:29:23 +0900

    Add generator support

--- packages/babel-generator/src/generators/expressions.ts
+++ packages/babel-generator/src/generators/expressions.ts
@@ -300,3 +300,18 @@ export function V8IntrinsicIdentifier(
   this.token("%");
   this.word(node.name);
 }
+
+export function ModuleExpression(node: Object) {
+  this.word("module");
+  this.space();
+  this.token("{");
+  if (node.body.length === 0) {
+    this.token("}");
+  } else {
+    this.newline();
+    this.printSequence(node.body, node, {
+      indent: true,
+    });
+    this.rightBrace();
+  }
+}
