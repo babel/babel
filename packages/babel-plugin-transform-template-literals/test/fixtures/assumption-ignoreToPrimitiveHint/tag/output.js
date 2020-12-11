4c2961e58fc2c446b15ba0e8aae493ff754244bb
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-12-11 20:28:38 +0100

    Implement `mutableTemplateObject` and `ignoreToPrimitiveHint` (#12408)

---
+++ packages/babel-plugin-transform-template-literals/test/fixtures/assumption-ignoreToPrimitiveHint/tag/output.js
@@ -0,0 +1,5 @@
+var _templateObject;
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+tag(_templateObject || (_templateObject = _taggedTemplateLiteral(["foo ", " baz"])), bar);
---
+++ packages/babel-plugin-transform-template-literals/test/fixtures/loose/mutableTemplateObject/output.js
@@ -0,0 +1,5 @@
+var _templateObject;
+
+function _taggedTemplateLiteralLoose(strings, raw) { if (!raw) { raw = strings.slice(0); } strings.raw = raw; return strings; }
+
+var o = tag(_templateObject || (_templateObject = _taggedTemplateLiteralLoose(["foo ", " baz"])), bar);
