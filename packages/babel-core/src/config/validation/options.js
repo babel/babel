169a4130ec2d9490d802f1c0ec8494856460eec7
Huáng Jùnliàng <jlhwung@gmail.com>
2020-12-14 17:46:29 -0500

    Implement `pureGetters` assumption (#12504)
    * feat: allow pureGetters in assumptions
    
    * feat: respect pureGetters assumption

--- packages/babel-core/src/config/validation/options.js
+++ packages/babel-core/src/config/validation/options.js
@@ -337,6 +337,7 @@ export const assumptionsNames = new Set<string>([
   "ignoreToPrimitiveHint",
   "iterableIsArray",
   "mutableTemplateObject",
+  "pureGetters",
   "setClassMethods",
   "setComputedProperties",
   "setPublicClassFields",
