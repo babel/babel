40307cc8dce1c8b7c3636fd219a77dc707bb816f
Huáng Jùnliàng <jlhwung@gmail.com>
2021-01-11 21:15:59 -0500

    test: apply external-helpers on preset-env test fixtures

--- packages/babel-preset-env/test/fixtures/export-namespace-from/auto-esm-not-supported/output.js
+++ packages/babel-preset-env/test/fixtures/export-namespace-from/auto-esm-not-supported/output.js
@@ -5,8 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.foo = void 0;
 
-var _foo = _interopRequireDefault(require("./foo.mjs"));
+var _foo = babelHelpers.interopRequireDefault(require("./foo.mjs"));
 
 exports.foo = _foo;
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- packages/babel-preset-env/test/fixtures/preset-options/include-scoped/output.mjs
+++ packages/babel-preset-env/test/fixtures/preset-options/include-scoped/output.mjs
@@ -1,5 +1,3 @@
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 let Foo = function Foo() {
-  _classCallCheck(this, Foo);
+  babelHelpers.classCallCheck(this, Foo);
 };
