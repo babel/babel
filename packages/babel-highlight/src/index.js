ad282f67dea20d9955a6a0617fcb2c0efbd516cb
Simon Lydell <simon.lydell@gmail.com>
2021-01-19 15:22:34 +0100

    Update `js-tokens` to 6.0.0
    backport of 5b3ca4332f1f3dcad183c459c53042cfd9b99f55, part 2
    
    Co-authored-by: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>

--- packages/babel-highlight/src/index.js
+++ packages/babel-highlight/src/index.js
@@ -45,7 +45,99 @@ const BRACKET = /^[()[\]{}]$/;
 
 let tokenize;
 
-{
+if (process.env.BABEL_8_BREAKING) {
+  /**
+   * Get the type of token, specifying punctuator type.
+   */
+  const getTokenType = function (token) {
+    if (token.type === "IdentifierName") {
+      if (
+        isKeyword(token.value) ||
+        isStrictReservedWord(token.value, true) ||
+        sometimesKeywords.has(token.value)
+      ) {
+        return "keyword";
+      }
+
+      if (token.value[0] !== token.value[0].toLowerCase()) {
+        return "capitalized";
+      }
+    }
+
+    if (token.type === "Punctuator" && BRACKET.test(token.value)) {
+      return "uncolored";
+    }
+
+    if (
+      token.type === "Invalid" &&
+      (token.value === "@" || token.value === "#")
+    ) {
+      return "punctuator";
+    }
+
+    switch (token.type) {
+      case "NumericLiteral":
+        return "number";
+
+      case "StringLiteral":
+      case "JSXString":
+      case "NoSubstitutionTemplate":
+        return "string";
+
+      case "RegularExpressionLiteral":
+        return "regex";
+
+      case "Punctuator":
+      case "JSXPunctuator":
+        return "punctuator";
+
+      case "MultiLineComment":
+      case "SingleLineComment":
+        return "comment";
+
+      case "Invalid":
+      case "JSXInvalid":
+        return "invalid";
+
+      case "JSXIdentifier":
+        return "jsxIdentifier";
+
+      default:
+        return "uncolored";
+    }
+  };
+
+  /**
+   * Turn a string of JS into an array of objects.
+   */
+  tokenize = function* (text: string) {
+    for (const token of jsTokens(text, { jsx: true })) {
+      switch (token.type) {
+        case "TemplateHead":
+          yield { type: "string", value: token.value.slice(0, -2) };
+          yield { type: "punctuator", value: "${" };
+          break;
+
+        case "TemplateMiddle":
+          yield { type: "punctuator", value: "}" };
+          yield { type: "string", value: token.value.slice(1, -2) };
+          yield { type: "punctuator", value: "${" };
+          break;
+
+        case "TemplateTail":
+          yield { type: "punctuator", value: "}" };
+          yield { type: "string", value: token.value.slice(1) };
+          break;
+
+        default:
+          yield {
+            type: getTokenType(token),
+            value: token.value,
+          };
+      }
+    }
+  };
+} else {
   // This is only available in js-tokens@4, and not in js-tokens@6
   const { matchToToken } = jsTokensNs;
 
