245b35df738ea1b0a59c42de74d0ec0891cda1a4
Huáng Jùnliàng <jlhwung@gmail.com>
2021-01-19 21:09:03 -0500

    add more test cases

--- packages/babel-parser/test/fixtures/experimental/class-static-block/await-binding-in-static-block/input.js
+++ packages/babel-parser/test/fixtures/experimental/class-static-block/await-binding-in-static-block/input.js
@@ -21,3 +21,7 @@ C = class { static { async ({ [await]: x}) => {} } };
 C = class { static { async ({ [await]: x}) } };
 
 async (x = class { static { await } }) => {};
+
+C = class { static { function await() {} } };
+
+C = class { static { await: 0 } };
