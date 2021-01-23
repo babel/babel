e148e0e259688594c63fe2e37482cc03286deef1
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-01-23 22:22:06 +0100

    Implement `constantReexports` and `enumerableModuleMeta` assumptions (#12618)

---
+++ packages/babel-plugin-transform-modules-amd/test/fixtures/assumption-constantReexports/export-from-4/output.js
@@ -0,0 +1,9 @@
+define(["exports", "foo"], function (_exports, _foo) {
+  "use strict";
+
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.bar = void 0;
+  _exports.bar = _foo.foo;
+});
