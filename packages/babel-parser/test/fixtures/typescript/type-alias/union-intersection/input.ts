9084cf95147562553b352c4bc08daa3b97d7dd2f
Federico Ciardi <fed.ciardi@gmail.com>
2021-02-04 16:25:50 +0100

    test: move tests

--- packages/babel-parser/test/fixtures/typescript/type-alias/union-intersection/input.ts
+++
@@ -1,7 +0,0 @@
-type J = number | string
-
-type F = number & string
-
-type K = | number | string
-
-type M = & number & string
--- packages/babel-parser/test/fixtures/typescript/types/union-intersection/input.ts
+++ packages/babel-parser/test/fixtures/typescript/types/union-intersection/input.ts
@@ -2,3 +2,8 @@ let union: number | null | undefined;
 let intersection: number & string;
 let precedence1: number | string & boolean;
 let precedence2: number & string | boolean;
+
+type J = number | string
+type F = number & string
+type K = | number | string
+type M = & number & string
