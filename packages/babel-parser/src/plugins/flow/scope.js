b9cce0f93f7252d38b43b73f8573b185c871c51c
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-02-10 21:06:52 +0100

    Update packages/babel-parser/src/plugins/flow/scope.js
    [skip ci]

--- packages/babel-parser/src/plugins/flow/scope.js
+++ packages/babel-parser/src/plugins/flow/scope.js
@@ -8,6 +8,7 @@ import {
 } from "../../util/scopeflags";
 import * as N from "../../types";
 
+// Reference implementation: https://github.com/facebook/flow/blob/23aeb2a2ef6eb4241ce178fde5d8f17c5f747fb5/src/typing/env.ml#L536-L584
 class FlowScope extends Scope {
   // declare function foo(): type;
   declareFunctions: string[] = [];
