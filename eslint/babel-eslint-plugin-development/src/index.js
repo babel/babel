04552fb94250bd7df350ec80057a4817132df1e1
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-02-12 17:35:53 +0100

    Avoid using `module` in `src` files

--- eslint/babel-eslint-plugin-development/src/index.js
+++ eslint/babel-eslint-plugin-development/src/index.js
@@ -2,10 +2,10 @@ import noDeprecatedClone from "./rules/no-deprecated-clone";
 import noUndefinedIdentifier from "./rules/no-undefined-identifier";
 import pluginName from "./rules/plugin-name";
 
-module.exports = {
-  rules: {
-    "no-deprecated-clone": noDeprecatedClone,
-    "no-undefined-identifier": noUndefinedIdentifier,
-    "plugin-name": pluginName,
-  },
+export const rules = {
+  "no-deprecated-clone": noDeprecatedClone,
+  "no-undefined-identifier": noUndefinedIdentifier,
+  "plugin-name": pluginName,
 };
+
+export default { rules };
