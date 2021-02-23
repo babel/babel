d35c48f333a4ccc01828356871172c22f1293831
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-02-23 11:54:21 +0100

    Minimal

--- packages/babel-plugin-transform-runtime/test/fixtures/regression/12863/output.mjs
+++ packages/babel-plugin-transform-runtime/test/fixtures/regression/12863/output.mjs
@@ -4,27 +4,23 @@ import _inherits from "@babel/runtime-corejs3/helpers/inherits";
 import _possibleConstructorReturn from "@babel/runtime-corejs3/helpers/possibleConstructorReturn";
 import _getPrototypeOf from "@babel/runtime-corejs3/helpers/getPrototypeOf";
 import _defineProperty from "@babel/runtime-corejs3/helpers/defineProperty";
-import _concatInstanceProperty from "@babel/runtime-corejs3/core-js/instance/concat";
+import _concatInstanceProperty from "@babel/runtime-corejs3/core-js-stable/instance/concat";
 
 function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = _Reflect$construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
 
 function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !_Reflect$construct) return false; if (_Reflect$construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(_Reflect$construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
 
-var B = /*#__PURE__*/function (_A) {
+let B = /*#__PURE__*/function (_A) {
   _inherits(B, _A);
 
   var _super = _createSuper(B);
 
-  function B() {
+  function B(...args) {
     var _context;
 
     var _this;
 
     _classCallCheck(this, B);
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
 
     _this = _super.call.apply(_super, _concatInstanceProperty(_context = [this]).call(_context, args));
 
