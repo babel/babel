6953ce51b9052c6e2f23d97bda633f9ac811d789
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-02-26 23:17:54 +0100

    Put back ESM helpers in a folder where we can use `.js`

--- packages/babel-runtime/helpers/esm/temporalRef.js
+++ packages/babel-runtime/helpers/esm/temporalRef.js
@@ -1,1 +1,5 @@
-export { default } from "../temporalRef/_index.mjs"
+import undef from "./temporalUndefined.js";
+import err from "./tdz.js";
+export default function _temporalRef(val, name) {
+  return val === undef ? err(name) : val;
+}
