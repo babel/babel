51b1c22554b408366603083537a7e9c079f8e00d
Huáng Jùnliàng <jlhwung@gmail.com>
2021-03-02 14:58:32 -0500

    add config end marker

--- packages/babel-core/src/config/config-chain.js
+++ packages/babel-core/src/config/config-chain.js
@@ -261,7 +261,8 @@ export function* buildRootChain(
         // print config by the order of ascending priority
         [configReport, babelRcReport, programmaticReport]
           .filter(x => !!x)
-          .join("\n\n"),
+          .join("\n\n") +
+        "\n-----End Babel configs-----",
     );
   }
   // Insert file chain in front so programmatic options have priority
