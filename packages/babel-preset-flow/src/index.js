16d83002defc74dcbf0d73d8b71e9646acf04f16
Huáng Jùnliàng <jlhwung@gmail.com>
2021-03-25 19:33:02 -0400

    [babel 8] type checking preset-flow options (#12751)
    Co-authored-by: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>

--- packages/babel-preset-flow/src/index.js
+++ packages/babel-preset-flow/src/index.js
@@ -1,8 +1,10 @@
 import { declare } from "@babel/helper-plugin-utils";
 import transformFlowStripTypes from "@babel/plugin-transform-flow-strip-types";
+import normalizeOptions from "./normalize-options";
 
-export default declare((api, { all, allowDeclareFields }) => {
+export default declare((api, opts) => {
   api.assertVersion(7);
+  const { all, allowDeclareFields } = normalizeOptions(opts);
 
   return {
     plugins: [[transformFlowStripTypes, { all, allowDeclareFields }]],
---
+++ packages/babel-preset-flow/test/normalize-options.spec.js
@@ -0,0 +1,55 @@
+import normalizeOptions from "../src/normalize-options";
+describe("normalize options", () => {
+  (process.env.BABEL_8_BREAKING ? describe : describe.skip)("Babel 8", () => {
+    it("should throw on unknown options", () => {
+      expect(() => normalizeOptions({ al: true }))
+        .toThrowErrorMatchingInlineSnapshot(`
+        "@babel/preset-flow: 'al' is not a valid top-level option.
+        - Did you mean 'all'?"
+      `);
+    });
+    it("should throw on Babel 7 `allowDeclareFields` option", () => {
+      expect(() =>
+        normalizeOptions({ allowDeclareFields: true }),
+      ).toThrowErrorMatchingInlineSnapshot(
+        `"@babel/preset-flow: Since Babel 8, \`declare property: A\` is always supported, and the \\"allowDeclareFields\\" option is no longer available. Please remove it from your config."`,
+      );
+    });
+    it.each(["all"])("should throw when `%p` is not a boolean", optionName => {
+      expect(() => normalizeOptions({ [optionName]: 0 })).toThrow(
+        `@babel/preset-flow: '${optionName}' option must be a boolean.`,
+      );
+    });
+    it("should not throw when options is not defined", () => {
+      expect(() => normalizeOptions()).not.toThrowError();
+    });
+    it("default values", () => {
+      expect(normalizeOptions({})).toMatchInlineSnapshot(`
+        Object {
+          "all": undefined,
+        }
+      `);
+    });
+  });
+  (process.env.BABEL_8_BREAKING ? describe.skip : describe)("Babel 7", () => {
+    it("should not throw on unknown options", () => {
+      expect(() =>
+        normalizeOptions({ allDeclareField: true }),
+      ).not.toThrowError();
+    });
+    it.each(["all", "allowDeclareFields"])(
+      "should not throw when `%p` is not a boolean",
+      optionName => {
+        expect(() => normalizeOptions({ [optionName]: 0 })).not.toThrowError();
+      },
+    );
+    it("default values", () => {
+      expect(normalizeOptions({})).toMatchInlineSnapshot(`
+        Object {
+          "all": undefined,
+          "allowDeclareFields": undefined,
+        }
+      `);
+    });
+  });
+});
