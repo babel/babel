f53f69a9c83ea3a093c54eb0d9a7378d10c5b0c7
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-03-31 18:26:01 +0200

    Update Babel 8 fixtures

--- packages/babel-preset-env/test/fixtures/corejs3/usage-object-destructuring/output.mjs
+++ packages/babel-preset-env/test/fixtures/corejs3/usage-object-destructuring/output.mjs
@@ -1,8 +1,8 @@
 import "core-js/modules/es.function.bind.js";
 import "core-js/modules/es.object.entries.js";
+import "core-js/modules/es.date.to-string.js";
 import "core-js/modules/es.object.to-string.js";
 import "core-js/modules/es.regexp.to-string.js";
-import "core-js/modules/es.date.to-string.js";
 import "core-js/modules/es.string.iterator.js";
 import "core-js/modules/es.array.iterator.js";
 import "core-js/modules/web.dom-collections.iterator.js";
--- packages/babel-preset-env/test/fixtures/corejs3/usage-promise-finally/output.mjs
+++ packages/babel-preset-env/test/fixtures/corejs3/usage-promise-finally/output.mjs
@@ -1,5 +1,5 @@
-import "core-js/modules/es.promise.js";
 import "core-js/modules/es.object.to-string.js";
+import "core-js/modules/es.promise.js";
 import "core-js/modules/es.promise.finally.js";
 var p = Promise.resolve(0);
 p.finally(function () {
