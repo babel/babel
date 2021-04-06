a1186624e8996ca50f6c924d616b8e4a1540c6a7
Pig Fang <g-plane@hotmail.com>
2021-04-06 23:38:31 +0800

    add more tests

--- packages/babel-parser/test/fixtures/typescript/class/modifiers-override/input.ts
+++ packages/babel-parser/test/fixtures/typescript/class/modifiers-override/input.ts
@@ -5,4 +5,13 @@ class MyClass extends BaseClass {
   override size = 5;
   override readonly size = 5;
   readonly override size = 5;
+
+  override get text() {}
+  override set text(value) {}
+
+  override async fetch() {}
+}
+
+declare class DeclaredClass extends BaseClass {
+  override test() {}
 }
