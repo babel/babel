f8ce04de302b6b0f293d5ac7d0d7e8acd94e99f5
Pig Fang <g-plane@hotmail.com>
2021-04-06 23:33:55 +0800

    split tests

---
+++ packages/babel-parser/test/fixtures/typescript/class/modifiers-override-errors/input.ts
@@ -0,0 +1,6 @@
+class MyClass2 extends BaseClass {
+  override constructor() {}
+  override [x: string]: any;
+  override static size = 5;
+  static override size = 5;
+}
--- packages/babel-parser/test/fixtures/typescript/class/modifiers-override/input.ts
+++ packages/babel-parser/test/fixtures/typescript/class/modifiers-override/input.ts
@@ -1,15 +1,8 @@
-class MyClass1 extends BaseClass {
+class MyClass extends BaseClass {
   override show() {}
   override public show() {}
   public override show() {}
   override size = 5;
   override readonly size = 5;
   readonly override size = 5;
-}
-
-class MyClass2 extends BaseClass {
-  override constructor() {}
-  override [x: string]: any;
-  override static size = 5;
-  static override size = 5;
 }
