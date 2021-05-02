808a0e619ba617859069b1963647f9f3d06e9062
overlookmotel <theoverlookmotel@gmail.com>
2021-05-02 19:45:12 +0100

    Shorten test

--- packages/babel-plugin-transform-block-scoping/test/fixtures/const-violations/assignment-expression/input.js
+++ packages/babel-plugin-transform-block-scoping/test/fixtures/const-violations/assignment-expression/input.js
@@ -1,19 +1,7 @@
 const a = 5;
 let b = 0;
 a += b++;
-
-const c = 32;
-let d = 1;
-c >>>= ++d;
-
-const e = 0;
-let f = 1;
-e ||= ++f;
-
-const g = 0;
-let h = 1;
-g &&= ++h;
-
-const i = 0;
-let j = 1;
-i ??= ++j;
+a >>>= b++;
+a ||= b++;
+a &&= b++;
+a ??= b++;
