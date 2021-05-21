c5c4098f80900033d6fc95c71ffb08f3a101a9e7
Bogdan Savluk <savluk.bogdan@gmail.com>
2021-05-21 19:22:57 +0200

    babel-preset-env

--- packages/babel-preset-env/src/normalize-options.ts
+++ packages/babel-preset-env/src/normalize-options.ts
@@ -115,6 +115,7 @@ export const checkDuplicateIncludeExcludes = (
 const normalizeTargets = (targets): Options["targets"] => {
   // TODO: Allow to use only query or strings as a targets from next breaking change.
   if (typeof targets === "string" || Array.isArray(targets)) {
+    // @ts-expect-error
     return { browsers: targets };
   }
   return { ...targets };
