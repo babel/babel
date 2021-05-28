c9823c517728a383b45387ab1b7205217fe67956
J. S. Choi <jschoi@jschoi.org>
2021-05-28 03:04:54 -0400

    plugin-proposal-pipeline: Revert stylistic tweaks to smartVisitor
    See https://github.com/babel/babel/pull/13191#discussion_r640982581.

--- packages/babel-plugin-proposal-pipeline-operator/src/smartVisitor.js
+++ packages/babel-plugin-proposal-pipeline-operator/src/smartVisitor.js
@@ -4,56 +4,45 @@ const updateTopicReferenceVisitor = {
   PipelinePrimaryTopicReference(path) {
     path.replaceWith(t.cloneNode(this.topicId));
   },
-
   PipelineTopicExpression(path) {
     path.skip();
   },
 };
 
-export default {
+const smartVisitor = {
   BinaryExpression(path) {
     const { scope } = path;
     const { node } = path;
     const { operator, left, right } = node;
-
     if (operator !== "|>") return;
 
-    const topicVariable = scope.generateUidIdentifierBasedOnNode(left);
-    scope.push({ id: topicVariable });
+    const placeholder = scope.generateUidIdentifierBasedOnNode(left);
+    scope.push({ id: placeholder });
 
-    const typeDispatcher = {
-      PipelineTopicExpression() {
-        path
-          .get("right")
-          .traverse(updateTopicReferenceVisitor, { topicId: topicVariable });
+    let call;
+    if (t.isPipelineTopicExpression(right)) {
+      path
+        .get("right")
+        .traverse(updateTopicReferenceVisitor, { topicId: placeholder });
 
-        return right.expression;
-      },
+      call = right.expression;
+    } else {
+      // PipelineBareFunction
+      let callee = right.callee;
+      if (t.isIdentifier(callee, { name: "eval" })) {
+        callee = t.sequenceExpression([t.numericLiteral(0), callee]);
+      }
 
-      PipelineBareFunction() {
-        let callee = right.callee;
-        if (t.isIdentifier(callee, { name: "eval" })) {
-          callee = t.sequenceExpression([t.numericLiteral(0), callee]);
-        }
-
-        return t.callExpression(callee, [t.cloneNode(topicVariable)]);
-      },
-    };
-
-    const throwInvalidTypeError = type => {
-      throw new TypeError(`Invalid smart-pipe body type "${type}."`);
-    };
-
-    const typeDispatcherMethod =
-      typeDispatcher[right.type] || throwInvalidTypeError();
-
-    const bodySubexpr = typeDispatcherMethod();
+      call = t.callExpression(callee, [t.cloneNode(placeholder)]);
+    }
 
     path.replaceWith(
       t.sequenceExpression([
-        t.assignmentExpression("=", t.cloneNode(topicVariable), left),
-        bodySubexpr,
+        t.assignmentExpression("=", t.cloneNode(placeholder), left),
+        call,
       ]),
     );
   },
 };
+
+export default smartVisitor;
