24218dda2e5c9b847982ec70e999438351685d67
J. S. Choi <jschoi@jschoi.org>
2021-05-28 08:58:53 -0400

    parser/plugin-pipeline: Tests for topic-ref identity

--- packages/babel-plugin-proposal-pipeline-operator/test/fixtures/hack-hash/within-for-classic-statement-init/output.js
+++ packages/babel-plugin-proposal-pipeline-operator/test/fixtures/hack-hash/within-for-classic-statement-init/output.js
@@ -1,5 +1,9 @@
 let sum = 0;
-for (var i = 0 |> #; i <= 10; i++)
+
+for (var i = (_ref = 0, _ref); i <= 10; i++) {
+  var _ref;
+
   sum += i;
+}
 
 expect(sum).toBe(10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1);
