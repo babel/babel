d0f2dfd540320fadb277eea2737dce9be9df014b
Huáng Jùnliàng <jlhwung@gmail.com>
2021-06-03 18:13:58 -0400

    chore: specify @babel/eslint-parser deps

--- eslint/babel-eslint-shared-fixtures/utils/RuleTester.js
+++ eslint/babel-eslint-shared-fixtures/utils/RuleTester.js
@@ -1,15 +1,12 @@
-const path = require("path");
 const { RuleTester } = require("eslint");
 
 RuleTester.setDefaultConfig({
-  parser: require.resolve("../../babel-eslint-parser"),
+  parser: require.resolve("@babel/eslint-parser"),
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 11,
     babelOptions: {
-      configFile: require.resolve(
-        path.join(__dirname, "../config/babel.config.js")
-      ),
+      configFile: require.resolve("../config/babel.config.js"),
     },
   },
 });
