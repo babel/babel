fad6be15455f160619410167cc3d8c41a47bb3d4
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-06-14 16:52:54 +0200

    Fix lint

--- packages/babel-helper-create-class-features-plugin/src/fields.ts
+++ packages/babel-helper-create-class-features-plugin/src/fields.ts
@@ -671,8 +671,9 @@ const thisContextVisitor = traverse.visitors.merge([
 
 const innerReferencesVisitor = {
   ReferencedIdentifier(path, state) {
-    const { name } = path.node;
-    if (path.scope.bindingIdentifierEquals(path.node.name, state.innerBinding)) {
+    if (
+      path.scope.bindingIdentifierEquals(path.node.name, state.innerBinding)
+    ) {
       state.needsClassRef = true;
       path.node.name = state.classRef.name;
     }
