1e19f1ac39983546fb52d7f17ff4adc0e35440a5
sosukesuzuki <aosukeke@gmail.com>
2021-06-15 02:15:33 +0900

    Follow up formatting by Prettier 2.3.1

--- packages/babel-plugin-transform-typescript/src/namespace.ts
+++ packages/babel-plugin-transform-typescript/src/namespace.ts
@@ -69,11 +69,8 @@ function handleVariableDeclaration(
   const { declarations } = node;
   if (
     declarations.every(
-      (
-        declarator,
-      ): declarator is t.VariableDeclarator & {
-        id: t.Identifier;
-      } => t.isIdentifier(declarator.id),
+      (declarator): declarator is t.VariableDeclarator & { id: t.Identifier } =>
+        t.isIdentifier(declarator.id),
     )
   ) {
     // `export const a = 1` transforms to `const a = N.a = 1`, the output
