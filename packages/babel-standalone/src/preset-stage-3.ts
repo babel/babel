608ac7a94ab07a42591accac2741b04cae751663
sosukesuzuki <aosukeke@gmail.com>
2021-06-17 15:16:47 +0900

    Remove options from preset-stage-3

--- packages/babel-standalone/src/preset-stage-3.ts
+++ packages/babel-standalone/src/preset-stage-3.ts
@@ -1,13 +1,6 @@
 import * as babelPlugins from "./generated/plugins";
 
-export default (_: any, opts: any) => {
-  let loose = false;
-
-  if (opts !== undefined) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    if (opts.loose !== undefined) loose = opts.loose;
-  }
-
+export default () => {
   return {
     plugins: [
       babelPlugins.syntaxImportAssertions,
