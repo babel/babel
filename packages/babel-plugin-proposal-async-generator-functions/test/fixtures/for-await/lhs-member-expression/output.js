d452c97a2bf3fc91e29be6acb58a61722385b400
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-06-19 16:24:16 +0200

    Compile to generators

--- packages/babel-plugin-proposal-async-generator-functions/test/fixtures/for-await/lhs-member-expression/output.js
+++ packages/babel-plugin-proposal-async-generator-functions/test/fixtures/for-await/lhs-member-expression/output.js
@@ -1,11 +1,11 @@
-(async function () {
+babelHelpers.asyncToGenerator(function* () {
   var _iteratorAbruptCompletion = false;
   var _didIteratorError = false;
 
   var _iteratorError;
 
   try {
-    for (var _iterator = babelHelpers.asyncIterator(y), _step; _iteratorAbruptCompletion = !(_step = await _iterator.next()).done; _iteratorAbruptCompletion = false) {
+    for (var _iterator = babelHelpers.asyncIterator(y), _step; _iteratorAbruptCompletion = !(_step = yield _iterator.next()).done; _iteratorAbruptCompletion = false) {
       obj.x = _step.value;
     }
   } catch (err) {
@@ -14,7 +14,7 @@
   } finally {
     try {
       if (_iteratorAbruptCompletion && _iterator.return != null) {
-        await _iterator.return();
+        yield _iterator.return();
       }
     } finally {
       if (_didIteratorError) {
