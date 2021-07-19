23d4f8c5633ef4b347be38ae91dcd6ba78fd1d58
dhrubesh <dhrubesh97@gmail.com>
2021-07-19 17:15:01 +0530

    Derived constructors should not be allowed to return primitives (#13571)
    Closes gh-13571

---
+++ packages/babel-plugin-transform-classes/test/fixtures/spec/derived-constructor-no-primitive-return/output.js
@@ -0,0 +1,17 @@
+var Foo = /*#__PURE__*/function (_Bar) {
+  "use strict";
+
+  babelHelpers.inherits(Foo, _Bar);
+
+  var _super = babelHelpers.createSuper(Foo);
+
+  function Foo() {
+    var _this;
+
+    babelHelpers.classCallCheck(this, Foo);
+    _this = _super.call(this);
+    return babelHelpers.possibleConstructorReturn(_this, 3);
+  }
+
+  return Foo;
+}(Bar);
