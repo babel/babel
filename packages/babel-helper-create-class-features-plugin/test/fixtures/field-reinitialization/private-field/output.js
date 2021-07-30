06300db45d617986c85831fb3876a09bed87c9e2
Felipe Armoni <felipe.armoni@gmail.com>
2021-07-30 10:25:41 -0300

    Fixed unit test

--- packages/babel-helper-create-class-features-plugin/test/fixtures/field-reinitialization/private-field/output.js
+++ packages/babel-helper-create-class-features-plugin/test/fixtures/field-reinitialization/private-field/output.js
@@ -1,32 +1,3 @@
-'use strict';
-
-function _classPrivateFieldGet(receiver, privateMap) {
-  var descriptor = _classExtractFieldDescriptor(receiver, privateMap, 'get');
-  return _classApplyDescriptorGet(receiver, descriptor);
-}
-
-function _classExtractFieldDescriptor(receiver, privateMap, action) {
-  if (!privateMap.has(receiver)) {
-    throw new TypeError(
-      'attempted to ' + action + ' private field on non-instance'
-    );
-  }
-  return privateMap.get(receiver);
-}
-
-function _classApplyDescriptorGet(receiver, descriptor) {
-  if (descriptor.get) {
-    return descriptor.get.call(receiver);
-  }
-  return descriptor.value;
-}
-
-function _checkPrivateFieldInitSpec(obj, privateMap) {
-  if (privateMap.has(obj)) {
-    throw new TypeError();
-  }
-}
-
 class Base {
   constructor(obj) {
     return obj;
@@ -41,14 +12,14 @@ var _foo = /*#__PURE__*/new WeakMap();
 class Derived extends Base {
   constructor(...args) {
     super(...args);
-
-    _privateFieldInitSpec(this, _foo, {
+    babelHelpers.classPrivateFieldInitSpec(this, _foo, {
       writable: true,
-      value: ++counter,
+      value: ++counter
     });
   }
 
   static get(obj) {
-    return babelHelpers._classPrivateFieldGet(obj, _foo);
+    return babelHelpers.classPrivateFieldGet(obj, _foo);
   }
+
 }
