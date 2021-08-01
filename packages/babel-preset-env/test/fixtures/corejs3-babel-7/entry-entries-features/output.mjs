93bade2a719c82d553443cb236a2789c0a141de9
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-08-01 09:22:09 +0200

    Update `core-js@3` packages (#13621)
    * Update `core-js@3` packages
    
    * Update fixtures
    
    * Update Babel 8 fixtures

--- packages/babel-preset-env/test/fixtures/corejs3-babel-7/entry-entries-features/output.mjs
+++ packages/babel-preset-env/test/fixtures/corejs3-babel-7/entry-entries-features/output.mjs
@@ -1,3 +1,4 @@
+import "core-js/modules/es.array.iterator.js";
 import "core-js/modules/es.object.to-string.js";
 import "core-js/modules/es.set.js";
 import "core-js/modules/es.string.iterator.js";
@@ -19,8 +20,8 @@ import "core-js/modules/esnext.set.reduce.js";
 import "core-js/modules/esnext.set.some.js";
 import "core-js/modules/esnext.set.symmetric-difference.js";
 import "core-js/modules/esnext.set.union.js";
-import "core-js/modules/web.dom-collections.iterator.js";
 import "core-js/modules/es.array.from.js";
-import "core-js/modules/es.array.iterator.js";
 import "core-js/modules/es.object.from-entries.js";
+import "core-js/modules/es.regexp.exec.js";
+import "core-js/modules/es.string.replace.js";
 import "core-js/modules/esnext.string.replace-all.js";
