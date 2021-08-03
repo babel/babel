4e4e7888b732c426980510035a0702a4f633d303
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-08-03 22:57:11 +0200

    Undo breaking change

--- packages/babel-standalone/src/preset-stage-3.ts
+++ packages/babel-standalone/src/preset-stage-3.ts
@@ -1,11 +1,27 @@
 import * as babelPlugins from "./generated/plugins";
 
-export default () => {
-  return {
-    plugins: [
-      babelPlugins.syntaxImportAssertions,
-      babelPlugins.proposalClassStaticBlock,
+export default (_: any, { loose = false } = {}) => {
+  const plugins = [
+    babelPlugins.syntaxImportAssertions,
+    babelPlugins.proposalClassStaticBlock,
+  ];
+
+  if (!process.env.BABEL_8_BREAKING) {
+    // These are Stage 4
+    plugins.push(
+      babelPlugins.syntaxImportMeta,
+      babelPlugins.syntaxTopLevelAwait,
+      babelPlugins.proposalExportNamespaceFrom,
+      babelPlugins.proposalLogicalAssignmentOperators,
+      [babelPlugins.proposalOptionalChaining, { loose }],
+      [babelPlugins.proposalNullishCoalescingOperator, { loose }],
+      [babelPlugins.proposalClassProperties, { loose }],
+      babelPlugins.proposalJsonStrings,
+      babelPlugins.proposalNumericSeparator,
+      [babelPlugins.proposalPrivateMethods, { loose }],
       babelPlugins.proposalPrivatePropertyInObject,
-    ],
-  };
+    );
+  }
+
+  return { plugins };
 };
