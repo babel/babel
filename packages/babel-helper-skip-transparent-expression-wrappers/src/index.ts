e0c3969a241599d550335dc37be7461a2f8519b8
Mickey Rose <lightmare@users.noreply.github.com>
2021-08-20 11:08:05 +0200

    convert `@babel/helper-skip-transparent-expression-wrappers` to typescript (#13686)

--- packages/babel-helper-skip-transparent-expression-wrappers/src/index.ts
+++ packages/babel-helper-skip-transparent-expression-wrappers/src/index.ts
@@ -1,5 +1,3 @@
-// @flow
-
 import {
   isParenthesizedExpression,
   isTSAsExpression,
@@ -7,14 +5,25 @@ import {
   isTSTypeAssertion,
   isTypeCastExpression,
 } from "@babel/types";
+
+import type * as t from "@babel/types";
 import type { NodePath } from "@babel/traverse";
+
+export type TransparentExprWrapper =
+  | t.TSAsExpression
+  | t.TSTypeAssertion
+  | t.TSNonNullExpression
+  | t.TypeCastExpression
+  | t.ParenthesizedExpression;
 
 // A transparent expression wrapper is an AST node that most plugins will wish
 // to skip, as its presence does not affect the behaviour of the code. This
 // includes expressions used for types, and extra parenthesis. For example, in
 // (a as any)(), this helper can be used to skip the TSAsExpression when
 // determining the callee.
-export function isTransparentExprWrapper(node: Node) {
+export function isTransparentExprWrapper(
+  node: t.Node,
+): node is TransparentExprWrapper {
   return (
     isTSAsExpression(node) ||
     isTSTypeAssertion(node) ||
@@ -24,10 +33,11 @@ export function isTransparentExprWrapper(node: Node) {
   );
 }
 
-export function skipTransparentExprWrappers(path: NodePath): NodePath {
+export function skipTransparentExprWrappers(
+  path: NodePath<t.Expression>,
+): NodePath<t.Expression> {
   while (isTransparentExprWrapper(path.node)) {
     path = path.get("expression");
   }
-
   return path;
 }
