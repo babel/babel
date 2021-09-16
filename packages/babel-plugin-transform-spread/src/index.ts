89268255aad7aaab38d1a235e3df7a491f1c9f60
wafuwafu13 <mariobaske@i.softbank.jp>
2021-09-16 18:06:08 +0900

    convert @babel/plugin-transform-spread to ts

--- packages/babel-plugin-transform-spread/src/index.ts
+++ packages/babel-plugin-transform-spread/src/index.ts
@@ -1,6 +1,7 @@
 import { declare } from "@babel/helper-plugin-utils";
 import { skipTransparentExprWrappers } from "@babel/helper-skip-transparent-expression-wrappers";
-import { types as t } from "@babel/core";
+import { types as t, File } from "@babel/core";
+import type { NodePath, Scope } from "@babel/traverse";
 
 export default declare((api, options) => {
   api.assertVersion(7);
@@ -9,7 +10,10 @@ export default declare((api, options) => {
   const arrayLikeIsIterable =
     options.allowArrayLike ?? api.assumption("arrayLikeIsIterable");
 
-  function getSpreadLiteral(spread, scope) {
+  function getSpreadLiteral(
+    spread: t.SpreadElement,
+    scope: Scope,
+  ): t.SpreadElement["argument"] {
     if (
       iterableIsArray &&
       !t.isIdentifier(spread.argument, { name: "arguments" })
@@ -20,11 +24,18 @@ export default declare((api, options) => {
     }
   }
 
-  function hasHole(spread) {
+  function hasHole(spread: t.ArrayExpression): boolean {
     return spread.elements.some(el => el === null);
   }
 
-  function hasSpread(nodes) {
+  function hasSpread(
+    nodes: Array<
+      | t.Expression
+      | t.SpreadElement
+      | t.JSXNamespacedName
+      | t.ArgumentPlaceholder
+    >,
+  ): boolean {
     for (let i = 0; i < nodes.length; i++) {
       if (t.isSpreadElement(nodes[i])) {
         return true;
@@ -33,15 +44,41 @@ export default declare((api, options) => {
     return false;
   }
 
-  function push(_props, nodes) {
+  function push(
+    _props: Array<
+      | t.Expression
+      | t.SpreadElement
+      | t.JSXNamespacedName
+      | t.ArgumentPlaceholder
+    >,
+    nodes: Array<t.Expression>,
+  ): Array<
+    t.Expression | t.SpreadElement | t.JSXNamespacedName | t.ArgumentPlaceholder
+  > {
     if (!_props.length) return _props;
-    nodes.push(t.arrayExpression(_props));
+    nodes.push(
+      t.arrayExpression(_props as Array<t.Expression | t.SpreadElement>),
+    );
     return [];
   }
 
-  function build(props: Array, scope, file) {
-    const nodes = [];
-    let _props = [];
+  function build(
+    props: Array<
+      | t.Expression
+      | t.SpreadElement
+      | t.JSXNamespacedName
+      | t.ArgumentPlaceholder
+    >,
+    scope: Scope,
+    file: File,
+  ): t.Expression[] {
+    const nodes: Array<t.Expression> = [];
+    let _props: Array<
+      | t.Expression
+      | t.SpreadElement
+      | t.JSXNamespacedName
+      | t.ArgumentPlaceholder
+    > = [];
 
     for (const prop of props) {
       if (t.isSpreadElement(prop)) {
@@ -74,7 +111,7 @@ export default declare((api, options) => {
     name: "transform-spread",
 
     visitor: {
-      ArrayExpression(path) {
+      ArrayExpression(path: NodePath<t.ArrayExpression>): void {
         const { node, scope } = path;
         const elements = node.elements;
         if (!hasSpread(elements)) return;
@@ -86,7 +123,10 @@ export default declare((api, options) => {
         // the element was transformed (Array.prototype.slice.call or toConsumableArray)
         // we know that the transformed code already takes care of cloning the array.
         // So we can simply return that element.
-        if (nodes.length === 1 && first !== elements[0].argument) {
+        if (
+          nodes.length === 1 &&
+          first !== (elements[0] as t.SpreadElement).argument
+        ) {
           path.replaceWith(first);
           return;
         }
@@ -110,15 +150,14 @@ export default declare((api, options) => {
           ),
         );
       },
-
-      CallExpression(path) {
+      CallExpression(path: NodePath<t.CallExpression>): void {
         const { node, scope } = path;
 
         const args = node.arguments;
         if (!hasSpread(args)) return;
-
-        const calleePath = skipTransparentExprWrappers(path.get("callee"));
-
+        const calleePath = skipTransparentExprWrappers(
+          path.get("callee") as NodePath<t.Expression>,
+        );
         if (calleePath.isSuper()) {
           // NOTE: spread and classes have almost the same compat data, so this is very unlikely to happen in practice.
           throw path.buildCodeFrameError(
@@ -126,14 +165,20 @@ export default declare((api, options) => {
               "Please add '@babel/plugin-transform-classes' to your Babel configuration.",
           );
         }
-
-        let contextLiteral = scope.buildUndefinedNode();
-
+        let contextLiteral = scope.buildUndefinedNode() as
+          | t.Expression
+          | t.UnaryExpression
+          | t.ThisExpression
+          | t.Identifier;
         node.arguments = [];
 
-        let nodes;
-        if (args.length === 1 && args[0].argument.name === "arguments") {
-          nodes = [args[0].argument];
+        let nodes: t.Expression[];
+        if (
+          args.length === 1 &&
+          ((args[0] as t.SpreadElement).argument as t.Identifier).name ===
+            "arguments"
+        ) {
+          nodes = [(args[0] as t.SpreadElement).argument];
         } else {
           nodes = build(args, scope, this);
         }
@@ -150,7 +195,7 @@ export default declare((api, options) => {
           node.arguments.push(first);
         }
 
-        const callee = calleePath.node;
+        const callee = calleePath.node as t.MemberExpression;
 
         if (calleePath.isMemberExpression()) {
           const temp = scope.maybeGenerateMemoised(callee.object);
@@ -163,8 +208,10 @@ export default declare((api, options) => {
         }
 
         // We use the original callee here, to preserve any types/parentheses
-        node.callee = t.memberExpression(node.callee, t.identifier("apply"));
-
+        node.callee = t.memberExpression(
+          node.callee as t.Expression,
+          t.identifier("apply"),
+        );
         if (t.isSuper(contextLiteral)) {
           contextLiteral = t.thisExpression();
         }
@@ -172,7 +219,7 @@ export default declare((api, options) => {
         node.arguments.unshift(t.cloneNode(contextLiteral));
       },
 
-      NewExpression(path) {
+      NewExpression(path: NodePath<t.NewExpression>): void {
         const { node, scope } = path;
         let args = node.arguments;
         if (!hasSpread(args)) return;
@@ -185,15 +232,15 @@ export default declare((api, options) => {
           args = t.callExpression(
             t.memberExpression(first, t.identifier("concat")),
             nodes,
-          );
+          ) as any;
         } else {
-          args = first;
+          args = first as any;
         }
 
         path.replaceWith(
           t.callExpression(path.hub.addHelper("construct"), [
             node.callee,
-            args,
+            args as any,
           ]),
         );
       },
