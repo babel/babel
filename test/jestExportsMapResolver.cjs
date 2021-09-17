c4eab3827f9b28ba90924c5ab44c0b08b069795c
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-09-17 14:33:55 +0200

    Update Jest to `27.2.0`

--- test/jestExportsMapResolver.cjs
+++ test/jestExportsMapResolver.cjs
@@ -1,11 +1,27 @@
 // Temporary workaround for https://github.com/facebook/jest/issues/9771
 // Source: https://github.com/facebook/jest/issues/9771#issuecomment-841624042
 
-const resolver = require("enhanced-resolve").create.sync({
-  conditionNames: ["node", "require", "default"],
-  extensions: [".js", ".json", ".node", ".ts"],
-});
+const enhancedResolve = require("enhanced-resolve");
+
+const EXTENSIONS = [".js", ".json", ".node", ".ts"];
+
+const resolvers = new Map();
+function getResolver(conditionNames) {
+  const key = conditionNames.join(":::");
+
+  let resolver = resolvers.get(key);
+  if (!resolver) {
+    resolver = enhancedResolve.create.sync({
+      conditionNames,
+      extensions: EXTENSIONS,
+    });
+    resolvers.set(key, resolver);
+  }
+
+  return resolver;
+}
 
 module.exports = function (request, options) {
+  const resolver = getResolver(options.conditions || ["default"]);
   return resolver(options.basedir, request);
 };
