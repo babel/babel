a77931f0f7ad74beeddaf99dcad98be2dae9c3aa
Huáng Jùnliàng <jlhwung@gmail.com>
2021-10-04 17:12:28 -0400

    fix: restore context after enter / traverse

--- packages/babel-traverse/src/path/context.ts
+++ packages/babel-traverse/src/path/context.ts
@@ -61,6 +61,14 @@ export function isDenylisted(this: NodePath): boolean {
 // TODO: Remove in Babel 8
 export { isDenylisted as isBlacklisted };
 
+function restoreContext(path: NodePath, context: TraversalContext) {
+  if (path.context !== context) {
+    path.context = context;
+    path.state = context.state;
+    path.opts = context.opts;
+  }
+}
+
 export function visit(this: NodePath): boolean {
   if (!this.node) {
     return false;
@@ -74,6 +82,7 @@ export function visit(this: NodePath): boolean {
     return false;
   }
 
+  const currentContext = this.context;
   // Note: We need to check "this.shouldSkip" first because
   // another visitor can set it to true. Usually .shouldSkip is false
   // before calling the enter visitor, but it can be true in case of
@@ -83,6 +92,7 @@ export function visit(this: NodePath): boolean {
     this.debug("Skip...");
     return this.shouldStop;
   }
+  restoreContext(this, currentContext);
 
   this.debug("Recursing into...");
   traverse.node(
@@ -93,6 +103,8 @@ export function visit(this: NodePath): boolean {
     this,
     this.skipKeys,
   );
+
+  restoreContext(this, currentContext);
 
   this.call("exit");
 
