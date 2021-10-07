6029252bec3e9c091d7cf8ff0b10b4c92953ca5f
overlookmotel <theoverlookmotel@gmail.com>
2021-10-07 22:34:12 +0100

    Fix missing inner comments in function expressions (#13825)

---
+++ packages/babel-generator/test/fixtures/comments/function-inner-comment/output.js
@@ -0,0 +1,7 @@
+const f = function /*foo*/ () {};
+const g = async function /*foo*/ () {};
+const h = async function /*foo*/ () {};
+const i = function* /*foo*/ () {};
+const j = function* /*foo*/ () {};
+const k = async function* /*foo*/ () {};
+const l = function /*foo*/ () {};
