b9ea196259deda9f77f23a7f37309936c7bc9198
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-10-07 23:57:25 +0200

    Await promises from sync iterators with `for-await` (#13824)
    Co-authored-by: Mickey Rose <lightmare@users.noreply.github.com>

--- packages/babel-helpers/src/helpers.ts
+++ packages/babel-helpers/src/helpers.ts
@@ -16,20 +16,6 @@ const helper = (minVersion: string) => (tpl: TemplateStringsArray) => ({
   ast: () => template.program.ast(tpl),
 });
 
-helpers.asyncIterator = helper("7.0.0-beta.0")`
-  export default function _asyncIterator(iterable) {
-    var method;
-    if (typeof Symbol !== "undefined") {
-      if (Symbol.asyncIterator) method = iterable[Symbol.asyncIterator];
-      if (method == null && Symbol.iterator) method = iterable[Symbol.iterator];
-    }
-    if (method == null) method = iterable["@@asyncIterator"];
-    if (method == null) method = iterable["@@iterator"]
-    if (method == null) throw new TypeError("Object is not async iterable");
-    return method.call(iterable);
-  }
-`;
-
 helpers.AwaitValue = helper("7.0.0-beta.0")`
   export default function _AwaitValue(value) {
     this.wrapped = value;
