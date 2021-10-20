28ce4494814c550209e17916619cb5f9fe098097
Huáng Jùnliàng <jlhwung@gmail.com>
2021-10-20 09:46:01 -0400

    revert bugfixes targets update

--- packages/babel-compat-data/scripts/build-bugfixes-targets.js
+++ packages/babel-compat-data/scripts/build-bugfixes-targets.js
@@ -22,19 +22,17 @@ const allReplacedFeatures = {};
 const has = Function.call.bind(Object.hasOwnProperty);
 
 for (const [plugin, { replaces, features }] of Object.entries(data)) {
-  for (const replace of replaces) {
-    if (!has(overlappingPlugins, replace)) {
-      overlappingPlugins[replace] = [];
-      generatedTargets[replace] = {};
-      allReplacedFeatures[replace] = [];
-    }
-
-    allReplacedFeatures[replace].push(...features);
-
-    overlappingPlugins[replace].push(plugin);
+  if (!has(overlappingPlugins, replaces)) {
+    overlappingPlugins[replaces] = [];
+    generatedTargets[replaces] = {};
+    allReplacedFeatures[replaces] = [];
   }
-
   generatedTargets[plugin] = {};
+
+  allReplacedFeatures[replaces].push(...features);
+
+  overlappingPlugins[replaces].push(plugin);
+
   for (const env of environments) {
     const supportedWithBugfix = getLowestImplementedVersion({ features }, env);
     if (supportedWithBugfix) {
