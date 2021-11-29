d27f33d0dd54794b38854bfdb6dbca8e9fae8143
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-11-29 22:15:55 +0100

    Review

--- packages/babel-helper-environment-visitor/src/index.ts
+++ packages/babel-helper-environment-visitor/src/index.ts
@@ -4,10 +4,9 @@ import type * as t from "@babel/types";
 
 // TODO (Babel 8): Don't export this function.
 export function skipAllButComputedKey(
-  path: NodePath<t.Method | t.ClassProperty | t.ClassPrivateProperty>,
+  path: NodePath<t.Method | t.ClassProperty>,
 ) {
   // If the path isn't computed, just skip everything.
-  // @ts-expect-error todo(flow->ts) check node type before cheking the property
   if (!path.node.computed) {
     path.skip();
     return;
@@ -21,23 +20,17 @@ export function skipAllButComputedKey(
   }
 }
 
+// Methods are handled by the Method visitor; arrows are not skipped because they inherit the context.
 const skipKey = process.env.BABEL_8_BREAKING
-  ? "StaticBlock|ClassPrivateProperty|TypeAnnotation"
-  : `${staticBlock ? "StaticBlock|" : ""}ClassPrivateProperty|TypeAnnotation`;
+  ? "StaticBlock|ClassPrivateProperty|TypeAnnotation|FunctionDeclaration|FunctionExpression"
+  : (staticBlock ? "StaticBlock|" : "") +
+    "ClassPrivateProperty|TypeAnnotation|FunctionDeclaration|FunctionExpression";
 
 // environmentVisitor should be used when traversing the whole class and not for specific class elements/methods.
 // For perf reasons, the environmentVisitor might be traversed with `{ noScope: true }`, which means `path.scope` is undefined.
 // Avoid using `path.scope` here
 export default {
   [skipKey]: path => path.skip(),
-
-  Function(path: NodePath<t.Function>) {
-    // Methods will be handled by the Method visit
-    if (path.isMethod()) return;
-    // Arrow functions inherit their parent's environment
-    if (path.isArrowFunctionExpression()) return;
-    path.skip();
-  },
 
   "Method|ClassProperty"(path: NodePath<t.Method | t.ClassProperty>) {
     skipAllButComputedKey(path);
