a0ba89150d724371fe03d0cd18f7ebf4c4c4108e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-12-05 16:18:30 +0100

    [babel 8] Drawback: plugins must be stored in separate files

--- packages/babel-register/test/fixtures/internal-modules/index.js
+++ packages/babel-register/test/fixtures/internal-modules/index.js
@@ -1,23 +1,10 @@
 const register = require('../../..');
 
-// Plugin to add '/* transformed */' comment to start of function bodies
-const plugin = () => ( {
-  visitor: {
-    Function(path) {
-      const bodyNode = path.node.body;
-      (bodyNode.leadingComments || (bodyNode.leadingComments = [])).push( {
-        type: 'CommentBlock',
-        value: ' transformed '
-      } );
-    },
-  },
-} );
-
 register( {
   ignore: [],
   babelrc: false,
   configFile: false,
-  plugins: [plugin]
+  plugins: [require.resolve("./plugin")]
 } );
 
 console.log(
