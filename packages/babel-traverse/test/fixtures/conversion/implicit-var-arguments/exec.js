1e2f19b6f523e5a345c2994616acd09f677fdf6c
Sneh Khatri <khatris2014@gmail.com>
2021-12-11 16:04:22 +0530

    explicit expect added for test implicit var arguments

--- packages/babel-traverse/test/fixtures/conversion/implicit-var-arguments/exec.js
+++ packages/babel-traverse/test/fixtures/conversion/implicit-var-arguments/exec.js
@@ -1,7 +1,7 @@
 var arguments = [1, 2, 3];
 var arr = () => arguments[0];
 
-arr(); // 1
+expect(arr()).toStrictEqual(1)
 
 function foo(n) {
 var f = () => arguments[0] + n; // foo's implicit arguments binding. arguments[0] is n
