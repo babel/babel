7d243d618758a9fe0d6db5679fae874493e38594
Huáng Jùnliàng <jlhwung@gmail.com>
2021-12-15 17:30:09 -0500

    test: add prettier e2e test

---
+++ scripts/integration-tests/e2e-prettier.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#==============================================================================#
+#                                  SETUP                                       #
+#==============================================================================#
+
+# Start in scripts/integration-tests/ even if run from root directory
+cd "$(dirname "$0")" || exit
+root="$PWD"
+
+source utils/local-registry.sh
+source utils/cleanup.sh
+
+# Echo every command being executed
+set -x
+
+# Clone prettier
+git clone --depth=1 https://github.com/prettier/prettier /tmp/prettier
+cd /tmp/prettier || exit
+
+# Update @babel/* dependencies
+bump_deps="$root/utils/bump-babel-dependencies.js"
+node "$bump_deps"
+for d in ./packages/*/
+do
+  (cd "$d"; node "$bump_deps")
+done
+
+#==============================================================================#
+#                                 ENVIRONMENT                                  #
+#==============================================================================#
+node -v
+yarn --version
+
+#==============================================================================#
+#                                   TEST                                       #
+#==============================================================================#
+
+startLocalRegistry "$root"/verdaccio-config.yml
+yarn install
+
+if [ "$BABEL_8_BREAKING" = true ] ; then
+  # This option is removed in Babel 8
+  sed -i 's/allowDeclareFields: true,\?/\/* allowDeclareFields: true *\//g' babel.config.js
+fi
+
+# Only run js and jsx format tests
+yarn test "tests/format/jsx?"
+
+cleanup
