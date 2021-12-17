bd0f2e1ae1ab0274c677af34c3c48d468839e537
Huáng Jùnliàng <jlhwung@gmail.com>
2021-12-17 15:01:06 -0500

    remove invalid scripts

--- scripts/integration-tests/e2e-prettier.sh
+++ scripts/integration-tests/e2e-prettier.sh
@@ -15,16 +15,12 @@ source utils/cleanup.sh
 set -x
 
 # Clone prettier
-git clone --depth=1 https://github.com/prettier/prettier /tmp/prettier
-cd /tmp/prettier || exit
+git clone --depth=1 https://github.com/prettier/prettier tmp/prettier
+cd tmp/prettier || exit
 
 # Update @babel/* dependencies
 bump_deps="$root/utils/bump-babel-dependencies.js"
 node "$bump_deps"
-for d in ./packages/*/
-do
-  (cd "$d"; node "$bump_deps")
-done
 
 #==============================================================================#
 #                                 ENVIRONMENT                                  #
@@ -38,11 +34,6 @@ yarn --version
 
 startLocalRegistry "$root"/verdaccio-config.yml
 yarn install
-
-if [ "$BABEL_8_BREAKING" = true ] ; then
-  # This option is removed in Babel 8
-  sed -i 's/allowDeclareFields: true,\?/\/* allowDeclareFields: true *\//g' babel.config.js
-fi
 
 # Only run js,jsx,misc format tests
 yarn test "tests/format/(jsx?|misc)" --update-snapshot
