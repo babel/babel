83c7fa0130dceaf376b2fe8e46faaf16b25539ac
magic-akari <akari.ccino@gmail.com>
2022-01-01 09:17:43 +0800

    Update enum tests

--- packages/babel-plugin-transform-typescript/test/fixtures/enum/mix-references/input.ts
+++ packages/babel-plugin-transform-typescript/test/fixtures/enum/mix-references/input.ts
@@ -1,6 +1,12 @@
 var x = 10;
+
+enum Bar {
+    d = 1,
+}
+
 enum Foo {
     a = 10,
     b = a,
     c = b + x,
+    d = Bar.d,
 }
--- packages/babel-plugin-transform-typescript/test/fixtures/enum/mix-references/output.js
+++ packages/babel-plugin-transform-typescript/test/fixtures/enum/mix-references/output.js
@@ -1,8 +1,15 @@
 var x = 10;
+var Bar;
+
+(function (Bar) {
+  Bar[Bar["d"] = 1] = "d";
+})(Bar || (Bar = {}));
+
 var Foo;
 
 (function (Foo) {
   Foo[Foo["a"] = 10] = "a";
   Foo[Foo["b"] = 10] = "b";
   Foo[Foo["c"] = Foo.b + x] = "c";
+  Foo[Foo["d"] = Bar.d] = "d";
 })(Foo || (Foo = {}));
