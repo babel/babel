c494c6118b522bc961b8bd0201f175309e5966e4
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-01-06 16:54:34 +0100

    Update `core-js` 3

--- packages/babel-preset-env/test/fixtures/corejs3-babel-7/entry-stable-chrome-71/output.mjs
+++ packages/babel-preset-env/test/fixtures/corejs3-babel-7/entry-stable-chrome-71/output.mjs
@@ -4,5 +4,6 @@ import "core-js/modules/es.array.unscopables.flat.js";
 import "core-js/modules/es.array.unscopables.flat-map.js";
 import "core-js/modules/es.math.hypot.js";
 import "core-js/modules/es.object.from-entries.js";
+import "core-js/modules/es.typed-array.set.js";
 import "core-js/modules/es.typed-array.sort.js";
 import "core-js/modules/web.immediate.js";
