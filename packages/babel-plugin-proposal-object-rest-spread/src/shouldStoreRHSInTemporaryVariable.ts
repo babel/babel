0befc4da843caaa33fcd339c547800a2f76dd340
zhangenming <282126346@qq.com>
2022-01-21 13:04:56 +0800

    Update shouldStoreRHSInTemporaryVariable.ts
    use more deconstruction.

--- packages/babel-plugin-proposal-object-rest-spread/src/shouldStoreRHSInTemporaryVariable.ts
+++ packages/babel-plugin-proposal-object-rest-spread/src/shouldStoreRHSInTemporaryVariable.ts
@@ -1,6 +1,6 @@
 import { types as t } from "@babel/core";
 
-const { isObjectProperty } = t;
+const { isObjectProperty, isArrayPattern, isObjectPattern, isAssignmentPattern, isRestElement, isIdentifier, LVal } = t;
 /**
  * This is a helper function to determine if we should create an intermediate variable
  * such that the RHS of an assignment is not duplicated.
@@ -8,12 +8,12 @@ const { isObjectProperty } = t;
  * See https://github.com/babel/babel/pull/13711#issuecomment-914388382 for discussion
  * on further optimizations.
  */
-export default function shouldStoreRHSInTemporaryVariable(node: t.LVal) {
-  if (t.isArrayPattern(node)) {
+export default function shouldStoreRHSInTemporaryVariable(node: LVal) {
+  if (isArrayPattern(node)) {
     const nonNullElements = node.elements.filter(element => element !== null);
     if (nonNullElements.length > 1) return true;
     else return shouldStoreRHSInTemporaryVariable(nonNullElements[0]);
-  } else if (t.isObjectPattern(node)) {
+  } else if (isObjectPattern(node)) {
     const { properties } = node;
     if (properties.length > 1) return true;
     else if (properties.length === 0) return false;
@@ -21,15 +21,15 @@ export default function shouldStoreRHSInTemporaryVariable(node: t.LVal) {
       const firstProperty = properties[0];
       if (isObjectProperty(firstProperty)) {
         // the value of the property must be an LVal
-        return shouldStoreRHSInTemporaryVariable(firstProperty.value as t.LVal);
+        return shouldStoreRHSInTemporaryVariable(firstProperty.value as LVal);
       } else {
         return shouldStoreRHSInTemporaryVariable(firstProperty);
       }
     }
-  } else if (t.isAssignmentPattern(node)) {
+  } else if (isAssignmentPattern(node)) {
     return shouldStoreRHSInTemporaryVariable(node.left);
-  } else if (t.isRestElement(node)) {
-    if (t.isIdentifier(node.argument)) return true;
+  } else if (isRestElement(node)) {
+    if (isIdentifier(node.argument)) return true;
     return shouldStoreRHSInTemporaryVariable(node.argument);
   } else {
     // node is Identifier or MemberExpression
