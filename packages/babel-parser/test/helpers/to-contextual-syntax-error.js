c140ba83e8e87c9cf0a84d500e5e673fb14a25d8
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2022-01-23 07:44:47 -0800

    Fix stack traces and only generate the context error when we need it.
    Reviewed by @tolmasky.

--- packages/babel-parser/test/helpers/to-contextual-syntax-error.js
+++ packages/babel-parser/test/helpers/to-contextual-syntax-error.js
@@ -9,7 +9,7 @@ export default function toContextualSyntaxError(
 ) {
   if (!(error instanceof SyntaxError)) return error;
 
-  const { startLine = 1, startColumn = 0, sourceFilename } = options || {};
+  const { startLine = 1, startColumn = 0 } = options || {};
   const line = error.loc.line - startLine + 1;
   const column =
     1 + (line === 1 ? error.loc.column - startColumn : error.loc.column);
@@ -21,19 +21,20 @@ export default function toContextualSyntaxError(
 
   // Limit this stack trace to 1. Everything after that is just stuff from the
   // test.
-  const originalStackTraceLimit = Error.stackTraceLimit;
-  Error.stackTraceLimit = 1;
-
   Object.defineProperty(error, "context", {
-    value: (error.context = runInThisContext("(f => f())", {
-      filename: sourceFilename,
-    })(() => SyntaxError(`${error.message}\n${frame}`))),
-    enumerable: false,
+    get() {
+      const message = JSON.stringify(`${error.message}\n${frame}`);
+      const originalStackTraceLimit = Error.stackTraceLimit;
+
+      Error.stackTraceLimit = 1;
+
+      const context = runInThisContext(`SyntaxError(${message})`, { filename });
+
+      Error.stackTraceLimit = originalStackTraceLimit;
+
+      return Object.assign(context, { cause: this });
+    },
   });
-
-  Error.stackTraceLimit = originalStackTraceLimit;
-
-  error.context.cause = error;
 
   return error;
 }
