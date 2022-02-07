80ef63b801c94da638a682d6cf9590bbd4330ac6
Justin Ridgewell <justin@ridgewell.name>
2022-02-07 03:01:21 -0500

    Update to remapping@2.1.0 to support source location override

--- packages/babel-core/src/transformation/file/merge-map.ts
+++ packages/babel-core/src/transformation/file/merge-map.ts
@@ -6,27 +6,18 @@ export default function mergeSourceMap(
   map: SourceMap,
   source: string,
 ): SourceMap {
-  const outputSources = map.sources;
+  const result = remapping(rootless(map), (s, ctx) => {
+    if (s === source) {
+      // We empty the source location, which will prevent the sourcemap from
+      // becoming relative to the input's location. Eg, if we're transforming a
+      // file 'foo/bar.js', and it is a transformation of a `baz.js` file in the
+      // same directory, the expected output is just `baz.js`. Without this step,
+      // it would become `foo/baz.js`.
+      ctx.source = "";
 
-  const index = outputSources.indexOf(source);
+      return rootless(inputMap);
+    }
 
-  if (index > -1) {
-    // We empty the source index, which will prevent the sourcemap from
-    // becoming relative to the input's location. Eg, if we're transforming a
-    // file 'foo/bar.js', and it is a transformation of a `baz.js` file in the
-    // same directory, the expected output is just `baz.js`. Without this step,
-    // it would become `foo/baz.js`.
-    map.sources[index] = "";
-  }
-
-  let count = 0;
-  const result = remapping(rootless(map), () => {
-    // Return the inputMap only when we hit the correct source index, so that
-    // remapping will trace through it into the original locations. Remapping
-    // calls the loader cb for each source in depth-first order, and since
-    // we're not returning any other sourcemaps, the source is guaranteed to be
-    // the index'th call.
-    if (count++ === index) return rootless(inputMap);
     return null;
   });
 
