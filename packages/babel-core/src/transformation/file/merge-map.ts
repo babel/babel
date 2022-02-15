42c765b999717b75913ce03b418418b38a93dbbb
Justin Ridgewell <jridgewell@google.com>
2022-02-15 17:26:59 -0500

    Fix infinite recursion when merging sourcemaps (#14274)
    * Fix infinite recursion when merge sourcemaps
    
    * Add test case
    
    * Externalize the sourcemap

--- packages/babel-core/src/transformation/file/merge-map.ts
+++ packages/babel-core/src/transformation/file/merge-map.ts
@@ -6,8 +6,14 @@ export default function mergeSourceMap(
   map: SourceMap,
   source: string,
 ): SourceMap {
+  // Prevent an infinite recursion if one of the input map's sources has the
+  // same resolved path as the input map. In the case, it would keep find the
+  // input map, then get it's sources which will include a path like the input
+  // map, on and on.
+  let found = false;
   const result = remapping(rootless(map), (s, ctx) => {
-    if (s === source) {
+    if (s === source && !found) {
+      found = true;
       // We empty the source location, which will prevent the sourcemap from
       // becoming relative to the input's location. Eg, if we're transforming a
       // file 'foo/bar.js', and it is a transformation of a `baz.js` file in the
---
+++ packages/babel-core/test/fixtures/transformation/source-maps/input-source-map-same-location/input.js
@@ -0,0 +1,6 @@
+var foo = function () {
+  return 4;
+};
+
+//# sourceMappingURL=input.js.map
+
