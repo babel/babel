cd37858854e01a960ccfc80eb5836c198feeb1ff
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-02-17 16:48:43 +0100

    Simplify test

--- packages/babel-core/test/merge-map.js
+++ packages/babel-core/test/merge-map.js
@@ -1,28 +1,27 @@
 import _mergeSourceMap from "../lib/transformation/file/merge-map.js";
 const mergeSourceMap = _mergeSourceMap.default;
 
-const inputMap = {
-  file: "file.js",
-  mappings: [],
-  names: [],
-  sources: ["file.ts"],
-  version: 3,
-};
-
-const outputMap = {
-  file: "file.transpiled.js",
-  mappings: [],
-  names: [],
-  sources: ["file.js"],
-  version: 3,
-};
-
 describe("merge-map", () => {
   it("returns a plain js object", () => {
+    const inputMap = {
+      file: "file.js",
+      mappings: [],
+      names: [],
+      sources: ["file.ts"],
+      version: 3,
+    };
+
+    const outputMap = {
+      file: "file.transpiled.js",
+      mappings: [],
+      names: [],
+      sources: ["file.js"],
+      version: 3,
+    };
+
     const map = mergeSourceMap(inputMap, outputMap, "file.transpiled.js");
-    const proto = Object.getPrototypeOf(map) || Object.getPrototypeOf({});
     expect(typeof map).toBe("object");
     expect(Object.prototype.toString.call(map)).toBe("[object Object]");
-    expect(Object.getPrototypeOf(proto)).toBeNull();
+    expect(Object.getPrototypeOf(map)).toBe(Object.prototype);
   });
 });
