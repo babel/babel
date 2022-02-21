2de3f1c6146e94f2db9ce59e9d14a790c61b3daf
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-02-21 23:16:49 +0100

    Remove Jest workaround for Babel 8 e2e test (#14224)

--- scripts/integration-tests/e2e-babel.sh
+++ scripts/integration-tests/e2e-babel.sh
@@ -32,21 +32,7 @@ startLocalRegistry "$PWD"/scripts/integration-tests/verdaccio-config.yml
 # and not from npm.
 node "$PWD"/scripts/integration-tests/utils/bump-babel-dependencies.js
 
-# Update deps, build and test
-if [ "$BABEL_8_BREAKING" = true ] ; then
-  YARN_ENABLE_IMMUTABLE_INSTALLS=false make -j build-standalone-ci
-
-  # Jest hangs forever in the Babel 8 e2e test when using multiple workers,
-  # but we don't know yet why. Until we figure it out (see
-  # https://github.com/babel/babel/pull/13618) we can use --runInBand.
-  # Additinally, in Babel 8 tests Jest needs to run with ESM support because
-  # @babel/eslint-parser/lib/worker/babel-core.cjs uses dynamic import() to
-  # load @babel/core.
-  NODE_OPTIONS="--experimental-vm-modules" BABEL_ENV=test yarn jest --ci --runInBand
-
-  make -j test-clean
-else
-  YARN_ENABLE_IMMUTABLE_INSTALLS=false make -j test-ci
-fi
+# Build and test
+YARN_ENABLE_IMMUTABLE_INSTALLS=false make -j test-ci
 
 cleanup
