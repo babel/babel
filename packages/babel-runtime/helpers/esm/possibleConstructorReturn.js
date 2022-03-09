65e0a447eb844a99affb16105c9dc4828500b60d
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-03-09 23:42:01 +0100

    Undo unrelated changes

--- packages/babel-runtime/helpers/esm/possibleConstructorReturn.js
+++ packages/babel-runtime/helpers/esm/possibleConstructorReturn.js
@@ -1,4 +1,4 @@
-import _typeof from "@babel/runtime/helpers/typeof";
+import _typeof from "./typeof.js";
 import assertThisInitialized from "./assertThisInitialized.js";
 export default function _possibleConstructorReturn(self, call) {
   if (call && (_typeof(call) === "object" || typeof call === "function")) {
