a7b1181fef65b69a7ae6cd5e59045d311470e70a
magic-akari <akari.ccino@gmail.com>
2022-03-11 03:13:55 +0800

    Fix update expression for exported bigints (#14341)

--- packages/babel-plugin-transform-modules-amd/test/fixtures/amd/remap/output.js
+++ packages/babel-plugin-transform-modules-amd/test/fixtures/amd/remap/output.js
@@ -5,10 +5,13 @@ define(["exports"], function (_exports) {
     value: true
   });
   _exports.test = _exports.f = _exports.e = _exports.c = _exports.a = void 0;
+
+  var _test;
+
   var test = 2;
   _exports.test = test;
   _exports.test = test = 5;
-  _exports.test = test = test + 1;
+  _test = test++, _exports.test = test, _test;
 
   (function () {
     var test = 2;
