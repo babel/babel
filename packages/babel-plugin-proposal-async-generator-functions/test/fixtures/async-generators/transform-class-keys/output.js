a946c5e62fbbaba95c6f3191a11a9c288e404320
Yokubjon-J <ezenxaanmeiji@gmail.com>
2022-03-25 02:25:53 +0500

    test added

---
+++ packages/babel-plugin-proposal-async-generator-functions/test/fixtures/async-generators/transform-class-keys/output.js
@@ -0,0 +1,18 @@
+function fn() {
+  return _fn.apply(this, arguments);
+}
+
+function _fn() {
+  _fn = babelHelpers.wrapAsyncGenerator(function* () {
+    class A {
+      [yield 1]() {}
+
+    }
+
+    class B extends A {
+      [await 1]() {}
+
+    }
+  });
+  return _fn.apply(this, arguments);
+}
