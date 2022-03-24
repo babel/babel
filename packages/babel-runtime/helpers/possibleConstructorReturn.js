645e36c040017517c3944807f3d1066e89cc022d
Yokubjon-J <ezenxaanmeiji@gmail.com>
2022-03-25 02:39:15 +0500

    import correct tests

--- packages/babel-runtime/helpers/possibleConstructorReturn.js
+++ packages/babel-runtime/helpers/possibleConstructorReturn.js
@@ -1,4 +1,4 @@
-var _typeof = require("@babel/runtime/helpers/typeof")["default"];
+var _typeof = require("./typeof.js")["default"];
 
 var assertThisInitialized = require("./assertThisInitialized.js");
 
