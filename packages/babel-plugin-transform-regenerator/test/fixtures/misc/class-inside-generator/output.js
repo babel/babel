085c68e80c9989819e6498adc59c1f3e1339599f
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-04-01 15:13:59 +0200

    Update `regenerator-transform`

---
+++ packages/babel-plugin-transform-regenerator/test/fixtures/misc/class-inside-generator/output.js
@@ -0,0 +1,21 @@
+var _marked = /*#__PURE__*/regeneratorRuntime.mark(f);
+
+function f() {
+  return regeneratorRuntime.wrap(function f$(_context) {
+    while (1) switch (_context.prev = _context.next) {
+      case 0:
+        class A {
+          foo() {
+            return 1;
+          }
+
+        }
+
+        ;
+
+      case 2:
+      case "end":
+        return _context.stop();
+    }
+  }, _marked);
+}
