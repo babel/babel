da8bae8cc519585773d85415b34e27cbfb8ca7b7
magic-akari <akari.ccino@gmail.com>
2022-04-11 23:02:31 +0800

    chore: update test cases

--- packages/babel-generator/test/fixtures/typescript/types-typeof-private/output.js
+++ packages/babel-generator/test/fixtures/typescript/types-typeof-private/output.js
@@ -1,14 +1,3 @@
-class C {
-  #a = 'a';
-
-  constructor() {
-    const a: typeof this.#a = ''; // Ok
-
-    const b: typeof this.#a = 1; // Error
-  }
-
-}
-
 class Container {
   #data = "hello!";
 
--- packages/babel-parser/test/fixtures/typescript/types/typeof-private/input.ts
+++ packages/babel-parser/test/fixtures/typescript/types/typeof-private/input.ts
@@ -1,15 +1,3 @@
-class C {
-    #a = 'a';
-
-    constructor() {
-        const a: typeof this.#a = ''; // Ok
-        const b: typeof this.#a = 1;  // Error
-    }
-}
-
-const c = new C();
-const a: typeof c.#a = '';
-
 class Container {
     #data = "hello!";
 
