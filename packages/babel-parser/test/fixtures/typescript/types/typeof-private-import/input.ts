f1840072f847aaf2b1982bbbb48964768c1e61d8
magic-akari <akari.ccino@gmail.com>
2022-04-14 14:23:55 +0800

    chore: update test case

--- packages/babel-parser/test/fixtures/typescript/types/typeof-private-import/input.ts
+++ packages/babel-parser/test/fixtures/typescript/types/typeof-private-import/input.ts
@@ -1,4 +1,13 @@
+// @filename: main.ts
 export class Foo {
-    static #p = 1;
-    m(param: typeof import("./lib").Foo.#p) {}
+    static #a = 1;
+    static #b(_: typeof Foo.#a) {}
+    static #c(_: typeof this.#a) {}
+    static #d(_: typeof import("./lib").Bar.#a) {}
+
+    // TODO: handle this
+    // static #e(_: typeof import("./lib").#a) {}
 }
+
+// @filename: lib.ts
+export { Foo as Bar } from "./main"
