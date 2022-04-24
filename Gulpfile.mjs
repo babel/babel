86830e8b40e273a5e0740005119e863d92d9e073
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-04-24 15:24:02 +0200

    Revert #14462 (Rolluo always normalizes paths now)

--- Gulpfile.mjs
+++ Gulpfile.mjs
@@ -297,8 +297,6 @@ function buildRollup(packages, targetBrowsers) {
           input,
           external,
           onwarn(warning, warn) {
-            const osifyPath = str => str.split("/").join(path.sep);
-
             if (warning.code === "CIRCULAR_DEPENDENCY") return;
             if (warning.code === "UNUSED_EXTERNAL_IMPORT") {
               warn(warning);
@@ -310,15 +308,14 @@ function buildRollup(packages, targetBrowsers) {
             // We can safely ignore this warning, and let Rollup replace it with undefined.
             if (
               warning.code === "MISSING_EXPORT" &&
-              warning.exporter ===
-                osifyPath("packages/babel-core/src/index.ts") &&
+              warning.exporter === "packages/babel-core/src/index.ts" &&
               warning.missing === "default" &&
               [
                 "@babel/helper-define-polyfill-provider",
                 "babel-plugin-polyfill-corejs2",
                 "babel-plugin-polyfill-corejs3",
                 "babel-plugin-polyfill-regenerator",
-              ].some(pkg => warning.importer.includes(osifyPath(pkg)))
+              ].some(pkg => warning.importer.includes(pkg))
             ) {
               return;
             }
