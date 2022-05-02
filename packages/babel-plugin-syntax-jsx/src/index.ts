7129096c1d0bf44caede5de8ba59ffc113009da8
Conrad Buck <conartist6@gmail.com>
2022-05-02 12:04:43 -0700

    Minor legibility cleanup (#14448)

--- packages/babel-plugin-syntax-jsx/src/index.ts
+++ packages/babel-plugin-syntax-jsx/src/index.ts
@@ -7,17 +7,14 @@ export default declare(api => {
     name: "syntax-jsx",
 
     manipulateOptions(opts, parserOpts) {
+      const { plugins } = parserOpts;
       // If the Typescript plugin already ran, it will have decided whether
       // or not this is a TSX file.
-      if (
-        parserOpts.plugins.some(
-          p => (Array.isArray(p) ? p[0] : p) === "typescript",
-        )
-      ) {
+      if (plugins.some(p => (Array.isArray(p) ? p[0] : p) === "typescript")) {
         return;
       }
 
-      parserOpts.plugins.push("jsx");
+      plugins.push("jsx");
     },
   };
 });
