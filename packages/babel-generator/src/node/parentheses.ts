9c1774a63f080fbf9f0a60d6674e8f220d2d7d70
Huáng Jùnliàng <jlhwung@gmail.com>
2022-05-06 10:18:45 -0400

    fix: perserve parentheses of lhs id with rhs unamed fn (#14524)
    * fix: perserve () of lhs id with rhs unamed fn
    
    * review comments

--- packages/babel-generator/src/node/parentheses.ts
+++ packages/babel-generator/src/node/parentheses.ts
@@ -18,6 +18,7 @@ import {
   isForInStatement,
   isForOfStatement,
   isForStatement,
+  isFunctionExpression,
   isIfStatement,
   isIndexedAccessType,
   isIntersectionTypeAnnotation,
@@ -345,6 +346,16 @@ export function Identifier(
   parent: t.Node,
   printStack: Array<t.Node>,
 ): boolean {
+  // 13.15.2 AssignmentExpression RS: Evaluation
+  // (fn) = function () {};
+  if (
+    node.extra?.parenthesized &&
+    isAssignmentExpression(parent, { left: node }) &&
+    (isFunctionExpression(parent.right) || isClassExpression(parent.right)) &&
+    parent.right.id == null
+  ) {
+    return true;
+  }
   // Non-strict code allows the identifier `let`, but it cannot occur as-is in
   // certain contexts to avoid ambiguity with contextual keyword `let`.
   if (node.name === "let") {
