9610a03dc4ef12dc11ad2864739e6edd40e65bfd
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-05-11 00:36:41 +0200

    Use availableHelper, avoid IIFEs

--- packages/babel-plugin-transform-regenerator/src/index.ts
+++ packages/babel-plugin-transform-regenerator/src/index.ts
@@ -1,4 +1,5 @@
 import { declare } from "@babel/helper-plugin-utils";
+import type { types as t } from "@babel/core";
 import regeneratorTransform from "regenerator-transform";
 
 export default declare(({ types: t, assertVersion }) => {
@@ -13,16 +14,29 @@ export default declare(({ types: t, assertVersion }) => {
       // We visit MemberExpression so that we always transform
       // regeneratorRuntime before babel-plugin-polyfill-regenerator.
       MemberExpression(path) {
+        if (!this.availableHelper?.("regeneratorRuntime")) {
+          // When using an older @babel/helpers version, fallback
+          // to the old behavior.
+          // TODO: Remove this in Babel 8.
+          return;
+        }
+
         const obj = path.get("object");
         if (obj.isIdentifier({ name: "regeneratorRuntime" })) {
-          try {
-            obj.replaceWith(
-              t.callExpression(this.addHelper("regeneratorRuntime"), []),
-            );
-          } catch {
-            // When using an older @babel/helpers version, fallback
-            // to the old behavior
+          const helper = this.addHelper("regeneratorRuntime") as
+            | t.Identifier
+            | t.ArrowFunctionExpression;
+
+          if (
+            // TODO: Remove this in Babel 8, it's necessary to
+            // avoid the IIFE when using older Babel versions.
+            t.isArrowFunctionExpression(helper)
+          ) {
+            obj.replaceWith(helper.body);
+            return;
           }
+
+          obj.replaceWith(t.callExpression(helper, []));
         }
       },
     },
--- packages/babel-plugin-transform-runtime/test/fixtures/runtime/full/output.mjs
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime/full/output.mjs
@@ -1,11 +1,11 @@
 import _regeneratorRuntime from "@babel/runtime/regenerator";
 
-var _marked = /*#__PURE__*/(() => _regeneratorRuntime)().mark(giveWord);
+var _marked = /*#__PURE__*/_regeneratorRuntime.mark(giveWord);
 
 import foo, * as bar from "someModule";
 export const myWord = Symbol("abc");
 export function giveWord() {
-  return (() => _regeneratorRuntime)().wrap(function giveWord$(_context) {
+  return _regeneratorRuntime.wrap(function giveWord$(_context) {
     while (1) switch (_context.prev = _context.next) {
       case 0:
         _context.next = 2;
