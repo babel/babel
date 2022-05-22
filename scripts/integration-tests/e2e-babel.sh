9bfc19e74580ae116ff46c06eb027ed00c7fad8b
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-05-23 07:21:55 +0800

    test

--- scripts/integration-tests/e2e-babel.sh
+++ scripts/integration-tests/e2e-babel.sh
@@ -33,6 +33,10 @@ startLocalRegistry "$PWD"/scripts/integration-tests/verdaccio-config.yml
 node "$PWD"/scripts/integration-tests/utils/bump-babel-dependencies.js
 
 # Build and test
-YARN_ENABLE_IMMUTABLE_INSTALLS=false make -j test-ci
+YARN_ENABLE_IMMUTABLE_INSTALLS=false make -j build-standalone-ci
+for i in {1..50}; do
+  echo "RUN NUMBER $i"
+  BABEL_ENV=test yarn jest --maxWorkers=4 --ci
+done
 
 cleanup
