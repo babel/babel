64961e057b9af1f8c038ffc8b1c36914884c4c82
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-05-23 08:59:52 +0800

    fix

--- packages/babel-core/src/transformation/util/clone-deep-browser.ts
+++ packages/babel-core/src/transformation/util/clone-deep-browser.ts
@@ -1,19 +1,5 @@
-const serialized = "$$ babel internal serialized type" + Math.random();
-
-function serialize(key, value) {
-  if (typeof value !== "bigint") return value;
-  return {
-    [serialized]: "BigInt",
-    value: value.toString(),
-  };
-}
-
-function revive(key, value) {
-  if (!value || typeof value !== "object") return value;
-  if (value[serialized] !== "BigInt") return value;
-  return BigInt(value.value);
-}
+import cloneDeep from "lodash.clonedeep";
 
 export default function (value) {
-  return JSON.parse(JSON.stringify(value, serialize), revive);
+  return cloneDeep(value);
 }
--- packages/babel-core/src/transformation/util/clone-deep.ts
+++ packages/babel-core/src/transformation/util/clone-deep.ts
@@ -1,8 +1,13 @@
 import v8 from "v8";
-import cloneDeep from "./clone-deep-browser";
+import cloneDeep from "lodash.clonedeep";
+import semver from "semver";
 
 export default function (value) {
-  if (v8.deserialize && v8.serialize) {
+  if (
+    v8.deserialize &&
+    v8.serialize &&
+    semver.satisfies(process.versions.node, ">=18.2.0")
+  ) {
     return v8.deserialize(v8.serialize(value));
   }
   return cloneDeep(value);
