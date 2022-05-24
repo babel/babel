bee8e4aa6a598fb0a7bb5b28030e4745867666a3
Huáng Jùnliàng <jlhwung@gmail.com>
2022-05-24 15:13:49 -0400

    fix typing errors

--- packages/babel-preset-env/src/normalize-options.ts
+++ packages/babel-preset-env/src/normalize-options.ts
@@ -116,7 +116,6 @@ export const checkDuplicateIncludeExcludes = (
 const normalizeTargets = (targets): Options["targets"] => {
   // TODO: Allow to use only query or strings as a targets from next breaking change.
   if (typeof targets === "string" || Array.isArray(targets)) {
-    // @ts-expect-error
     return { browsers: targets };
   }
   return { ...targets };
