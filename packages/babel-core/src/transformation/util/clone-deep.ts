558270c55f3057d713bc82a5e3d392ba4dd37694
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-05-25 20:07:14 +0800

    review

--- packages/babel-core/src/transformation/util/clone-deep.ts
+++ packages/babel-core/src/transformation/util/clone-deep.ts
@@ -3,6 +3,8 @@ import cloneDeep from "lodash.clonedeep";
 import semver from "semver";
 
 export default function (value) {
+  // There was a memory leak prior to node 18.2.0
+  // https://github.com/nodejs/node/issues/40828
   if (
     v8.deserialize &&
     v8.serialize &&
