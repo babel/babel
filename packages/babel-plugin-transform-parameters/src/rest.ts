3557925b0fe8d22a7e03755290e5b7d495a912f1
Huáng Jùnliàng <jlhwung@gmail.com>
2022-05-30 13:23:34 -0400

    transform-parameters

--- packages/babel-plugin-transform-parameters/src/rest.ts
+++ packages/babel-plugin-transform-parameters/src/rest.ts
@@ -1,13 +1,47 @@
 import { template, types as t } from "@babel/core";
 import type { NodePath, Visitor } from "@babel/traverse";
 
+const buildRest = template.statement(`
+  for (var LEN = ARGUMENTS.length,
+           ARRAY = new Array(ARRAY_LEN),
+           KEY = START;
+       KEY < LEN;
+       KEY++) {
+    ARRAY[ARRAY_KEY] = ARGUMENTS[KEY];
+  }
+`);
+
+const restIndex = template.expression(`
+  (INDEX < OFFSET || ARGUMENTS.length <= INDEX) ? undefined : ARGUMENTS[INDEX]
+`);
+
+const restIndexImpure = template.expression(`
+  REF = INDEX, (REF < OFFSET || ARGUMENTS.length <= REF) ? undefined : ARGUMENTS[REF]
+`);
+
+const restLength = template.expression(`
+  ARGUMENTS.length <= OFFSET ? 0 : ARGUMENTS.length - OFFSET
+`);
+
+function referencesRest(
+  path: NodePath<t.Identifier | t.JSXIdentifier>,
+  state: State,
+) {
+  if (path.node.name === state.name) {
+    // Check rest parameter is not shadowed by a binding in another scope.
+    return path.scope.bindingIdentifierEquals(state.name, state.outerBinding);
+  }
+
+  return false;
+}
+
 type Candidate = {
-  cause: "indexGetter" | "lengthGetter" | "argSpread";
+  cause: "argSpread" | "indexGetter" | "lengthGetter";
   path: NodePath<t.Identifier | t.JSXIdentifier>;
 };
 
 type State = {
-  references: [];
+  references: NodePath<t.Identifier | t.JSXIdentifier>[];
   offset: number;
 
   argumentsNode: t.Identifier;
@@ -32,42 +66,8 @@ type State = {
   nested function.
   */
   deopted: boolean;
-  noOptimise: boolean;
+  noOptimise?: boolean;
 };
-
-const buildRest = template(`
-  for (var LEN = ARGUMENTS.length,
-           ARRAY = new Array(ARRAY_LEN),
-           KEY = START;
-       KEY < LEN;
-       KEY++) {
-    ARRAY[ARRAY_KEY] = ARGUMENTS[KEY];
-  }
-`);
-
-const restIndex = template(`
-  (INDEX < OFFSET || ARGUMENTS.length <= INDEX) ? undefined : ARGUMENTS[INDEX]
-`);
-
-const restIndexImpure = template(`
-  REF = INDEX, (REF < OFFSET || ARGUMENTS.length <= REF) ? undefined : ARGUMENTS[REF]
-`);
-
-const restLength = template(`
-  ARGUMENTS.length <= OFFSET ? 0 : ARGUMENTS.length - OFFSET
-`);
-
-function referencesRest(
-  path: NodePath<t.Identifier | t.JSXIdentifier>,
-  state: State,
-) {
-  if (path.node.name === state.name) {
-    // Check rest parameter is not shadowed by a binding in another scope.
-    return path.scope.bindingIdentifierEquals(state.name, state.outerBinding);
-  }
-
-  return false;
-}
 
 const memberExpressionOptimisationVisitor: Visitor<State> = {
   Scope(path, state) {
@@ -157,7 +157,10 @@ const memberExpressionOptimisationVisitor: Visitor<State> = {
               state.candidates.push({ cause: "indexGetter", path });
               return;
             }
-          } else if (parentPath.node.property.name === "length") {
+          } else if (
+            // @ts-expect-error .length must not be a private name
+            parentPath.node.property.name === "length"
+          ) {
             // args.length
             state.candidates.push({ cause: "lengthGetter", path });
             return;
@@ -194,7 +197,7 @@ const memberExpressionOptimisationVisitor: Visitor<State> = {
   },
 };
 
-function getParamsCount(node) {
+function getParamsCount(node: t.Function) {
   let count = node.params.length;
   // skip the first parameter if it is a TypeScript 'this parameter'
   if (count > 0 && t.isIdentifier(node.params[0], { name: "this" })) {
@@ -203,33 +206,38 @@ function getParamsCount(node) {
   return count;
 }
 
-function hasRest(node) {
+function hasRest(node: t.Function) {
   const length = node.params.length;
   return length > 0 && t.isRestElement(node.params[length - 1]);
 }
 
-function optimiseIndexGetter(path, argsId, offset) {
+function optimiseIndexGetter(
+  path: NodePath<t.Identifier | t.JSXIdentifier>,
+  argsId: t.Identifier,
+  offset: number,
+) {
   const offsetLiteral = t.numericLiteral(offset);
   let index;
+  const parent = path.parent as t.MemberExpression;
 
-  if (t.isNumericLiteral(path.parent.property)) {
-    index = t.numericLiteral(path.parent.property.value + offset);
+  if (t.isNumericLiteral(parent.property)) {
+    index = t.numericLiteral(parent.property.value + offset);
   } else if (offset === 0) {
     // Avoid unnecessary '+ 0'
-    index = path.parent.property;
+    index = parent.property;
   } else {
     index = t.binaryExpression(
       "+",
-      path.parent.property,
+      parent.property,
       t.cloneNode(offsetLiteral),
     );
   }
 
-  const { scope } = path;
+  const { scope, parentPath } = path;
   if (!scope.isPure(index)) {
     const temp = scope.generateUidIdentifierBasedOnNode(index);
     scope.push({ id: temp, kind: "var" });
-    path.parentPath.replaceWith(
+    parentPath.replaceWith(
       restIndexImpure({
         ARGUMENTS: argsId,
         OFFSET: offsetLiteral,
@@ -238,7 +246,6 @@ function optimiseIndexGetter(path, argsId, offset) {
       }),
     );
   } else {
-    const parentPath = path.parentPath;
     parentPath.replaceWith(
       restIndex({
         ARGUMENTS: argsId,
@@ -246,22 +253,29 @@ function optimiseIndexGetter(path, argsId, offset) {
         INDEX: index,
       }),
     );
+    const replacedParentPath = parentPath as NodePath<t.ConditionalExpression>;
 
     // See if we can statically evaluate the first test (i.e. index < offset)
     // and optimize the AST accordingly.
-    const offsetTestPath = parentPath.get("test").get("left");
-    const valRes = offsetTestPath.evaluate();
+    const offsetTestPath = replacedParentPath.get(
+      "test",
+    ) as NodePath<t.BinaryExpression>;
+    const valRes = offsetTestPath.get("left").evaluate();
     if (valRes.confident) {
       if (valRes.value === true) {
-        parentPath.replaceWith(parentPath.scope.buildUndefinedNode());
+        replacedParentPath.replaceWith(scope.buildUndefinedNode());
       } else {
-        parentPath.get("test").replaceWith(parentPath.get("test").get("right"));
+        offsetTestPath.replaceWith(offsetTestPath.get("right"));
       }
     }
   }
 }
 
-function optimiseLengthGetter(path, argsId, offset) {
+function optimiseLengthGetter(
+  path: NodePath<t.Identifier | t.JSXIdentifier>,
+  argsId: t.Identifier,
+  offset: number,
+) {
   if (offset) {
     path.parentPath.replaceWith(
       restLength({
@@ -274,15 +288,13 @@ function optimiseLengthGetter(path, argsId, offset) {
   }
 }
 
-export default function convertFunctionRest(path) {
+export default function convertFunctionRest(path: NodePath<t.Function>) {
   const { node, scope } = path;
   if (!hasRest(node)) return false;
 
-  let rest = node.params.pop().argument;
-
-  if (rest.name === "arguments") scope.rename(rest.name);
-
-  const argsId = t.identifier("arguments");
+  let rest = (node.params.pop() as t.RestElement).argument as
+    | t.Pattern
+    | t.Identifier;
 
   if (t.isPattern(rest)) {
     const pattern = rest;
@@ -291,37 +303,23 @@ export default function convertFunctionRest(path) {
     const declar = t.variableDeclaration("let", [
       t.variableDeclarator(pattern, rest),
     ]);
-    node.body.body.unshift(declar);
+    path.ensureBlock();
+    (node.body as t.BlockStatement).body.unshift(declar);
+  } else if (rest.name === "arguments") {
+    scope.rename(rest.name);
   }
 
+  const argsId = t.identifier("arguments");
   const paramsCount = getParamsCount(node);
 
   // check and optimise for extremely common cases
-  const state = {
+  const state: State = {
     references: [],
     offset: paramsCount,
-
     argumentsNode: argsId,
     outerBinding: scope.getBindingIdentifier(rest.name),
-
-    // candidate member expressions we could optimise if there are no other references
     candidates: [],
-
-    // local rest binding name
     name: rest.name,
-
-    /*
-    It may be possible to optimize the output code in certain ways, such as
-    not generating code to initialize an array (perhaps substituting direct
-    references to arguments[i] or arguments.length for reads of the
-    corresponding rest parameter property) or positioning the initialization
-    code so that it may not have to execute depending on runtime conditions.
-
-    This property tracks eligibility for optimization. "deopted" means give up
-    and don't perform optimization. For example, when any of rest's elements /
-    properties is assigned to at the top level, or referenced at all in a
-    nested function.
-    */
     deopted: false,
   };
 
@@ -385,7 +383,7 @@ export default function convertFunctionRest(path) {
   });
 
   if (state.deopted) {
-    node.body.body.unshift(loop);
+    (node.body as t.BlockStatement).body.unshift(loop);
   } else {
     let target = path
       .getEarliestCommonAncestorFrom(state.references)
