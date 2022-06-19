819ae34e82e523463371e3eeaa48c63acb752809
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-06-19 14:38:42 +0200

    Update `babel-helper-plugin-test-runner/esm.mjs`

--- packages/babel-helper-plugin-test-runner/esm.mjs
+++ packages/babel-helper-plugin-test-runner/esm.mjs
@@ -1,11 +1,12 @@
 import _cjs from "./lib/index.js";
+const cjs = _cjs.default || _cjs;
 
-const adapter = _cjs.default.bind();
+const adapter = cjs.bind();
 
 // For backward compatibility with the CJS-only version, this makes
 //    import _x from "@babel/helper-plugin-test-runner"
 //    const x = _x.default
 // still work.
-adapter.default = _cjs.default;
+adapter.default = cjs;
 
 export default adapter;
