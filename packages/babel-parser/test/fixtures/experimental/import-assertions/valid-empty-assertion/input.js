840740f61bc37fccfce3b62703b9592c18754e84
Huáng Jùnliàng <jlhwung@gmail.com>
2022-06-21 13:19:35 -0400

    address review comment

--- packages/babel-parser/test/fixtures/experimental/import-assertions/valid-empty-assertion/input.js
+++ packages/babel-parser/test/fixtures/experimental/import-assertions/valid-empty-assertion/input.js
@@ -1,1 +1,4 @@
-import "foo" assert {};
+import foo, { bar } from "foo" assert {};
+import * as ns from "foo" assert {};
+export { quux } from "foo" assert {};
+export * as ns2 from "foo" assert {};
