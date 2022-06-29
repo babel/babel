7cdc9ef29c61dc1ccf57d3c1b52e157147fb725b
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-06-29 11:10:12 +0200

    Add failing test

---
+++ packages/babel-plugin-transform-modules-commonjs/test/fixtures/misc/local-exports-decl-with-esm-exports/output.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.x = void 0;
+var _exports = 1;
+let x = 2;
+exports.x = x;
+
+function fn() {
+  exports.x = x = 3;
+  var exports = 4;
+}
