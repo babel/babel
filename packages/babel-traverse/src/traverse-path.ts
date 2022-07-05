3e6fe1821cd236d56d541653422359cdd3dead3b
Daniel <daniel.s@crowdbotics.com>
2022-07-05 13:47:48 +0100

    Support opts in traversePath

--- packages/babel-traverse/src/traverse-path.ts
+++ packages/babel-traverse/src/traverse-path.ts
@@ -1,28 +1,44 @@
 import TraversalContext from "./context";
-import type { TraverseOptions } from "./index";
 import type NodePath from "./path";
 import type { Node } from "@babel/types";
+import type { Visitor } from "./types";
 import { VISITOR_KEYS } from "@babel/types";
+import { explode } from "./visitors";
+
+export type TraversePathOptions = {
+  visitSelf?: boolean;
+};
 
 /**
  * Traverse the given path, optionally visiting itself too
  *
+ * @function
  * @param path NodePath to be visited
- * @param opts The traverse options used to create a new traversal context
+ * @param visitor The traverse options used to create a new traversal context
  * @param state A user data storage provided as the second callback argument for traversal visitors
+ * @param opts User provided options for traversal
  * @returns Whether the traversal stops early
  */
-export const traversePath = (
+export function traversePath(
   path: NodePath,
-  opts: TraverseOptions,
-  visitSelf: boolean,
+  visitor: Visitor,
   state?: any,
-): boolean => {
-  const { node, type, scope } = path;
-  const context = new TraversalContext(scope, opts, state, path);
+  opts?: TraversePathOptions,
+): boolean {
+  const { type, scope } = path;
 
-  if (visitSelf) return context.visitQueue([path]);
+  explode(visitor as Visitor);
+  const context = new TraversalContext(scope, visitor, state, path);
 
+  if (opts && opts.visitSelf) return context.visitQueue([path]);
+
+  const queue = buildQueue(path, type);
+  if (!queue.length) return false;
+  return context.visitQueue(queue);
+}
+
+function buildQueue(path: NodePath, type: string): NodePath[] {
+  const { node } = path;
   const queue: NodePath[] | [] = [];
   const keys = VISITOR_KEYS[type];
 
@@ -39,6 +55,5 @@ export const traversePath = (
     }
   }
 
-  if (!queue) return false;
-  return context.visitQueue(queue);
-};
+  return queue;
+}
