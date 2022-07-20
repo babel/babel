ea5ff29f3ec5bc6d53a8a7783b6f7d677cc4f1f7
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-07-20 17:42:13 +0200

    Extract string parsing to a separate package (#14772)

---
+++ packages/babel-helper-string-parser/src/index.ts
@@ -0,0 +1,396 @@
+import * as charCodes from "charcodes";
+
+// The following character codes are forbidden from being
+// an immediate sibling of NumericLiteralSeparator _
+const forbiddenNumericSeparatorSiblings = {
+  decBinOct: new Set<number>([
+    charCodes.dot,
+    charCodes.uppercaseB,
+    charCodes.uppercaseE,
+    charCodes.uppercaseO,
+    charCodes.underscore, // multiple separators are not allowed
+    charCodes.lowercaseB,
+    charCodes.lowercaseE,
+    charCodes.lowercaseO,
+  ]),
+  hex: new Set<number>([
+    charCodes.dot,
+    charCodes.uppercaseX,
+    charCodes.underscore, // multiple separators are not allowed
+    charCodes.lowercaseX,
+  ]),
+};
+
+const isAllowedNumericSeparatorSibling = {
+  // 0 - 1
+  bin: (ch: number) => ch === charCodes.digit0 || ch === charCodes.digit1,
+
+  // 0 - 7
+  oct: (ch: number) => ch >= charCodes.digit0 && ch <= charCodes.digit7,
+
+  // 0 - 9
+  dec: (ch: number) => ch >= charCodes.digit0 && ch <= charCodes.digit9,
+
+  // 0 - 9, A - F, a - f,
+  hex: (ch: number) =>
+    (ch >= charCodes.digit0 && ch <= charCodes.digit9) ||
+    (ch >= charCodes.uppercaseA && ch <= charCodes.uppercaseF) ||
+    (ch >= charCodes.lowercaseA && ch <= charCodes.lowercaseF),
+};
+
+export type StringContentsErrorHandlers = EscapedCharErrorHandlers & {
+  unterminated(
+    initialPos: number,
+    initialLineStart: number,
+    initialCurLine: number,
+  ): void;
+};
+
+export function readStringContents(
+  type: "single" | "double" | "template",
+  input: string,
+  pos: number,
+  lineStart: number,
+  curLine: number,
+  errors: StringContentsErrorHandlers,
+) {
+  const initialPos = pos;
+  const initialLineStart = lineStart;
+  const initialCurLine = curLine;
+
+  let out = "";
+  let containsInvalid = false;
+  let chunkStart = pos;
+  const { length } = input;
+  for (;;) {
+    if (pos >= length) {
+      errors.unterminated(initialPos, initialLineStart, initialCurLine);
+      break;
+    }
+    const ch = input.charCodeAt(pos);
+    if (isStringEnd(type, ch, input, pos)) {
+      out += input.slice(chunkStart, pos);
+      break;
+    }
+    if (ch === charCodes.backslash) {
+      out += input.slice(chunkStart, pos);
+      let escaped;
+      ({
+        ch: escaped,
+        pos,
+        lineStart,
+        curLine,
+      } = readEscapedChar(
+        input,
+        pos,
+        lineStart,
+        curLine,
+        type === "template",
+        errors,
+      ));
+      if (escaped === null) {
+        containsInvalid = true;
+      } else {
+        out += escaped;
+      }
+      chunkStart = pos;
+    } else if (
+      ch === charCodes.lineSeparator ||
+      ch === charCodes.paragraphSeparator
+    ) {
+      ++pos;
+      ++curLine;
+      lineStart = pos;
+    } else if (ch === charCodes.lineFeed || ch === charCodes.carriageReturn) {
+      if (type === "template") {
+        out += input.slice(chunkStart, pos) + "\n";
+        ++pos;
+        if (
+          ch === charCodes.carriageReturn &&
+          input.charCodeAt(pos) === charCodes.lineFeed
+        ) {
+          ++pos;
+        }
+        ++curLine;
+        chunkStart = lineStart = pos;
+      } else {
+        errors.unterminated(initialPos, initialLineStart, initialCurLine);
+      }
+    } else {
+      ++pos;
+    }
+  }
+  return { pos, str: out, containsInvalid, lineStart, curLine };
+}
+
+function isStringEnd(
+  type: "single" | "double" | "template",
+  ch: number,
+  input: string,
+  pos: number,
+) {
+  if (type === "template") {
+    return (
+      ch === charCodes.graveAccent ||
+      (ch === charCodes.dollarSign &&
+        input.charCodeAt(pos + 1) === charCodes.leftCurlyBrace)
+    );
+  }
+  return (
+    ch === (type === "double" ? charCodes.quotationMark : charCodes.apostrophe)
+  );
+}
+
+export type EscapedCharErrorHandlers = HexCharErrorHandlers &
+  CodePointErrorHandlers & {
+    strictNumericEscape(pos: number): void;
+  };
+
+export function readEscapedChar(
+  input: string,
+  pos: number,
+  lineStart: number,
+  curLine: number,
+  inTemplate: boolean,
+  errors: EscapedCharErrorHandlers,
+) {
+  const throwOnInvalid = !inTemplate;
+  pos++; // skip '\'
+
+  const res = (ch: string | null) => ({ pos, ch, lineStart, curLine });
+
+  const ch = input.charCodeAt(pos++);
+  switch (ch) {
+    case charCodes.lowercaseN:
+      return res("\n");
+    case charCodes.lowercaseR:
+      return res("\r");
+    case charCodes.lowercaseX: {
+      let code;
+      ({ code, pos } = readHexChar(
+        input,
+        pos,
+        2,
+        false,
+        throwOnInvalid,
+        errors,
+      ));
+      return res(code === null ? null : String.fromCharCode(code));
+    }
+    case charCodes.lowercaseU: {
+      let code;
+      ({ code, pos } = readCodePoint(input, pos, throwOnInvalid, errors));
+      return res(code === null ? null : String.fromCodePoint(code));
+    }
+    case charCodes.lowercaseT:
+      return res("\t");
+    case charCodes.lowercaseB:
+      return res("\b");
+    case charCodes.lowercaseV:
+      return res("\u000b");
+    case charCodes.lowercaseF:
+      return res("\f");
+    case charCodes.carriageReturn:
+      if (input.charCodeAt(pos) === charCodes.lineFeed) {
+        ++pos;
+      }
+    // fall through
+    case charCodes.lineFeed:
+      lineStart = pos;
+      ++curLine;
+    // fall through
+    case charCodes.lineSeparator:
+    case charCodes.paragraphSeparator:
+      return res("");
+    case charCodes.digit8:
+    case charCodes.digit9:
+      if (inTemplate) {
+        return res(null);
+      } else {
+        errors.strictNumericEscape(pos - 1);
+      }
+    // fall through
+    default:
+      if (ch >= charCodes.digit0 && ch <= charCodes.digit7) {
+        const startPos = pos - 1;
+        const match = input.slice(startPos, pos + 2).match(/^[0-7]+/)!;
+
+        let octalStr = match[0];
+
+        let octal = parseInt(octalStr, 8);
+        if (octal > 255) {
+          octalStr = octalStr.slice(0, -1);
+          octal = parseInt(octalStr, 8);
+        }
+        pos += octalStr.length - 1;
+        const next = input.charCodeAt(pos);
+        if (
+          octalStr !== "0" ||
+          next === charCodes.digit8 ||
+          next === charCodes.digit9
+        ) {
+          if (inTemplate) {
+            return res(null);
+          } else {
+            errors.strictNumericEscape(startPos);
+          }
+        }
+
+        return res(String.fromCharCode(octal));
+      }
+
+      return res(String.fromCharCode(ch));
+  }
+}
+
+type HexCharErrorHandlers = IntErrorHandlers & {
+  invalidEscapeSequence(pos: number, startPos: number): void;
+};
+
+// Used to read character escape sequences ('\x', '\u').
+function readHexChar(
+  input: string,
+  pos: number,
+  len: number,
+  forceLen: boolean,
+  throwOnInvalid: boolean,
+  errors: HexCharErrorHandlers,
+) {
+  const initialPos = pos;
+  let n;
+  ({ n, pos } = readInt(input, pos, 16, len, forceLen, false, errors));
+  if (n === null) {
+    if (throwOnInvalid) {
+      errors.invalidEscapeSequence(pos, initialPos);
+    } else {
+      pos = initialPos - 1;
+    }
+  }
+  return { code: n, pos };
+}
+
+export type IntErrorHandlers = {
+  numericSeparatorInEscapeSequence(pos: number): void;
+  unexpectedNumericSeparator(pos: number): void;
+  // It can return "true" to indicate that the error was handled
+  // and the int parsing should continue.
+  invalidDigit(pos: number, radix: number): boolean;
+};
+
+export function readInt(
+  input: string,
+  pos: number,
+  radix: number,
+  len: number | undefined,
+  forceLen: boolean,
+  allowNumSeparator: boolean | "bail",
+  errors: IntErrorHandlers,
+) {
+  const start = pos;
+  const forbiddenSiblings =
+    radix === 16
+      ? forbiddenNumericSeparatorSiblings.hex
+      : forbiddenNumericSeparatorSiblings.decBinOct;
+  const isAllowedSibling =
+    radix === 16
+      ? isAllowedNumericSeparatorSibling.hex
+      : radix === 10
+      ? isAllowedNumericSeparatorSibling.dec
+      : radix === 8
+      ? isAllowedNumericSeparatorSibling.oct
+      : isAllowedNumericSeparatorSibling.bin;
+
+  let invalid = false;
+  let total = 0;
+
+  for (let i = 0, e = len == null ? Infinity : len; i < e; ++i) {
+    const code = input.charCodeAt(pos);
+    let val;
+
+    if (code === charCodes.underscore && allowNumSeparator !== "bail") {
+      const prev = input.charCodeAt(pos - 1);
+      const next = input.charCodeAt(pos + 1);
+
+      if (!allowNumSeparator) {
+        errors.numericSeparatorInEscapeSequence(pos);
+      } else if (
+        Number.isNaN(next) ||
+        !isAllowedSibling(next) ||
+        forbiddenSiblings.has(prev) ||
+        forbiddenSiblings.has(next)
+      ) {
+        errors.unexpectedNumericSeparator(pos);
+      }
+
+      // Ignore this _ character
+      ++pos;
+      continue;
+    }
+
+    if (code >= charCodes.lowercaseA) {
+      val = code - charCodes.lowercaseA + charCodes.lineFeed;
+    } else if (code >= charCodes.uppercaseA) {
+      val = code - charCodes.uppercaseA + charCodes.lineFeed;
+    } else if (charCodes.isDigit(code)) {
+      val = code - charCodes.digit0; // 0-9
+    } else {
+      val = Infinity;
+    }
+    if (val >= radix) {
+      // If we found a digit which is too big, errors.invalidDigit can return true to avoid
+      // breaking the loop (this is used for error recovery).
+      if (val <= 9 && errors.invalidDigit(pos, radix)) {
+        val = 0;
+      } else if (forceLen) {
+        val = 0;
+        invalid = true;
+      } else {
+        break;
+      }
+    }
+    ++pos;
+    total = total * radix + val;
+  }
+  if (pos === start || (len != null && pos - start !== len) || invalid) {
+    return { n: null, pos };
+  }
+
+  return { n: total, pos };
+}
+
+export type CodePointErrorHandlers = HexCharErrorHandlers & {
+  invalidCodePoint(pos: number): void;
+};
+
+export function readCodePoint(
+  input: string,
+  pos: number,
+  throwOnInvalid: boolean,
+  errors: CodePointErrorHandlers,
+) {
+  const ch = input.charCodeAt(pos);
+  let code;
+
+  if (ch === charCodes.leftCurlyBrace) {
+    ++pos;
+    ({ code, pos } = readHexChar(
+      input,
+      pos,
+      input.indexOf("}", pos) - pos,
+      true,
+      throwOnInvalid,
+      errors,
+    ));
+    ++pos;
+    if (code !== null && code > 0x10ffff) {
+      if (throwOnInvalid) {
+        errors.invalidCodePoint(pos);
+      } else {
+        return { code: null, pos };
+      }
+    }
+  } else {
+    ({ code, pos } = readHexChar(input, pos, 4, false, throwOnInvalid, errors));
+  }
+  return { code, pos };
+}
