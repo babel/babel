cad4ea76cd7616aa2f6af5d8c1cb31ab4a0d1288
Huáng Jùnliàng <jlhwung@gmail.com>
2022-08-27 10:15:47 -0400

    Fix helper-builder-react-jsx compat with Babel 7.9 (#14886)
    * add test
    
    * fix: build-react-jsx should pass PluginPass in post

---
+++ packages/babel-helper-builder-react-jsx/test/index.js
@@ -0,0 +1,72 @@
+import _helper from "../lib/index.js";
+import { transformSync } from "@babel/core";
+import * as t from "@babel/types";
+
+const helper = _helper.default || _helper;
+
+describe("@babel/helper-builder-react-jsx", () => {
+  // The builder-react-jsx usage in transform-react-jsx 7.9.0
+  // https://github.com/babel/babel/blob/v7.9.0/packages/babel-plugin-transform-react-jsx/src/transform-classic.js#L43
+  it("shuold pass post with plugin pass", () => {
+    const fn = jest.fn().mockReturnValue(t.identifier("foo"));
+    const visitor = helper({
+      post(state, pass) {
+        state.callee = pass.get("jsxIdentifier")();
+      },
+    });
+    visitor.Program = function enter(_, state) {
+      state.set("jsxIdentifier", fn);
+    };
+    const plugin = () => ({ visitor });
+    const input = `<element></element>`;
+    transformSync(input, {
+      filename: "builder-react-jsx-test.jsx",
+      configFile: false,
+      plugins: [plugin],
+      parserOpts: { plugins: ["jsx"] },
+    });
+    expect(fn).toBeCalledTimes(1);
+  });
+
+  it("shuold pass pre with plugin pass", () => {
+    const fn = jest.fn().mockReturnValue(t.identifier("foo"));
+    const visitor = helper({
+      pre(state, pass) {
+        state.callee = pass.get("jsxIdentifier")();
+      },
+    });
+    visitor.Program = function enter(_, state) {
+      state.set("jsxIdentifier", fn);
+    };
+    const plugin = () => ({ visitor });
+    const input = `<element></element>`;
+    transformSync(input, {
+      filename: "builder-react-jsx-test.jsx",
+      configFile: false,
+      plugins: [plugin],
+      parserOpts: { plugins: ["jsx"] },
+    });
+    expect(fn).toBeCalledTimes(1);
+  });
+
+  it("shuold pass filter with plugin pass", () => {
+    const fn = jest.fn().mockReturnValue(false);
+    const visitor = helper({
+      filter(_, pass) {
+        return pass.get("filterAll")();
+      },
+    });
+    visitor.Program = function enter(_, state) {
+      state.set("filterAll", fn);
+    };
+    const plugin = () => ({ visitor });
+    const input = `<element></element>`;
+    transformSync(input, {
+      filename: "builder-react-jsx-test.jsx",
+      configFile: false,
+      plugins: [plugin],
+      parserOpts: { plugins: ["jsx"] },
+    });
+    expect(fn).toBeCalledTimes(1);
+  });
+});
