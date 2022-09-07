37ef28c6b060a3341e836ad6866b9f04b6a5745c
Huáng Jùnliàng <jlhwung@gmail.com>
2022-09-07 15:16:55 -0400

    fix: forward the original rule in ESLint 8

--- eslint/babel-eslint-plugin/src/rules/no-invalid-this.cjs
+++ eslint/babel-eslint-plugin/src/rules/no-invalid-this.cjs
@@ -1,33 +1,40 @@
 const ruleComposer = require("eslint-rule-composer");
 const eslint = require("eslint");
+const eslintVersion = eslint.ESLint.version;
 
 const noInvalidThisRule = new eslint.Linter().getRules().get("no-invalid-this");
 
-module.exports = ruleComposer.filterReports(noInvalidThisRule, problem => {
-  let inClassMember = false;
-  let node = problem.node;
+if (parseInt(eslintVersion, 10) >= 8) {
+  // ESLint 8 supports class properties / private methods natively
+  // so we simply forward the original rule
+  module.exports = noInvalidThisRule;
+} else {
+  module.exports = ruleComposer.filterReports(noInvalidThisRule, problem => {
+    let inClassMember = false;
+    let node = problem.node;
 
-  while (node) {
-    if (
-      node.type === "ClassPrivateMethod" ||
-      node.type === "ClassPrivateProperty" ||
-      node.type === "ClassProperty" ||
-      node.type === "PropertyDefinition" ||
-      (node.type === "MethodDefinition" &&
-        node.key.type === "PrivateIdentifier")
-    ) {
-      inClassMember = true;
-      break;
-    } else if (
-      node.type === "FunctionDeclaration" ||
-      node.type === "FunctionExpression"
-    ) {
-      inClassMember = false;
-      break;
+    while (node) {
+      if (
+        node.type === "ClassPrivateMethod" ||
+        node.type === "ClassPrivateProperty" ||
+        node.type === "ClassProperty" ||
+        node.type === "PropertyDefinition" ||
+        (node.type === "MethodDefinition" &&
+          node.key.type === "PrivateIdentifier")
+      ) {
+        inClassMember = true;
+        break;
+      } else if (
+        node.type === "FunctionDeclaration" ||
+        node.type === "FunctionExpression"
+      ) {
+        inClassMember = false;
+        break;
+      }
+
+      node = node.parent;
     }
 
-    node = node.parent;
-  }
-
-  return !inClassMember;
-});
+    return !inClassMember;
+  });
+}
