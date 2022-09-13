2c1b6fafe708d25c19fdbef83e0d9fc049dbf3ca
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-09-13 11:28:00 +0800

    update tests

--- packages/babel-preset-env/test/fixtures/preset-options-babel-7/browserslist-defaults-not-ie/output.js
+++ packages/babel-preset-env/test/fixtures/preset-options-babel-7/browserslist-defaults-not-ie/output.js
@@ -1,8 +1,6 @@
 "use strict";
 
-var test = function test(a, _ref, c) {
-  var _ref2 = babelHelpers.slicedToArray(_ref, 1),
-      b = _ref2[0];
-
+const test = (a, _ref, c) => {
+  let [b] = _ref;
   return "";
 };
