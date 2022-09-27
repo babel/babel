0ba2a036efbb72685f601088bfb6ee288daa1cb0
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-09-27 15:45:56 +0200

    Update after rebase

--- packages/babel-plugin-proposal-decorators/test/fixtures/legacy-regression/8559/output.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/legacy-regression/8559/output.js
@@ -7,8 +7,6 @@ exports.default = wrap;
 
 var _coreDecorators = require("core-decorators");
 
-function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object['ke' + 'ys'](descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object['define' + 'Property'](target, property, desc); desc = null; } return desc; }
-
 function wrap() {
   return function () {
     var _class;
@@ -16,7 +14,7 @@ function wrap() {
     let Foo = (_class = class Foo {
       method() {}
 
-    }, (_applyDecoratedDescriptor(_class.prototype, "method", [_coreDecorators.autobind], Object.getOwnPropertyDescriptor(_class.prototype, "method"), _class.prototype)), _class);
+    }, (babelHelpers.applyDecoratedDescriptor(_class.prototype, "method", [_coreDecorators.autobind], Object.getOwnPropertyDescriptor(_class.prototype, "method"), _class.prototype)), _class);
     return Foo;
   };
 }
