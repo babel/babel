6bcee3eda742fc3f2fa317f21395257a6f40042d
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-10-15 13:19:45 +0800

    rebase

--- packages/babel-generator/test/fixtures/sourcemaps/function-identifier-name/output.js
+++ packages/babel-generator/test/fixtures/sourcemaps/function-identifier-name/output.js
@@ -1,44 +1,29 @@
 function fn() {}
-
 export default function () {}
 var a = {
   fn() {},
-
   fn: function () {},
   [fn]: function () {},
   ["fn"]: function () {},
   [function () {}]: function () {},
   [() => {}]: function () {},
-
   [fn]() {},
-
   ["fn"]() {},
-
   [function () {}]() {},
-
   [() => {}]() {}
-
 };
-
 class b {
   fn() {}
-
   fn = function () {};
   [fn] = function () {};
   ["fn"] = function () {};
   [function () {}] = function () {};
   [() => {}] = function () {};
-
   [fn]() {}
-
   ["fn"]() {}
-
   [function () {}]() {}
-
   [() => {}]() {}
-
 }
-
 var aa = {
   fn: function a() {},
   [fn]: function a() {},
@@ -46,7 +31,6 @@ var aa = {
   [function () {}]: function a() {},
   [() => {}]: function a() {}
 };
-
 class bb {
   fn = function a() {};
   [fn] = function a() {};
@@ -54,17 +38,10 @@ class bb {
   [function () {}] = function a() {};
   [() => {}] = function a() {};
 }
-
 var x = function fn() {};
-
 var x = function () {};
-
 (function fn() {});
-
 var z = () => {};
-
 var z = x => {};
-
 var z = x => {};
-
 var z = (x, y, z) => {};
