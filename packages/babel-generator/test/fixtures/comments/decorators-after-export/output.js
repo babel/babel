1c1fa44c413db786999fb6ae5db2aada2b21a23e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-10-25 15:18:14 +0200

    Add generator tests

--- packages/babel-generator/test/fixtures/comments/decorators-after-export/output.js
+++ packages/babel-generator/test/fixtures/comments/decorators-after-export/output.js
@@ -1,3 +1,8 @@
-/* 1 */export /* 2 */@dec1
+/* 1 */export /* 4 */ /* 2 */@dec1
 /* 3 */@dec2
-/* 4 */class /* 5 */C /* 6 */ {/* 7 */} /* 8 */
+class /* 5 */C /* 6 */ {/* 7 */} /* 8 */
+
+/* A */
+export default /* E */ /* C */@dec1
+/* D */@dec2
+class /* F */{/* G */} /* H */
--- packages/babel-generator/test/fixtures/comments/decorators-before-export/output.js
+++ packages/babel-generator/test/fixtures/comments/decorators-before-export/output.js
@@ -1,3 +1,10 @@
+/* 3 */
 /* 1 */@dec1
 /* 2 */@dec2
-/* 3 */export /* 4 */class /* 5 */C /* 6 */ {/* 7 */} /* 8 */
+export /* 4 */class /* 5 */C /* 6 */ {/* 7 */} /* 8 */
+
+/* A */
+/* C */
+@dec1
+/* B */@dec2
+export default /* E */class /* F */{/* G */} /* H */
