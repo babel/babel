86366059b65ac7c766781c9a69e709376eb57fe9
Huáng Jùnliàng <jlhwung@gmail.com>
2022-10-28 04:07:12 -0400

    bump typescript and linter versions (#15086)
    * bump typescript and linter versions
    
    * check uncommitted files after lint
    
    * Update .github/workflows/ci.yml

--- scripts/assert-dir-git-clean.js
+++ scripts/assert-dir-git-clean.js
@@ -2,9 +2,11 @@
 
 import { execSync } from "child_process";
 
+const fixCommand = `make ${process.argv[2]}`;
+
 if (execSync("git status --porcelain=v1", { encoding: "utf8" })) {
   console.log(
-    'Please re-run "make build" and checkout the following changes to git'
+    `Please re-run "${fixCommand}" and checkout the following changes to git`
   );
   execSync("git status", { stdio: "inherit" });
   // eslint-disable-next-line no-process-exit
