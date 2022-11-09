4511a961a5bbdc748601cf71fe80069bfc034bde
Huáng Jùnliàng <jlhwung@gmail.com>
2022-11-09 10:23:28 -0500

    build: improve artifacts IO (#15165)
    * build: improve artifacts io
    
    * simplify babel 8 artifact name

---
+++ scripts/get-artifact-files.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# The artifact files will be packed into an archive shared among CI runners
+# usage:
+#   $ get-artifact-files.sh | tar --null -cvf babel-artifact.tar --files-from=-
+find . \
+  -type d -name "*node_modules*" -prune -false \
+  -o \( \
+    -type f -path "./codemods/*/lib/*" \
+    -o -type f -path "./eslint/*/lib/*" \
+    -o -type f -path "./packages/*/lib/*" \
+    -o -type f -path "./packages/babel-standalone/*" \
+    -o -type f -path "./packages/babel-runtime/*" -name "*.js" \
+    -o -type f -path "./packages/babel-runtime-corejs2/*" -name "*.js" \
+    -o -type f -path "./packages/babel-runtime-corejs3/*" -name "*.js" \
+  \) \
+  -print0
