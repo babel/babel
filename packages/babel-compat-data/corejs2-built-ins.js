5627ecd5d54fcb118840d5f95d064bc451521440
Huáng Jùnliàng <jlhwung@gmail.com>
2022-11-11 12:14:23 -0500

    preserve corejs2-built-ins

--- packages/babel-compat-data/corejs2-built-ins.js
+++ packages/babel-compat-data/corejs2-built-ins.js
@@ -1,2 +1,4 @@
 // Todo (Babel 8): remove this file as Babel 8 drop support of core-js 2
-module.exports = require("./data/corejs2-built-ins.json");
+module.exports = process.env.BABEL_8_BREAKING
+  ? require("./data/corejs2-built-ins-next.json")
+  : require("./data/corejs2-built-ins.json");
