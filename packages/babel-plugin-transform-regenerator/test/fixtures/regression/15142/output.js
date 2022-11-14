90131125683c0b930853471733b4677240df5279
SuperSodaSea <bobby825@126.com>
2022-11-15 03:38:26 +0800

    Bump `regenerator` and add tests

---
+++ packages/babel-plugin-transform-regenerator/test/fixtures/regression/15142/output.js
@@ -0,0 +1,17 @@
+/*#__PURE__*/babelHelpers.asyncToGenerator( /*#__PURE__*/babelHelpers.regeneratorRuntime().mark(function _callee() {
+  return babelHelpers.regeneratorRuntime().wrap(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return 0;
+        case 2:
+          _context.t0 = _context.sent;
+          return _context.abrupt("return", [_context.t0,,]);
+        case 4:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, _callee);
+}));
