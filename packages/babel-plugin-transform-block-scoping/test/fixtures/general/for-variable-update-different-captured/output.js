fc66e9962c3aa0b5d57cba177401b20801aa5c9e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-12-23 21:56:12 +0100

    Fix for binding shadowing outer var with loop closure (#15309)
    * Fix for binding shadowing outer var with loop closure
    
    Fix updating of non-captured variable with loop closure
    
    * Rename

---
+++ packages/babel-plugin-transform-block-scoping/test/fixtures/general/for-variable-update-different-captured/output.js
@@ -0,0 +1,11 @@
+var _loop = function (_i) {
+  var y = _i;
+  (function () {
+    return y;
+  });
+  _i++;
+  i = _i;
+};
+for (var i = 4; i < 6; i++) {
+  _loop(i);
+}
---
+++ packages/babel-plugin-transform-block-scoping/test/fixtures/general/issue-15308-for-variable-shadow-and-update/input.js
@@ -0,0 +1,6 @@
+let i;
+for (let i = 4; i < 6; i++) {
+  let y = i;
+  () => y;
+  i++;
+}
