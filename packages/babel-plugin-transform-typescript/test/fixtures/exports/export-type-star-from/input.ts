602cc247a1ca8a69d01ba5486b373f6aeed4c52b
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-01-30 18:45:45 +0100

    typo

--- packages/babel-plugin-transform-typescript/test/fixtures/exports/export-type-star-from/input.ts
+++ packages/babel-plugin-transform-typescript/test/fixtures/exports/export-type-star-from/input.ts
@@ -1,7 +1,7 @@
 export type * from './mod';
 export type * as ns from './mod';
 // Note: TSC doesn't support string module specifiers yet,
-// but it's easier for us to support it that not.
+// but it's easier for us to support them than not.
 export type * as "ns2" from './mod';
 
 ;
--- packages/babel-plugin-transform-typescript/test/fixtures/exports/export-type-star-from/output.mjs
+++ packages/babel-plugin-transform-typescript/test/fixtures/exports/export-type-star-from/output.mjs
@@ -1,5 +1,5 @@
 // Note: TSC doesn't support string module specifiers yet,
-// but it's easier for us to support it that not.
+// but it's easier for us to support them than not.
 
 ;
 export {};
