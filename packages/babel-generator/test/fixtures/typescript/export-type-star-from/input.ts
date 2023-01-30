d1b7a376507b786754066adb3e7d79a0cf04b921
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-01-30 17:47:54 +0100

    Add `export type * as` tests

--- packages/babel-generator/test/fixtures/typescript/export-type-star-from/input.ts
+++ packages/babel-generator/test/fixtures/typescript/export-type-star-from/input.ts
@@ -1,1 +1,5 @@
 export type * from './mod';
+export type * as ns from './mod';
+// Note: TSC doesn't support string module specifiers yet,
+// but it's easier for us to support it that not.
+export type * as "ns2" from './mod';
--- packages/babel-plugin-transform-typescript/test/fixtures/exports/export-type-star-from/output.mjs
+++ packages/babel-plugin-transform-typescript/test/fixtures/exports/export-type-star-from/output.mjs
@@ -1,1 +1,5 @@
+// Note: TSC doesn't support string module specifiers yet,
+// but it's easier for us to support it that not.
+
 ;
+export {};
