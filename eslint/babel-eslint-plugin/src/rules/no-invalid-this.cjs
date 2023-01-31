5144ff30652da3d0b6dda08fd377e1ca0245a244
”jiatianqi“ <jiatianqi@baidu.com>
2023-01-31 19:37:45 +0800

    fix: Version lower than Eslint 7, no ESLint class

--- eslint/babel-eslint-plugin/src/rules/no-invalid-this.cjs
+++ eslint/babel-eslint-plugin/src/rules/no-invalid-this.cjs
@@ -1,6 +1,6 @@
 const ruleComposer = require("eslint-rule-composer");
 const eslint = require("eslint");
-const eslintVersion = eslint.ESLint.version;
+const eslintVersion = eslint?.ESLint.version;
 
 const noInvalidThisRule = new eslint.Linter().getRules().get("no-invalid-this");
 
