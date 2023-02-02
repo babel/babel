3aee4e367ac28fe5ce2bfac4e8ca35976978f879
Huáng Jùnliàng <jlhwung@gmail.com>
2023-02-02 09:57:54 -0500

    Disallows specifiers after export * as ns (#15385)
    * disallow specifiers after export * as ns
    
    * update test cases
    
    * ignore js/exports/jsfmt.spec.js e2e tests
    
    * Update scripts/integration-tests/e2e-prettier.sh
    
    Co-authored-by: fisker Cheung <lionkay@gmail.com>
    
    ---------
    
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- scripts/integration-tests/e2e-prettier.sh
+++ scripts/integration-tests/e2e-prettier.sh
@@ -42,6 +42,11 @@ yarn lint:typecheck
 # https://github.com/babel/babel/pull/14892#issuecomment-1233180626
 echo "export default () => () => {}" > src/main/create-print-pre-check-function.js
 
+# https://github.com/babel/babel/pull/15385#issuecomment-1409840781
+# Ignore this test until prettier update the snapshot
+rm tests/format/js/exports/jsfmt.spec.js
+rm tests/format/js/export-extension/jsfmt.spec.js
+
 yarn test "tests/format/(jsx?|misc|typescript|flow|flow-repo)/" --update-snapshot --runInBand
 
 cleanup
