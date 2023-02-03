09c23e8ec64abaf67b0a89c28bfedf7a751612da
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-02-03 17:51:31 +0100

    Add comments attachment tests to generator

---
+++ packages/babel-generator/test/fixtures/comments/decorators-before-export-to-after/output.js
@@ -0,0 +1,9 @@
+/* 1 */export /* 4 */@dec1
+/* 2 */@dec2
+/* 3 */class /* 5 */C /* 6 */ {/* 7 */} /* 8 */
+
+/* A */
+export
+/* D */ default /* E */@dec1
+/* B */@dec2
+/* C */class /* F */{/* G */} /* H */
