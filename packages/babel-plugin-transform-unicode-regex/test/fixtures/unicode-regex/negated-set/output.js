1f2c798254e011afce3bd9b160d33d848c689a8b
Huáng Jùnliàng <jlhwung@gmail.com>
2023-02-09 09:33:36 -0500

    update test fixtures

--- packages/babel-plugin-transform-unicode-regex/test/fixtures/unicode-regex/negated-set/output.js
+++ packages/babel-plugin-transform-unicode-regex/test/fixtures/unicode-regex/negated-set/output.js
@@ -1,6 +1,6 @@
-/(?:(?!K)[\s\S])/.test(string);
-/(?:(?!k)[\s\S])/.test(string);
-/(?:(?!\u212A)[\s\S])/.test(string);
-/(?:(?![K\u212A])[\s\S])/i.test(string);
-/(?:(?![k\u212A])[\s\S])/i.test(string);
-/(?:(?![K\u212A])[\s\S])/i.test(string);
+/(?:[\0-JL-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])/.test(string);
+/(?:[\0-jl-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])/.test(string);
+/(?:[\0-\u2129\u212B-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])/.test(string);
+/(?:(?![K\u212A\uD800-\uDFFF])[\s\S]|[\uD800-\uDBFF][\uDC00-\uDFFF])/i.test(string);
+/(?:(?![k\u212A\uD800-\uDFFF])[\s\S]|[\uD800-\uDBFF][\uDC00-\uDFFF])/i.test(string);
+/(?:(?![K\u212A\uD800-\uDFFF])[\s\S]|[\uD800-\uDBFF][\uDC00-\uDFFF])/i.test(string);
