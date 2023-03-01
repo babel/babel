5b8a4b37a08fe7ef3ad9bd317fc0be16f2765e07
Maciej Krüger <mkg20001@gmail.com>
2023-03-01 19:03:29 +0100

    fixup! remove node 12, convert should be done by build system

--- packages/babel-register/src/loader.mjs
+++ packages/babel-register/src/loader.mjs
@@ -36,11 +36,7 @@ global._BABEL_ESM_REGISTER = {
         ignoreNodeModules: false,
       });
 
-      JS_EXTENSIONS = new Set(
-        opts.extensions != null
-          ? opts.extensions
-          : client.getDefaultExtensions()
-      );
+      JS_EXTENSIONS = new Set(opts.extensions ?? client.getDefaultExtensions());
     }
   },
   revert() {
@@ -52,10 +48,14 @@ global._BABEL_ESM_REGISTER = {
   },
 };
 
+let readyPromise;
+
 if (env.BABEL_REGISTER) {
-  const { default: opts } = await import(env.BABEL_REGISTER);
-  const { default: register } = await import("./experimental-worker.js");
-  register(opts);
+  readyPromise = (async () => {
+    const { default: opts } = await import(env.BABEL_REGISTER);
+    const { default: register } = await import("./experimental-worker.js");
+    register(opts);
+  })();
 }
 
 function tryCJSResolve(specifier, context) {
@@ -104,6 +104,11 @@ async function exists(path) {
 }
 
 export async function resolve(specifier, context, nextResolve) {
+  if (readyPromise) {
+    await readyPromise;
+    readyPromise = null;
+  }
+
   if (!compile) {
     return nextResolve(specifier, context);
   }
