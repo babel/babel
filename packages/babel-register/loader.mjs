fe0297cbd561235bc68766155a723d0f74f6d50c
Maciej Krüger <mkg20001@gmail.com>
2023-03-01 17:50:39 +0100

    fixup! exports

--- packages/babel-register/loader.mjs
+++ packages/babel-register/loader.mjs
@@ -1,21 +1,21 @@
-/*import {
+import {
   load as _load,
   resolve as _resolve,
+  globalPreload as _globalPreload,
   getSource as _getSource,
   getFormat as _getFormat,
-  globalPreload as _globalPreload,
   getGlobalPreloadCode as _getGlobalPreloadCode,
 } from "./lib/loader.mjs";
 
 const [major] = process.versions.node.split(".").map(Number);
 
-export const load = major >= 16 && _load;
-export const resolve = major >= 16 && _resolve;
-export const globalPreload = major >= 16 && _globalPreload;
+export const load = _load;
+export const resolve = _resolve;
+export const globalPreload = _globalPreload;
 
-export const getSource = major < 16 && _getSource;
-export const getFormat = major < 16 && _getFormat;
-export const getGlobalPreloadCode = major < 16 && _getGlobalPreloadCode;
-*/
-
-export * from "./lib/loader.mjs";
+// put the old exports behind a conditional export
+// to prevent deprecation warnings on newer node versions
+export const getSource = major < 16 ? _getSource : undefined;
+export const getFormat = major < 16 ? _getFormat : undefined;
+export const getGlobalPreloadCode =
+  major < 16 ? _getGlobalPreloadCode : undefined;
