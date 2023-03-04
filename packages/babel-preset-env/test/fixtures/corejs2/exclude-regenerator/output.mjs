9a85d70381beda6011e8703e13b3a5fd6056654a
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2023-03-04 17:17:24 +0800

    chore: Update `caniuse-lite` (#15468)
    update

--- packages/babel-preset-env/test/fixtures/corejs2/exclude-regenerator/output.mjs
+++ packages/babel-preset-env/test/fixtures/corejs2/exclude-regenerator/output.mjs
@@ -1,4 +1,3 @@
-import "core-js/modules/es7.array.includes.js";
 import "core-js/modules/web.timers.js";
 import "core-js/modules/web.immediate.js";
 import "core-js/modules/web.dom.iterable.js";
--- packages/babel-preset-env/test/fixtures/corejs3/entry-stable/output.mjs
+++ packages/babel-preset-env/test/fixtures/corejs3/entry-stable/output.mjs
@@ -1,3 +1,2 @@
-import "core-js/modules/es.array.includes.js";
 import "core-js/modules/es.regexp.flags.js";
 import "core-js/modules/web.immediate.js";
