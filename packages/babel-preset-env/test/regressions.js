d7e212eaa4351923eeb69a7f72b85377be504e6b
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-03-08 12:38:56 +0100

    Fix test

--- packages/babel-preset-env/test/regressions.js
+++ packages/babel-preset-env/test/regressions.js
@@ -4,25 +4,38 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 describe("#12880", () => {
+  afterEach(() => {
+    jest.spyOn(process, "cwd").mockRestore();
+  });
+
   it("read the .browserslistrc file when using @babel/core < 7.13.0", () => {
     const root = path.join(
       path.dirname(fileURLToPath(import.meta.url)),
       "regressions",
     );
 
-    // The browserslistrc file contains "firefox 50".
-    // a ** b is supported starting from firefox 52;
-    // a => b is supported starting from firefox 45.
-    const out = babel7_12.transformSync("a ** b; a => b;", {
-      configFile: false,
-      presets: [[env, { modules: false }]],
-      filename: path.join(root, "input.js"),
-      root,
-    });
+    // Unfortunatly, when loading the browserslist config through
+    // @babel/preset-env, it's resolved from process.cwd() and not
+    // from root. Mock it to isolate this test.
+    const spy = jest.spyOn(process, "cwd").mockReturnValue(root);
 
-    expect(out.code).toMatchInlineSnapshot(`
-      "Math.pow(a, b);
-      a => b;"
-    `);
+    try {
+      // The browserslistrc file contains "firefox 50".
+      // a ** b is supported starting from firefox 52;
+      // a => b is supported starting from firefox 45.
+      const out = babel7_12.transformSync("a ** b; a => b;", {
+        configFile: false,
+        presets: [[env, { modules: false }]],
+        filename: path.join(root, "input.js"),
+        root,
+      });
+
+      expect(out.code).toMatchInlineSnapshot(`
+        "Math.pow(a, b);
+        a => b;"
+      `);
+    } finally {
+      spy.mockRestore();
+    }
   });
 });
