eb56dc33eef4e428035a93d8e5fc3e16ef9a8a08
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-03-10 13:35:26 +0100

    Skip node deprecation warnings when used by a `@babel` package

--- packages/babel-types/src/utils/deprecationWarning.ts
+++ packages/babel-types/src/utils/deprecationWarning.ts
@@ -8,9 +8,16 @@ export default function deprecationWarning(
   if (warnings.has(oldName)) return;
   warnings.add(oldName);
 
-  const stack = captureShortStackTrace(1, 2);
+  const { internal, trace } = captureShortStackTrace(1, 2);
+  if (internal) {
+    // If usage comes from an internal package, there is no point in warning because
+    // 1. The new version of the package will already use the new API
+    // 2. When the deprecation will become an error (in a future major version), users
+    //    will have to update every package anyway.
+    return;
+  }
   console.warn(
-    `${prefix}\`${oldName}\` has been deprecated, please migrate to \`${newName}\`\n${stack}`,
+    `${prefix}\`${oldName}\` has been deprecated, please migrate to \`${newName}\`\n${trace}`,
   );
 }
 
@@ -26,8 +33,11 @@ function captureShortStackTrace(skip: number, length: number) {
   Error.stackTraceLimit = stackTraceLimit;
   Error.prepareStackTrace = prepareStackTrace;
 
-  return stackTrace
-    .slice(1 + skip, 1 + skip + length)
-    .map(frame => `    at ${frame}`)
-    .join("\n");
+  if (!stackTrace) return { internal: false, trace: "" };
+
+  const shortStackTrace = stackTrace.slice(1 + skip, 1 + skip + length);
+  return {
+    internal: /[\\/]@babel[\\/]/.test(shortStackTrace[1].getFileName()),
+    trace: shortStackTrace.map(frame => `    at ${frame}`).join("\n"),
+  };
 }
