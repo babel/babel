9e67898cb91bc6cffd820dccf0268f2ac8d3fb0f
magic-akari <akari.ccino@gmail.com>
2023-03-14 21:28:29 +0800

    fix: Handle `export { type foo }` as inType (#15494)

--- packages/babel-plugin-transform-typescript/src/index.ts
+++ packages/babel-plugin-transform-typescript/src/index.ts
@@ -24,9 +24,12 @@ function isInType(path: NodePath) {
       );
     case "ExportSpecifier":
       return (
+        // export { type foo };
+        path.parent.exportKind === "type" ||
+        // export type { foo };
         // @ts-expect-error: DeclareExportDeclaration does not have `exportKind`
         (path.parentPath as NodePath<t.ExportSpecifier>).parent.exportKind ===
-        "type"
+          "type"
       );
     default:
       return false;
