0eaf3b7c4ee7b98bfff998469219fdbcfb329ff5
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-03-29 12:12:20 +0200

    Skip cts config tests since they are not working yet
    Copied from https://github.com/babel/babel/pull/15478
    
    Co-authored-by: liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>

--- packages/babel-core/test/config-ts.js
+++ packages/babel-core/test/config-ts.js
@@ -1,5 +1,6 @@
 import { loadPartialConfigSync } from "../lib/index.js";
 import path from "path";
+import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { createRequire } from "module";
 import semver from "semver";
@@ -7,13 +8,22 @@ import semver from "semver";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const require = createRequire(import.meta.url);
 
+let USE_ESM = false;
+try {
+  const type = readFileSync(
+    new URL("../../../.module-type", import.meta.url),
+    "utf-8",
+  ).trim();
+  USE_ESM = type === "module";
+} catch {}
+
 // We skip older versions of node testing for two reasons.
 // 1. ts-node does not support the old version of node.
 // 2. In the old version of node, jest has been registered in `require.extensions`, which will cause babel to disable the transforming as expected.
+// TODO: Make it work with USE_ESM.
+const shouldSkip = semver.lt(process.version, "12.0.0") || USE_ESM;
 
-const lessThanNode12 = semver.lt(process.version, "12.0.0");
-
-(lessThanNode12 ? describe : describe.skip)(
+(shouldSkip ? describe : describe.skip)(
   "@babel/core config with ts [dummy]",
   () => {
     it("dummy", () => {
@@ -22,72 +32,69 @@ const lessThanNode12 = semver.lt(process.version, "12.0.0");
   },
 );
 
-(lessThanNode12 ? describe.skip : describe)(
-  "@babel/core config with ts",
-  () => {
-    it("should work with simple .cts", () => {
-      const config = loadPartialConfigSync({
-        configFile: path.join(
-          __dirname,
-          "fixtures/config-ts/simple-cts/babel.config.cts",
-        ),
-      });
+(shouldSkip ? describe.skip : describe)("@babel/core config with ts", () => {
+  it("should work with simple .cts", () => {
+    const config = loadPartialConfigSync({
+      configFile: path.join(
+        __dirname,
+        "fixtures/config-ts/simple-cts/babel.config.cts",
+      ),
+    });
 
-      expect(config.options.targets).toMatchInlineSnapshot(`
+    expect(config.options.targets).toMatchInlineSnapshot(`
           Object {
             "node": "12.0.0",
           }
         `);
-    });
+  });
 
-    it("should throw with invalid .ts register", () => {
-      require.extensions[".ts"] = () => {
-        throw new Error("Not support .ts.");
-      };
-      try {
-        expect(() => {
-          loadPartialConfigSync({
-            configFile: path.join(
-              __dirname,
-              "fixtures/config-ts/invalid-cts-register/babel.config.cts",
-            ),
-          });
-        }).toThrow(/Unexpected identifier.*/);
-      } finally {
-        delete require.extensions[".ts"];
-      }
-    });
-
-    it("should work with ts-node", async () => {
-      const service = require("ts-node").register({
-        experimentalResolver: true,
-        compilerOptions: {
-          module: "CommonJS",
-        },
-      });
-      service.enabled(true);
-
-      try {
-        require(path.join(
-          __dirname,
-          "fixtures/config-ts/simple-cts-with-ts-node/babel.config.cts",
-        ));
-
-        const config = loadPartialConfigSync({
+  it("should throw with invalid .ts register", () => {
+    require.extensions[".ts"] = () => {
+      throw new Error("Not support .ts.");
+    };
+    try {
+      expect(() => {
+        loadPartialConfigSync({
           configFile: path.join(
             __dirname,
-            "fixtures/config-ts/simple-cts-with-ts-node/babel.config.cts",
+            "fixtures/config-ts/invalid-cts-register/babel.config.cts",
           ),
         });
+      }).toThrow(/Unexpected identifier.*/);
+    } finally {
+      delete require.extensions[".ts"];
+    }
+  });
 
-        expect(config.options.targets).toMatchInlineSnapshot(`
+  it("should work with ts-node", async () => {
+    const service = require("ts-node").register({
+      experimentalResolver: true,
+      compilerOptions: {
+        module: "CommonJS",
+      },
+    });
+    service.enabled(true);
+
+    try {
+      require(path.join(
+        __dirname,
+        "fixtures/config-ts/simple-cts-with-ts-node/babel.config.cts",
+      ));
+
+      const config = loadPartialConfigSync({
+        configFile: path.join(
+          __dirname,
+          "fixtures/config-ts/simple-cts-with-ts-node/babel.config.cts",
+        ),
+      });
+
+      expect(config.options.targets).toMatchInlineSnapshot(`
             Object {
               "node": "12.0.0",
             }
           `);
-      } finally {
-        service.enabled(false);
-      }
-    });
-  },
-);
+    } finally {
+      service.enabled(false);
+    }
+  });
+});
