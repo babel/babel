d6486d8d04fa8d1a7eddff81235d28ec75cf532a
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-03-30 13:00:37 +0200

    Fix failing regenerator runtime tests with Babel 8

--- packages/babel-plugin-transform-runtime/test/fixtures/runtime/full/output.mjs
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime/full/output.mjs
@@ -1,9 +1,9 @@
-import _regeneratorRuntime from "@babel/runtime/regenerator";
-var _marked = /*#__PURE__*/_regeneratorRuntime.mark(giveWord);
+import _regeneratorRuntime from "@babel/runtime/helpers/regeneratorRuntime";
+var _marked = /*#__PURE__*/_regeneratorRuntime().mark(giveWord);
 import foo, * as bar from "someModule";
 export const myWord = Symbol("abc");
 export function giveWord() {
-  return _regeneratorRuntime.wrap(function giveWord$(_context) {
+  return _regeneratorRuntime().wrap(function giveWord$(_context) {
     while (1) switch (_context.prev = _context.next) {
       case 0:
         _context.next = 2;
