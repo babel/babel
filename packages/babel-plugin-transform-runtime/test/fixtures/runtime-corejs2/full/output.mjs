6e27b5fff036847a8a98641402ad28fbc7aa5d74
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-03-30 16:03:28 +0200

    Fix failing regenerator runtime tests with Babel 8 (#15529)

--- packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs2/full/output.mjs
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs2/full/output.mjs
@@ -1,10 +1,10 @@
-import _regeneratorRuntime from "@babel/runtime-corejs2/regenerator";
-var _marked = /*#__PURE__*/_regeneratorRuntime.mark(giveWord);
+import _regeneratorRuntime from "@babel/runtime-corejs2/helpers/regeneratorRuntime";
+var _marked = /*#__PURE__*/_regeneratorRuntime().mark(giveWord);
 import _Symbol from "@babel/runtime-corejs2/core-js/symbol";
 import foo, * as bar from "someModule";
 export const myWord = _Symbol("abc");
 export function giveWord() {
-  return _regeneratorRuntime.wrap(function giveWord$(_context) {
+  return _regeneratorRuntime().wrap(function giveWord$(_context) {
     while (1) switch (_context.prev = _context.next) {
       case 0:
         _context.next = 2;
--- packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs3/full/output.mjs
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime-corejs3/full/output.mjs
@@ -1,10 +1,10 @@
-import _regeneratorRuntime from "@babel/runtime-corejs3/regenerator";
-var _marked = /*#__PURE__*/_regeneratorRuntime.mark(giveWord);
+import _regeneratorRuntime from "@babel/runtime-corejs3/helpers/regeneratorRuntime";
+var _marked = /*#__PURE__*/_regeneratorRuntime().mark(giveWord);
 import _Symbol from "@babel/runtime-corejs3/core-js-stable/symbol";
 import foo, * as bar from "someModule";
 export const myWord = _Symbol("abc");
 export function giveWord() {
-  return _regeneratorRuntime.wrap(function giveWord$(_context) {
+  return _regeneratorRuntime().wrap(function giveWord$(_context) {
     while (1) switch (_context.prev = _context.next) {
       case 0:
         _context.next = 2;
