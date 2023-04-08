f41adcc22b2a596ea3c32d3ebe1e4badf6a2823e
Yvan Yang <ybyang@best-inc.com>
2023-04-08 14:59:55 +0800

    mark the nodeType parameter as string | null | undefined

--- packages/babel-types/src/validators/isType.ts
+++ packages/babel-types/src/validators/isType.ts
@@ -14,7 +14,10 @@ export default function isType(
 /**
  * Test if a `nodeType` is a `targetType` or if `targetType` is an alias of `nodeType`.
  */
-export default function isType(nodeType: string, targetType: string): boolean {
+export default function isType(
+  nodeType: string | null | undefined,
+  targetType: string,
+): boolean {
   if (nodeType === targetType) return true;
 
   // if nodeType is null or undefined, it can't be an alias of targetType
