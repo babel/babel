cdfe7669cb8a2cfce3cca1be7367013e41177e6e
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2023-05-26 18:27:17 +0800

    improve

--- packages/babel-traverse/scripts/generators/asserts.js
+++ packages/babel-traverse/scripts/generators/asserts.js
@@ -8,13 +8,20 @@ export default function generateAsserts() {
 import type * as t from "@babel/types";
 import type NodePath from "../index";
 
+type Opts<Object> = Partial<{
+  [Prop in keyof Object]: Object[Prop] extends t.Node
+    ? t.Node | Object[Prop]
+    : Object[Prop] extends t.Node[]
+    ? t.Node[] | Object[Prop]
+    : Object[Prop];
+}>;
 
 export interface NodePathAssertions {`;
 
   for (const type of [...t.TYPES].sort()) {
     output += `
   assert${type}(
-    opts?: object,
+    opts?: Opts<t.${type}>,
   ): asserts this is NodePath<t.${type}>;`;
   }
 
--- packages/babel-traverse/scripts/generators/validators.js
+++ packages/babel-traverse/scripts/generators/validators.js
@@ -9,11 +9,19 @@ import type * as t from "@babel/types";
 import type NodePath from "../index";
 import type { VirtualTypeNodePathValidators } from "../lib/virtual-types-validator";
 
+type Opts<Object> = Partial<{
+  [Prop in keyof Object]: Object[Prop] extends t.Node
+    ? t.Node | Object[Prop]
+    : Object[Prop] extends t.Node[]
+    ? t.Node[] | Object[Prop]
+    : Object[Prop];
+}>;
+
 interface BaseNodePathValidators {
 `;
 
   for (const type of [...t.TYPES].sort()) {
-    output += `is${type}<T extends t.Node>(this: NodePath<T>, opts?: object): this is NodePath<T & t.${type}>;`;
+    output += `is${type}<T extends t.Node>(this: NodePath<T>, opts?: Opts<t.${type}>): this is NodePath<T & t.${type}>;`;
   }
 
   output += `
