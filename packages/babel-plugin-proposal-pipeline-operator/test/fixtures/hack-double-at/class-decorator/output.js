41e33a3a945e3d0cf5148adae7aa6dbe5264b674
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-06-01 18:24:31 +0200

    Other fixtures

--- packages/babel-plugin-proposal-pipeline-operator/test/fixtures/hack-double-at/class-decorator/output.js
+++ packages/babel-plugin-proposal-pipeline-operator/test/fixtures/hack-double-at/class-decorator/output.js
@@ -7,6 +7,6 @@ const result = ((_class = class {
   constructor() {
     this.value = expectedValue;
   }
-}, [_decorated_class, _initClass] = babelHelpers.applyDecs(_class, [], [decorator]), _initClass()), _decorated_class);
+}, [_decorated_class, _initClass] = babelHelpers.applyDecs2305(_class, [], [decorator]).c, _initClass()), _decorated_class);
 expect(result.decoratorValue).toBe(expectedValue);
 expect(new result().value).toBe(expectedValue);
