4501489257cfa6d69ab49c0c29f670cc1fa42282
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-06-07 12:27:45 +0200

    fix test

--- packages/babel-preset-env/test/regressions.js
+++ packages/babel-preset-env/test/regressions.js
@@ -6,71 +6,60 @@ import { fileURLToPath } from "url";
 import { USE_ESM } from "$repo-utils";
 
 const itBabel7 = process.env.BABEL_8_BREAKING ? it.skip : it;
-const describeBabel7Node14plusCjs =
+const itBabel7Node14plusCjs =
   process.env.BABEL_8_BREAKING || parseInt(process.version) < 14 || USE_ESM
-    ? describe.skip
-    : describe;
+    ? it.skip
+    : it;
 
 describe("regressions", () => {
   it("empty", () => {
     // TODO(Babel 8): Delete this file
   });
 
-  describe("#12880", () => {
-    afterEach(() => {
-      jest.spyOn(process, "cwd").mockRestore();
-    });
+  itBabel7(
+    "#12880 - read the .browserslistrc file when using @babel/core < 7.13.0",
+    () => {
+      const root = path.join(
+        path.dirname(fileURLToPath(import.meta.url)),
+        "regressions",
+      );
 
-    itBabel7(
-      "read the .browserslistrc file when using @babel/core < 7.13.0",
-      () => {
-        const root = path.join(
-          path.dirname(fileURLToPath(import.meta.url)),
-          "regressions",
-        );
+      // Unfortunatly, when loading the browserslist config through
+      // @babel/preset-env, it's resolved from process.cwd() and not
+      // from root. Mock it to isolate this test.
+      const spy = jest.spyOn(process, "cwd").mockReturnValue(root);
 
-        // Unfortunatly, when loading the browserslist config through
-        // @babel/preset-env, it's resolved from process.cwd() and not
-        // from root. Mock it to isolate this test.
-        const spy = jest.spyOn(process, "cwd").mockReturnValue(root);
+      try {
+        // The browserslistrc file contains "firefox 50".
+        // a ** b is supported starting from firefox 52;
+        // a => b is supported starting from firefox 45.
+        const out = babel7_12.transformSync("a ** b; a => b;", {
+          configFile: false,
+          presets: [[env, { modules: false }]],
+          filename: path.join(root, "input.js"),
+          root,
+        });
 
-        try {
-          // The browserslistrc file contains "firefox 50".
-          // a ** b is supported starting from firefox 52;
-          // a => b is supported starting from firefox 45.
-          const out = babel7_12.transformSync("a ** b; a => b;", {
-            configFile: false,
-            presets: [[env, { modules: false }]],
-            filename: path.join(root, "input.js"),
-            root,
-          });
-
-          expect(out.code).toMatchInlineSnapshot(`
+        expect(out.code).toMatchInlineSnapshot(`
                     "Math.pow(a, b);
                     a => b;"
                 `);
-        } finally {
-          spy.mockRestore();
-        }
-      },
-    );
-  });
+      } finally {
+        spy.mockRestore();
+      }
+    },
+  );
 
-  describeBabel7Node14plusCjs(
-    "create-reat-app missing dependency fallback",
-    () => {
-      let consoleWarn;
-      beforeEach(() => {
-        jest.useFakeTimers();
-        consoleWarn = jest.spyOn(console, "warn").mockImplementation(() => {});
-      });
-      afterEach(() => {
-        jest.useRealTimers();
-        consoleWarn.mockRestore();
-      });
-
-      // jest fake timers only work in the Jest version we are using for Node.js 14+
-      it("proposal-private-property-in-object should warn and fallback to transform-...", async () => {
+  // create-reat-app missing dependency fallback
+  // jest fake timers only work in the Jest version we are using for Node.js 14+
+  itBabel7Node14plusCjs(
+    "proposal-private-property-in-object should warn and fallback to transform-...",
+    async () => {
+      jest.useFakeTimers();
+      const consoleWarn = jest
+        .spyOn(console, "warn")
+        .mockImplementation(() => {});
+      try {
         const out = babel.transformSync("class A { #a; x = #a in this }", {
           configFile: false,
           presets: [
@@ -90,7 +79,10 @@ describe("regressions", () => {
           x = _aBrandCheck.has(_checkInRHS(this));
         }"
       `);
-      });
+      } finally {
+        jest.useRealTimers();
+        consoleWarn.mockRestore();
+      }
     },
   );
 });
