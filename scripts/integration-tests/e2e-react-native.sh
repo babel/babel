6f3e2033aedaeb890fb6b17459634e6849d4ba36
Huáng Jùnliàng <jlhwung@gmail.com>
2023-06-23 17:42:42 -0400

    Apply flow-enums transform on non-TS files only
    flow-enums inherits flow syntax plugin, which will collide with TS syntax plugin.

--- scripts/integration-tests/e2e-react-native.sh
+++ scripts/integration-tests/e2e-react-native.sh
@@ -37,8 +37,9 @@ if [ "$BABEL_8_BREAKING" = true ] ; then
   # metro-react-native-babel-preset unconditionally enables the Flow plugin, even on TS files.
   # https://github.com/facebook/metro/blob/2c16fa67/packages/metro-react-native-babel-preset/src/configs/main.js#L25
   npx replace '(?=\[require\("@babel/plugin-syntax-flow")' '//' node_modules/metro-react-native-babel-preset/src/configs/main.js
+  npx replace '(?=\[require\("babel-plugin-transform-flow-enums")' '//' node_modules/metro-react-native-babel-preset/src/configs/main.js
   # https://github.com/facebook/metro/blob/2c16fa67/packages/metro-react-native-babel-preset/src/configs/main.js#L169
-  npx replace '(?=plugins:.*?flow-strip-types)' 'exclude: [isTypeScriptSource, isTSXSource],' node_modules/metro-react-native-babel-preset/src/configs/main.js
+  npx replace 'plugins: \[require\("@babel/plugin-transform-flow-strip-types"\)]' 'exclude: [isTypeScriptSource, isTSXSource],plugins: [require("@babel/plugin-transform-flow-strip-types"),require("babel-plugin-transform-flow-enums")]' node_modules/metro-react-native-babel-preset/src/configs/main.js
   # Use `usePath` API instead of reassigning named exports
   # https://github.com/facebook/metro/blob/29bb5f2ad3319ba8f4764c3993aa85c15f59af23/packages/metro-source-map/src/generateFunctionMap.js#L182
   npx replace '_traverse.default.cache.path = previousCache;' '_traverse.default.cache.usePath(previousCache);' node_modules/metro-source-map/src/generateFunctionMap.js
