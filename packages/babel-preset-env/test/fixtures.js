7c16b063ac05861da3cc5a98e5cb32d379eacb4d
Huáng Jùnliàng <jlhwung@gmail.com>
2023-06-27 15:39:53 -0400

    Revert "set temporary resolution in preset-env tests"
    This reverts commit 19ddaf6a0380282c53485b610d533d1ba92f30e4.

--- packages/babel-preset-env/test/fixtures.js
+++ packages/babel-preset-env/test/fixtures.js
@@ -1,18 +1,3 @@
 import runner from "@babel/helper-plugin-test-runner";
-import { execSync } from "child_process";
 
-describe("preset-env", () => {
-  // todo: remove temporary resolution when @babel/helper-define-polyfill-provider bumps
-  // @babel/helper-compilation-targets to the latest version
-  beforeAll(() => {
-    execSync(
-      "yarn set resolution '@babel/helper-compilation-targets@npm:^7.17.7' 'workspace:*'",
-    );
-  });
-  afterAll(() => {
-    execSync(
-      "yarn set resolution '@babel/helper-compilation-targets@npm:^7.17.7' '7.22.5'",
-    );
-  });
-  runner(import.meta.url);
-});
+runner(import.meta.url);
