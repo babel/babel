7376e6290774762dc0e186a3d82e9bb77ce90427
Huáng Jùnliàng <jlhwung@gmail.com>
2023-06-27 14:45:33 -0400

    set temporary resolution in preset-env tests

--- packages/babel-preset-env/test/fixtures.js
+++ packages/babel-preset-env/test/fixtures.js
@@ -1,3 +1,18 @@
 import runner from "@babel/helper-plugin-test-runner";
+import { execSync } from "child_process";
 
-runner(import.meta.url);
+describe("preset-env", () => {
+  // todo: remove temporary resolution when @babel/helper-define-polyfill-provider bumps
+  // @babel/helper-compilation-targets to the latest version
+  beforeAll(() => {
+    execSync(
+      "yarn set resolution '@babel/helper-compilation-targets@npm:^7.17.7' 'workspace:*'",
+    );
+  });
+  afterAll(() => {
+    execSync(
+      "yarn set resolution '@babel/helper-compilation-targets@npm:^7.17.7' '7.22.5'",
+    );
+  });
+  runner(import.meta.url);
+});
