b04dfcdb5604f6b1e31f4429698145b79f527299
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-06-29 12:31:02 +0200

    Skip tests that require newer helpers

--- scripts/integration-tests/e2e-babel-old-version.sh
+++ scripts/integration-tests/e2e-babel-old-version.sh
@@ -24,16 +24,16 @@ startLocalRegistry "$PWD"/scripts/integration-tests/verdaccio-config.yml
 
 node "$PWD"/scripts/integration-tests/utils/bump-babel-dependencies.js
 
-node -e "\
-  var pkg = require('./package.json');\
-  pkg.devDependencies['@babel/core'] = '7.0.0';\
-  Object.assign(pkg.resolutions, {\
-    '@babel/core@npm:*': '7.0.0',\
-    '@babel/core@npm:7.0.0/@babel/traverse': '7.0.0',\
-    '@babel/core@npm:7.0.0/@babel/helpers': '7.1.0',\
-    '@babel/helpers@npm:7.1.0/@babel/traverse': '7.0.0',\
-  });\
-  fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2));\
+node -e "
+  var pkg = require('./package.json');
+  pkg.devDependencies['@babel/core'] = '7.0.0';
+  Object.assign(pkg.resolutions, {
+    '@babel/core@npm:*': '7.0.0',
+    '@babel/core@npm:7.0.0/@babel/traverse': '7.0.0',
+    '@babel/core@npm:7.0.0/@babel/helpers': '7.1.0',
+    '@babel/helpers@npm:7.1.0/@babel/traverse': '7.0.0',
+  });
+  fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2));
 "
 
 # Older @babel/core versions don't support "targets" and "assumptions"
@@ -55,6 +55,13 @@ node -e "
 # https://github.com/babel/babel/pull/14124 - This test is fixed in Babel 7.17;
 # Todo(Babel 8): enable this test
 rm packages/babel-standalone/test/built-into-es5.js
+
+# Our 'WorkerClient' implementation uses static private getters, that are only supported
+# when using '@babel/helpers@^7.6.0'
+# NOTE: When running this command on MacOS, use gsed from 'brew install gnu-sed'
+sed -i 's/describe, "worker"/describe.skip, "worker"/g' packages/babel-register/test/index.js
+sed -i 's/babel7node12(/babel7node12.skip(/g' eslint/babel-eslint-tests/test/integration/parser-override.js
+sed -i 's/itNode12upNoESM(/itNode12upNoESM.skip(/g' eslint/babel-eslint-tests/test/integration/config-files.js
 
 # Update deps, build and test
 rm yarn.lock
