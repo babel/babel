aaf9d5be3ece8ddb6562cd7b58c67d967642b0db
Huáng Jùnliàng <jlhwung@gmail.com>
2023-07-06 16:51:56 -0400

    properly bind jest.it

--- packages/babel-standalone/test/transform-script-tags.test.js
+++ packages/babel-standalone/test/transform-script-tags.test.js
@@ -3,8 +3,6 @@ import { createRequire } from "module";
 import { itGte } from "$repo-utils";
 const require = createRequire(import.meta.url);
 
-const nodeGte16 = itGte("16.0.0");
-
 describe("transformScriptTags", () => {
   let standaloneSource;
   let JSDOM;
@@ -15,28 +13,31 @@ describe("transformScriptTags", () => {
     );
     JSDOM = require("jsdom").JSDOM;
   });
-  nodeGte16("should transform script element with type 'text/babel'", () => {
-    const dom = new JSDOM(
-      `<!DOCTYPE html><head><script>${standaloneSource}</script><script type="text/babel">globalThis ?? window</script></head>`,
-      { runScripts: "dangerously" },
-    );
-    return new Promise((resolve, reject) => {
-      dom.window.addEventListener("DOMContentLoaded", () => {
-        try {
-          const transformedScriptElement =
-            dom.window.document.head.children.item(2);
-          expect(transformedScriptElement.getAttribute("type")).toBeNull();
-          expect(transformedScriptElement.innerHTML).toContain(
-            "globalThis !== null && globalThis !== void 0 ? globalThis : window",
-          );
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
+  itGte("16.0.0")(
+    "should transform script element with type 'text/babel'",
+    () => {
+      const dom = new JSDOM(
+        `<!DOCTYPE html><head><script>${standaloneSource}</script><script type="text/babel">globalThis ?? window</script></head>`,
+        { runScripts: "dangerously" },
+      );
+      return new Promise((resolve, reject) => {
+        dom.window.addEventListener("DOMContentLoaded", () => {
+          try {
+            const transformedScriptElement =
+              dom.window.document.head.children.item(2);
+            expect(transformedScriptElement.getAttribute("type")).toBeNull();
+            expect(transformedScriptElement.innerHTML).toContain(
+              "globalThis !== null && globalThis !== void 0 ? globalThis : window",
+            );
+            resolve();
+          } catch (err) {
+            reject(err);
+          }
+        });
       });
-    });
-  });
-  nodeGte16(
+    },
+  );
+  itGte("16.0.0")(
     "should pass through the nonce attribute to the transformed script element",
     () => {
       const nonceAttribute = "nonce_example";
