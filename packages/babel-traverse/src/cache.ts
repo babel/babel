c1fe357e69280636727cd1672ecb9abb15ac9778
Nicolò Ribaudo <hello@nicr.dev>
2023-07-06 14:15:42 +0200

    Revert "Use `NodePath#hub` as part of the paths cache key" (#15754)

--- packages/babel-traverse/src/cache.ts
+++ packages/babel-traverse/src/cache.ts
@@ -1,13 +1,8 @@
 import type { Node } from "@babel/types";
 import type NodePath from "./path";
 import type Scope from "./scope";
-import type { HubInterface } from "./hub";
 
-let pathsCache: WeakMap<
-  HubInterface | typeof nullHub,
-  WeakMap<Node, Map<Node, NodePath>>
-> = new WeakMap();
-export { pathsCache as path };
+export let path: WeakMap<Node, Map<Node, NodePath>> = new WeakMap();
 export let scope: WeakMap<Node, Scope> = new WeakMap();
 
 export function clear() {
@@ -16,29 +11,9 @@ export function clear() {
 }
 
 export function clearPath() {
-  pathsCache = new WeakMap();
+  path = new WeakMap();
 }
 
 export function clearScope() {
   scope = new WeakMap();
-}
-
-// NodePath#hub can be null, but it's not a valid weakmap key because it
-// cannot be collected by GC. Use an object, knowing tht it will not be
-// collected anyway. It's not a memory leak because pathsCache.get(nullHub)
-// is itself a weakmap, so its entries can still be collected.
-const nullHub = Object.freeze({} as const);
-
-export function getCachedPaths(hub: HubInterface | null, parent: Node) {
-  return pathsCache.get(hub ?? nullHub)?.get(parent);
-}
-
-export function getOrCreateCachedPaths(hub: HubInterface | null, parent: Node) {
-  let parents = pathsCache.get(hub ?? nullHub);
-  if (!parents) pathsCache.set(hub ?? nullHub, (parents = new WeakMap()));
-
-  let paths = parents.get(parent);
-  if (!paths) parents.set(parent, (paths = new Map()));
-
-  return paths;
 }
