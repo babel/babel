b82b1296d189431b5ab7a8771a975c0d1a0fd72e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-07-20 14:27:48 +0200

    Fix usage in ts enum transform

--- packages/babel-plugin-transform-typescript/test/fixtures/regression/15768/input.ts
+++ packages/babel-plugin-transform-typescript/test/fixtures/regression/15768/input.ts
@@ -1,11 +1,21 @@
 const v = 42;
 const v2 = Infinity;
-var Infinity = 1; // Known inconsistencies
-enum StateEnum {
-	okay = 0,
-	neg = -Infinity,
-	pos = Infinity,
-	nan = NaN,
-	ext = v,
-  ext2 = v2,
+const v3 = NaN;
+
+{
+	let Infinity = 1;
+	let NaN = 2;
+
+	enum StateEnum {
+		okay = 0,
+		neg = -Infinity,
+		pos = Infinity,
+		nan = NaN,
+		negReal = -1 / 0,
+		posReal = 1 / 0,
+		nanReal = 0 / 0,
+		ext = v,
+		ext2 = v2,
+		ext3 = v3,
+	}
 }
