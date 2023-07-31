b0709752503395a453230b6c5654ae46a6193abf
Huáng Jùnliàng <jlhwung@gmail.com>
2023-07-31 10:24:45 -0400

    fix: add meta info to babel-eslint-plugin

--- eslint/babel-eslint-plugin-development-internal/src/index.cjs
+++ eslint/babel-eslint-plugin-development-internal/src/index.cjs
@@ -1,9 +1,15 @@
+const meta = {
+  name: PACKAGE_JSON.name,
+  version: PACKAGE_JSON.version,
+};
+
 const rules = {
   "report-error-message-format": require("./rules/report-error-message-format.cjs"),
   "require-default-import-fallback": require("./rules/require-default-import-fallback.cjs"),
 };
 
+exports.meta = meta;
 exports.rules = rules;
 if (!process.env.BABEL_8_BREAKING) {
-  exports.default = { rules };
+  exports.default = { meta, rules };
 }
--- eslint/babel-eslint-plugin-development/src/index.cjs
+++ eslint/babel-eslint-plugin-development/src/index.cjs
@@ -2,13 +2,19 @@ const noDeprecatedClone = require("./rules/no-deprecated-clone.cjs");
 const noUndefinedIdentifier = require("./rules/no-undefined-identifier.cjs");
 const pluginName = require("./rules/plugin-name.cjs");
 
+const meta = {
+  name: PACKAGE_JSON.name,
+  version: PACKAGE_JSON.version,
+};
+
 const rules = {
   "no-deprecated-clone": noDeprecatedClone,
   "no-undefined-identifier": noUndefinedIdentifier,
   "plugin-name": pluginName,
 };
 
+exports.meta = meta;
 exports.rules = rules;
 if (!process.env.BABEL_8_BREAKING) {
-  exports.default = { rules };
+  exports.default = { meta, rules };
 }
