565d7be5d6a0307d8012103bbb0aac73e272ea26
Nicolò Ribaudo <hello@nicr.dev>
2023-08-03 17:05:36 +0200

    Enable Babel e2e test for Babel 8 with ESM (#15836)

--- scripts/integration-tests/e2e-babel.sh
+++ scripts/integration-tests/e2e-babel.sh
@@ -20,19 +20,23 @@ cd ../..
 #                                   TEST                                       #
 #==============================================================================#
 
-if [ "$BABEL_8_BREAKING" = true ] ; then
-  # This option is removed in Babel 8
-  sed -i 's/allowDeclareFields: true,\?/\/* allowDeclareFields: true *\//g' babel.config.js
-fi
-
 startLocalRegistry "$PWD"/scripts/integration-tests/verdaccio-config.yml
 
 # We only bump dependencies in the top-level package.json, because workspaces
 # already use the workspace: protocol so will get the version in the monorepo
 # and not from npm.
 node "$PWD"/scripts/integration-tests/utils/bump-babel-dependencies.js
+export YARN_ENABLE_IMMUTABLE_INSTALLS=false
+
+if [ "$BABEL_8_BREAKING" = true ] ; then
+  # This option is removed in Babel 8
+  sed -i 's/allowDeclareFields: true,\?/\/* allowDeclareFields: true *\//g' babel.config.js
+
+  # Babel 8 only supports ESM
+  make use-esm
+fi
 
 # Build and test
-YARN_ENABLE_IMMUTABLE_INSTALLS=false make -j test-ci
+make -j test-ci
 
 cleanup
