7f66d745f47caefcf9cec34a6cf69b5dc949fdb8
Huáng Jùnliàng <jlhwung@gmail.com>
2023-08-08 09:11:35 -0400

    Update packages/babel-preset-typescript/src/normalize-options.ts
    Co-authored-by: Nicolò Ribaudo <hello@nicr.dev>

--- packages/babel-preset-typescript/src/normalize-options.ts
+++ packages/babel-preset-typescript/src/normalize-options.ts
@@ -36,7 +36,7 @@ export default function normalizeOptions(options: Options = {}) {
   if (process.env.BABEL_8_BREAKING) {
     v.invariant(
       !("allowDeclareFields" in options),
-      "The .allowDeclareFields options have been removed. Please remove it from your config.",
+      "The .allowDeclareFields option has been removed and it's now always enabled. Please remove it from your config.",
     );
     v.invariant(
       !("allExtensions" in options) && !("isTSX" in options),
