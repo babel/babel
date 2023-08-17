828fac4a7d7c8e420b0b626a88e1dbdf0fb2a7b2
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-08-17 14:38:06 +0200

    Fix support for private fields

---
+++ packages/babel-plugin-proposal-optional-chaining-assign/test/fixtures/assumption-noDocumentAll/private-field-transformed/output.js
@@ -0,0 +1,18 @@
+var _x = /*#__PURE__*/new WeakMap();
+class A {
+  constructor() {
+    babelHelpers.classPrivateFieldInitSpec(this, _x, {
+      writable: true,
+      value: void 0
+    });
+  }
+  method() {
+    var _obj, _obj2, _obj3, _obj4, _obj5, _obj6;
+    (_obj = obj) == null ? void 0 : babelHelpers.classPrivateFieldSet(_obj, _x, 1);
+    (_obj2 = obj) == null ? void 0 : babelHelpers.classPrivateFieldSet(_obj2, _x, babelHelpers.classPrivateFieldGet(_obj2, _x) + 2);
+    (_obj3 = obj) == null ? void 0 : babelHelpers.classPrivateFieldGet(_obj3, _x) ?? babelHelpers.classPrivateFieldSet(_obj3, _x, 3);
+    (_obj4 = obj) == null ? void 0 : babelHelpers.classPrivateFieldGet(_obj4, _x).y = 4;
+    (_obj5 = obj) == null ? void 0 : babelHelpers.classPrivateFieldGet(_obj5, _x).y += 5;
+    (_obj6 = obj) == null ? void 0 : babelHelpers.classPrivateFieldGet(_obj6, _x).y ??= 6;
+  }
+}
