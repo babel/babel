170c83478fbb3115aca98bef532bd8e8cc3bf7ae
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-08-17 15:08:19 +0200

    Simplify `?.` output when chain result is ignored

--- packages/babel-plugin-transform-classes/test/fixtures/assumption-constantSuper/accessing-super-property-optional-chain/output.js
+++ packages/babel-plugin-transform-classes/test/fixtures/assumption-constantSuper/accessing-super-property-optional-chain/output.js
@@ -5,8 +5,8 @@ var Test = /*#__PURE__*/function (_Foo) {
   function Test() {
     var _Foo$prototype$foo, _Foo$prototype$foo2;
     var _this;
-    (_Foo$prototype$foo = _Foo.prototype.foo) == null ? void 0 : _Foo$prototype$foo.bar;
-    (_Foo$prototype$foo2 = _Foo.prototype.foo) == null ? void 0 : _Foo$prototype$foo2.call(babelHelpers.assertThisInitialized(_this));
+    (_Foo$prototype$foo = _Foo.prototype.foo) == null || _Foo$prototype$foo.bar;
+    (_Foo$prototype$foo2 = _Foo.prototype.foo) == null || _Foo$prototype$foo2.call(babelHelpers.assertThisInitialized(_this));
     return babelHelpers.assertThisInitialized(_this);
   }
   return Test;
--- packages/babel-plugin-transform-optional-chaining/test/fixtures/general/function-call-loose/output.js
+++ packages/babel-plugin-transform-optional-chaining/test/fixtures/general/function-call-loose/output.js
@@ -1,5 +1,5 @@
 var _foo, _foo2;
-foo == null ? void 0 : foo(foo);
-(_foo = foo) == null ? void 0 : _foo.bar();
-foo.bar == null ? void 0 : foo.bar(foo.bar, false);
+foo == null || foo(foo);
+(_foo = foo) == null || _foo.bar();
+foo.bar == null || foo.bar(foo.bar, false);
 (_foo2 = foo) == null || _foo2.bar == null ? void 0 : _foo2.bar(foo.bar, true);
