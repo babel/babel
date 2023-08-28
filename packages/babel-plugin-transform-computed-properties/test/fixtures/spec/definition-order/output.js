2f31c556ce24e489bba577e8df75b10c8b415d1e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-08-28 20:04:53 +0200

    Remove extra identifier in computed properties transform

--- packages/babel-plugin-transform-computed-properties/test/fixtures/spec/definition-order/output.js
+++ packages/babel-plugin-transform-computed-properties/test/fixtures/spec/definition-order/output.js
@@ -1,14 +1,14 @@
 var _a, _b, _y;
 var a = (_a = {}, babelHelpers.defineAccessor("get", _a, "x", function () {
   return 0;
-}), babelHelpers.defineProperty(_a, "y", 1), _a);
+}), babelHelpers.defineProperty(_a, "y", 1));
 var b = (_b = {}, babelHelpers.defineAccessor("get", _b, "x", function () {
   return 0;
-}), babelHelpers.defineProperty(_b, "x", 1), _b);
+}), babelHelpers.defineProperty(_b, "x", 1));
 var x = 1;
 var y = (_y = {
   x
 }, babelHelpers.defineAccessor("get", _y, "x", function () {
   return 0;
-}), babelHelpers.defineProperty(_y, "x", x), _y);
+}), babelHelpers.defineProperty(_y, "x", x));
 y.x = 2;
