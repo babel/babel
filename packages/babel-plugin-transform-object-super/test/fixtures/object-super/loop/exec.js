f443a1baa85fc1c9408dc2ec7d38aa48174491cb
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2023-09-08 21:07:17 +0800

    add test

---
+++ packages/babel-plugin-transform-object-super/test/fixtures/object-super/loop/exec.js
@@ -0,0 +1,11 @@
+const objects = [];
+for (const proto of [{x: 0}, {x: 1}]) {
+  objects.push({
+    __proto__: proto,
+    foo() {
+      return super.x
+    }
+  });
+}
+expect(objects[0].foo()).toBe(0);
+expect(objects[1].foo()).toBe(1);
