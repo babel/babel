48d5a27cad6f5fdeb8e876ccaa89eaae0b88af99
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-09-08 17:20:42 +0200

    Review

--- packages/babel-preset-typescript/src/plugin-rewrite-ts-imports.ts
+++ packages/babel-preset-typescript/src/plugin-rewrite-ts-imports.ts
@@ -2,18 +2,20 @@ import { declare } from "@babel/helper-plugin-utils";
 import type { types as t } from "@babel/core";
 import type { NodePath } from "@babel/traverse";
 
-export default declare(function () {
+export default declare(function ({ types: t }) {
   return {
+    name: "preset-typescript/plugin-rewrite-ts-imports",
     visitor: {
-      "ImportDeclaration|ExportAllDeclaration|ExportNamedDeclaration"(
-        path: NodePath<
-          | t.ImportDeclaration
-          | t.ExportAllDeclaration
-          | t.ExportNamedDeclaration
-        >,
-      ) {
-        const { source } = path.node;
-        if (source) {
+      "ImportDeclaration|ExportAllDeclaration|ExportNamedDeclaration"({
+        node,
+      }: NodePath<
+        t.ImportDeclaration | t.ExportAllDeclaration | t.ExportNamedDeclaration
+      >) {
+        const { source } = node;
+        const kind = t.isImportDeclaration(node)
+          ? node.importKind
+          : node.exportKind;
+        if (kind === "value" && source && /[\\/]/.test(source.value)) {
           source.value = source.value.replace(/(\.[mc]?)ts$/, "$1js");
         }
       },
