a0ee20dd766ef9b32e3cfbdb1881e0c24ac5865d
Jan Joneš <jjones@outlook.cz>
2023-09-16 14:35:27 +0200

    Update snapshot

--- packages/babel-plugin-transform-async-to-generator/test/fixtures/regression/15978/output.js
+++ packages/babel-plugin-transform-async-to-generator/test/fixtures/regression/15978/output.js
@@ -1,17 +1,22 @@
-var _f;
 babelHelpers.asyncToGenerator(function* () {
   let items = [1, 2, 3, 4];
   for (const item of items) {
-    var _ref2;
-    yield f(function (_x) {
-      return (_ref2 = _ref2 || babelHelpers.asyncToGenerator(function* (x) {
+    yield f( /*#__PURE__*/function () {
+      var _ref2 = babelHelpers.asyncToGenerator(function* (x) {
         console.log(item);
-      })).apply(this, arguments);
-    });
+      });
+      return function (_x) {
+        return _ref2.apply(this, arguments);
+      };
+    }());
   }
 })();
 function f(_x2) {
-  return (_f = _f || babelHelpers.asyncToGenerator(function* (lambda) {
+  return _f.apply(this, arguments);
+}
+function _f() {
+  _f = babelHelpers.asyncToGenerator(function* (lambda) {
     yield lambda();
-  })).apply(this, arguments);
+  });
+  return _f.apply(this, arguments);
 }
