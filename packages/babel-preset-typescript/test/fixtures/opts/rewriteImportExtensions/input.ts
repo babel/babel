c9be2af7cf5f89f056d4d25d7aba21bf451b94fa
james.diefenderfer <james.diefenderfer@salesforce.com>
2023-10-06 16:47:10 -0700

    rewrite TSX extensions

--- packages/babel-preset-typescript/test/fixtures/opts/rewriteImportExtensions/input.ts
+++ packages/babel-preset-typescript/test/fixtures/opts/rewriteImportExtensions/input.ts
@@ -1,6 +1,7 @@
 import "./a.ts";
 import "./a.mts";
 import "./a.cts";
+import "./react.tsx";
 import "a-package/file.ts";
 // Bare import, it's either a node package or remapped by an import map
 import "soundcloud.ts";
--- packages/babel-preset-typescript/test/fixtures/opts/rewriteImportExtensions/output.mjs
+++ packages/babel-preset-typescript/test/fixtures/opts/rewriteImportExtensions/output.mjs
@@ -1,6 +1,7 @@
 import "./a.js";
 import "./a.mjs";
 import "./a.cjs";
+import "./react.js";
 import "a-package/file.js";
 // Bare import, it's either a node package or remapped by an import map
 import "soundcloud.ts";
