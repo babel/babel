ff83a0a8c4d6a91d9ade9a32fd108f1f8824928f
Qingyu Wang <qingyu.wang@aliyun.com>
2023-10-28 01:19:02 +0800

    Strip type-only TS namespace
    We move `GLOBAL_TYPES`-related codes into `global-types.ts` since we
    would like to share codes between `index.ts` and `namespace.ts` and we
    do not want to export these functions outside this package.
    
    Change-Id: I833a7e1a63ee56e78db44b3a70953a280e0064be
    Signed-off-by: Qingyu Wang <qingyu.wang@aliyun.com>

---
+++ packages/babel-plugin-transform-typescript/src/global-types.ts
@@ -0,0 +1,24 @@
+import type { NodePath, Scope } from "@babel/traverse";
+
+export const GLOBAL_TYPES = new WeakMap<Scope, Set<string>>();
+
+export function isGlobalType({ scope }: NodePath, name: string) {
+  if (scope.hasBinding(name)) return false;
+  if (GLOBAL_TYPES.get(scope).has(name)) return true;
+
+  console.warn(
+    `The exported identifier "${name}" is not declared in Babel's scope tracker\n` +
+      `as a JavaScript value binding, and "@babel/plugin-transform-typescript"\n` +
+      `never encountered it as a TypeScript type declaration.\n` +
+      `It will be treated as a JavaScript value.\n\n` +
+      `This problem is likely caused by another plugin injecting\n` +
+      `"${name}" without registering it in the scope tracker. If you are the author\n` +
+      ` of that plugin, please use "scope.registerDeclaration(declarationPath)".`,
+  );
+
+  return false;
+}
+
+export function registerGlobalType(programScope: Scope, name: string) {
+  GLOBAL_TYPES.get(programScope).add(name);
+}
