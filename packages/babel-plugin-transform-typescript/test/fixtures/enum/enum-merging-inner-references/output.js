b6e961531582c7d6b8b3ef349ac1dc0a7fdfac0b
Nicolò Ribaudo <hello@nicr.dev>
2023-11-28 15:32:48 +0100

    Add test

---
+++ packages/babel-plugin-transform-typescript/test/fixtures/enum/enum-merging-inner-references/output.js
@@ -0,0 +1,9 @@
+var Animals = /*#__PURE__*/function (Animals) {
+  Animals[Animals["Cat"] = 1] = "Cat";
+  Animals[Animals["Dog"] = 2] = "Dog";
+  return Animals;
+}(Animals || {});
+Animals = function (Animals) {
+  Animals[Animals["CatDog"] = Animals.Cat - Dog] = "CatDog";
+  return Animals;
+}(Animals || {});
