d8814d4bca1667d51447976f4150d58868c1dbb5
Nicolò Ribaudo <hello@nicr.dev>
2023-11-28 15:04:46 +0100

    Add test

---
+++ packages/babel-plugin-transform-classes/test/fixtures/misc/missing-class-properties-transform-private/output.js
@@ -0,0 +1,14 @@
+let Test = /*#__PURE__*/function () {
+  "use strict";
+
+  function Test() {
+    babelHelpers.classCallCheck(this, Test);
+  }
+  babelHelpers.createClass(Test, [{
+    key: "method",
+    value: function method() {
+      return this.#key;
+    }
+  }]);
+  return Test;
+}();
