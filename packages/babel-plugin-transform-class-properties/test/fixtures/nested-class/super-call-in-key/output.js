0aa950f9cc45de6eb47048ea03955cc2de2e61c1
Huáng Jùnliàng <jlhwung@gmail.com>
2023-12-06 15:31:03 -0500

    reuse computed key memoiser

--- packages/babel-plugin-transform-class-properties/test/fixtures/nested-class/super-call-in-key/output.js
+++ packages/babel-plugin-transform-class-properties/test/fixtures/nested-class/super-call-in-key/output.js
@@ -12,13 +12,12 @@ let Outer = /*#__PURE__*/function (_Hello) {
   babelHelpers.inherits(Outer, _Hello);
   var _super = babelHelpers.createSuper(Outer);
   function Outer() {
-    let _this2;
     var _this;
     babelHelpers.classCallCheck(this, Outer);
-    _this2 = _this = _super.call(this);
+    _this = _super.call(this);
     let Inner = /*#__PURE__*/babelHelpers.createClass(function Inner() {
       babelHelpers.classCallCheck(this, Inner);
-      babelHelpers.defineProperty(this, _this2, "hello");
+      babelHelpers.defineProperty(this, _this, "hello");
     });
     return babelHelpers.possibleConstructorReturn(_this, new Inner());
   }
