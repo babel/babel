3dade17b12424937b230764df6bd823dcf7c225b
Huáng Jùnliàng <jlhwung@gmail.com>
2023-12-07 15:01:41 -0500

    remove debug test case

--- packages/babel-plugin-proposal-decorators/test/fixtures/2021-12-accessors/context-name-debug/input.js
+++
@@ -1,8 +0,0 @@
-const logs = [];
-const dec = (value, context) => { logs.push(context.name) };
-const f = () => { logs.push("computing f"); return { [Symbol.toPrimitive]: () => (logs.push("calling toPrimitive"), "f()") }; };
-class Foo {
-  @dec static accessor [f()];
-}
-
-expect(logs).toStrictEqual(["computing f", "f()"]);
