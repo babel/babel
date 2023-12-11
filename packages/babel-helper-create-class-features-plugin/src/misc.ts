93dd4078f883c26c0a14240ac86d632093ffed78
Huáng Jùnliàng <jlhwung@gmail.com>
2023-12-11 05:44:47 -0500

    Reuse computed key memoiser (#16159)

--- packages/babel-helper-create-class-features-plugin/src/misc.ts
+++ packages/babel-helper-create-class-features-plugin/src/misc.ts
@@ -142,21 +142,37 @@ export function extractComputedKeys(
     // Make sure computed property names are only evaluated once (upon class definition)
     // and in the right order in combination with static properties
     if (!computedKey.isConstantExpression()) {
-      const ident = path.scope.generateUidIdentifierBasedOnNode(
-        computedNode.key,
-      );
-      // Declaring in the same block scope
-      // Ref: https://github.com/babel/babel/pull/10029/files#diff-fbbdd83e7a9c998721c1484529c2ce92
-      path.scope.push({
-        id: ident,
-        kind: "let",
-      });
-      declarations.push(
-        t.expressionStatement(
-          t.assignmentExpression("=", t.cloneNode(ident), computedNode.key),
-        ),
-      );
-      computedNode.key = t.cloneNode(ident);
+      const scope = path.scope;
+      const isUidReference =
+        t.isIdentifier(computedKey.node) && scope.hasUid(computedKey.node.name);
+      const isMemoiseAssignment =
+        computedKey.isAssignmentExpression({ operator: "=" }) &&
+        t.isIdentifier(computedKey.node.left) &&
+        scope.hasUid(computedKey.node.left.name);
+      if (isUidReference) {
+        continue;
+      } else if (isMemoiseAssignment) {
+        declarations.push(t.expressionStatement(t.cloneNode(computedNode.key)));
+        computedNode.key = t.cloneNode(
+          (computedNode.key as t.AssignmentExpression).left as t.Identifier,
+        );
+      } else {
+        const ident = path.scope.generateUidIdentifierBasedOnNode(
+          computedNode.key,
+        );
+        // Declaring in the same block scope
+        // Ref: https://github.com/babel/babel/pull/10029/files#diff-fbbdd83e7a9c998721c1484529c2ce92
+        scope.push({
+          id: ident,
+          kind: "let",
+        });
+        declarations.push(
+          t.expressionStatement(
+            t.assignmentExpression("=", t.cloneNode(ident), computedNode.key),
+          ),
+        );
+        computedNode.key = t.cloneNode(ident);
+      }
     }
   }
 
