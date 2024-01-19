d999bff0b36ebe21b836be35104affc22ae43315
Nicolò Ribaudo <hello@nicr.dev>
2024-01-19 13:30:06 +0100

    Ensure that the test is testing the correct behavior

--- packages/babel-plugin-transform-modules-systemjs/test/fixtures/preset-env/requeue-helpers/input.js
+++ packages/babel-plugin-transform-modules-systemjs/test/fixtures/preset-env/requeue-helpers/input.js
@@ -3,6 +3,7 @@
 delete global.Symbol
 
 require("core-js/modules/es.symbol.js");
+require("core-js/modules/es.symbol.to-primitive");
 
 const symbol = Symbol("test");
 
@@ -10,12 +11,24 @@ const symbol = Symbol("test");
 expect(eval('typeof symbol')).toBe("object");
 expect(typeof symbol).toBe("symbol");
 
+function objectWithPrimitive(prim) {
+  return { [Symbol.toPrimitive]: () => prim }
+}
+
 expect(() => {
   class AxiosHeaders {
-    [Symbol.iterator]() {
+    [objectWithPrimitive(symbol)]() {
       return;
     }
   }
 }).not.toThrow()
+
+expect(() => {
+  class AxiosHeaders {
+    [objectWithPrimitive({})]() {
+      return;
+    }
+  }
+}).toThrow()
 
 return "ok";
