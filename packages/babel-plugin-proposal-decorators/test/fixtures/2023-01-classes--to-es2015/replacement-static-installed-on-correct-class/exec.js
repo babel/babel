352383f381eb8e3c1c346d30b2553ca012af7d99
Huáng Jùnliàng <jlhwung@gmail.com>
2024-02-04 10:24:19 -0500

    Remove logic for deprecated decorator versions from Babel 8 (#16256)
    * refactor: reuse staticFieldInitializerAssignments
    
    * refactor: rename fieldInitializerAssignments to expressions
    
    * test: expand replacement-static-installed-on-correct-class case
    
    * chore: maybePrivateBrandName typo
    
    * wrap version "2023-05" check within Babel 7 branch

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-01-classes--to-es2015/replacement-static-installed-on-correct-class/exec.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-01-classes--to-es2015/replacement-static-installed-on-correct-class/exec.js
@@ -1,4 +1,4 @@
-let hasX, hasM, OriginalFoo;
+let hasX, hasA, hasM, OriginalFoo;
 
 class Bar {}
 
@@ -10,24 +10,32 @@ function dec(Foo) {
 @dec
 class Foo {
   static #x;
+  static accessor #a;
   static #m() {}
 
   static x;
+  static accessor a;
   static m() {}
 
   static {
     hasX = o => #x in o;
+    hasA = o => #a in o;
     hasM = o => #m in o;
   }
 }
 
 expect(hasX(Bar)).toBe(true);
+expect(hasA(Bar)).toBe(true);
 expect(hasM(Bar)).toBe(true);
 expect(hasX(OriginalFoo)).toBe(false);
+expect(hasA(OriginalFoo)).toBe(false);
 expect(hasM(OriginalFoo)).toBe(false);
 
 expect(Bar.hasOwnProperty("x")).toBe(true);
 expect(OriginalFoo.hasOwnProperty("x")).toBe(false);
+
+expect(Bar.hasOwnProperty("a")).toBe(false);
+expect(OriginalFoo.hasOwnProperty("a")).toBe(true);
 
 expect(Bar.hasOwnProperty("m")).toBe(false);
 expect(OriginalFoo.hasOwnProperty("m")).toBe(true);
