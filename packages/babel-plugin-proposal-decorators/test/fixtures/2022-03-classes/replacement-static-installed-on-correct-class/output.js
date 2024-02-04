b78f8643c4b4cd9b5e742c0d1dc08fa6eeffb938
Huáng Jùnliàng <jlhwung@gmail.com>
2024-02-04 10:24:19 -0500

    Remove logic for deprecated decorator versions from Babel 8 (#16256)
    * refactor: reuse staticFieldInitializerAssignments
    
    * refactor: rename fieldInitializerAssignments to expressions
    
    * test: expand replacement-static-installed-on-correct-class case
    
    * chore: maybePrivateBrandName typo
    
    * wrap version "2023-05" check within Babel 7 branch

--- packages/babel-plugin-proposal-decorators/test/fixtures/2022-03-classes/replacement-static-installed-on-correct-class/output.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2022-03-classes/replacement-static-installed-on-correct-class/output.js
@@ -1,6 +1,6 @@
 var _initClass;
 const dec = () => {};
-let hasX, hasM;
+let hasX, hasA, hasM;
 let _Foo;
 new class extends babelHelpers.identity {
   static {
@@ -8,15 +8,30 @@ new class extends babelHelpers.identity {
       static {
         [_Foo, _initClass] = babelHelpers.applyDecs2203R(this, [], [dec]).c;
       }
+      static get a() {
+        return this.#B;
+      }
+      static set a(v) {
+        this.#B = v;
+      }
       static m() {}
     }
   }
   #x;
+  #A;
+  get #a() {
+    return this.#A;
+  }
+  set #a(v) {
+    this.#A = v;
+  }
   #m() {}
   x;
+  #B;
   constructor() {
     super(_Foo), (() => {
       hasX = o => #x in o;
+      hasA = o => #a in o;
       hasM = o => #m in o;
     })(), _initClass();
   }
