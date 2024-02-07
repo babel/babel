1b3e183e3af0eee62e25be73112770d35235ec1d
Nicolò Ribaudo <hello@nicr.dev>
2024-02-06 18:04:11 -0800

    Preserve `static` on decorated private `accessor` (#16266)

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-01-accessors--to-es2015/context-name/output.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-01-accessors--to-es2015/context-name/output.js
@@ -10,14 +10,7 @@ const f = () => {
   };
 };
 _computedKey = babelHelpers.toPropertyKey(f());
-var _a = /*#__PURE__*/new WeakMap();
 class Foo {
-  constructor() {
-    babelHelpers.classPrivateFieldInitSpec(this, _a, {
-      get: _get_a2,
-      set: _set_a2
-    });
-  }
   static get a() {
     return babelHelpers.classStaticPrivateFieldSpecGet(this, Foo, _A);
   }
@@ -74,6 +67,10 @@ function _set_a2(v) {
 function _get_a2() {
   return _get_a(this);
 }
+var _a = {
+  get: _get_a2,
+  set: _set_a2
+};
 (() => {
   [_init_a, _init_a2, _get_a, _set_a, _init_computedKey, _init_computedKey2, _init_computedKey3, _init_computedKey4, _init_computedKey5, _init_computedKey6, _init_computedKey7, _initStatic] = babelHelpers.applyDecs2301(_Foo, [[dec, 6, "a"], [dec, 6, "a", o => babelHelpers.classStaticPrivateFieldSpecGet(o, _Foo, _B), (o, v) => babelHelpers.classStaticPrivateFieldSpecSet(o, _Foo, _B, v)], [dec, 6, "b"], [dec, 6, "c"], [dec, 6, 0], [dec, 6, 1], [dec, 6, 2n], [dec, 6, 3n], [dec, 6, _computedKey]], []).e;
   _initStatic(_Foo);
