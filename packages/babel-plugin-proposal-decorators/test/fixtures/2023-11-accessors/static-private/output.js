bf649f8ccf6d20b185b030e205c7dd6d58cb5f34
Nicolò Ribaudo <hello@nicr.dev>
2024-02-07 01:58:29 +0100

    Preserve `static` on decorated private `accessor`

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-accessors/static-private/output.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-accessors/static-private/output.js
@@ -5,17 +5,17 @@ class Foo {
     [_init_a, _get_a, _set_a, _init_extra_a, _init_b, _get_b, _set_b, _init_extra_b] = babelHelpers.applyDecs2311(this, [[dec, 9, "a", o => o.#A, (o, v) => o.#A = v], [dec, 9, "b", o => o.#B, (o, v) => o.#B = v]], []).e;
   }
   static #A = _init_a(this);
-  set #a(v) {
+  static set #a(v) {
     _set_a(this, v);
   }
-  get #a() {
+  static get #a() {
     return _get_a(this);
   }
   static #B = (_init_extra_a(this), _init_b(this, 123));
-  set #b(v) {
+  static set #b(v) {
     _set_b(this, v);
   }
-  get #b() {
+  static get #b() {
     return _get_b(this);
   }
   static {
