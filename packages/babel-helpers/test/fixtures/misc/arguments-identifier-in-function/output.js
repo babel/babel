d00e7cdb42dd690403cf66b04962b67df2a6cd7a
Nicolò Ribaudo <hello@nicr.dev>
2024-02-08 13:28:36 -0800

    Do not consider `arguments` in a helper as a global reference (#16268)

---
+++ packages/babel-helpers/test/fixtures/misc/arguments-identifier-in-function/output.js
@@ -0,0 +1,2 @@
+function _assertClassBrand(e, t, n) { if ("function" == typeof t ? t === e : t.has(e)) return arguments.length < 3 ? e : n; throw new TypeError("Private element is not present on this object"); }
+var arguments;
