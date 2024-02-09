da81c4488b21f8d474a2f2ee6c16e8bd033c1c04
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2024-02-09 09:08:46 +0800

    Update packages/babel-helpers/src/helpers/toSetter.ts
    Co-authored-by: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>

--- packages/babel-helpers/src/helpers/toSetter.ts
+++ packages/babel-helpers/src/helpers/toSetter.ts
@@ -1,9 +1,11 @@
 /* @minVersion 7.22.0 */
 
 export default function _toSetter(fn: Function, args: any[], thisArg: any) {
+  var l = args.length;
   return Object.defineProperty({}, "_", {
     set: function (v) {
-      fn.apply(thisArg, args.concat([v]));
+      args[l] = v;
+      fn.apply(thisArg, args);
     },
   });
 }
