0c1e42e69f0f16a353a64a72753dc5ead8bb7a54
Huáng Jùnliàng <jlhwung@gmail.com>
2024-02-13 16:02:08 -0500

    update test output

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc--to-es2015/valid-expression-formats/output.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc--to-es2015/valid-expression-formats/output.js
@@ -1,11 +1,12 @@
-var _initProto, _initClass, _classDecs, _dec, _dec2, _dec3, _obj, _dec4, _Foo2;
+var _initProto, _initClass, _obj, _classDecs, _dec, _dec2, _dec3, _obj2, _dec4, _Foo2;
 const dec = () => {};
-_classDecs = [void 0, dec, void 0, call(), void 0, chain.expr(), void 0, arbitrary + expr, array, array[expr]];
+_obj = array;
+_classDecs = [void 0, dec, void 0, call(), void 0, chain.expr(), void 0, arbitrary + expr, _obj, _obj[expr]];
 _dec = call();
 _dec2 = chain.expr();
 _dec3 = arbitrary + expr;
-_obj = array;
-_dec4 = _obj[expr];
+_obj2 = array;
+_dec4 = _obj2[expr];
 let _Foo;
 var _a = /*#__PURE__*/new WeakMap();
 class Foo {
@@ -27,5 +28,5 @@ _Foo2 = Foo;
 ({
   e: [_initProto],
   c: [_Foo, _initClass]
-} = babelHelpers.applyDecs2311(_Foo2, [[[void 0, dec, void 0, _dec, void 0, _dec2, void 0, _dec3, _obj, _dec4], 18, "method"]], _classDecs, 1));
+} = babelHelpers.applyDecs2311(_Foo2, [[[void 0, dec, void 0, _dec, void 0, _dec2, void 0, _dec3, _obj2, _dec4], 18, "method"]], _classDecs, 1));
 _initClass();
--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc/super-in-decorator/output.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc/super-in-decorator/output.js
@@ -1,8 +1,9 @@
 class A extends B {
   m() {
-    var _initProto, _initClass, _classDecs, _obj, _dec;
-    _classDecs = [this, super.dec1];
+    var _initProto, _initClass, _obj, _classDecs, _obj2, _dec;
     _obj = this;
+    _classDecs = [_obj, super.dec1];
+    _obj2 = this;
     _dec = super.dec2;
     let _C;
     class C {
@@ -10,7 +11,7 @@ class A extends B {
         ({
           e: [_initProto],
           c: [_C, _initClass]
-        } = babelHelpers.applyDecs2311(this, [[[_obj, _dec], 18, "m2"]], _classDecs, 1));
+        } = babelHelpers.applyDecs2311(this, [[[_obj2, _dec], 18, "m2"]], _classDecs, 1));
       }
       constructor() {
         _initProto(this);
