ff2288d3379f44d08e360c3aba9e0c0756a528b8
Huáng Jùnliàng <jlhwung@gmail.com>
2024-02-14 09:52:16 -0500

    Fix decorator this memoization (#16279)

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc/this/exec.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc/this/exec.js
@@ -9,13 +9,17 @@ function dec() {
 let o1 = { dec };
 let o2 = { dec };
 let o3 = { o: { dec } };
+let o4oCounter = 0;
+let o4 = { o() { o4oCounter++; return o1 } };
 
 @o1.dec
 @dec
 @o2.dec
+@(o4.o().dec)
 class A {
     @o2.dec
     @o3.o.dec
+    @(o4.o().dec)
     x;
 
     @o2.dec
@@ -23,4 +27,5 @@ class A {
     y;
 }
 
-expect(_this).toEqual([o3.o, o2, undefined, o2, o2, undefined, o1]);
+expect(_this).toEqual([o1, o3.o, o2, undefined, o2, o1, o2, undefined, o1]);
+expect(o4oCounter).toBe(2);
--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc/this/input.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc/this/input.js
@@ -1,9 +1,11 @@
 @o1.dec
 @dec
 @o2.dec
+@(o4.o().dec)
 class A {
     @o2.dec
     @o3.o.dec
+    @(o4.o().dec)
     x;
 
     @o2.dec
