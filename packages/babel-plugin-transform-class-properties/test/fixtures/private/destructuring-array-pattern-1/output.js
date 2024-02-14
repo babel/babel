f7293ab4b54599e2105cf89949b96bc0c0d8618b
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2024-02-14 22:03:14 +0800

    Improve `toSetter` helper (#16269)
    Co-authored-by: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>

--- packages/babel-plugin-transform-class-properties/test/fixtures/private/destructuring-array-pattern-1/output.js
+++ packages/babel-plugin-transform-class-properties/test/fixtures/private/destructuring-array-pattern-1/output.js
@@ -5,5 +5,5 @@ let Foo = /*#__PURE__*/babelHelpers.createClass(function Foo(props) {
   babelHelpers.classCallCheck(this, Foo);
   babelHelpers.classPrivateFieldInitSpec(this, _client, void 0);
   babelHelpers.classPrivateFieldSet2(this, _client, 1);
-  [this.x = babelHelpers.classPrivateFieldGet2(this, _client), babelHelpers.toSetter(babelHelpers.classPrivateFieldSet2, null, this, _client)._, this.y = babelHelpers.classPrivateFieldGet2(this, _client)] = props;
+  [this.x = babelHelpers.classPrivateFieldGet2(this, _client), babelHelpers.toSetter(babelHelpers.classPrivateFieldSet2, [this, _client])._, this.y = babelHelpers.classPrivateFieldGet2(this, _client)] = props;
 });
