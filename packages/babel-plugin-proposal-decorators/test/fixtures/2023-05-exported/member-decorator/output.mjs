2c84663e77bf0b902672064779fb1d6582a81d08
Huáng Jùnliàng <jlhwung@gmail.com>
2024-02-14 11:13:23 -0500

    Reduce element decorator temp variables (#16280)
    * optimize: allocate temp variable for every decorated element
    
    * update test fixtures
    
    * refactor: sort decorator info once
    
    * refactor: simplify maybeExtractDecorators interface
    
    Also improve decoratorThis storage. It turns out we don't have to maintain the map from decorator to its this value.
    
    * refactor: rename maybeExtractDecorators to handleDecoratorExpressions
    
    * unify haveThis and hasThis

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-05-exported/member-decorator/output.mjs
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-05-exported/member-decorator/output.mjs
@@ -1,8 +1,8 @@
-var _dec, _init_x;
-_dec = dec;
+var _xDecs, _init_x;
+_xDecs = dec;
 export class A {
   static {
-    [_init_x] = babelHelpers.applyDecs2305(this, [[_dec, 0, "x"]], []).e;
+    [_init_x] = babelHelpers.applyDecs2305(this, [[_xDecs, 0, "x"]], []).e;
   }
   x = _init_x(this);
 }
