b36fdfc03ed4b013eddee9b64cba79c467d072b1
Huáng Jùnliàng <jlhwung@gmail.com>
2024-02-16 13:13:30 -0500

    test: add replacement-static-decorator-initializer-this test (#16288)

---
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-classes/replacement-static-decorator-initializer-this/input.js
@@ -0,0 +1,31 @@
+
+let original, replaced, accessorThis, getterThis, setterThis, methodThis, propertyThis;
+
+function dec(Klass) {
+  original = Klass;
+  replaced = class extends Klass {};
+
+  return replaced;
+}
+
+function captureInitializerThis(callback) {
+  return function (_, context) {
+    context.addInitializer(function () {
+      callback(this);
+    })
+  }
+}
+
+@dec
+class Foo {
+  @(captureInitializerThis(v => accessorThis = v))
+  static accessor;
+  @(captureInitializerThis(v => getterThis = v))
+  static get getter() {};
+  @(captureInitializerThis(v => setterThis = v))
+  static set setter(_) {};
+  @(captureInitializerThis(v => methodThis = v))
+  static method() {}
+  @(captureInitializerThis(v => propertyThis = v))
+  static property;
+}
