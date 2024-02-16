02f569240d045098e853dd4845d8df07170328eb
Huáng Jùnliàng <jlhwung@gmail.com>
2024-02-16 13:13:30 -0500

    test: add replacement-static-decorator-initializer-this test (#16288)

---
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-classes/replacement-static-decorator-initializer-this/output.js
@@ -0,0 +1,43 @@
+var _initStatic, _initClass, _accessorDecs, _init_accessor, _init_extra_accessor, _getterDecs, _setterDecs, _methodDecs, _propertyDecs, _init_property, _init_extra_property;
+let original, replaced, accessorThis, getterThis, setterThis, methodThis, propertyThis;
+function dec(Klass) {
+  original = Klass;
+  replaced = class extends Klass {};
+  return replaced;
+}
+function captureInitializerThis(callback) {
+  return function (_, context) {
+    context.addInitializer(function () {
+      callback(this);
+    });
+  };
+}
+_accessorDecs = captureInitializerThis(v => accessorThis = v);
+_getterDecs = captureInitializerThis(v => getterThis = v);
+_setterDecs = captureInitializerThis(v => setterThis = v);
+_methodDecs = captureInitializerThis(v => methodThis = v);
+_propertyDecs = captureInitializerThis(v => propertyThis = v);
+let _Foo;
+new class extends babelHelpers.identity {
+  static {
+    class Foo {
+      static {
+        ({
+          e: [_init_accessor, _init_extra_accessor, _init_property, _init_extra_property, _initStatic],
+          c: [_Foo, _initClass]
+        } = babelHelpers.applyDecs2311(this, [[_getterDecs, 11, "getter"], [_setterDecs, 12, "setter"], [_methodDecs, 10, "method"], [_accessorDecs, 8, "accessor"], [_propertyDecs, 8, "property"]], [dec]));
+        _initStatic(this);
+      }
+      static get getter() {}
+      static set setter(_) {}
+      static method() {}
+    }
+  }
+  accessor = _init_accessor(this);
+  property = (_init_extra_accessor(this), _init_property(this));
+  constructor() {
+    super(_Foo), (() => {
+      _init_extra_property(this);
+    })(), _initClass();
+  }
+}();
