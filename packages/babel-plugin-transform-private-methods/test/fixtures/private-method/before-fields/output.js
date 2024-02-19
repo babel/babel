fa48c7a125ac6b3d1c96ad35cd7a635cabb028ef
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2024-02-20 07:16:53 +0800

    Do not use `classPrivateMethodGet` (#16283)
    classPrivateMethodGet

--- packages/babel-plugin-transform-private-methods/test/fixtures/private-method/before-fields/output.js
+++ packages/babel-plugin-transform-private-methods/test/fixtures/private-method/before-fields/output.js
@@ -3,8 +3,8 @@ var _Cl_brand = /*#__PURE__*/new WeakSet();
 class Cl {
   constructor() {
     babelHelpers.classPrivateMethodInitSpec(this, _Cl_brand);
-    babelHelpers.defineProperty(this, "prop", babelHelpers.classPrivateMethodGet(this, _Cl_brand, _method).call(this, 1));
-    babelHelpers.classPrivateFieldInitSpec(this, _priv, babelHelpers.classPrivateMethodGet(this, _Cl_brand, _method).call(this, 2));
+    babelHelpers.defineProperty(this, "prop", babelHelpers.assertClassBrand(this, _Cl_brand, _method).call(this, 1));
+    babelHelpers.classPrivateFieldInitSpec(this, _priv, babelHelpers.assertClassBrand(this, _Cl_brand, _method).call(this, 2));
   }
   getPriv() {
     return babelHelpers.classPrivateFieldGet2(this, _priv);
