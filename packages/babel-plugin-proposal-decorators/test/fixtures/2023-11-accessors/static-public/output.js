f27049754adf0e226cbf7cab82411b70d545b645
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2024-02-19 23:26:23 +0800

    Reduce decorator static property size (#16287)
    * improve
    
    * fix new test
    
    * fix test
    
    * add test
    
    * Apply suggestions from code review
    
    Co-authored-by: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
    
    * lint
    
    ---------
    
    Co-authored-by: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-accessors/static-public/output.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-accessors/static-public/output.js
@@ -4,21 +4,21 @@ class Foo {
   static {
     [_init_a, _init_extra_a, _init_b, _init_extra_b, _init_computedKey, _init_extra_computedKey] = babelHelpers.applyDecs2311(this, [[dec, 9, "a"], [dec, 9, "b"], [dec, 9, 'c']], []).e;
   }
-  static #A = _init_a(this);
+  static #A = _init_a();
   static get a() {
     return Foo.#A;
   }
   static set a(v) {
     Foo.#A = v;
   }
-  static #B = (_init_extra_a(this), _init_b(this, 123));
+  static #B = (_init_extra_a(), _init_b(123));
   static get b() {
     return Foo.#B;
   }
   static set b(v) {
     Foo.#B = v;
   }
-  static #C = (_init_extra_b(this), _init_computedKey(this, 456));
+  static #C = (_init_extra_b(), _init_computedKey(456));
   static get ['c']() {
     return Foo.#C;
   }
@@ -26,6 +26,6 @@ class Foo {
     Foo.#C = v;
   }
   static {
-    _init_extra_computedKey(this);
+    _init_extra_computedKey();
   }
 }
