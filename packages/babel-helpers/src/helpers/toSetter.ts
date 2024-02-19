9971e5a1a1081a42440611098bba4f4fc9b20085
Nicolò Ribaudo <hello@nicr.dev>
2024-02-19 10:43:06 +0100

    Implement `noUninitializedPrivateFieldAccess` assumption (#16267)

--- packages/babel-helpers/src/helpers/toSetter.ts
+++ packages/babel-helpers/src/helpers/toSetter.ts
@@ -1,6 +1,7 @@
 /* @minVersion 7.22.0 */
 
 export default function _toSetter(fn: Function, args: any[], thisArg: any) {
+  if (!args) args = [];
   var l = args.length++;
   return Object.defineProperty({}, "_", {
     set: function (v) {
