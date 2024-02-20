198e865259a78e3c7cf9ace96642161d6d74b5d6
Huáng Jùnliàng <jlhwung@gmail.com>
2024-02-20 15:13:28 -0500

    enable field-initializers-after-methods-private test

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-ordering/field-initializers-after-methods-private/output.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-ordering/field-initializers-after-methods-private/output.js
@@ -1,18 +1,18 @@
-var _dec, _init_foo, _init_extra_foo;
+var _fooDecs, _init_foo, _init_extra_foo;
 var counter = 0;
-_dec = (_, {
-  addInitializer
-}) => {
-  addInitializer(function () {
-    counter++;
-    expect(typeof this.method).toBe("function");
-    expect(this.#foo).toBe("#foo");
-    expect(() => this.#bar).toThrow();
-  });
-};
 class A {
   static {
-    [_init_foo, _init_extra_foo] = babelHelpers.applyDecs2311(this, [[_dec, 0, "foo", o => o.#foo, (o, v) => o.#foo = v]], [], 0, _ => #bar in _).e;
+    _fooDecs = (_, {
+      addInitializer
+    }) => {
+      addInitializer(function () {
+        counter++;
+        expect(typeof this.method).toBe("function");
+        expect(this.#foo).toBe("#foo");
+        expect(() => this.#bar).toThrow();
+      });
+    };
+    [_init_foo, _init_extra_foo] = babelHelpers.applyDecs2311(this, [[_fooDecs, 0, "foo", o => o.#foo, (o, v) => o.#foo = v]], [], 0, _ => #bar in _).e;
   }
   #foo = _init_foo(this, (() => {
     counter++;
