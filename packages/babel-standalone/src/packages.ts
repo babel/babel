8e6c5069bf2aa7fffc993f038cba282f9fcfbac3
flyafly <6063915+ajihyf@users.noreply.github.com>
2024-02-28 18:31:57 +0800

    Export babel tooling packages in `@babel/standalone` (#11696)
    Co-authored-by: Nicolò Ribaudo <hello@nicr.dev>

---
+++ packages/babel-standalone/src/packages.ts
@@ -0,0 +1,5 @@
+export * as generator from "@babel/generator";
+export * as parser from "@babel/parser";
+export * as template from "@babel/template";
+export * as traverse from "@babel/traverse";
+export * as types from "@babel/types";
