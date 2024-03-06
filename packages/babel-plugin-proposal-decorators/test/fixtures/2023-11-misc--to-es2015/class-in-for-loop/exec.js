97612885ce97cf61e4940ede9f3859677ab59632
Huáng Jùnliàng <jlhwung@gmail.com>
2024-03-06 10:23:29 -0500

    fix: register decorator memoisers as let binding

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc--to-es2015/class-in-for-loop/exec.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-misc--to-es2015/class-in-for-loop/exec.js
@@ -1,7 +1,7 @@
 {
   const logs = [];
   const classes = [];
-  function noop() {}
+  const noop = () => {}
 
   for (let i = 0; i < 2; i++) {
     classes.push(class C {
@@ -19,7 +19,7 @@
 {
   const logs = [];
   const classes = [];
-  function noop() {}
+  const noop = () => {}
 
   for (let i = 0; i < 2; i++) {
     classes.push(@noop class C {
@@ -37,7 +37,7 @@
 {
   const logs = [];
   const classes = [];
-  function noop() {}
+  const noop = () => {}
 
   for (let i = 0; i < 2; i++) {
     classes.push(@noop class C {
@@ -50,4 +50,45 @@
   }
 
   expect(logs.join()).toBe("0,1");
+}
+
+{
+  const logs = [];
+  const classes = [];
+  const setValueTo = i => () => () => i
+
+  for (let i = 0; i < 2; i++) {
+    classes.push(class C {
+      @setValueTo(i) [i];
+    })
+  }
+
+  for (const clazz of classes) {
+    const c = new clazz();
+    const key = Reflect.ownKeys(c)[0];
+    logs.push([key, c[key]].join(":"));
+  }
+
+  expect(logs.join()).toBe("0:0,1:1");
+}
+
+{
+  const logs = [];
+  const classes = [];
+  const noop = () => {}
+  const setValueTo = i => (_, { access, addInitializer }) => addInitializer(function() { access.set(this, i) })
+
+  for (let i = 0; i < 2; i++) {
+    classes.push(@noop class C {
+      @setValueTo(i) [i];
+    })
+  }
+
+  for (const clazz of classes) {
+    const c = new clazz();
+    const key = Reflect.ownKeys(c)[0];
+    logs.push([key, c[key]].join(":"));
+  }
+
+  expect(logs.join()).toBe("0:0,1:1");
 }
