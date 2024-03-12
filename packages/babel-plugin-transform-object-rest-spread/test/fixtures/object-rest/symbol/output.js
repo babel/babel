8c866bc9be977816e8763baaa3aab755da4bee7a
Nicolò Ribaudo <hello@nicr.dev>
2024-03-12 16:07:05 +0100

    Symbols are like literals

--- packages/babel-plugin-transform-object-rest-spread/test/fixtures/object-rest/symbol/output.js
+++ packages/babel-plugin-transform-object-rest-spread/test/fixtures/object-rest/symbol/output.js
@@ -3,13 +3,13 @@ let _ref = {},
   {
     [Symbol.for("foo")]: foo
   } = _ref,
-  rest = babelHelpers.objectWithoutProperties(_ref, [Symbol.for("foo")].map(babelHelpers.toPropertyKey));
+  rest = babelHelpers.objectWithoutProperties(_ref, [Symbol.for("foo")]);
 var _ref2 = {};
 ({
   [Symbol.for("foo")]: foo
 } = _ref2);
-rest = babelHelpers.objectWithoutProperties(_ref2, [Symbol.for("foo")].map(babelHelpers.toPropertyKey));
+rest = babelHelpers.objectWithoutProperties(_ref2, [Symbol.for("foo")]);
 _ref2;
 if (_ref3 = {}, ({
   [Symbol.for("foo")]: foo
-} = _ref3), rest = babelHelpers.objectWithoutProperties(_ref3, [Symbol.for("foo")].map(babelHelpers.toPropertyKey)), _ref3) {}
+} = _ref3), rest = babelHelpers.objectWithoutProperties(_ref3, [Symbol.for("foo")]), _ref3) {}
