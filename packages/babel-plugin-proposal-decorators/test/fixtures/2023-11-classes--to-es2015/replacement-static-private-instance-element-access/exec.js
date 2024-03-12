34708a67e46ff69010624c8df19b0117b01ca6e8
Huáng Jùnliàng <jlhwung@gmail.com>
2024-03-12 14:08:02 -0400

    add a private access combo

--- packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-classes--to-es2015/replacement-static-private-instance-element-access/exec.js
+++ packages/babel-plugin-proposal-decorators/test/fixtures/2023-11-classes--to-es2015/replacement-static-private-instance-element-access/exec.js
@@ -241,3 +241,70 @@
   expect(staticThis).toBe(Bar);
   expect(thisMethodLength).toBe(1);
 }
+
+{
+  "instance private access in everywhere mentioned above";
+  let hasX, getX, setX, hasA, getA, setA, hasM, callM, staticThis, thisMethodLength, OriginalFoo;
+
+  class Bar {}
+
+  const dec = (Foo) => {
+    OriginalFoo = Foo;
+    return Bar;
+  };
+
+  @dec
+  class Foo {
+    #x;
+    accessor #a;
+    #m() { return "#m" }
+
+    x;
+    accessor a;
+    m() {}
+
+    static #method({
+      [(staticThis = this, thisMethodLength = this.#method.length, getX = (o) => o.#x)]: _
+    } = (
+      hasM = (o) => #m in o
+    )) {
+      hasA = (o) => #a in o;
+    }
+
+    static {
+      hasX = (o) => #x in o;
+      getA = (o) => o.#a;
+      callM = (o) => o.#m();
+      this.#method();
+    }
+
+    static #field = (
+      setX = (o, v) => o.#x = v,
+      setA = (o, v) => o.#a = v
+    );
+  }
+
+  const foo = new OriginalFoo();
+  const bar = new Foo();
+
+  expect(hasX(foo)).toBe(true);
+  expect(getX((setX(foo, "#x"), foo))).toBe("#x");
+  expect(hasA(foo)).toBe(true);
+  expect(getA((setA(foo, "#a"), foo))).toBe("#a");
+  expect(hasM(foo)).toBe(true);
+  expect(callM(foo)).toBe("#m");
+  expect(hasX(bar)).toBe(false);
+  expect(hasA(bar)).toBe(false);
+  expect(hasM(bar)).toBe(false);
+
+  expect(foo.hasOwnProperty("x")).toBe(true);
+  expect(bar.hasOwnProperty("x")).toBe(false);
+
+  expect(OriginalFoo.prototype.hasOwnProperty("a")).toBe(true);
+  expect(Bar.prototype.hasOwnProperty("a")).toBe(false);
+  expect(OriginalFoo.prototype.hasOwnProperty("m")).toBe(true);
+  expect(Bar.prototype.hasOwnProperty("m")).toBe(false);
+
+  expect(staticThis).toBe(Bar);
+  expect(thisMethodLength).toBe(1);
+}
