c5332a8611fc2b69b02c319a5054042b83d8771d
Nicolò Ribaudo <hello@nicr.dev>
2024-03-13 13:50:48 +0100

    Update fixtures

--- packages/babel-plugin-proposal-json-modules/test/fixtures/node-esm/import-declaration-multiple/output.mjs
+++ packages/babel-plugin-proposal-json-modules/test/fixtures/node-esm/import-declaration-multiple/output.mjs
@@ -1,5 +1,4 @@
-import { readFileSync as _readFileSync2 } from "fs";
-import { readFileSync as _readFileSync } from "fs";
+import { readFileSync as _readFileSync, readFileSync as _readFileSync2 } from "fs";
 const j = JSON.parse(_readFileSync(new URL(import.meta.resolve("./x.json")))),
   j2 = JSON.parse(_readFileSync2(new URL(import.meta.resolve("./x2.json"))));
 someBody;
