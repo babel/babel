c04eccf8c0b7f4e5d8aaed23cbab8e65a571baa3
Nicolò Ribaudo <hello@nicr.dev>
2024-03-14 08:12:45 +0100

    Support merging imports in import injector (#16349)
    Co-authored-by: Conrad Buck <conartist6@gmail.com>
    Co-authored-by: Babel Bot <babel-bot@users.noreply.github.com>

--- packages/babel-plugin-proposal-import-wasm-source/test/fixtures/node-esm/import-declaration-multiple/output.mjs
+++ packages/babel-plugin-proposal-import-wasm-source/test/fixtures/node-esm/import-declaration-multiple/output.mjs
@@ -1,5 +1,4 @@
-import { readFileSync as _readFileSync2 } from "fs";
-import { readFileSync as _readFileSync } from "fs";
+import { readFileSync as _readFileSync, readFileSync as _readFileSync2 } from "fs";
 const s = new WebAssembly.Module(_readFileSync(new URL(import.meta.resolve("./x.wasm")))),
   s2 = new WebAssembly.Module(_readFileSync2(new URL(import.meta.resolve("./x2.wasm"))));
 someBody;
