c3447c762913bb4283bdee997aa176a8508c155f
Rom Grk <romgrk.cc@gmail.com>
2024-03-16 00:44:25 -0400

    lint

--- packages/babel-runtime-corejs3/helpers/esm/objectWithoutPropertiesLoose.js
+++ packages/babel-runtime-corejs3/helpers/esm/objectWithoutPropertiesLoose.js
@@ -1,14 +1,12 @@
-import _Object$hasOwn from "core-js-pure/features/object/has-own.js";
 import _indexOfInstanceProperty from "core-js-pure/features/instance/index-of.js";
 export default function _objectWithoutPropertiesLoose(source, excluded) {
   if (source == null) return {};
   var target = {};
   var key;
   for (key in source) {
-    if (_Object$hasOwn(source, key)) {
-      if (_indexOfInstanceProperty(excluded).call(excluded, key) === -1) {
-        target[key] = source[key];
-      }
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      if (_indexOfInstanceProperty(excluded).call(excluded, key) >= 0) continue;
+      target[key] = source[key];
     }
   }
   return target;
