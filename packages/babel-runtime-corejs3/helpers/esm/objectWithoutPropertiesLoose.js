f72d4d593a20da98e9c6f03f612fda129771dede
Rom Grk <romgrk.cc@gmail.com>
2024-03-16 00:30:34 -0400

    perf: improve objectWithoutPropertiesLoose

--- packages/babel-runtime-corejs3/helpers/esm/objectWithoutPropertiesLoose.js
+++ packages/babel-runtime-corejs3/helpers/esm/objectWithoutPropertiesLoose.js
@@ -1,14 +1,15 @@
-import _Object$keys from "core-js-pure/features/object/keys.js";
+import _Object$hasOwn from "core-js-pure/features/object/has-own.js";
 import _indexOfInstanceProperty from "core-js-pure/features/instance/index-of.js";
 export default function _objectWithoutPropertiesLoose(source, excluded) {
   if (source == null) return {};
   var target = {};
-  var sourceKeys = _Object$keys(source);
-  var key, i;
-  for (i = 0; i < sourceKeys.length; i++) {
-    key = sourceKeys[i];
-    if (_indexOfInstanceProperty(excluded).call(excluded, key) >= 0) continue;
-    target[key] = source[key];
+  var key;
+  for (key in source) {
+    if (_Object$hasOwn(source, key)) {
+      if (_indexOfInstanceProperty(excluded).call(excluded, key) === -1) {
+        target[key] = source[key];
+      }
+    }
   }
   return target;
 }
