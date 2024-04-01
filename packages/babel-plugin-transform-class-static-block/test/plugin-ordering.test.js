f97b3904b874f33610afe40b0292b8b15c3bcbaf
Nicolò Ribaudo <hello@nicr.dev>
2024-04-01 18:23:26 +0200

    Avoid `toPrimitive` in snapshot since its output changes

--- packages/babel-plugin-transform-class-static-block/test/plugin-ordering.test.js
+++ packages/babel-plugin-transform-class-static-block/test/plugin-ordering.test.js
@@ -1,6 +1,7 @@
 import * as babel from "@babel/core";
 import proposalClassStaticBlock from "../lib/index.js";
 import proposalClassProperties from "@babel/plugin-transform-class-properties";
+import externalHelpers from "@babel/plugin-external-helpers";
 
 describe("plugin ordering", () => {
   it("should work when @babel/plugin-transform-class-static-block is after class features plugin", () => {
@@ -17,17 +18,18 @@ describe("plugin ordering", () => {
         highlightCode: false,
         configFile: false,
         babelrc: false,
-        plugins: [proposalClassProperties, proposalClassStaticBlock],
+        plugins: [
+          proposalClassProperties,
+          proposalClassStaticBlock,
+          externalHelpers,
+        ],
       }).code,
     ).toMatchInlineSnapshot(`
       "var _Foo;
-      function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-      function _toPropertyKey(t) { var i = _toPrimitive(t, \\"string\\"); return \\"symbol\\" == typeof i ? i : i + \\"\\"; }
-      function _toPrimitive(t, r) { if (\\"object\\" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || \\"default\\"); if (\\"object\\" != typeof i) return i; throw new TypeError(\\"@@toPrimitive must return a primitive value.\\"); } return (\\"string\\" === r ? String : Number)(t); }
       class Foo {}
       _Foo = Foo;
       _Foo.foo = _Foo.bar;
-      _defineProperty(Foo, \\"bar\\", 42);"
+      babelHelpers.defineProperty(Foo, \\"bar\\", 42);"
     `);
   });
 });
