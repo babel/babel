5c484d105e5d5027acc10958b7d862d96559e9a0
Huáng Jùnliàng <jlhwung@gmail.com>
2024-04-03 15:58:23 -0400

    update fixtures

--- packages/babel-runtime-corejs3/helpers/esm/usingCtx.js
+++ packages/babel-runtime-corejs3/helpers/esm/usingCtx.js
@@ -22,7 +22,9 @@ export default function _usingCtx() {
         d: o,
         a: r
       });
-    }
+    } else r && _pushInstanceProperty(e).call(e, {
+      a: r
+    });
     return n;
   }
   return {
@@ -34,7 +36,7 @@ export default function _usingCtx() {
       function next() {
         for (; r = e.pop();) try {
           var r,
-            t = r.d.call(r.v);
+            t = r.d && r.d.call(r.v);
           if (r.a) return _Promise.resolve(t).then(next, err);
         } catch (r) {
           return err(r);
