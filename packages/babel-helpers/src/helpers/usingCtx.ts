8869938f4c2d670f153c083eba1d395a0bc9655c
Huáng Jùnliàng <jlhwung@gmail.com>
2024-04-05 16:15:50 -0400

    rename enums

--- packages/babel-helpers/src/helpers/usingCtx.ts
+++ packages/babel-helpers/src/helpers/usingCtx.ts
@@ -2,17 +2,17 @@
 
 const enum DisposeFlag {
   // set when the dispose function is a [Symbol.asyncDispose] method
-  ASYNC_FLAG = 1,
-  // alias of AWAIT_FLAG: set when the resource is an `await using` disposable
-  ASYNC_MASK = 2,
+  ASYNC_DISPOSE = 1,
+  // alias of AWAIT_USING: set when the resource is an `await using` disposable
+  ASYNC_DISPOSE_MASK = 2,
 }
 
 const enum DisposeKind {
-  SYNC = 0,
+  USING_DISPOSE = 0,
   // AWAIT_FLAG
-  AWAIT_SYNC = 2,
-  // AWAIT_FLAG | ASYNC_FLAG
-  AWAIT_ASYNC = 3,
+  AWAIT_USING_DISPOSE = 2,
+  // Flag.AWAIT_USING | Flag.ASYNC_DISPOSE
+  AWAIT_USING_ASYNC_DISPOSE = 3,
 }
 
 type Stack = {
@@ -46,11 +46,11 @@ export default function _usingCtx() {
       if (isAwait) {
         var dispose =
             value[Symbol.asyncDispose || Symbol.for("Symbol.asyncDispose")],
-          kind = DisposeKind.AWAIT_ASYNC;
+          kind = DisposeKind.AWAIT_USING_ASYNC_DISPOSE;
       }
       if (dispose === undefined) {
         dispose = value[Symbol.dispose || Symbol.for("Symbol.dispose")];
-        kind &= DisposeFlag.ASYNC_MASK;
+        kind &= DisposeFlag.ASYNC_DISPOSE_MASK;
       }
       if (typeof dispose !== "function") {
         throw new TypeError("Object is not disposable.");
@@ -58,7 +58,7 @@ export default function _usingCtx() {
       stack.push({ v: value, d: dispose, k: kind });
     } else if (isAwait) {
       // provide the nullish `value` as `d` for minification gain
-      stack.push({ d: value, k: DisposeKind.AWAIT_SYNC });
+      stack.push({ d: value, k: DisposeKind.AWAIT_USING_DISPOSE });
     }
     return value;
   }
@@ -82,7 +82,7 @@ export default function _usingCtx() {
             if (resource.k) {
               return Promise.resolve(
                 // do not await the promise returned from the sync @@dispose
-                resource.k & DisposeFlag.ASYNC_FLAG && disposalResult,
+                resource.k & DisposeFlag.ASYNC_DISPOSE && disposalResult,
               ).then(next, err);
             }
           } catch (e) {
