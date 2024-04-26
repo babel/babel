75a2e5f23ae64341fa94eadaa49ee8d5cdadfa35
Huáng Jùnliàng <jlhwung@gmail.com>
2024-04-26 11:31:51 -0400

    bump glob to 10.3.12

--- packages/babel-cli/src/babel/options.ts
+++ packages/babel-cli/src/babel/options.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 
 import commander from "commander";
 import { version, DEFAULT_EXTENSIONS } from "@babel/core";
-import glob from "glob";
+import { sync as globSync } from "glob";
 
 import type { InputOptions } from "@babel/core";
 
@@ -217,13 +217,13 @@ export default function parseArgv(args: Array<string>): CmdOptions | null {
       // glob 9+ no longer sorts the result, here we maintain the glob 7 behaviour
       // https://github.com/isaacs/node-glob/blob/c3cd57ae128faa0e9190492acc743bb779ac4054/common.js#L151
       // eslint-disable-next-line no-var
-      var files = glob
-        .sync(input, { dotRelative: true })
-        .sort(function alphasort(a, b) {
+      var files = globSync(input, { dotRelative: true }).sort(
+        function alphasort(a, b) {
           return a.localeCompare(b, "en");
-        });
+        },
+      );
     } else {
-      files = glob.sync(input);
+      files = globSync(input);
     }
     if (!files.length) files = [input];
     globbed.push(...files);
