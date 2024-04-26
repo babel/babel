7667d96322a01791eb44ef2f0afeb55c6af39cfb
Nicolò Ribaudo <hello@nicr.dev>
2024-04-26 17:45:30 +0200

    Export `Expression` type from `@babel/parser`

--- packages/babel-parser/src/index.ts
+++ packages/babel-parser/src/index.ts
@@ -22,7 +22,7 @@ import {
 } from "./tokenizer/types.ts";
 
 import type { Expression, File } from "./types.ts";
-export type { File };
+export type { Expression, File };
 
 export function parse(input: string, options?: Options): File {
   if (options?.sourceType === "unambiguous") {
