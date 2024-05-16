156ff09db2b142509c7265a8e3dbe9dcbb950402
Blake Wilson <blake@blake.id>
2024-05-16 16:17:53 -0500

    WIP: types for `iterableToArrayLimit`

--- packages/babel-helpers/src/helpers/iterableToArrayLimit.ts
+++ packages/babel-helpers/src/helpers/iterableToArrayLimit.ts
@@ -1,53 +1,48 @@
 /* @minVersion 7.0.0-beta.0 */
 
-export default function _iterableToArrayLimit(arr, i) {
+export default function _iterableToArrayLimit<T>(arr: any, i: number) {
   // this is an expanded form of \`for...of\` that properly supports abrupt completions of
-  // iterators etc. variable names have been minimised to reduce the size of this massive
-  // helper. sometimes spec compliance is annoying :(
-  //
-  // _n = _iteratorNormalCompletion
-  // _d = _didIteratorError
-  // _e = _iteratorError
-  // _i = _iterator
-  // _s = _step
-  // _x = _next
-  // _r = _return
+  // iterators etc.
 
-  var _i =
+  let iterator =
     arr == null
       ? null
       : (typeof Symbol !== "undefined" && arr[Symbol.iterator]) ||
         arr["@@iterator"];
-  if (_i == null) return;
+  if (iterator == null) return;
 
-  var _arr = [];
-  var _n = true;
-  var _d = false;
-  var _s, _e, _x, _r;
+  const _arr: Array<T> = [];
+  let iteratorNormalCompletion = true;
+  let didIteratorError = false;
+  let step, iteratorError, next, _return;
   try {
-    _x = (_i = _i.call(arr)).next;
+    next = (iterator = iterator.call(arr)).next;
     if (i === 0) {
-      if (Object(_i) !== _i) return;
-      _n = false;
+      if (Object(iterator) !== iterator) return;
+      iteratorNormalCompletion = false;
     } else {
-      for (; !(_n = (_s = _x.call(_i)).done); _n = true) {
-        _arr.push(_s.value);
+      for (
+        ;
+        !(iteratorNormalCompletion = (step = next.call(iterator)).done);
+        iteratorNormalCompletion = true
+      ) {
+        _arr.push(step.value);
         if (_arr.length === i) break;
       }
     }
   } catch (err) {
-    _d = true;
-    _e = err;
+    didIteratorError = true;
+    iteratorError = err;
   } finally {
     try {
-      if (!_n && _i["return"] != null) {
-        _r = _i["return"]();
+      if (!iteratorNormalCompletion && iterator["return"] != null) {
+        _return = iterator["return"]();
         // eslint-disable-next-line no-unsafe-finally
-        if (Object(_r) !== _r) return;
+        if (Object(_return) !== _return) return;
       }
     } finally {
       // eslint-disable-next-line no-unsafe-finally
-      if (_d) throw _e;
+      if (didIteratorError) throw iteratorError;
     }
   }
   return _arr;
