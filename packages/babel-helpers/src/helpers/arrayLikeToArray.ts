41dd7eaff57e041ff4029775647127cc6e2dc67e
Blake Wilson <blake@blake.id>
2024-05-16 15:39:35 -0500

    Typings for `arrayLikeToArray`

--- packages/babel-helpers/src/helpers/arrayLikeToArray.ts
+++ packages/babel-helpers/src/helpers/arrayLikeToArray.ts
@@ -1,7 +1,10 @@
 /* @minVersion 7.9.0 */
 
-export default function _arrayLikeToArray(arr, len) {
+export default function _arrayLikeToArray<T>(
+  arr: ArrayLike<T>,
+  len: number | null,
+) {
   if (len == null || len > arr.length) len = arr.length;
-  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+  for (var i = 0, arr2 = new Array<T>(len); i < len; i++) arr2[i] = arr[i];
   return arr2;
 }
