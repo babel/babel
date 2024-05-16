76e44271a2ecaa2425570c1eb608d7049007ceef
Blake Wilson <blake@blake.id>
2024-05-16 15:49:53 -0500

    Add types to `arrayWithHoles`

--- packages/babel-helpers/src/helpers/arrayWithHoles.ts
+++ packages/babel-helpers/src/helpers/arrayWithHoles.ts
@@ -1,5 +1,5 @@
 /* @minVersion 7.0.0-beta.0 */
 
-export default function _arrayWithHoles(arr) {
-  if (Array.isArray(arr)) return arr;
+export default function _arrayWithHoles<T>(arr: any) {
+  if (Array.isArray(arr)) return arr as Array<T>;
 }
