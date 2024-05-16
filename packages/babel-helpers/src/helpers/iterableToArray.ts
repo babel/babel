8e6a0c63ba94d68fa8a630f5b6757f4dcc41c3b0
Blake Wilson <blake@blake.id>
2024-05-16 15:58:59 -0500

    Types for `iterableToArray`

--- packages/babel-helpers/src/helpers/iterableToArray.ts
+++ packages/babel-helpers/src/helpers/iterableToArray.ts
@@ -1,6 +1,14 @@
 /* @minVersion 7.0.0-beta.0 */
 
-export default function _iterableToArray(iter) {
+export default function _iterableToArray<T>(
+  // Note the '@@iterator' property is not recognized here by TypeScript.
+  // In fact, it looks like it's used only as the spec name to denote the symbol.
+  // We may be able to remove.
+  // https://stackoverflow.com/a/29672061/11711383
+  // https://262.ecma-international.org/6.0/#sec-well-known-symbols
+  // iter: Iterable<T>,
+  iter: Iterable<T> & { "@@iterator": any },
+) {
   if (
     (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null) ||
     iter["@@iterator"] != null
