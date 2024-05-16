e4e217ecd5ececa52ca3d4bce5c1af0cbcab0ac9
Blake Wilson <blake@blake.id>
2024-05-16 16:37:30 -0500

    Types for `maybeArrayLike`

--- packages/babel-helpers/src/helpers/maybeArrayLike.ts
+++ packages/babel-helpers/src/helpers/maybeArrayLike.ts
@@ -2,10 +2,14 @@
 
 import arrayLikeToArray from "./arrayLikeToArray.ts";
 
-export default function _maybeArrayLike(next, arr, i) {
+export default function _maybeArrayLike<T>(
+  next: (arr: any, i: number) => any,
+  arr: any,
+  i: number,
+) {
   if (arr && !Array.isArray(arr) && typeof arr.length === "number") {
     var len = arr.length;
-    return arrayLikeToArray(arr, i !== void 0 && i < len ? i : len);
+    return arrayLikeToArray<T>(arr, i !== void 0 && i < len ? i : len);
   }
   return next(arr, i);
 }
