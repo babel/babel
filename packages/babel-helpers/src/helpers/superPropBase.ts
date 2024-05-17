c5195d2a0eabaabddd2f0d0526085bbefa5dbe12
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-05-17 12:11:20 -0300

    Migrate `superPropBase`

--- packages/babel-helpers/src/helpers/superPropBase.ts
+++ packages/babel-helpers/src/helpers/superPropBase.ts
@@ -1,8 +1,15 @@
 /* @minVersion 7.0.0-beta.0 */
 
-import getPrototypeOf from "getPrototypeOf";
+import getPrototypeOf from "./getPrototypeOf.ts";
 
-export default function _superPropBase(object, property) {
+type Object = {
+  __proto__?: any;
+} & { [key: string]: unknown };
+
+export default function _superPropBase<T extends PropertyKey>(
+  object: Object,
+  property: T,
+) {
   // Yes, this throws if object is null to being with, that's on purpose.
   while (!Object.prototype.hasOwnProperty.call(object, property)) {
     object = getPrototypeOf(object);
