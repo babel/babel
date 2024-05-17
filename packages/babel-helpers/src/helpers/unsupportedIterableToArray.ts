036c0ea905ca816390493079a8f89905896f5da7
Blake Wilson <blake@blake.id>
2024-05-16 23:24:28 -0500

    WIP: types for `unsupportedIterableToArray`

--- packages/babel-helpers/src/helpers/unsupportedIterableToArray.ts
+++ packages/babel-helpers/src/helpers/unsupportedIterableToArray.ts
@@ -2,9 +2,9 @@
 
 import arrayLikeToArray from "./arrayLikeToArray.ts";
 
-export default function _unsupportedIterableToArray(o, minLen) {
+export default function _unsupportedIterableToArray<T>(o: any, minLen: number) {
   if (!o) return;
-  if (typeof o === "string") return arrayLikeToArray(o, minLen);
+  if (typeof o === "string") return arrayLikeToArray<T>(o, minLen);
   var name = Object.prototype.toString.call(o).slice(8, -1);
   if (name === "Object" && o.constructor) name = o.constructor.name;
   if (name === "Map" || name === "Set") return Array.from(o);
