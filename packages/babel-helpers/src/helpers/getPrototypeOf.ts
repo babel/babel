032842b04d6a355ed215dcd165f578c6b6548bda
Nicolò Ribaudo <hello@nicr.dev>
2024-05-18 13:58:41 +0200

    Suggestion from Sukka

--- packages/babel-helpers/src/helpers/getPrototypeOf.ts
+++ packages/babel-helpers/src/helpers/getPrototypeOf.ts
@@ -1,18 +1,12 @@
 /* @minVersion 7.0.0-beta.0 */
 
-declare global {
-  interface Object {
-    __proto__: object;
-  }
-}
-
 export default function _getPrototypeOf(o: object) {
   // @ts-expect-error explicitly assign to function
   _getPrototypeOf = Object.setPrototypeOf
     ? // @ts-expect-error -- intentionally omitting the argument
       Object.getPrototypeOf.bind(/* undefined */)
     : function _getPrototypeOf<T extends Object>(o: T) {
-        return o.__proto__ || Object.getPrototypeOf(o);
+        return (o as any).__proto__ || Object.getPrototypeOf(o);
       };
   return _getPrototypeOf(o);
 }
