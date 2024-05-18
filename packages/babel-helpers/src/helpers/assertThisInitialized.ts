b6bc364089be79eb0a0bcf8f155c59deb9a51d52
Amjad Yahia <amjadyahia.dev@gmail.com>
2024-05-18 13:08:48 +0300

    Migrate assertThisInitialized helper to typescript

--- packages/babel-helpers/src/helpers/assertThisInitialized.ts
+++ packages/babel-helpers/src/helpers/assertThisInitialized.ts
@@ -1,6 +1,6 @@
 /* @minVersion 7.0.0-beta.0 */
 
-export default function _assertThisInitialized(self) {
+export default function _assertThisInitialized<T>(self: T): T {
   if (self === void 0) {
     throw new ReferenceError(
       "this hasn't been initialised - super() hasn't been called",
