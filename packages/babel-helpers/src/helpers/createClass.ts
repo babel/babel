3b1a3c0be9df65140260a316c1a21adcf948645d
Amjad Yahia Robeen Hassan <amjadyahia.dev@gmail.com>
2024-05-20 12:10:26 +0300

    [helpers TS conversion] classes (#16511)
    Co-authored-by: Sukka <isukkaw@gmail.com>

--- packages/babel-helpers/src/helpers/createClass.ts
+++ packages/babel-helpers/src/helpers/createClass.ts
@@ -1,7 +1,12 @@
 /* @minVersion 7.0.0-beta.0 */
 
-import toPropertyKey from "toPropertyKey";
-function _defineProperties(target, props) {
+import toPropertyKey from "./toPropertyKey.ts";
+
+interface Prop extends PropertyDescriptor {
+  key: PropertyKey;
+}
+
+function _defineProperties(target: object, props: Prop[]): void {
   for (var i = 0; i < props.length; i++) {
     var descriptor = props[i];
     descriptor.enumerable = descriptor.enumerable || false;
@@ -11,7 +16,11 @@ function _defineProperties(target, props) {
   }
 }
 
-export default function _createClass(Constructor, protoProps, staticProps) {
+export default function _createClass<T extends new (...args: any[]) => any>(
+  Constructor: T,
+  protoProps?: Prop[],
+  staticProps?: Prop[],
+): T {
   if (protoProps) _defineProperties(Constructor.prototype, protoProps);
   if (staticProps) _defineProperties(Constructor, staticProps);
   Object.defineProperty(Constructor, "prototype", { writable: false });
