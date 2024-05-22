9b3667a831d87180e9215faa26f0a702a8a5f61f
Amjad Yahia Robeen Hassan <amjadyahia.dev@gmail.com>
2024-05-22 15:20:38 +0300

    [helpers ts conversion] temporal dead zone (#16520)
    Co-authored-by: Nicolò Ribaudo <hello@nicr.dev>

--- packages/babel-helpers/src/helpers/temporalRef.js
+++
@@ -1,8 +0,0 @@
-/* @minVersion 7.0.0-beta.0 */
-
-import undef from "temporalUndefined";
-import err from "tdz";
-
-export default function _temporalRef(val, name) {
-  return val === undef ? err(name) : val;
-}
--- packages/babel-helpers/src/helpers/temporalUndefined.ts
+++ packages/babel-helpers/src/helpers/temporalUndefined.ts
@@ -2,4 +2,4 @@
 
 // This function isn't mean to be called, but to be used as a reference.
 // We can't use a normal object because it isn't hoisted.
-export default function _temporalUndefined() {}
+export default function _temporalUndefined(this: never): void {}
