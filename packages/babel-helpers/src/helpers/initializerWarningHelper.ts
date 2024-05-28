3867132be65fb8f30fdbaa78de4d87e7f330681b
Amjad Yahia <amjadyahia.dev@gmail.com>
2024-05-28 17:35:38 +0300

    refactor: migrate _initializerWarningHelper to typescript

--- packages/babel-helpers/src/helpers/initializerWarningHelper.ts
+++ packages/babel-helpers/src/helpers/initializerWarningHelper.ts
@@ -1,7 +1,10 @@
 /* @minVersion 7.0.0-beta.0 */
 
-// eslint-disable-next-line no-unused-vars
-export default function _initializerWarningHelper(descriptor, context) {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export default function _initializerWarningHelper(
+  descriptor: PropertyDescriptor,
+  context: DecoratorContext,
+): never {
   throw new Error(
     "Decorating class property failed. Please ensure that " +
       "transform-class-properties is enabled and runs after the decorators transform.",
