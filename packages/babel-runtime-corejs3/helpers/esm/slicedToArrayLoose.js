ab465cc0ab11196bcc8ef48068984e16193d8198
Blake Wilson <blake@blake.id>
2024-05-27 21:14:20 -0500

    Delete unused array helpers (#16525)
    * Delete src of unused helpers
    
    * Generate runtime helpers
    
    * make build
    
    * Remove unused corejs3 helpers

--- packages/babel-runtime-corejs3/helpers/esm/slicedToArrayLoose.js
+++
@@ -1,8 +0,0 @@
-import arrayWithHoles from "./arrayWithHoles.js";
-import iterableToArrayLimitLoose from "./iterableToArrayLimitLoose.js";
-import unsupportedIterableToArray from "./unsupportedIterableToArray.js";
-import nonIterableRest from "./nonIterableRest.js";
-function _slicedToArrayLoose(r, e) {
-  return arrayWithHoles(r) || iterableToArrayLimitLoose(r, e) || unsupportedIterableToArray(r, e) || nonIterableRest();
-}
-export { _slicedToArrayLoose as default };
