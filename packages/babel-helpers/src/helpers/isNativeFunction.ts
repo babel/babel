7d9a51fc32b8628c8eb8498f5bdb958d4054f983
Amjad Yahia <amjadyahia.dev@gmail.com>
2024-06-10 18:31:43 +0300

    migrate isNativeFunction to typescript

--- packages/babel-helpers/src/helpers/isNativeFunction.ts
+++ packages/babel-helpers/src/helpers/isNativeFunction.ts
@@ -1,6 +1,6 @@
 /* @minVersion 7.0.0-beta.0 */
 
-export default function _isNativeFunction(fn) {
+export default function _isNativeFunction(fn: unknown): fn is Function {
   // Note: This function returns "true" for core-js functions.
   try {
     return Function.toString.call(fn).indexOf("[native code]") !== -1;
