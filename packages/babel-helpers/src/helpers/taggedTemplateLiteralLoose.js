de089715c8c670ea818916ccecfa585fb791f515
Sukka <isukkaw@gmail.com>
2024-06-19 21:37:51 +0800

    [helpers TS conversion] Others (#16563)
    Co-authored-by: Nicolò Ribaudo <hello@nicr.dev>

--- packages/babel-helpers/src/helpers/taggedTemplateLiteralLoose.js
+++
@@ -1,9 +0,0 @@
-/* @minVersion 7.0.0-beta.0 */
-
-export default function _taggedTemplateLiteralLoose(strings, raw) {
-  if (!raw) {
-    raw = strings.slice(0);
-  }
-  strings.raw = raw;
-  return strings;
-}
