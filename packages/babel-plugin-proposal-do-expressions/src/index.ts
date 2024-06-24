a0ba6f6e41a882029bf28bca843ba894ec5ac127
nerodesu017 <46645625+nerodesu017@users.noreply.github.com>
2024-06-24 21:20:52 +0300

    reverted babel-plugin-proposal-do-expressions back

--- packages/babel-plugin-proposal-do-expressions/src/index.ts
+++ packages/babel-plugin-proposal-do-expressions/src/index.ts
@@ -20,8 +20,7 @@ export default declare(api => {
           if (body.length) {
             path.replaceExpressionWithStatements(body);
           } else {
-            // path.replaceWith(path.scope.buildUndefinedNode());
-            path.remove();
+            path.replaceWith(path.scope.buildUndefinedNode());
           }
         },
       },
