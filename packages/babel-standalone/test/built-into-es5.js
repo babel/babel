0b3e0ffb02a530322d3f7c40d7d5e3490665ef4a
Nicolò Ribaudo <hello@nicr.dev>
2024-07-13 00:25:08 +0200

    Fix test

--- packages/babel-standalone/test/built-into-es5.js
+++ packages/babel-standalone/test/built-into-es5.js
@@ -2,6 +2,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 import fs from "fs";
 import { parse as acornParse } from "acorn";
+import { itGte } from "$repo-utils";
 
 describe("@babel/standalone", () => {
   let babelStandaloneSource;
@@ -21,12 +22,13 @@ describe("@babel/standalone", () => {
     }).not.toThrow();
   });
 
-  it("should not contain extra require() calls", () => {
+  const nodeGte20 = itGte("12.0.0");
+  nodeGte20("should not contain extra require() calls", () => {
     // When the number of `require(` calls changes, make sure that none of
     // them is an actual CommonJS require call. The bundle must be self-contained.
 
     expect(babelStandaloneSource.split(/(?<![."])require\(/g).length - 1).toBe(
-      13,
+      process.env.BABEL_8_BREAKING ? 6 : 13,
     );
   });
 });
