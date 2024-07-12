7e6d33af37858c75d964d4940dae53c92b0bcd83
Nicolò Ribaudo <hello@nicr.dev>
2024-07-13 00:10:51 +0200

    Add a test

--- packages/babel-standalone/test/built-into-es5.js
+++ packages/babel-standalone/test/built-into-es5.js
@@ -4,17 +4,29 @@ import fs from "fs";
 import { parse as acornParse } from "acorn";
 
 describe("@babel/standalone", () => {
-  it("should be built into ES5", () => {
-    const babelStandaloneSource = fs.readFileSync(
-      require.resolve("../babel.js"),
-      { encoding: "utf8" },
-    );
+  let babelStandaloneSource;
 
+  beforeAll(() => {
+    babelStandaloneSource = fs.readFileSync(require.resolve("../babel.js"), {
+      encoding: "utf8",
+    });
+  });
+
+  it("should be built into ES5", () => {
     expect(() => {
       acornParse(babelStandaloneSource, {
         ecmaVersion: 5,
         sourceType: "script",
       });
     }).not.toThrow();
+  });
+
+  it("should not contain extra require() calls", () => {
+    // When the number of `require(` calls changes, make sure that none of
+    // them is an actual CommonJS require call. The bundle must be self-contained.
+
+    expect(babelStandaloneSource.split(/(?<![."])require\(/g).length - 1).toBe(
+      13,
+    );
   });
 });
