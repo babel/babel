d6193cdfd688ff1d375e2b057cde72061ce99d84
Huáng Jùnliàng <jlhwung@gmail.com>
2024-07-19 09:10:04 -0400

    Merge two tests
    The ESLint tests will be run against Node >= 12 only, so nodeGte12NoESM implies itNoESM, which can be merged with itESM into it.

--- eslint/babel-eslint-tests/test/integration/config-files.js
+++ eslint/babel-eslint-tests/test/integration/config-files.js
@@ -1,25 +1,23 @@
 import { ESLint } from "eslint";
-import path from "path";
 import { fileURLToPath } from "url";
-import { itESM, itGteNoESM } from "$repo-utils";
 
 describe("Babel config files", () => {
-  const nodeGte12NoESM = itGteNoESM("12.0.0");
-
-  itESM("works with babel.config.mjs - ESLint " + ESLint.version, async () => {
+  it("works with babel.config.mjs - ESLint " + ESLint.version, async () => {
     if (parseInt(ESLint.version, 10) >= 9) {
       const engine = new ESLint({
         ignore: false,
-        overrideConfigFile: path.resolve(
-          path.dirname(fileURLToPath(import.meta.url)),
-          `../fixtures/mjs-config-file/eslint.config.js`,
+        overrideConfigFile: fileURLToPath(
+          new URL(
+            "../fixtures/mjs-config-file/eslint.config.js",
+            import.meta.url,
+          ),
         ),
       });
+      // eslint-disable-next-line jest/no-conditional-expect
       expect(
         await engine.lintFiles([
-          path.resolve(
-            path.dirname(fileURLToPath(import.meta.url)),
-            `../fixtures/mjs-config-file/a.js`,
+          fileURLToPath(
+            new URL("../fixtures/mjs-config-file/a.js", import.meta.url),
           ),
         ]),
       ).toMatchObject([{ errorCount: 0, messages: [] }]);
@@ -27,50 +25,17 @@ describe("Babel config files", () => {
       const engine = new ESLint({
         ignore: false,
       });
+      // eslint-disable-next-line jest/no-conditional-expect
       expect(
         await engine.lintFiles([
-          path.resolve(
-            path.dirname(fileURLToPath(import.meta.url)),
-            `../fixtures/mjs-config-file-eslint-8/a.js`,
+          fileURLToPath(
+            new URL(
+              "../fixtures/mjs-config-file-eslint-8/a.js",
+              import.meta.url,
+            ),
           ),
         ]),
       ).toMatchObject([{ errorCount: 0, messages: [] }]);
     }
   });
-
-  nodeGte12NoESM(
-    "experimental worker works with babel.config.mjs - ESLint " +
-      ESLint.version,
-    async () => {
-      if (parseInt(ESLint.version, 10) >= 9) {
-        const engine = new ESLint({
-          ignore: false,
-          overrideConfigFile: path.resolve(
-            path.dirname(fileURLToPath(import.meta.url)),
-            `../fixtures/mjs-config-file/eslint.config.js`,
-          ),
-        });
-        expect(
-          await engine.lintFiles([
-            path.resolve(
-              path.dirname(fileURLToPath(import.meta.url)),
-              `../fixtures/mjs-config-file/a.js`,
-            ),
-          ]),
-        ).toMatchObject([{ errorCount: 0, messages: [] }]);
-      } else {
-        const engine = new ESLint({
-          ignore: false,
-        });
-        expect(
-          await engine.lintFiles([
-            path.resolve(
-              path.dirname(fileURLToPath(import.meta.url)),
-              `../fixtures/mjs-config-file-eslint-8/a.js`,
-            ),
-          ]),
-        ).toMatchObject([{ errorCount: 0, messages: [] }]);
-      }
-    },
-  );
 });
