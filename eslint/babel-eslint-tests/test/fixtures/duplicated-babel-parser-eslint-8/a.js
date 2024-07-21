976f30f03b57bd94f428155bb6b7562bea5f5df1
Huáng Jùnliàng <jlhwung@gmail.com>
2024-07-20 20:10:31 -0400

    update duplicated-babel-parser test to ESLint 9

---
+++ eslint/babel-eslint-tests/test/fixtures/duplicated-babel-parser-eslint-8/a.js
@@ -0,0 +1,5 @@
+import { func1 } from "./index.js";
+
+export function five() {
+  return { five: `number(${5})` };
+}
--- eslint/babel-eslint-tests/test/integration/duplicated-babel-parser.js
+++ eslint/babel-eslint-tests/test/integration/duplicated-babel-parser.js
@@ -1,16 +1,47 @@
 import { ESLint } from "eslint";
-import path from "path";
 import { fileURLToPath } from "url";
 
 describe("https://github.com/babel/babel/issues/12985", () => {
-  it("works with different copies of @babel/parser", async () => {
-    const engine = new ESLint({ ignore: false });
-
-    await engine.lintFiles(
-      path.resolve(
-        path.dirname(fileURLToPath(import.meta.url)),
-        `../fixtures/duplicated-babel-parser/a.js`,
-      ),
-    );
-  });
+  it(
+    "works with different copies of @babel/parser - ESLint " + ESLint.version,
+    async () => {
+      if (parseInt(ESLint.version, 10) >= 9) {
+        const engine = new ESLint({
+          ignore: false,
+          overrideConfigFile: fileURLToPath(
+            new URL(
+              "../fixtures/duplicated-babel-parser/eslint.config.cjs",
+              import.meta.url,
+            ),
+          ),
+        });
+        // eslint-disable-next-line jest/no-conditional-expect
+        expect(
+          await engine.lintFiles([
+            fileURLToPath(
+              new URL(
+                "../fixtures/duplicated-babel-parser/a.js",
+                import.meta.url,
+              ),
+            ),
+          ]),
+        ).toMatchObject([{ errorCount: 0, messages: [] }]);
+      } else {
+        const engine = new ESLint({
+          ignore: false,
+        });
+        // eslint-disable-next-line jest/no-conditional-expect
+        expect(
+          await engine.lintFiles([
+            fileURLToPath(
+              new URL(
+                "../fixtures/duplicated-babel-parser-eslint-8/a.js",
+                import.meta.url,
+              ),
+            ),
+          ]),
+        ).toMatchObject([{ errorCount: 0, messages: [] }]);
+      }
+    },
+  );
 });
