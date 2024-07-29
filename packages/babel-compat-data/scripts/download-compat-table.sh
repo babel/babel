b4dd055b4bc7ede06b07706471be049d82765dfc
Huáng Jùnliàng <jlhwung@gmail.com>
2024-07-29 12:37:01 -0400

    update compat-table commit

--- packages/babel-compat-data/scripts/download-compat-table.sh
+++ packages/babel-compat-data/scripts/download-compat-table.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-COMPAT_TABLE_COMMIT=083da1dc9e31faf17fef52825afb1083ee8a24fe
+COMPAT_TABLE_COMMIT=f92024c6d6b7049ae149ba73c89401d1853745db
 GIT_HEAD=build/compat-table/.git/HEAD
 
 if [ -d "build/compat-table" ]; then
@@ -16,5 +16,5 @@ fi
 
 rm -rf build/compat-table
 mkdir -p build
-git clone --single-branch --shallow-since=2020-04-01 https://github.com/kangax/compat-table.git build/compat-table
+git clone --single-branch --shallow-since=2024-04-01 https://github.com/compat-table/compat-table.git build/compat-table
 cd build/compat-table && git checkout -q $COMPAT_TABLE_COMMIT
