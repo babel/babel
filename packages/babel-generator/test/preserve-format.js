9d43cffe4fe8388b7f2e05946c2f504240d81b0f
Nicolò Ribaudo <hello@nicr.dev>
2024-08-02 15:01:36 +0200

    Disable jsescOption in preserve-format tests

--- packages/babel-generator/test/preserve-format.js
+++ packages/babel-generator/test/preserve-format.js
@@ -40,6 +40,7 @@ describe("preserveFormat", () => {
             retainLines: true,
             preserveFormat: true,
             comments: true,
+            jsescOption: null,
             // TODO: Disallow these when preserveFormat is used
             minified: false,
             compact: false,
@@ -82,7 +83,6 @@ const FAILURES = [
   "decoratorsBeforeExport/true-to-false/input.js",
   "decoratorsBeforeExport/true-to-true/input.js",
   "edgecase/large-file-concise/input.js",
-  "escapes/numeric-literals/input.js",
   "flow/array-types/input.js",
   "flow/arrow-functions/input.js",
   "flow/call-properties/input.js",
