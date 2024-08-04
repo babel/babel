c385c5f6f097afd53c9ddedac562abcf09f5180f
Nicolò Ribaudo <hello@nicr.dev>
2024-08-04 23:20:29 +0200

    Revert "test"
    This reverts commit 38e9fb96cf160f88120754219ebc152d7333be86.

--- packages/babel-node/test/split-args.test.js
+++ packages/babel-node/test/split-args.test.js
@@ -1,10 +1,6 @@
-import { readFileSync } from "fs";
 import { splitArgs } from "../lib/split-args.js";
 
 console.log(splitArgs.toString());
-console.log(
-  readFileSync(new URL("../lib/split-args.js", import.meta.url), "utf8"),
-);
 
 describe("babel-node", () => {
   describe("splitArgs", () => {
