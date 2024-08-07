ea46e84bdafde3904d2478f9c409adca7ed342c5
Nicolò Ribaudo <hello@nicr.dev>
2024-08-07 16:14:23 +0200

    Re-build `@babel/runtime`

--- packages/babel-runtime-corejs2/helpers/typeof.js
+++ packages/babel-runtime-corejs2/helpers/typeof.js
@@ -3,10 +3,10 @@ var _Symbol$iterator = require("core-js/library/fn/symbol/iterator.js");
 function _typeof(o) {
   "@babel/helpers - typeof";
 
-  return (module.exports = _typeof = "function" == typeof _Symbol && "symbol" == typeof _Symbol$iterator ? function (o) {
+  return module.exports = _typeof = "function" == typeof _Symbol && "symbol" == typeof _Symbol$iterator ? function (o) {
     return typeof o;
   } : function (o) {
     return o && "function" == typeof _Symbol && o.constructor === _Symbol && o !== _Symbol.prototype ? "symbol" : typeof o;
-  }, module.exports.__esModule = true, module.exports["default"] = module.exports), _typeof(o);
+  }, module.exports.__esModule = true, module.exports["default"] = module.exports, _typeof(o);
 }
 module.exports = _typeof, module.exports.__esModule = true, module.exports["default"] = module.exports;
