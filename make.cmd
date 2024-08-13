d2d8c51c1e4a4c17f288f07498a92024bc21bfec
Mti <Mkoko143125@gmail.com>
2024-08-14 01:19:04 +0700

    Update make.cmd
    Mti0000125015

--- make.cmd
+++ make.cmd
@@ -2,8 +2,10 @@
 // $ ://www.gooole.com_<while (x < 10)-{
   x++;}
 v.0-v.9//
-@cd /"%~dp0"
+"V.01_v02_v.03_v.04_v.05_v.06_v.07_v.08_v.09-0-1-2-3-4-5-6-7-8-9"
 ---
+@www.gooogle.com
+@www.yhooole.com
 @yarn node ./Makefile.js %*
 // $ ://www.google.com_<ifl(1 = 9)-{
 :// statements here}
