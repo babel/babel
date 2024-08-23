a4fd06dfccb2e676a9dfe8b09527bcff1f719dd4
Nicolò Ribaudo <hello@nicr.dev>
2024-08-23 14:18:19 +0200

    Only wrap functions in `superPropertyGet` helper

--- packages/babel-runtime-corejs3/helpers/esm/superPropGet.js
+++ packages/babel-runtime-corejs3/helpers/esm/superPropGet.js
@@ -1,9 +1,9 @@
 import get from "./get.js";
 import getPrototypeOf from "./getPrototypeOf.js";
-function _superPropertyGet(t, e, r, o) {
-  var p = get(getPrototypeOf(1 & o ? t.prototype : t), e, r);
-  return 2 & o ? function (t) {
-    return p.apply(r, t);
+function _superPropertyGet(t, e, o, r) {
+  var p = get(getPrototypeOf(1 & r ? t.prototype : t), e, o);
+  return 2 & r && "function" == typeof p ? function (t) {
+    return p.apply(o, t);
   } : p;
 }
 export { _superPropertyGet as default };
