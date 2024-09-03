81c4160d0684b59c6d15320613a197b06c634f6d
Nicolò Ribaudo <hello@nicr.dev>
2024-09-03 10:57:48 +0200

    Add test

---
+++ packages/babel-plugin-transform-classes/test/fixtures/spec/super-guaranteed/output.js
@@ -0,0 +1,54 @@
+var Base = /*#__PURE__*/babelHelpers.createClass(function Base() {
+  "use strict";
+
+  babelHelpers.classCallCheck(this, Base);
+});
+;
+new (/*#__PURE__*/function (_Base) {
+  "use strict";
+
+  function _class() {
+    var _this;
+    babelHelpers.classCallCheck(this, _class);
+    (_this = babelHelpers.callSuper(this, _class)) || null;
+    return _this;
+  }
+  babelHelpers.inherits(_class, _Base);
+  return babelHelpers.createClass(_class);
+}(Base))();
+new (/*#__PURE__*/function (_Base2) {
+  "use strict";
+
+  function _class2() {
+    var _this2;
+    babelHelpers.classCallCheck(this, _class2);
+    (_this2 = babelHelpers.callSuper(this, _class2))?.();
+    return _this2;
+  }
+  babelHelpers.inherits(_class2, _Base2);
+  return babelHelpers.createClass(_class2);
+}(Base))();
+new (/*#__PURE__*/function (_Base3) {
+  "use strict";
+
+  function _class3() {
+    var _this3;
+    babelHelpers.classCallCheck(this, _class3);
+    (_this3 = babelHelpers.callSuper(this, _class3))?.[null];
+    return _this3;
+  }
+  babelHelpers.inherits(_class3, _Base3);
+  return babelHelpers.createClass(_class3);
+}(Base))();
+new (/*#__PURE__*/function (_Base4) {
+  "use strict";
+
+  function _class4() {
+    var _this4;
+    babelHelpers.classCallCheck(this, _class4);
+    1 + (_this4 = babelHelpers.callSuper(this, _class4));
+    return _this4;
+  }
+  babelHelpers.inherits(_class4, _Base4);
+  return babelHelpers.createClass(_class4);
+}(Base))();
