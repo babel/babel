ca9f7660f379bb872e8314391964f40b4a4b28e9
Nicolò Ribaudo <hello@nicr.dev>
2024-09-06 11:30:15 +0200

    Prettier e2e

--- scripts/integration-tests/e2e-prettier.sh
+++ scripts/integration-tests/e2e-prettier.sh
@@ -41,9 +41,12 @@ if [ "$BABEL_8_BREAKING" = true ] ; then
   rm tests/format/typescript/prettier-ignore/format.test.js
   rm tests/format/typescript/keyword-types/conditional-types.ts
   rm tests/format/typescript/comments/mapped_types.ts
-  # https://github.com/babel/babel/pull/16741
+  # https://github.com/babel/babel/pull/16808
   sed -i 's/"decimal",//' src/language-js/parse/babel.js
   rm tests/format/js/babel-plugins/decimal.js
+  # https://github.com/babel/babel/pull/16808
+  sed -i 's/"importReflection",//' src/language-js/parse/babel.js
+  rm tests/format/js/babel-plugins/import-reflection.js
 fi
 
 #==============================================================================#
