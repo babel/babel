491778448de3e060f7fd2e5805f9dbc9e1fee3b8
dpiercey <dpiercey@ebay.com>
2024-09-24 14:21:50 -0700

    fix: error when startLine and startIndex are used without a startColumn

--- packages/babel-parser/src/options.ts
+++ packages/babel-parser/src/options.ts
@@ -115,12 +115,14 @@ export function getOptions(opts?: Options | null): OptionsWithDefaults {
     if (opts[key] != null) options[key] = opts[key];
   }
 
-  if (
-    options.startIndex > 0 &&
-    options.startLine === 1 &&
-    typeof opts.startColumn !== "number"
-  ) {
-    options.startColumn = options.startIndex;
+  if (options.startIndex > 0 && typeof opts.startColumn !== "number") {
+    if (options.startLine === 1) {
+      options.startColumn = options.startIndex;
+    } else {
+      throw new Error(
+        "When `startIndex` is used with a `startLine` > 1 you must also provide a `startColumn`.",
+      );
+    }
   }
 
   return options;
