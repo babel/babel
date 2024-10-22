af917594e4df3decdde2ce0b1614d607b27367a5
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2024-10-22 21:30:01 +0800

    fix: Accidentally publishing useless files (#16917)

---
+++ scripts/generators/npm-ignore.js
@@ -0,0 +1,49 @@
+import { glob } from "glob";
+import { repoRoot } from "$repo-utils";
+import { existsSync, readFileSync, rmSync, writeFileSync } from "fs";
+import path from "path";
+
+const commonIgnore = [
+  "src",
+  "test",
+  "*.log",
+  "tsconfig.json",
+  "tsconfig.tsbuildinfo",
+];
+
+const extraIgnore = {
+  "babel-core": ["cjs-proxy-dev.cjs"],
+  "babel-plugin-proposal-decorators": ["CONTRIB.md"],
+  "babel-compat-data": ["build"],
+};
+
+const packages = glob
+  .sync("./@(codemods|packages|eslint)/*", {
+    cwd: repoRoot,
+    absolute: true,
+  })
+  .filter(packageDir => {
+    return existsSync(path.join(packageDir, "package.json"));
+  });
+
+for (const packageDir of packages) {
+  const packageJson = JSON.parse(
+    readFileSync(path.join(packageDir, "package.json"), "utf8")
+  );
+
+  if (packageJson.private || packageJson.files) {
+    rmSync(path.join(packageDir, ".npmignore"), {
+      force: true,
+    });
+    continue;
+  }
+
+  const name = path.basename(packageDir);
+  const ignore = commonIgnore.concat(extraIgnore[name] || []);
+
+  if (existsSync(path.join(packageDir, "scripts"))) {
+    ignore.push("scripts");
+  }
+
+  writeFileSync(path.join(packageDir, ".npmignore"), ignore.join("\n") + "\n");
+}
