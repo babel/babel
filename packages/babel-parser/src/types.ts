1903d67f8a0f805149d01d8e9811016d86006bbb
Huáng Jùnliàng <jlhwung@gmail.com>
2024-11-27 11:00:47 -0500

    breaking: create TSEnumBody for Babel 8

--- packages/babel-parser/src/types.ts
+++ packages/babel-parser/src/types.ts
@@ -1768,6 +1768,14 @@ export interface TsEnumDeclaration extends DeclarationBase {
   type: "TSEnumDeclaration";
   const?: true;
   id: Identifier;
+  body: TsEnumBody;
+  /**
+   * @deprecated
+   */
+  members?: TsEnumMember[];
+}
+
+export interface TsEnumBody extends NodeBase {
   members: TsEnumMember[];
 }
 
