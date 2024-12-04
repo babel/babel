bb55483bcbc67b19a59bb75c79019871b659616b
Huáng Jùnliàng <jlhwung@gmail.com>
2024-12-04 11:25:27 -0500

    ci: add ember e2e test

---
+++ scripts/integration-tests/e2e-ember.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#==============================================================================#
+#                                  SETUP                                       #
+#==============================================================================#
+
+# Start in scripts/integration-tests/ even if run from root directory
+cd "$(dirname "$0")" || exit
+root="$PWD"
+
+source utils/local-registry.sh
+source utils/cleanup.sh
+
+# Echo every command being executed
+set -x
+
+# Create example app
+cd /tmp
+npx ember-cli new ember-quickstart --lang en
+cd /tmp/ember-quickstart
+
+#==============================================================================#
+#                                   TEST                                       #
+#==============================================================================#
+
+startLocalRegistry "$root"/verdaccio-config.yml
+
+npm install --ignore-scripts
+
+# Test
+
+npm run build
+
+cleanup
