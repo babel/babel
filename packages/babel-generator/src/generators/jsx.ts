1e7eb19e2441f7b2b93635ec062cc47d045e1571
Huáng Jùnliàng <jlhwung@gmail.com>
2024-12-16 17:25:01 -0500

    fix typing errors

--- packages/babel-generator/src/generators/jsx.ts
+++ packages/babel-generator/src/generators/jsx.ts
@@ -86,10 +86,10 @@ export function JSXOpeningElement(this: Printer, node: t.JSXOpeningElement) {
     //@ts-ignore(Babel 7 vs Babel 8) Babel 8 AST
     this.print(node.typeArguments);
   } else {
-    // @ts-ignore(Babel 7 vs Babel 8) Removed in Babel 8
     if (node.typeArguments) {
       this.print(node.typeArguments); // Flow AST
     }
+    // @ts-ignore(Babel 7 vs Babel 8) Removed in Babel 8
     this.print(node.typeParameters); // Legacy TS AST
   }
 
