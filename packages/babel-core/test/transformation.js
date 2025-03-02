985f051c7c68a09f12ea2089c6cc3d0452b46249
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2025-03-03 02:31:55 +0800

    fix: "Map maximum size exceeded" in deepClone (#17142)

--- packages/babel-core/test/transformation.js
+++ packages/babel-core/test/transformation.js
@@ -1,6 +1,8 @@
 import runner from "@babel/helper-transform-fixture-test-runner";
 import { fileURLToPath } from "url";
 import path from "path";
+import cloneDeep from "../lib/transformation/util/clone-deep.js";
+import { itBabel8 } from "$repo-utils";
 
 (runner.default || runner)(
   path.join(
@@ -9,3 +11,43 @@ import path from "path";
   ),
   "transformation",
 );
+
+describe("util", () => {
+  const clone = cloneDeep.default || cloneDeep;
+  itBabel8("deep clone", () => {
+    const circle = {};
+    circle.circle = circle;
+    const object = {};
+
+    const ast = {
+      type: "Program",
+      extra: {
+        circle,
+        circle2: circle,
+      },
+      object,
+      object2: object,
+    };
+    const cloned = clone(ast);
+
+    expect(cloned.object === ast.object).toBe(false);
+    expect(cloned.object === cloned.object2).toBe(false);
+
+    expect(cloned.extra.circle === ast.extra.circle).toBe(false);
+    expect(cloned.extra.circle === cloned.extra.circle2).toBe(true);
+    expect(cloned.extra.circle === cloned.extra.circle.circle).toBe(true);
+  });
+
+  itBabel8("deep clone circle", () => {
+    const circle = {};
+    circle.circle = circle;
+
+    const ast = {
+      type: "Program",
+      circle,
+    };
+    expect(() => {
+      clone(ast);
+    }).toThrow("Babel-deepClone: Cycles are not allowed in AST");
+  });
+});
