f5fe927e3b0d5cc9d647c76cc7e01c7d4940ee72
Huáng Jùnliàng <jlhwung@gmail.com>
2025-03-12 10:32:14 -0400

    fix: improve duplicate named groups check

---
+++ packages/babel-helper-create-regexp-features-plugin/test/util.skip-bundled.js
@@ -0,0 +1,15 @@
+import { generateRegexpuOptions } from "../lib/util.js";
+
+describe("generateRegexpuOptions", () => {
+  describe("should handle maliciously crafted pattern", () => {
+    it("\\(?<\\(?<...", () => {
+      const pattern = "" + new RegExp("\\(?<".repeat(4e4));
+      const startTime = Date.now();
+      const options = generateRegexpuOptions(pattern, 0);
+      const timeTaken = Date.now() - startTime;
+      console.log(timeTaken);
+      expect(timeTaken).toBeLessThan(2000);
+      expect(options).toHaveProperty("namedGroups", false);
+    });
+  });
+});
