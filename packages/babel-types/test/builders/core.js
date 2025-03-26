0d098d77a83f1ae00a4bd70759427e115bb31f53
Nicolò Ribaudo <hello@nicr.dev>
2025-03-26 15:25:23 +0100

    [babel 8] Deprecate uppercase builders (#17133)
    * [babel 8] Deprecate uppercase builders
    
    * fix
    
    * Add test
    
    * Fix capitalization
    
    * Fix
    
    * Fix tests
    
    * Only warn once
    
    * Fix test

--- packages/babel-types/test/builders/core.js
+++ packages/babel-types/test/builders/core.js
@@ -1,5 +1,6 @@
 import * as t from "../../lib/index.js";
 import { itBabel8 } from "$repo-utils";
+import cp from "child_process";
 
 describe("builders", function () {
   itBabel8("t.numericLiteral expects a non-negative finite value", () => {
@@ -9,7 +10,27 @@ describe("builders", function () {
     expect(() => t.numericLiteral(Infinity)).toThrow();
     expect(() => t.numericLiteral(NaN)).toThrow();
   });
+
   it("t.bigIntLiteral expects a string value", () => {
     expect(t.bigIntLiteral("1")).toHaveProperty("value", "1");
+  });
+
+  it("uppercase builders", () => {
+    expect(t.ThisTypeAnnotation()).toEqual(t.thisTypeAnnotation());
+  });
+
+  itBabel8("uppercase builders are deprecated", async () => {
+    // Spawn a separate process, because the warnig only happens once for all builders
+    // and it relies on globa state to du the deduplication
+
+    const { stderr } = cp.spawn(process.execPath, [
+      "-p",
+      `import(${JSON.stringify(new URL("../../lib/index.js", import.meta.url))}).then(t => t.ThisTypeAnnotation())`,
+    ]);
+
+    const logs = (await stderr.toArray()).join("");
+    expect(logs).toMatch(
+      /Usage of builders starting with an uppercase letter such as `ThisTypeAnnotation` has been deprecated/,
+    );
   });
 });
