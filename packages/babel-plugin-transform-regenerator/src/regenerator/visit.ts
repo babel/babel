c407ea5d2980c4518dd768bdfd9b6ba98230fb48
Mistra1n_Ricko <136186968+Mistra1n@users.noreply.github.com>
2025-03-29 20:30:09 -0400

    Update visit.ts

--- packages/babel-plugin-transform-regenerator/src/regenerator/visit.ts
+++ packages/babel-plugin-transform-regenerator/src/regenerator/visit.ts
@@ -1,38 +1,13 @@
 "use strict";
 
-import { types as t } from "@babel/core";
-import type { NodePath } from "@babel/traverse";
 import assert from "assert";
-import { hoist } from "./hoist";
-import { Emitter } from "./emit";
-import replaceShorthandObjectMethod from "./replaceShorthandObjectMethod";
-import * as util from "./util";
+import { hoist } from "./hoist.ts";
+import { Emitter } from "./emit.ts";
+import replaceShorthandObjectMethod from "./replaceShorthandObjectMethod.ts";
+import * as util from "./util.ts";
 
-interface VisitorState {
-  opts: {
-    asyncGenerators?: boolean;
-    generators?: boolean;
-    async?: boolean;
-  };
-}
-
-interface Context {
-  usesThis: boolean;
-  usesArguments: boolean;
-  getArgsId: () => t.Identifier;
-}
-
-type FunctionNode = t.FunctionDeclaration | t.FunctionExpression | t.Method;
-
-interface MarkInfo {
-  decl?: t.VariableDeclaration;
-  declPath?: NodePath<t.VariableDeclaration>;
-}
-
-const markInfo = new WeakMap<t.Node, MarkInfo>();
-
-export const getVisitor = (_t: typeof t) => ({
-  Method(path: NodePath<t.Method>, state: VisitorState) {
+export const getVisitor = (t: any) => ({
+  Method(path: any, state: any) {
     const node = path.node;
 
     if (!shouldRegenerate(node, state)) return;
@@ -42,24 +17,28 @@ export const getVisitor = (_t: typeof t) => ({
       [],
       t.cloneNode(node.body, false),
       node.generator,
-      node.async
+      node.async,
     );
 
     path
       .get("body")
       .set("body", [t.returnStatement(t.callExpression(container, []))]);
 
+    // Regardless of whether or not the wrapped function is a an async method
+    // or generator the outer function should not be
     node.async = false;
     node.generator = false;
+
+    // Unwrap the wrapper IIFE's environment so super and this and such still work.
     path.get("body.body.0.argument.callee").unwrapFunctionEnvironment();
   },
-
   Function: {
-    exit: util.wrapWithTypes(t, (path: NodePath<t.Function>, state: VisitorState) => {
+    exit: util.wrapWithTypes(t, function (path: any, state: any) {
       let node = path.node;
 
       if (!shouldRegenerate(node, state)) return;
 
+      // if this is an ObjectMethod, we need to convert it to an ObjectProperty
       path = replaceShorthandObjectMethod(path);
       node = path.node;
 
@@ -70,19 +49,29 @@ export const getVisitor = (_t: typeof t) => ({
       const bodyBlockPath = path.get("body");
 
       if (node.async) {
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
         bodyBlockPath.traverse(awaitVisitor);
       }
 
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
       bodyBlockPath.traverse(functionSentVisitor, {
         context: contextId,
       });
 
-      const outerBody: t.Statement[] = [];
-      const innerBody: t.Statement[] = [];
+      const outerBody: any[] = [];
+      const innerBody: any[] = [];
 
-      bodyBlockPath.get("body").forEach((childPath: NodePath<t.Node>) => {
+      bodyBlockPath.get("body").forEach(function (childPath: any) {
         const node = childPath.node;
-        if (t.isExpressionStatement(node) && t.isStringLiteral(node.expression)) {
+        if (
+          t.isExpressionStatement(node) &&
+          t.isStringLiteral(node.expression)
+        ) {
+          // Babylon represents directives like "use strict" as elements
+          // of a bodyBlockPath.node.directives array, but they could just
+          // as easily be represented (by other parsers) as traditional
+          // string-literal-valued expression statements, so we need to
+          // handle that here. (#248)
           outerBody.push(node);
         } else if (node?._blockHoist != null) {
           outerBody.push(node);
@@ -92,34 +81,41 @@ export const getVisitor = (_t: typeof t) => ({
       });
 
       if (outerBody.length > 0) {
+        // Only replace the inner body if we actually hoisted any statements
+        // to the outer body.
         bodyBlockPath.node.body = innerBody;
       }
 
       const outerFnExpr = getOuterFnExpr(path);
+      // Note that getOuterFnExpr has the side-effect of ensuring that the
+      // function has a name (so node.id will always be an Identifier), even
+      // if a temporary name has to be synthesized.
       t.assertIdentifier(node.id);
       const innerFnId = t.identifier(node.id.name + "$");
 
+      // Turn all declarations into vars, and replace the original
+      // declarations with equivalent assignment expressions.
       let vars = hoist(path);
 
-      const context: Context = {
+      const context = {
         usesThis: false,
         usesArguments: false,
-        getArgsId: () => t.cloneNode(argsId),
+        getArgsId: () => t.clone(argsId),
       };
-
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
       path.traverse(argumentsThisVisitor, context);
 
       if (context.usesArguments) {
         vars = vars || t.variableDeclaration("var", []);
         vars.declarations.push(
-          t.variableDeclarator(t.cloneNode(argsId), t.identifier("arguments"))
+          t.variableDeclarator(t.clone(argsId), t.identifier("arguments")),
         );
       }
 
-      const emitter = new Emitter(contextId);
+      const emitter = new (Emitter as any)(contextId);
       emitter.explode(path.get("body"));
 
-      if (vars?.declarations.length > 0) {
+      if (vars && vars.declarations.length > 0) {
         outerBody.push(vars);
       }
 
@@ -129,6 +125,9 @@ export const getVisitor = (_t: typeof t) => ({
       if (node.generator) {
         wrapArgs.push(outerFnExpr);
       } else if (context.usesThis || tryLocsList || node.async) {
+        // Async functions that are not generators don't care about the
+        // outer function because they don't need it to be marked and don't
+        // inherit from its .prototype.
         wrapArgs.push(t.nullLiteral());
       }
       if (context.usesThis) {
@@ -143,24 +142,32 @@ export const getVisitor = (_t: typeof t) => ({
       }
 
       if (node.async) {
+        // Rename any locally declared "Promise" variable,
+        // to use the global one.
         let currentScope = path.scope;
         do {
-          if (currentScope.hasOwnBinding("Promise")) currentScope.rename("Promise");
+          if (currentScope.hasOwnBinding("Promise"))
+            currentScope.rename("Promise");
         } while ((currentScope = currentScope.parent));
+
         wrapArgs.push(t.identifier("Promise"));
       }
 
       const wrapCall = t.callExpression(
         util.runtimeProperty(node.async ? "async" : "wrap"),
-        wrapArgs
+        wrapArgs,
       );
 
       outerBody.push(t.returnStatement(wrapCall));
       node.body = t.blockStatement(outerBody);
-      path.get("body.body").forEach(p => p.scope.registerDeclaration(p));
+      // We injected a few new variable declarations (for every hoisted var),
+      // so we need to add them to the scope.
+      path.get("body.body").forEach((p: any) => p.scope.registerDeclaration(p));
 
       const oldDirectives = bodyBlockPath.node.directives;
       if (oldDirectives) {
+        // Babylon represents directives like "use strict" as elements of
+        // a bodyBlockPath.node.directives array. (#248)
         node.body.directives = oldDirectives;
       }
 
@@ -176,7 +183,7 @@ export const getVisitor = (_t: typeof t) => ({
       if (wasGeneratorFunction && t.isExpression(node)) {
         util.replaceWithOrRemove(
           path,
-          t.callExpression(util.runtimeProperty("mark"), [node])
+          t.callExpression(util.runtimeProperty("mark"), [node]),
         );
         path.addComment("leading", "#__PURE__");
       }
@@ -184,63 +191,89 @@ export const getVisitor = (_t: typeof t) => ({
       const insertedLocs = emitter.getInsertedLocs();
 
       path.traverse({
-        NumericLiteral(path: NodePath<t.NumericLiteral>) {
-          if (insertedLocs.has(path.node)) {
-            path.replaceWith(t.numericLiteral(path.node.value));
+        NumericLiteral(path: any) {
+          if (!insertedLocs.has(path.node)) {
+            return;
           }
+
+          path.replaceWith(t.numericLiteral(path.node.value));
         },
       });
 
+      // Generators are processed in 'exit' handlers so that regenerator only has to run on
+      // an ES5 AST, but that means traversal will not pick up newly inserted references
+      // to things like 'regeneratorRuntime'. To avoid this, we explicitly requeue.
       path.requeue();
     }),
   },
 });
 
-function shouldRegenerate(
-  node: { generator?: boolean; async?: boolean },
-  state: VisitorState
-): boolean {
+// Check if a node should be transformed by regenerator
+function shouldRegenerate(node: any, state: any) {
   if (node.generator) {
-    return node.async
-      ? state.opts.asyncGenerators !== false
-      : state.opts.generators !== false;
+    if (node.async) {
+      // Async generator
+      return state.opts.asyncGenerators !== false;
+    } else {
+      // Plain generator
+      return state.opts.generators !== false;
+    }
+  } else if (node.async) {
+    // Async function
+    return state.opts.async !== false;
+  } else {
+    // Not a generator or async function.
+    return false;
   }
-  return node.async ? state.opts.async !== false : false;
 }
 
-function getOuterFnExpr(funPath: NodePath<FunctionNode>): t.Expression {
+// Given a NodePath for a Function, return an Expression node that can be
+// used to refer reliably to the function object from inside the function.
+// This expression is essentially a replacement for arguments.callee, with
+// the key advantage that it works in strict mode.
+function getOuterFnExpr(funPath: any) {
+  const t = util.getTypes();
   const node = funPath.node;
   t.assertFunction(node);
 
   if (!node.id) {
+    // Default-exported function declarations, and function expressions may not
+    // have a name to reference, so we explicitly add one.
     node.id = funPath.scope.parent.generateUidIdentifier("callee");
   }
 
-  if (node.generator && t.isFunctionDeclaration(node)) {
+  if (
+    node.generator && // Non-generator functions don't need to be marked.
+    t.isFunctionDeclaration(node)
+  ) {
+    // Return the identifier returned by runtime.mark(<node.id>).
     return getMarkedFunctionId(funPath);
   }
 
-  return t.cloneNode(node.id);
+  return t.clone(node.id);
 }
 
-function getMarkInfo(node: t.Node): MarkInfo {
-  let info = markInfo.get(node);
-  if (!info) {
-    info = {};
-    markInfo.set(node, info);
+const markInfo = new WeakMap();
+
+function getMarkInfo(node: any) {
+  if (!markInfo.has(node)) {
+    markInfo.set(node, {});
   }
-  return info;
+  return markInfo.get(node);
 }
 
-function getMarkedFunctionId(funPath: NodePath<t.FunctionDeclaration>): t.Identifier {
+function getMarkedFunctionId(funPath: any) {
+  const t = util.getTypes();
   const node = funPath.node;
   t.assertIdentifier(node.id);
 
-  const blockPath = funPath.findParent((path): path is NodePath<t.BlockStatement | t.Program> =>
-    path.isProgram() || path.isBlockStatement()
-  );
+  const blockPath = funPath.findParent(function (path: any) {
+    return path.isProgram() || path.isBlockStatement();
+  });
 
-  if (!blockPath) return t.cloneNode(node.id);
+  if (!blockPath) {
+    return node.id;
+  }
 
   const block = blockPath.node;
   assert.ok(Array.isArray(block.body));
@@ -249,69 +282,84 @@ function getMarkedFunctionId(funPath: NodePath<t.FunctionDeclaration>): t.Identi
   if (!info.decl) {
     info.decl = t.variableDeclaration("var", []);
     blockPath.unshiftContainer("body", info.decl);
-    info.declPath = blockPath.get("body.0") as NodePath<t.VariableDeclaration>;
+    info.declPath = blockPath.get("body.0");
   }
 
+  assert.strictEqual(info.declPath.node, info.decl);
+
+  // Get a new unique identifier for our marked variable.
   const markedId = blockPath.scope.generateUidIdentifier("marked");
   const markCallExp = t.callExpression(util.runtimeProperty("mark"), [
-    t.cloneNode(node.id)
+    t.clone(node.id),
   ]);
 
-  info.decl.declarations.push(t.variableDeclarator(markedId, markCallExp));
-  const markCallExpPath = info.declPath!.get(
-    `declarations.${info.decl.declarations.length - 1}.init`
-  );
+  const index =
+    info.decl.declarations.push(t.variableDeclarator(markedId, markCallExp)) -
+    1;
+
+  const markCallExpPath = info.declPath.get("declarations." + index + ".init");
+
+  assert.strictEqual(markCallExpPath.node, markCallExp);
+
   markCallExpPath.addComment("leading", "#__PURE__");
 
-  return t.cloneNode(markedId);
+  return t.clone(markedId);
 }
 
 const argumentsThisVisitor = {
-  "FunctionExpression|FunctionDeclaration|Method"(path: NodePath<FunctionNode>) {
+  "FunctionExpression|FunctionDeclaration|Method": function (path: any) {
     path.skip();
   },
 
-  Identifier(path: NodePath<t.Identifier>, state: Context) {
+  Identifier: function (path: any, state: any) {
     if (path.node.name === "arguments" && util.isReference(path)) {
       util.replaceWithOrRemove(path, state.getArgsId());
       state.usesArguments = true;
     }
   },
 
-  ThisExpression(_path: NodePath<t.ThisExpression>, state: Context) {
+  ThisExpression: function (path: any, state: any) {
     state.usesThis = true;
-  }
+  },
 };
 
 const functionSentVisitor = {
-  MetaProperty(path: NodePath<t.MetaProperty>, state: { context: t.Identifier }) {
+  MetaProperty(path: any) {
     const { node } = path;
+
     if (node.meta.name === "function" && node.property.name === "sent") {
+      const t = util.getTypes();
       util.replaceWithOrRemove(
         path,
         t.memberExpression(
-          t.cloneNode(state.context),
-          t.identifier("_sent")
-        )
+          t.clone((this as any).context),
+          t.identifier("_sent"),
+        ),
       );
     }
-  }
+  },
 };
 
 const awaitVisitor = {
-  Function(path: NodePath<FunctionNode>) {
-    path.skip();
+  Function: function (path: any) {
+    path.skip(); // Don't descend into nested function scopes.
   },
 
-  AwaitExpression(path: NodePath<t.AwaitExpression>) {
+  AwaitExpression: function (path: any) {
+    const t = util.getTypes();
+
+    // Convert await expressions to yield expressions.
+    const argument = path.node.argument;
+
+    // Transforming `await x` to `yield regeneratorRuntime.awrap(x)`
+    // causes the argument to be wrapped in such a way that the runtime
+    // can distinguish between awaited and merely yielded values.
     util.replaceWithOrRemove(
       path,
       t.yieldExpression(
-        t.callExpression(util.runtimeProperty("awrap"), [
-          path.node.argument
-        ]),
-        false
-      )
+        t.callExpression(util.runtimeProperty("awrap"), [argument]),
+        false,
+      ),
     );
-  }
+  },
 };
