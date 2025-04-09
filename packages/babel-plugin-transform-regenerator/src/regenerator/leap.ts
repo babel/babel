9c351e5220df774498e26a0fcd337921085f065a
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2025-04-10 01:44:21 +0800

    Use `class` and add type definitions for `regenerator`  (#17220)
    Co-authored-by: Nicolò Ribaudo <nicolo.ribaudo@gmail.com>

--- packages/babel-plugin-transform-regenerator/src/regenerator/leap.ts
+++ packages/babel-plugin-transform-regenerator/src/regenerator/leap.ts
@@ -1,176 +1,141 @@
 import assert from "node:assert";
-import { Emitter } from "./emit.ts";
-import { inherits } from "node:util";
-import { getTypes } from "./util.ts";
+import type { Emitter } from "./emit.ts";
+import type { types as t } from "@babel/core";
 
-function Entry(this: any) {
-  assert.ok(this instanceof Entry);
+export class Entry {}
+
+export class FunctionEntry extends Entry {
+  returnLoc: t.NumericLiteral;
+
+  constructor(returnLoc: t.NumericLiteral) {
+    super();
+    this.returnLoc = returnLoc;
+  }
 }
 
-export function FunctionEntry(this: any, returnLoc: any) {
-  Entry.call(this);
-  getTypes().assertLiteral(returnLoc);
-  this.returnLoc = returnLoc;
+export class LoopEntry extends Entry {
+  breakLoc: t.NumericLiteral;
+  continueLoc: t.NumericLiteral;
+  label: t.Identifier;
+
+  constructor(
+    breakLoc: t.NumericLiteral,
+    continueLoc: t.NumericLiteral,
+    label: t.Identifier = null,
+  ) {
+    super();
+    this.breakLoc = breakLoc;
+    this.continueLoc = continueLoc;
+    this.label = label;
+  }
 }
 
-inherits(FunctionEntry, Entry);
+export class SwitchEntry extends Entry {
+  breakLoc: t.NumericLiteral;
 
-export function LoopEntry(
-  this: any,
-  breakLoc: any,
-  continueLoc: any,
-  label: any,
-) {
-  Entry.call(this);
+  constructor(breakLoc: t.NumericLiteral) {
+    super();
+    this.breakLoc = breakLoc;
+  }
+}
 
-  const t = getTypes();
+export class TryEntry extends Entry {
+  firstLoc: t.NumericLiteral;
+  catchEntry: CatchEntry;
+  finallyEntry: FinallyEntry;
 
-  t.assertLiteral(breakLoc);
-  t.assertLiteral(continueLoc);
+  constructor(
+    firstLoc: t.NumericLiteral,
+    catchEntry: CatchEntry | null = null,
+    finallyEntry: FinallyEntry | null = null,
+  ) {
+    super();
+    assert.ok(catchEntry || finallyEntry);
+    this.firstLoc = firstLoc;
+    this.catchEntry = catchEntry;
+    this.finallyEntry = finallyEntry;
+  }
+}
 
-  if (label) {
-    t.assertIdentifier(label);
-  } else {
-    label = null;
+export class CatchEntry extends Entry {
+  firstLoc: t.NumericLiteral;
+  paramId: t.Identifier;
+
+  constructor(firstLoc: t.NumericLiteral, paramId: t.Identifier) {
+    super();
+    this.firstLoc = firstLoc;
+    this.paramId = paramId;
+  }
+}
+
+export class FinallyEntry extends Entry {
+  firstLoc: t.NumericLiteral;
+  afterLoc: t.NumericLiteral;
+
+  constructor(firstLoc: t.NumericLiteral, afterLoc: t.NumericLiteral) {
+    super();
+    this.firstLoc = firstLoc;
+    this.afterLoc = afterLoc;
+  }
+}
+
+export class LabeledEntry extends Entry {
+  breakLoc: t.NumericLiteral;
+  label: t.Identifier;
+
+  constructor(breakLoc: t.NumericLiteral, label: t.Identifier) {
+    super();
+    this.breakLoc = breakLoc;
+    this.label = label;
+  }
+}
+
+export class LeapManager {
+  emitter: Emitter;
+  entryStack: Entry[];
+
+  constructor(emitter: Emitter) {
+    this.emitter = emitter;
+    this.entryStack = [new FunctionEntry(emitter.finalLoc)];
   }
 
-  this.breakLoc = breakLoc;
-  this.continueLoc = continueLoc;
-  this.label = label;
-}
-
-inherits(LoopEntry, Entry);
-
-export function SwitchEntry(this: any, breakLoc: any) {
-  Entry.call(this);
-  getTypes().assertLiteral(breakLoc);
-  this.breakLoc = breakLoc;
-}
-
-inherits(SwitchEntry, Entry);
-
-export function TryEntry(
-  this: any,
-  firstLoc: any,
-  catchEntry: any,
-  finallyEntry: any,
-) {
-  Entry.call(this);
-
-  const t = getTypes();
-  t.assertLiteral(firstLoc);
-
-  if (catchEntry) {
-    assert.ok(catchEntry instanceof CatchEntry);
-  } else {
-    catchEntry = null;
-  }
-
-  if (finallyEntry) {
-    assert.ok(finallyEntry instanceof FinallyEntry);
-  } else {
-    finallyEntry = null;
-  }
-
-  // Have to have one or the other (or both).
-  assert.ok(catchEntry || finallyEntry);
-
-  this.firstLoc = firstLoc;
-  this.catchEntry = catchEntry;
-  this.finallyEntry = finallyEntry;
-}
-
-inherits(TryEntry, Entry);
-
-export function CatchEntry(this: any, firstLoc: any, paramId: any) {
-  Entry.call(this);
-
-  const t = getTypes();
-
-  t.assertLiteral(firstLoc);
-  t.assertIdentifier(paramId);
-
-  this.firstLoc = firstLoc;
-  this.paramId = paramId;
-}
-
-inherits(CatchEntry, Entry);
-
-export function FinallyEntry(this: any, firstLoc: any, afterLoc: any) {
-  Entry.call(this);
-  const t = getTypes();
-  t.assertLiteral(firstLoc);
-  t.assertLiteral(afterLoc);
-  this.firstLoc = firstLoc;
-  this.afterLoc = afterLoc;
-}
-
-inherits(FinallyEntry, Entry);
-
-export function LabeledEntry(this: any, breakLoc: any, label: any) {
-  Entry.call(this);
-
-  const t = getTypes();
-
-  t.assertLiteral(breakLoc);
-  t.assertIdentifier(label);
-
-  this.breakLoc = breakLoc;
-  this.label = label;
-}
-
-inherits(LabeledEntry, Entry);
-
-export function LeapManager(this: any, emitter: any) {
-  assert.ok(this instanceof LeapManager);
-
-  assert.ok(emitter instanceof Emitter);
-
-  (this as any).emitter = emitter;
-  (this as any).entryStack = [
-    // @ts-expect-error -- TODO (regenerator import)
-    new FunctionEntry(emitter.finalLoc),
-  ];
-}
-
-const LMp = LeapManager.prototype;
-
-LMp.withEntry = function (entry: any, callback: any) {
-  assert.ok(entry instanceof Entry);
-  this.entryStack.push(entry);
-  try {
-    callback.call(this.emitter);
-  } finally {
-    const popped = this.entryStack.pop();
-    assert.strictEqual(popped, entry);
-  }
-};
-
-LMp._findLeapLocation = function (property: any, label: any) {
-  for (let i = this.entryStack.length - 1; i >= 0; --i) {
-    const entry = this.entryStack[i];
-    const loc = entry[property];
-    if (loc) {
-      if (label) {
-        if (entry.label && entry.label.name === label.name) {
-          return loc;
-        }
-      } else if (entry instanceof LabeledEntry) {
-        // Ignore LabeledEntry entries unless we are actually breaking to
-        // a label.
-      } else {
-        return loc;
-      }
+  withEntry(entry: Entry, callback: any) {
+    this.entryStack.push(entry);
+    try {
+      callback.call(this.emitter);
+    } finally {
+      const popped = this.entryStack.pop();
+      assert.strictEqual(popped, entry);
     }
   }
 
-  return null;
-};
+  _findLeapLocation(property: "breakLoc" | "continueLoc", label: t.Identifier) {
+    for (let i = this.entryStack.length - 1; i >= 0; --i) {
+      const entry = this.entryStack[i];
+      // @ts-expect-error Element implicitly has an 'any' type
+      const loc = entry[property] as t.NumericLiteral;
+      if (loc) {
+        if (label) {
+          // @ts-expect-error entry.label may not exist
+          if (entry.label && entry.label.name === label.name) {
+            return loc;
+          }
+        } else if (entry instanceof LabeledEntry) {
+          // Ignore LabeledEntry entries unless we are actually breaking to
+          // a label.
+        } else {
+          return loc;
+        }
+      }
+    }
+    return null;
+  }
 
-LMp.getBreakLoc = function (label: any) {
-  return this._findLeapLocation("breakLoc", label);
-};
+  getBreakLoc(label: t.Identifier) {
+    return this._findLeapLocation("breakLoc", label);
+  }
 
-LMp.getContinueLoc = function (label: any) {
-  return this._findLeapLocation("continueLoc", label);
-};
+  getContinueLoc(label: t.Identifier) {
+    return this._findLeapLocation("continueLoc", label);
+  }
+}
