e913b56034cffbad607c32875d0844d71b1e8613
Nicolò Ribaudo <hello@nicr.dev>
2025-04-12 12:36:46 +0200

    Add test for current behavior

---
+++ packages/babel-plugin-transform-regenerator/test/fixtures/misc/existing-regeneratorRuntime-usage/output.js
@@ -0,0 +1,11 @@
+var _marked = /*#__PURE__*/babelHelpers.regeneratorRuntime().mark(fn);
+function fn() {
+  return babelHelpers.regeneratorRuntime().wrap(function fn$(_context) {
+    while (1) switch (_context.prev = _context.next) {
+      case 0:
+      case "end":
+        return _context.stop();
+    }
+  }, _marked);
+}
+babelHelpers.regeneratorRuntime().isGeneratorFunction(fn);
