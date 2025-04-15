2281c837ec52690e211735b1f7f45cf5d3f9a914
Huáng Jùnliàng <jlhwung@gmail.com>
2025-04-15 15:00:30 -0400

    remove Babel 8 only record/tuple tests

--- packages/babel-plugin-proposal-record-and-tuple/test/fixtures/import-polyfill/default-name/output.mjs
+++
@@ -1,8 +0,0 @@
-import { Record as _Record, Tuple as _Tuple } from "@bloomberg/record-tuple-polyfill";
-const r2 = _Record({
-  a: _Record({
-    b: 456
-  }),
-  e: 789
-});
-const t2 = _Tuple(1, _Tuple(2, 3, _Tuple(4), 5), 6);
