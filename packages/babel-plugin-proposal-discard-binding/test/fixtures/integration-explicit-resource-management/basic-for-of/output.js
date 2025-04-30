543e4ce829d8a4754a562d27ae2dc8ffb04f10fc
Huáng Jùnliàng <jlhwung@gmail.com>
2025-04-30 08:16:50 -0400

    transform discard binding

---
+++ packages/babel-plugin-proposal-discard-binding/test/fixtures/integration-explicit-resource-management/basic-for-of/output.js
@@ -0,0 +1,11 @@
+{
+  for (const _ref of []) try {
+    var _usingCtx = babelHelpers.usingCtx();
+    const _ = _usingCtx.u(_ref);
+    ;
+  } catch (_) {
+    _usingCtx.e = _;
+  } finally {
+    _usingCtx.d();
+  }
+}
---
+++ packages/babel-plugin-proposal-discard-binding/test/fixtures/integration-object-rest-spread/for-binding/output.js
@@ -0,0 +1,15 @@
+let rest1, rest2;
+for (const [, _ref, ...rest2_] = [0, {
+  p: 1,
+  q: 2,
+  r: 3
+}]; true;) {
+  rest1 = rest1_;
+  rest2 = rest2_;
+  break;
+}
+var {
+    p: _
+  } = _ref,
+  rest1_ = babelHelpers.objectWithoutProperties(_ref, ["p"]);
+;
