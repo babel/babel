d3e125bc13b722e6050cedf2099f0d0c9409b567
Huáng Jùnliàng <jlhwung@gmail.com>
2025-05-01 09:53:34 -0400

    perf: enable V8 compile cache for cli

--- packages/babel-cli/bin/babel.js
+++ packages/babel-cli/bin/babel.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+// Enable Node compile cache to speed up initialization
+const mod = require("node:module");
+if (mod.enableCompileCache != null) mod.enableCompileCache();
+
 require("../lib/babel");
