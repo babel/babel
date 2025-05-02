7f16f7e742efc2235e97ed9980ddeacc3eec730a
Huáng Jùnliàng <jlhwung@gmail.com>
2025-05-02 10:40:35 -0400

    add more test cases

---
+++ packages/babel-plugin-proposal-discard-binding/test/fixtures/integration-object-rest-spread/assignment-in-assignment-pattern/output.js
@@ -0,0 +1,13 @@
+var _;
+let rest1, rest2;
+[, _ref = {
+  p: 1,
+  q: 2,
+  r: 3
+}, ...rest2] = [0];
+var _ref2 = _ref;
+({
+  p: _
+} = _ref2);
+rest1 = babelHelpers.objectWithoutProperties(_ref2, ["p"]);
+_ref2;
