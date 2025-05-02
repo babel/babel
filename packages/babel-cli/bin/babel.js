d42174e0a4d42e0377fb39448f2eebec792dca86
Huáng Jùnliàng <jlhwung@gmail.com>
2025-05-02 14:03:25 -0400

    Enable Node compile cache for `@babel/cli` (#17285)
    * add babel-cli benchmark
    
    * perf: enable V8 compile cache for cli
    
    * disable node protocol import for bin/babel
    
    * revert babel-cli package type change

--- packages/babel-cli/bin/babel.js
+++ packages/babel-cli/bin/babel.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+/* eslint-disable unicorn/prefer-node-protocol */
+
+// Enable Node compile cache to speed up initialization
+const mod = require("module");
+if (mod.enableCompileCache != null) mod.enableCompileCache();
 
 require("../lib/babel");
