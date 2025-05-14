348106013cffab44990e8f44922a003d6eb31142
Huáng Jùnliàng <jlhwung@gmail.com>
2025-05-14 13:46:05 -0400

    Update packages/babel-parser/src/parse-error/parse-expression-errors.ts
    Co-authored-by: Nicolò Ribaudo <hello@nicr.dev>

--- packages/babel-parser/src/parse-error/parse-expression-errors.ts
+++ packages/babel-parser/src/parse-error/parse-expression-errors.ts
@@ -4,5 +4,5 @@ export default {
   ParseExpressionEmptyInput:
     "Unexpected parseExpression() input: The input is empty or contains only comments.",
   ParseExpressionExpectsEOF: ({ unexpected }: { unexpected: string }) =>
-    `Unexpected parseExpression() input: The input should contain exactly one expression, but \`${unexpected}\` is seen.`,
+    `Unexpected parseExpression() input: The input should contain exactly one expression, but the first expression is followed by the unexpected character \`${unexpected}\`.`,
 } satisfies ParseErrorTemplates;
