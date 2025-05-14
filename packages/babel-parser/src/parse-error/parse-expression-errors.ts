36f8d66dd3c170a936bcf5ebb71438bcd83c905b
Huáng Jùnliàng <jlhwung@gmail.com>
2025-05-14 13:48:33 -0400

    fix: only print the first redundant character

--- packages/babel-parser/src/parse-error/parse-expression-errors.ts
+++ packages/babel-parser/src/parse-error/parse-expression-errors.ts
@@ -3,6 +3,6 @@ import type { ParseErrorTemplates } from "../parse-error.ts";
 export default {
   ParseExpressionEmptyInput:
     "Unexpected parseExpression() input: The input is empty or contains only comments.",
-  ParseExpressionExpectsEOF: ({ unexpected }: { unexpected: string }) =>
-    `Unexpected parseExpression() input: The input should contain exactly one expression, but the first expression is followed by the unexpected character \`${unexpected}\`.`,
+  ParseExpressionExpectsEOF: ({ unexpected }: { unexpected: number }) =>
+    `Unexpected parseExpression() input: The input should contain exactly one expression, but the first expression is followed by the unexpected character \`${String.fromCodePoint(unexpected)}\`.`,
 } satisfies ParseErrorTemplates;
