c79a32f7185bf3b5ae116e62cfbe3d84d898f6d0
Huáng Jùnliàng <jlhwung@gmail.com>
2025-05-27 09:53:51 -0400

    add new multiple-void test case

---
+++ packages/babel-plugin-proposal-discard-binding/test/fixtures/basic/access-side-effect-multiple-void/output.js
@@ -0,0 +1,24 @@
+class MyObject {
+  pAccessCount = 0;
+  get p() {
+    this.pAccessCount++;
+    return 0;
+  }
+}
+class MyArray {
+  length = 0;
+  *[Symbol.iterator]() {
+    this.length++;
+    yield;
+    this.length++;
+    yield;
+    this.length++;
+    yield new MyObject();
+  }
+}
+var arr = new MyArray();
+var [,, {
+  p: _,
+  p: _2,
+  ...rest1
+}, ...rest2] = arr;
