8e1cec89eed8376a6e9fcb083ca073a66152f498
Nicolò Ribaudo <hello@nicr.dev>
2025-06-03 15:46:16 +0200

    Add test

---
+++ packages/babel-plugin-transform-regenerator/test/fixtures/misc/await-yield/output.js
@@ -0,0 +1,20 @@
+var _marked = /*#__PURE__*/babelHelpers.regenerator().m(f);
+function f() {
+  var res, _t, _t2;
+  return babelHelpers.regeneratorAsyncGen(function (_context) {
+    while (1) switch (_context.n) {
+      case 0:
+        _t = babelHelpers;
+        _context.n = 1;
+        return 1;
+      case 1:
+        _t2 = _context.v;
+        _context.n = 2;
+        return _t.awaitAsyncGenerator.call(_t, _t2);
+      case 2:
+        res = _context.v;
+      case 3:
+        return _context.a(2);
+    }
+  }, _marked, null, null, Promise);
+}
