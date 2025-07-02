e9e912d5ab8f8ce1fbed2795cf7aa2b32bb602c4
Huáng Jùnliàng <jlhwung@gmail.com>
2025-07-02 11:34:46 -0400

    Update docs.js
    Co-authored-by: Brian Ng <bng412@gmail.com>

--- packages/babel-types/scripts/generators/docs.js
+++ packages/babel-types/scripts/generators/docs.js
@@ -98,7 +98,7 @@ const aliasDescriptions = {
   FunctionParent:
     "A cover of AST nodes that start an execution context with new [VariableEnvironment](https://tc39.es/ecma262/#table-additional-state-components-for-ecmascript-code-execution-contexts). In other words, they define the scope of `var` declarations. FunctionParent did not include `Program` since Babel 7.",
   FunctionParameter:
-    "A cover of function parameters. They the elements of [FormalParameterList](https://tc39.es/ecma262/#prod-FormalParameterList).",
+    "A cover of function parameters. They are the elements of [FormalParameterList](https://tc39.es/ecma262/#prod-FormalParameterList).",
   Immutable:
     "A cover of immutable objects and JSX elements. An object is [immutable](https://tc39.es/ecma262/#immutable-prototype-exotic-object) if no other properties can be defined once created.",
   ImportOrExportDeclaration:
