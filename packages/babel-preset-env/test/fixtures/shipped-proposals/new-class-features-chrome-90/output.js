cc3749793693ca6b4769529adf24219e25bd3bae
Nicolò Ribaudo <hello@nicr.dev>
2025-07-18 14:30:57 +0200

    Update fixtures

--- packages/babel-preset-env/test/fixtures/shipped-proposals/new-class-features-chrome-90/output.js
+++ packages/babel-preset-env/test/fixtures/shipped-proposals/new-class-features-chrome-90/output.js
@@ -1,5 +1,7 @@
+var _staticBlock;
 var _fooBrandCheck = /*#__PURE__*/new WeakSet();
 class A {
   #foo = void _fooBrandCheck.add(this);
-  static #_ = register(A, _fooBrandCheck.has(babelHelpers.checkInRHS(A)));
+  static #_ = _staticBlock = () => register(A, _fooBrandCheck.has(babelHelpers.checkInRHS(A)));
 }
+_staticBlock();
