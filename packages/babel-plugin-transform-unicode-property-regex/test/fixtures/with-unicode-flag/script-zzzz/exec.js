15d9f533eb595cf4cda4f0464d8f235a6a4bd083
Huáng Jùnliàng <jlhwung@gmail.com>
2025-09-10 11:39:39 -0400

    improve script unknown test

--- packages/babel-plugin-transform-unicode-property-regex/test/fixtures/with-unicode-flag/script-zzzz/exec.js
+++ packages/babel-plugin-transform-unicode-property-regex/test/fixtures/with-unicode-flag/script-zzzz/exec.js
@@ -1,3 +1,8 @@
+// Surrogate code points
 expect(/\p{Script=Zzzz}/u.test("\uD800")).toBe(true);
 
+// Unassigned code points
 expect(/\p{Script_Extensions=Unknown}/u.test("\u{80000}")).toBe(true);
+
+// Private Use Area code points
+expect(/\p{Script_Extensions=Zzzz}/u.test("\uE800")).toBe(true);
