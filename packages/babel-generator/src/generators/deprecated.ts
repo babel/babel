26f8dae1ee8bc85a4ad4cc0631f92e5ff195be58
Huáng Jùnliàng <jlhwung@gmail.com>
2025-11-19 15:11:22 -0500

    chore: move Record/Tuple generator to deprecated

--- packages/babel-generator/src/generators/deprecated.ts
+++ packages/babel-generator/src/generators/deprecated.ts
@@ -4,7 +4,9 @@ import type * as t from "@babel/types";
 export type DeprecatedBabel7ASTTypes =
   | "Noop"
   | "TSExpressionWithTypeArguments"
-  | "DecimalLiteral";
+  | "DecimalLiteral"
+  | "RecordExpression"
+  | "TupleExpression";
 
 export function addDeprecatedGenerators(PrinterClass: typeof Printer) {
   // Add Babel 7 generator methods that is removed in Babel 8
@@ -28,6 +30,85 @@ export function addDeprecatedGenerators(PrinterClass: typeof Printer) {
           return;
         }
         this.word(node.value + "m");
+      },
+
+      // @ts-ignore(Babel 7 vs Babel 8) - t.RecordExpression only exists in Babel 7
+      RecordExpression(this: Printer, node: t.RecordExpression) {
+        const props = node.properties;
+
+        let startToken;
+        let endToken;
+
+        if (this.format.recordAndTupleSyntaxType === "bar") {
+          startToken = "{|";
+          endToken = "|}";
+        } else if (
+          this.format.recordAndTupleSyntaxType !== "hash" &&
+          this.format.recordAndTupleSyntaxType != null
+        ) {
+          throw new Error(
+            `The "recordAndTupleSyntaxType" generator option must be "bar" or "hash" (${JSON.stringify(
+              this.format.recordAndTupleSyntaxType,
+            )} received).`,
+          );
+        } else {
+          startToken = "#{";
+          endToken = "}";
+        }
+
+        this.token(startToken);
+
+        if (props.length) {
+          this.space();
+          this.printList(
+            props,
+            this.shouldPrintTrailingComma(endToken),
+            true,
+            true,
+          );
+          this.space();
+        }
+        this.token(endToken);
+      },
+
+      // @ts-ignore(Babel 7 vs Babel 8) - t.TupleExpression only exists in Babel 7
+      TupleExpression(this: Printer, node: t.TupleExpression) {
+        const elems = node.elements;
+        const len = elems.length;
+
+        let startToken;
+        let endToken;
+        if (process.env.BABEL_8_BREAKING) {
+          startToken = "#[";
+          endToken = "]";
+        } else {
+          if (this.format.recordAndTupleSyntaxType === "bar") {
+            startToken = "[|";
+            endToken = "|]";
+          } else if (this.format.recordAndTupleSyntaxType === "hash") {
+            startToken = "#[";
+            endToken = "]";
+          } else {
+            throw new Error(
+              `${this.format.recordAndTupleSyntaxType} is not a valid recordAndTuple syntax type`,
+            );
+          }
+        }
+
+        this.token(startToken);
+
+        for (let i = 0; i < elems.length; i++) {
+          const elem = elems[i];
+          if (elem) {
+            if (i > 0) this.space();
+            this.print(elem);
+            if (i < len - 1 || this.shouldPrintTrailingComma(endToken)) {
+              this.token(",", false, i);
+            }
+          }
+        }
+
+        this.token(endToken);
       },
     } satisfies Record<
       DeprecatedBabel7ASTTypes,
