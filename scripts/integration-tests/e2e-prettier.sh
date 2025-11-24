35f0a1b933fa9e2fbf15671ccbb75c18f7bbc302
Nicolò Ribaudo <hello@nicr.dev>
2025-11-24 15:59:47 +0100

    Update prettier e2e test

--- scripts/integration-tests/e2e-prettier.sh
+++ scripts/integration-tests/e2e-prettier.sh
@@ -42,6 +42,8 @@ if [ "$BABEL_8_BREAKING" = true ] ; then
   # Removed in Babel 8
   sed -i 's/"TupleExpression",//g' src/language-js/traverse/visitor-keys.evaluate.js
   sed -i 's/"RecordExpression",//g' src/language-js/traverse/visitor-keys.evaluate.js
+
+  rm tests/format/js/babel-plugins/import-assertions-static.js
 fi
 
 # Test typings for @babel/parser
