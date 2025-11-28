6287e94600ea3ff60450696cbce0bed82435d905
Nicolò Ribaudo <hello@nicr.dev>
2025-11-28 11:44:31 +0100

    Run transform-runtime tests also in Babel 8 (#17615)

--- packages/babel-plugin-transform-runtime/test/fixtures/regression/12863/output.mjs
+++ packages/babel-plugin-transform-runtime/test/fixtures/regression/12863/output.mjs
@@ -1,19 +1,13 @@
-import _Reflect$construct from "@babel/runtime-corejs3/core-js-stable/reflect/construct";
-import _createClass from "@babel/runtime-corejs3/helpers/createClass";
-import _classCallCheck from "@babel/runtime-corejs3/helpers/classCallCheck";
-import _possibleConstructorReturn from "@babel/runtime-corejs3/helpers/possibleConstructorReturn";
-import _getPrototypeOf from "@babel/runtime-corejs3/helpers/getPrototypeOf";
-import _inherits from "@babel/runtime-corejs3/helpers/inherits";
-import _defineProperty from "@babel/runtime-corejs3/helpers/defineProperty";
-import _concatInstanceProperty from "@babel/runtime-corejs3/core-js-stable/instance/concat";
-function _callSuper(t, o, e) { return o = _getPrototypeOf(o), _possibleConstructorReturn(t, _isNativeReflectConstruct() ? _Reflect$construct(o, e || [], _getPrototypeOf(t).constructor) : o.apply(t, e)); }
-function _isNativeReflectConstruct() { try { var t = !Boolean.prototype.valueOf.call(_Reflect$construct(Boolean, [], function () {})); } catch (t) {} return (_isNativeReflectConstruct = function () { return !!t; })(); }
+import _createClass from "@babel/runtime/helpers/createClass";
+import _classCallCheck from "@babel/runtime/helpers/classCallCheck";
+import _callSuper from "@babel/runtime/helpers/callSuper";
+import _inherits from "@babel/runtime/helpers/inherits";
+import _defineProperty from "@babel/runtime/helpers/defineProperty";
 let B = /*#__PURE__*/function (_A) {
   function B(...args) {
-    var _context;
     var _this;
     _classCallCheck(this, B);
-    _this = _callSuper(this, B, _concatInstanceProperty(_context = []).call(_context, args));
+    _this = _callSuper(this, B, [].concat(args));
     _defineProperty(_this, "b", 8);
     return _this;
   }
---
+++ packages/babel-plugin-transform-runtime/test/fixtures/regression/16366-babel-7/output.mjs
@@ -0,0 +1,2 @@
+import _Promise from "@babel/runtime-corejs3/core-js-stable/promise";
+_Promise.all(1, 2, 3);
