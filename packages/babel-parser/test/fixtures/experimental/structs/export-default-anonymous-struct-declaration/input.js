d336b669425f42674ab41bee9ee1887ee819281a
Huáng Jùnliàng <jlhwung@gmail.com>
2025-11-28 14:23:44 -0500

    feat: parse non-shared struct declaration

---
+++ packages/babel-parser/test/fixtures/experimental/structs/export-default-anonymous-struct-declaration/input.js
@@ -0,0 +1,19 @@
+export default struct {
+  property = 0;
+  method() {}
+  #private = "";
+  #PrivateMethod() {}
+
+  static staticProperty;
+  static staticMethod() {}
+  static #staticPrivate;
+  static #staticPrivateMethod() {}
+
+  [computedMethod]() {};
+  [computedProperty];
+
+  get accessor() {}
+  set accessor(value) {}
+
+  static {}
+}
