4baf31a88dc717b16f90f52a37d75255bdad4737
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2026-01-30 02:36:58 +0800

    Add `require` ESM test (#17744)

--- test/esm/index.js
+++
@@ -1,8 +0,0 @@
-import babelRuntimeTestcases from "./babel-runtime.js";
-import babelRuntimeCorejs3Testcases from "./babel-runtime-corejs3.js";
-import testRunner from "./test-runner.js";
-
-(async () => {
-  await testRunner(babelRuntimeTestcases);
-  await testRunner(babelRuntimeCorejs3Testcases);
-})();
