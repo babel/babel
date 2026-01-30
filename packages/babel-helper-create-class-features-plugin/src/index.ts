f3f0844f711efd5c70d63777fb50c5d9deae0c7f
Huáng Jùnliàng <jlhwung@gmail.com>
2026-01-30 10:52:36 -0500

    chore: cleanup optional AST checks

--- packages/babel-helper-create-class-features-plugin/src/index.ts
+++ packages/babel-helper-create-class-features-plugin/src/index.ts
@@ -188,11 +188,7 @@ export function createClassFeaturePlugin({
             constructor = path;
           } else {
             elements.push(path);
-            if (
-              path.isProperty() ||
-              path.isPrivate() ||
-              path.isStaticBlock?.()
-            ) {
+            if (path.isProperty() || path.isPrivate() || path.isStaticBlock()) {
               props.push(path as PropPath);
             }
           }
@@ -264,7 +260,7 @@ export function createClassFeaturePlugin({
             (referenceVisitor, state) => {
               for (const prop of props) {
                 // @ts-expect-error: TS doesn't infer that prop.node is not a StaticBlock
-                if (t.isStaticBlock?.(prop.node) || prop.node.static) continue;
+                if (t.isStaticBlock(prop.node) || prop.node.static) continue;
                 prop.traverse(referenceVisitor, state);
               }
             },
