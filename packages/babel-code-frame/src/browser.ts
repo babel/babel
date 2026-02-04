8ddfe9c590f49cee5d5265e8325070035fda79b1
fisker Cheung <lionkay@gmail.com>
2026-02-04 23:16:10 +0800

    Remove deprecated default export from `@babel/code-frame` (#17772)
    * Remove deprecated default export from `@babel/code-frame`
    
    * Clean
    
    * Update tests

--- packages/babel-code-frame/src/browser.ts
+++ packages/babel-code-frame/src/browser.ts
@@ -4,8 +4,6 @@ import {
   type Options,
 } from "./common.ts";
 
-let deprecationWarningShown = false;
-
 export type { Options };
 
 export function codeFrameColumns(
@@ -14,36 +12,6 @@ export function codeFrameColumns(
   opts: Options = {},
 ): string {
   return _codeFrameColumns(rawLines, loc, opts);
-}
-
-/**
- * Create a code frame, adding line numbers, code highlighting, and pointing to a given position.
- */
-
-export default function (
-  rawLines: string,
-  lineNumber: number,
-  colNumber?: number | null,
-  opts: Options = {},
-): string {
-  if (!deprecationWarningShown) {
-    deprecationWarningShown = true;
-
-    const message =
-      "Passing lineNumber and colNumber is deprecated to @babel/code-frame. Please use `codeFrameColumns`.";
-
-    const deprecationError = new Error(message);
-    deprecationError.name = "DeprecationWarning";
-    console.warn(new Error(message));
-  }
-
-  colNumber = Math.max(colNumber, 0);
-
-  const location: NodeLocation = {
-    start: { column: colNumber, line: lineNumber },
-  };
-
-  return codeFrameColumns(rawLines, location, opts);
 }
 
 export function highlight(code: string) {
