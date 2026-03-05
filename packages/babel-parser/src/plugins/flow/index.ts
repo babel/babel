21f7ec17f4d49b84cc720c6c7e59d25b8e8df5cc
Huáng Jùnliàng <jlhwung@gmail.com>
2026-03-05 10:15:43 -0500

    chore: remove unused ts-expect-error

--- packages/babel-parser/src/plugins/flow/index.ts
+++ packages/babel-parser/src/plugins/flow/index.ts
@@ -363,8 +363,6 @@ export default (superClass: ClassWithMixin<typeof Parser, IJSXParserMixin>) =>
         this.raise(FlowErrors.UnexpectedSpaceBetweenModuloChecks, moduloLoc);
       }
       if (this.eat(tt.parenL)) {
-        // @ts-expect-error Expression can not be assigned to DeclaredPredicate.value
-        // consider refine typings
         (node as Undone<N.DeclaredPredicate>).value = super.parseExpression();
         this.expect(tt.parenR);
         return this.finishNode(node, "DeclaredPredicate");
@@ -886,8 +884,7 @@ export default (superClass: ClassWithMixin<typeof Parser, IJSXParserMixin>) =>
       const ident = this.flowParseTypeAnnotatableIdentifier();
       node.name = ident.name;
       node.variance = variance;
-      // @ts-expect-error migrate to Babel types
-      node.bound = ident.typeAnnotation;
+      node.bound = ident.typeAnnotation as N.TypeAnnotation | null;
 
       if (this.match(tt.eq)) {
         this.eat(tt.eq);
@@ -1230,12 +1227,12 @@ export default (superClass: ClassWithMixin<typeof Parser, IJSXParserMixin>) =>
             this.flowParseObjectTypeCallProperty(node, isStatic),
           );
         } else {
-          let kind = "init";
+          let kind: N.ObjectTypeProperty["kind"] = "init";
 
           if (this.isContextual(tt._get) || this.isContextual(tt._set)) {
             const lookahead = this.lookahead();
             if (tokenIsLiteralPropertyName(lookahead.type)) {
-              kind = this.state.value;
+              kind = this.state.value as "get" | "set";
               this.next();
             }
           }
@@ -1295,7 +1292,7 @@ export default (superClass: ClassWithMixin<typeof Parser, IJSXParserMixin>) =>
       isStatic: boolean,
       protoStartLoc: Position | undefined | null,
       variance: N.Variance | undefined | null,
-      kind: string,
+      kind: N.ObjectTypeProperty["kind"],
       allowSpread: boolean,
       allowInexact: boolean,
     ): N.ObjectTypeProperty | N.ObjectTypeSpreadProperty | null {
@@ -1342,12 +1339,10 @@ export default (superClass: ClassWithMixin<typeof Parser, IJSXParserMixin>) =>
           this.flowParseType();
         return this.finishNode(node, "ObjectTypeSpreadProperty");
       } else {
-        // @ts-expect-error Allow NumericLiteral | StringLiteral for id
         (node as Undone<N.ObjectTypeProperty>).key =
           this.flowParseObjectPropertyKey();
         (node as Undone<N.ObjectTypeProperty>).static = isStatic;
         (node as Undone<N.ObjectTypeProperty>).proto = protoStartLoc != null;
-        // @ts-expect-error Todo: kind is not defined in ObjectTypeProperty
         (node as Undone<N.ObjectTypeProperty>).kind = kind;
 
         let optional = false;
