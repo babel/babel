87a048dbbc13e723f27c6e6f449a83d8701f8f55
Nicolò Ribaudo <hello@nicr.dev>
2026-03-11 16:26:29 +0100

    Add `sourceSelf` option to `@babel/plugin-transform-react-jsx-development` (#17862)

--- packages/babel-plugin-transform-react-jsx-development/test/fixtures/cross-platform/dev-with-source-self.cjs
+++
@@ -1,7 +0,0 @@
-const createPlugin = require("@babel/plugin-transform-react-jsx/lib/create-plugin").default;
-
-module.exports = createPlugin({
-  name: "transform-react-jsx/development",
-  development: true,
-  developmentSourceSelf: true,
-});
