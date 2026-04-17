65b9a5d3cef80acdfdf8d92843ce2c24e598c916
Huáng Jùnliàng <jlhwung@gmail.com>
2026-04-17 13:22:42 -0400

    fixup babel-helpers

--- packages/babel-helpers/test/define-helper.skip-bundled.js
+++ packages/babel-helpers/test/define-helper.skip-bundled.js
@@ -1,7 +1,7 @@
 import testRunner from "@babel/helper-transform-fixture-test-runner";
 
-const fixtures = new URL(
-  "./define-helper-fixtures",
-  import.meta.url,
-).pathname.slice(1);
+let fixtures = new URL("./define-helper-fixtures", import.meta.url).pathname;
+if (process.platform === "win32") {
+  fixtures = fixtures.slice(1);
+}
 testRunner(fixtures, "define-helper");
