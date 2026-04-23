cae576d7017e4f7df3d763da2c18b1e85e8e561d
Huáng Jùnliàng <jlhwung@gmail.com>
2026-04-23 11:36:54 -0400

    refactor(eslint-plugin): use builtin no-undef rule
    The builtin no-undef rule already supports accessor property when babel/eslint-parser is used, thanks to https://github.com/babel/babel/pull/16884.

--- eslint/babel-eslint-plugin/src/rules/no-undef.js
+++ eslint/babel-eslint-plugin/src/rules/no-undef.js
@@ -1,23 +1,5 @@
-import * as ruleComposer from "../rule-composer.js";
 // eslint-disable-next-line import/no-unresolved
 import { builtinRules } from "eslint/use-at-your-own-risk";
 const rule = builtinRules.get("no-undef");
 
-/**
- * Returns whether a node is an accessor field name.
- * @param  {ASTNode}  node CallExpression node
- * @returns {Boolean} Returns true if the node is an accessor field name.
- */
-function isAccessorFieldName(node) {
-  const parent = node.parent;
-  return (
-    parent.type === "AccessorProperty" &&
-    parent.key === node &&
-    !parent.computed
-  );
-}
-
-export default ruleComposer.filterReports(
-  rule,
-  problem => !isAccessorFieldName(problem.node),
-);
+export default rule;
