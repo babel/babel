9eaeb5366a02b5c8011ae2d3e94eb934d9ab5885
Huáng Jùnliàng <jlhwung@gmail.com>
2026-04-28 10:29:56 -0400

    test: add index.tst.ts

---
+++ eslint/babel-eslint-plugin/test/index.tst.ts
@@ -0,0 +1,43 @@
+import { expect, it, describe } from "tstyche";
+import type { Linter, Rule } from "eslint";
+import plugin from "../src/index.ts";
+
+describe("babel-eslint-plugin", () => {
+  it("meta should be valid", () => {
+    expect(plugin.meta).type.toBeAssignableTo<{
+      name: string;
+      version: string;
+    }>();
+  });
+  it("rules should be valid", () => {
+    expect(plugin.rules).type.toBeAssignableTo<
+      Record<string, Rule.RuleModule>
+    >();
+  });
+  it("configs should be valid", () => {
+    expect(plugin.configs).type.toBeAssignableTo<
+      Record<string, Linter.Config>
+    >();
+  });
+  it("configs can be used in a config array", () => {
+    const config = [plugin.configs.recommended, plugin.configs.all];
+    expect(config).type.toBeAssignableTo<Linter.Config[]>();
+  });
+  it("configs can be used in a config array with overrides", () => {
+    const config = [
+      {
+        ...plugin.configs.recommended,
+        files: ["some-file.js"],
+      },
+      {
+        ...plugin.configs.all,
+        files: ["some-other-file.cjs"],
+      },
+      {
+        files: ["one-more-file.mjs"],
+        rules: plugin.configs.recommended.rules,
+      },
+    ];
+    expect(config).type.toBeAssignableTo<Linter.Config[]>();
+  });
+});
--- scripts/generators/tsconfig.ts
+++ scripts/generators/tsconfig.ts
@@ -333,6 +333,8 @@ maybeWriteFile(
           "*.mts",
           "*.ts",
           "scripts/**/*.ts",
+          "codemods/*/test/*.tst.ts",
+          "eslint/*/test/*.tst.ts",
           "packages/*/test/*.tst.ts",
         ],
         exclude: [],
