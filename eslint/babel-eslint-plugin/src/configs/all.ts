078c19548b9202b836f3437e84f7115d7c3f4a83
Huáng Jùnliàng <jlhwung@gmail.com>
2026-04-28 10:14:47 -0400

    feat(eslint-plugin): add configs.all / recommended

---
+++ eslint/babel-eslint-plugin/src/configs/all.ts
@@ -0,0 +1,14 @@
+import type { SeverityString } from "../types";
+
+export const name = "babel/eslint-plugin/all";
+export const rules: Record<string, SeverityString> = {
+  // ESLint core rules
+  "new-cap": "off",
+  "no-undef": "off",
+  "no-unused-expressions": "off",
+
+  // babel-eslint-plugin rules
+  "babel/new-cap": "error",
+  "babel/no-undef": "error",
+  "babel/no-unused-expressions": "error",
+};
--- eslint/babel-eslint-plugin/src/index.ts
+++ eslint/babel-eslint-plugin/src/index.ts
@@ -2,6 +2,9 @@ import type { ESLint, Rule } from "eslint";
 import newCap from "./rules/new-cap.ts";
 import noUndef from "./rules/no-undef.ts";
 import noUnusedExpressions from "./rules/no-unused-expressions.ts";
+import * as recommendedConfig from "./configs/recommended.ts";
+import * as allConfig from "./configs/all.ts";
+import type { ESLint } from "eslint";
 
 const meta: ESLint.Plugin["meta"] = {
   name: PACKAGE_JSON.name,
@@ -22,5 +25,10 @@ const rulesConfig = {
   "no-unused-expressions": "off",
 };
 
+const configs = {
+  recommended: recommendedConfig,
+  all: allConfig,
+};
+
 // @ts-expect-error TODO(Babel 9): Remove rulesConfig in favor of configs
-export default { meta, rules, rulesConfig } satisfies ESLint.Plugin;
+export default { configs, meta, rules, rulesConfig } satisfies ESLint.Plugin;
