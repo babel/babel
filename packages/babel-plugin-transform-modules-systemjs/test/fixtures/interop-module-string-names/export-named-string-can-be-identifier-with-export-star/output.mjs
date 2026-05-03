266387c41a5a142a42cbbe1a2ff16ace9f139d84
Huáng Jùnliàng <jlhwung@gmail.com>
2026-05-03 10:11:50 -0400

    test: add cases

---
+++ packages/babel-plugin-transform-modules-systemjs/test/fixtures/interop-module-string-names/export-named-string-can-be-identifier-with-export-star/output.mjs
@@ -0,0 +1,17 @@
+System.register(["./other.mjs"], function (_export, _context) {
+  "use strict";
+
+  var foo, bar;
+  return {
+    setters: [function (_otherMjs) {
+      var _exportObj = {};
+      for (var _key in _otherMjs) {
+        if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _otherMjs[_key];
+      }
+      _exportObj.defaultExports = _otherMjs.foo;
+      _exportObj.bar = _otherMjs.bar;
+      _export(_exportObj);
+    }],
+    execute: function () {}
+  };
+});
