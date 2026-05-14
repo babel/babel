feba0a3654c596bd369d1ef1231f5d56666d56dc
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-05-14 14:28:19 +0200

    Preserve original identifier names from input sourcemaps (#17992) (#17998)
    * Add failing test case
    
    * Preserve original identifier names from input sourcemaps
    
    * fix stuff

---
+++ packages/babel-core/test/fixtures/transformation/source-maps/input-source-map-external-preserve-original-name/output.mjs
@@ -0,0 +1,14 @@
+import "./chunk-BUSYA2B4.js";
+
+// ../../../../private/var/folders/ws/tlwxqvs5407f21nxy7w8bgr80000gp/T/vite-inline-return-0dbOSY/node_modules/dep/index.js
+var marker$$$ = "react/jsx-runtime";
+var root$$$ = {
+  outer: 1
+};
+function test$$$() {
+  let root2$$$ = {
+    inner: 2
+  };
+  return root2$$$;
+}
+export { marker$$$ as marker, root$$$ as root, test$$$ as test };
