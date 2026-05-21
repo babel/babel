0fb5fbf35c0edd5899a1389663bbb214a339ff80
Huáng Jùnliàng <jlhwung@gmail.com>
2026-05-21 11:06:42 -0400

    feat: add no-empty to recommended presets

--- eslint/babel-eslint-plugin/src/configs/all.ts
+++ eslint/babel-eslint-plugin/src/configs/all.ts
@@ -4,11 +4,13 @@ export const name = "babel/eslint-plugin/all";
 export const rules: Record<string, SeverityString> = {
   // ESLint core rules
   "new-cap": "off",
+  "no-empty": "off",
   "no-undef": "off",
   "no-unused-expressions": "off",
 
   // babel-eslint-plugin rules
   "babel/new-cap": "error",
+  "babel/no-empty": "error",
   "babel/no-undef": "error",
   "babel/no-unused-expressions": "error",
 };
