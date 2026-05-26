a303fa5f8315b752996c0d037b08ddd664a2822f
Huáng Jùnliàng <jlhwung@gmail.com>
2026-05-26 14:35:53 -0400

    test: migrate regenerator test runner to jest

---
+++ packages/babel-plugin-transform-regenerator/test/regenerator-fixtures/jest.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/** @type {import('jest').Config} */
+const config = {
+  // https://github.com/nodejs/node/pull/58588#issuecomment-2961692890
+  testTimeout: 10000,
+  // Disable transform for test files since they are already transformed by the convert step in regenerator.js.
+  transform: {},
+}
+
+module.exports = config;
