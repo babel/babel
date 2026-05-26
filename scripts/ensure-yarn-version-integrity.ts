834b8e9437fa5408fb5c0eca1c33b4f921214d06
Huáng Jùnliàng <jlhwung@gmail.com>
2026-05-26 09:53:34 -0400

    Address review comments

--- scripts/ensure-yarn-version-integrity.ts
+++ scripts/ensure-yarn-version-integrity.ts
@@ -11,11 +11,12 @@ function readPackageManagerSpec() {
 }
 
 const packageManagerSpecRegex = /^yarn@(\d+\.\d+\.\d+)\+sha512\.([a-f0-9]+)$/;
+const yarnPathRegex = /^yarnPath:\s*(\S+)/m;
 
 const match = packageManagerSpecRegex.exec(readPackageManagerSpec());
 if (!match) {
   throw new Error(
-    "Invalid packageManager format in package.json, expected 'yarn@<version>+sha512.<hash>'. Use 'corepack prepare yarn@<version>' to get the correct hash."
+    "Invalid packageManager format in package.json, expected 'yarn@<version>+sha512.<hash>'. Use 'corepack use yarn@<version>' to get the correct hash."
   );
 }
 const [, yarnVersion] = match;
@@ -23,10 +24,22 @@ const [, yarnVersion] = match;
 cp.execSync(`corepack use yarn@${yarnVersion}`);
 
 // Read the package manager spec again to get the expected hash, since corepack might have updated it
-const match2 = packageManagerSpecRegex.exec(readPackageManagerSpec())!;
+const updatedPackageManagerSpec = readPackageManagerSpec();
+const match2 = packageManagerSpecRegex.exec(updatedPackageManagerSpec);
+if (!match2) {
+  throw new Error(
+    `Invalid packageManager format in package.json after corepack update, expected 'yarn@<version>+sha512.<hash>', got '${updatedPackageManagerSpec}'.`
+  );
+}
 const [, , expectedHash] = match2;
 
-const yarnPath = `./.yarn/releases/yarn-${yarnVersion}.cjs`;
+const yarnPath = yarnPathRegex.exec(
+  fs.readFileSync(".yarnrc.yml", "utf-8")
+)?.[1];
+
+if (!yarnPath) {
+  throw new Error("yarnPath is missing in .yarnrc.yml");
+}
 
 const actualHash = createHash("sha512")
   .update(fs.readFileSync(yarnPath))
