965e03c46f15a4e1c2a00c10dd7b43789b727b09
Huáng Jùnliàng <jlhwung@gmail.com>
2026-05-26 15:08:37 -0400

    fix: specify test runner and environment

--- packages/babel-plugin-transform-regenerator/test/regenerator-fixtures/jest.config.js
+++ packages/babel-plugin-transform-regenerator/test/regenerator-fixtures/jest.config.js
@@ -2,10 +2,12 @@
 
 /** @type {import('jest').Config} */
 const config = {
+  runner: "jest-light-runner",
+  testEnvironment: "node",
   // https://github.com/nodejs/node/pull/58588#issuecomment-2961692890
   testTimeout: 10000,
   // Disable transform for test files since they are already transformed by the convert step in regenerator.js.
   transform: {},
-}
+};
 
 module.exports = config;
