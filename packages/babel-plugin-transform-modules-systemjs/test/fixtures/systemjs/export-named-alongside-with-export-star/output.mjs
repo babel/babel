3dd038b2061f200206ded01c368bdef7a787fce1
Huáng Jùnliàng <jlhwung@gmail.com>
2026-05-29 11:16:39 -0400

    fix(systemjs): support __proto__ as an export name

--- packages/babel-plugin-transform-modules-systemjs/test/fixtures/systemjs/export-named-alongside-with-export-star/output.mjs
+++ packages/babel-plugin-transform-modules-systemjs/test/fixtures/systemjs/export-named-alongside-with-export-star/output.mjs
@@ -3,14 +3,18 @@ System.register(["foo", "bar"], function (_export, _context) {
 
   return {
     setters: [function (_foo) {
-      var _exportObj = {};
+      var _exportObj = {
+        __proto__: null
+      };
       for (var _key in _foo) {
         if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _foo[_key];
       }
       _exportObj.default = _foo.default;
       _export(_exportObj);
     }, function (_bar) {
-      var _exportObj2 = {};
+      var _exportObj2 = {
+        __proto__: null
+      };
       for (var _key2 in _bar) {
         if (_key2 !== "default" && _key2 !== "__esModule") _exportObj2[_key2] = _bar[_key2];
       }
