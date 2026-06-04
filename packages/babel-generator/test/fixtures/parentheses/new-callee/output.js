39f38e064a9f13c3de2b25185d2bab91652b3b86
Huáng Jùnliàng <jlhwung@gmail.com>
2026-06-04 13:03:50 -0400

    fix(generator): improve new callee parens check (#18046)

--- packages/babel-generator/test/fixtures/parentheses/new-callee/output.js
+++ packages/babel-generator/test/fixtures/parentheses/new-callee/output.js
@@ -3,11 +3,19 @@ new (f())();
 new (g?.())();
 new (f.g?.h)();
 new (f?.g.h)();
+new (f().g)();
+new (f?.().g)();
+new (f()?.g)();
+new (f?.()?.g)();
+new (f()`g`)();
 new (import("foo"))();
 new (import("foo").bar)();
+new (import("foo")`bar`)();
 new (super())();
 new (super().foo)();
+new (super()`foo`)();
 
 // The following outer parentheses can be removed
 new f[g()]();
-new (f?.g).h();
+new ((f?.g).h)();
+new new f()();
