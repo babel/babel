d903acd007d567262a04cc30a77e1fc0a1df8a8b
Davyd NRB <4661784+retyui@users.noreply.github.com>
2026-06-09 14:07:07 +0200

    Update interopRequireWildcard helper version
    Issue: https://github.com/babel/babel/issues/18050

--- packages/babel-helpers/src/helpers-generated.ts
+++ packages/babel-helpers/src/helpers-generated.ts
@@ -760,7 +760,7 @@ const helpers: Record<string, Helper> = {
   ),
   // size: 510, gzip size: 310
   interopRequireWildcard: helper(
-    "7.14.0",
+    "7.0.0-beta.0",
     'function _interopRequireWildcard(e,t){if("function"==typeof WeakMap)var r=new WeakMap,n=new WeakMap;return(_interopRequireWildcard=function(e,t){if(!t&&e&&e.__esModule)return e;var o,i,f={__proto__:null,default:e};if(null===e||"object"!=typeof e&&"function"!=typeof e)return f;if(o=t?n:r){if(o.has(e))return o.get(e);o.set(e,f)}for(const t in e)"default"!==t&&{}.hasOwnProperty.call(e,t)&&((i=(o=Object.defineProperty)&&Object.getOwnPropertyDescriptor(e,t))&&(i.get||i.set)?o(f,t,i):f[t]=e[t]);return f})(e,t)}',
     {
       globals: ["WeakMap", "Object"],
