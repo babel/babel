ef2a7aadb19360cc06feeb85107b74475cc155ad
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2026-06-14 16:23:29 +0800

    Remove corejs exports for `@babe/runtime-corejs3` (#18036)

--- test/runtime-integration/src/require-corejs.cjs
+++
@@ -1,7 +0,0 @@
-const Set = require("@babel/runtime-corejs3/core-js/set.js");
-
-console.log("=============== require - corejs ====================");
-console.log("typeof Set:", typeof Set);
-
-const arr = Array.from(new Set([1, 2, 3]));
-console.log("arr:", arr.toString());
