12fe960762414b4869ce010382ea2346cf55dfef
Nicolò Ribaudo <hello@nicr.dev>
2026-07-15 17:23:44 +0200

    Update output

--- packages/babel-plugin-proposal-bytes-modules/test/fixtures/browser/with-json-import/output.mjs
+++ packages/babel-plugin-proposal-bytes-modules/test/fixtures/browser/with-json-import/output.mjs
@@ -1,3 +1,2 @@
-const json = await fetch(import.meta.resolve("./x")).then(r => r.json());
-const bytes = await fetch(import.meta.resolve("./x")).then(r => r.bytes()).then(_immutableUint8Array);
+const [bytes, json] = await Promise.all([fetch(import.meta.resolve("./x")).then(r => r.bytes()).then(_immutableUint8Array), fetch(import.meta.resolve("./x")).then(r => r.json())]);
 function _immutableUint8Array(r) { var t = r.buffer; return new Uint8Array(t.transferToImmutable ? t.transferToImmutable() : t); }
