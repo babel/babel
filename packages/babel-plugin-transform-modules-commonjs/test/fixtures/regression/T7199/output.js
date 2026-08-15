984a87af38fe9bf37b0aa5219e81a96e76a00f25
webdevelopersrinu <webdeveloper.srinu9@gmail.com>
2026-08-15 12:07:38 +0530

    Update T7199 fixture output for new _arrayWithHoles helper

--- packages/babel-plugin-transform-modules-commonjs/test/fixtures/regression/T7199/output.js
+++ packages/babel-plugin-transform-modules-commonjs/test/fixtures/regression/T7199/output.js
@@ -7,7 +7,7 @@ function _nonIterableRest() { throw new TypeError("Invalid attempt to destructur
 function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
 function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
 function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
-function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+function _arrayWithHoles(r) { if (Array.isArray(r) && ("undefined" == typeof Symbol || r[Symbol.iterator] === Array.prototype[Symbol.iterator])) return r; }
 const _bar = bar,
   _bar2 = _slicedToArray(_bar, 1),
   x = _bar2[0];
