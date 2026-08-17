a60dffc56a59294ffab8599285c020e8979aad81
webdevelopersrinu <webdeveloper.srinu9@gmail.com>
2026-08-17 20:22:03 +0530

    merge arrayWithHoles into the iterable helpers, read iterator once

--- packages/babel-helpers/src/helpers/iterableToArray.ts
+++ packages/babel-helpers/src/helpers/iterableToArray.ts
@@ -1,10 +1,25 @@
 /* @minVersion 7.0.0-beta.0 */
 
 export default function _iterableToArray<T>(iter: Iterable<T>) {
+  // no == null check: reading the property of null/undefined must throw,
+  // like it does when spreading them
+  var iterator =
+    (typeof Symbol !== "undefined" && iter[Symbol.iterator]) ||
+    (iter as any)["@@iterator"];
+  // fast path, but not for arrays (or proxies) with a custom iterator
   if (
-    (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null) ||
-    (iter as any)["@@iterator"] != null
+    Array.isArray(iter) &&
+    (iterator == null ||
+      iterator ===
+        (typeof Symbol !== "undefined" && Array.prototype[Symbol.iterator]))
   ) {
-    return Array.from(iter);
+    return iter as T[];
   }
+  if (iterator == null) return;
+  // don't use Array.from, it would read the iterator a second time
+  var arr: T[] = [];
+  for (var it = iterator.call(iter), step; !(step = it.next()).done;) {
+    arr.push(step.value);
+  }
+  return arr;
 }
--- packages/babel-runtime-corejs3/helpers/esm/iterableToArray.js
+++ packages/babel-runtime-corejs3/helpers/esm/iterableToArray.js
@@ -1,7 +1,13 @@
 import _Symbol from "core-js-pure/features/symbol/index.js";
 import _getIteratorMethod from "core-js-pure/features/get-iterator-method.js";
-import _Array$from from "core-js-pure/features/array/from.js";
+import _Array$isArray from "core-js-pure/features/array/is-array.js";
+import _pushInstanceProperty from "core-js-pure/features/instance/push.js";
 function _iterableToArray(r) {
-  if ("undefined" != typeof _Symbol && null != _getIteratorMethod(r) || null != r["@@iterator"]) return _Array$from(r);
+  var e = "undefined" != typeof _Symbol && _getIteratorMethod(r) || r["@@iterator"];
+  if (_Array$isArray(r) && (null == e || e === ("undefined" != typeof _Symbol && _getIteratorMethod(Array.prototype)))) return r;
+  if (null != e) {
+    for (var t, o = [], a = e.call(r); !(t = a.next()).done;) _pushInstanceProperty(o).call(o, t.value);
+    return o;
+  }
 }
 export { _iterableToArray as default };
