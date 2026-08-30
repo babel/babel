89d5ea26920a18a8fee6fcb58c7c999ea96e4f5a
mck09 <mck097@gmail.com>
2026-08-31 00:40:36 +0200

    fix: only collapse the empty-object check when it targets the declared binding

--- packages/babel-plugin-transform-destructuring/src/util.ts
+++ packages/babel-plugin-transform-destructuring/src/util.ts
@@ -715,7 +715,12 @@ export function convertVariableDeclaration(
     t.isVariableDeclaration(nodesOut[0]) &&
     t.isExpressionStatement(nodesOut[1]) &&
     t.isCallExpression(nodesOut[1].expression) &&
-    nodesOut[0].declarations.length === 1
+    nodesOut[0].declarations.length === 1 &&
+    t.isIdentifier(nodesOut[0].declarations[0].id) &&
+    nodesOut[1].expression.arguments.length === 1 &&
+    t.isIdentifier(nodesOut[1].expression.arguments[0], {
+      name: nodesOut[0].declarations[0].id.name,
+    })
   ) {
     // This can only happen when we generate this code:
     //    var _ref = DESTRUCTURED_VALUE;
