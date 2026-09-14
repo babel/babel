babel/babel

commit 941cee23d861f9bb8cc0e08dbbb30da5be047190

Improve `@babel/parser` AST types (#16425)

packages/babel-parser/src/plugins/flow/index.ts
@@ -234,7 +234,9 @@ function isEsModuleType(bodyElement: N.Node): boolean {
   );
 }
 
-function hasTypeImportKind(node: N.Node): boolean {
+function hasTypeImportKind(
+  node: Undone<N.ImportSpecifier | N.ImportDeclaration>,
+): boolean {
   return node.importKind === "type" || node.importKind === "typeof";
 }
 
@@ -410,7 +412,7 @@ export default (superClass: typeof Parser) =>
 
       const id = (node.id = this.parseIdentifier());
 
-      const typeNode = this.startNode<N.FlowDeclareFunction>();
+      const typeNode = this.startNode<N.FlowFunctionTypeAnnotation>();
       const typeContainer = this.startNode<N.TypeAnnotation>();
 
       if (this.match(tt.lt)) {
@@ -638,7 +640,7 @@ export default (superClass: typeof Parser) =>
 
           node.type = "Declare" + node.type;
 
-          return node as N.Node;
+          return node as N.FlowDeclareExportDeclaration;
         }
       }
 
@@ -660,7 +662,9 @@ export default (superClass: typeof Parser) =>
       node: Undone<N.FlowDeclareTypeAlias>,
     ): N.FlowDeclareTypeAlias {
       this.next();
-      const finished = this.flowParseTypeAlias(node);
+      const finished = this.flowParseTypeAlias(
+        node,
+      ) as unknown as N.FlowDeclareTypeAlias;
       // Don't do finishNode as we don't want to process comments twice
       finished.type = "DeclareTypeAlias";
       return finished;
@@ -670,7 +674,10 @@ export default (superClass: typeof Parser) =>
       node: Undone<N.FlowDeclareOpaqueType>,
     ): N.FlowDeclareOpaqueType {
       this.next();
-      const finished = this.flowParseOpaqueType(node, true);
+      const finished = this.flowParseOpaqueType(
+        node,
+        true,
+      ) as unknown as N.FlowDeclareOpaqueType;
       // Don't do finishNode as we don't want to process comments twice
       finished.type = "DeclareOpaqueType";
       return finished;
@@ -1388,7 +1395,7 @@ export default (superClass: typeof Parser) =>
     flowParseQualifiedTypeIdentifier(
       startLoc?: Position,
       id?: N.Identifier,
-    ): N.FlowQualifiedTypeIdentifier {
+    ): N.FlowQualifiedTypeIdentifier | N.Identifier {
       startLoc ??= this.state.startLoc;
       let node: N.Identifier | N.FlowQualifiedTypeIdentifier =
         id || this.flowParseRestrictedIdentifier(true);
@@ -1508,9 +1515,9 @@ export default (superClass: typeof Parser) =>
 
     flowIdentToTypeAnnotation(
       startLoc: Position,
-      node: Undone<N.FlowTypeAnnotation>,
+      node: Undone<N.FlowType>,
       id: N.Identifier,
-    ): N.FlowTypeAnnotation {
+    ): N.FlowType {
       switch (id.name) {
         case "any":
           return this.finishNode(node, "AnyTypeAnnotation");
@@ -1543,9 +1550,9 @@ export default (superClass: typeof Parser) =>
     // The parsing of types roughly parallels the parsing of expressions, and
     // primary types are kind of like primary expressions...they're the
     // primitives with which other types are constructed.
-    flowParsePrimaryType(): N.FlowTypeAnnotation {
+    flowParsePrimaryType(): N.FlowType {
       const startLoc = this.state.startLoc;
-      const node = this.startNode<N.FlowTypeAnnotation>();
+      const node = this.startNode<N.FlowOtherTypeAnnotation>();
       let tmp;
       let type;
       let isGroupedType = false;
@@ -1576,7 +1583,8 @@ export default (superClass: typeof Parser) =>
           this.state.noAnonFunctionType = oldNoAnonFunctionType;
           return type;
 
-        case tt.lt:
+        case tt.lt: {
+          const node = this.startNode<N.FlowFunctionTypeAnnotation>();
           node.typeParameters = this.flowParseTypeParameterDeclaration();
           this.expect(tt.parenL);
           tmp = this.flowParseFunctionTypeParams();
@@ -1590,8 +1598,10 @@ export default (superClass: typeof Parser) =>
           node.returnType = this.flowParseType();
 
           return this.finishNode(node, "FunctionTypeAnnotation");
+        }
 
-        case tt.parenL:
+        case tt.parenL: {
+          const node = this.startNode<N.FlowFunctionTypeAnnotation>();
           this.next();
 
           // Check to see if this is actually a grouped type
@@ -1646,6 +1656,7 @@ export default (superClass: typeof Parser) =>
           node.typeParameters = null;
 
           return this.finishNode(node, "FunctionTypeAnnotation");
+        }
 
         case tt.string:
           return this.parseLiteral<N.StringLiteralTypeAnnotation>(
@@ -1740,7 +1751,7 @@ export default (superClass: typeof Parser) =>
       this.unexpected();
     }
 
-    flowParsePostfixType(): N.FlowTypeAnnotation {
+    flowParsePostfixType(): N.FlowType {
       const startLoc = this.state.startLoc;
       let type = this.flowParsePrimaryType();
       let seenOptionalIndexedAccess = false;
@@ -1748,7 +1759,7 @@ export default (superClass: typeof Parser) =>
         (this.match(tt.bracketL) || this.match(tt.questionDot)) &&
         !this.canInsertSemicolon()
       ) {
-        const node = this.startNodeAt<N.FlowTypeAnnotation>(startLoc);
+        const node = this.startNodeAt<N.FlowOtherTypeAnnotation>(startLoc);
         const optional = this.eat(tt.questionDot);
         seenOptionalIndexedAccess = seenOptionalIndexedAccess || optional;
         this.expect(tt.bracketL);
@@ -1779,8 +1790,8 @@ export default (superClass: typeof Parser) =>
       return type;
     }
 
-    flowParsePrefixType(): N.FlowTypeAnnotation {
-      const node = this.startNode<N.FlowTypeAnnotation>();
+    flowParsePrefixType(): N.FlowType {
+      const node = this.startNode<N.FlowOtherTypeAnnotation>();
       if (this.eat(tt.question)) {
         node.typeAnnotation = this.flowParsePrefixType();
         return this.finishNode(node, "NullableTypeAnnotation");
@@ -1789,7 +1800,7 @@ export default (superClass: typeof Parser) =>
       }
     }
 
-    flowParseAnonFunctionWithoutParens(): N.FlowTypeAnnotation {
+    flowParseAnonFunctionWithoutParens(): N.FlowType {
       const param = this.flowParsePrefixType();
       if (!this.state.noAnonFunctionType && this.eat(tt.arrow)) {
         // TODO: This should be a type error. Passing in a SourceLocation, and it expects a Position.
@@ -1806,8 +1817,8 @@ export default (superClass: typeof Parser) =>
       return param;
     }
 
-    flowParseIntersectionType(): N.FlowTypeAnnotation {
-      const node = this.startNode<N.FlowTypeAnnotation>();
+    flowParseIntersectionType(): N.FlowType {
+      const node = this.startNode<N.FlowOtherTypeAnnotation>();
       this.eat(tt.bitwiseAND);
       const type = this.flowParseAnonFunctionWithoutParens();
       node.types = [type];
@@ -1819,8 +1830,8 @@ export default (superClass: typeof Parser) =>
         : this.finishNode(node, "IntersectionTypeAnnotation");
     }
 
-    flowParseUnionType(): N.FlowTypeAnnotation {
-      const node = this.startNode<N.FlowTypeAnnotation>();
+    flowParseUnionType(): N.FlowType {
+      const node = this.startNode<N.FlowOtherTypeAnnotation>();
       this.eat(tt.bitwiseOR);
       const type = this.flowParseIntersectionType();
       node.types = [type];
@@ -1832,7 +1843,7 @@ export default (superClass: typeof Parser) =>
         : this.finishNode(node, "UnionTypeAnnotation");
     }
 
-    flowParseType(): N.FlowTypeAnnotation {
+    flowParseType(): N.FlowType {
       const oldInType = this.state.inType;
       this.state.inType = true;
       const type = this.flowParseUnionType();
@@ -1840,7 +1851,7 @@ export default (superClass: typeof Parser) =>
       return type;
     }
 
-    flowParseTypeOrImplicitInstantiation(): N.FlowTypeAnnotation {
+    flowParseTypeOrImplicitInstantiation(): N.FlowType {
       if (this.state.type === tt.name && this.state.value === "_") {
         const startLoc = this.state.startLoc;
         const node = this.parseIdentifier();
@@ -1869,8 +1880,8 @@ export default (superClass: typeof Parser) =>
       return ident;
     }
 
-    typeCastToParameter(node: N.Node): N.Node {
-      node.expression.typeAnnotation = node.typeAnnotation;
+    typeCastToParameter(node: N.TypeCastExpression): N.Expression {
+      (node.expression as N.Identifier).typeAnnotation = node.typeAnnotation;
 
       this.resetEndLocation(node.expression, node.typeAnnotation.loc.end);
 
@@ -2017,7 +2028,7 @@ export default (superClass: typeof Parser) =>
       return super.isExportDefaultSpecifier();
     }
 
-    parseExportDefaultExpression(): N.Expression | N.Declaration {
+    parseExportDefaultExpression() {
       if (this.shouldParseEnums() && this.isContextual(tt._enum)) {
         const node = this.startNode();
         this.next();
@@ -2134,13 +2145,14 @@ export default (superClass: typeof Parser) =>
 
       while (stack.length !== 0) {
         const node = stack.pop();
-        if (node.type === "ArrowFunctionExpression") {
+        if (
+          node.type === "ArrowFunctionExpression" &&
+          node.body.type !== "BlockStatement"
+        ) {
           if (node.typeParameters || !node.returnType) {
             // This is an arrow expression without ambiguity, so check its parameters
-            // @ts-expect-error: refine typings
             this.finishArrowValidation(node);
           } else {
-            // @ts-expect-error: refine typings
             arrows.push(node);
           }
           stack.push(node.body);
@@ -2191,14 +2203,14 @@ export default (superClass: typeof Parser) =>
       return result;
     }
 
-    parseParenItem(
-      node: N.Expression,
-
+    parseParenItem<T extends N.Expression | N.RestElement | N.SpreadElement>(
+      node: T,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       startLoc: Position,
-    ): N.Expression {
-      node = super.parseParenItem(node, startLoc);
+    ): T | N.TypeCastExpression | N.TsTypeCastExpression {
+      const newNode = super.parseParenItem(node, startLoc);
       if (this.eat(tt.question)) {
-        node.optional = true;
+        (newNode as N.Identifier).optional = true;
         // Include questionmark in location of node
         // Don't use this.finishNode() as otherwise we might process comments twice and
         // include already consumed parens
@@ -2207,13 +2219,13 @@ export default (superClass: typeof Parser) =>
 
       if (this.match(tt.colon)) {
         const typeCastNode = this.startNodeAt<N.TypeCastExpression>(startLoc);
-        typeCastNode.expression = node;
+        typeCastNode.expression = newNode as N.Expression;
         typeCastNode.typeAnnotation = this.flowParseTypeAnnotation();
 
         return this.finishNode(typeCastNode, "TypeCastExpression");
       }
 
-      return node;
+      return newNode;
     }
 
     assertModuleNodeAllowed(node: N.Node) {
@@ -2278,11 +2290,15 @@ export default (superClass: typeof Parser) =>
       }
     }
 
-    eatExportStar(node: N.Node): boolean {
+    eatExportStar(
+      node: Undone<N.Node>,
+    ): node is Undone<N.ExportNamedDeclaration | N.ExportAllDeclaration> {
       if (super.eatExportStar(node)) return true;
 
       if (this.isContextual(tt._type) && this.lookahead().type === tt.star) {
-        node.exportKind = "type";
+        (
+          node as Undone<N.ExportNamedDeclaration | N.ExportAllDeclaration>
+        ).exportKind = "type";
         this.next();
         this.next();
         return true;
@@ -2291,7 +2307,9 @@ export default (superClass: typeof Parser) =>
       return false;
     }
 
-    maybeParseExportNamespaceSpecifier(node: N.Node): boolean {
+    maybeParseExportNamespaceSpecifier(
+      node: Undone<N.ExportNamedDeclaration | N.ExportAllDeclaration>,
+    ): node is Undone<N.ExportNamedDeclaration> {
       const { startLoc } = this.state;
       const hasNamespace = super.maybeParseExportNamespaceSpecifier(node);
       if (hasNamespace && node.exportKind === "type") {
@@ -2400,7 +2418,7 @@ export default (superClass: typeof Parser) =>
         node.type === "AssignmentExpression" &&
         node.left.type === "TypeCastExpression"
       ) {
-        node.left = this.typeCastToParameter(node.left);
+        node.left = this.typeCastToParameter(node.left) as N.Assignable;
       }
       super.toAssignable(node, isLHS);
     }
@@ -2423,9 +2441,14 @@ export default (superClass: typeof Parser) =>
     // this is a list of nodes, from something like a call expression, we need to filter the
     // type casts that we've found that are illegal in this context
     toReferencedList(
-      exprList: ReadonlyArray<N.Expression | undefined | null>,
+      exprList:
+        | ReadonlyArray<N.Expression | N.SpreadElement>
+        | ReadonlyArray<N.Expression | N.RestElement>,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       isParenthesizedExpr?: boolean,
-    ): ReadonlyArray<N.Expression | undefined | null> {
+    ):
+      | ReadonlyArray<N.Expression | N.SpreadElement>
+      | ReadonlyArray<N.Expression | N.RestElement> {
       for (let i = 0; i < exprList.length; i++) {
         const expr = exprList[i];
         if (
@@ -2610,15 +2633,15 @@ export default (superClass: typeof Parser) =>
     }
 
     // parse type parameters for object method shorthand
-    parseObjPropValue(
-      prop: Undone<N.ObjectMethod | N.ObjectProperty>,
+    parseObjPropValue<T extends N.ObjectMember>(
+      prop: Undone<T>,
       startLoc: Position | undefined | null,
       isGenerator: boolean,
       isAsync: boolean,
       isPattern: boolean,
       isAccessor: boolean,
       refExpressionErrors?: ExpressionErrors | null,
-    ): N.ObjectMethod | N.ObjectProperty {
+    ): T {
       if ((prop as any).variance) {
         this.unexpected((prop as any).variance.loc.start);
       }
@@ -2944,7 +2967,7 @@ export default (superClass: typeof Parser) =>
 
         let typeParameters: N.TypeParameterDeclaration;
 
-        const arrow = this.tryParse(abort => {
+        const arrow = this.tryParse((abort: () => never) => {
           typeParameters = this.flowParseTypeParameterDeclaration();
 
           const arrowExpression = this.forwardNoArrowParamsConversionAt(
@@ -2986,7 +3009,6 @@ export default (superClass: typeof Parser) =>
 
         if (
           arrow.node &&
-          // @ts-expect-error: refine tryParse typings
           this.maybeUnwrapTypeCastExpression(arrow.node).type ===
             "ArrowFunctionExpression"
         ) {
@@ -3000,7 +3022,6 @@ export default (superClass: typeof Parser) =>
                 typeParameters,
               );
             }
-            // @ts-expect-error: refine tryParse typings
             return arrow.node;
           }
 
@@ -3045,7 +3066,7 @@ export default (superClass: typeof Parser) =>
     ): Undone<N.ArrowFunctionExpression> | undefined | null {
       if (this.match(tt.colon)) {
         // @ts-expect-error todo(flow->ts)
-        const result = this.tryParse(() => {
+        const result = this.tryParse<N.TypeAnnotation>(() => {
           const oldNoAnonFunctionType = this.state.noAnonFunctionType;
           this.state.noAnonFunctionType = true;
 
@@ -3085,11 +3106,13 @@ export default (superClass: typeof Parser) =>
     }
 
     setArrowFunctionParameters(
-      node: N.ArrowFunctionExpression,
-      params: N.Pattern[],
+      node: Undone<N.ArrowFunctionExpression>,
+      params:
+        | Array<N.Expression | N.SpreadElement>
+        | Array<N.Expression | N.RestElement>,
     ): void {
       if (this.state.noArrowParamsConversionAt.indexOf(node.start) !== -1) {
-        node.params = params;
+        node.params = params as N.ArrowFunctionExpression["params"];
       } else {
         super.setArrowFunctionParameters(node, params);
       }
@@ -3455,7 +3478,7 @@ export default (superClass: typeof Parser) =>
     }
 
     flowEnumMemberRaw(): {
-      id: N.Node;
+      id: N.Identifier;
       init: EnumMemberInit;
     } {
       const loc = this.state.startLoc;
@@ -3488,10 +3511,22 @@ export default (superClass: typeof Parser) =>
       explicitType: EnumExplicitType;
     }): {
       members: {
-        booleanMembers: Array<N.Node>;
-        numberMembers: Array<N.Node>;
-        stringMembers: Array<N.Node>;
-        defaultedMembers: Array<N.Node>;
+        booleanMembers: Extract<
+          N.FlowEnumMember,
+          { type: "EnumBooleanMember" }
+        >[];
+        numberMembers: Extract<
+          N.FlowEnumMember,
+          { type: "EnumNumberMember" }
+        >[];
+        stringMembers: Extract<
+          N.FlowEnumMember,
+          { type: "EnumStringMember" }
+        >[];
+        defaultedMembers: Extract<
+          N.FlowEnumMember,
+          { type: "EnumDefaultedMember" }
+        >[];
       };
       hasUnknownMembers: boolean;
     } {
@@ -3512,7 +3547,7 @@ export default (superClass: typeof Parser) =>
           hasUnknownMembers = true;
           break;
         }
-        const memberNode = this.startNode<N.Node>();
+        const memberNode = this.startNode<N.FlowEnumMember>();
         const { id, init } = this.flowEnumMemberRaw();
         const memberName = id.name;
         if (memberName === "") {
@@ -3657,7 +3692,7 @@ export default (superClass: typeof Parser) =>
       return value;
     }
 
-    flowEnumBody(node: Undone<N.Node>, id: N.Node): N.Node {
+    flowEnumBody(node: Undone<N.FlowEnumBody>, id: N.Identifier): N.Node {
       const enumName = id.name;
       const nameLoc = id.loc.start;
       const explicitType = this.flowEnumParseExplicitType({ enumName });
@@ -3746,7 +3781,9 @@ export default (superClass: typeof Parser) =>
       }
     }
 
-    flowParseEnumDeclaration(node: Undone<N.Node>): N.Node {
+    flowParseEnumDeclaration(
+      node: Undone<N.FlowEnumDeclaration>,
+    ): N.FlowEnumDeclaration {
       const id = this.parseIdentifier();
       node.id = id;
       node.body = this.flowEnumBody(this.startNode(), id);

packages/babel-parser/src/plugins/jsx/index.ts
@@ -46,7 +46,7 @@ const JsxErrors = ParseErrorEnum`jsx`({
 
 /* eslint-disable sort-keys */
 
-function isFragment(object?: N.JSXElement | null): boolean {
+function isFragment(object?: N.JSXTag | null): object is N.JSXFragmentTag {
   return object
     ? object.type === "JSXOpeningFragment" ||
         object.type === "JSXClosingFragment"
@@ -75,6 +75,7 @@ function getQualifiedJSXName(
   }
 
   // istanbul ignore next
+  // @ts-expect-error - object is 'never'
   throw new Error("Node had unexpected type: " + object.type);
 }
 
@@ -275,7 +276,7 @@ export default (superClass: typeof Parser) =>
 
     // Parse namespaced identifier.
 
-    jsxParseNamespacedName(): N.JSXNamespacedName {
+    jsxParseNamespacedName(): N.JSXNamespacedName | N.JSXIdentifier {
       const startLoc = this.state.startLoc;
       const name = this.jsxParseIdentifier();
       if (!this.eat(tt.colon)) return name;
@@ -294,7 +295,8 @@ export default (superClass: typeof Parser) =>
       | N.JSXNamespacedName
       | N.JSXMemberExpression {
       const startLoc = this.state.startLoc;
-      let node = this.jsxParseNamespacedName();
+      let node: N.JSXIdentifier | N.JSXNamespacedName | N.JSXMemberExpression =
+        this.jsxParseNamespacedName();
       if (node.type === "JSXNamespacedName") {
         return node;
       }
@@ -309,7 +311,10 @@ export default (superClass: typeof Parser) =>
 
     // Parses any type of JSX attribute value.
 
-    jsxParseAttributeValue(): N.Expression {
+    jsxParseAttributeValue():
+      | N.JSXExpressionContainer
+      | N.JSXElement
+      | N.StringLiteral {
       let node;
       switch (this.state.type) {
         case tt.braceL:
@@ -324,7 +329,7 @@ export default (superClass: typeof Parser) =>
 
         case tt.jsxTagStart:
         case tt.string:
-          return this.parseExprAtom();
+          return this.parseExprAtom() as N.JSXElement | N.StringLiteral;
 
         default:
           throw this.raise(JsxErrors.UnsupportedJsxValue, this.state.startLoc);
@@ -386,7 +391,7 @@ export default (superClass: typeof Parser) =>
 
     // Parses following JSX attribute name-value pair.
 
-    jsxParseAttribute(): N.JSXAttribute {
+    jsxParseAttribute(): N.JSXAttribute | N.JSXSpreadAttribute {
       const node = this.startNode<N.JSXAttribute | N.JSXSpreadAttribute>();
       if (this.match(tt.braceL)) {
         this.setContext(tc.brace);
@@ -405,12 +410,13 @@ export default (superClass: typeof Parser) =>
 
     // Parses JSX opening tag starting after "<".
 
-    jsxParseOpeningElementAt(startLoc: Position): N.JSXOpeningElement {
+    jsxParseOpeningElementAt(
+      startLoc: Position,
+    ): N.JSXOpeningElement | N.JSXOpeningFragment {
       const node = this.startNodeAt<N.JSXOpeningElement | N.JSXOpeningFragment>(
         startLoc,
       );
       if (this.eat(tt.jsxTagEnd)) {
-        // @ts-expect-error migrate to Babel types
         return this.finishNode(node, "JSXOpeningFragment");
       }
       node.name = this.jsxParseElementName();
@@ -422,7 +428,7 @@ export default (superClass: typeof Parser) =>
     jsxParseOpeningElementAfterName(
       node: Undone<N.JSXOpeningElement>,
     ): N.JSXOpeningElement {
-      const attributes: N.JSXAttribute[] = [];
+      const attributes: (N.JSXAttribute | N.JSXSpreadAttribute)[] = [];
       while (!this.match(tt.slash) && !this.match(tt.jsxTagEnd)) {
         attributes.push(this.jsxParseAttribute());
       }
@@ -434,7 +440,9 @@ export default (superClass: typeof Parser) =>
 
     // Parses JSX closing tag starting after "</".
 
-    jsxParseClosingElementAt(startLoc: Position): N.JSXClosingElement {
+    jsxParseClosingElementAt(
+      startLoc: Position,
+    ): N.JSXClosingElement | N.JSXClosingFragment {
       const node = this.startNodeAt<N.JSXClosingFragment | N.JSXClosingElement>(
         startLoc,
       );
@@ -449,8 +457,8 @@ export default (superClass: typeof Parser) =>
     // Parses entire JSX element, including it"s opening tag
     // (starting after "<"), attributes, contents and closing tag.
 
-    jsxParseElementAt(startLoc: Position): N.JSXElement {
-      const node = this.startNodeAt<N.JSXElement>(startLoc);
+    jsxParseElementAt(startLoc: Position): N.JSXElement | N.JSXFragment {
+      const node = this.startNodeAt<N.JSXElement | N.JSXFragment>(startLoc);
       const children = [];
       const openingElement = this.jsxParseOpeningElementAt(startLoc);
       let closingElement = null;
@@ -469,7 +477,7 @@ export default (superClass: typeof Parser) =>
               break;
 
             case tt.jsxText:
-              children.push(this.parseExprAtom());
+              children.push(this.parseLiteral(this.state.value, "JSXText"));
               break;
 
             case tt.braceL: {
@@ -538,7 +546,7 @@ export default (superClass: typeof Parser) =>
 
     // Parses entire JSX element from current position.
 
-    jsxParseElement(): N.JSXElement {
+    jsxParseElement(): N.JSXElement | N.JSXFragment {
       const startLoc = this.state.startLoc;
       this.next();
       return this.jsxParseElementAt(startLoc);
@@ -554,9 +562,7 @@ export default (superClass: typeof Parser) =>
     // ==================================
 
     parseExprAtom(refExpressionErrors?: ExpressionErrors | null): N.Expression {
-      if (this.match(tt.jsxText)) {
-        return this.parseLiteral(this.state.value, "JSXText");
-      } else if (this.match(tt.jsxTagStart)) {
+      if (this.match(tt.jsxTagStart)) {
         return this.jsxParseElement();
       } else if (
         this.match(tt.lt) &&

commit 3e7af35ffd1880c5560eccff341cb9a3bfc15a39

fix(parser): convert params of block-bodied arrows in flow conditional consequents
Disclaimer: This commit description was generated by an LLM (Claude).

With the `flow` plugin, the params of an arrow at the start of a conditional consequent are not converted to patterns when the arrow is parsed, so that `a ? (b): c => d` can be disambiguated afterwards in `parseConditional`. `getArrowLikeExpressions` then converts them, but it skipped arrows with a block body entirely. As a result:

- The params of a block-bodied arrow in that position stayed as expressions: `ObjectExpression`, `ArrayExpression`, `AssignmentExpression` and `TypeCastExpression`, and `SpreadElement` for async arrows. For example, in `a ? async ({ b }) => {} : c` the param is an `ObjectExpression`. The params were also never validated, so `a ? ({ b, b }) => {} : c` and `a ? async (...b = 1) => {} : c` parsed without errors, while `a ? ({ b, b }) => b : c` throws `Argument name clash`.
- A block-bodied arrow with a return type was never considered for re-parsing, so `a ? (b) : c => {};` threw `Unexpected token, expected ":"`, although `a ? (b) : c => d;` parses as `a ? b : (c => d)`.

This is a regression from #16425, first released in 7.24.5: it added the `node.body.type !== "BlockStatement"` condition to narrow `node.body` for `stack.push(node.body)`, which also stopped block-bodied arrows from being validated or collected. 7.24.4 handles all of the cases above.

Block-bodied arrows are now validated, or collected as possibly ambiguous, in the same way as arrows with an expression body. Only the traversal into the body is still limited to expression bodies, since a block body can't contain the conditional's `:`.

Found via https://github.com/react/metro/issues/1725, where the `SpreadElement` param crashed a downstream Babel plugin.

packages/babel-parser/src/plugins/flow/index.ts
@@ -2200,17 +2200,16 @@ export default (superClass: ClassWithMixin<typeof Parser, IJSXParserMixin>) =>
 
       while (stack.length !== 0) {
         const node = stack.pop()!;
-        if (
-          node.type === "ArrowFunctionExpression" &&
-          node.body.type !== "BlockStatement"
-        ) {
+        if (node.type === "ArrowFunctionExpression") {
           if (node.typeParameters || !node.returnType) {
             // This is an arrow expression without ambiguity, so check its parameters
             this.finishArrowValidation(node);
           } else {
             arrows.push(node);
           }
-          stack.push(node.body);
+          if (node.body.type !== "BlockStatement") {
+            stack.push(node.body);
+          }
         } else if (node.type === "ConditionalExpression") {
           stack.push(node.consequent);
           stack.push(node.alternate);

packages/babel-parser/test/fixtures/flow/arrows-in-ternaries/block-body-invalid-params/input.js
@@ -1,0 +1,1 @@
+a ? ({ b, b }) => {} : c;

packages/babel-parser/test/fixtures/flow/arrows-in-ternaries/block-body-params/input.js
@@ -1,0 +1,3 @@
+a ? ({ b }) => {} : c;
+a ? async (...b) => {} : c;
+a ? ({ b }): T => {} : c;

packages/babel-parser/test/fixtures/flow/arrows-in-ternaries/block-body-return-type-like/input.js
@@ -1,0 +1,1 @@
+a ? (b) : c => {}; // a ? b : (c => {})